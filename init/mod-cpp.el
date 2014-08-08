3714e41b474740d897f755392b28f323162756ea
Henrik Lissner <henrik@lissner.net>
2014-08-07 23:35:16 -0400

    mod-cpp: header + clang completion

--- init/mod-cpp.el
+++ init/mod-cpp.el
@@ -1,5 +1,14 @@
 
-(use-package c++-mode :mode "\\.h\\'")
+(mapc 'my/install-package '(auto-complete-clang auto-complete-c-headers))
+
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (use-package auto-complete-clang)
+            (use-package auto-complete-c-headers)
+
+            (setq ac-sources '(ac-source-clang ac-source-c-headers ac-source-yasnippet))))
 
 ;; TODO Better SFML & build settings
 
