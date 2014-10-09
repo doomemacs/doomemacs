4349a31b5a0b6baaee660db17fc53645c360f1d8
Henrik Lissner <henrik@lissner.net>
2014-10-09 18:38:35 -0400

    Simpler csharp support (back to Monodevelop)

--- init/init-csharp.el
+++ init/init-csharp.el
@@ -1,23 +1,22 @@
 (provide 'init-csharp)
 
+;; (use-package omnisharp :defer t
+;;   :config
+;;   (progn
+;;     (setq omnisharp-server-executable-path "~/Dropbox/projects/lib/Omnisharp/server/OmniSharp/bin/Debug/OmniSharp.exe")
+;;     (use-package company
+;;       :config
+;;       (add-to-list 'company-backends 'company-omnisharp))))
+
+(use-package csharp-mode :mode "\\.cs$")
+;; (use-package csharp-mode
+;;   :mode "\\.cs$"
+;;   :config
+;;   (imap csharp-mode-map (kbd "C-SPC") 'omnisharp-auto-complete))
+;;   :init
+;;   (add-hook! 'csharp-mode-hook (omnisharp-mode t) (flycheck-mode t)))
+
+;; unity shaders
+(use-package shaderlab-mode :mode "\\.shader$")
+
 ;; TODO Make more Unity-friendly
-
-(use-package omnisharp :defer t
-  :config
-  (progn
-    ;; (setq flycheck-idle-change-delay 2)
-    (setq omnisharp-server-executable-path "/Users/hlissner/Omni/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp.exe")))
-
-(use-package shaderlab-mode :defer t
-  :mode "\\.shader")
-
-(use-package csharp-mode
-  :mode "\\.cs\\'"
-  :config
-  (progn
-    (diminish 'abbrev-mode)
-    (imap csharp-mode-map (kbd "C-SPC") 'omnisharp-auto-complete))
-  :init
-  (progn
-    (setq csharp-want-imenu nil)
-    (add-hook! 'csharp-mode-hook (omnisharp-mode t) (flycheck-mode t))))
