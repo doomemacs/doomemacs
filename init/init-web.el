1d9ebc68ee6ca0e1ba24d74d6d8a5d67cee1e5fe
Henrik Lissner <henrik@lissner.net>
2015-03-17 02:36:28 -0400

    Move smartparens config for web-mode into init-web.el

--- init/init-web.el
+++ init/init-web.el
@@ -22,6 +22,17 @@
           web-mode-script-padding        2
           web-mode-block-padding         2)
 
+    (after "smartparens"
+      (add-hook! 'web-mode-hook (setq web-mode-enable-auto-pairing nil))
+
+      (defun sp-web-mode-is-code-context (id action context)
+      (when (and (eq action 'insert)
+                 (not (or (get-text-property (point) 'part-side)
+                          (get-text-property (point) 'block-side))))
+        t))
+
+      (sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context)))
+
     (after "web-beautify"
       (add-hook! 'web-mode-hook (setenv "jsbeautify_indent_size" "4"))
       (bind 'motion web-mode-map "gQ" 'web-beautify-html))
