d2f680d4058b8463aca0a3a57dbb7b85a7c7f43d
Henrik Lissner <henrik@lissner.net>
2015-07-17 12:08:37 +0200

    Clean up scss-mode init

--- modules/module-sass.el
+++ modules/module-sass.el
@@ -9,12 +9,10 @@
   :mode "\\.scss$"
   :init
   (add-hook! scss-mode 'narf|enable-tab-width-2)
+  (add-hook! scss-mode 'hs-minor-mode)
   (setq-default css-indent-offset 2)
   (setq scss-compile-at-save nil)
   :config
-  ;; Syntax coloring breaks on consecutive loads for some reason. This fixes that:
-  (add-hook! scss-mode 'css-mode)
-
   (after! web-beautify
     (add-hook! scss-mode (setenv "jsbeautify_indent_size" "2"))
     (bind! :map scss-mode-map :m "gQ" 'web-beautify-css))
