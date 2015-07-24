6046400fd68a8ed8ce4fe269abbffbf786c6673b
Henrik Lissner <henrik@lissner.net>
2015-07-24 13:06:57 +0200

    Replace ,; and ,: in scss-mode with helm-css-scss

--- modules/module-sass.el
+++ modules/module-sass.el
@@ -13,6 +13,10 @@
   (setq-default css-indent-offset 2)
   (setq scss-compile-at-save nil)
   :config
+  (bind! :map scss-mode-map
+         :n ",;" 'helm-css-scss
+         :n ",:" 'helm-css-scss-multi)
+
   (after! web-beautify
     (add-hook! scss-mode (setenv "jsbeautify_indent_size" "2"))
     (bind! :map scss-mode-map :m "gQ" 'web-beautify-css))
