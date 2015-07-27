7d2163c6b238f8c8dccacb5d2051601a16c3f3b4
Henrik Lissner <henrik@lissner.net>
2015-07-27 15:11:47 +0200

    Smarter scss toggle block/inline defun

--- modules/lib/defuns-scss.el
+++ modules/lib/defuns-scss.el
@@ -6,16 +6,26 @@
   (interactive)
   (save-excursion
     (let* ((bounds (ignore-errors (evil-a-curly)))
-           (beg (car bounds))
-           (end (cadr bounds)))
-      (goto-char beg)
+           beg end)
       (unless bounds
         (user-error "No block found"))
+      (setq beg (car bounds))
+      (setq end (cadr bounds))
       (if (= (line-number-at-pos beg) (line-number-at-pos end))
-          (progn (replace-regexp ";[\s\t]*" ";\n" nil beg end)
-                 (save-excursion (goto-char (1+ beg)) (insert "\n")
-                                 (evil-indent beg (+ 2 end))))
-        (evil-join beg end)))))
+          (save-excursion
+            (goto-char (1+ beg)) (insert "\n")
+            (unless (string-match ";[\s\t]*}$" (buffer-substring-no-properties beg (1+ end)))
+              (goto-char end) (insert "\n"))
+            (replace-regexp ";[\s\t]*" ";\n" nil beg (1+ end))
+            (setq end (cadr (evil-a-curly)))
+            (evil-indent beg end)
+            (delete-trailing-whitespace beg end))
+        (goto-char beg)
+        (evil-join beg end)
+        (goto-char (1+ beg))
+        (just-one-space)
+        (goto-char (cadr (evil-inner-curly)))
+        (just-one-space)))))
 
 (provide 'defuns-scss)
 ;;; defuns-scss.el ends here
