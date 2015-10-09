54f7e84b686f730c735633f47ba37858160ccc0e
Henrik Lissner <henrik@lissner.net>
2015-10-09 18:00:39 -0400

    Add hack-mode

--- modules/module-php.el
+++ modules/module-php.el
@@ -14,5 +14,7 @@
   (require 'php-refactor-mode)
   (add-hook! php-mode '(turn-on-eldoc-mode emr-initialize php-refactor-mode)))
 
+(use-package hack-mode :mode "\\.hh$")
+
 (provide 'module-php)
 ;;; module-php.el ends here
