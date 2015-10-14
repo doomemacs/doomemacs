c7f2f10b18e3953abe9d08e5da7df57efd2a025c
Henrik Lissner <henrik@lissner.net>
2015-10-14 02:13:42 -0400

    Fix evil-complete-(next|previous) regression

--- core/lib/defuns-company.el
+++ core/lib/defuns-company.el
@@ -1,13 +1,13 @@
 ;;; defuns-company.el
 
 ;;;###autoload
-(defun narf/company-evil-complete-next ()
+(defun narf/company-evil-complete-next (&optional arg)
   (call-interactively 'company-dabbrev)
   (if (eq company-candidates-length 1)
       (company-complete)))
 
 ;;;###autoload
-(defun narf/company-evil-complete-previous ()
+(defun narf/company-evil-complete-previous (&optional arg)
   (let ((company-selection-wrap-around t))
     (call-interactively 'company-dabbrev)
     (if (eq company-candidates-length 1)
