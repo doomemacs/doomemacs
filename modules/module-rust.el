38b251c954e170c1462449770bf52a29780724bd
Henrik Lissner <henrik@lissner.net>
2015-10-18 02:28:36 -0400

    Fix racer-find-definition

--- modules/module-rust.el
+++ modules/module-rust.el
@@ -12,16 +12,17 @@
   :mode "\\.rs$"
   :config
   (define-builder! rust-mode "cargo run" "Cargo.toml")
+  (define-builder! toml-mode "cargo run" "Cargo.toml")
 
   (use-package flycheck-rust
     :config (add-hook! rust-mode 'flycheck-mode))
 
   (use-package racer
-    :if (! (file-exists-p "/usr/local/bin/racer"))
+    :if (file-exists-p "/usr/local/bin/racer")
     :config
     (setq racer-cmd "/usr/local/bin/racer"
           racer-rust-src-path "~/Dropbox/lib/rust/src/")
-    (bind! :m "gd" 'racer-find-definition)
+    (bind! :map rust-mode-map :m "gd" 'racer-find-definition)
 
     (add-hook! rust-mode '(racer-mode eldoc-mode flycheck-rust-setup))
     (define-company-backend! rust-mode (racer))))
