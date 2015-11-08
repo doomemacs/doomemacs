860ca2d96c3401c4b86e801868d0c941909a9ba9
Henrik Lissner <henrik@lissner.net>
2015-11-08 17:58:18 -0500

    what-face now ignores hl-line-mode face

--- core/lib/defuns-debug.el
+++ core/lib/defuns-debug.el
@@ -4,9 +4,12 @@
 (defun what-face (pos)
   "Tells you the name of the face (point) is on."
   (interactive "d")
-  (let ((face (or (get-char-property (point) 'read-face-name)
-                  (get-char-property (point) 'face))))
-    (if face (message "Face: %s" face) (message "No face at %d" pos))))
+  (let ((hl-line-p hl-line-mode))
+    (if hl-line-p (hl-line-mode -1))
+    (let ((face (or (get-char-property (point) 'read-face-name)
+                    (get-char-property (point) 'face))))
+      (if face (message "Face: %s" face) (message "No face at %d" pos)))
+    (if hl-line-p (hl-line-mode 1))))
 
 ;;;###autoload
 (defun what-col ()
