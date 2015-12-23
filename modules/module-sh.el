3ffde24ca9e20737b498eb7b38e3c4983ecd8a1b
Henrik Lissner <henrik@lissner.net>
2015-12-23 03:43:30 -0500

    sh-mode: extract mode associations; use electric-indent

--- modules/module-sh.el
+++ modules/module-sh.el
@@ -1,12 +1,18 @@
 ;;; module-sh.el --- description
 
+(associate! sh-mode :match "/\\.dotfiles/aliases$")
+(associate! sh-mode :match "/\\.?z\\(sh/.*\\|profile\\|login\\|logout\\|shrc\\|shenv\\)$")
+(associate! sh-mode :match "/\\.?bash\\(/.*\\|rc\\|_profile\\)$")
 (after! sh-script
   ;; [pedantry intensifies]
   (defadvice sh-mode (after sh-mode-rename-modeline activate)
     (setq mode-name "sh"))
 
+  (setq sh-indent-after-continuation 'always)
+
   (define-repl! sh-mode narf/inf-shell)
   (add-hook! sh-mode 'flycheck-mode)
+  (add-hook! sh-mode 'electric-indent-local-mode)
 
   ;; Fontify variables in strings
   (add-hook 'sh-mode-hook 'narf|sh-extra-font-lock-activate))
