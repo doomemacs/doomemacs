bb168a672c86e8acff283d4605cb3d9fd2035cf5
Henrik Lissner <henrik@lissner.net>
2016-01-23 20:51:07 -0500

    Refactor generate-autoloads.el

--- scripts/generate-autoloads.el
+++ scripts/generate-autoloads.el
@@ -1,15 +1,14 @@
 #!emacs --script
-
 (load (concat user-emacs-directory "init-packages.el"))
 
-(require 'f)
-(setq generated-autoload-file (concat narf-core-dir "autoloads.el"))
-(when (f-exists? generated-autoload-file)
-  (delete-file generated-autoload-file))
-
-(let ((dirs (list (concat narf-core-dir "lib")
-                  (concat narf-modules-dir "lib")
-                  narf-contrib-dir)))
-  (apply #'update-directory-autoloads dirs)
-  (message "Scanned: %s" dirs))
+(let ((generated-autoload-file (concat narf-core-dir "autoloads.el")))
+  (when (file-exists-p generated-autoload-file)
+    (delete-file generated-autoload-file))
+  (mapc (lambda (dir)
+          (update-directory-autoloads dir)
+          (message "Scanned: %s" dir))
+        (list (concat narf-core-dir "lib")
+              (concat narf-modules-dir "lib")
+              narf-contrib-dir))
+  (message "Done!"))
 
