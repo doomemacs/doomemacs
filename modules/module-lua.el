9cdebff9885216e66cdb3e0dbb72c745fd21dfd8
Henrik Lissner <henrik@lissner.net>
2016-01-29 07:04:14 -0500

    Move hammerspoon-mode into module-lua

--- modules/module-lua.el
+++ modules/module-lua.el
@@ -19,8 +19,19 @@
   :lighter " ♥"
   :keymap (make-sparse-keymap)
   (add-yas-minor-mode! 'love-mode))
-(associate! love-mode :files ("main.lua") :in (lua-mode))
+(associate! love-mode :files ("main.lua" "conf.lua"))
 (define-builder! love-mode "open -a love.app '%s'" "main.lua")
+
+(define-minor-mode hammerspoon-mode
+  :init-value nil
+  :lighter " hammer"
+  :keymap (let ((map ))
+            (map! :map map
+                  (:localleader
+                    :n "b" (λ! (shell-command (format "open hammerspoon://reload")))))
+            map)
+  (add-yas-minor-mode! 'hammerspoon-mode))
+(associate! hammerspoon-mode :match "/\\.?hammerspoon/.+\\.lua$")
 
 (provide 'module-lua)
 ;;; module-lua.el ends here
