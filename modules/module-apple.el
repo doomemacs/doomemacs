bafc3e29610cb4bcea7f4219722f1bb00af7e9a1
Henrik Lissner <henrik@lissner.net>
2016-02-18 05:05:52 -0500

    lb6-mode: make M-b and :build reinstall the action

--- modules/module-apple.el
+++ modules/module-apple.el
@@ -16,18 +16,14 @@
 (defun narf-lb6-reload ()
   (interactive)
   (let ((dir (f-traverse-upwards (lambda (f) (string-suffix-p ".lbaction" f)))))
-    (shell-command (format "open %s" dir))))
+    (shell-command (format "open '%s'" dir))))
 
 (define-minor-mode lb6-mode
   "Launchbar development mode."
   :init-value nil
   :lighter    " lb6"
-  :keymap (let ((map (make-sparse-keymap)))
-            (map! :map map
-                  (:localleader
-                    :n "b" 'narf-lb6-reload))
-            map)
   (add-yas-minor-mode! 'lb6-mode))
+(define-builder! lb6-mode narf-lb6-reload)
 (associate! lb6-mode :match "\\.lb\\(action\\|ext\\)/.+$")
 
 (provide 'module-apple)
