05868a88d50f4d411fe36b6431223fde3c66f4fc
Henrik Lissner <henrik@lissner.net>
2016-03-26 01:21:29 -0400

    Update generate-autoloads script: new path convention

--- scripts/generate-autoloads.el
+++ scripts/generate-autoloads.el
@@ -1,11 +1,11 @@
 #!emacs --script
 (load (concat user-emacs-directory "bootstrap.el"))
 
-(let ((generated-autoload-file (concat narf-core-dir "autoloads.el")))
+(let ((generated-autoload-file (concat narf-core-dir "/autoloads.el")))
   (when (file-exists-p generated-autoload-file)
     (delete-file generated-autoload-file))
   (mapc (lambda (dir)
-          (update-directory-autoloads (concat dir "defuns"))
+          (update-directory-autoloads (concat dir "/defuns"))
           (message "Scanned: %s" dir))
         (list narf-core-dir narf-modules-dir))
   (message "Done!"))
