124452adbba458b0d164198660aeadcf5d4d40ac
Henrik Lissner <henrik@lissner.net>
2016-03-29 18:11:56 -0400

    haskell: integrate repl

--- modules/module-haskell.el
+++ modules/module-haskell.el
@@ -3,8 +3,17 @@
 (use-package haskell-mode
   :mode "\\.hs$"
   :config
+  (define-repl! haskell-mode switch-to-haskell)
+
   ;; haskell-mode complains that this function isn't defined, and it isn't!
-  (defun haskell-mode-after-save-handler ()))
+  (defun haskell-mode-after-save-handler ())
+
+  (use-package inf-haskell
+    :commands (switch-to-haskell)
+    :init (evil-set-initial-state 'inferior-haskell-mode 'emacs)
+    :config
+    (map! :map inf-haskell-mode-map
+          "ESC ESC" 'narf/popup-close)))
 
 (provide 'module-haskell)
 ;;; module-haskell.el ends here
