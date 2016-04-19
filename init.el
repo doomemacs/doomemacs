dbcea69193ace8f20c1f40a42803c2b5ebdb99a6
Henrik Lissner <henrik@lissner.net>
2016-04-19 03:23:49 -0400

    v0.9.9 bump

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;;
 ;; Author:  Henrik Lissner <henrik@lissner.net>
 ;; URL:     https://github.com/hlissner/.emacs.d
-;; Version: 0.9.8
+;; Version: 0.9.9
 ;;
 ;;; Are you pondering what I'm pondering, Pinky?
 ;;
