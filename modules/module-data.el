e5703106060e62189181c7971621f3fe5e5d2f2e
Henrik Lissner <henrik@lissner.net>
2016-04-23 22:10:17 -0400

    Add module-data.el

---
+++ modules/module-data.el
@@ -0,0 +1,32 @@
+;;; module-data.el
+
+(associate! conf-mode :match "/\\.?editorconfig$")
+(associate! nxml-mode :match "\\.plist$")
+
+(use-package toml-mode :mode "\\.toml$")
+(use-package yaml-mode :mode "\\.ya?ml$"
+  :config (def-electric! yaml-mode :chars (?\n ?: ?-)))
+(use-package json-mode :mode "\\.js\\(on\\|hintrc\\)$"
+  :config (def-electric! json-mode :chars (?\n ?: ?{ ?})))
+
+(use-package dockerfile-mode :mode "/Dockerfile$"
+  :config
+  (def-docset! dockerfile-mode "docker")
+  (def-builder! dockerfile-mode dockerfile-build-buffer "Dockerfile"))
+
+(use-package vimrc-mode :mode ("/\\.?g?vimrc$" "\\.vim$" "/\\.vim/rc/.+$"))
+
+;;
+(def-project-type! ansible-mode "ans"
+  :modes (yaml-mode)
+  :files ("roles/"))
+
+(use-package company-ansible
+  :commands (company-ansible)
+  :init (def-company-backend! ansible-mode (ansible)))
+
+(def-project-type! vagrant "vagrant"
+  :files ("Vagrantfile"))
+
+(provide 'module-data)
+;;; module-data.el ends here
