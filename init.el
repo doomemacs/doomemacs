c0386cad9356cc5a55ff70800d41cfc90ba9aead
Henrik Lissner <henrik@lissner.net>
2016-06-09 00:31:11 -0400

    v1.2.2 bump

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;;
 ;; Author:  Henrik Lissner <henrik@lissner.net>
 ;; URL:     https://github.com/hlissner/.emacs.d
-;; Version: 1.2.1
+;; Version: 1.2.2
 ;;
 ;;   =================     ===============     ===============   ========  ========
 ;;   \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
