a11c419652e2637cf57b896c43b9e7f120763e2d
Henrik Lissner <henrik@lissner.net>
2016-09-12 16:33:35 +0200

    fixup! Add eldoc mode-line theme

--- core/defuns/defuns-ui.el
+++ core/defuns/defuns-ui.el
@@ -61,33 +61,6 @@
                    (imenu-list-minor-mode -1))))
         (doom/get-visible-buffers (doom/get-real-buffers))))
 
-;;;###autoload
-(defun doom/eldoc-show-in-mode-line (input)
-  "Display string STR in the mode-line next to minibuffer."
-  (with-current-buffer (eldoc-current-buffer)
-    (let* ((max              (window-width (selected-window)))
-           (str              (and (stringp input) (concat " " input)))
-           (len              (length str))
-           (tmp-str          str)
-           (mode-line-format (or (and str `(:eval (spaceline-ml-eldoc)))
-                                 mode-line-format))
-           roll mode-line-in-non-selected-windows)
-      (catch 'break
-        (if (and (> len max) eldoc-mode-line-rolling-flag)
-            (progn
-              (while (setq roll (sit-for 0.3))
-                (setq tmp-str (substring tmp-str 2)
-                      mode-line-format (concat tmp-str " [<]" str))
-                (force-mode-line-update)
-                (when (< (length tmp-str) 2) (setq tmp-str str)))
-              (unless roll
-                (when eldoc-mode-line-stop-rolling-on-input
-                  (setq eldoc-mode-line-rolling-flag nil))
-                (throw 'break nil)))
-          (force-mode-line-update)
-          (sit-for eldoc-show-in-mode-line-delay))))
-    (force-mode-line-update)))
-
 (put 'doom-hide-mode-line-mode 'permanent-local t)
 (put 'doom--mode-line 'permanent-local t)
 
