b0075eab8a8a945787bb01bb161aaba6aed98467
Henrik Lissner <henrik@lissner.net>
2017-01-31 05:09:39 -0500

    Add modules/ui/doom-modeline

---
+++ modules/ui/doom-modeline/packages.el
@@ -0,0 +1,23 @@
+;;; packages.el
+
+;;; These are the invisible dependencies
+;; Required
+;;(require 'f)
+;;(require 's)
+;;(require 'evil)
+;;(require 'projectile)
+;;(require 'all-the-icons)
+
+;; Optional
+;;(require 'flycheck)
+;;(require 'anzu)
+;;(require 'evil-anzu)
+;;(require 'iedit)
+;;(require 'evil-multiedit)
+
+(package! powerline :demand t)
+
+(package! all-the-icons :when (display-graphic-p) :demand t)
+
+(package! eldoc-eval :demand t)
+
