89c7ee0273975cc71df51b93ed7eb05895bd67e7
Henrik Lissner <henrik@lissner.net>
2017-02-04 02:52:41 -0500

    Add modules/feature/version-control

--- modules/feature/version-control/config.el
+++ modules/feature/version-control/config.el
@@ -1,32 +1,7 @@
 ;;; core/version-control/config.el
 
-(use-package! gitconfig-mode
-  :mode "/\\.?git/?config$"
-  :mode "/\\.gitmodules$"
-  :init (add-hook 'gitconfig-mode-hook 'flyspell-mode))
-
-(use-package! gitignore-mode
-  :mode "/\\.?git/?config$"
-  :mode "/\\.gitmodules$")
-
-
-(use-package! browse-at-remote
-  :commands (browse-at-remote/browse browse-at-remote/get-url))
-
-
-(use-package! magit
-  :commands magit-status
-  :config
-  (def-popup! ("^\\*magit.+" :regexp t))
-  (after! evil-snipe
-    ;; evil-snipe conflicts with magit
-    (add-hook 'magit-mode-hook 'turn-off-evil-snipe-override-mode)))
-
-
-(use-package! evil-magit
-  :when (featurep 'evil)
-  :after magit)
-
+(load! +git)
+;; (load! +hg)
 
 (after! vc-annotate
   (set! :popup
--- modules/feature/version-control/packages.el
+++ modules/feature/version-control/packages.el
@@ -1,8 +1,15 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; core/version-control/packages.el
 
+;;; config.el
+;; n/a
+
+;;; +git
 (package! browse-at-remote)
 (package! evil-magit :needs evil)
+(package! git-gutter-fringe)
 (package! gitconfig-mode)
 (package! gitignore-mode)
 (package! magit)
+
+;;; TODO +hg
