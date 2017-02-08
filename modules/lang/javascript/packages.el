55e4434d7748795fd2b5341d6a4975c60946175b
Henrik Lissner <henrik@lissner.net>
2017-02-08 02:23:27 -0500

    lang/javascript: update packages.el

--- modules/lang/javascript/packages.el
+++ modules/lang/javascript/packages.el
@@ -1,28 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/javascript/packages.el
 
-(package! js2-mode)
-(package! jsx-mode)
 (package! coffee-mode)
-(package! tern)
-(package! company-tern)
+(package! company-tern :needs +company)
+(package! js2-mode)
 (package! js2-refactor)
+(package! jsx-mode)
+(package! tern)
 
-(bootstrap! javascript
- "Sets up NodeJS, the TrepanJS debugger."
-
- :if-arch
- (unless (and (executable-find "node")
-              (executable-find "npm"))
-   (sudo "pacman --needed --noconfirm -S nodejs npm"))
-
- ;; :if-deb
- ;; TODO
-
- :if-macos
- (unless (executable-find "node")
-   (sh "brew install node"))
-
- :after
- (unless (executable-find "tern")
-   (sh "npm -g install trepanjs tern")))
