b135b7bf531d478c236e27dd397a8a8c8aada0b8
Henrik Lissner <henrik@lissner.net>
2017-02-19 18:09:44 -0500

    Add doom/toggle-sticky to core/autoload/editor

--- core/autoload/editor.el
+++ core/autoload/editor.el
@@ -186,3 +186,16 @@ consistent throughout a selected region, depending on `indent-tab-mode'."
       (tabify beg end)
     (untabify beg end)))
 
+;;;###autoload
+(defun doom/toggle-sticky (&optional beg end)
+  "Make a selection sticky by placing it in the header line. Possibly helpful
+for function signatures or notes. Run again to clear the header line."
+  (interactive "r")
+  (setq header-line-format
+        (when mark-active
+          (concat (propertize (format nlinum-format (line-number-at-pos beg))
+                              'face 'font-lock-comment-face)
+                  (let ((content (buffer-substring beg end)))
+                    (setq content (s-replace "\n" " " content))
+                    (setq content (replace-regexp-in-string "\\s-+" " " content))
+                    content)))))
