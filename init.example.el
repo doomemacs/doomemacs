f5e4a30d199f04699f6fce6471b2ff3d81fd877c
Henrik Lissner <henrik@lissner.net>
2017-02-19 18:59:15 -0500

    Update init.example.el

--- init.example.el
+++ init.example.el
@@ -30,7 +30,7 @@
 
 (require 'core (concat user-emacs-directory "core/core"))
 
-(doom! :feature
+(@doom :feature
        evil            ; come to the dark side, we have cookies
        ;; TODO jump            ; navigating your code
        snippets        ; my elves. They type so I don't have to
@@ -39,6 +39,8 @@
        syntax-checker  ; tasing you for every forgotten semicolon
        version-control ; remember, remember that commit in November
        workspaces      ; tab emulation, persistence and separate workspaces
+       eval            ; repls, runners 'n builders; run code, run
+       debug           ; stepping through code, to help you add bugs
 
        :completion
        company         ; auto-completion backend
@@ -51,12 +53,8 @@
        doom            ; doom-one; a look inspired by Atom's Dark One
        ;; TODO doom-dashboard  ; a nifty splash screen for Emacs
        doom-modeline   ; a snazzy Atom-inspired mode-line
-
-       :os
-       macos           ; resolves some issues running Emacs on MacOS
-
-       :fun
        doom-quit       ; DOOM quit-message prompts when you quit Emacs
+       hl-todo         ; highlight TODO/FIXME/NOTE tags
 
        :tools
        eshell          ; a consistent, cross-platform shell (WIP)
@@ -67,45 +65,32 @@
        upload          ; map local to remote projects via ssh/ftp
 
        :lang
-       asm             ; assembly for fun or debugging
+       assembly        ; assembly for fun or debugging
        cc              ; C/C++/Obj-C madness
        crystal         ; ruby at the speed of c
        csharp          ; unity, .NET, and mono shenanigans
-       css             ; #big-bang::before { content: ""; }
        data            ; config/data formats
        emacs-lisp      ; drown in parentheses
        go              ; the hipster dialect
        haskell         ; a language that's lazier than I am
-       html            ; The end is always near </html>
        java            ; the poster child for carpal tunnel syndrome
        javascript      ; all(hope(abandon(ye(who(enter(here))))))
        julia           ; a better, faster MATLAB
        latex           ; writing papers in Emacs has never been so fun
        lua             ; one-based indices? one-based indices
+       markdown        ; writing docs for people to ignore
        octave          ; math isn't a choice, it's a way of life
        org             ; for organized fearless leader (WIP)
        php             ; make php less awful to work with
        processing      ; for prototyping
        python          ; beautiful is better than ugly
-       rest            ; emacs as a REST client
        ruby            ; 1.step do {|i| p "Ruby is #{i&1==0?'love':'life'}"}
        rust            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        scala           ; java, but good
        sh              ; she sells (ba|z)sh shells on the C xor
        swift           ; who asked for emoji variables?
-       text            ; writing docs for people to ignore
        typescript      ; javascript, but better
-       viml            ; PURGE THE HERETICS, FOR THE EMPEROR
-
-       :frameworks
-       angular
-       jekyll
-       laravel
-       nodejs
-       rails
-       react
-       launchbar
-       screeps
+       web             ; the tubes
 
        ;; Applications are large, toggle-able states that transform Emacs to
        ;; fulfill a specific purpose. See `doom/toggle'. They should be loaded
