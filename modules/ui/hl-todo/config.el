7fc29c26a3c37b27d6be4ea5f3a694a041db174a
Henrik Lissner <henrik@lissner.net>
2017-02-19 18:13:15 -0500

    Add modules/ui/hl-todo

---
+++ modules/ui/hl-todo/config.el
@@ -0,0 +1,11 @@
+;;; ui/hl-todo/packages.el
+
+(@def-package hl-todo
+  :commands hl-todo-mode
+  :init
+  (add-hook 'prog-mode-hook 'hl-todo-mode)
+  :config
+  (setq hl-todo-keyword-faces
+        `(("TODO"  . ,(face-foreground 'warning))
+          ("FIXME" . ,(face-foreground 'error))
+          ("NOTE"  . ,(face-foreground 'success)))))
---
+++ modules/ui/hl-todo/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/hl-todo/packages.el
+
+(@package hl-todo)
