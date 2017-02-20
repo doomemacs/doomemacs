e0e865613c77ac7a6b13c0b3b8881678214c79cc
Henrik Lissner <henrik@lissner.net>
2017-02-20 00:44:38 -0500

    feature/eval: make +repl:eval-region operators not move point

--- modules/feature/eval/autoload/evil.el
+++ modules/feature/eval/autoload/evil.el
@@ -2,11 +2,13 @@
 
 ;;;###autoload (autoload '+repl:eval-region "feature/eval/autoload/evil" nil t)
 (evil-define-operator +repl:eval-region (beg end)
+  :move-point nil
   (interactive "<r>")
   (+repl/eval-region beg end))
 
 ;;;###autoload (autoload '+repl:eval-region-and-replace "feature/eval/autoload/evil" nil t)
 (evil-define-operator +repl:eval-region-and-replace (beg end)
+  :move-point nil
   (interactive "<r>")
   (+repl/eval-region-and-replace beg end))
 
