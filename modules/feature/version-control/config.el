691bfd037b4b360e2d2995c697386ec71dd31f28
Henrik Lissner <henrik@lissner.net>
2017-02-21 16:05:12 -0500

    feature/version-control: fix @set :evil-state call + refactor

--- modules/feature/version-control/config.el
+++ modules/feature/version-control/config.el
@@ -6,20 +6,20 @@
 (@after vc-annotate
   (@set :popup
     '("*vc-diff*" :size 15 :noselect t)
-    '("*vc-change-log*" :size 15 :select t)
+    '("*vc-change-log*" :size 15)
     '(vc-annotate-mode :same t))
 
   (@set :evil-state
-    '(vc-annotate-mode normal)
-    '(vc-git-log-view-mode normal))
+    '(vc-annotate-mode . normal)
+    '(vc-git-log-view-mode . normal))
 
   (@map :map vc-annotate-mode-map
-        :n "q" 'kill-this-buffer
-        :n "d" 'vc-annotate-show-diff-revision-at-line
-        :n "D" 'vc-annotate-show-changeset-diff-revision-at-line
+        :n "q"   'kill-this-buffer
+        :n "d"   'vc-annotate-show-diff-revision-at-line
+        :n "D"   'vc-annotate-show-changeset-diff-revision-at-line
         :n "SPC" 'vc-annotate-show-log-revision-at-line
-        :n "]]" 'vc-annotate-next-revision
-        :n "[[" 'vc-annotate-prev-revision
+        :n "]]"  'vc-annotate-next-revision
+        :n "[["  'vc-annotate-prev-revision
         :n [tab] 'vc-annotate-toggle-annotation-visibility
         :n "RET" 'vc-annotate-find-revision-at-line))
 
