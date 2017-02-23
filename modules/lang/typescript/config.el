f3f70aee88b376eb003e9250a880764c45c35746
Henrik Lissner <henrik@lissner.net>
2017-02-23 00:14:45 -0500

    lang/typescript: comment out :emr setting

--- modules/lang/typescript/config.el
+++ modules/lang/typescript/config.el
@@ -8,15 +8,15 @@
   (set! :company-backend 'typescript-mode '(company-tide))
   (set! :electric 'typescript-mode :chars '(?\} ?\)) :words '("||" "&&"))
 
-  ;; TODO emr definitions for:
-  ;; + tide-jump-back
-  ;; + (tide-jump-to-definition t)
-  (set! :emr 'typescript-mode
-        '(tide-find-references             "find usages")
-        '(tide-rename-symbol               "rename symbol")
-        '(tide-jump-to-definition          "jump to definition")
-        '(tide-documentation-at-point      "current type documentation")
-        '(tide-restart-server              "restart tide server"))
+  ;; TODO tide-jump-back
+  ;; TODO (tide-jump-to-definition t)
+  ;; TODO convert into keybinds
+  ;; (set! :emr 'typescript-mode
+  ;;       '(tide-find-references             "find usages")
+  ;;       '(tide-rename-symbol               "rename symbol")
+  ;;       '(tide-jump-to-definition          "jump to definition")
+  ;;       '(tide-documentation-at-point      "current type documentation")
+  ;;       '(tide-restart-server              "restart tide server"))
 
   (defun +typescript|tide-setup ()
     (when (or (eq major-mode 'typescript-mode)
