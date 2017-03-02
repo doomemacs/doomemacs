25091ee375f993a2abd4c3394bef43d01efa4ea4
Henrik Lissner <henrik@lissner.net>
2017-03-01 23:45:39 -0500

    core/autoload/memoize: add autoload cookies

--- core/autoload/memoize.el
+++ core/autoload/memoize.el
@@ -1,6 +1,7 @@
 ;;; memoize.el
 (provide 'doom-lib-memoize)
 
+;;;###autoload
 (defvar doom-memoized-table (make-hash-table :test 'equal :size 10)
   "A lookup table containing memoized functions. The keys are argument lists,
 and the value is the function's return value.")
@@ -14,6 +15,7 @@ and the value is the function's return value.")
          (or (gethash key doom-memoized-table)
              (puthash key (apply ',func args) doom-memoized-table))))))
 
+;;;###autoload
 (defmacro def-memoized! (name arglist &rest body)
   "Create a memoize'd function. NAME, ARGLIST, DOCSTRING and BODY
 have the same meaning as in `defun'."
