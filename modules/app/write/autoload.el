2752cdc9b90de32b2c5887116359f2ab1096ffea
Henrik Lissner <henrik@lissner.net>
2017-03-08 21:34:03 -0500

    Add modules/app/write (WIP)

---
+++ modules/app/write/autoload.el
@@ -0,0 +1,19 @@
+;;; app/write/autoload.el
+
+(defvar-local +write--buffer-mode nil
+  "TODO")
+
+;;;###autoload
+(define-minor-mode +write-mode
+  :init-value nil
+  :keymap nil
+  (let ((arg (if +write-mode +1 -1)))
+    (when (and (featurep 'doom-themes)
+               (not +write--buffer-mode)
+               +write-mode)
+      (setq +write--buffer-mode doom-buffer-mode))
+    (when +write--buffer-mode
+      (doom-buffer-mode (if +write-mode -1 +1)))
+    (text-scale-set (if +write-mode 1.5 0))
+    (visual-fill-column-mode arg)
+    (setq line-spacing (if +write-mode 6))))
