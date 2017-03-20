3722f17808a976a149439c1fd63af9d8374b8cb2
Henrik Lissner <henrik@lissner.net>
2017-03-20 03:47:59 -0400

    lang/javascript: remove +javascript/install (replaced by bootstrap)

--- modules/lang/javascript/autoload.el
+++ modules/lang/javascript/autoload.el
@@ -1,24 +1,5 @@
 ;;; lang/javascript/autoload.el
 
-;;;###autoload
-(defun +javascript/install ()
-  "Installs NodeJS and tern."
-  (interactive)
-  (pcase (doom-system-os)
-    ('arch
-     (unless (and (executable-find "node")
-                  (executable-find "npm"))
-       (doom-sudo "pacman --needed --noconfirm -S nodejs npm")))
-    ('debian) ;; TODO
-    ('macos
-     (unless (executable-find "node")
-       (doom-sh "brew install node")))
-    (_ (error "Not a supported OS")))
-  (unless (executable-find "node")
-    (error "node isn't installed, run `+javascript/install' again"))
-  (unless (executable-find "tern")
-    (sh "npm -g install tern")))
-
 (defvar +javascript-npm-conf (make-hash-table :test 'equal))
 
 ;;;###autoload
