7866fc3fe4ad8d38985ef0bf0597d12c9433730e
Henrik Lissner <henrik@lissner.net>
2017-03-28 16:22:41 -0400

    lang/haskell: add bootstrap

--- modules/lang/haskell/packages.el
+++ modules/lang/haskell/packages.el
@@ -4,3 +4,17 @@
 (package! haskell-mode)
 (package! dante)
 
+;;
+(def-bootstrap! haskell
+  (pcase (doom-system-os)
+    ('arch
+     (let ((pkgs (cl-remove-if 'executable-find '("ghc" "happy" "alex"))))
+       (unless (executable-find "cabal")
+         (push "cabal-install" pkgs))
+       (sudo "pacman --noconfirm -S ghc cabal-install happy alex" (s-join " " pkgs))))
+    ('debian) ;; TODO
+    ('macos
+     (unless (executable-find "ghc")
+       (sh "brew install ghc"))
+     (unless (executable-find "cabal")
+       (sh "brew install cabal-install")))))
