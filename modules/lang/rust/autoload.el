4bd408d476ddf737bca558e075cebab5ac980a79
Henrik Lissner <henrik@lissner.net>
2017-04-07 01:27:34 -0400

    lang/rust: remove +rust/install-racer (replaced with bootstrapper)

--- modules/lang/rust/autoload.el
+++ modules/lang/rust/autoload.el
@@ -1,11 +1,2 @@
 ;;; lang/rust/autoload.el
 
-;;;###autoload
-(defun +rust/install-racer ()
-  "Install and compile racer server."
-  (interactive)
-  (let ((racer-dir (expand-file-name "racer" +rust-cache-dir)))
-    (doom-fetch :github "rust-lang/rust.git" (expand-file-name "rust" +rust-cache-dir))
-    (doom-fetch :github "phildawes/racer.git" racer-dir)
-    (let ((default-directory racer-dir))
-      (doom-sh "cargo build --release"))))
