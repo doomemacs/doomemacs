3a3ae6ff5c0872a559ffa97db1e5b9ff3510503d
Henrik Lissner <henrik@lissner.net>
2017-04-11 08:29:38 -0400

    lang/org: add rust babel support

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -7,6 +7,7 @@
 (package! ob-mongo)
 (package! ob-redis)
 (package! ob-restclient)
+(package! ob-rust :recipe (:fetcher github :repo "zweifisch/ob-rust"))
 (package! ob-sql-mode)
 (package! ob-translate)
 ;; (package! ox-pandox)
