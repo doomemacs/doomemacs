b1442cb4eb752253bedbe8f180517b0c1e30f199
Henrik Lissner <henrik@lissner.net>
2017-04-12 11:00:49 -0400

    Fix ansi-format tests

--- test/core/autoload/test-message.el
+++ test/core/autoload/test-message.el
@@ -1,24 +1,26 @@
 ;;; test/core/autoload/test-message.el
 
 (def-test-group! core/autoload/message
-
   (ert-deftest ansi-format ()
-    (should (equal (ansi-format! "Hello %s" "World") "Hello World"))
-    (should (equal (ansi-format! (red "Hello %s" "World")) "[31mHello World[0m"))
-    (should (equal (ansi-format! (green "Hello %s" "World"))
-                   (format "\e[%dm%s\e[0m"
-                           (cdr (assq 'green doom-ansi-fg))
-                           "Hello World"))))
+    (let ((noninteractive t))
+      (should (equal (ansi-format! "Hello %s" "World") "Hello World"))
+      (should (equal (ansi-format! (red "Hello %s" "World")) "[31mHello World[0m"))
+      (should (equal (ansi-format! (green "Hello %s" "World"))
+                     (format "\e[%dm%s\e[0m"
+                             (cdr (assq 'green doom-ansi-fg))
+                             "Hello World")))))
 
   (ert-deftest ansi-format-nested ()
-    (should (equal (ansi-format! (bold (red "Hello %s" "World")))
-                   (format "\e[%dm%s\e[0m" 1 (format "\e[%dm%s\e[0m" 31 "Hello World"))))
-    (should (equal (ansi-format! (red (bold "Hello %s" "World")))
-                   (format "\e[%dm%s\e[0m" 31 (format "\e[%dm%s\e[0m" 1 "Hello World")))))
+    (let ((noninteractive t))
+      (should (equal (ansi-format! (bold (red "Hello %s" "World")))
+                     (format "\e[%dm%s\e[0m" 1 (format "\e[%dm%s\e[0m" 31 "Hello World"))))
+      (should (equal (ansi-format! (red (bold "Hello %s" "World")))
+                     (format "\e[%dm%s\e[0m" 31 (format "\e[%dm%s\e[0m" 1 "Hello World"))))))
 
   (ert-deftest ansi-format-apply ()
-    (should (equal (ansi-format! (color 'red "Hello %s" "World"))
-                   (ansi-format! (red "Hello %s" "World"))))
-    (should (equal (ansi-format! (color (if nil 'red 'blue) "Hello %s" "World"))
-                   (ansi-format! (blue "Hello %s" "World"))))))
+    (let ((noninteractive t))
+      (should (equal (ansi-format! (color 'red "Hello %s" "World"))
+                     (ansi-format! (red "Hello %s" "World"))))
+      (should (equal (ansi-format! (color (if nil 'red 'blue) "Hello %s" "World"))
+                     (ansi-format! (blue "Hello %s" "World")))))))
 
