c370e1cdddfc53005113a4bddae0d88c408f869b
Henrik Lissner <henrik@lissner.net>
2017-04-12 10:52:42 -0400

    Add :disabled property to def-test-group!

--- core/autoload/test.el
+++ core/autoload/test.el
@@ -3,7 +3,8 @@
 ;;;###autoload
 (defmacro def-test-group! (name &rest body)
   (declare (indent defun))
-  (dolist (form body)
-    (when (eq (car form) 'ert-deftest)
-      (setf (cadr form) (intern (format "test-%s-%s" name (symbol-name (cadr form)))))))
-  `(progn ,@body))
+  (unless (plist-get body :disabled)
+    (dolist (form body)
+      (when (eq (car form) 'ert-deftest)
+        (setf (cadr form) (intern (format "test-%s-%s" name (symbol-name (cadr form)))))))
+    `(progn ,@body)))
