05b01a431d62f9e843586a48080bc1a419aa4cc8
Henrik Lissner <henrik@lissner.net>
2017-04-22 01:49:44 -0400

    Add bin/org-capture script

--- modules/lang/org/autoload/capture.el
+++ modules/lang/org/autoload/capture.el
@@ -1,3 +1,11 @@
 ;;; lang/org/autoload/capture.el
 
-
+;;;###autoload
+(defun +org-capture (&optional key string)
+  "Initializes the current frame as a pop-up `org-capture' frame."
+  (interactive)
+  (let ((key (or key "n"))
+        (string (unless (string-empty-p string) string)))
+    (if string
+        (org-capture-string string key)
+      (org-capture nil key))))
