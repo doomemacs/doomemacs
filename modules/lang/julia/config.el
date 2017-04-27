09efa24ab857d9d9ea594bdcf4a047b523b67003
Henrik Lissner <henrik@lissner.net>
2017-04-26 23:29:31 -0400

    lang/julia: stea--borrow matlab.el's fontification of math operators

--- modules/lang/julia/config.el
+++ modules/lang/julia/config.el
@@ -4,5 +4,28 @@
   :mode "\\.jl$"
   :interpreter "julia"
   :config
-  (set! :repl 'julia-mode #'+julia/repl))
+  (set! :repl 'julia-mode #'+julia/repl)
+
+  ;; Borrow matlab.el's fontification of math operators
+  ;; From <https://ogbe.net/emacsconfig.html>
+  (font-lock-add-keywords
+   'julia-mode
+   `((,(let ((OR "\\|"))
+         (concat "\\(" ;; stolen `matlab.el' operators first
+                 "[<>!]=?" OR
+                 "\\.[/*^']" OR
+                 "==" OR
+                 "=>" OR
+                 "\\<xor\\>" OR
+                 "[-+*\\/^&|$]=?" OR ;; this has to come before next (updating operators)
+                 "[-!^&|*+\\/~:]" OR
+                 ;; more extra julia operators follow
+                 "[%$]" OR
+                 ;; bitwise operators
+                 ">>>" OR ">>" OR "<<" OR
+                 ">>>=" OR ">>" OR "<<" OR
+                 ;; comparison
+                 "[<>!]=?" OR
+                 "\\)"))
+      1 font-lock-type-face))))
 
