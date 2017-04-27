448cab5f578be0f90ec02390f360e2de6935363b
Henrik Lissner <henrik@lissner.net>
2017-04-27 18:03:37 -0400

    lang/org/+babel: add missing org-babel-get-header fn (temporary fix)

--- modules/lang/org/+babel.el
+++ modules/lang/org/+babel.el
@@ -34,6 +34,14 @@
              translate
              )))
 
+  ;; In a recent update, `org-babel-get-header' was removed from org-mode, which
+  ;; is something a fair number of babel plugins use. So until those plugins update...
+  (defun org-babel-get-header (params key &optional others)
+    (delq nil
+          (mapcar
+           (lambda (p) (when (funcall (if others #'not #'identity) (eq (car p) key)) p))
+           params)))
+
   ;; I prefer C-c C-c for confirming over the default C-c '
   (map! :map org-src-mode-map "C-c C-c" 'org-edit-src-exit)
   ;; I know the keybindings, no need for the header line
