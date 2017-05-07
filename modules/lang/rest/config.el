d64ee3c2f67ecd987b9eee8ed3bd4121eddb166a
Henrik Lissner <henrik@lissner.net>
2017-05-07 02:27:18 +0200

    lang/rest: update (w/ keybindings + :popup)

--- modules/lang/rest/config.el
+++ modules/lang/rest/config.el
@@ -2,7 +2,15 @@
 
 (def-package! restclient
   :commands restclient-mode
-  :mode ("\\.http$" . restclient-mode))
+  :mode ("\\.http$" . restclient-mode)
+  :config
+  (set! :popup "*HTTP Response*" :size 30 :select t :noesc t :autokill t)
+  (map! :mode restclient-mode
+        :n [M-return] 'restclient-http-send-current
+        :localleader
+        :desc "Execute HTTP request"     :n "e" 'restclient-http-send-current
+        :desc "Execute raw HTTP request" :n "E" 'restclient-http-send-current-raw
+        :desc "Copy curl command"        :n "c" 'restclient-copy-curl-command))
 
 
 (def-package! company-restclient
