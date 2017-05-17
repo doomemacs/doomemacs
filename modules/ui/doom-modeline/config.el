03ebdf7855077c7d27e86cce361229900de01b92
Henrik Lissner <henrik@lissner.net>
2017-05-17 17:28:36 +0200

    ui/doom-modeline: revise face defs (to match doom-themes update)

--- modules/ui/doom-modeline/config.el
+++ modules/ui/doom-modeline/config.el
@@ -119,32 +119,32 @@
   :group 'doom)
 
 (defface doom-modeline-buffer-path
-  '((t (:inherit mode-line :bold t)))
+  '((t (:inherit mode-line-emphasis :bold t)))
   "Face used for the dirname part of the buffer path."
   :group '+doom-modeline)
 
 (defface doom-modeline-buffer-file
-  '((t (:inherit doom-modeline-buffer-path)))
+  '((t (:inherit mode-line-buffer-id)))
   "Face used for the filename part of the mode-line buffer path."
   :group '+doom-modeline)
 
 (defface doom-modeline-buffer-modified
-  '((t (:inherit highlight :background nil)))
+  '((t (:inherit error :background nil :bold t)))
   "Face used for the 'unsaved' symbol in the mode-line."
   :group '+doom-modeline)
 
 (defface doom-modeline-buffer-major-mode
-  '((t (:inherit mode-line :bold t)))
+  '((t (:inherit mode-line-emphasis :bold t)))
   "Face used for the major-mode segment in the mode-line."
   :group '+doom-modeline)
 
 (defface doom-modeline-highlight
-  '((t (:inherit mode-line)))
+  '((t (:inherit mode-line-emphasis)))
   "Face for bright segments of the mode-line."
   :group '+doom-modeline)
 
 (defface doom-modeline-panel
-  '((t (:inherit mode-line)))
+  '((t (:inherit mode-line-highlight)))
   "Face for 'X out of Y' segments, such as `+doom-modeline--anzu', `+doom-modeline--evil-substitute' and
 `iedit'"
   :group '+doom-modeline)
@@ -165,16 +165,16 @@
   :group '+doom-modeline)
 
 ;; Bar
-(defface doom-modeline-bar '((t (:inherit highlight :foreground nil)))
+(defface doom-modeline-bar '((t (:inherit highlight)))
   "The face used for the left-most bar on the mode-line of an active window."
   :group '+doom-modeline)
 
-(defface doom-modeline-eldoc-bar '((t (:inherit shadow :foreground nil)))
+(defface doom-modeline-eldoc-bar '((t (:inherit shadow)))
   "The face used for the left-most bar on the mode-line when eldoc-eval is
 active."
   :group '+doom-modeline)
 
-(defface doom-modeline-inactive-bar '((t (:inherit mode-line-inactive)))
+(defface doom-modeline-inactive-bar '((t (:inherit warning :inverse-video t)))
   "The face used for the left-most bar on the mode-line of an inactive window."
   :group '+doom-modeline)
 
@@ -547,14 +547,15 @@ with `evil-ex-substitute', and/or 4. The number of active `iedit' regions."
   (+doom-modeline--make-xpm
    (face-background (if (active)
                         'doom-modeline-bar
-                      'doom-modeline-inactive-bar))
+                      'doom-modeline-inactive-bar)
+                    nil t)
    +doom-modeline-height
    +doom-modeline-bar-width))
 
 (def-modeline-segment! eldoc-bar
   "A differently colored bar, to signify an eldoc display."
   (+doom-modeline--make-xpm
-   (face-background 'doom-modeline-eldoc-bar)
+   (face-background 'doom-modeline-eldoc-bar nil t)
    +doom-modeline-height
    +doom-modeline-bar-width))
 
