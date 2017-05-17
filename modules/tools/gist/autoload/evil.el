b1d545dafea6a3ede840f85e644fe26e61dec7d2
Henrik Lissner <henrik@lissner.net>
2017-05-17 21:07:58 +0200

    tools/gist: make gists private by default

--- modules/tools/gist/autoload/evil.el
+++ modules/tools/gist/autoload/evil.el
@@ -2,14 +2,16 @@
 
 ;;;###autoload (autoload '+gist:send "tools/gist/autoload/evil" nil t)
 (evil-define-operator +gist:send (beg end search bang)
+  "Create a private gist from the buffer. If BANG then make it public."
   :type inclusive :repeat nil
   (interactive "<r><!>")
   (if bang
-      (gist-region-or-buffer-private)
-    (gist-region-or-buffer)))
+      (gist-region-or-buffer)
+    (gist-region-or-buffer-private)))
 
 ;;;###autoload (autoload '+gist:list "tools/gist/autoload/evil" nil t)
 (evil-define-command +gist:list (&optional username)
+  "Pop up a listing of gists."
   (interactive "<a>")
   (if username
       (gist-list-user username)
