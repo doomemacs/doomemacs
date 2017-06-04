8a70e47e6d739e0138bd19ed29a9683cbccf8cfd
Henrik Lissner <henrik@lissner.net>
2017-06-04 02:29:39 +0200

    lang/scala: refactor + fix ensime-mode init

--- modules/lang/scala/config.el
+++ modules/lang/scala/config.el
@@ -2,10 +2,10 @@
 
 (def-package! scala-mode
   :mode "\\.s\\(cala\\|bt\\)$"
-  :init
-  (add-hook 'scala-mode-hook #'eldoc-mode)
   :config
-  (set! :company-backend 'scala-mode '(ensime-company (company-yasnippet))))
+  (add-hook 'scala-mode-hook #'ensime-mode)
+  (setq scala-indent:align-parameters t)
+  (set! :company-backend 'scala-mode '(ensime-company company-yasnippet)))
 
 
 (def-package! sbt-mode :after scala-mode)
@@ -13,6 +13,10 @@
 
 (def-package! ensime
   :commands (ensime ensime-mode ensime-scala-mode-hook)
-  :init
-  (add-hook 'scala-mode-hook #'ensime-scala-mode-hook))
+  :config
+  (setq ensime-startup-snapshot-notification nil
+        ensime-startup-notification nil
+        ensime-eldoc-hints t)
+
+  (add-hook 'ensime-mode-hook #'eldoc-mode))
 
