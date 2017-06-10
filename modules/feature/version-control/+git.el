df4ce8616d3b9bd2af21327f4dc135c4857ef241
Henrik Lissner <henrik@lissner.net>
2017-06-10 11:52:07 +0200

    feature/version-control: refactor

--- modules/feature/version-control/+git.el
+++ modules/feature/version-control/+git.el
@@ -41,27 +41,7 @@
   ;; showing revision details in the minibuffer, show them in
   ;; `header-line-format', which has better visibility.
   (setq git-timemachine-show-minibuffer-details nil)
-
-  (defun +vcs|toggle-header-line ()
-    (if git-timemachine-mode
-        (+vcs*update-header-line)
-      (setq-local header-line-format nil)))
-
-  (defun +vcs*update-header-line (&rest _)
-    (when (and git-timemachine-mode git-timemachine-revision)
-      (let* ((revision git-timemachine-revision)
-             (date-relative (nth 3 revision))
-             (date-full (nth 4 revision))
-             (author (if git-timemachine-show-author (concat (nth 6 revision) ": ") ""))
-             (sha-or-subject (if (eq git-timemachine-minibuffer-detail 'commit) (car revision) (nth 5 revision))))
-        (setq-local
-         header-line-format
-         (format "%s%s [%s (%s)]"
-                 (propertize author 'face 'git-timemachine-minibuffer-author-face)
-                 (propertize sha-or-subject 'face 'git-timemachine-minibuffer-detail-face)
-                 date-full date-relative)))))
-
-  (add-hook 'git-timemachine-mode-hook #'+vcs|toggle-header-line)
+  (add-hook 'git-timemachine-mode-hook #'+vcs|init-header-line)
   (advice-add #'git-timemachine-show-revision :after #'+vcs*update-header-line)
 
   ;; Force evil to rehash keybindings for the current state
