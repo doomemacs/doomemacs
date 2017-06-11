37012b86ca34f3da05959df4fe80bed52fdbaa23
Henrik Lissner <henrik@lissner.net>
2017-06-12 01:40:20 +0200

    +pass/ivy: add get-field action (#103)

--- modules/tools/password-store/autoload.el
+++ modules/tools/password-store/autoload.el
@@ -12,6 +12,19 @@
 
 
 (after! ivy
+  (defun +pass-ivy-action--get-field (item)
+    (let* ((data (auth-pass-parse-entry item))
+           (field (if data (completing-read "Field: " (mapcar #'car data) nil t))))
+      (if data
+          (progn
+            (password-store-clear)
+            (message "Copied %s's %s field to clipboard. Will clear in %s seconds"
+                     item field (password-store-timeout))
+            (kill-new (cdr (assoc field data)))
+            (setq password-store-timeout-timer
+                  (run-at-time (password-store-timeout) nil 'password-store-clear)))
+        (error "Couldn't find entry: %s" item))))
+
   (defun +pass-ivy-action--copy-username (item)
     (if-let (user (cl-loop with data = (auth-pass-parse-entry item)
                            for key in +pass-user-fields
@@ -27,7 +40,8 @@
    '(("o" password-store-copy "copy password")
      ("u" +pass-ivy-action--copy-username "copy username")
      ("e" password-store-edit "edit entry")
-     ("b" password-store-url "open url in browser"))))
+     ("b" password-store-url "open url in browser")
+     ("f" +pass-ivy-action--get-field "get field"))))
 
 ;;;###autoload
 (defun +pass/ivy (&optional browse-url)
