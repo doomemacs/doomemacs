2b3311faa22ffa1b0ade2d26c194c94524d753a4
Henrik Lissner <henrik@lissner.net>
2017-06-11 16:40:57 +0200

    tools/password-store: refactor

--- modules/tools/password-store/config.el
+++ modules/tools/password-store/config.el
@@ -4,16 +4,27 @@
   :config
   (setq password-store-password-length 12))
 
-(def-package! pass
-  :commands (pass)
-  :bind (:map pass-mode-map
-         ("j" . pass-next-entry)
-         ("k" . pass-prev-entry)
-         ("d" . pass-kill)
-         ("C-j" . pass-next-directory)
-         ("C-k" . pass-next-directory)))
 
-(def-package! auth-password-store
-  :demand t
+(def-package! pass
+  :commands pass
   :config
-  (auth-pass-enable))
+  (set! :evil-state 'pass-mode 'emacs)
+  (set! :popup "*Password-Store*" :align 'left :size 32 :select t :autokill t :noesc t)
+  (map! :map pass-mode-map
+        "j"   #'pass-next-entry
+        "k"   #'pass-prev-entry
+        "d"   #'pass-kill
+        "C-j" #'pass-next-directory
+        "C-k" #'pass-next-directory))
+
+
+(cond ((version< emacs-version "26")
+       ;; included with `pass'
+       (def-package! auth-password-store
+         :demand t
+         :config (auth-pass-enable)))
+
+      (t ;; built into Emacs 26
+       (require 'auth-store-pass)
+       (auth-source-pass-enable)))
+
