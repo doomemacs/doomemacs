0574ba2d84307030f4eecdcf69e71f49d7411ed2
Henrik Lissner <henrik@lissner.net>
2017-06-16 02:07:02 +0200

    Refactor lang/markdown; add fontify code blocks

--- modules/lang/markdown/config.el
+++ modules/lang/markdown/config.el
@@ -1,14 +1,17 @@
 ;;; lang/markdown/config.el -*- lexical-binding: t; -*-
 
 (def-package! markdown-mode
-  :mode ("\\.m\\(d\\|arkdown\\)$" "/README$"
-         ("/README\\.md$" . gfm-mode))
+  :mode ("/README\\.md$" . gfm-mode)
+  :mode "\\.m\\(d\\|arkdown\\)$"
+  :mode "/README$"
   :init
   (setq markdown-enable-wiki-links t
         markdown-enable-math t
         markdown-italic-underscore t
         markdown-make-gfm-checkboxes-buttons t
-        markdown-gfm-additional-languages '("sh"))
+        markdown-gfm-additional-languages '("sh")
+        markdown-fontify-code-blocks-natively t
+        markdown-hide-urls nil) ; trigger with `markdown-toggle-url-hiding'
 
   :config
   (add-hook! markdown-mode
@@ -20,37 +23,36 @@
    '(markdown-mode gfm-mode)
    "\`\`\`" "\`\`\`" :post-handlers '(("||\n" "RET")))
 
-  (map! :map gfm-mode-map
-        "`" #'self-insert-command
+  (map! (:map gfm-mode-map
+          "`" #'self-insert-command)
 
-        :map markdown-mode-map
-        "<backspace>"  nil
-        "<M-left>"     nil
-        "<M-right>"    nil
-        "M-*"      #'markdown-insert-list-item
-        "M-b"      #'markdown-insert-bold
-        "M-i"      #'markdown-insert-italic
-        "M-`"      #'+markdown/insert-del
-        :m "gj"    #'markdown-next-visible-heading
-        :m "gk"    #'markdown-previous-visible-heading
-        ;; Assumes you have a markdown renderer plugin in chrome
-        :n "M-r"   #'browse-url-of-file
-        ;; TODO: Make context sensitive
-        :n "[p"    #'markdown-promote
-        :n "]p"    #'markdown-demote
-        :n "[l"    #'markdown-next-link
-        :n "]l"    #'markdown-previous-link
-        :n "gf"    #'markdown-follow-thing-at-point
-        :i "M--"   #'markdown-insert-hr
+        (:map markdown-mode-map
+          [remap find-file-at-point] #'markdown-follow-thing-at-point
+          "M-*"      #'markdown-insert-list-item
+          "M-b"      #'markdown-insert-bold
+          "M-i"      #'markdown-insert-italic
+          "M-`"      #'+markdown/insert-del
+          :m "gj"    #'markdown-next-visible-heading
+          :m "gk"    #'markdown-previous-visible-heading
+          ;; Assumes you have a markdown renderer plugin in chrome
+          :n "M-r"   #'browse-url-of-file
+          ;; TODO: Make context sensitive
+          :m "]h"    #'markdown-next-visible-heading
+          :m "[h"    #'markdown-previous-visible-heading
+          :m "[p"    #'markdown-promote
+          :m "]p"    #'markdown-demote
+          :m "[l"    #'markdown-next-link
+          :m "]l"    #'markdown-previous-link
+          :i "M--"   #'markdown-insert-hr)
 
-        :localleader
-        :nv "o"    #'markdown-open
-        :nv "b"    #'markdown-preview
-        (:prefix "i"
-          :nv "t"    #'markdown-toc-generate-toc
-          :nv "i"    #'markdown-insert-image
-          :nv "l"    #'markdown-insert-link
-          :nv "L"    #'markdown-insert-reference-link-dwim)))
+        (:localleader
+          :nv "o" #'markdown-open
+          :nv "b" #'markdown-preview
+          (:prefix "i"
+            :nv "t" #'markdown-toc-generate-toc
+            :nv "i" #'markdown-insert-image
+            :nv "l" #'markdown-insert-inline-link-dwim
+            :nv "L" #'markdown-insert-reference-link-dwim))))
 
 
 (def-package! markdown-toc
