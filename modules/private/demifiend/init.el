725aecf20415a21cad7fc361d79e60f72c9f113f
Matthew Graybosch <public@matthewgraybosch.com>
2017-06-26 22:09:41 -0400

    Adding org-publish config.wq

---
+++ modules/private/demifiend/init.el
@@ -0,0 +1,6 @@
+;;; private/demifiend/init.el -*- lexical-binding: t; -*-
+
+(setq user-login-name "demifiend")
+(setq user-full-name "Matthew Graybosch")
+(setq user-email-address "public@matthewgraybosch.com")
+(setq user-website "https://www.matthewgraybosch.com")
