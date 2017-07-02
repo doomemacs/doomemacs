3ffe496f79317bc660f41ebbf68a8bdd8a5395ec
Benjamin Andresen <bandresen@gmail.com>
2017-07-02 17:51:11 +0200

    Add: feature/version-control: make git-gutter hydra prettier

--- modules/feature/version-control/+git.el
+++ modules/feature/version-control/+git.el
@@ -38,24 +38,27 @@
     (require 'hydra)
     (defhydra +hydra-git-gutter (:body-pre (git-gutter-mode 1)
                                           :hint nil)
-      "
-Git gutter:
-  _j_: next hunk        _s_tage hunk     _q_uit
-  _k_: previous hunk    _r_evert hunk    _Q_uit and deactivate git-gutter
-  ^ ^                   _p_opup hunk
-  _h_: first hunk
-  _l_: last hunk        set start _R_evision
-"
+    "
+                                     ╭─────────────────┐
+  Movement   Hunk Actions     Misc.  │ gg: +%-4s(car (git-gutter:statistic))/ -%-3s(cdr (git-gutter:statistic)) │
+  ╭──────────────────────────────────┴─────────────────╯
+     ^_g_^       [_s_] stage        [_R_] set start Rev
+     ^_k_^       [_r_] revert
+     ^↑ ^      [_m_] mark
+     ^↓ ^      [_p_] popup          ╭──────────────────────
+     ^_j_^                          │[_q_] quit
+     ^_G_^                          │[_Q_] Quit and disable"
       ("j" (progn (git-gutter:next-hunk 1)
                   (recenter)))
       ("k" (progn (git-gutter:previous-hunk 1)
                   (recenter)))
-      ("h" (progn (goto-char (point-min))
+      ("g" (progn (goto-char (point-min))
                   (git-gutter:next-hunk 1)))
-      ("l" (progn (goto-char (point-min))
+      ("G" (progn (goto-char (point-min))
                   (git-gutter:previous-hunk 1)))
       ("s" git-gutter:stage-hunk)
       ("r" git-gutter:revert-hunk)
+      ("m" git-gutter:mark-hunk)
       ("p" git-gutter:popup-hunk)
       ("R" git-gutter:set-start-revision)
       ("q" nil :color blue)
