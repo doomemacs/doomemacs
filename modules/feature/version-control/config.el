8479e1c8b9c759713266ecdf6f058fb60ddee40d
Benjamin Andresen <bandresen@gmail.com>
2017-07-02 17:50:48 +0200

    Add: feature/version-control: make smerge hydra prettier

--- modules/feature/version-control/config.el
+++ modules/feature/version-control/config.el
@@ -31,25 +31,27 @@
       (defalias 'smerge-diff-upper-lower 'smerge-diff-mine-other)
       (defalias 'smerge-diff-base-lower 'smerge-diff-base-other))
 
-    (defhydra +hydra-smerge (:color pink
-                             :hint nil
+    (defhydra +hydra-smerge (:hint nil
                              :pre (smerge-mode 1)
                              ;; Disable `smerge-mode' when quitting hydra if
                              ;; no merge conflicts remain.
                              :post (smerge-auto-leave))
-      "
-^Move^       ^Keep^               ^Diff^                 ^Other^
-^^-----------^^-------------------^^---------------------^^-------
-_g_          _b_ase               _<_: upper/base        _C_ombine
-^^           _u_pper              _=_: upper/lower       _r_esolve
-_j_          _l_ower              _>_: base/lower        _R_emove
-_k_          _a_ll                _H_ighlight diff
-^^           _RET_: current       _E_diff
-_G_                                             "
+    "
+                                                    ╭────────┐
+  Movement   Keep           Diff              Other │ smerge │
+  ╭─────────────────────────────────────────────────┴────────╯
+     ^_g_^       [_b_] base       [_<_] upper/base    [_C_] Combine
+     ^_C-k_^     [_u_] upper      [_=_] upper/lower   [_r_] resolve
+     ^_k_ ↑^     [_l_] lower      [_>_] base/lower    [_R_] remove
+     ^_j_ ↓^     [_a_] all        [_H_] hightlight
+     ^_C-j_^     [_RET_] current  [_E_] ediff             ╭──────────
+     ^_G_^                                            │ [_q_] quit"
       ("g" (progn (goto-char (point-min)) (smerge-next)))
       ("G" (progn (goto-char (point-max)) (smerge-prev)))
-      ("j" smerge-next)
-      ("k" smerge-prev)
+      ("C-j" smerge-next)
+      ("C-k" smerge-prev)
+      ("j" next-line)
+      ("k" previous-line)
       ("b" smerge-keep-base)
       ("u" smerge-keep-upper)
       ("l" smerge-keep-lower)
@@ -64,4 +66,4 @@ _G_                                             "
       ("C" smerge-combine-with-next)
       ("r" smerge-resolve)
       ("R" smerge-kill-current)
-      ("q" nil "cancel" :color blue))))
+      ("q" nil :color blue))))
