4be4756f90bfe20e200ffe51bf8b0d3f8ce5f36d
Henrik Lissner <henrik@lissner.net>
2017-09-14 12:50:54 +0200

    ui/hl-todo: fix comment detection for some major modes
    For major modes with poorly implemented syntax-tables, or simply can't
    use them for their comments.

--- modules/ui/hl-todo/config.el
+++ modules/ui/hl-todo/config.el
@@ -7,4 +7,10 @@
   (setq hl-todo-keyword-faces
         `(("TODO"  . ,(face-foreground 'warning))
           ("FIXME" . ,(face-foreground 'error))
-          ("NOTE"  . ,(face-foreground 'success)))))
+          ("NOTE"  . ,(face-foreground 'success))))
+
+  ;; Use a more primitive todo-keyword detection method in major modes that
+  ;; don't use/have a valid syntax table entry for comments.
+  (add-hook!
+    (pug-mode haml-mode)
+    #'+hl-todo|use-face-detection))
