f9321b1b3bb64f81e73678fa4e0a57998e035798
Undead Kernel <carloscg@gmail.com>
2017-10-08 23:34:25 +0200

    Module to visually select windows with either ace-window or switch-window
    Modularize ace-window and add switch-window as a replacement: Issue #218

---
+++ modules/ui/window-select/packages.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/window-select/packages.el
+
+(cond ((featurep! +switch-window)
+       ;; Install switch-window if the user indicated the '+switch-window' module flag
+       (package! switch-window))
+      ((or (featurep! +ace-window) t)
+       ;; Install ace-window if the user selects the flag '+ace-window' or by default
+       ;; ... if the user did not specify a module flag
+       (package! ace-window)))
+
