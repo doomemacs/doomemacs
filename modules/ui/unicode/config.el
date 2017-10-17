9b884857f2c35c1efd4d67343c07c8af6332b783
Henrik Lissner <henrik@lissner.net>
2017-10-18 00:58:32 +0200

    ui/unicode: initialize more intelligently

--- modules/ui/unicode/config.el
+++ modules/ui/unicode/config.el
@@ -1,10 +1,19 @@
 ;;; ui/unicode/config.el -*- lexical-binding: t; -*-
 
 (def-package! unicode-fonts
-  :demand t
   :init
   (setq-default bidi-display-reordering t
                 doom-unicode-font nil)
-  :config
-  ;; NOTE will impact startup time on first run
-  (unicode-fonts-setup))
+
+  (defun +unicode|init-fonts (&optional frame)
+    "Initialize `unicode-fonts', if in a GUI session."
+    (when (display-graphic-p frame)
+      (with-selected-frame frame
+        (require 'unicode-fonts)
+        ;; NOTE will impact startup time on first run
+        (unicode-fonts-setup))))
+
+  (add-hook! 'after-init-hook
+    (if initial-window-system
+        (+unicode|init-fonts)
+      (add-hook 'after-make-frame-functions #'+unicode|init-fonts))))
