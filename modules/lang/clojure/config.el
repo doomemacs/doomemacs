56d43a93be138facdb0db96631415fbfcbdb6bda
teesloane <tylersloane@gmail.com>
2017-11-03 13:41:54 -0400

    Add cljs mode and localleader keybindings

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -1,9 +1,17 @@
 ;;; lang/clojure/config.el -*- lexical-binding: t; -*-
 
 (def-package! clojure-mode
-  :mode "\\.clj$")
+  :mode (("\\.clj$" . clojure-mode)
+         ("\\.cljs$". clojurescript-mode))
+  :commands (clojure-mode clojurescript-mode) ;; this might not be necessary
+  :config
+  (map! :map clojure-mode-map
+        (:localleader
+          "'"      #'cider-jack-in
+          "\""     #'cider-jack-in-clojurescript
+          "r"      #'cider-eval-region)))
 
 (def-package! cider
-  :commands (cider-jack-in cider-mode)
+  :commands (cider-jack-in cider-mode cider-jack-in-clojurescript)
   :config
   (setq nrepl-hide-special-buffers t))
