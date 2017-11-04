40a1aaae8e3ea7d369115df4e2733a227f32fbe8
Henrik Lissner <henrik@lissner.net>
2017-11-04 22:13:10 +0100

    Make async a core package

--- core/core-packages.el
+++ core/core-packages.el
@@ -68,7 +68,7 @@ package's name as a symbol, and whose CDR is the plist supplied to its
 `package!' declaration. Set by `doom-initialize-packages'.")
 
 (defvar doom-core-packages
-  '(persistent-soft quelpa use-package)
+  '(persistent-soft quelpa use-package async)
   "A list of packages that must be installed (and will be auto-installed if
 missing) and shouldn't be deleted.")
 
