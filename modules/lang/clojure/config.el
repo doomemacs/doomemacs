26c16d431bf56c2b3d359503906adde5fa69f35f
teesloane <tylersloane@gmail.com>
2017-12-05 09:58:46 -0500

    PR review updates: fix repl, remove unidiomatic use-package things.

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -3,18 +3,7 @@
 (def-package! clojure-mode
   :mode (("\\.clj$" . clojure-mode)
          ("\\.cljs$". clojurescript-mode))
-  :commands (clojure-mode clojurescript-mode)
   :config
-
-
-  (def-package! clj-refactor  ;; prob a better place to do this...using hooks?
-    :demand t
-    :config
-    ;; setup some extra namespace auto completion for great awesome
-    (dolist (mapping '(("re-frame" . "re-frame.core")
-                       ("reagent"  . "reagent.core")
-                       ("str"      . "clojure.str")))
-      (add-to-list 'cljr-magic-require-namespaces mapping t)))
 
   (map! :map clojure-mode-map
         (:localleader
@@ -30,15 +19,25 @@
          :n  "r"      #'cider-eval-region)))
 
 
+(def-package! clj-refactor
+  :after clojure-mode
+  :config
+
+  ;; setup some extra namespace auto completion for great awesome
+  (dolist (mapping '(("re-frame" . "re-frame.core")
+                     ("reagent"  . "reagent.core")
+                     ("str"      . "clojure.str")))
+    (add-to-list 'cljr-magic-require-namespaces mapping t)))
+
+
 (def-package! cider
-  ;; BUG: if you don't have an org directory set (the dir doesn't exist), cider jack in won't work.
+  ;; NOTE: if you don't have an org directory set (the dir doesn't exist), cider jack in won't work.
   :commands (cider-jack-in cider-mode cider-jack-in-clojurescript)
   :config
+
   (setq nrepl-hide-special-buffers t)
-
-  ;; TODO: figure out how to set cider to the right window rather than shackle window (https://github.com/hlissner/doom-emacs/issues/171)
-  ;; (set! :popup "^\\*cider-.*" :align 'left :noesc t) ;; no luck
-
+  ;; settings for cider repl as a popup (prevent it from being closed on escape, especially.)
+  (set! :popup '("^\\*cider"  :regexp t :noselect t :autokill f :autoclose f :noesc t))
   ;; Setup cider for clojurescript / figwheel dev.
   (setq cider-cljs-lein-repl
         "(do (require 'figwheel-sidecar.repl-api)
