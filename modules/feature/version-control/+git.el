961a55e764e68ff2f11e0a8ba12c7297c2311f75
Henrik Lissner <henrik@lissner.net>
2017-12-17 01:35:59 -0500

    Activate insert-mode in git-commit-mode #282

--- modules/feature/version-control/+git.el
+++ modules/feature/version-control/+git.el
@@ -1,6 +1,10 @@
 ;;; feature/version-control/+git.el -*- lexical-binding: t; -*-
 ;;;###if (not (featurep! -git))
 
+(when (featurep! :feature evil)
+  (add-hook 'git-commit-mode-hook #'evil-insert-state))
+
+
 (def-package! gitconfig-mode
   :mode "/\\.?git/?config$"
   :mode "/\\.gitmodules$")
