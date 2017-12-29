96ed8170d743de7f0dd9efb4a037a1e656fede64
Henrik Lissner <henrik@lissner.net>
2017-12-29 04:17:31 -0500

    Rewrite ui/doom-dashboard tests

--- modules/ui/doom-dashboard/test/doom-dashboard.el
+++ modules/ui/doom-dashboard/test/doom-dashboard.el
@@ -4,21 +4,31 @@
 (require! :ui doom-dashboard)
 (+doom-dashboard|init)
 
-(defun -dashboard-test-cwd (spec file)
-  (let ((+doom-dashboard-pwd-policy (car spec))
+(defun -dashboard-test-pwd (spec file)
+  (let ((kill-buffer-query-functions '(+doom-dashboard|reload-on-kill))
+        (+doom-dashboard-pwd-policy (car spec))
+        (fallback-buffer (doom-fallback-buffer))
         +doom-dashboard--last-cwd
         projectile-enable-caching)
     (with-temp-buffer
-      (setq-local buffer-file-name file)
-      (setq-local default-directory (file-name-directory file)))
+      (setq buffer-file-name file
+            default-directory (file-name-directory file)
+            doom-real-buffer-p t))
     (should +doom-dashboard--last-cwd)
-    (+doom-dashboard-reload t)
-    (should (equal (buffer-local-value 'default-directory (doom-fallback-buffer))
+    (+doom-dashboard-update-pwd)
+    (should (equal (buffer-local-value 'default-directory fallback-buffer)
                    (cdr spec)))))
 
 ;;
-(def-test! pwd-policy
-  (let ((default-directory doom-emacs-dir)
+(def-test! dashboard-p
+  (let ((fallback-buffer (doom-fallback-buffer)))
+    (should (equal (buffer-name fallback-buffer) +doom-dashboard-name))
+    (should (+doom-dashboard-p fallback-buffer))
+    (with-current-buffer fallback-buffer
+      (should (+doom-dashboard-p)))))
+
+(def-test! get-pwd
+  (let ((default-directory doom-core-dir)
         (+doom-dashboard--last-cwd doom-core-dir)
         projectile-enable-caching)
     (dolist (spec (list (cons 'last-project doom-emacs-dir)
@@ -29,19 +39,13 @@
       (let ((+doom-dashboard-pwd-policy (car spec)))
         (should (equal (+doom-dashboard--get-pwd) (cdr spec)))))))
 
-(def-test! kill-buffer-query
-  (let (+doom-dashboard--last-cwd)
-    (with-temp-buffer
-      (setq buffer-file-name (expand-file-name "core.el" doom-core-dir)
-            default-directory doom-core-dir)
-      (kill-buffer (current-buffer)))
-    (should (equal +doom-dashboard--last-cwd doom-core-dir))))
-
-(def-test! dashboard-reload-pwd-policy
+(def-test! pwd-policy
   (dolist (spec (list (cons 'last-project doom-emacs-dir)
                       (cons 'last doom-core-dir)
-                      (cons "~" (expand-file-name "~"))
-                      (cons (lambda (x) "/tmp") (expand-file-name "/tmp"))))
-    (-dashboard-test-cwd spec (expand-file-name "core.el" doom-core-dir))))
+                      (cons "~" (expand-file-name "~/"))
+                      (cons (lambda (x) "/tmp") "/tmp/")))
+    (-dashboard-test-pwd spec (expand-file-name "core.el" doom-core-dir))))
 
-
+;;
+(def-test! inhibit-refresh :skip t)
+(def-test! inhibit-functions :skip t)
