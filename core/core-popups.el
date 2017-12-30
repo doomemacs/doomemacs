5813b6e69a66105fd62d9b9d483b9c0106eda51f
Henrik Lissner <henrik@lissner.net>
2017-12-30 01:20:05 -0500

    Autokill help buffers

--- core/core-popups.el
+++ core/core-popups.el
@@ -122,7 +122,7 @@ recognized by DOOM's popup system. They are:
           ("*Backtrace*" :size 20 :noselect t)
           ("*Warnings*"  :size 12 :noselect t :autofit t)
           ("*Messages*"  :size 12 :noselect t)
-          ("*Help*" :size 0.3)
+          ("*Help*" :size 0.3 :autokill t)
           ("^\\*.*Shell Command.*\\*$" :regexp t :size 20 :noselect t :autokill t)
           (apropos-mode :size 0.3 :autokill t :autoclose t)
           (Buffer-menu-mode :size 20 :autokill t)
