e91cb1124391541f7b6b62d2fa67ce78d3a24a66
Undead Kernel <carloscg@gmail.com>
2018-01-01 20:48:23 +0100

    Latex module revamped
    Latex language module with previews, latexmk, reftex, bibtex and others.
    Completion with company mode.
    Selection of bibliography using Ivy or Helm.
    Later preview panel or okular as viewers.
    LatexMk for compiling code.
    Prettified indentation with adaptive-wrap along with good indentation of environments.
    Additional fontification of common commands.

---
+++ modules/lang/latex/autoload.el
@@ -0,0 +1,43 @@
+;;; lang/latex/autoload.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +latex/LaTeX-indent-item ()
+  "Provide proper indentation for LaTeX \"itemize\",\"enumerate\", and \"description\" environments.
+
+  \"\\item\" is indented `LaTeX-indent-level' spaces relative to
+  the the beginning of the environment.
+
+  Continuation lines are indented either twice
+  `LaTeX-indent-level', or `LaTeX-indent-level-item-continuation'
+  if the latter is bound."
+  (save-match-data
+    (let* ((offset LaTeX-indent-level)
+           (contin (or (and (boundp '+latex-indent-level-item-continuation)
+                            +latex-indent-level-item-continuation)
+                       (* 4 LaTeX-indent-level)))
+           (re-beg "\\\\begin{")
+           (re-end "\\\\end{")
+           (re-env "\\(itemize\\|\\enumerate\\|description\\)")
+           (indent (save-excursion
+                     (when (looking-at (concat re-beg re-env "}"))
+                       (end-of-line))
+                     (LaTeX-find-matching-begin)
+                     (current-column))))
+      (cond ((looking-at (concat re-beg re-env "}"))
+             (or (save-excursion
+                   (beginning-of-line)
+                   (ignore-errors
+                     (LaTeX-find-matching-begin)
+                     (+ (current-column)
+                        (if (looking-at (concat re-beg re-env "}"))
+                            contin
+                          offset))))
+                 indent))
+            ((looking-at (concat re-end re-env "}"))
+             indent)
+            ((looking-at "\\\\item")
+             (+ offset indent))
+            (t
+             (+ contin indent))))))
+
+
--- modules/lang/latex/packages.el
+++ modules/lang/latex/packages.el
@@ -2,7 +2,18 @@
 ;;; lang/latex/packages.el
 
 (package! auctex)
-;; (package! auctex-latexmk)
+(package! flyspell)
+(package! adaptive-wrap)
+
+;; Optional module features:
+
+(when (featurep! +latexmk)
+  (package! auctex-latexmk))
+
+(when (featurep! +preview-pane)
+  (package! latex-preview-pane))
+
+;; Features according to other user selected options
 
 (when (featurep! :completion company)
   (package! company-auctex))
@@ -10,3 +21,4 @@
   (package! ivy-bibtex))
 (when (featurep! :completion helm)
   (package! helm-bibtex))
+
