d738a8d13aa785d638633cdcc276c0d7d76f5a72
Henrik Lissner <henrik@lissner.net>
2018-01-06 04:15:36 -0500

    lang/org: fix unit test

--- modules/lang/org/test/org.el
+++ modules/lang/org/test/org.el
@@ -1,9 +1,5 @@
 ;;; lang/org/test/org.el -*- lexical-binding: t; -*-
 
-(when (featurep 'org)
-  (unload-feature 'org t))
 (require! :lang org)
-
-(require 'org (locate-library "org" nil doom--package-load-path))
 
 ;;
