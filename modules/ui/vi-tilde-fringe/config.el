8531b5245eb2c1b276c45ce1199caeabce6f7f43
Henrik Lissner <henrik@lissner.net>
2018-01-06 03:03:24 -0500

    ui/vi-tilde-fringe: use minor mode instead of global mode
    The global mode is a tad greedy, affecting buffers I don't want it to.
    So only activate it in certain places. This may change.

--- modules/ui/vi-tilde-fringe/config.el
+++ modules/ui/vi-tilde-fringe/config.el
@@ -2,10 +2,5 @@
 
 ;; indicators for empty lines past EOF
 (def-package! vi-tilde-fringe
-  :commands vi-tilde-fringe-mode
-  :hook (doom-init-ui . global-vi-tilde-fringe-mode)
-  :config
-  (add-hook! +doom-dashboard-mode
-    (when (bound-and-true-p vi-tilde-fringe-mode)
-      (vi-tilde-fringe-mode -1))))
+  :hook ((prog-mode text-mode conf-mode) . vi-tilde-fringe-mode))
 
