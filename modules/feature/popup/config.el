7872faad3ac2c2e2e49ef9ceb8ddf7410140fd85
Henrik Lissner <henrik@lissner.net>
2018-01-07 00:20:07 -0500

    feature/popup: don't forcibly hide modeline in org-agenda popups

--- modules/feature/popup/config.el
+++ modules/feature/popup/config.el
@@ -237,7 +237,6 @@ ALIST supports one custom parameter: `size', which will resolve to
   (setq org-agenda-window-setup 'other-window
         org-agenda-restore-windows-after-quit nil)
   ;; Hide modeline in org-agenda
-  (add-hook 'org-agenda-finalize-hook #'doom-hide-modeline-mode)
   (add-hook 'org-agenda-finalize-hook #'org-fit-window-to-buffer)
   ;; Don't monopolize frame!
   (advice-add #'org-agenda :around #'+popup*suppress-delete-other-windows))
