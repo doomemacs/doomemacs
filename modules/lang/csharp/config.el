6b164a6103c40cc4b9b5eb77fb7b9bb80788ea1e
Henrik Lissner <henrik@lissner.net>
2018-01-19 04:17:19 -0500

    lang/csharp: adapt to :lookup

--- modules/lang/csharp/config.el
+++ modules/lang/csharp/config.el
@@ -15,34 +15,37 @@
 
   (set! :company-backend 'csharp-mode '(company-omnisharp))
 
+  (set! :lookup 'csharp-mode
+    :definition #'omnisharp-go-to-definition
+    :references #'omnisharp-find-usages
+    :documentation #'omnisharp-current-type-documentation)
+
   (map! :map omnisharp-mode-map
-        :m "gd" #'omnisharp-go-to-definition
+        :localleader
+        :n "b" #'omnisharp-recompile
 
-        (:localleader
-         :n "b" #'omnisharp-recompile
+        (:prefix "r"
+          :n "i"  #'omnisharp-fix-code-issue-at-point
+          :n "u"  #'omnisharp-fix-usings
+          :n "r"  #'omnisharp-rename
+          :n "a"  #'omnisharp-show-last-auto-complete-result
+          :n "o"  #'omnisharp-show-overloads-at-point)
 
-         (:prefix "r"
-           :n "i"  #'omnisharp-fix-code-issue-at-point
-           :n "u"  #'omnisharp-fix-usings
-           :n "r"  #'omnisharp-rename
-           :n "a"  #'omnisharp-show-last-auto-complete-result
-           :n "o"  #'omnisharp-show-overloads-at-point)
+        (:prefix "f"
+          :n "u"  #'omnisharp-find-usages
+          :n "i"  #'omnisharp-find-implementations
+          :n "f"  #'omnisharp-navigate-to-current-file-member
+          :n "m"  #'omnisharp-navigate-to-solution-member
+          :n "M"  #'omnisharp-navigate-to-solution-file-then-file-member
+          :n "F"  #'omnisharp-navigate-to-solution-file
+          :n "r"  #'omnisharp-navigate-to-region
+          :n "ti" #'omnisharp-current-type-information
+          :n "td" #'omnisharp-current-type-documentation)
 
-         (:prefix "f"
-           :n "u"  #'omnisharp-find-usages
-           :n "i"  #'omnisharp-find-implementations
-           :n "f"  #'omnisharp-navigate-to-current-file-member
-           :n "m"  #'omnisharp-navigate-to-solution-member
-           :n "M"  #'omnisharp-navigate-to-solution-file-then-file-member
-           :n "F"  #'omnisharp-navigate-to-solution-file
-           :n "r"  #'omnisharp-navigate-to-region
-           :n "ti" #'omnisharp-current-type-information
-           :n "td" #'omnisharp-current-type-documentation)
-
-         (:prefix "t"
-           :n "r" (λ! (omnisharp-unit-test "fixture"))
-           :n "s" (λ! (omnisharp-unit-test "single"))
-           :n "a" (λ! (omnisharp-unit-test "all"))))))
+        (:prefix "t"
+          :n "r" (λ! (omnisharp-unit-test "fixture"))
+          :n "s" (λ! (omnisharp-unit-test "single"))
+          :n "a" (λ! (omnisharp-unit-test "all")))))
 
 
 (def-package! shader-mode :mode "\\.shader$") ; unity shaders
