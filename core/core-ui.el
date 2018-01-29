08908260831c4eaa32633f533ace9bcf95a92845
Henrik Lissner <henrik@lissner.net>
2018-01-29 17:09:09 -0500

    window-divider-default-bottom-width = 1

--- core/core-ui.el
+++ core/core-ui.el
@@ -309,7 +309,7 @@ DEFAULT is non-nil, set the default mode-line for all buffers."
 ;; The native border "consumes" a pixel of the fringe on righter-most splits,
 ;; `window-divider' does not. Available since Emacs 25.1.
 (setq-default window-divider-default-places t
-              window-divider-default-bottom-width 0
+              window-divider-default-bottom-width 1
               window-divider-default-right-width 1)
 (add-hook 'doom-init-ui-hook #'window-divider-mode)
 
