03a03853e42eeb05be5cbaeb631bc69c62694aed
Henrik Lissner <henrik@lissner.net>
2018-02-01 19:57:46 -0500

    core-editor: reset expand-region on doom/escape #317

--- core/core-editor.el
+++ core/core-editor.el
@@ -213,7 +213,8 @@ extension, try to guess one."
   (defun doom*quit-expand-region ()
     (when (memq last-command '(er/expand-region er/contract-region))
       (er/contract-region 0)))
-  (advice-add #'evil-escape :before #'doom*quit-expand-region))
+  (advice-add #'evil-escape :before #'doom*quit-expand-region)
+  (advice-add #'doom/escape :before #'doom*quit-expand-region))
 
 (def-package! helpful
   :commands (helpful-callable helpful-function helpful-macro helpful-command
