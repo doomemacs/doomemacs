871883cd891af6a85b6fd61ea69eea804c9fa941
Henrik Lissner <henrik@lissner.net>
2018-02-14 07:44:23 -0500

    lang/hy: set :repl, :company-backend & :interpreter

--- modules/lang/hy/config.el
+++ modules/lang/hy/config.el
@@ -1,4 +1,8 @@
 ;;; lang/hy/config.el -*- lexical-binding: t; -*-
 
 (def-package! hy-mode
-  :mode "\\.hy$")
+  :mode "\\.hy$"
+  :interpreter "hy"
+  :config
+  (set! :repl 'hy-mode #'hy-shell-start-or-switch-to-shell)
+  (set! :company-backend 'hy-mode '(company-hy)))
