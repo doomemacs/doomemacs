5ad4246cb9ffcff608d4dd2f6c40ba640d0382b0
Henrik Lissner <henrik@lissner.net>
2018-02-14 06:08:25 -0500

    lang/clojure: refactor; rainbow-delimiters-mode; :lookup
    Also add auto-mode-alist entry for clojurec-mode

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -3,19 +3,22 @@
 (def-package! clojure-mode
   :mode "\\.clj$"
   :mode ("\\.cljs$" . clojurescript-mode)
+  :mode ("\\.cljc$" . clojurec-mode)
   :config
+  (add-hook 'clojure-mode #'rainbow-delimiters-mode)
+
   (map! :map clojure-mode-map
-        (:localleader
-         :n  "'"      #'cider-jack-in
-         :n  "\""     #'cider-jack-in-clojurescript
-         :n  "B"      #'cider-switch-to-repl-buffer
-         :n  "b"      #'cider-eval-buffer
-         :n  "n"      #'cider-repl-set-ns
-         :n  "j"      #'cider-find-var
-         :n  "d"      #'cider-doc
-         :n  "c"      #'cider-repl-clear-buffer
-         :n  "p"      #'cider-eval-sexp-at-point
-         :n  "r"      #'cider-eval-region)))
+        :localleader
+        :n  "'"  #'cider-jack-in
+        :n  "\"" #'cider-jack-in-clojurescript
+        :n  "B"  #'cider-switch-to-repl-buffer
+        :n  "b"  #'cider-eval-buffer
+        :n  "n"  #'cider-repl-set-ns
+        :n  "j"  #'cider-find-var
+        :n  "d"  #'cider-doc
+        :n  "c"  #'cider-repl-clear-buffer
+        :n  "p"  #'cider-eval-sexp-at-point
+        :n  "r"  #'cider-eval-region))
 
 
 (def-package! clj-refactor
@@ -29,13 +32,19 @@
 
 
 (def-package! cider
-  ;; NOTE: if you don't have an org directory set (the dir doesn't exist), cider jack in won't work.
+  ;; NOTE: if you don't have an org directory set (the dir doesn't exist), cider
+  ;; jack in won't work.
   :commands (cider-jack-in cider-mode cider-jack-in-clojurescript)
   :config
   (setq nrepl-hide-special-buffers t)
 
-  ;; settings for cider repl as a popup (prevent it from being closed on escape, especially.)
+  ;; settings for cider repl as a popup (prevent it from being closed on escape,
+  ;; especially.)
   (set! :popup "^\\*cider" nil '((quit) (select)))
+
+  (set! :lookup 'clojure-mode
+    :definition #'cider-browse-ns-find-at-point
+    :documentation #'cider-browse-ns-doc-at-point)
 
   ;; Setup cider for clojurescript / figwheel dev.
   (setq cider-cljs-lein-repl
