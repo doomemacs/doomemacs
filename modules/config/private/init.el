a1580e59199b457c17b6b628a8a3f584af62b3d8
Henrik Lissner <henrik@lissner.net>
2018-02-17 18:41:47 -0500

    config/private: wrap init.el in file-directory-p check

--- modules/config/private/init.el
+++ modules/config/private/init.el
@@ -7,12 +7,13 @@
   "The directory that serves as the root of your external private config for
 Doom Emacs.")
 
-;; Ensure `doom//reload-autoloads', `doom//byte-compile' and
-;; `doom-initialize-packages' will treat `+private-config-path' as the root of
-;; this module.
-(add-to-list 'doom-psuedo-module-dirs +private-config-path)
-(add-to-list 'load-path +private-config-path)
+(when (file-directory-p +private-config-path)
+  ;; Ensure `doom//reload-autoloads', `doom//byte-compile' and
+  ;; `doom-initialize-packages' will treat `+private-config-path' as the root of
+  ;; this module.
+  (add-to-list 'doom-psuedo-module-dirs +private-config-path)
+  (add-to-list 'load-path +private-config-path)
 
-;;
-(load (expand-file-name "init.el" +private-config-path)
-      'noerror 'nomessage)
+  ;;
+  (load (expand-file-name "init.el" +private-config-path)
+        :noerror :nomessage))
