be94fc4d910a2d19ea3bacdc081db9763c2543e2
Henrik Lissner <henrik@lissner.net>
2018-02-18 03:07:45 -0500

    lang/org: remove unused evil autoloads

--- modules/lang/org/autoload/org-capture.el
+++ modules/lang/org/autoload/org-capture.el
@@ -1,16 +1,6 @@
 ;;; lang/org/autoload/org-capture.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! +capture)
 
-(when (featurep! :feature evil)
-;;;###autoload (autoload '+org-capture:open "lang/org/autoload/org-capture" nil t)
-  (evil-define-operator +org-capture:open (&optional beg end)
-    "Evil ex interface to `+org-capture/dwim'."
-    :move-point nil :type inclusive
-    (interactive "<r>")
-    (+org-capture/open
-     (unless (or (evil-normal-state-p) (evil-insert-state-p))
-       (buffer-substring beg end)))))
-
 ;;;###autoload
 (defun +org-capture/open (&optional string key)
   "Sends STRING, the current selection or prompted input to `org-capture'.
