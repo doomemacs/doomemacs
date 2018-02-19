634844e582acfedaa2c0f9036e937f84233435cb
Henrik Lissner <henrik@lissner.net>
2018-02-18 20:10:41 -0500

    tools/pdf: fix epdfinfo prompt at compile time; autoload pdf-tools

--- modules/tools/pdf/config.el
+++ modules/tools/pdf/config.el
@@ -1,10 +1,14 @@
 ;;; tools/pdf/config.el -*- lexical-binding: t; -*-
 
 (def-package! pdf-tools
-  :defer t
-  :init (load "pdf-tools-autoloads.el" nil t t)
+  :mode ("\\.pdf$" . pdf-view-mode)
+  :init (load "pdf-tools-autoloads" nil t)
   :config
-  (pdf-tools-install)
+  (unless noninteractive
+    (pdf-tools-install))
+
+  (map! :map pdf-view-mode-map "q" #'kill-this-buffer)
+
   (setq-default pdf-view-display-size 'fit-page)
   ;; turn off cua so copy works
   (add-hook! 'pdf-view-mode-hook (cua-mode 0)))
