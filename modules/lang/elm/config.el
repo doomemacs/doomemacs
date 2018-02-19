45c8ce9f62846199d67f64a6b527b29c60157814
Henrik Lissner <henrik@lissner.net>
2018-02-18 21:25:05 -0500

    lang/elm: fix elm-mode-autoloads typo

--- modules/lang/elm/config.el
+++ modules/lang/elm/config.el
@@ -3,7 +3,7 @@
 (def-package! elm-mode
   :mode "\\.elm$"
   :config
-  (load "elm-mode-autolods" nil t)
+  (load "elm-mode-autoloads" nil t)
   (add-hook! 'elm-mode-hook #'(flycheck-mode rainbow-delimiters-mode))
   (set! :company-backend 'elm-mode '(company-elm))
   (set! :repl 'elm-mode #'run-elm-interactive)
