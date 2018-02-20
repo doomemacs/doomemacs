b5e53b82b9ac449d5580a3175dadb2487616407a
Henrik Lissner <henrik@lissner.net>
2018-02-19 20:21:11 -0500

    config/default: SPC f d = dired

--- modules/config/default/+bindings.el
+++ modules/config/default/+bindings.el
@@ -177,6 +177,7 @@
           :desc "Find file from here"       :n "?" #'counsel-file-jump
           :desc "Find other file"           :n "a" #'projectile-find-other-file
           :desc "Open project editorconfig" :n "c" #'editorconfig-find-current-editorconfig
+          :desc "Find directory"            :n "d" #'dired
           :desc "Find file in emacs.d"      :n "e" #'+default/find-in-emacsd
           :desc "Browse emacs.d"            :n "E" #'+default/browse-emacsd
           :desc "Recent files"              :n "r" #'recentf-open-files
