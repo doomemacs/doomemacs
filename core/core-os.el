cacf13f2265a386fdd6edfca6a1538f38561cd8a
Henrik Lissner <henrik@lissner.net>
2018-02-27 22:39:10 -0500

    Don't byte-compile exec-path on MacOS

--- core/core-os.el
+++ core/core-os.el
@@ -36,15 +36,11 @@
               ;; environment, so envvars will be wrong. That includes the PATH
               ;; Emacs picks up. `exec-path-from-shell' fixes this. This is slow
               ;; and benefits greatly from compilation.
-              (setq exec-path
-                    (or (eval-when-compile
-                          (when (require 'exec-path-from-shell nil t)
-                            (setq exec-path-from-shell-check-startup-files nil
-                                  exec-path-from-shell-arguments (delete "-i" exec-path-from-shell-arguments))
-                            (nconc exec-path-from-shell-variables '("GOPATH" "GOROOT" "PYTHONPATH"))
-                            (exec-path-from-shell-initialize)
-                            exec-path))
-                        exec-path)))
+              (when (require 'exec-path-from-shell nil t)
+                (setq exec-path-from-shell-check-startup-files nil
+                      exec-path-from-shell-arguments (delete "-i" exec-path-from-shell-arguments))
+                (nconc exec-path-from-shell-variables '("GOPATH" "GOROOT" "PYTHONPATH"))
+                (exec-path-from-shell-initialize)))
              (t
               (when (require 'osx-clipboard nil t)
                 (osx-clipboard-mode +1)))))
