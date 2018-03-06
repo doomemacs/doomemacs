4086ffd8e2e4d019b73fc75477e07706d0f9b574
Henrik Lissner <henrik@lissner.net>
2018-03-05 23:01:17 -0500

    config/private: ensure config.el loads as late as possible #453

--- modules/config/private/config.el
+++ modules/config/private/config.el
@@ -1,4 +1,8 @@
 ;;; config/private/config.el -*- lexical-binding: t; -*-
 
-(load (expand-file-name "config.el" +private-config-path)
-      'noerror 'nomessage)
+(defun +private|load-config ()
+  "Loads your private config.el in `+private-config-path'."
+  (load (expand-file-name "config.el" +private-config-path)
+        'noerror 'nomessage))
+
+(add-hook 'after-init-hook #'+private|load-config)
