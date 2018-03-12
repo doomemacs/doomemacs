4f0dc72962117703e62cb4143d9d1ed3b2cd1dba
Henrik Lissner <henrik@lissner.net>
2018-03-12 13:19:31 -0400

    lang/org: fix smartparens autopairing in org-mode

--- modules/lang/org/config.el
+++ modules/lang/org/config.el
@@ -100,11 +100,11 @@ unfold to point on startup."
   ;; make delimiter auto-closing a little more conservative
   (after! smartparens
     (sp-with-modes 'org-mode
-      (sp-local-pair "*" nil :unless '(sp-point-after-word-p sp-point-before-word-p sp-point-at-bol-p))
-      (sp-local-pair "_" nil :unless '(sp-point-after-word-p sp-point-before-word-p))
-      (sp-local-pair "/" nil :unless '(sp-point-after-word-p sp-point-before-word-p +org-sp-point-in-checkbox-p))
-      (sp-local-pair "~" nil :unless '(sp-point-after-word-p sp-point-before-word-p))
-      (sp-local-pair "=" nil :unless '(sp-point-after-word-p sp-point-before-word-p)))))
+      (sp-local-pair "*" nil :unless '(:add sp-point-before-word-p sp-point-at-bol-p))
+      (sp-local-pair "_" nil :unless '(:add sp-point-before-word-p))
+      (sp-local-pair "/" nil :unless '(:add sp-point-before-word-p +org-sp-point-in-checkbox-p))
+      (sp-local-pair "~" nil :unless '(:add sp-point-before-word-p))
+      (sp-local-pair "=" nil :unless '(:add sp-point-before-word-p)))))
 
 (defun +org|enable-auto-reformat-tables ()
   "Realign tables exiting insert mode (`evil-mode')."
