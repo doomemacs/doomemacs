683e98895239cc05683d75c8dac8380a1c6f7b7e
Henrik Lissner <henrik@lissner.net>
2018-03-13 01:28:22 -0400

    feature/snippets: refactor +snippets/expand-on-region

--- modules/feature/snippets/autoload/evil.el
+++ modules/feature/snippets/autoload/evil.el
@@ -11,8 +11,6 @@ and switches to insert mode if there are editable fields."
   (cl-letf (((symbol-function 'region-beginning) (lambda () evil-visual-beginning))
             ((symbol-function 'region-end)       (lambda () evil-visual-end)))
     (yas-insert-snippet))
-  (when-let* ((snippet (car-safe (yas-active-snippets))))
-    (let ((fields (yas--snippet-fields snippet)))
-      (evil-insert-state +1)
-      (unless fields (evil-change-state 'normal)))))
+  (when (yas-active-snippets)
+    (evil-insert-state +1)))
 
