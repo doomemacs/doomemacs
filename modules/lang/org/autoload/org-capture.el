c0e5152b8f54d2e493bd0ac98fca85c87b91b02e
Henrik Lissner <henrik@lissner.net>
2018-03-14 17:25:25 -0400

    lang/org: replace +org-capture/open with org-capture

--- modules/lang/org/autoload/org-capture.el
+++ modules/lang/org/autoload/org-capture.el
@@ -1,23 +1,6 @@
 ;;; lang/org/autoload/org-capture.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! +capture)
 
-;;;###autoload
-(defun +org-capture/open (&optional string key)
-  "Sends STRING, the current selection or prompted input to `org-capture'.
-
-Uses the capture template specified by KEY. Otherwise, prompts you for one."
-  (interactive)
-  (let ((key (or key "n")))
-    (if-let* ((string (cond ((not (equal string ""))
-                             string)
-                            ((region-active-p)
-                             (buffer-substring-no-properties
-                              (region-beginning)
-                              (region-end))))))
-        (org-capture-string string key)
-      (org-capture nil key))))
-
-
 ;; --- External frame ---------------------
 
 (defvar +org-capture-window-params
