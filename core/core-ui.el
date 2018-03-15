2a45b80e2a77a1966c4df8669d88862a5f04d554
Henrik Lissner <henrik@lissner.net>
2018-03-15 04:49:49 -0400

    Revert cfe2fe87; needs better checks

--- core/core-ui.el
+++ core/core-ui.el
@@ -417,8 +417,6 @@ character that looks like a space that `whitespace-mode' won't affect.")
 ;; Theme & font
 ;;
 
-(defvar doom-last-frame-type window-system)
-
 (defun doom|init-theme ()
   "Set the theme and load the font, in that order."
   (when doom-theme
@@ -461,9 +459,7 @@ character that looks like a space that `whitespace-mode' won't affect.")
 ;; frames, however. There's always `doom//reload-theme' if you need it!
 (defun doom|init-theme-in-frame (frame)
   "Reloads the theme in new daemon or tty frames."
-  (when (and (not (eq doom-last-frame-type window-system))
-             (or (daemonp) (not (display-graphic-p))))
-    (setq doom-last-frame-type window-system)
+  (when (or (daemonp) (not (display-graphic-p)))
     (with-selected-frame frame
       (doom|init-theme))))
 
