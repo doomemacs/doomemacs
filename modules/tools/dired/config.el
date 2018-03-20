328269168fd3803febfa7e929c64993be9ce2ea2
Henrik Lissner <henrik@lissner.net>
2018-03-19 23:55:45 -0400

    tools/dired: fix redundancy in image-dired-db-file

--- modules/tools/dired/config.el
+++ modules/tools/dired/config.el
@@ -11,7 +11,7 @@
         auto-revert-verbose nil
         ;; files
         image-dired-dir (concat doom-cache-dir "image-dired/")
-        image-dired-db-file (concat image-dired-dir "image-dired/db.el")
+        image-dired-db-file (concat image-dired-dir "db.el")
         image-dired-gallery-dir (concat image-dired-dir "gallery/")
         image-dired-temp-image-file (concat image-dired-dir "temp-image")
         image-dired-temp-rotate-image-file (concat image-dired-dir "temp-rotate-image"))
