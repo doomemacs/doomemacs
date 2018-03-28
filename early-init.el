0e1b2453aa506682db3ddf49472bb5e1d9a946e4
Henrik Lissner <henrik@lissner.net>
2018-03-28 00:41:57 -0400

    Revise comments in early-init.el

--- early-init.el
+++ early-init.el
@@ -1,7 +1,9 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;; Later versions of Emacs 27 (on master) introduce a new behavior:
-;;
+;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; before most of its package and UI initialization happens. We can use this
+;; opportunity to cull parts of the startup process early.
+
 ;; Package initialize occurs automatically, before `user-init-file' is loaded,
 ;; but after `early-init-file'. Doom handles package initialization, so we must
 ;; prevent Emacs from doing it early!
@@ -11,3 +13,5 @@
 (setq tool-bar-mode nil
       menu-bar-mode nil)
 (set-scroll-bar-mode nil)
+
+;; TODO Once Emacs 27 hits stable, perhaps replace init.el with early-init.el
