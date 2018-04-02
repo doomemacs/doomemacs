fbf80489da7500bb5ab744f4a303738bd5b12b4f
Benjamin Andresen <bandresen@gmail.com>
2018-04-02 08:07:44 +0200

    Document: app/irc: add docstrings for all variables

--- modules/app/irc/config.el
+++ modules/app/irc/config.el
@@ -1,7 +1,9 @@
 ;;; app/irc/config.el -*- lexical-binding: t; -*-
 
 (defvar +irc-left-padding 13
-  "TODO")
+  "By how much spaces the left hand side of the line should be padded.
+Below a value of 12 this may result in uneven alignment between the various
+types of messages.")
 
 (defvar +irc-truncate-nick-char ?…
   "Character to displayed when nick > `+irc-left-padding' in length.")
@@ -11,16 +13,23 @@
   "If these commands are called pre prompt the buffer will scroll to `point-max'.")
 
 (defvar +irc-disconnect-hook nil
-  "TODO")
+  "Runs each hook when circe noticies the connection has been disconnected.
+Useful for scenarios where an instant reconnect will not be successful.")
 
 (defvar +irc-bot-list '("fsbot" "rudybot")
-  "TODO")
+  "Nicks listed have `circe-fool-face' applied and will not be tracked.")
 
 (defvar +irc-time-stamp-format "%H:%M"
-  "TODO")
+  "The format of time stamps.
+
+See `format-time-string' for a full description of available
+formatting directives. ")
 
 (defvar +irc-notifications-watch-strings nil
-  "TODO")
+  "A list of strings which can trigger a notification.  You don't need to put
+your nick here.
+
+See `circe-notifications-watch-strings'.")
 
 (defvar +irc-defer-notifications nil
   "How long to defer enabling notifications, in seconds (e.g. 5min = 300).
