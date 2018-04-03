0739e8cd0a84ddaaf13ab4e74c0f42001219c233
Henrik Lissner <henrik@lissner.net>
2018-04-03 04:20:08 -0400

    completion/helm: fix void-function with-helm-buffer error
    Presumably caused by eager expansion trying to expand the macro before
    helm is loaded.

--- modules/completion/helm/config.el
+++ modules/completion/helm/config.el
@@ -62,7 +62,7 @@
 
   (defun +helm*hide-minibuffer-maybe ()
     "Hide minibuffer in Helm session if we use the header line as input field."
-    (when (with-helm-buffer helm-echo-input-in-header-line)
+    (when (buffer-local-value 'helm-echo-input-in-header-line (helm-buffer-get))
       (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
         (overlay-put ov 'window (selected-window))
         (overlay-put ov 'face
