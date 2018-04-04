0026f08e27c9aa5f2000088777388650a2c6f7ca
Henrik Lissner <henrik@lissner.net>
2018-04-03 23:31:04 -0400

    Update init.{example,test}.el & travis for new config policy

--- init.test.el
+++ init.test.el
@@ -1,21 +1,14 @@
 ;;; init.test.el -- for automated unit tests -*- lexical-binding: t; -*-
 
-(setq doom-private-dir nil)
-(require 'core (concat user-emacs-directory "core/core"))
-
 (doom! :feature
        evil
        workspaces
-
        :completion
        company
-
        :ui
        doom-dashboard
-
        :tools
        password-store
-
        :lang
        org
        web)
