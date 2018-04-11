ea7663d4ea26c296a36ca56db35e709773dcd2b2
UndeadKernel <carloscg@gmail.com>
2018-04-11 12:31:33 +0200

    EIN module to work with Jupyter notebooks

--- init.example.el
+++ init.example.el
@@ -41,6 +41,7 @@
 
        :tools
        dired             ; making dired pretty [functional]
+       ein               ; tame Jupyter notebooks with emacs
        electric-indent   ; smarter, keyword-based electric-indent
        eshell            ; a consistent, cross-platform shell (WIP)
        gist              ; interacting with github gists
---
+++ modules/tools/ein/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; tools/ein/packages.el
+
+(package! ein)
