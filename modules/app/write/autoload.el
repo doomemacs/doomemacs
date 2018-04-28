c23974e1d5c2af6c0f47dc0ba13dffbb1acea964
Henrik Lissner <henrik@lissner.net>
2018-04-28 03:28:59 -0400

    app/write: new +write-mode-map keymap

--- modules/app/write/autoload.el
+++ modules/app/write/autoload.el
@@ -1,10 +1,14 @@
 ;;; app/write/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
+(defvar +write-mode-map (make-sparse-keymap)
+  "TODO")
+
+;;;###autoload
 (define-minor-mode +write-mode
   "Turns Emacs into a more comfortable writing environment and word processor."
   :init-value nil
-  :keymap nil
+  :keymap +write-mode-map
   (setq-local visual-fill-column-center-text t)
   (when +write-text-scale
     (text-scale-set (if +write-mode 2 0)))
