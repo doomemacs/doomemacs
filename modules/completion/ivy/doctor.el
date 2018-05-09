b3f38a8bed6e3529a31fb738abffd47f5152e95d
Henrik Lissner <henrik@lissner.net>
2018-05-09 10:21:42 +0200

    completion/ivy: fix header vars in doctor.el

--- modules/completion/ivy/doctor.el
+++ modules/completion/ivy/doctor.el
@@ -1,4 +1,5 @@
-;;; completion/ivy/doctor.el -*- lexical-binding: t; -*-
+;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; completion/ivy/doctor.el
 
 (when (and (not EMACS26+) (featurep! +childframe))
   (error! "The +childframe feature requires Emacs 26+"))
