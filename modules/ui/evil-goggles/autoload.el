3e6d6279a692b1a2a173cda32a4d0b5dde1e9d30
Henrik Lissner <henrik@lissner.net>
2018-05-16 13:06:07 +0200

    ui/evil-goggles: remove unnecessary toggle wrapper command

--- modules/ui/evil-goggles/autoload.el
+++
@@ -1,9 +0,0 @@
-;;; feature/ui/evil-goggles/autoload.el -*- lexical-binding: t; -*-
-
-;;;###autoload
-(defun +evil-goggles/toggle ()
-  "Toggle evil goggles mode."
-  (interactive)
-  (if evil-goggles-mode
-      (evil-goggles-mode -1)
-    (evil-goggles-mode +1)))
