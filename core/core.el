a19a64b16b29ced9610ffc10c5fc03e9ade20cb5
Henrik Lissner <henrik@lissner.net>
2018-05-19 18:01:49 +0200

    Complain if using Emacs <25
    More helpful than advice-add errors.

--- core/core.el
+++ core/core.el
@@ -1,5 +1,10 @@
 ;;; core.el --- the heart of the beast -*- lexical-binding: t; -*-
 
+(eval-when-compile
+  (when (version< emacs-version "25")
+    (error "Doom only supports Emacs 25.1 and higher!")))
+
+;;
 (defvar doom-version "2.0.9"
   "Current version of DOOM emacs.")
 
