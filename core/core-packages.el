dc7488df73c66f15a80bfecbe1268a04a8b3801d
Henrik Lissner <henrik@lissner.net>
2018-05-25 19:26:08 +0200

    Remove package-initialize hack before package commands
    Doesn't appear to be necessary anymore.

--- core/core-packages.el
+++ core/core-packages.el
@@ -803,15 +803,6 @@ loads MODULE SUBMODULE's packages.el file."
 ;; Make package.el cooperate with Doom
 ;;
 
-(defun doom*initialize-packages (&rest _)
-  (unless package--initialized
-    (package-initialize)))
-
-(advice-add #'package-delete           :before #'doom*initialize-packages)
-(advice-add #'package-install          :before #'doom*initialize-packages)
-(advice-add #'package-refresh-contents :before #'doom*initialize-packages)
-(advice-add #'package-reinstall        :before #'doom*initialize-packages)
-
 ;; Updates QUELPA after deleting a package
 (advice-add #'package-delete :after #'doom*package-delete)
 
