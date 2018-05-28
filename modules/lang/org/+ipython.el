21631aa8bf7f9e222a9b3bcc486542f2731db0d2
fuxialexander <fuxialexander@gmail.com>
2018-05-28 14:09:03 +0800

    Add: org: +ipython `+right-popup` option

--- modules/lang/org/+ipython.el
+++ modules/lang/org/+ipython.el
@@ -8,34 +8,35 @@
   :config
   (setq ob-ipython-resources-dir +org-ob-ipython-resources-dir)
   ;; popup
-  (set!
-    :popup "^\\*Org Src"
-    '((size . 100)
-      (side . right)
-      (slot . -1)
-      (window-height . 0.6))
-    '((quit)
-      (select . t)
-      (modeline)))
-  (set!
-    :popup "^\\*Python"
-    '((slot . 0)
-      (side . right)
-      (size . 100))
-    '((select) (quit) (transient)))
-  (set!
-    :popup "\\*ob-ipython.*"
-    '((slot . 2)
-      (side . right)
-      (size . 100)
-      (window-height . 0.2))
-    '((select) (quit) (transient)))
-  (set!
-    :popup "\\*Python:.*"
-    '((slot . 0)
-      (side . right)
-      (size . 100))
-    '((select) (quit) (transient)))
+  (when (featurep! +right-popup)
+    (set!
+      :popup "^\\*Org Src"
+      '((size . 100)
+        (side . right)
+        (slot . -1)
+        (window-height . 0.6))
+      '((quit)
+        (select . t)
+        (modeline)))
+    (set!
+      :popup "^\\*Python"
+      '((slot . 0)
+        (side . right)
+        (size . 100))
+      '((select) (quit) (transient)))
+    (set!
+      :popup "\\*ob-ipython.*"
+      '((slot . 2)
+        (side . right)
+        (size . 100)
+        (window-height . 0.2))
+      '((select) (quit) (transient)))
+    (set!
+      :popup "\\*Python:.*"
+      '((slot . 0)
+        (side . right)
+        (size . 100))
+      '((select) (quit) (transient))))
   ;; advices for remote kernel and org-src-edit
   (advice-add 'org-babel-edit-prep:ipython :override #'+org*org-babel-edit-prep:ipython)
   (advice-add 'org-babel-ipython-initiate-session :override #'+org*org-babel-ipython-initiate-session)
