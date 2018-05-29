6bb59467659ac52445bbcb7413be80b64ae6ea8f
Henrik Lissner <henrik@lissner.net>
2018-05-29 15:34:45 +0200

    tools/pdf: rename +pdf-tools-pages segment => +pdf-pages

--- modules/tools/pdf/+modeline.el
+++ modules/tools/pdf/+modeline.el
@@ -1,10 +1,9 @@
 ;;; tools/pdf/+modeline.el -*- lexical-binding: t; -*-
 
-
-(def-modeline-segment! +pdf-tools-pages
+(def-modeline-segment! +pdf-pages
   "Current and total page indicator for PDF documents."
   (format " P %d/%d" (pdf-view-current-page) (pdf-cache-number-of-pages)))
 
 (def-modeline! pdf-tools-modeline
-  (bar matches " " buffer-info  +pdf-tools-pages)
+  (bar matches " " buffer-info +pdf-pages)
   (major-mode vcs))
