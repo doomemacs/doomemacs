1f5dae917677fc32ef75645c2d5225293c79d893
Henrik Lissner <henrik@lissner.net>
2018-05-31 11:32:26 +0200

    Merge evil-collection-anaconda into lang/python #628
    This is the first step of removing evil-collection from Doom.

--- modules/feature/evil/config.el
+++ modules/feature/evil/config.el
@@ -4,8 +4,9 @@
 ;; strives to make Emacs a much better vim than vim was.
 
 (defvar +evil-collection-disabled-list
-  '(kotlin-mode ; doesn't do anything useful
-    simple)     ; ditto
+  '(kotlin-mode     ; doesn't do anything useful
+    simple
+    anaconda-mode)  ; we'll do it ourselves
   "A list of `evil-collection' modules to disable. See the definition of this
 variable for an explanation of the defaults (in comments). See
 `evil-collection-mode-list' for a list of available options.")
--- modules/lang/python/config.el
+++ modules/lang/python/config.el
@@ -124,3 +124,15 @@ environment variables."
         :n "O" #'nosetests-pdb-one
         :n "V" #'nosetests-pdb-module))
 
+
+;;
+;; Evil integration
+;;
+
+(when (featurep! :feature evil +everywhere)
+  (add-hook 'anaconda-mode-hook #'evil-normalize-keymaps)
+  (map! :after anaconda-mode
+        :map anaconda-view-mode-map
+        :m "]]" #'anaconda-view-mode-next-definition
+        :m "[[" #'anaconda-view-mode-previous-definition
+        :n "q"  #'quit-window))
