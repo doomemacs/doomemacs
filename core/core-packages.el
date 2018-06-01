a0bec8499761f1c8cec8ee07ede0e734e630e46f
Henrik Lissner <henrik@lissner.net>
2018-06-02 01:08:23 +0200

    Don't complain if no private packages.el

--- core/core-packages.el
+++ core/core-packages.el
@@ -340,11 +340,11 @@ them."
               ;; We load the private packages file twice to ensure disabled
               ;; packages are seen ASAP, and a second time to ensure privately
               ;; overridden packages are properly overwritten.
-              (_load (expand-file-name "packages.el" doom-private-dir))
+              (_load (expand-file-name "packages.el" doom-private-dir) t)
               (cl-loop for key being the hash-keys of doom-modules
                        for path = (doom-module-path (car key) (cdr key) "packages.el")
                        do (let ((doom--current-module key)) (_load path t)))
-              (_load (expand-file-name "packages.el" doom-private-dir)))))))))
+              (_load (expand-file-name "packages.el" doom-private-dir) t))))))))
 
 
 ;;
