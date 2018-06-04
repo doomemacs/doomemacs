686d102ee7372b6c5b0f8dd08ca09527c0c4a4ed
Edmund Miller <eman0088@gmail.com>
2018-06-04 18:42:15 -0500

    Add conda

---
+++ modules/lang/python/+conda.el
@@ -0,0 +1,18 @@
+;;; lang/python/+conda.el -*- lexical-binding: t; -*-
+;;;###if (featurep! +conda)
+
+(def-package! conda
+  :when (featurep! +conda)
+  :after (python)
+  :init
+  (defvar +python-conda-home '("~/.anaconda3" "/usr/bin/anaconda3" "~/.anaconda")
++  "A list of host pattern and corresponding anaconda home.")
+  :config
+  (advice-add 'anaconda-mode-bootstrap :override #'*anaconda-mode-bootstrap)
+  (conda-env-autoactivate-mode -1)
+  ;; (add-hook 'python-mode-hook #'conda-env-activate-for-buffer)
+  (conda-env-initialize-interactive-shells)
+  (conda-env-initialize-eshell)
+  ;; Version management with conda
+  (add-hook 'conda-postactivate-hook #'+python|add-version-to-modeline)
+  (add-hook 'conda-postdeactivate-hook #'+python|add-version-to-modeline))
