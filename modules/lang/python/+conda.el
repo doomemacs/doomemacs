732c2979c45840c6927c9b60c71f89dfde930820
fuxialexander <fuxialexander@gmail.com>
2018-06-07 01:06:02 +0800

    Naming convention and add docs; Avoid conflicting pyenv.

--- modules/lang/python/+conda.el
+++ modules/lang/python/+conda.el
@@ -8,11 +8,11 @@
   (defvar +python-conda-home '("~/.anaconda3" "/usr/bin/anaconda3" "~/.anaconda")
     "A list of host pattern and corresponding anaconda home.")
   :config
-  (advice-add 'anaconda-mode-bootstrap :override #'*anaconda-mode-bootstrap)
+  (advice-add 'anaconda-mode-bootstrap :override #'+python*anaconda-mode-bootstrap)
   (conda-env-autoactivate-mode -1)
   ;; (add-hook 'python-mode-hook #'conda-env-activate-for-buffer)
   (conda-env-initialize-interactive-shells)
   (conda-env-initialize-eshell)
   ;; Version management with conda
-  (add-hook 'conda-postactivate-hook #'+python|add-version-to-modeline)
-  (add-hook 'conda-postdeactivate-hook #'+python|add-version-to-modeline))
+  (add-hook 'conda-postactivate-hook #'+python|add-conda-env-to-modeline)
+  (add-hook 'conda-postdeactivate-hook #'+python|add-conda-env-to-modeline))
