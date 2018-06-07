f22eeff7634532ad2f1882cf3b41eb4bc1caed7f
Henrik Lissner <henrik@lissner.net>
2018-06-07 02:45:45 +0200

    Add treemacs-mode to evil-escape-excluded-major-modes
    Fixes an unresponsive "j" key in treemacs-mode.

--- modules/feature/evil/config.el
+++ modules/feature/evil/config.el
@@ -239,7 +239,7 @@ variable for an explanation of the defaults (in comments). See
   :commands (evil-escape evil-escape-mode evil-escape-pre-command-hook)
   :init
   (setq evil-escape-excluded-states '(normal visual multiedit emacs motion)
-        evil-escape-excluded-major-modes '(neotree-mode)
+        evil-escape-excluded-major-modes '(neotree-mode treemacs-mode)
         evil-escape-key-sequence "jk"
         evil-escape-delay 0.25)
   (add-hook 'pre-command-hook #'evil-escape-pre-command-hook)
