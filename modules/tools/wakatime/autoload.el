e88de1692d9c6ec26d820c4a38dfe9be9e3fb5ef
Aria Edmonds <aria@ar1as.space>
2018-06-09 23:43:39 +1000

    Don't shadow wakatime-api-key

--- modules/tools/wakatime/autoload.el
+++ modules/tools/wakatime/autoload.el
@@ -4,13 +4,10 @@
 (add-hook 'doom-after-switch-buffer-hook #'+wakatime-init)
 
 ;;;###autoload
-(defvar wakatime-api-key nil)
-
-;;;###autoload
 (defun +wakatime-init ()
   "Initialize wakatime (if `wakatime-api-key' is set, otherwise no-op with a
 warning)."
-  (if wakatime-api-key
+  (if (boundp 'wakatime-api-key)
       (global-wakatime-mode +1)
     (message "No `wakatime-api-key' set! wakaktime-mode will stay disabled."))
   (remove-hook 'doom-after-switch-buffer-hook #'+wakatime-init))
