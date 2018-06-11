b9a179eeed5faa948a562ba17584ec6ee31bcde7
Aria Edmonds <aria@ar1as.space>
2018-06-11 12:28:03 +1000

    Make +wakatime-init interactive
    Sometimes it doesn't prompt to decrypt gpg, etc etc. Regardless, I think
    this should be interactive so it's easily re-initialized

--- modules/tools/wakatime/autoload.el
+++ modules/tools/wakatime/autoload.el
@@ -7,6 +7,7 @@
 (defun +wakatime-init ()
   "Initialize wakatime (if `wakatime-api-key' is set, otherwise no-op with a
 warning)."
+  (interactive)
   (if (boundp 'wakatime-api-key)
       (global-wakatime-mode +1)
     (message "No `wakatime-api-key' set! wakaktime-mode will stay disabled."))
