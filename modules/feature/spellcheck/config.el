d0abe742f96552c0913c84ca09bc5e631baff54e
Henrik Lissner <henrik@lissner.net>
2018-06-15 20:52:00 +0200

    Don't set ispell-program-name
    ispell already looks for aspell, ispell, and hunspell on initialization.

--- modules/feature/spellcheck/config.el
+++ modules/feature/spellcheck/config.el
@@ -15,8 +15,7 @@ Since spellchecking can be slow in some buffers, this can be disabled with:
   :init
   (add-hook 'flyspell-mode-hook #'+spellcheck|immediately)
   :config
-  (setq ispell-program-name (executable-find "aspell")
-        ispell-list-command "--list"
+  (setq ispell-list-command "--list"
         ispell-extr-args '("--dont-tex-check-comments"))
 
   (defun +spellcheck|immediately ()
