7b1db08ea2d1956b7b19336bca307d934a752fba
Henrik Lissner <henrik@lissner.net>
2018-06-15 13:36:43 +0200

    Replace :devdocs with set-devdocs! autodef

--- modules/feature/lookup/autoload/devdocs.el
+++ modules/feature/lookup/autoload/devdocs.el
@@ -1,11 +1,20 @@
 ;;; feature/lookup/autoload/devdocs.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! +devdocs)
 
+;;;###autodef
+(defun set-devdocs! (modes docset)
+  "Map major MODES (one major-mode symbol or a list of them) to a devdocs
+DOCSET (a string).
+
+See `devdocs-alist' for the defaults. "
+  (after! (:when (boundp 'devdocs-alist))
+    (map-put devdocs-alist mode docsets)))
+
 ;;;###autoload
 (def-setting! :devdocs (modes docset)
   "Map major MODES (one major-mode symbol or a list of them) to a devdocs
 DOCSET (a string).
 
 See `devdocs-alist' for the defaults. "
-  `(dolist (mode ',modes)
-     (push (cons mode ,docset) devdocs-alist)))
+  :obsolete set-devdocs!
+  `(set-devdocs! ,modes ,docset))
