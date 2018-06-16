3ad16f660fbbbd918ae06df2166740ef30db6d9e
Henrik Lissner <henrik@lissner.net>
2018-06-16 12:46:57 +0200

    Refactor set-company-backend!

--- modules/completion/company/autoload.el
+++ modules/completion/company/autoload.el
@@ -5,17 +5,16 @@
   "Prepends BACKENDS to `company-backends' in major MODES.
 
 MODES should be one major-mode symbol or a list of them."
-  (cl-loop for mode in modes
-           for def-name = (intern (format "doom--init-company-%s" mode))
-           do
-           (fset def-name
-                 (lambda () (when (or (eq major-mode mode)
-                                 (and (boundp mode) (symbol-value mode)))
-                         (require 'company)
-                         (make-variable-buffer-local 'company-backends)
-                         (dolist (backend backends)
-                           (cl-pushnew backend company-backends :test #'equal)))))
-           and do (add-hook (intern (format "%s-hook" mode)) def-name)))
+  (dolist (mode (doom-enlist modes))
+    (let ((def-name (intern (format "+company--init-%s" mode))))
+      (fset def-name
+            (lambda () (when (or (eq major-mode mode)
+                            (and (boundp mode) (symbol-value mode)))
+                    (require 'company)
+                    (make-variable-buffer-local 'company-backends)
+                    (dolist (backend backends)
+                      (cl-pushnew backend company-backends :test #'equal)))))
+      (add-hook (intern (format "%s-hook" mode)) def-name))))
 
 ;; FIXME obsolete :company-backend
 ;;;###autoload
