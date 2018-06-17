77a5825cedbeefbe11a4bcce9e0fbf92bd69ab87
HaoZeke <rohit.goswami@aol.com>
2018-06-17 13:06:42 +0530

    literate: Allow literate packages [HACKY]

--- modules/config/literate/init.el
+++ modules/config/literate/init.el
@@ -2,6 +2,8 @@
 
 ;;; config/literate/config.el -*- lexical-binding: t; -*-
 
+;;; config/literate/packages.el -*- lexical-binding: t; -*-
+
 (defvar +literate-config-file
   (expand-file-name "config.org" doom-private-dir)
   "The file path of your literate config file.")
@@ -11,14 +13,24 @@
   "The file path that `+literate-config-file' will be tangled to, then
 byte-compiled from.")
 
+(defvar +literate-packages-file
+  (expand-file-name "packages.org" doom-private-dir)
+  "The file path of your literate packages file.")
+
+(defvar +literate-packages-dest-file
+  (expand-file-name "packages.el" doom-private-dir)
+  "The file path that `+literate-packages-file' will be tangled to, then
+byte-compiled from.")
 
 ;;
 (defun +literate-compile (&optional load)
-  "Tangles & compiles `+literate-config-file' if it has changed. If LOAD is
-non-nil, load it too!"
-  (let ((org +literate-config-file)
-        (el  +literate-config-dest-file))
-    (when (file-newer-than-file-p org el)
+  "Tangles & compiles `+literate-config-file' and `+literate-packages-file' if
+ it has changed. If LOAD is non-nil, load it too!"
+  (let ((orgConf +literate-config-file)
+        (orgPack +literate-packages-file)
+        (elConf  +literate-config-dest-file)
+        (elPack +literate-packages-dest-file))
+    (when  (file-newer-than-file-p orgConf elConf)
       (message "Compiling your literate config...")
 
       ;; We tangle in a separate, blank process because loading it here would
@@ -27,8 +39,21 @@ non-nil, load it too!"
                   "emacs" nil nil nil
                   "-q" "--batch" "-l" "ob-tangle" "--eval"
                   (format "(org-babel-tangle-file \"%s\" \"%s\" \"emacs-lisp\")"
-                          org el)))
+                          orgConf elConf)))
           (warn "There was a problem tangling your literate config!"))
+
+      (message "Done!"))
+    (when  (file-newer-than-file-p orgPack elPack)
+      (message "Compiling your literate packages...")
+
+      ;; We tangle in a separate, blank process because loading it here would
+      ;; load all of :lang org (very expensive!). We only need ob-tangle.
+      (or (zerop (call-process
+                  "emacs" nil nil nil
+                  "-q" "--batch" "-l" "ob-tangle" "--eval"
+                  (format "(org-babel-tangle-file \"%s\" \"%s\" \"emacs-lisp\")"
+                          orgPack elPack)))
+          (warn "There was a problem tangling your literate packages!"))
 
       (message "Done!"))))
 
