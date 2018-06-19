7a98919597849c8d44ebf45e6f030dce0f5d5e9c
Henrik Lissner <henrik@lissner.net>
2018-06-19 20:50:16 +0200

    Fix quoting in babel popup rules
    Reported by @fuxialexander

--- modules/lang/org/+babel.el
+++ modules/lang/org/+babel.el
@@ -73,16 +73,16 @@ string). Stops at the first function to return non-nil.")
   :config
   (set-popup-rules!
     '(("^\\*Org Src"
-       :side 'right :size 100  :height 0.6 :slot -1
+       :side right :size 100  :height 0.6 :slot -1
        :quit nil :select t)
       ("^\\*Python"
-       :slot 0 :side 'right :size 100
+       :slot 0 :side right :size 100
        :select nil :quit nil :ttl nil)
       ("\\*ob-ipython.*"
-       :slot 2 :side 'right :size 100 :height 0.2
+       :slot 2 :side right :size 100 :height 0.2
        :select nil :quit nil :transient nil)
       ("\\*Python:.*"
-       :slot 0 :side 'right :size 100
+       :slot 0 :side right :size 100
        :select nil :quit nil :transient nil)))
   ;; TODO Add more popup styles
 
