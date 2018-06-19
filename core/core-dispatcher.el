e069139b1c30c716db6af9cf8a649d7945c25f6a
Henrik Lissner <henrik@lissner.net>
2018-06-19 20:51:14 +0200

    Prevent kill-emacs-hook running after byte-compile
    Prevents harmless errors after byte-compiling, or recentf needlessly
    saving its cache to file.

--- core/core-dispatcher.el
+++ core/core-dispatcher.el
@@ -739,7 +739,9 @@ If RECOMPILE-P is non-nil, only recompile out-of-date files."
       ;; Assemble el files we want to compile; taking into account that
       ;; MODULES may be a list of MODULE/SUBMODULE strings from the command
       ;; line.
-      (let ((target-files (doom-files-in targets :depth 1 :match "\\.el$")))
+      (let ((target-files (doom-files-in targets :depth 1 :match "\\.el$"))
+            (load-path load-path)
+            kill-emacs-hook kill-buffer-query-functions)
         (unless target-files
           (if targets
               (message "Couldn't find any valid targets")
