9b7cbfb14bc24d3b0b136205d90fcc41e7eaa645
Edmund Miller <Edmund.A.Miller@protonmail.com>
2018-06-22 19:36:06 -0500

    Add shell +lsp
    Co-authored-by: Aria Edmonds <aria@ar1as.space>

--- modules/lang/sh/packages.el
+++ modules/lang/sh/packages.el
@@ -6,3 +6,12 @@
 
 (when (featurep! :completion company)
   (package! company-shell))
+
+(when (featurep! +lsp)
+  (depends-on! :tools lsp
+               (after! sh-script
+                 (lsp-define-stdio-client lsp-sh
+                                          "sh"
+                                          #'projectile-project-root
+                                          '("bash-language-server" "start"))
+                 (add-hook 'sh-mode-hook #'lsp-sh-enable))))
