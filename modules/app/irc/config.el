7a6407eb92302f95e9cb2bc0cef035fc4b390d68
Mark Redeman <markredeman@gmail.com>
2018-06-23 14:44:40 +0200

    Add: reasonable default keybindings for circe
    The =+irc/send-message= function was added so that we can send private
    messages using =SPC m m= or =M-x= with =+irc/send-message=.

--- modules/app/irc/config.el
+++ modules/app/irc/config.el
@@ -125,7 +125,23 @@ playback.")
 
   (after! solaire-mode
     ;; distinguish chat/channel buffers from server buffers.
-    (add-hook 'circe-chat-mode-hook #'solaire-mode)))
+    (add-hook 'circe-chat-mode-hook #'solaire-mode))
+
+  (map!
+   (:localleader
+     (:map circe-mode-map
+       :desc "Next active buffer"     :n "a" #'tracking-next-buffer
+       :desc "Join channel"           :n "j" #'circe-command-JOIN
+       :desc "Send private message"   :n "m" #'+irc/send-message
+       :desc "Part current channel"   :n "p" #'circe-command-PART
+       :desc "Quit irc"               :n "Q" #'+irc/quit
+       :desc "Reconnect"              :n "R" #'circe-reconnect
+
+       (:when (featurep! :completion ivy)
+         :desc "Jump to channel"      :n "c" #'+irc/ivy-jump-to-channel))
+
+     (:map circe-channel-mode-map
+       :desc "Show names"           :n "n" #'circe-command-NAMES)))
 
 
 (def-package! circe-color-nicks
