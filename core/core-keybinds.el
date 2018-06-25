017ba8d79c8eab111326a636e42c3dc88230dbef
Henrik Lissner <henrik@lissner.net>
2018-06-26 01:42:35 +0200

    Autoload defhydras & use new naming convention
    Phasing out the +module@name convention for plain old
    +module-name-hydra, which is more compatible with elisp reflection tools
    like describe-function and such.
    
    Also, Emacs starts up faster now. Tee hee.

--- core/core-keybinds.el
+++ core/core-keybinds.el
@@ -63,54 +63,8 @@ If any hook returns non-nil, all hooks after it are ignored.")
   (which-key-mode +1))
 
 
-(def-package! hydra
-  :defer t
-  :config
-  (setq lv-use-seperator t)
-
-  (defhydra doom@text-zoom (:hint t :color red)
-    "
-      Text zoom: _j_:zoom in, _k_:zoom out, _0_:reset
-"
-    ("j" text-scale-increase "in")
-    ("k" text-scale-decrease "out")
-    ("0" (text-scale-set 0) "reset"))
-
-  (defhydra doom@window-nav (:hint nil)
-    "
-          Split: _v_ert  _s_:horz
-         Delete: _c_lose  _o_nly
-  Switch Window: _h_:left  _j_:down  _k_:up  _l_:right
-        Buffers: _p_revious  _n_ext  _b_:select  _f_ind-file
-         Resize: _H_:splitter left  _J_:splitter down  _K_:splitter up  _L_:splitter right
-           Move: _a_:up  _z_:down  _i_menu
-"
-    ("z" scroll-up-line)
-    ("a" scroll-down-line)
-    ("i" idomenu)
-
-    ("h" windmove-left)
-    ("j" windmove-down)
-    ("k" windmove-up)
-    ("l" windmove-right)
-
-    ("p" previous-buffer)
-    ("n" next-buffer)
-    ("b" switch-to-buffer)
-    ("f" find-file)
-
-    ("s" split-window-below)
-    ("v" split-window-right)
-
-    ("c" delete-window)
-    ("o" delete-other-windows)
-
-    ("H" hydra-move-splitter-left)
-    ("J" hydra-move-splitter-down)
-    ("K" hydra-move-splitter-up)
-    ("L" hydra-move-splitter-right)
-
-    ("q" nil)))
+;; `hydra'
+(setq lv-use-seperator t)
 
 
 ;;
---
+++ modules/completion/ivy/autoload/hydras.el
@@ -0,0 +1,43 @@
+;;; completion/ivy/autoload/hydras.el -*- lexical-binding: t; -*-
+
+;;;###autoload (autoload '+ivy-coo-hydra/body "completion/ivy/autoload/hydras" nil nil)
+(defhydra +ivy-coo-hydra (:hint nil :color pink)
+  "
+ Move     ^^^^^^^^^^ | Call         ^^^^ | Cancel^^ | Options^^ | Action _w_/_s_/_a_: %s(ivy-action-name)
+----------^^^^^^^^^^-+--------------^^^^-+-------^^-+--------^^-+---------------------------------
+ _g_ ^ ^ _k_ ^ ^ _u_ | _f_orward _o_ccur | _i_nsert | _c_alling: %-7s(if ivy-calling \"on\" \"off\") _C_ase-fold: %-10`ivy-case-fold-search
+ ^↨^ _h_ ^+^ _l_ ^↕^ | _RET_ done     ^^ | _q_uit   | _m_atcher: %-7s(ivy--matcher-desc) _t_runcate: %-11`truncate-lines
+ _G_ ^ ^ _j_ ^ ^ _d_ | _TAB_ alt-done ^^ | ^ ^      | _<_/_>_: shrink/grow
+"
+  ;; arrows
+  ("j" ivy-next-line)
+  ("k" ivy-previous-line)
+  ("l" ivy-alt-done)
+  ("h" ivy-backward-delete-char)
+  ("g" ivy-beginning-of-buffer)
+  ("G" ivy-end-of-buffer)
+  ("d" ivy-scroll-up-command)
+  ("u" ivy-scroll-down-command)
+  ("e" ivy-scroll-down-command)
+  ;; actions
+  ("q" keyboard-escape-quit :exit t)
+  ("C-g" keyboard-escape-quit :exit t)
+  ("<escape>" keyboard-escape-quit :exit t)
+  ("C-o" nil)
+  ("i" nil)
+  ("TAB" ivy-alt-done :exit nil)
+  ("C-j" ivy-alt-done :exit nil)
+  ("RET" ivy-done :exit t)
+  ("C-m" ivy-done :exit t)
+  ("C-SPC" ivy-call-and-recenter :exit nil)
+  ("f" ivy-call)
+  ("c" ivy-toggle-calling)
+  ("m" ivy-toggle-fuzzy)
+  (">" ivy-minibuffer-grow)
+  ("<" ivy-minibuffer-shrink)
+  ("w" ivy-prev-action)
+  ("s" ivy-next-action)
+  ("a" ivy-read-action)
+  ("t" (setq truncate-lines (not truncate-lines)))
+  ("C" ivy-toggle-case-fold)
+  ("o" ivy-occur :exit t))
