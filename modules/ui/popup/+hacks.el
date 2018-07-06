0b7faa468cf22be0a76ba193860825ab0c8e5cba
Henrik Lissner <henrik@lissner.net>
2018-07-06 14:57:40 +0200

    Fix which-key sharing windows with other popups
    Which-key is now lazy-loaded, so the modified which-key-popup-type gets
    overwritten.

--- modules/ui/popup/+hacks.el
+++ modules/ui/popup/+hacks.el
@@ -281,16 +281,17 @@ instead of switch-to-buffer-*."
 
 
 ;; `which-key'
-(setq which-key-popup-type 'custom
-      which-key-custom-popup-max-dimensions-function (lambda (_) (which-key--side-window-max-dimensions))
-      which-key-custom-hide-popup-function #'which-key--hide-buffer-side-window
-      which-key-custom-show-popup-function
-      (lambda (act-popup-dim)
-        (cl-letf (((symbol-function 'display-buffer-in-side-window)
-                   (lambda (buffer alist)
-                     (+popup-display-buffer-stacked-side-window
-                      buffer (append '((vslot . -9999)) alist)))))
-          (which-key--show-buffer-side-window act-popup-dim))))
+(after! which-key
+  (setq which-key-popup-type 'custom
+        which-key-custom-popup-max-dimensions-function (lambda (_) (which-key--side-window-max-dimensions))
+        which-key-custom-hide-popup-function #'which-key--hide-buffer-side-window
+        which-key-custom-show-popup-function
+        (lambda (act-popup-dim)
+          (cl-letf (((symbol-function 'display-buffer-in-side-window)
+                     (lambda (buffer alist)
+                       (+popup-display-buffer-stacked-side-window
+                        buffer (append '((vslot . -9999)) alist)))))
+            (which-key--show-buffer-side-window act-popup-dim)))))
 
 
 ;; `windmove'
