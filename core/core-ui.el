64867cbed4edf619782a932a0e4f14fe2678582f
Henrik Lissner <henrik@lissner.net>
2018-07-06 12:48:41 +0200

    Don't lazy load doom|disable-show-paren-mode
    In case it is used early (e.g. by org-mode).

--- core/core-ui.el
+++ core/core-ui.el
@@ -315,10 +315,11 @@ DEFAULT is non-nil, set the default mode-line for all buffers."
 ;; highlight matching delimiters
 (def-package! paren
   :after-call (after-find-file doom-exit-buffer-hook)
-  :config
+  :init
   (defun doom|disable-show-paren-mode ()
     "Turn off `show-paren-mode' buffer-locally."
     (set (make-local-variable 'show-paren-mode) nil))
+  :config
   (setq show-paren-delay 0.1
         show-paren-highlight-openparen t
         show-paren-when-point-inside-paren t)
