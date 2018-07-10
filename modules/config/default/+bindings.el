04b7b5f1efccfb6586ec02c292312415a2d19da0
Henrik Lissner <henrik@lissner.net>
2018-07-10 14:20:21 +0200

    Add gs/, gs? & gs<space> evil-easymotion binds
    Adds evil-search and avy-goto-char-timer interfaces to evil-easymotion.

--- modules/config/default/+bindings.el
+++ modules/config/default/+bindings.el
@@ -180,7 +180,14 @@
                            :pre-hook (save-excursion (call-interactively #'evil-snipe-s))
                            :bind ((evil-snipe-scope 'buffer)
                                   (evil-snipe-enable-highlight)
-                                  (evil-snipe-enable-incremental-highlight))))
+                                  (evil-snipe-enable-incremental-highlight)))
+        " " #'avy-goto-char-timer
+        "/" (evilem-create #'evil-ex-search-next
+                           :pre-hook (save-excursion (call-interactively #'evil-ex-search-forward))
+                           :bind ((evil-search-wrap)))
+        "?" (evilem-create #'evil-ex-search-previous
+                           :pre-hook (save-excursion (call-interactively #'evil-ex-search-backward))
+                           :bind ((evil-search-wrap))))
 
       ;; evil
       (:after evil
