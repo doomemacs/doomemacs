f540c6c40ab9b12f9e5269351666ac2de53f837a
Henrik Lissner <henrik@lissner.net>
2018-07-11 01:09:14 +0200

    Disable neotree, enable treemacs (by default)

--- init.example.el
+++ init.example.el
@@ -33,8 +33,8 @@
       ;fci               ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
-       neotree           ; a project drawer, like NERDTree for vim
-      ;treemacs          ; a project drawer, like neotree but cooler
+      ;neotree           ; a project drawer, like NERDTree for vim
+       treemacs          ; a project drawer, like neotree but cooler
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
