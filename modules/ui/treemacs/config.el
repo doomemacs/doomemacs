e4778f992352c034c189b98e595d0cabcb272739
Henrik Lissner <henrik@lissner.net>
2018-07-10 23:06:26 +0200

    Disable treemacs-follow-mode by default
    This has proven intrusive, especially when you have files from different
    projects open and you switch between them rapidly. Enabling
    treemacs-follow-mode is left up to the user. Otherwise, the SPC o P
    keybind and treemacs-find-file command can this manually and on-demand.
    
    Also remove hide-mode-line-mode from treemacs because it's redundant
    with the doom-themes treemacs icon theme.

--- modules/ui/treemacs/config.el
+++ modules/ui/treemacs/config.el
@@ -28,12 +28,9 @@ There are 2 possible values:
   (defvar treemacs-collapse-dirs
     (if (executable-find "python3") 3 0))
 
-  (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
   (when (memq +treemacs-use-git-mode '(simple extended))
-    (treemacs-git-mode +treemacs-use-git-mode))
-
-  (add-hook 'treemacs-mode-hook #'hide-mode-line-mode))
+    (treemacs-git-mode +treemacs-use-git-mode)))
 
 
 (def-package! treemacs-evil
