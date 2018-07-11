287460cb050c94010f4d8ded0fbfecf479c1772a
Henrik Lissner <henrik@lissner.net>
2018-07-11 12:44:04 +0200

    Add SPC o p/P keybind for treemacs/neotree
    + SPC o p invokes treemacs/neotree
    + SPC o P finds the current file in the treemacs/neotree pane

--- modules/config/default/+bindings.el
+++ modules/config/default/+bindings.el
@@ -671,8 +671,12 @@
         :desc "REPL"                  :n  "r" #'+eval/open-repl
                                       :v  "r" #'+eval:repl
         :desc "Dired"                 :n  "-" #'dired-jump
-        :desc "Neotree"               :n  "n" #'+neotree/open
-        :desc "Neotree: find file"    :n  "N" #'+neotree/find-this-file
+        (:when (featurep! :ui neotree)
+          :desc "Project sidebar"              :n  "p" #'+neotree/open
+          :desc "Find file in project sidebar" :n  "P" #'+neotree/find-this-file)
+        (:when (featurep! :ui treemacs)
+          :desc "Project sidebar"              :n  "p" #'+treemacs/toggle
+          :desc "Find file in project sidebar" :n  "P" #'treemacs-find-file)
         :desc "Imenu sidebar"         :nv "i" #'imenu-list-smart-toggle
         :desc "Terminal"              :n  "t" #'+term/open
         :desc "Terminal in popup"     :n  "T" #'+term/open-popup-in-project
