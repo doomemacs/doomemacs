69c8ef4141ba3ead9765aedc87d3dbd1c81a32e9
Henrik Lissner <henrik@lissner.net>
2018-07-15 20:44:11 +0200

    Fix void-function haskell-interactive-mode

--- modules/lang/haskell/config.el
+++ modules/lang/haskell/config.el
@@ -11,7 +11,7 @@
   :hook (haskell-mode . hindent-mode))
 
 (after! haskell-mode
-  (add-hook 'haskell-mode-hook #'haskell-interactive-mode)
+  (add-hook 'haskell-mode-hook #'interactive-haskell-mode)
   (set-file-template! 'haskell-mode :trigger #'haskell-auto-insert-module-template :project t)
   (set-repl-handler! '(haskell-mode haskell-cabal-mode literate-haskell-mode) #'+haskell-repl-buffer)
   (add-to-list 'completion-ignored-extensions ".hi"))
