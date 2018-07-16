1dac12c9c0a79d300db950502c1b6fbf8eb1039d
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-16 17:10:57 +0200

    Add bindings for nix-mode

--- modules/lang/nix/config.el
+++ modules/lang/nix/config.el
@@ -13,3 +13,14 @@
 (def-package! nix-repl
   :after nix-mode
   :commands (nix-repl-show))
+
+(def-package! nix-mode
+  :config
+  (map! :map nix-mode-map
+        :localleader
+        :n "f" #'nix-update-fetch
+        :n "p" #'nix-format-buffer
+        :n "r" #'nix-repl-show
+        :n "s" #'nix-shell
+        :n "b" #'nix-build
+        :n "u" #'nix-unpack))
