6b236929857c7dd2c9536fb091b410963fd4c4a5
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-16 17:15:05 +0200

    Whoops! this is available on melpa after all

--- modules/lang/nix/packages.el
+++ modules/lang/nix/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/nix/packages.el
 
 (package! nix-mode)
-(package! nix-update :recipe (:fetcher github :repo "jwiegley/nix-update-el"))
+(package! nix-update)
 
 (when (featurep! :completion company)
   (package! company-nixos-options))
