f8581c3206e3530c4c472ecdbf26ea15ce6a5dac
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-16 12:28:41 +0200

    Added hlint support for intero users

--- modules/lang/haskell/+intero.el
+++ modules/lang/haskell/+intero.el
@@ -13,4 +13,7 @@ This is necessary because `intero-mode' doesn't do its own error checks."
         (message "Couldn't find stack. Refusing to enable intero-mode."))))
   (add-hook 'haskell-mode-hook #'+haskell|init-intero)
   :config
-  (set-lookup-handlers! 'intero-mode :definition #'intero-goto-definition))
+  (set-lookup-handlers! 'intero-mode :definition #'intero-goto-definition)
+  (when (featurep! :feature syntax-checker)
+    (add-hook! 'intero-mode
+      (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))))
