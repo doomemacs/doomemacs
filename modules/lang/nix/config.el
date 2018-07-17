0aa351842cd3498b8fcd8b1a7448932f8c825fc9
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-17 09:56:52 +0200

    clean-up + add helm-nixos-options

--- modules/lang/nix/config.el
+++ modules/lang/nix/config.el
@@ -1,21 +1,16 @@
 ;;; config.el --- description -*- lexical-binding: t; -*-
 
-(def-package! company-nixos-options
-  :when (featurep! :completion company)
-  :after nix-mode
-  :config
-  (set-company-backend! 'nix-mode 'company-nixos-options))
-
 (def-package! nix-update
-  :after nix-mode
   :commands (nix-update-fetch))
 
 (def-package! nix-repl
-  :after nix-mode
   :commands (nix-repl-show))
 
-(def-package! nix-mode
-  :config
+(def-package! helm-nixos-options
+  :when (featurep! :completion helm)
+  :commands (helm-nixos-options))
+
+(after! nix-mode
   (map! :map nix-mode-map
         :localleader
         :n "f" #'nix-update-fetch
@@ -23,4 +18,12 @@
         :n "r" #'nix-repl-show
         :n "s" #'nix-shell
         :n "b" #'nix-build
-        :n "u" #'nix-unpack))
+        :n "u" #'nix-unpack)
+
+  (when (featurep! :completion helm)
+    (map! :map nix-mode-map
+          :localleader
+          :n "o" #'helm-nixos-options))
+
+  (when (featurep! :completion company)
+    (set-company-backend! 'nix-mode 'company-nixos-options)))
