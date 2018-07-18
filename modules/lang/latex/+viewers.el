f497d1bf0d2e57a6661aea8075ecadb3a4fcd729
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-18 16:59:15 +0200

    Fix regression

--- modules/lang/latex/+viewers.el
+++ modules/lang/latex/+viewers.el
@@ -1,7 +1,6 @@
 ;;; lang/latex/+viewers.el -*- lexical-binding: t; -*-
 
 ;; Update PDF buffers after successful LaTeX runs
-(add-hook 'TeX-after-compilation-finished-function #'TeX-revert-document-buffer)
 
 (when (featurep! +okular)
   ;; Configure Okular as viewer. Including a bug fix
@@ -20,4 +19,5 @@
 ;; Or PDF-tools, but only if the module is also loaded
 (when (and (featurep! :tools pdf)
            (featurep! +pdf-tools))
-  (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools")))
+  (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools"))
+  (add-hook 'TeX-after-compilation-finished-function #'TeX-revert-document-buffer))
