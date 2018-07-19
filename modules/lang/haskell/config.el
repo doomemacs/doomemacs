3a5af1d59191b422c0ee679bd5b575c562603979
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-19 16:34:13 +0200

    Added subword mode hook.

--- modules/lang/haskell/config.el
+++ modules/lang/haskell/config.el
@@ -11,6 +11,7 @@
   :hook (haskell-mode . hindent-mode))
 
 (after! haskell-mode
+  (add-hook 'haskell-mode-hook 'subword-mode)
   (add-hook 'haskell-mode-hook #'interactive-haskell-mode)
   (set-lookup-handlers! 'haskell-mode :definition #'haskell-mode-jump-to-def-or-tag)
   (set-file-template! 'haskell-mode :trigger #'haskell-auto-insert-module-template :project t)
