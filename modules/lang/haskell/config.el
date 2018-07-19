ce9f7f9bde314682700d666645cc087b3118d648
Henrik Lissner <accounts@v0.io>
2018-07-19 19:03:05 +0200

    lang/haskell: minor reformatting & refactor
    Conform to conventions, use `add-hook!` for multiple hooks, use single semi-colon for eol comments, remove comments redundant with code, and use sharp-quote to indicate function symbol.

--- modules/lang/haskell/config.el
+++ modules/lang/haskell/config.el
@@ -11,13 +11,14 @@
   :hook (haskell-mode . hindent-mode))
 
 (after! haskell-mode
-  (setq haskell-process-suggest-remove-import-lines t ;; warnings for redundant imports etc.
-        haskell-process-auto-import-loaded-modules t) ;; auto import modules
+  (setq haskell-process-suggest-remove-import-lines t  ; warnings for redundant imports etc
+        haskell-process-auto-import-loaded-modules t)
   (when (featurep! :feature syntax-checker)
-    (setq haskell-process-show-overlays nil)) ;;flycheck makes this unnecessary
-  (add-hook 'haskell-mode-hook 'subword-mode) ;; improves text navigation with camelCase:
-  (add-hook 'haskell-mode-hook #'haskell-collapse-mode) ;; support collapsing haskell code blocks.
-  (add-hook 'haskell-mode-hook #'interactive-haskell-mode)
+    (setq haskell-process-show-overlays nil))  ; flycheck makes this unnecessary
+  (add-hook! 'haskell-mode-hook 
+    #'(subword-mode           ; improves text navigation with camelCase
+       haskell-collapse-mode  ; support folding haskell code blocks
+       interactive-haskell-mode))
   (set-lookup-handlers! 'haskell-mode :definition #'haskell-mode-jump-to-def-or-tag)
   (set-file-template! 'haskell-mode :trigger #'haskell-auto-insert-module-template :project t)
   (set-repl-handler! '(haskell-mode haskell-cabal-mode literate-haskell-mode) #'+haskell-repl-buffer)
