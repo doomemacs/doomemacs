884173112c1617b3958c980908752aac32711dfe
Henrik Lissner <henrik@lissner.net>
2018-07-29 17:54:17 +0200

    Add :lang racket to init.example.el

--- init.example.el
+++ init.example.el
@@ -113,6 +113,7 @@
       ;purescript        ; javascript, but functional
       ;python            ; beautiful is better than ugly
       ;qt                ; the 'cutest' gui framework ever
+      ;racket            ; a DSL for DSLs
       ;rest              ; Emacs as a REST client
       ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
       ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
