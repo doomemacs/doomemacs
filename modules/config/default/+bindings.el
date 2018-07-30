71e71b9b83919123fe01d5ebbf00d4ef36dc5250
Henrik Lissner <henrik@lissner.net>
2018-07-30 13:08:52 +0200

    Revert fe3a73c1: remap newline instead of binding RET

--- modules/config/default/+bindings.el
+++ modules/config/default/+bindings.el
@@ -15,8 +15,8 @@
       :nmvo doom-localleader-key nil
 
       ;; Swap RET/C-j in insert mode
-      :gi "RET" #'newline-and-indent
-      :i  "C-j" #'+default/newline
+      :i [remap newline] #'newline-and-indent
+      :i "C-j" #'+default/newline
 
       ;; --- Global keybindings ---------------------------
       ;; Make M-x available everywhere
