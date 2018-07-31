9079fdd9eca4051fc5a039a650b0dbe21110c0ae
Henrik Lissner <henrik@lissner.net>
2018-07-31 19:11:41 +0200

    app/write: fix +write-mode not disabling other modes

--- modules/app/write/config.el
+++ modules/app/write/config.el
@@ -6,12 +6,23 @@
 (defvar +write-line-spacing nil
   "What to set `line-spacing' in `+write-mode'.")
 
+(defun +write|init-line-numbers ()
+  (if +write-mode
+      (doom|enable-line-numbers)
+    (doom|disable-line-numbers)))
+
+(defun +write|init-mixed-pitch ()
+  (mixed-pitch-mode (if +write-mode +1 -1)))
+
+(defun +write|init-visual-fill-column ()
+  (visual-fill-column-mode (if +write-mode +1 -1)))
+
 (add-hook! '+write-mode-hook
   #'(flyspell-mode
-     visual-fill-column-mode
      visual-line-mode
-     mixed-pitch-mode
-     doom|enable-line-numbers
+     +write|init-mixed-pitch
+     +write|init-visual-fill-column
+     +write|init-line-numbers
      +write|init-org-mode))
 
 
