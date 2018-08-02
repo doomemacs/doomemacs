5f1618e7a15fa83c1b6f9a08fea6ac549e3e88d2
Henrik Lissner <henrik@lissner.net>
2018-08-02 16:40:47 +0200

    feature/spellcheck: minor reformatting

--- modules/feature/spellcheck/config.el
+++ modules/feature/spellcheck/config.el
@@ -8,16 +8,14 @@ Since spellchecking can be slow in some buffers, this can be disabled with:
   (setq-hook! 'LaTeX-mode-hook +spellcheck-immediately nil)")
 
 ;; `ispell'
-(setq ispell-dictionary "english")
+(setq ispell-dictionary "english"
+      ispell-list-command "--list"
+      ispell-extr-args '("--dont-tex-check-comments"))
 
 (def-package! flyspell ; built-in
   :defer t
-  :init
-  (add-hook 'flyspell-mode-hook #'+spellcheck|immediately)
+  :init (add-hook 'flyspell-mode-hook #'+spellcheck|immediately)
   :config
-  (setq ispell-list-command "--list"
-        ispell-extr-args '("--dont-tex-check-comments"))
-
   (defun +spellcheck|immediately ()
     "Spellcheck the buffer when `flyspell-mode' is enabled."
     (when (and flyspell-mode +spellcheck-immediately)
