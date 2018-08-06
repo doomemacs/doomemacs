e266ec9eecded13bec4720089067d6b59844ac21
Henrik Lissner <henrik@lissner.net>
2018-08-06 23:03:02 +0200

    Add company-ispell to default text-mode backends

--- modules/completion/company/autoload.el
+++ modules/completion/company/autoload.el
@@ -2,7 +2,7 @@
 
 ;;;###autoload
 (defvar +company-backend-alist
-  '((text-mode :derived (company-dabbrev company-yasnippet))
+  '((text-mode :derived (company-dabbrev company-yasnippet company-ispell))
     (prog-mode :derived (:separate company-capf company-yasnippet))
     (conf-mode :derived company-capf company-dabbrev-code company-yasnippet))
   "An alist matching modes to company backends. The backends for any mode is
