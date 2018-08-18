6393ebe16bef4e581726329f7adc41022b0cd96f
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-08-18 14:51:21 +0200

    Removed redundant config

--- modules/lang/coq/config.el
+++ modules/lang/coq/config.el
@@ -1,7 +1,5 @@
 ;;; lang/coq/config.el -*- lexical-binding: t; -*-
 
 (def-package! proof-site
-  :load-path +coq-pg-loc
-  :defer t
   :mode ("\\.v\\'" . coq-mode)
   :hook (coq-mode . company-coq-mode))
