14fa855b357c6784b51ae618f1115cf8d53bddf9
The-BigDaddy <pragmatic.programmer@protonmail.com>
2018-08-20 18:54:12 -0500

    Recreated lsp-css using projectile-project-root

--- modules/lang/web/autoload/css.el
+++ modules/lang/web/autoload/css.el
@@ -82,31 +82,3 @@ Meant for `comment-line-break-function'."
           (insert "\n" (make-string pre-indent 32))
           (delete-char -1))))))
 
-;;
-;; LSP-modes
-;;
-
-;;;###autoload
-(defun +lsp-css|css-mode ()
-  "Only enable in strictly `css-mode'.
-`css-mode-hook'  fires for `scss-mode' because `scss-mode' is derived from `css-mode'."
-  (when (eq major-mode 'css-mode)
-    (condition-case nil
-        (lsp-css-enable)
-      (user-error nil))))
-
-;;;###autoload
-(defun +lsp-css|scss-mode ()
-  "Only enable in strictly sass-/scss-mode, not ANY. OTHER. MODES."
-  (when (memq major-mode '(sass-mode scss-mode))
-    (condition-case nil
-        (lsp-scss-enable)
-      (user-error nil))))
-
-;;;###autoload
-(defun +lsp-css|less-mode ()
-  "Only enable in strictly `less-css-mode', not ANY. OTHER. MODE."
-  (when (eq major-mode 'less-css-mode)
-    (condition-case nil
-        (lsp-less-enable)
-      (user-error nil))))
