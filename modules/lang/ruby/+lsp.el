85b4d36dfff52c95cb4d92d6f004d9d17df07fb6
The-BigDaddy <pragmatic.programmer@protonmail.com>
2018-08-21 18:45:07 -0500

    Added support for Solargraph & Ruby project modes
    Fixed weird parenthesis issue

---
+++ modules/lang/ruby/+lsp.el
@@ -0,0 +1,13 @@
+;;; lang/ruby/+lsp.el -*- lexical-binding: t; -*-
+
+(lsp-define-stdio-client lsp-solargraph
+                         "ruby"
+                         #'projectile-project-root
+                         `(,(executable-find "solargraph") "stdio")
+                         :ignore-regex
+                         '("^Solargraph is listening on stdio PID=*$"))
+
+(add-hook! 'enh-ruby-mode #'lsp-solargraph-enable)
+(after! 'enh-ruby-mode
+  (set-company-backend! 'enh-ruby-mode 'company-lsp))
+;;; +lsp.el ends here
