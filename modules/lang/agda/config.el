f6d6096bf253290a03375053a74c306ca5a14728
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-08-25 17:27:42 +0200

    load agda input method; bypass agda2; keybindings

--- modules/lang/agda/config.el
+++ modules/lang/agda/config.el
@@ -2,10 +2,18 @@
 
 (defvar +agda-dir
   (when (executable-find "agda-mode")
-    (file-name-directory (shell-command-to-string "agda-mode locate")))
-  "TODO")
+    (file-name-directory (shell-command-to-string "agda-mode locate"))))
 
+(def-package! agda-input
+  :load-path +agda-dir)
 
-(def-package! agda2
-  :load-path +agda-dir
-  :defer t)
+(def-package! agda2-mode
+  :mode "\\.agda\\'"
+  :after agda-input
+  :config
+  (map! :map agda2-mode-map
+                :localleader
+                :n "l" #'agda2-load
+                :n "d" #'agda2-infer-type-maybe-toplevel
+                :n "o" #'agda2-module-contents-maybe-toplevel
+                :n "n" #'agda2-compute-normalised-maybe-toplevel))
