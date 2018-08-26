247b219ea102b6edc76728c28c3eb93dda47a280
Henrik Lissner <henrik@lissner.net>
2018-08-26 12:00:13 +0200

    doom-project-find-file: set default-directory too #833
    Be extra sure!

--- core/autoload/projects.el
+++ core/autoload/projects.el
@@ -73,8 +73,9 @@ If NOCACHE, don't fetch a cached answer."
 ;;;###autoload
 (defun doom-project-find-file (dir)
   "Fuzzy-find a file under DIR."
-  (let ((projectile-project-root dir))
-    (without-project-cache!
+  (without-project-cache!
+   (let ((default-directory dir)
+         (projectile-project-root dir))
      (call-interactively
       ;; completion modules may remap this command
       (or (command-remapping #'projectile-find-file)
