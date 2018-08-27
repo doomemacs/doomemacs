doomemacs/doomemacs

commit e40a58c423bc6b351a6b8d063c5e6eef53e95dd4

Optimize +workspace-new & workspace creation
This is much faster than the old method of switching to the persp to
modify them.

modules/feature/workspaces/autoload/workspaces.el
@@ -155,7 +155,13 @@ Otherwise return t on success, nil otherwise."
     (error "Can't create a new '%s' workspace" name))
   (when (+workspace-exists-p name)
     (error "A workspace named '%s' already exists" name))
-  (persp-add-new name))
+  (let ((persp (persp-add-new name))
+        (+popup--inhibit-transient t))
+    (save-window-excursion
+      (delete-other-windows)
+      (switch-to-buffer (doom-fallback-buffer))
+      (setf (persp-window-conf persp)
+            (funcall persp-window-state-get-function (selected-frame))))))
 
 ;;;###autoload
 (defun +workspace-rename (name new-name)
@@ -342,8 +348,6 @@ workspace, otherwise the new workspace is blank."
             (clone-p (persp-copy name t))
             (t
              (+workspace-switch name t)
-             (persp-delete-other-windows)
-             (switch-to-buffer (doom-fallback-buffer))
              (+workspace/display)))
     ((debug error) (+workspace-error (cadr e) t))))
 

commit bd7f1f6d17d752f490fa290ca850a1e3784229c5

Fix wrong-type-arg on project switch #836
A regression caused by e40a58c4, which made +workspace-new return
something other than a perspective struct.

modules/feature/workspaces/autoload/workspaces.el
@@ -161,7 +161,8 @@ Otherwise return t on success, nil otherwise."
       (delete-other-windows)
       (switch-to-buffer (doom-fallback-buffer))
       (setf (persp-window-conf persp)
-            (funcall persp-window-state-get-function (selected-frame))))))
+            (funcall persp-window-state-get-function (selected-frame))))
+    persp))
 
 ;;;###autoload
 (defun +workspace-rename (name new-name)