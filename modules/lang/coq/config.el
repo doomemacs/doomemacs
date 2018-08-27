5492315053fdc0957ed4e60e096551039c11a395
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-08-27 13:57:00 +0200

    Added lookup handlers

--- modules/lang/coq/config.el
+++ modules/lang/coq/config.el
@@ -1,5 +1,9 @@
 ;;; lang/coq/config.el -*- lexical-binding: t; -*-
 
 (after! company-coq
+  (set-lookup-handlers! 'company-coq-mode
+    :definition #'company-coq-jump-to-definition
+    :references #'company-coq-occur
+    :documentation #'company-coq-doc)
   (when (not (featurep! :completion company))
     (setq company-coq-disabled-features '(company company-defaults))))
