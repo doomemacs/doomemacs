d550b082ef80899d4dd71c278a56ad3bfff69130
The-BigDaddy <pragmatic.programmer@protonmail.com>
2018-08-31 14:48:12 -0500

    added support for OCaml's UTop

--- modules/lang/ocaml/config.el
+++ modules/lang/ocaml/config.el
@@ -1,7 +1,23 @@
 ;;; lang/ocaml/config.el -*- lexical-binding: t; -*-
 
 (def-package! tuareg
-  :mode ("\\.ml[4ilpy]?\\'" . tuareg-mode))
+  :mode ("\\.ml[4ilpy]?\\'" . tuareg-mode)
+  :config
+  (def-package! utop
+    :when (and (featurep! :feature eval)
+               (executable-find "opam")
+               (executable-find "utop"))
+    :init
+    (setq utop-command "opam config exec -- utop -emacs")
+    :hook
+    (tuareg-mode . utop-mode)
+    :config
+    (set-popup-rules! "*\\utop" :quit nil :ttl nil)
+    (set-repl-handler! 'tuareg-mode #'utop)
+    (set-lookup-handlers! 'tuareg-mode
+      :definition #'utop-type-at-point
+      :documentation #'utop-ident-at-point)))
+
 
 
 (def-package! merlin
