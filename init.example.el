3d5c8ba27964e09054b54d6d568d2a690c12358d
Henrik Lissner <henrik@lissner.net>
2018-09-02 17:18:55 +0200

    Add :lang idris to init.example.el
    Needs a witty comment!

--- init.example.el
+++ init.example.el
@@ -94,6 +94,7 @@
       ;go                ; the hipster dialect
       ;(haskell +intero) ; a language that's lazier than I am
       ;hy                ; readability of scheme w/ speed of python
+      ;idris             ;
       ;(java +meghanada) ; the poster child for carpal tunnel syndrome
       ;javascript        ; all(hope(abandon(ye(who(enter(here))))))
       ;julia             ; a better, faster MATLAB
