dc3a3660943e18d93d0f7d925ec9ca3b12f2fd19
Ruslan Kamashev <rynffoll@gmail.com>
2018-09-08 03:51:37 +0300

    Fix bindings

--- modules/tools/docker/config.el
+++ modules/tools/docker/config.el
@@ -1,22 +1,12 @@
 ;;; tools/docker/config.el -*- lexical-binding: t; -*-
 
-(def-package! docker)
-(def-package! docker-tramp)
-(def-package! dockerfile-mode)
-
-(after! docker
+(def-package! docker
+  :config
   (set-evil-initial-state! 'docker-container-mode 'emacs)
   (set-evil-initial-state! 'docker-image-mode 'emacs)
   (set-evil-initial-state! 'docker-network-mode 'emacs)
   (set-evil-initial-state! 'docker-volume-mode 'emacs)
   (set-evil-initial-state! 'docker-machine-mode 'emacs))
 
-(map!
- (:leader
-   (:prefix "d"
-     :desc "Docker":n            "." #'docker
-     :desc "Docker Containers":n "c" #'docker-containers
-     :desc "Docker Images":n     "i" #'docker-images
-     :desc "Docker Networks":n   "n" #'docker-networks
-     :desc "Docker Volumes":n    "v" #'docker-volumes
-     :desc "Docker Machines":n   "m" #'docker-machines)))
+(def-package! docker-tramp)
+(def-package! dockerfile-mode)
