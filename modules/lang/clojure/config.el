1535a8d64ab845fe2670779a67c3bea97876aa25
Henrik Lissner <henrik@lissner.net>
2018-09-09 16:35:16 -0400

    lang/clojure: move flycheck-joker block

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -40,8 +40,8 @@
     :config
     (map! :map clj-refactor-map
           :localleader
-          :desc "Refactor hydra" :n "r" #'hydra-cljr-help-menu/body)))
+          :desc "Refactor hydra" :n "r" #'hydra-cljr-help-menu/body))
 
-(when (featurep! :feature syntax-checker)
   (def-package! flycheck-joker
-    :after (flycheck clojure-mode)))
+    :when (featurep! :feature syntax-checker)
+    :after flycheck))
