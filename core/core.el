142d3646efd43e985349ce074433e533a87b5524
Henrik Lissner <henrik@lissner.net>
2018-09-13 11:07:32 -0400

    Add doom-private-dir to load-path
    Makes private autoload paths simpler

--- core/core.el
+++ core/core.el
@@ -466,6 +466,8 @@ in interactive sessions, nil otherwise (but logs a warning)."
 (unless noninteractive
   (doom-initialize-modules))
 
+(add-to-list 'load-path doom-private-dir)
+
 (after! package
   (require 'core-packages))
 
