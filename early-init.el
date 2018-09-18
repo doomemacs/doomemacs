fae47fc448b7b5f50e5a899ce6f876d7ef9a6d7b
Edwin Török <edwin@etorok.net>
2018-09-19 00:25:17 +0100

    Emacs27+: reduce number of GCs on startup from 4 to 1
    Signed-off-by: Edwin Török <edwin@etorok.net>

--- early-init.el
+++ early-init.el
@@ -3,6 +3,12 @@
 ;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
 ;; before package and UI initialization happens.
 
+
+(defconst doom-gc-cons-upper-limit 268435456 ; 256mb
+          "The temporary value for `gc-cons-threshold' to defer it.")
+;; This reduces gcs-done from 4 to 1 on startup
+(setq gc-cons-threshold doom-gc-cons-upper-limit)
+
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'. Doom handles package
 ;; initialization, so we must prevent Emacs from doing it early!
