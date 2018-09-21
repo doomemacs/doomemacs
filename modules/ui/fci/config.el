02a5109c4f76243dce7f572428b78047c9672938
Henrik Lissner <henrik@lissner.net>
2018-09-20 22:28:39 -0400

    Comment on disabling fci-mode in org-mode

--- modules/ui/fci/config.el
+++ modules/ui/fci/config.el
@@ -17,6 +17,8 @@ Changes to this variable do not take effect until `fci-mode' is restarted.")
 (def-package! fill-column-indicator
   :hook ((text-mode prog-mode conf-mode) . turn-on-fci-mode)
   :config
+  ;; fci is broken in `org-mode' when `org-indent-mode' is active. org-indent is
+  ;; more important to me, so...
   (add-hook 'org-mode-hook #'turn-off-fci-mode)
 
   (defun +fci|set-color ()
