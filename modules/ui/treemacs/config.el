d5f8ecdfcee614dec435485b1b8aee8acbaab054
Henrik Lissner <henrik@lissner.net>
2018-09-27 18:16:21 -0400

    ui/treemacs: fix leader key in treemacs
    This is an ugly hack and I should be ashamed.
    
    Shame.
    
    Shame.
    
    Shame.
    
    But alas, it shall do until general.el is in.
    
    Fixes #860

--- modules/ui/treemacs/config.el
+++ modules/ui/treemacs/config.el
@@ -48,7 +48,86 @@ There are 2 possible values:
 (def-package! treemacs-evil
   :when (featurep! :feature evil +everywhere)
   :after treemacs
-  :config (define-key evil-treemacs-state-map [escape] #'delete-window))
+  :config
+  ;; FIXME Remove this when general.el is in
+  (set-evil-initial-state! 'treemacs-mode 'normal)
+  (map! :map treemacs-mode-map
+        :n "?"               #'treemacs-helpful-hydra
+        :n [mouse-1]         #'treemacs-leftclick-action
+        :n [double-mouse-1]  #'treemacs-doubleclick-action
+        :n [tab]             #'treemacs-TAB-action
+        :n [?\t]             #'treemacs-TAB-action
+        :n [return]          #'treemacs-RET-action
+        :n "RET"       #'treemacs-RET-action
+        :n "r"         #'treemacs-refresh
+        :n "d"         #'treemacs-delete
+        :n "cf"        #'treemacs-create-file
+        :n "cd"        #'treemacs-create-dir
+        :n "R"         #'treemacs-rename
+        :n "u"         #'treemacs-goto-parent-node
+        :n "q"         #'bury-buffer
+        :n "Q"         #'treemacs-kill-buffer
+        :n "ov"        #'treemacs-visit-node-vertical-split
+        :n "oh"        #'treemacs-visit-node-horizontal-split
+        :n "oo"        #'treemacs-visit-node-no-split
+        :n "oaa"       #'treemacs-visit-node-ace
+        :n "oah"       #'treemacs-visit-node-ace-horizontal-split
+        :n "oav"       #'treemacs-visit-node-ace-vertical-split
+        :n "ox"        #'treemacs-visit-node-in-external-application
+        :n "P"         #'treemacs-peek
+        :n "n"         #'treemacs-next-line
+        :n "p"         #'treemacs-previous-line
+        :n "M-N"       #'treemacs-next-line-other-window
+        :n "M-P"       #'treemacs-previous-line-other-window
+        :n "<prior>"   #'treemacs-previous-page-other-window
+        :n "<next>"    #'treemacs-next-page-other-window
+        :n "M-n"       #'treemacs-next-neighbour
+        :n "M-p"       #'treemacs-previous-neighbour
+        :n "th"        #'treemacs-toggle-show-dotfiles
+        :n "tw"        #'treemacs-toggle-fixed-width
+        :n "tv"        #'treemacs-fringe-indicator-mode
+        :n "tg"        #'treemacs-git-mode
+        :n "tf"        #'treemacs-follow-mode
+        :n "ta"        #'treemacs-filewatch-mode
+        :n "w"         #'treemacs-set-width
+        :n "yy"        #'treemacs-copy-path-at-point
+        :n "yr"        #'treemacs-copy-project-root
+        :n "s"         #'treemacs-resort
+        :n "b"         #'treemacs-add-bookmark
+        :n "C-p r"     #'treemacs-rename-project
+        :n "C-p a"     #'treemacs-add-project-to-workspace
+        :n "C-p d"     #'treemacs-remove-project-from-workspace
+        :n "C-p c c"   #'treemacs-collapse-project
+        :n "C-p c o"   #'treemacs-collapse-other-projects
+        :n "C-p c a"   #'treemacs-collapse-all-projects
+        :n "<backtab>" #'treemacs-collapse-all-projects
+        :n "C-j"       #'treemacs-next-project
+        :n "C-k"       #'treemacs-previous-project
+        :n "h"         #'treemacs-root-up
+        :n "l"         #'treemacs-root-down
+        :n [escape] #'delete-window
+        :n "j"   #'treemacs-next-line
+        :n "k"   #'treemacs-previous-line
+        :n "M-j" #'treemacs-next-neighbour
+        :n "M-k" #'treemacs-previous-neighbour
+        :n "M-J" #'treemacs-next-line-other-window
+        :n "M-K" #'treemacs-previous-line-other-window
+        :n "th"  #'treemacs-toggle-show-dotfiles
+        :n "tw"  #'treemacs-toggle-fixed-width
+        :n "tv"  #'treemacs-fringe-indicator-mode
+        :n "tf"  #'treemacs-follow-mode
+        :n "ta"  #'treemacs-filewatch-mode
+        :n "tg"  #'treemacs-git-mode
+        :n "w"   #'treemacs-set-width
+        :n "b"   #'treemacs-add-bookmark
+        :n "?"   #'treemacs-helpful-hydra
+        :n "RET" #'treemacs-RET-action
+        :n "yr"     #'treemacs-copy-project-root
+        :n "yy"     #'treemacs-copy-path-at-point
+        :n "gr"     #'treemacs-refresh
+        :n [down-mouse-1] #'ignore
+        :n "h"      #'treemacs-root-up
+        :n "l"      #'treemacs-root-down))
 
 
 (def-package! treemacs-projectile
