702e0a61590c6615318552d24ed55f7dec3f9904
Henrik Lissner <henrik@lissner.net>
2018-10-03 02:32:22 -0400

    ui/popup: fix typo in d8928d4a

--- modules/ui/popup/+hacks.el
+++ modules/ui/popup/+hacks.el
@@ -302,7 +302,7 @@ instead of switch-to-buffer-*."
 
 ;; `which-key'
 (after! which-key
-  (when (eq which-key-popup-type 'side)
+  (when (eq which-key-popup-type 'side-window)
     (setq which-key-popup-type 'custom
           which-key-custom-popup-max-dimensions-function (lambda (_) (which-key--side-window-max-dimensions))
           which-key-custom-hide-popup-function #'which-key--hide-buffer-side-window
