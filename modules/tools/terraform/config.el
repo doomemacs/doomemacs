112d7729955a2151d4a81ebc2caa81af017ae4f1
Henrik Lissner <henrik@lissner.net>
2018-10-15 23:17:29 -0400

    tools/terraform: use map!'s :after

--- modules/tools/terraform/config.el
+++ modules/tools/terraform/config.el
@@ -1,11 +1,12 @@
 ;;; tools/terraform/config.el -*- lexical-binding: t; -*-
 
-(after! terraform-mode
-  (map! :map terraform-mode-map
-        :localleader
-        :n "a" (λ! (compile "terraform apply"))
-        :n "i" (λ! (compile "terraform init"))
-        :n "p" (λ! (compile "terraform plan"))))
+(map! :after terraform-mode
+      :map terraform-mode-map
+      :localleader
+      :n "a" (λ! (compile "terraform apply"))
+      :n "i" (λ! (compile "terraform init"))
+      :n "p" (λ! (compile "terraform plan")))
+
 
 (def-package! company-terraform
   :when (featurep! :completion company)
