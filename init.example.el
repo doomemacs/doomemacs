85c698587c24c28a9653a0f3129b395e3fe7c882
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2018-10-16 06:22:34 +0300

    Add vala-mode

--- init.example.el
+++ init.example.el
@@ -127,8 +127,9 @@
        ;;scala             ; java, but good
        (sh +fish)        ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       ;;swtft             ; who asked for emoji variables?
        ;;web               ; the tubes
+       ;;vala              ; GObjective-C
 
        ;; Applications are complex and opinionated modules that transform Emacs
        ;; toward a specific purpose. They may have additional dependencies and
