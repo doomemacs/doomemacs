19539dacef1d37e187e4cfe9d57c14473e196bee
Henrik Lissner <accounts@v0.io>
2018-11-13 18:15:32 -0500

    Alphabetize & disable lisp in init.example.el

--- init.example.el
+++ init.example.el
@@ -48,10 +48,10 @@
 
        :editor
        ;;(format +onsave)  ; automated prettiness
+       ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;;parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
-       lispy             ; vim for lisp, for people who dont like vim
 
        :emacs
        dired             ; making dired pretty [functional]
