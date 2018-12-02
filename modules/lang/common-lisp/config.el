89c1180b7336dbb99aeb69176e5631e71006b0be
Michael Fiano <mail@michaelfiano.com>
2018-12-02 00:42:15 -0500

    Minor: lexicographically sort bindings
    Just so I can see what is bound already more easily.

--- modules/lang/common-lisp/config.el
+++ modules/lang/common-lisp/config.el
@@ -70,14 +70,16 @@ bin/doom while packages at compile-time (not a runtime though)."
         :localleader
         :n "'" #'sly
         (:prefix "g"
+          :n "b" #'sly-pop-find-definition-stack
           :n "d" #'sly-edit-definition
           :n "D" #'sly-edit-definition-other-window
-          :n "b" #'sly-pop-find-definition-stack
           :n "n" #'sly-next-note
           :n "N" #'sly-previous-note
           :n "s" #'sly-stickers-next-sticker
           :n "S" #'sly-stickers-prev-sticker)
         (:prefix "h"
+          :n "<" #'sly-who-calls
+          :n ">" #'sly-calls-who
           :n "a" #'sly-apropos
           :n "b" #'sly-who-binds
           :n "d" #'sly-disassemble-symbol
@@ -87,9 +89,7 @@ bin/doom while packages at compile-time (not a runtime though)."
           :n "p" #'sly-apropos-package
           :n "r" #'sly-who-references
           :n "s" #'sly-who-specializes
-          :n "S" #'sly-who-sets
-          :n "<" #'sly-who-calls
-          :n ">" #'sly-calls-who)
+          :n "S" #'sly-who-sets)
         (:prefix "c"
           :n "c" #'sly-compile-file
           :n "C" #'sly-compile-and-load-file
@@ -142,44 +142,18 @@ bin/doom while packages at compile-time (not a runtime though)."
     (evil-define-key 'normal sly-parent-map
       (kbd "C-t") #'sly-pop-find-definition-stack)
     (evil-define-key 'normal sly-db-mode-map
-      (kbd "RET") 'sly-db-default-action
-      (kbd "C-m") 'sly-db-default-action
-      [return] 'sly-db-default-action
-      [mouse-2]  'sly-db-default-action/mouse
       [follow-link] 'mouse-face
-      "\C-i" 'sly-db-cycle
-      "g?" 'describe-mode
-      "S" 'sly-db-show-source
-      "e" 'sly-db-eval-in-frame
-      "d" 'sly-db-pprint-eval-in-frame
-      "D" 'sly-db-disassemble
-      "i" 'sly-db-inspect-in-frame
-      "gj" 'sly-db-down
-      "gk" 'sly-db-up
+      [mouse-2]  'sly-db-default-action/mouse
+      [return] 'sly-db-default-action
+      (kbd "C-i") 'sly-db-cycle
       (kbd "C-j") 'sly-db-down
       (kbd "C-k") 'sly-db-up
-      "]" 'sly-db-details-down
-      "[" 'sly-db-details-up
+      (kbd "C-m") 'sly-db-default-action
       (kbd "C-S-j") 'sly-db-details-down
       (kbd "C-S-k") 'sly-db-details-up
-      "gg" 'sly-db-beginning-of-backtrace
-      "G" 'sly-db-end-of-backtrace
-      "t" 'sly-db-toggle-details
-      "gr" 'sly-db-restart-frame
-      "I" 'sly-db-invoke-restart-by-name
-      "R" 'sly-db-return-from-frame
-      "c" 'sly-db-continue
-      "s" 'sly-db-step
-      "n" 'sly-db-next
-      "o" 'sly-db-out
-      "b" 'sly-db-break-on-return
-      "a" 'sly-db-abort
-      "q" 'sly-db-quit
-      "A" 'sly-db-break-with-system-debugger
-      "B" 'sly-db-break-with-default-debugger
-      "P" 'sly-db-print-condition
-      "C" 'sly-db-inspect-condition
-      "g:" 'sly-interactive-eval
+      (kbd "RET") 'sly-db-default-action
+      "]" 'sly-db-details-down
+      "[" 'sly-db-details-up
       "0" 'sly-db-invoke-restart-0
       "1" 'sly-db-invoke-restart-1
       "2" 'sly-db-invoke-restart-2
@@ -189,46 +163,72 @@ bin/doom while packages at compile-time (not a runtime though)."
       "6" 'sly-db-invoke-restart-6
       "7" 'sly-db-invoke-restart-7
       "8" 'sly-db-invoke-restart-8
-      "9" 'sly-db-invoke-restart-9)
+      "9" 'sly-db-invoke-restart-9
+      "a" 'sly-db-abort
+      "A" 'sly-db-break-with-system-debugger
+      "b" 'sly-db-break-on-return
+      "B" 'sly-db-break-with-default-debugger
+      "c" 'sly-db-continue
+      "C" 'sly-db-inspect-condition
+      "d" 'sly-db-pprint-eval-in-frame
+      "D" 'sly-db-disassemble
+      "e" 'sly-db-eval-in-frame
+      "g:" 'sly-interactive-eval
+      "g?" 'describe-mode
+      "gg" 'sly-db-beginning-of-backtrace
+      "gj" 'sly-db-down
+      "gk" 'sly-db-up
+      "gr" 'sly-db-restart-frame
+      "G" 'sly-db-end-of-backtrace
+      "i" 'sly-db-inspect-in-frame
+      "I" 'sly-db-invoke-restart-by-name
+      "n" 'sly-db-next
+      "o" 'sly-db-out
+      "P" 'sly-db-print-condition
+      "q" 'sly-db-quit
+      "R" 'sly-db-return-from-frame
+      "s" 'sly-db-step
+      "S" 'sly-db-show-source
+      "t" 'sly-db-toggle-details)
     (evil-define-key 'normal sly-inspector-mode-map
-      [return] 'sly-inspector-operate-on-point
-      (kbd "C-m") 'sly-inspector-operate-on-point
+      [backtab] 'sly-inspector-previous-inspectable-object
       [mouse-1] 'sly-inspector-operate-on-click
       [mouse-2] 'sly-inspector-operate-on-click
       [mouse-6] 'sly-inspector-pop
       [mouse-7] 'sly-inspector-next
-      "gk" 'sly-inspector-pop
+      [return] 'sly-inspector-operate-on-point
+      [(shift tab)] 'sly-inspector-previous-inspectable-object
+      (kbd "C-i") 'sly-inspector-next-inspectable-object
       (kbd "C-k") 'sly-inspector-pop
+      (kbd "C-m") 'sly-inspector-operate-on-point
+      "." 'sly-inspector-show-source
+      "e" 'sly-inspector-eval
       "gj" 'sly-inspector-next
+      "gk" 'sly-inspector-pop
+      "gr" 'sly-inspector-reinspect
+      "gR" 'sly-inspector-fetch-all
+      "gv" 'sly-inspector-toggle-verbose
       "j" 'sly-inspector-next
+      "h" 'sly-inspector-history
       "k" 'sly-inspector-previous-inspectable-object
       "K" 'sly-inspector-describe
       "p" 'sly-inspector-pprint
-      "e" 'sly-inspector-eval
-      "h" 'sly-inspector-history
-      "gr" 'sly-inspector-reinspect
-      "gv" 'sly-inspector-toggle-verbose
-      "\C-i" 'sly-inspector-next-inspectable-object
-      [(shift tab)] 'sly-inspector-previous-inspectable-object ; Emacs translates S-TAB
-      [backtab] 'sly-inspector-previous-inspectable-object     ; to BACKTAB on X.
-      "." 'sly-inspector-show-source
-      "gR" 'sly-inspector-fetch-all
-      "q" 'sly-inspector-quit)
+      "q" 'sly-inspector-quit
     (evil-define-key 'normal sly-mode-map
       (kbd "C-t") 'sly-pop-find-definition-stack)
     (evil-define-key 'normal sly-popup-buffer-mode-map
-      "q" 'quit-window
-      (kbd "C-t") 'sly-pop-find-definition-stack)
+      (kbd "C-t") 'sly-pop-find-definition-stack
+      "q" 'quit-window)
     (evil-define-key 'normal sly-xref-mode-map
       (kbd "RET") 'sly-goto-xref
       (kbd "S-<return>") 'sly-goto-xref
-      "go" 'sly-show-xref
-      "gj" 'sly-xref-next-line
-      "gk" 'sly-xref-prev-line
       (kbd "C-j") 'sly-xref-next-line
       (kbd "C-k") 'sly-xref-prev-line
       "]" 'sly-xref-next-line
       "[" 'sly-xref-prev-line
+      "gj" 'sly-xref-next-line
+      "gk" 'sly-xref-prev-line
+      "go" 'sly-show-xref
       "gr" 'sly-recompile-xref
       "gR" 'sly-recompile-all-xrefs
       "r" 'sly-xref-retract)))
