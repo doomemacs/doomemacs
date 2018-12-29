d78c3272d8f35716b824b67b35b3b71871369156
Brett Mandler <brettmandler@gmail.com>
2018-12-29 02:33:52 +0000

    Added +ivy-hex-colors

--- modules/completion/ivy/autoload/ivy.el
+++ modules/completion/ivy/autoload/ivy.el
@@ -1,5 +1,61 @@
 ;;; completion/ivy/autoload/ivy.el -*- lexical-binding: t; -*-
 
+;;;###autoload
+(defun +ivy-colors-action (string)
+  "Jumps to the line that a color is on"
+  (let* ((start 0)
+         (end (length string)))
+    (set-text-properties 0 (length string) nil string)
+    (goto-line (string-to-number string))))
+
+;;;###autoload
+(defun +ivy--hex-colors-candidates ()
+  "Return a list of `+ivy-hex-colors' candidates."
+  (let ((matches))
+    (save-match-data
+      (save-excursion
+        (with-current-buffer (current-buffer)
+          (save-excursion
+            (goto-char (point-min))
+            (while (re-search-forward "\\(#\\(?:[0-9a-fA-F]\\{3\\}\\)+\\{1,4\\}\\)" nil t 1)
+              (push
+               (propertize
+                (format-mode-line "%l")
+                'hex
+                (downcase
+                 (match-string-no-properties 0)))
+               matches)))))
+      matches)))
+
+;;;###autoload
+(defun +ivy/hex-colors ()
+  "Open a list of hex colors in the current buffer to select with ivy"
+  (interactive)
+  (let* ((colors (+ivy--hex-colors-candidates))
+         (blank (make-string 10 ?\s))
+         (fmt (format "%%-%ds %%s %%s"
+                      (apply #'max 0 (mapcar #'string-width colors))))
+         (ivy-format-function
+          (counsel-colors--formatter
+           (lambda (color)
+             (let ((hex (get-text-property 0 'hex color)))
+               (format fmt color
+                       (propertize hex 'face (list :foreground hex))
+                       (propertize blank 'face (list :background hex))))))))
+    (ivy-read
+     "Jump to color: " colors
+     :require-match t
+     :history '+ivy-hex-colors-history
+     :sort t
+     :action #'+ivy--hex-colors-action)))
+(defun +ivy--is-workspace-or-other-buffer-p (buffer)
+  (let ((buffer (car buffer)))
+    (when (stringp buffer)
+      (setq buffer (get-buffer buffer)))
+    (and (not (eq buffer (current-buffer)))
+         (+workspace-contains-buffer-p buffer))))
+
+
 (defun +ivy--is-workspace-or-other-buffer-p (buffer)
   (let ((buffer (car buffer)))
     (when (stringp buffer)
