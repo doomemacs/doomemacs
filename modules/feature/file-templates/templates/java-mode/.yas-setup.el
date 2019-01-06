b0fd3ee9a5b3d14b421961c1636851f7d5ef726f
Aaron Cordova <acordova05@gmail.com>
2019-01-06 07:41:39 -0700

    Fixes java-mode project package function
    - Use doom-project-dir instead of undefined narf function.
    - Add support for maven/gradle main/java and test/java prefix paths.

--- modules/feature/file-templates/templates/java-mode/.yas-setup.el
+++ modules/feature/file-templates/templates/java-mode/.yas-setup.el
@@ -1,7 +1,8 @@
 (defun yas-java-project-package ()
   (if (eq major-mode 'java-mode)
-    (s-chop-suffix "." (s-replace "/" "." (f-dirname (f-relative (buffer-file-name)
-                                                                 (concat (narf/project-root) "/src/")))))
+    (s-chop-prefixes '("main.java." "test.java.")
+                     (s-chop-suffix "." (s-replace "/" "." (f-dirname (f-relative (buffer-file-name)
+                                                                 (concat (doom-project-root) "/src/"))))))
     ""))
 
 (defun yas-java-class-name ()
