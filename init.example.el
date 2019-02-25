b7228fdfad2ac70bd8cc2c42a064c7d0df718263
Henrik Lissner <henrik@lissner.net>
2019-02-24 22:24:57 -0500

    Default to ivy, rather than helm
    As an ivy user, I maintain the ivy module more actively. Also, my former
    misgivings with ivy have mostly be resolved.

--- init.example.el
+++ init.example.el
@@ -13,9 +13,9 @@
 
        :completion
        company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
+       ;;helm             ; the *other* search engine for love and life
        ;;ido              ; the other *other* search engine...
-       ;;ivy              ; a search engine for love and life
+       ivy              ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
