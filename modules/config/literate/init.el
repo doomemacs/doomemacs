5cdc2127be71001c05fd6b8083bba4317e77b161
Henrik Lissner <henrik@lissner.net>
2019-03-28 01:59:25 -0400

    Refactor config/literate module
    + Now recompiles literate config if you modify org files in DOOMDIR
    + Replaced +literate/compile with +literate/reload
    + Calls org-babel-tangle-file directly if org package is loaded
    + Change mtime check mechanism for literate tangling at startup

--- modules/config/literate/init.el
+++ modules/config/literate/init.el
@@ -4,34 +4,41 @@
   (expand-file-name "config.org" doom-private-dir)
   "The file path of your literate config file.")
 
-(defvar +literate-config-dest-file
-  (expand-file-name "config.el" doom-private-dir)
+(defvar +literate-config-cache-file
+  (expand-file-name "literate-last-compile" doom-cache-dir)
   "The file path that `+literate-config-file' will be tangled to, then
 byte-compiled from.")
 
 
 ;;
-(defun +literate-compile (&optional load)
-  "Tangles & compiles `+literate-config-file' if it has changed. If LOAD is
-non-nil, load it too!"
-  (let ((org +literate-config-file)
-        (el  +literate-config-dest-file))
-    (when (file-newer-than-file-p org el)
+(defun +literate-tangle (&optional force-p)
+  "Tangles `+literate-config-file' if it has changed."
+  (let ((default-directory doom-private-dir)
+        (org +literate-config-file))
+    (when (or force-p (file-newer-than-file-p org +literate-config-cache-file))
       (message "Compiling your literate config...")
 
-      ;; We tangle in a separate, blank process because loading it here would
-      ;; load all of :lang org (very expensive!). We only need ob-tangle.
-      (or (zerop (call-process
-                  "emacs" nil nil nil
-                  "-q" "--batch" "-l" "ob-tangle" "--eval"
-                  (format "(org-babel-tangle-file \"%s\" \"%s\" \"emacs-lisp\")"
-                          org el)))
+      (or (and (if (fboundp 'org-babel-tangle-file)
+                   (org-babel-tangle-file org nil "emacs-lisp")
+                 ;; We tangle in a separate, blank process because loading it here
+                 ;; would load all of :lang org (very expensive!).
+                 (zerop (call-process
+                         "emacs" nil nil nil
+                         "-q" "--batch" "-l" "ob-tangle" "--eval"
+                         (format "(org-babel-tangle-file %S nil \"emacs-lisp\")"
+                                 org))))
+               ;; Write the cache file to serve as our mtime cache
+               (with-temp-file +literate-config-cache-file t))
           (warn "There was a problem tangling your literate config!"))
 
       (message "Done!"))))
 
 ;; Let 'er rip!
-(+literate-compile)
-
+(+literate-tangle doom-reloading-p)
 ;; No need to load the resulting file. Doom will do this for us after all
 ;; modules have finished loading.
+
+
+;; Recompile our literate config if we modify it
+(after! org
+  (add-hook 'after-save-hook #'+literate|recompile-maybe))
