8e18b402ce916f595f24964682e19064175cb833
Henrik Lissner <henrik@lissner.net>
2019-04-10 18:51:37 -0400

    lang/rust: run cargo commands from Cargo.toml dir
    Instead of the current directory, which breaks file links in the
    compilation buffer.

--- modules/lang/rust/autoload.el
+++ modules/lang/rust/autoload.el
@@ -6,3 +6,9 @@
 (defun +rust-cargo-project-p ()
   "Return t if this is a cargo project."
   (locate-dominating-file buffer-file-name "Cargo.toml"))
+
+;;;###autoload
+(defun +rust-cargo-compile (command)
+  "TODO"
+  (let ((default-directory (+rust-cargo-project-p)))
+    (compile command)))
