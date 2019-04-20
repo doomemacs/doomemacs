fc2e0e3970da6f3e1e5c06cff9e943f984948a61
Henrik Lissner <henrik@lissner.net>
2019-04-20 11:43:46 -0400

    Update modules/README.org
    And alphabetize treemacs in init.example.el

--- init.example.el
+++ init.example.el
@@ -32,9 +32,9 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       treemacs          ; a project drawer, like neotree but cooler
        ;;pretty-code       ; replace bits of code with pretty symbols
        ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
