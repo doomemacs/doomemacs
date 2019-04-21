7e121c9ce51d832cd5920637215c34594b23bf78
prow <goldenmice@qq.com>
2019-04-21 18:09:07 +0800

    use directory local variable to switch between lsp & gtags
    when open a projectile does not contain 'gtags-enable-for-this-project',
    it will ask user whether enable gtags. And supply a function to let user
    figure out which mode they want to enable gtags.
    
    Todo:
    find a way to trigger dir local variable detect when open file using
    'find-file'

--- modules/tools/gtags/config.el
+++ modules/tools/gtags/config.el
@@ -1,22 +1,33 @@
 ;;; tools/gtags/config.el -*- lexical-binding: t; -*-
 
-(defvar +ggtags-default-enabled-mode
-   (list 'c-mode
-         'c++-mode
-         'java-mode)
-   "A list of mode which ggtags enabled")
+(defun +gtags-ask-enabled-for-project ()
+  "check `+gtags-check-project-enabled-flag' "
+  (when (not (boundp '+gtags-enable-for-this-project))
+    (let ((default-directory (projectile-project-root)))
+      (add-dir-local-variable nil '+gtags-enable-for-this-project
+                              (yes-or-no-p "Do you want enable gtags for the project?"))
+      (save-buffer)
+      (kill-buffer)
+      (hack-dir-local-variables-non-file-buffer))))
+
+(defmacro +gtags-enable-for-mode! (mode)
+  `(add-hook! ',(intern-soft (format "%s-local-vars-hook" mode))
+     (progn
+       (when +gtags-enable-for-this-project
+         (ggtags-mode 1)
+         (set-lookup-handlers! mode
+           :definition #'ggtags-find-definition
+           :references #'ggtags-find-reference)
+         (when (featurep 'lsp-mode)
+           (lsp-mode -1)
+           (lsp-shutdown-workspace)))
+
+       (message "hook done"))))
 
 (def-package! ggtags
+  :init
+  (add-hook 'projectile-find-file-hook #'+gtags-ask-enabled-for-project)
   :config
-  (add-hook 'c-mode-common-hook
-            (λ!
-             (cl-loop for i in +ggtags-default-enabled-mode
-                      do (when (derived-mode-p i)
-                        (ggtags-mode 1)))))
-  (set-lookup-handlers! +ggtags-default-enabled-mode
-    :definition #'ggtags-find-definition
-    :references #'ggtags-find-reference)
-
   (map! :leader
         (:prefix ("c". "code")
           :desc "Find Symbol" "s" #'ggtags-find-other-symbol)))
