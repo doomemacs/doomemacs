c8974eb0a8a2bd051eaf5a9eeb3d6eee9456a569
Andrew Whatson <whatson@gmail.com>
2019-04-22 18:56:25 +1000

    Nitpick comment alignment in init.el

--- init.example.el
+++ init.example.el
@@ -20,9 +20,9 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;helm             ; the *other* search engine for love and life
-       ;;ido              ; the other *other* search engine...
-       ivy              ; a search engine for love and life
+       ;;helm              ; the *other* search engine for love and life
+       ;;ido               ; the other *other* search engine...
+       ivy               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
