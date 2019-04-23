d4ff90219e215a7066bb798a67757b5fdbcaa4bd
Henrik Lissner <henrik@lissner.net>
2019-04-23 14:03:16 -0400

    lang/cc: remove +irony and +rtags flags from docs
    They were removed a while back. Irony and rtags (et co) will be disabled
    if +lsp is enabled, or can be disabled through package! declarations:
    
      ;; in ~/.doom.d/packages.el
      (package! irony :disable t)
      (package! rtags :disable t)

--- init.example.el
+++ init.example.el
@@ -90,7 +90,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;;(cc +irony +rtags); C/C++/Obj-C madness
+       ;;cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
