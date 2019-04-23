ae11d92042b1e864bcc35f88bda488881aebd48d
chrunchyjesus <chrunchyjesus@tutanota.com>
2019-04-23 20:08:52 +0200

    ony search for shellcheck executable when flycheck is enabled

--- modules/lang/sh/doctor.el
+++ modules/lang/sh/doctor.el
@@ -1,5 +1,5 @@
 ;;; lang/sh/doctor.el -*- lexical-binding: t; -*-
 
-(when! (not (executable-find "shellcheck"))
-  (warn! "Couldn't find shellcheck. Shell script linting will not work"))
-
+(when (featurep! :tools flycheck)
+ (unless (executable-find "shellcheck")
+  (warn! "Couldn't find shellcheck. Shell script linting will not work")))
