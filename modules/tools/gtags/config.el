f73468f842c812007b72848017d48279f1e58458
prow <goldenmice@qq.com>
2019-04-24 11:55:59 +0800

    refactor lsp exclude mechanism

--- modules/tools/gtags/config.el
+++ modules/tools/gtags/config.el
@@ -1,32 +1,30 @@
 ;;; tools/gtags/config.el -*- lexical-binding: t; -*-
 
-(defun +gtags-ask-enabled-for-project ()
-  "check `+gtags-check-project-enabled-flag' "
-  (when (not (boundp '+gtags-enable-for-this-project))
-    (let ((default-directory (projectile-project-root)))
-      (add-dir-local-variable nil '+gtags-enable-for-this-project
-                              (yes-or-no-p "Do you want enable gtags for the project?"))
-      (save-buffer)
-      (kill-buffer)
-      (hack-dir-local-variables-non-file-buffer))))
+(defgroup gtags nil
+  "Gtags related settings"
+  :group 'programming)
+
+(defcustom +gtags-enable-proj-paths '() "specify the path that gtags enable"
+  :group 'gtags
+  :type '(repeat string))
 
 (defmacro +gtags-enable-for-mode! (mode)
-  `(add-hook! ',(intern-soft (format "%s-local-vars-hook" mode))
-     (progn
-       (when +gtags-enable-for-this-project
-         (ggtags-mode 1)
-         (set-lookup-handlers! mode
-           :definition #'ggtags-find-definition
-           :references #'ggtags-find-reference)
-         (when (featurep 'lsp-mode)
-           (lsp-mode -1)
-           (lsp-shutdown-workspace)))
+  `(add-hook! ',(intern-soft (format "%s-hook" mode))
+     (when (or (not (featurep 'lsp-mode)) (member (projectile-project-root) +gtags-enable-proj-paths))
+       (ggtags-mode 1)
+       (set-lookup-handlers! ',mode
+         :definition #'ggtags-find-definition
+         :references #'ggtags-find-reference)
+       (message "hook done!"))))
 
-       (message "hook done"))))
+(advice-add! '(lsp) :around
+             (lambda (orig-fn &rest args)
+               (if (not (member (projectile-project-root) +gtags-enable-proj-paths))
+                   (apply orig-fn args)
+                 t)))
+
 
 (def-package! ggtags
-  :init
-  (add-hook 'projectile-find-file-hook #'+gtags-ask-enabled-for-project)
   :config
   (map! :leader
         (:prefix ("c". "code")
