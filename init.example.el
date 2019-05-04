e9b750cc74ba9b67b4583695f1d42f75046c10b5
Henrik Lissner <henrik@lissner.net>
2019-05-04 19:18:19 -0400

    lang/sh: disable +fish by default

--- init.example.el
+++ init.example.el
@@ -136,7 +136,7 @@
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (sh +fish)        ; she sells (ba|z|fi)sh shells on the C xor
+       sh                  ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
