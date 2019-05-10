f7f96d3abb0d9d2f08f02f5b81b7257488890767
Henrik Lissner <henrik@lissner.net>
2019-05-10 02:02:43 -0400

    ui/indent-guides: disable responsive guides
    It substantially slows down Emacs, so I cannot endorse it as a default.

--- modules/ui/indent-guides/config.el
+++ modules/ui/indent-guides/config.el
@@ -3,8 +3,7 @@
 (def-package! highlight-indent-guides
   :hook ((prog-mode text-mode conf-mode) . highlight-indent-guides-mode)
   :init
-  (setq highlight-indent-guides-method 'character
-        highlight-indent-guides-responsive 'top)
+  (setq highlight-indent-guides-method 'character)
   :config
   ;; Don't display first level of indentation
   (defun +indent-guides-for-all-but-first-column (level responsive display)
