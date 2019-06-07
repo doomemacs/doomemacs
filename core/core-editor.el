02e2940ed0a833f8833598deb219f1d1595fcfb6
Henrik Lissner <henrik@lissner.net>
2019-06-07 23:01:58 +0200

    Fix #1444: recentf hangs Emacs on quit & cleanup
    Caused by using file-truename on (disconnected) remote files, which
    invoked TRAMP authentication.
    
    Replaces #1451

--- core/core-editor.el
+++ core/core-editor.el
@@ -105,13 +105,18 @@ detected.")
         recentf-auto-cleanup 'never
         recentf-max-menu-items 0
         recentf-max-saved-items 200
-        recentf-filename-handlers '(file-truename abbreviate-file-name)
         recentf-exclude
-        (list #'file-remote-p "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$"
-              "^/tmp/" "^/ssh:" "\\.?ido\\.last$" "\\.revive$" "/TAGS$"
-              "^/var/folders/.+$"
+        (list "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$" "^/tmp/" "^/ssh:"
+              "\\.?ido\\.last$" "\\.revive$" "/TAGS$" "^/var/folders/.+$"
               ;; ignore private DOOM temp files
               (recentf-apply-filename-handlers doom-local-dir)))
+
+  (defun doom--recent-file-truename (file)
+    (if (or (file-remote-p file nil t)
+            (not (file-remote-p file)))
+        (file-truename file)
+      file))
+  (setq recentf-filename-handlers '(doom--recent-file-truename abbreviate-file-name))
 
   (defun doom|recentf-touch-buffer ()
     "Bump file in recent file list when it is switched or written to."
