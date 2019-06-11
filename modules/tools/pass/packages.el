6b6ea699f402c0848180189f518b3d730d2e11af
Henrik Lissner <henrik@lissner.net>
2019-06-11 08:07:25 +0200

    tools/pass: use latest version of auth-source-pass
    The upstream version is several major versions (v5.0) ahead of the
    built-in one (v2.0).

--- modules/tools/pass/packages.el
+++ modules/tools/pass/packages.el
@@ -5,9 +5,10 @@
 (package! password-store)
 (package! password-store-otp)
 
-;; `auto-source-pass' is built into Emacs 26+
-(unless EMACS26+
-  (package! auth-source-pass))
+;; an older version of `auto-source-pass' is built into Emacs 26+, so we must
+;; install the new version directly from the source and with a psuedonym.
+(package! auth-source-pass-new
+  :recipe (auth-source-pass :fetcher github :repo "DamienCassou/auth-password-store"))
 
 (when (featurep! :completion ivy)
   (package! ivy-pass))
