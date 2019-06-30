7a449bd29760f8ee36b442dbb3a8b17e9ef019ad
Henrik Lissner <henrik@lissner.net>
2019-06-30 13:20:46 +0200

    tools/lsp: refactor out dash

--- modules/tools/lsp/autoload.el
+++ modules/tools/lsp/autoload.el
@@ -19,6 +19,7 @@ been moved out to their respective modules, or these hooks:
                              (lsp--try-project-root-workspaces (equal arg '(4))
                                                                (and arg (not (equal arg 1)))))))
     (lsp-mode 1)
-    (lsp--info "Connected to %s."
-               (apply 'concat (--map (format "[%s]" (lsp--workspace-print it))
-                                     lsp--buffer-workspaces)))))
+    (lsp--info
+     "Connected to %s."
+     (apply #'concat (mapcar (lambda (it) (format "[%s]" (lsp--workspace-print it)))
+                             lsp--buffer-workspaces)))))
