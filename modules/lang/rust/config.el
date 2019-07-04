b516f128505afc8ae83fbfb7053e3f533f5340bd
chrunchyjesus <chrunchyjesus@tutanota.com>
2019-07-04 21:33:13 +0200

    remove b prefix, small refactor

--- modules/lang/rust/config.el
+++ modules/lang/rust/config.el
@@ -7,22 +7,24 @@
   (when (featurep! +lsp)
     (add-hook 'rust-mode-hook #'lsp!))
 
-  (map! :map rust-mode-map
-        :localleader
-        :prefix "b"
-        :desc "cargo add"    "a" #'cargo-process-add
-        :desc "cargo build"  "b" #'cargo-process-build
-        :desc "cargo build"  "B" #'cargo-process-bench
-        :desc "cargo check"  "c" #'cargo-process-check
-        :desc "cargo clippy" "C" #'cargo-process-clippy
-        :desc "cargo doc"    "d" #'cargo-process-doc
-        :desc "cargo run"    "r" #'cargo-process-run
-        :desc "cargo search" "s" #'cargo-process-search
-        :desc "cargo update" "u" #'cargo-process-update
-        (:prefix-map ("t" . "cargo test")
-          :desc "all"          "a" #'cargo-process-test
-          :desc "current file" "f" #'cargo-process-current-file-tests
-          :desc "current test" "t" #'cargo-process-current-test)))
+  (def-package! cargo
+    :defer t
+    :init
+    (map! :map rust-mode-map
+          :localleader
+          :desc "cargo add"    "a" #'cargo-process-add
+          :desc "cargo build"  "b" #'cargo-process-build
+          :desc "cargo bench"  "B" #'cargo-process-bench
+          :desc "cargo check"  "c" #'cargo-process-check
+          :desc "cargo clippy" "C" #'cargo-process-clippy
+          :desc "cargo doc"    "d" #'cargo-process-doc
+          :desc "cargo run"    "r" #'cargo-process-run
+          :desc "cargo search" "s" #'cargo-process-search
+          :desc "cargo update" "u" #'cargo-process-update
+          (:prefix ("t" . "cargo test")
+            :desc "all"          "a" #'cargo-process-test
+            :desc "current file" "f" #'cargo-process-current-file-tests
+            :desc "current test" "t" #'cargo-process-current-test))))
 
 
 (def-package! racer
