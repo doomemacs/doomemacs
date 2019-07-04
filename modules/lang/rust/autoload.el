9f37363764beedf81f58b763c4feac9ff9fb0493
chrunchyjesus <chrunchyjesus@tutanota.com>
2019-07-04 20:58:44 +0200

    use keybindings from cargo mode

--- modules/lang/rust/autoload.el
+++ modules/lang/rust/autoload.el
@@ -8,12 +8,6 @@
   (locate-dominating-file buffer-file-name "Cargo.toml"))
 
 ;;;###autoload
-(defun +rust-cargo-compile (command)
-  "TODO"
-  (let ((default-directory (+rust-cargo-project-p)))
-    (compile command)))
-
-;;;###autoload
 (defun +rust-racer-lookup-documentation (identifier)
   "A `+lookup/documentation' handler for Rust + Racer."
   (let ((buf (racer--describe identifier)))
