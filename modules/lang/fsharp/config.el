d7f5dc1a79079c85cad9bb68d9bdaf3c49e81fd2
Reed Mullanix <reed.mullanix@mybazinga.com>
2019-07-05 10:58:55 -0700

    Make requested changes for F# lsp support

--- modules/lang/fsharp/config.el
+++ modules/lang/fsharp/config.el
@@ -5,18 +5,16 @@
       (progn
         (setq fsharp-ac-intellisense-enabled nil)
         (setq lsp-fsharp-server-install-dir (concat doom-etc-dir "fsautocomplete/"))
-        (add-hook 'fsharp-mode-hook #'lsp))
-    (progn
-      (setq fsharp-ac-use-popup nil) ; Use a buffer for docs rather than a pop-up
-      (set-lookup-handlers! 'fsharp-mode :async t :definition #'fsharp-ac/gotodefn-at-point)
-      (set-company-backend! 'fsharp-mode 'fsharp-ac/company-backend)
-      (map! :localleader
-            :map fsharp-mode-map
-            "q" #'fsharp-ac/stop-process
-            "t" #'fsharp-ac/show-tooltip-at-point)))
+        (add-hook 'fsharp-mode-local-vars-hook #'lsp!))
+    (setq fsharp-ac-use-popup nil) ; Use a buffer for docs rather than a pop-up
+    (set-lookup-handlers! 'fsharp-mode :async t :definition #'fsharp-ac/gotodefn-at-point)
+    (set-company-backend! 'fsharp-mode 'fsharp-ac/company-backend))
   (set-repl-handler! 'fsharp-mode #'run-fsharp)
   (map! :localleader
         :map fsharp-mode-map
         "b" #'fsharp-ac/pop-gotodefn-stack ; Useful for re-tracing your steps
         "e" #'fsharp-eval-region
-        "l" #'fsharp-load-buffer-file))
+        "l" #'fsharp-load-buffer-file
+        (:unless (featurep! +lsp)
+          "q" #'fsharp-ac/stop-process
+          "t" #'fsharp-ac/show-tooltip-at-point)))
