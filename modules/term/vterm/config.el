648e81f765eaa34268df433faf856176c9128849
Henrik Lissner <henrik@lissner.net>
2019-07-06 21:49:52 +0200

    term/vterm: remove evil config
    It's been implemented in evil-collection-vterm.

--- modules/term/vterm/config.el
+++ modules/term/vterm/config.el
@@ -13,32 +13,4 @@
   ;; Modeline serves no purpose in vterm
   (add-hook 'vterm-mode-hook #'hide-mode-line-mode)
   ;; Don't prompt about processes when killing vterm
-  (setq-hook! 'vterm-mode-hook confirm-kill-processes nil)
-
-  (when (featurep! :editor evil)
-    (evil-set-initial-state 'vterm-mode 'insert)
-    ;; Go back to normal state but don't move cursor backwards. Moving cursor
-    ;; backwards is the default Vim behavior but it is not appropriate in some
-    ;; cases like terminals.
-    (setq-hook! 'vterm-mode-hook evil-move-cursor-back nil)
-    ;; Those keys are commonly needed by terminals.
-    (evil-define-key* 'insert vterm-mode-map
-      (kbd "C-a") #'vterm--self-insert
-      (kbd "C-b") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-d") #'vterm--self-insert
-      (kbd "C-e") #'vterm--self-insert
-      (kbd "C-f") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-k") #'vterm--self-insert
-      (kbd "C-l") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-n") #'vterm--self-insert
-      (kbd "C-o") #'vterm--self-insert
-      (kbd "C-p") #'vterm--self-insert
-      (kbd "C-q") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-r") #'vterm--self-insert
-      (kbd "C-s") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-t") #'vterm--self-insert
-      (kbd "C-u") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-v") #'vterm--self-insert ; Should not be necessary.
-      (kbd "C-w") #'vterm--self-insert
-      (kbd "C-y") #'vterm--self-insert
-      (kbd "C-z") #'vterm--self-insert)))
+  (setq-hook! 'vterm-mode-hook confirm-kill-processes nil))
