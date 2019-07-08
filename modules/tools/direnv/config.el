dd2a9f0cc8949af6a92d7a5dc8d7d117c2faf42f
Henrik Lissner <henrik@lissner.net>
2019-07-08 21:23:49 +0200

    tools/direnv: consult direnv less frequently
    By checking on doom-switch-{buffer,window}-hook and focus-in-hook, which
    fires a lot less often than post-command-hook.

--- modules/tools/direnv/config.el
+++ modules/tools/direnv/config.el
@@ -3,5 +3,15 @@
 (def-package! direnv
   :after-call (after-find-file dired-initial-position-hook)
   :config
+  (defun +direnv|init ()
+    "Instead of checking for direnv on `post-command-hook', check on
+buffer/window/frame switch, which is less expensive."
+    (direnv--disable)
+    (when direnv-mode
+      (add-hook 'doom-switch-buffer-hook #'direnv--maybe-update-environment)
+      (add-hook 'doom-switch-window-hook #'direnv--maybe-update-environment)
+      (add-hook 'focus-in-hook #'direnv--maybe-update-environment)))
+  (add-hook 'direnv-mode-hook #'+direnv|init)
+
   (when (executable-find "direnv")
     (direnv-mode +1)))
