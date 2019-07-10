35372bc34bebf251eee87c90f85805bbf1f79204
Henrik Lissner <henrik@lissner.net>
2019-07-10 02:25:00 +0200

    Add direnv-envrc-mode file template

--- modules/editor/file-templates/config.el
+++ modules/editor/file-templates/config.el
@@ -38,6 +38,8 @@ don't have a :trigger property in `+file-templates-alist'.")
     ("\\.h\\(?:h\\|pp\\|xx\\)$"   :trigger "__hpp" :mode c++-mode)
     ("\\.h$" :trigger "__h" :mode c-mode)
     (c-mode  :trigger "__c")
+    ;; direnv
+    ("/\\.envrc$" :trigger "__envrc" :mode direnv-envrc-mode)
     ;; go
     ("/main\\.go$" :trigger "__main.go" :mode go-mode :project t)
     (go-mode :trigger "__.go")
