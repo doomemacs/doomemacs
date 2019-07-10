9733db65d2c816482796aeb1b1d0b734b80098a6
Henrik Lissner <henrik@lissner.net>
2019-07-10 02:29:13 +0200

    tools/direnv: fontify direnv stdlib keywords

--- modules/tools/direnv/config.el
+++ modules/tools/direnv/config.el
@@ -1,5 +1,12 @@
 ;;; tools/direnv/config.el -*- lexical-binding: t; -*-
 
+(defvar +direnv--keywords
+  '("direnv_layout_dir" "PATH_add" "path_add" "log_status" "log_error" "has"
+    "join_args" "expand_path" "dotenv" "user_rel_path" "find_up" "source_env"
+    "watch_file" "source_up" "direnv_load" "MANPATH_add" "load_prefix" "layout"
+    "use" "rvm" "use_nix" "use_guix")
+  "TODO")
+
 (def-package! direnv
   :after-call (after-find-file dired-initial-position-hook)
   :config
@@ -13,6 +20,12 @@ buffer/window/frame switch, which is less expensive."
       (add-hook 'doom-switch-frame-hook #'direnv--maybe-update-environment)
       (add-hook 'focus-in-hook #'direnv--maybe-update-environment)))
   (add-hook 'direnv-mode-hook #'+direnv|init)
+
+  (defun +direnv|envrc-fontify-keywords ()
+    (font-lock-add-keywords
+     nil `((,(regexp-opt +direnv--keywords 'symbols)
+            (0 font-lock-keyword-face)))))
+  (add-hook 'direnv-envrc-mode-hook #'+direnv|envrc-fontify-keywords)
 
   (defun +direnv*update (&rest _)
     "Update direnv. Useful to advise functions that may run
