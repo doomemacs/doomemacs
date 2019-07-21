62f2f6af481ab849cf0308e650f079806dabd759
Henrik Lissner <henrik@lissner.net>
2019-07-21 14:56:43 +0200

    lang/emacs-lisp: revise macrostep evil keybinds
    Most of these are upstream in evil-collection-macrostep.

--- modules/lang/emacs-lisp/config.el
+++ modules/lang/emacs-lisp/config.el
@@ -89,28 +89,10 @@ This marks a foldable marker for `outline-minor-mode' in elisp buffers.")
 ;;
 ;;; Packages
 
-(when (featurep! :editor evil)
-  (after! macrostep
-    (evil-define-key* 'normal macrostep-keymap
-      [return]  #'macrostep-expand
-      "e"       #'macrostep-expand
-      "u"       #'macrostep-collapse
-      "c"       #'macrostep-collapse
-
-      [tab]     #'macrostep-next-macro
-      "\C-n"    #'macrostep-next-macro
-      "J"       #'macrostep-next-macro
-
-      [backtab] #'macrostep-prev-macro
-      "K"       #'macrostep-prev-macro
-      "\C-p"    #'macrostep-prev-macro
-
-      "q"       #'macrostep-collapse-all
-      "C"       #'macrostep-collapse-all)
-
-    ;; `evil-normalize-keymaps' seems to be required for macrostep or it won't
-    ;; apply for the very first invocation
-    (add-hook 'macrostep-mode-hook #'evil-normalize-keymaps)))
+(map! :when (featurep! :editor evil)
+      :after macrostep
+      :map macrostep-keymap
+      :n [return] #'macrostep-expand)
 
 
 ;;;###package overseer
