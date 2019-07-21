ff5769f117a9d90d20fc3d54e565e44fbfbcc1a8
Henrik Lissner <henrik@lissner.net>
2019-07-21 20:03:11 +0200

    Remove reference to removed cache library

--- modules/tools/prodigy/config.el
+++ modules/tools/prodigy/config.el
@@ -3,10 +3,6 @@
 (after! prodigy
   (set-evil-initial-state! 'prodigy-mode 'emacs)
 
-  ;; Make services, etc persistent between Emacs sessions
-  (doom-cache-persist
-   :prodigy '(prodigy-services prodigy-tags prodigy-filters))
-
   (advice-add #'prodigy-services :around #'+prodigy*services)
 
   (define-key prodigy-mode-map "d" #'+prodigy/delete))
