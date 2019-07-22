d307de829771153232656f78882553fee1e2c353
Henrik Lissner <henrik@lissner.net>
2019-07-22 23:54:43 +0200

    tools/macos: conform to conventions

--- modules/tools/macos/autoload.el
+++ modules/tools/macos/autoload.el
@@ -19,30 +19,30 @@
     (shell-command command)))
 
 ;;;###autoload
-(defmacro +macos!open-with (id &optional app dir)
+(defmacro +macos--open-with (id &optional app dir)
   `(defun ,(intern (format "+macos/%s" id)) ()
      (interactive)
      (+macos-open-with ,app ,dir)))
 
 ;;;###autoload (autoload '+macos/open-in-default-program "tools/macos/autoload" nil t)
-(+macos!open-with open-in-default-program)
+(+macos--open-with open-in-default-program)
 
 ;;;###autoload (autoload '+macos/reveal-in-finder "tools/macos/autoload" nil t)
-(+macos!open-with reveal-in-finder "Finder" default-directory)
+(+macos--open-with reveal-in-finder "Finder" default-directory)
 
 ;;;###autoload (autoload '+macos/reveal-project-in-finder "tools/macos/autoload" nil t)
-(+macos!open-with reveal-project-in-finder "Finder"
-                  (or (doom-project-root) default-directory))
+(+macos--open-with reveal-project-in-finder "Finder"
+                   (or (doom-project-root) default-directory))
 
 ;;;###autoload (autoload '+macos/send-to-transmit "tools/macos/autoload" nil t)
-(+macos!open-with send-to-transmit "Transmit")
+(+macos--open-with send-to-transmit "Transmit")
 
 ;;;###autoload (autoload '+macos/send-cwd-to-transmit "tools/macos/autoload" nil t)
-(+macos!open-with send-cwd-to-transmit "Transmit" default-directory)
+(+macos--open-with send-cwd-to-transmit "Transmit" default-directory)
 
 ;;;###autoload (autoload '+macos/send-to-launchbar "tools/macos/autoload" nil t)
-(+macos!open-with send-to-launchbar "LaunchBar")
+(+macos--open-with send-to-launchbar "LaunchBar")
 
 ;;;###autoload (autoload '+macos/send-project-to-launchbar "tools/macos/autoload" nil t)
-(+macos!open-with send-project-to-launchbar "LaunchBar"
-                  (or (doom-project-root) default-directory))
+(+macos--open-with send-project-to-launchbar "LaunchBar"
+                   (or (doom-project-root) default-directory))
