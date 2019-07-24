650e9049f09926d8c7c46c15f2f57cb40942dd98
Henrik Lissner <henrik@lissner.net>
2019-07-24 15:25:24 +0200

    Autoload hl-fill-column-mode
    Because the package doesn't, for some reason.

--- modules/ui/fill-column/autoload.el
+++ modules/ui/fill-column/autoload.el
@@ -1,4 +1,6 @@
 ;;; ui/fill-column/autoload.el -*- lexical-binding: t; -*-
 
+;;;###autoload (autoload 'hl-fill-column-mode "hl-fill-column" nil t)
+
 ;;;###autoload
 (add-hook! (text-mode prog-mode conf-mode) #'hl-fill-column-mode)
