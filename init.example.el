6381a5c0ae9f3dd639b09636d80f950e7ba92ba1
Andrew Whatson <whatson@gmail.com>
2019-08-26 00:53:18 +1000

    Add :editor word-wrap module
    Defines a new minor-mode `+word-wrap-mode` which configures
    adaptive-wrap-prefix-mode and visual-line-mode for smart soft-wrapping
    of code.
    
    Evil users can toggle it with `SPC t w`.
    
    Enable globally with `+global-word-wrap-mode`.
    
    See the README for more details!

--- init.example.el
+++ init.example.el
@@ -54,6 +54,7 @@
        ;;parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
