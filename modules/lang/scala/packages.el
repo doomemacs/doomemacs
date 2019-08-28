cbd3a3b252a63ce1677c928374f0bc1dfbefd61e
Henrik Lissner <henrik@lissner.net>
2019-08-27 21:24:39 -0400

    Fix #1697: use 2.0 branch for ensime package

--- modules/lang/scala/packages.el
+++ modules/lang/scala/packages.el
@@ -5,4 +5,7 @@
 (package! scala-mode)
 
 (unless (featurep! +lsp)
-  (package! ensime))
+  ;; Fix #1697: Ensime doesn't have a master branch and its MELPA recipe doesn't
+  ;; specify a branch. Straight can't handle packages with non-standard primary
+  ;; branches (at the time of writing), so we must specify it manually:
+  (package! ensime :recipe (:branch "2.0")))
