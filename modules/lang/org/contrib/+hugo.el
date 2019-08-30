8457d2f8256096f3a0dac214ae32c186410660f3
Lorenzo Giuliani <lorenzo@frenzart.com>
2019-08-30 17:23:47 +0200

    descriptions and keybindings as per ox-hugo docs
    Mimicks the ox menu `C-c C-e H #`

--- modules/lang/org/contrib/+hugo.el
+++ modules/lang/org/contrib/+hugo.el
@@ -1,12 +1,30 @@
 ;;; +hugo.el --- ox-hugo support -*- lexical-binding: t; -*-
 ;;;###if (featurep! +hugo)
 
+(defun org-hugo-doom-subtree-to-file-and-open ()
+  (interactive)
+  (org-open-file (org-hugo-export-wim-to-md)))
+
+(defun org-hugo-doom-to-file-and-open ()
+  (interactive)
+  (org-open-file (org-hugo-export-to-md)))
+
+(defun org-hugo-doom-all-subtrees-to-files ()
+  (interactive)
+  (org-hugo-export-wim-to-md :all-subtrees))
+
 (use-package! ox-hugo
-  :after ox
-  (map! :map org-mode-map
-        :localleader
-        (:prefix ("e" . "export")
-          :desc "to hugo"        "h" #'org-hugo-export-to-md
-          :desc "to hugo & open" "H" #'org-hugo-export-as-md)))
+  :after ox)
+
+(map! :map org-mode-map
+      :localleader
+      (:prefix "e"
+        (:prefix ("H" . "hugo")
+          :desc "Subtree to file"         "H" #'org-hugo-export-wim-to-md
+          :desc "To file"                 "h" #'org-hugo-export-wim-to-md
+          :desc "Subtree to file & open"  "O" #'org-hugo-doom-subtree-to-file-and-open
+          :desc "To file & open"          "o" #'org-hugo-doom-to-file-and-open
+          :desc "All subtrees to files"   "a" #'org-hugo-doom-all-subtrees-to-files
+          :desc "To temporary buffer"     "t" #'org-hugo-export-as-md)))
 
 ;;; +hugo.el ends here
