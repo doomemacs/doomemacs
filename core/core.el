280a2407ab9c5db0768e9ebe0e023e6c156b5d7e
Henrik Lissner <henrik@lissner.net>
2019-09-06 14:11:55 -0400

    Fix doom-load-envvars-file
    A former regexp consumed the first letter of each environment variable's
    name.

--- core/core.el
+++ core/core.el
@@ -421,9 +421,9 @@ in interactive sessions, nil otherwise (but logs a warning)."
     (let (vars)
       (with-temp-buffer
         (insert-file-contents file)
-        (while (re-search-forward "\n *[^#] *\\([^= \n]+\\)=" nil t)
+        (while (re-search-forward "\n *\\([^#][^= \n]+\\)=" nil t)
           (save-excursion
-            (let ((var (match-string 1))
+            (let ((var (string-trim-left (match-string 1)))
                   (value (buffer-substring-no-properties
                           (point)
                           (1- (or (when (re-search-forward "^\\([^= ]+\\)=" nil t)
