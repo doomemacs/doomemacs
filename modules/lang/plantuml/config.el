9566050ee84c39f7d1f3963923ce615285a203a6
Sunn Yao <sunyour@gmail.com>
2019-09-07 14:10:29 +0800

    Fix a regexp bug

--- modules/lang/plantuml/config.el
+++ modules/lang/plantuml/config.el
@@ -30,7 +30,7 @@
       (let* ((origin-body body)
              (fix-body
               (replace-regexp-in-string
-               "^\\w*\\(@start\\)"
+               "^[[:blank:]]*\\(@start\\)"
                "\\\\\\1"
                origin-body)))
         (list fix-body params))))
