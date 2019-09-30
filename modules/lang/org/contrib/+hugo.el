ab144f89c2367814edfbd55c29bbe59c8a2da107
Lorenzo Giuliani <lorenzo@frenzart.com>
2019-09-30 16:10:26 +0200

    use the λ! macro

--- modules/lang/org/contrib/+hugo.el
+++ modules/lang/org/contrib/+hugo.el
@@ -10,15 +10,9 @@
         (:prefix ("H" . "hugo")
           :desc "Subtree or File to Md to file"        "H" #'org-hugo-export-wim-to-md
           :desc "File to Md file"                      "h" #'org-hugo-export-to-md
-          :desc "Subtree or File to Md to file & open" "O" '(lambda ()
-                                                              (interactive)
-                                                              (org-open-file (org-hugo-export-wim-to-md)))
-          :desc "File to Md file & open"               "o" '(lambda ()
-                                                              (interactive)
-                                                              (org-open-file (org-hugo-export-to-md)))
-          :desc "All subtrees (or File) to Md file(s)" "A" '(lambda ()
-                                                              (interactive)
-                                                              (org-hugo-export-wim-to-md :all-subtrees))
+          :desc "Subtree or File to Md to file & open" "O" '(λ! (org-open-file (org-hugo-export-wim-to-md)))
+          :desc "File to Md file & open"               "o" '(λ! (org-open-file (org-hugo-export-to-md)))
+          :desc "All subtrees (or File) to Md file(s)" "A" '(λ! (org-hugo-export-wim-to-md :all-subtrees))
           :desc "File to a temporary Md buffer"        "t" #'org-hugo-export-as-md)))
 
 ;;; +hugo.el ends here
