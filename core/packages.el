aa087e3af28d1a9e6c774aa705d69f7dcf5edf74
Andrew Whatson <whatson@gmail.com>
2019-10-01 22:37:18 +1000

    Experimental fixes for undo-tree
    Increase `undo-limit`, `undo-strong-limit` and `undo-outer-limit` by a
    factor of ten, which should prevent emacs from prematurely truncating
    the undo history and corrupting the tree.
    
    Restore `undo-tree-auto-save-history` which was disabled to avoid
    exacerbating the undo history truncation.
    
    Change the `undo-tree` recipe to grab the latest version from the
    maintainer's repostiory instead of the outdated ELPA version.
    
    Restore `undo-tree-enable-undo-in-region` which should be fixed in the
    latest version of `undo-tree`.

--- core/packages.el
+++ core/packages.el
@@ -32,7 +32,10 @@
   ;; potato.
   :recipe (:host github :repo "hlissner/emacs-so-long"))
 (package! osx-clipboard :ignore (not IS-MAC))
-(package! undo-tree)
+(package! undo-tree
+  ;; NOTE The version of undo-tree published to ELPA is over 5 years old and
+  ;; missing some fixes. Just use the maintainer's repo directly.
+  :recipe (:host nil :repo "http://www.dr-qubit.org/git/undo-tree.git"))
 (package! ws-butler)
 (package! xclip :ignore (not IS-LINUX))
 
