4480ab00518bc4b6c3b3b4385b3d6509b06f6905
Lorenzo Giuliani <lorenzo@frenzart.com>
2019-10-03 21:32:07 +0200

    unquote interactive lambda macros

--- modules/lang/org/contrib/+hugo.el
+++ modules/lang/org/contrib/+hugo.el
@@ -10,9 +10,9 @@
           (:prefix ("H" . "hugo")
             :desc "Subtree or File to Md to file"        "H" #'org-hugo-export-wim-to-md
             :desc "File to Md file"                      "h" #'org-hugo-export-to-md
-            :desc "Subtree or File to Md to file & open" "O" '(λ! (org-open-file (org-hugo-export-wim-to-md)))
-            :desc "File to Md file & open"               "o" '(λ! (org-open-file (org-hugo-export-to-md)))
-            :desc "All subtrees (or File) to Md file(s)" "A" '(λ! (org-hugo-export-wim-to-md :all-subtrees))
+            :desc "Subtree or File to Md to file & open" "O" (λ! (org-open-file (org-hugo-export-wim-to-md)))
+            :desc "File to Md file & open"               "o" (λ! (org-open-file (org-hugo-export-to-md)))
+            :desc "All subtrees (or File) to Md file(s)" "A" (λ! (org-hugo-export-wim-to-md :all-subtrees))
             :desc "File to a temporary Md buffer"        "t" #'org-hugo-export-as-md))))
 
 ;;; +hugo.el ends here
