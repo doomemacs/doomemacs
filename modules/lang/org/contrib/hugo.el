423a38c758bbea0fe4984de743a4bb6cc683e0c9
Henrik Lissner <henrik@lissner.net>
2019-10-04 12:17:47 -0400

    lang/org: move contrib/+hugo -> contrib/hugo
    + Refill +hugo flag's description in the README
    + Lazy load ox-hugo localleader keybinds by package, rather than by
      keymap deferral

--- modules/lang/org/contrib/hugo.el
+++ modules/lang/org/contrib/hugo.el
@@ -4,7 +4,8 @@
 (use-package! ox-hugo
   :after ox
   :preface
-  (map! :map org-mode-map
+  (map! :after org
+        :map org-mode-map
         :localleader
         (:prefix "e"
           (:prefix ("H" . "hugo")
@@ -14,5 +15,3 @@
             :desc "File to Md file & open"               "o" (λ! (org-open-file (org-hugo-export-to-md)))
             :desc "All subtrees (or File) to Md file(s)" "A" (λ! (org-hugo-export-wim-to-md :all-subtrees))
             :desc "File to a temporary Md buffer"        "t" #'org-hugo-export-as-md))))
-
-;;; +hugo.el ends here
