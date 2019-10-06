05f458a75ba6befeacd754b7b5bdc760d10a4db0
Henrik Lissner <henrik@lissner.net>
2019-10-06 02:28:44 -0400

    Disable benchmark in minibuffer if dashboard is enabled
    Redundant; also, printing to minibuffer during startup is somewhat
    expensive.

--- modules/ui/doom-dashboard/config.el
+++ modules/ui/doom-dashboard/config.el
@@ -129,6 +129,7 @@ PLIST can have the following properties:
     (add-hook 'persp-before-switch-functions #'+doom-dashboard--persp-record-project-h)))
 
 (add-hook 'doom-init-ui-hook #'+doom-dashboard-init-h)
+(remove-hook 'window-setup-hook #'doom-display-benchmark-h)
 
 
 ;;
