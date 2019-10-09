94ce52a70d2c8b0c8dea05f4e5458a26a696164a
Henrik Lissner <henrik@lissner.net>
2019-10-08 22:00:30 -0400

    Fix #1772: void-variable sly-contribs errors

--- modules/lang/common-lisp/autoload/common-lisp.el
+++ modules/lang/common-lisp/autoload/common-lisp.el
@@ -1,5 +1,9 @@
 ;;; lang/common-lisp/autoload/common-lisp.el -*- lexical-binding: t; -*-
 
+;; HACK Fix #1772: void-variable sly-contribs errors due to sly packages (like
+;; `sly-macrostep') trying to add to `sly-contribs' before it is defined.
+;;;###autoload (defvar sly-contribs '(sly-fancy))
+
 ;;;###autoload
 (defun +common-lisp--sly-last-sexp-a (command &rest args)
   "In normal-state or motion-state, last sexp ends at point."
