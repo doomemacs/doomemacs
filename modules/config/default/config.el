968618bbbff218201fa959884d09feb9c41eedba
Henrik Lissner <henrik@lissner.net>
2019-10-12 23:46:40 -0400

    Bind SPC h A and C-h A to apropos-documentation

--- modules/config/default/config.el
+++ modules/config/default/config.el
@@ -317,6 +317,7 @@
 
   ;; replaces `apropos-command'
   "a"    #'apropos
+  "A"    #'apropos-documentation
   ;; replaces `describe-copying' b/c not useful
   "C-c"  #'describe-coding-system
   ;; replaces `Info-got-emacs-command-node' b/c redundant w/ `Info-goto-node'
