8407af98fd5f8941be0d7097476c4e91cf5ba87c
Henrik Lissner <henrik@lissner.net>
2019-10-17 02:39:05 -0400

    tools/magit: disable magit-todos by default
    This plugin is a source of slowness and occasional bugs. Instead of
    enabling it by default, we keep it around for magit-todos-list, which
    could replace +ivy/tasks.

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -117,8 +117,7 @@ ensure it is built when we actually use Forge."
   (define-key magit-todos-section-map "j" nil)
   ;; Warns that jT isn't bound. Well, yeah, you don't need to tell me, that was
   ;; on purpose ya goose.
-  (advice-add #'magit-todos-mode :around #'doom-shut-up-a)
-  (magit-todos-mode +1))
+  (advice-add #'magit-todos-mode :around #'doom-shut-up-a))
 
 
 (use-package! magit-gitflow
