06da7fc20b32886634cb480564bb94fa3fd7b5f8
Henrik Lissner <henrik@lissner.net>
2019-10-19 14:37:08 -0400

    lang/nim: remove nimsuggest--get-dirty-dir hack
    Fixed upstream in nim-lang/nim-mode/commit/c8b50840

--- modules/lang/nim/config.el
+++ modules/lang/nim/config.el
@@ -12,18 +12,6 @@ nimsuggest isn't installed."
 
   (when IS-WINDOWS
     ;; TODO File PR/report upstream (https://github.com/nim-lang/nim-mode)
-    (defadvice! +nim--suggest-get-dirty-dir-a ()
-      "The original `nimsuggest--get-dirty-dir' incorrectly extracts the frame
-number from the string representation of `selected-frame', which can contain
-characters that are illegal on Windows, causing invalid argument errors when
-`nimsuggest--make-tempdir' tries to use it."
-      :override #'nimsuggest--get-dirty-dir
-      (let* ((frame-str (format "%s" (selected-frame)))
-             (frame-num-str (if (string-match " \\(0x[0-9a-z]+\\)>$" frame-str)
-                                (match-string 1 frame-str))))
-        (file-name-as-directory (concat nimsuggest-dirty-directory frame-num-str))))
-
-    ;; TODO File PR/report upstream (https://github.com/nim-lang/nim-mode)
     (defadvice! +nim--suggest-get-temp-file-name-a (path)
       "Removes invalid characters from the temp file path, including the unicode
 character that colon is replaced with, which is known to cause issues on
