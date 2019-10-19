92a5b5fb93c8f1fec43e2f8da9c2f02676d37458
Henrik Lissner <henrik@lissner.net>
2019-10-19 17:13:21 -0400

    lang/data: use conf-toml-mode instead, if available

--- modules/lang/data/packages.el
+++ modules/lang/data/packages.el
@@ -4,8 +4,11 @@
 (package! graphql-mode)
 (package! json-mode)
 (package! jsonnet-mode)
-(package! toml-mode)
 (package! yaml-mode)
 (package! csv-mode)
 (package! dhall-mode)
 (package! protobuf-mode :recipe (:host github :repo "emacsmirror/protobuf-mode" :files (:defaults "*")))
+
+;; DEPRECATED `conf-toml-mode' exists in Emacs 26+
+(unless (fboundp 'conf-toml-mode)
+  (package! toml-mode))
