5f9a630c2f68e779b5d30db13bc8f9b47e2847e9
Henrik Lissner <henrik@lissner.net>
2019-10-21 16:23:44 -0400

    term/term: fix void-function tramp-tramp-file-p error
    multi-term uses tramp functions before tramp is loaded.
    
    Remove this once manateelazycat/multi-term#2 is merged.

--- modules/term/term/config.el
+++ modules/term/term/config.el
@@ -1,9 +1,15 @@
 ;;; term/term/config.el -*- lexical-binding: t; -*-
 
+;;;###package term
+(add-hook 'term-mode-hook #'doom-mark-buffer-as-real-h)
+(add-hook 'term-mode-hook #'hide-mode-line-mode)
+
+
 ;;;###package multi-term
 (setq multi-term-dedicated-window-height 20
       multi-term-switch-after-close 'PREVIOUS)
 
-;;;###package term
-(add-hook 'term-mode-hook #'doom-mark-buffer-as-real-h)
-(add-hook 'term-mode-hook #'hide-mode-line-mode)
+;; REVIEW Fixes 'multi-term-switch-buffer: Symbol’s function definition is void:
+;;        tramp-tramp-file-p' error. Remove this once
+;;        manateelazycat/multi-term#2 is merged.
+(after! multi-term (require 'tramp))
