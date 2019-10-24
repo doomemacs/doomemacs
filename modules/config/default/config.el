c649401a92b2d80831821f49df5073c076692c64
Henrik Lissner <henrik@lissner.net>
2019-10-24 19:02:16 -0400

    Fix s-/ for non-evil users

--- modules/config/default/config.el
+++ modules/config/default/config.el
@@ -263,8 +263,9 @@
         "s--" #'doom/decrease-font-size
         ;; Conventional text-editing keys & motions
         "s-a" #'mark-whole-buffer
-        :gn "s-/" #'evilnc-comment-or-uncomment-lines
-        :v  "s-/" #'evilnc-comment-operator
+        "s-/" (λ! (save-excursion (comment-line 1)))
+        :n "s-/" #'evilnc-comment-or-uncomment-lines
+        :v "s-/" #'evilnc-comment-operator
         :gi  [s-backspace] #'doom/backward-kill-to-bol-and-indent
         :gi  [s-left]      #'doom/backward-to-bol-or-indent
         :gi  [s-right]     #'doom/forward-to-last-non-comment-or-eol
