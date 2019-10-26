d2fe021bd9c0ec4bba64e011813ed12d2ca45cb9
Henrik Lissner <henrik@lissner.net>
2019-10-26 03:54:09 -0400

    tools/eval: display in minibuffer if below threshold
    If it's just a couple lines, display in minibuffer instead.

--- modules/tools/eval/autoload/eval.el
+++ modules/tools/eval/autoload/eval.el
@@ -3,20 +3,25 @@
 ;;;###autoload
 (defun +eval-display-results-in-popup (output &optional source-buffer)
   "Display OUTPUT in a popup buffer."
-  (let ((output-buffer (get-buffer-create "*doom eval*"))
-        (origin (selected-window)))
-    (with-current-buffer output-buffer
-      (setq-local scroll-margin 0)
-      (erase-buffer)
-      (insert output)
-      (goto-char (point-min))
-      (if (fboundp '+word-wrap-mode)
-          (+word-wrap-mode +1)
-        (visual-line-mode +1)))
-    (when-let (win (display-buffer output-buffer))
-      (fit-window-to-buffer win))
-    (select-window origin)
-    output-buffer))
+  (if (with-temp-buffer
+        (insert output)
+        (>= (count-lines (point-min) (point-max))
+            +eval-overlay-max-lines))
+      (let ((output-buffer (get-buffer-create "*doom eval*"))
+            (origin (selected-window)))
+        (with-current-buffer output-buffer
+          (setq-local scroll-margin 0)
+          (erase-buffer)
+          (insert output)
+          (goto-char (point-min))
+          (if (fboundp '+word-wrap-mode)
+              (+word-wrap-mode +1)
+            (visual-line-mode +1)))
+        (when-let (win (display-buffer output-buffer))
+          (fit-window-to-buffer win))
+        (select-window origin)
+        output-buffer)
+    (message "%s" output)))
 
 ;;;###autoload
 (defun +eval-display-results-in-overlay (output &optional source-buffer)
