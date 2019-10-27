b06b6b5dbac30b8935f1e0db01ecb50eb4eaa20a
Henrik Lissner <henrik@lissner.net>
2019-10-27 13:29:40 -0400

    config/default: bind 'SPC / {j,m}'
    To 'jump to position in jump list' and 'jump to mark', respectively

--- modules/config/default/+evil-bindings.el
+++ modules/config/default/+evil-bindings.el
@@ -284,6 +284,8 @@
         :desc "Locate file"                   "f" #'locate
         :desc "Jump to symbol"                "i" #'imenu
         :desc "Jump to link"                  "l" #'ace-link
+        :desc "jump list"                     "j" #'evil-show-jumps
+        :desc "Jump to mark"                  "m"   #'evil-show-marks
         :desc "Look up online"                "o" #'+lookup/online
         :desc "Look up online (w/ prompt)"    "O" #'+lookup/online-select
         :desc "Look up in local docsets"      "k" #'+lookup/in-docsets
