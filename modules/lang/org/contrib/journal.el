843be4d2a62cc5cbf12e761e07e10201cff6b595
Henrik Lissner <henrik@lissner.net>
2019-10-31 23:09:43 -0400

    lang/org: refactor org-journal integration

--- modules/lang/org/contrib/journal.el
+++ modules/lang/org/contrib/journal.el
@@ -1,11 +1,17 @@
 ;;; lang/org/contrib/journal.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! +journal)
 
-(map! :after org-journal
-      :localleader
-      (:map org-journal-search-mode-map
-        "n" #'org-journal-search-next
-        "p" #'org-journal-search-prev)
-      (:map org-journal-mode-map
-        "n" #'org-journal-open-next-entry
-        "p" #'org-journal-open-previous-entry))
+(after! org-journal
+  (setq org-journal-dir (expand-file-name "journal/" org-directory)
+        org-journal-file-pattern
+        (expand-file-name "\\(?1:[0-9]\\{4\\}\\)\\(?2:[0-9][0-9]\\)\\(?3:[0-9][0-9]\\)\\'"
+                          org-journal-dir))
+
+  (map! :map org-mode-map
+        :localleader
+        (:map org-journal-search-mode-map
+          "n" #'org-journal-search-next
+          "p" #'org-journal-search-prev)
+        (:map org-journal-mode-map
+          "n" #'org-journal-open-next-entry
+          "p" #'org-journal-open-previous-entry))
