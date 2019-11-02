0d7d107bbf7ddd43d74162287af031edebff5109
Max Nickel <maxn@fb.com>
2019-11-02 14:29:51 -0400

    remove unnecessary dotfile functions and hooks

--- modules/emacs/dired/autoload.el
+++ modules/emacs/dired/autoload.el
@@ -14,18 +14,3 @@
        (not (file-remote-p default-directory))
        (locate-dominating-file "." ".git"))
       (dired-git-info-mode 1)))
-
-;;;###autoload
-(defun +dired-dotfiles-hide ()
-  (set (make-local-variable '+dired-dotfiles-show-p) nil)
-  (dired-mark-files-regexp "^\\\.")
-  (dired-do-kill-lines))
-
-;;;###autoload
-(defun +dired/dotfiles-toggle ()
-  (interactive)
-  (when (equal major-mode 'dired-mode)
-    (if (or (not (boundp '+dired-dotfiles-show-p)) +dired-dotfiles-show-p) ; if currently showing
-        (+dired-dotfiles-hide)
-      (progn (revert-buffer) ; otherwise just revert to re-show
-             (set (make-local-variable '+dired-dotfiles-show-p) t)))))
