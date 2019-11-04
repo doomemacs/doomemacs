684e69c9681d2919cbc7bcb62fc70c02717d36d4
Henrik Lissner <henrik@lissner.net>
2019-11-04 03:32:45 -0500

    lang/julia: ensure inferior-julia-program-name is defined
    For ob-julia

--- modules/lang/julia/autoload.el
+++ modules/lang/julia/autoload.el
@@ -1,5 +1,10 @@
 ;;; lang/julia/autoload.el -*- lexical-binding: t; -*-
 
+;; `ob-julia' needs this variable to be defined, but it's defined in
+;; `ess-custom', which won't be available if you're using :lang julia and not
+;; :lang ess.
+;;;###autoload (defvar inferior-julia-program-name (or (executable-find "julia-basic") "julia"))
+
 ;;;###autoload
 (defun +julia/open-repl ()
   "Run an inferior instance of `julia' inside Emacs."
