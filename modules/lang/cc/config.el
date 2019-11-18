caed1f2d1ac7fcbf479f196129918328167a0a6d
Andrew Whatson <whatson@gmail.com>
2019-11-19 09:58:44 +1000

    Use CMake docset for cmake-mode

--- modules/lang/cc/config.el
+++ modules/lang/cc/config.el
@@ -160,6 +160,10 @@ This is ignored by ccls.")
 ;;
 ;; Major modes
 
+(use-package! cmake-mode
+  :defer t
+  :config (set-docsets! 'cmake-mode "CMake"))
+
 (use-package! company-cmake  ; for `cmake-mode'
   :when (featurep! :completion company)
   :after cmake-mode
