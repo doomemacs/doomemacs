c7a6cf8403147348a4e7164110fc6aace0a985a3
Henrik Lissner <henrik@lissner.net>
2019-12-01 05:13:10 -0500

    lang/haskell: change default to +dante #2121

--- init.example.el
+++ init.example.el
@@ -112,7 +112,7 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        ;;go                ; the hipster dialect
-       ;;(haskell +intero) ; a language that's lazier than I am
+       ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
