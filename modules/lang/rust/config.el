9775939f9d8f1fe26cc5fd807cfcfe83ef1939c2
Henrik Lissner <henrik@lissner.net>
2019-12-02 16:34:11 -0500

    lang/rust: fix #2132
    Prevents rust-mode from taking over *.rs files.

--- modules/lang/rust/config.el
+++ modules/lang/rust/config.el
@@ -56,6 +56,12 @@
 (use-package! racer
   :unless (featurep! +lsp)
   :hook (rustic-mode . racer-mode)
+  :init
+  ;; Fix #2132: `racer' depends on `rust-mode', which tries to modify
+  ;; `auto-mode-alist'. We make extra sure that doesn't stick, especially when a
+  ;; buffer is reverted, as it is after rustfmt is done wiht it.
+  (after! rust-mode
+    (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode)) auto-mode-alist))
   :config
   (set-lookup-handlers! 'rustic-mode
     :definition '(racer-find-definition :async t)
