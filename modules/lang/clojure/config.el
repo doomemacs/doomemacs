3c90c6994b7b1fad0382b8f349cb1726635998a9
Henrik Lissner <henrik@lissner.net>
2019-12-02 20:20:07 -0500

    lang/clojure: set large-file-size threshold to 0.5mb

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -3,6 +3,10 @@
 (after! projectile
   (pushnew! projectile-project-root-files "project.clj" "build.boot" "deps.edn"))
 
+;; Large clojure buffers tend to be slower than large buffers of other modes, so
+;; it should have a lower threshold too.
+(add-to-list 'doom-large-file-size-alist '(clojure-mode . 0.5))
+
 
 ;;
 ;;; Packages
