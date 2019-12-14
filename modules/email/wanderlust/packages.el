b8d4fef93f50de028b55ec29bcd11e5f52ff408d
Henrik Lissner <henrik@lissner.net>
2019-12-13 19:55:45 -0500

    email/wanderlust: add flim/apel packages
    See:
    raxod502/straight.el#445
    wanderlust/wanderlust#172

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -1,4 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/wanderlust/packages.el
 
+;; HACK These are wanderlust's dependencies (wanderlust depends on semi, semi
+;;      depends on flim, flim on apel), but both flim and apel have non-standard
+;;      default branches, which straight cannot detect without our help.
+(package! flim :recipe (:branch "flim-1_14-wl"))
+(package! apel :recipe (:branch "semi-1_14-wl"))
+
 (package! wanderlust)
