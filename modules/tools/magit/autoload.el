48cd3e0ad03faa903ca6e175857a054aec494097
Henrik Lissner <henrik@lissner.net>
2019-12-15 23:40:48 -0500

    tools/magit: prevent 'cannot determine magit's version' warnings
    By ensuring the advice runs twice.

--- modules/tools/magit/autoload.el
+++ modules/tools/magit/autoload.el
@@ -3,7 +3,8 @@
 ;; HACK Magit complains loudly when it can't determine its own version, which is
 ;;      the case when magit is built through straight. The warning is harmless,
 ;;      however, so we just need it to shut up.
-;;;###autoload (advice-add #'magit-version :override #'ignore)
+;;;###autoload
+(advice-add #'magit-version :override #'ignore)
 
 ;;;###autoload
 (defun +magit-display-buffer-fn (buffer)
