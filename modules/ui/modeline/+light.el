9c73bff4f0ef454b228be42b1fcec8f5967f8045
Henrik Lissner <henrik@lissner.net>
2019-12-17 15:37:33 -0500

    ui/modeline: unset other format hooks
    If set-modeline-hook! is used after init.

--- modules/ui/modeline/+light.el
+++ modules/ui/modeline/+light.el
@@ -144,7 +144,10 @@ If DEFAULT is non-nil, apply to all future buffers. Modelines are defined with
 See `def-modeline!' on how modelines are defined."
   (let ((fn (intern (format "+modeline-set-%s-format-h" name))))
     (dolist (hook (doom-enlist hooks))
-      (add-hook hook fn 'append))))
+      (when after-init-time
+        (dolist (name (mapcar #'car +modeline-format-alist))
+          (remove-hook hook (intern (format "+modeline-set-%s-format-h" name)))))
+      (add-hook hook fn))))
 
 (defmacro def-modeline! (name lhs rhs)
   "Define a modeline format by NAME.
