56ccb0dbef437ab4f425d735e2ae51a37805e3ba
Henrik Lissner <henrik@lissner.net>
2019-12-22 00:02:26 -0500

    lang/org: load jupyter.el & add jupyter-* to org-src-lang-modes #2198

--- modules/lang/org/contrib/jupyter.el
+++ modules/lang/org/contrib/jupyter.el
@@ -7,13 +7,21 @@
   (after! ob-async
     (pushnew! ob-async-no-async-languages-alist "jupyter-python" "jupyter-julia"))
 
+  (after! org-src
+    (dolist (lang '(python julia R))
+      (cl-pushnew (cons (format "jupyter-%s" lang) lang)
+                  org-src-lang-modes :key #'car)))
+
   (add-hook! '+org-babel-load-functions
     (defun +org-babel-load-jupyter-h (lang)
       (when (string-prefix-p "jupyter-" (symbol-name lang))
-        (let ((lang (string-remove-prefix "jupyter-" (symbol-name lang))))
-          (unless (assoc lang org-src-lang-modes)
-            (require (intern (format "ob-%s" lang))
-                     nil t)))
+        (require 'jupyter)
+        (let* ((lang-name (symbol-name lang))
+               (lang-tail (string-remove-prefix "jupyter-" lang-name)))
+          (and (not (assoc lang-tail org-src-lang-modes))
+               (require (intern (format "ob-%s" lang-tail))
+                        nil t)
+               (add-to-list 'org-src-lang-modes (cons lang-name (intern lang-tail)))))
         (with-demoted-errors "Jupyter: %s"
           (require lang nil t)
           (require 'ob-jupyter nil t))))))
