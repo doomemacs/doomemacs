d0188b827a16c70ed669a7e6d1e64c34aa92dfa8
Henrik Lissner <henrik@lissner.net>
2019-12-22 12:26:50 -0500

    editor/evil: set evil-respect-visual-line-mode sooner
    So users are able to change it in time in their init.el

--- modules/editor/evil/config.el
+++ modules/editor/evil/config.el
@@ -27,6 +27,7 @@ directives. By default, this only recognizes C directives.")
 (defvar evil-want-C-w-delete t)
 (defvar evil-want-Y-yank-to-eol t)
 (defvar evil-want-abbrev-expand-on-insert-exit nil)
+(defvar evil-respect-visual-line-mode t)
 
 (use-package! evil
   :hook (doom-init-modules . evil-mode)
@@ -41,7 +42,6 @@ directives. By default, this only recognizes C directives.")
         evil-ex-visual-char-range t  ; column range for ex commands
         evil-insert-skip-empty-lines t
         evil-mode-line-format 'nil
-        evil-respect-visual-line-mode t
         ;; more vim-like behavior
         evil-symbol-word-search t
         ;; cursor appearance
