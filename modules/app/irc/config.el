da78751ce4d2ab3c10c79a0d23b28e2697f55f9b
Sean Farley <sean@farley.io>
2019-12-23 12:08:13 -0800

    irc: turn off smartparens mode for circe buffers
    Users probably don't want ":-(" to be autocompleted as ":-()" so let's
    turn of smartparens for chatting.

--- modules/app/irc/config.el
+++ modules/app/irc/config.el
@@ -93,6 +93,7 @@ playback.")
   (add-hook 'doom-real-buffer-functions #'+circe-buffer-p)
   (add-hook 'circe-channel-mode-hook #'turn-on-visual-line-mode)
   (add-hook 'circe-mode-hook #'+irc--add-circe-buffer-to-persp-h)
+  (add-hook 'circe-mode-hook #'turn-off-smartparens-mode)
 
   (defadvice! +irc--circe-run-disconnect-hook-a (&rest _)
     "Runs `+irc-disconnect-hook' after circe disconnects."
