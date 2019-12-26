1c7ba9d2cb712971148fa97a54ebd45726f522b6
Roey Darwish Dror <roey.ghost@gmail.com>
2019-12-26 14:31:51 +0200

    Set the directory for the Java LSP workspace

--- modules/lang/java/+lsp.el
+++ modules/lang/java/+lsp.el
@@ -5,7 +5,8 @@
   :after-call java-mode
   :init
   (add-hook 'java-mode-local-vars-hook #'lsp!)
-  (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/"))
+  (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/")
+        lsp-java-workspace-dir (concat doom-etc-dir "java-workspace"))
   :config
   ;; TODO keybinds
   ;; TODO treemacs integration (?)
