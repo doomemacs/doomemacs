9012635d3fc51e4142dd2eaae647656f68af82f2
Henrik Lissner <henrik@lissner.net>
2019-12-31 02:27:31 -0500

    Remove default doom-variable-pitch-font in config.el template
    "sans" may not be available.

--- core/templates/config.example.el
+++ core/templates/config.example.el
@@ -18,9 +18,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; test
-(setq doom-font (font-spec :family "monospace" :size 14)
-      doom-variable-pitch-font (font-spec :family "sans"))
+(setq doom-font (font-spec :family "monospace" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
