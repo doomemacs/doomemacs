d1b597fde462e633d4f106ce303f628b5010925d
Henrik Lissner <henrik@lissner.net>
2019-12-31 19:14:43 -0500

    Remove redundant evil keybinds for neotree
    Some of these were already bound by evil-collection-neotree.

--- modules/ui/neotree/config.el
+++ modules/ui/neotree/config.el
@@ -54,28 +54,19 @@
     (skip-chars-forward " \t\r"))
 
   (map! :map neotree-mode-map
-        :n "g"      nil
-        :n "TAB"    #'neotree-quick-look
-        :n "RET"    #'neotree-enter
-        :n [tab]    #'neotree-quick-look
-        :n [return] #'neotree-enter
+        :n [tab]    (neotree-make-executor
+                     :dir-fn  #'neo-open-dir
+                     :file-fn #'neotree-quick-look)
         :n "DEL"    #'evil-window-prev
-        :n "c"      #'neotree-create-node
-        :n "r"      #'neotree-rename-node
-        :n "d"      #'neotree-delete-node
-        :n "j"      #'neotree-next-line
-        :n "k"      #'neotree-previous-line
         :n "n"      #'neotree-next-line
         :n "p"      #'neotree-previous-line
-        :n "h"      #'+neotree/collapse-or-up
-        :n "l"      #'+neotree/expand-or-open
+        :m "h"      #'+neotree/collapse-or-up
+        :m "l"      #'+neotree/expand-or-open
         :n "J"      #'neotree-select-next-sibling-node
         :n "K"      #'neotree-select-previous-sibling-node
         :n "H"      #'neotree-select-up-node
         :n "L"      #'neotree-select-down-node
         :n "G"      #'evil-goto-line
         :n "gg"     #'evil-goto-first-line
-        :n "v"      #'neotree-enter-vertical-split
-        :n "s"      #'neotree-enter-horizontal-split
-        :n "q"      #'neotree-hide
-        :n "R"      #'neotree-refresh))
+        :n "v"      (neotree-make-executor :file-fn 'neo-open-file-vertical-split)
+        :n "s"      (neotree-make-executor :file-fn 'neo-open-file-horizontal-split)))
