a9763fe76be4a5bd0f50e6c7d83c24d213ffcb93
Henrik Lissner <henrik@lissner.net>
2020-01-02 16:22:52 -0500

    Fix vestigial reference to +org-attach/uri

--- modules/lang/org/autoload/contrib-dragndrop.el
+++ modules/lang/org/autoload/contrib-dragndrop.el
@@ -5,7 +5,7 @@
 (defun +org-dragndrop-download-dnd-fn (uri action)
   "Handle file links and base64 data uris."
   (if (eq major-mode 'org-mode)
-      (+org-attach/uri uri)
+      (+org/attach-file-and-insert-link uri)
     (let ((dnd-protocol-alist
            (rassq-delete-all '+org-attach-download-dnd
                              (copy-alist dnd-protocol-alist))))
