7f654f156614137ed5cca23907229e459969acfd
Henrik Lissner <henrik@lissner.net>
2020-01-02 21:09:37 -0500

    Bind SPC s {t,T} for dictionary/thesaurus lookups

--- modules/config/default/+evil-bindings.el
+++ modules/config/default/+evil-bindings.el
@@ -598,7 +598,9 @@
         :desc "Search other project"         "P" #'+default/search-other-project
         :desc "Jump to mark"                 "r" #'evil-show-marks
         :desc "Search buffer"                "s" #'swiper-isearch
-        :desc "Search buffer for thing at point" "S" #'swiper-isearch-thing-at-point)
+        :desc "Search buffer for thing at point" "S" #'swiper-isearch-thing-at-point
+        :desc "Dictionary"                   "t" #'+lookup/dictionary-definition
+        :desc "Thesaurus"                    "T" #'+lookup/synonyms)
 
       ;;; <leader> t --- toggle
       (:prefix-map ("t" . "toggle")
