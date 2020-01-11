20bc70096fa28c773ea0672ae3bfca26db61887b
Henrik Lissner <henrik@lissner.net>
2020-01-10 22:58:10 -0500

    Fix deprecation warnings for fly{check,spell} modules

--- core/core-modules.el
+++ core/core-modules.el
@@ -13,8 +13,8 @@
 
 (defconst doom-obsolete-modules
   '((:feature (version-control (:emacs vc) (:ui vc-gutter))
-              (spellcheck (:tools flyspell))
-              (syntax-checker (:tools flycheck))
+              (spellcheck (:checkers spell))
+              (syntax-checker (:checkers syntax))
               (evil (:editor evil))
               (snippets (:editor snippets))
               (file-templates (:editor file-templates))
@@ -25,8 +25,8 @@
     (:tools (rotate-text (:editor rotate-text))
             (vterm (:term vterm))
             (password-store (:tools pass))
-            (flycheck (:checker syntax))
-            (flyspell (:checker spell)))
+            (flycheck (:checkers syntax))
+            (flyspell (:checkers spell)))
     (:emacs (electric-indent (:emacs electric))
             (hideshow (:editor fold))
             (eshell (:term eshell))
