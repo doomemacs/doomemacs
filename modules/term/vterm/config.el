520fefbd0fefa0ba6abd0f3a3de218a72de42f54
Henrik Lissner <henrik@lissner.net>
2020-01-12 20:05:26 -0500

    Prevent premature h-scrolling in vterm buffers

--- modules/term/vterm/config.el
+++ modules/term/vterm/config.el
@@ -7,12 +7,16 @@
   :config
   (set-popup-rule! "^vterm" :size 0.25 :vslot -4 :select t :quit nil :ttl 0)
 
+  (setq-hook! 'vterm-mode-hook
+    ;; Don't prompt about processes when killing vterm
+    confirm-kill-processes nil
+    ;; Prevent premature horizontal scrolling
+    hscroll-margin 0)
+
   (add-hook 'vterm-mode-hook #'doom-mark-buffer-as-real-h)
   ;; Automatically kill buffer when vterm exits.
   (add-hook! 'vterm-exit-functions
     (defun +vterm-kill-buffer-on-quit-fn (buffer event)
       (if buffer (kill-buffer buffer))))
   ;; Modeline serves no purpose in vterm
-  (add-hook 'vterm-mode-hook #'hide-mode-line-mode)
-  ;; Don't prompt about processes when killing vterm
-  (setq-hook! 'vterm-mode-hook confirm-kill-processes nil))
+  (add-hook 'vterm-mode-hook #'hide-mode-line-mode))
