00e2f2cc6fbb8ed6c9e212434cf442d14ce43c6b
Dan LaManna <dan.lamanna@kitware.com>
2020-01-24 12:23:45 -0500

    lang/python: add additional checks

--- modules/lang/python/doctor.el
+++ modules/lang/python/doctor.el
@@ -20,6 +20,30 @@
   (unless (executable-find "conda")
     (warn! "Couldn't find conda in your PATH")))
 
+(when (featurep! +cython)
+  (unless (executable-find "cython")
+    (warn! "Couldn't find cython. cython-mode will not work.")))
+
 (when (featurep! +ipython)
   (unless (executable-find "ipython")
     (warn! "Couldn't find ipython in your PATH")))
+
+(if (and (featurep! :editor format)
+         (not (executable-find "black")))
+    (warn! "Couldn't find black. Code formatting will not work."))
+
+(if (not (executable-find "pytest"))
+    (warn! "Couldn't find pytest. Running tests through pytest will not work."))
+
+(if (not (executable-find "nosetests"))
+    (warn! "Couldn't find nosetests. Running tests through nose will not work."))
+
+(if (not (executable-find "pipenv"))
+    (warn! "Couldn't find pipenv. pipenv support will not work."))
+
+(if (not (executable-find "isort"))
+    (warn! "Couldn't find isort. Import sorting will not work."))
+
+(if (and (featurep! :editor format)
+         (not (executable-find "pyflakes")))
+    (warn! "Couldn't find pyflakes. Import management will not work."))
