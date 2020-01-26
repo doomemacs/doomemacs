1666ab3ddc5f83bf5b30f3ddcfd3974ea539653d
Henrik Lissner <henrik@lissner.net>
2020-01-26 17:37:33 -0500

    Fix #2398: recursive load in wanderlust
    Its dependencies weren't installed in the correct order (and a super old
    version of semi was installed because it didn't have an explicit package
    declaration).

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -2,9 +2,10 @@
 ;;; app/wanderlust/packages.el
 
 ;; HACK These are wanderlust's dependencies (wanderlust depends on semi, semi
-;;      depends on flim, flim on apel), but both flim and apel have non-standard
-;;      default branches, which straight cannot detect without our help.
-(package! flim :recipe (:branch "flim-1_14-wl") :pin "e4bd54fd7d")
+;;      depends on flim, flim on apel), but they all have non-standard default
+;;      branches which straight cannot detect without our help.
 (package! apel :recipe (:branch "apel-wl") :pin "d146ddbf88")
+(package! flim :recipe (:branch "flim-1_14-wl") :pin "e4bd54fd7d")
+(package! semi :recipe (:branch "semi-1_14-wl") :pin "16228dc2d1")
 
 (package! wanderlust :pin "7a919e422a")
