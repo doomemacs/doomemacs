735a7a3ad54284b0d5d8d2c13fad9bf48bee06dd
Henrik Lissner <accounts@v0.io>
2020-01-29 12:33:54 -0500

    Use longer SHA

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rustic :pin "124e028")
+(package! rustic :pin "124e0286dc")
 (unless (featurep! +lsp)
   (package! racer :pin "a0bdf778f0"))
