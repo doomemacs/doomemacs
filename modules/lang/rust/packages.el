d13f18aed2d1d3d9e33be668c4b659bf29b94ee3
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-01-29 13:59:34 +0100

    Bump to brotzeit/rustic@124e028
    Rustic was broken because of brotzeit/rustic#88

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rustic :pin "c7cab3fbe6")
+(package! rustic :pin "124e028")
 (unless (featurep! +lsp)
   (package! racer :pin "a0bdf778f0"))
