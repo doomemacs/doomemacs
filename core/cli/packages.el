9a0e0291355bc39ff1d02f026ce666765e41d7f7
Henrik Lissner <henrik@lissner.net>
2020-01-30 18:37:48 -0500

    Fix output truncation & indentation on 'doom update'

--- core/cli/packages.el
+++ core/cli/packages.el
@@ -212,8 +212,9 @@ declaration) or dependency thereof that hasn't already been."
                (puthash local-repo t repos-to-rebuild)
                (puthash package t packages-to-rebuild)
                (unless (string-empty-p output)
-                 (print-group! (print! (info "%s" output))))
-               (print! (success "(%d/%d) %s updated (%s -> %s)")
+                 (print! (start "\033[K(%d/%d) Updating %s...") i total local-repo)
+                 (print-group! (print! "%s" output)))
+               (print! (success "\033[K(%d/%d) %s updated (%s -> %s)")
                        i total local-repo
                        (doom--abbrev-commit ref)
                        (doom--abbrev-commit target-ref)))
