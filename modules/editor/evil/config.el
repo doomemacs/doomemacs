c733a6aa27a61763b65a4b868d0851fc5a361e47
Henrik Lissner <henrik@lissner.net>
2020-01-31 16:21:23 -0500

    evil-respect-visual-line-mode = nil
    Since this is vim's default behavior (gj and gk exist for navigating
    virtual lines). Also the cause of #2447

--- modules/editor/evil/config.el
+++ modules/editor/evil/config.el
@@ -27,7 +27,6 @@ directives. By default, this only recognizes C directives.")
 (defvar evil-want-C-w-delete t)
 (defvar evil-want-Y-yank-to-eol t)
 (defvar evil-want-abbrev-expand-on-insert-exit nil)
-(defvar evil-respect-visual-line-mode t)
 
 (use-package! evil
   :hook (doom-init-modules . evil-mode)
