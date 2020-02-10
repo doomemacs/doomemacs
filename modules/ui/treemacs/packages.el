ae1e3f005e5a2880b59bb3ae2c02e1c4492a265f
Henrik Lissner <henrik@lissner.net>
2020-02-10 02:38:12 -0500

    Bump to Alexander-Miller/treemacs@05d264ae8c
    Also affects treemacs-{evil,projectile,magit,persp}, since they are in
    the same repo.

--- modules/ui/treemacs/packages.el
+++ modules/ui/treemacs/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/treemacs/packages.el
 
-(package! treemacs :pin "f830f209dd")
+(package! treemacs :pin "05d264ae8c")
 (when (featurep! :editor evil +everywhere)
-  (package! treemacs-evil :pin "f830f209dd"))
-(package! treemacs-projectile :pin "f830f209dd")
+  (package! treemacs-evil :pin "05d264ae8c"))
+(package! treemacs-projectile :pin "05d264ae8c")
 (when (featurep! :tools magit)
-  (package! treemacs-magit :pin "f830f209dd"))
+  (package! treemacs-magit :pin "05d264ae8c"))
 (when (featurep! :ui workspaces)
-  (package! treemacs-persp :pin "f830f209dd"))
+  (package! treemacs-persp :pin "05d264ae8c"))
