1446f1a37c0d307927bb0b81aea73c774452df6a
Henrik Lissner <henrik@lissner.net>
2020-02-11 03:12:40 -0500

    Bump to Alexander-Miller/treemacs@4d4a955
    From Alexander-Miller/treemacs@05d264a
    
    Due to hlissner/emacs-doom-themes#395

--- modules/ui/treemacs/packages.el
+++ modules/ui/treemacs/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/treemacs/packages.el
 
-(package! treemacs :pin "05d264ae8c")
+(package! treemacs :pin "4d4a955fcb")
 (when (featurep! :editor evil +everywhere)
-  (package! treemacs-evil :pin "05d264ae8c"))
-(package! treemacs-projectile :pin "05d264ae8c")
+  (package! treemacs-evil :pin "4d4a955fcb"))
+(package! treemacs-projectile :pin "4d4a955fcb")
 (when (featurep! :tools magit)
-  (package! treemacs-magit :pin "05d264ae8c"))
+  (package! treemacs-magit :pin "4d4a955fcb"))
 (when (featurep! :ui workspaces)
-  (package! treemacs-persp :pin "05d264ae8c"))
+  (package! treemacs-persp :pin "4d4a955fcb"))
