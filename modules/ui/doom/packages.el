e8813ffa9055f7ab29adc6df23046845af4918c3
Henrik Lissner <henrik@lissner.net>
2020-02-11 03:22:49 -0500

    Bump to hlissner/emacs-doom-themes@de96a55
    From hlissner/emacs-doom-themes@7131b60

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "7131b60161")
+(package! doom-themes :pin "de96a5558d")
 (package! solaire-mode :pin "4ac324ccb0")
