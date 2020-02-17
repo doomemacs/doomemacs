aefb0672b4f41ae284dad753f86143c6f35fcad6
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-02-17 10:12:43 -0500

    Reformat configuration

--- modules/lang/dart/+flutter.el
+++
@@ -1,7 +0,0 @@
-;;; lang/dart/+flutter.el -*- lexical-binding: t; -*-
-
-(use-package! flutter
-  :config
-  (map! :map dart-mode-map
-        :localleader
-        "r" #'flutter-run-or-hot-reload))
--- modules/lang/dart/config.el
+++ modules/lang/dart/config.el
@@ -1,12 +1,18 @@
 ;;; lang/dart/config.el -*- lexical-binding: t; -*-
 
-(cond ((featurep! +flutter) (load! "+flutter"))
-      ((featurep! +lsp)    (load! "+lsp")))
-
 (use-package! dart-mode
+  :defer t
   :config
-  (when (featurep! +flutter)
-    (if IS-LINUX
-      (setq lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk/")))
+  (when (and (featurep! +flutter) IS-LINUX)
+    (setq lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk/"))
   (when (featurep! +lsp)
-    (add-hook 'dart-mode-hook 'lsp)))
+    (add-hook 'dart-mode-local-vars-hook #'lsp!)))
+
+
+(when (featurep! +flutter)
+  (use-package! flutter
+    :defer t
+    :config
+    (map! :map dart-mode-map
+          :localleader
+          "r" #'flutter-run-or-hot-reload)))
