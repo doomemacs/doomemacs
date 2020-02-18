e3d7b2662c3db8dbe0939e1a73923bf1ab2b0c28
Henrik Lissner <henrik@lissner.net>
2020-02-18 00:19:56 -0500

    Don't throw error if elfeed org file does not exist
    The user may not want an org file, so we shouldn't impose this error on
    them if one doesn't exist.

--- modules/app/rss/config.el
+++ modules/app/rss/config.el
@@ -66,7 +66,8 @@ easier to scroll through.")
   :init
   (setq rmh-elfeed-org-files (list "elfeed.org"))
   :config
-  (let ((default-directory org-directory))
-    (setq rmh-elfeed-org-files
-          (mapcar #'expand-file-name rmh-elfeed-org-files)))
-  (elfeed-org))
+  (and (let ((default-directory org-directory))
+         (setq rmh-elfeed-org-files
+               (cl-remove-if-not
+                #'file-exists-p (mapcar #'expand-file-name rmh-elfeed-org-files))))
+       (elfeed-org)))
