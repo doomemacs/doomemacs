ef055778e97a4d619bc4c425a7180dfd5da3fba3
Henrik Lissner <henrik@lissner.net>
2020-02-23 15:00:35 -0500

    Pin to mrkkrp/vimish-fold@d3248a4
    And add hideshow declaration (so `SPC h d p` knows where to find it).

--- modules/editor/fold/packages.el
+++ modules/editor/fold/packages.el
@@ -1,5 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/fold/packages.el
 
+(package! hideshow :built-in t)
+
+(package! vimish-fold :pin "d3248a41a7")
 (when (featurep! :editor evil)
   (package! evil-vimish-fold :pin "b6e0e6b91b"))
