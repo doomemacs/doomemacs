93936809099f79f227912aa61b0ff6cd1a217e90
Henrik Lissner <henrik@lissner.net>
2020-02-24 22:11:36 -0500

    Fix counsel-file-jump advice for windows users
    Was using the wrong arguments for windows version of ripgrep.

--- modules/completion/ivy/config.el
+++ modules/completion/ivy/config.el
@@ -267,7 +267,7 @@ evil-ex-specific constructs, so we disable it solely in evil-ex."
         (cond ((executable-find doom-projectile-fd-binary)
                (cons doom-projectile-fd-binary (list "-t" "f" "-E" ".git")))
               ((executable-find "rg")
-               (cons "rg" (list "--files" "--hidden" "--no-messages")))
+               (split-string (format counsel-rg-base-command "--files --no-messages") " " t))
               ((cons find-program args)))
       (unless (listp args)
         (user-error "`counsel-file-jump-args' is a list now, please customize accordingly."))
