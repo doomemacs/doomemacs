ccac04fbd44a734a1fdad27bf66e30c6ce12619c
Henrik Lissner <henrik@lissner.net>
2020-02-24 20:09:10 -0500

    Bump to seagle0128/doom-modeline@41015d7
    From seagle0128/doom-modeline@c4969ff

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "c4969ff149"))
+  (package! doom-modeline :pin "41015d72e7"))
 (package! anzu :pin "592f8ee6d0")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "9bca6ca14d"))
