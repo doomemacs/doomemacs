e46cd73f3d8be655679a03dda349a6939351bfb7
Henrik Lissner <henrik@lissner.net>
2020-02-29 12:34:28 -0500

    Bump :checkers spell
    d12frosted/flyspell-correct@b0353a4 -> d12frosted/flyspell-correct@e765d1a

--- modules/checkers/spell/packages.el
+++ modules/checkers/spell/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; checkers/spell/packages.el
 
-(package! flyspell-correct :pin "b0353a41a7")
+(package! flyspell-correct :pin "e765d1a3d9")
 (cond ((featurep! :completion ivy)
-       (package! flyspell-correct-ivy :pin "b0353a41a7"))
+       (package! flyspell-correct-ivy :pin "e765d1a3d9"))
       ((featurep! :completion helm)
-       (package! flyspell-correct-helm :pin "b0353a41a7"))
-      ((package! flyspell-correct-popup :pin "b0353a41a7")))
+       (package! flyspell-correct-helm :pin "e765d1a3d9"))
+      ((package! flyspell-correct-popup :pin "e765d1a3d9")))
 
 (package! flyspell-lazy :pin "3ebf68cc9e")
