a44df0b9c53f78e76b8d129eea4911370120cda0
Henrik Lissner <henrik@lissner.net>
2020-02-29 11:47:12 -0500

    Bump :completion helm
    emacs-helm/helm@8f56312 -> emacs-helm/helm@21e778b
    tumashu/posframe@087a7fc -> tumashu/posframe@8a9af54

--- modules/completion/helm/packages.el
+++ modules/completion/helm/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/helm/packages.el
 
-(package! helm :pin "8f56312053")
+(package! helm :pin "21e778bc88")
 (package! helm-rg :pin "785a80fe5c")
 (package! helm-c-yasnippet :pin "65ca732b51")
 (package! helm-company :pin "6eb5c2d730")
@@ -13,6 +13,6 @@
 (when (featurep! +fuzzy)
   (package! helm-flx :pin "6640fac5cb"))
 (when (featurep! +childframe)
-  (package! posframe :pin "087a7fc3c8"))
+  (package! posframe :pin "8a9af547e6"))
 (when (featurep! :lang org)
   (package! helm-org :pin "8457e1e462"))
