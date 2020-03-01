38a74db6266108ce53e9919777500ecafc230bf1
Henrik Lissner <henrik@lissner.net>
2020-03-01 12:07:28 -0500

    Bump :ui modeline
    seagle0128/doom-modeline@41015d7 -> seagle0128/doom-modeline@0df5585

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "41015d72e7"))
+  (package! doom-modeline :pin "0df5585984"))
 (package! anzu :pin "592f8ee6d0")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "9bca6ca14d"))
