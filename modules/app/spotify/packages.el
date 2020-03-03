684fca9e3b8eb479ca1ada21f43239d72b357b38
Otávio Schwanck <otavioschwanck@gmail.com>
2020-03-03 15:29:18 +0000

    fix wrong place of config

--- modules/app/spotify/packages.el
+++ modules/app/spotify/packages.el
@@ -3,15 +3,4 @@
 ;;
 ;;; Packages
 
-(package! helm-spotify-plus :pin "c3922ec368"
-  :when (featurep! helm)
-  :config
-  (map! :localleader
-        :map global-map
-        :prefix "5"
-        "p" #'helm-spotify-plus-previous
-        "n" #'helm-spotify-plus-next
-        "x" #'helm-spotify-plus-pause
-        "q" #'helm-spotify-plus-queue-stop
-        "s" #'helm-spotify-plus
-        ) :pin "c3922ec368")
+(package! helm-spotify-plus :pin "c3922ec368")
