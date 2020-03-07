83072d544023fa0175886dacdc1924be0a34b843
Henrik Lissner <henrik@lissner.net>
2020-03-06 20:38:34 -0500

    Load lsp-java sooner
    So its server appears in M-x lsp-install-server

--- modules/lang/java/+lsp.el
+++ modules/lang/java/+lsp.el
@@ -2,7 +2,7 @@
 ;;;###if (featurep! +lsp)
 
 (use-package! lsp-java
-  :after-call java-mode
+  :after lsp-clients
   :init
   (add-hook 'java-mode-local-vars-hook #'lsp!)
   (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/")
