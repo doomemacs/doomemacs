8fb076ada36acda2fc96d89425041c44e06de2e7
asymmetric <lorenzo@mailbox.org>
2020-03-08 15:40:48 +0000

    emacs: vc: browse-at-remote: bump to 771a3079e2
    Tagged as 0.14.0.

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,7 +5,7 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "aeee6bf38f")
+(package! browse-at-remote :pin "771a3079e2")
 (package! git-timemachine :pin "391eb61050")
 (package! gitconfig-mode :pin "55468314a5")
 (package! gitignore-mode :pin "55468314a5")
