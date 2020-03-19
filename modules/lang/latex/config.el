6b68dcceee7f9d43ca72d98589aef51a482d0a96
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-03-19 19:19:35 +0330

    Minor fixes in docs and check for :lang lsp.

--- modules/lang/latex/config.el
+++ modules/lang/latex/config.el
@@ -181,11 +181,9 @@ If no viewers are found, `latex-preview-pane' is used.")
   (add-to-list '+latex--company-backends #'+latex-symbols-company-backend nil #'eq))
 
 (after! tex-mode
-  (when (featurep! +lsp)
+  (when (and (featurep! +lsp) (featurep! :tools lsp))
     (add-hook 'tex-mode-local-vars-hook #'lsp!)
-    (add-hook 'latex-mode-local-vars-hook #'lsp!)
-    (after! yatex
-      (add-hook 'yatex-mode-local-vars-hook #'lsp!))))
+    (add-hook 'latex-mode-local-vars-hook #'lsp!)))
 
 ;; bibtex + reftex
 (load! "+ref")
