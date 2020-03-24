e43e9fadb98a52f2dc932d37a98616765b66dd45
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-03-24 13:45:36 +0430

    Added Dart-Snippets from GitHub.

--- modules/lang/dart/packages.el
+++ modules/lang/dart/packages.el
@@ -2,5 +2,17 @@
 ;;; lang/dart/packages.el
 
 (package! dart-mode :pin "04fcd649f1")
+
+;; Optional module features
+
 (when (featurep! +flutter)
   (package! flutter :pin "ec92a4df84"))
+
+;; Features according to other user selected options
+
+(when (featurep! :editor snippets)
+  (package! dart-snippets
+    :recipe (:host github
+                   :repo "MYDavoodeh/dart-snippets"
+                   :files ("*.el" ("snippets" "snippets/*")))
+    :pin "87ccb4defd"))
