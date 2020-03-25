49a21720f887ccb463fa2be0183a16deea93e9c8
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-03-26 00:20:59 +0430

    Removed the snippet pkg since they're in Doom's.

--- modules/lang/dart/packages.el
+++ modules/lang/dart/packages.el
@@ -7,12 +7,3 @@
 
 (when (featurep! +flutter)
   (package! flutter :pin "ec92a4df84"))
-
-;; Features according to other user selected options
-
-(when (featurep! :editor snippets)
-  (package! dart-snippets
-    :recipe (:host github
-                   :repo "MYDavoodeh/dart-snippets"
-                   :files ("*.el" ("snippets" "snippets/*")))
-    :pin "946ad5aaa5"))
