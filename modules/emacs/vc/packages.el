cb3cf55fbf9da443788906f35913bfa3f93990cb
Henrik Lissner <henrik@lissner.net>
2020-03-27 17:23:21 -0400

    Bump :emacs
    dgutov/diff-hl@fb9eb1c -> dgutov/diff-hl@2cf8b48
    jtbm37/all-the-icons-dired@980b774 -> jtbm37/all-the-icons-dired@816987d
    purcell/ibuffer-projectile@7649621 -> purcell/ibuffer-projectile@504b0ed
    purcell/ibuffer-vc@64cb038 -> purcell/ibuffer-vc@1249c1e
    ralesi/ranger.el@af6f781 -> ralesi/ranger.el@ae9b381
    rmuslimov/browse-at-remote@771a307 -> rmuslimov/browse-at-remote@6aecae4
    stsquad/dired-rsync@698294c -> stsquad/dired-rsync@bfd5c15

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,7 +5,7 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "771a3079e2")
+(package! browse-at-remote :pin "6aecae4b5d")
 (package! git-timemachine :pin "391eb61050")
 (package! gitconfig-mode :pin "55468314a5")
 (package! gitignore-mode :pin "55468314a5")
