2869c020a045674178a30ff5bd5c6b31cb7ce2d2
Henrik Lissner <henrik@lissner.net>
2020-03-27 18:11:03 -0400

    Bump :editor
    abo-abo/lispy@d6b19fe -> abo-abo/lispy@c7e282a
    hlissner/doom-snippets@2781b78 -> hlissner/doom-snippets@2a0c3cf
    joaotavora/yasnippet@3bf9a3b -> joaotavora/yasnippet@ac03c2f
    joaotavora/yasnippet@3bf9a3b -> joaotavora/yasnippet@ac03c2f
    noctuid/lispyville@56198f1 -> noctuid/lispyville@25a7012

--- modules/editor/snippets/packages.el
+++ modules/editor/snippets/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/snippets/packages.el
 
-(package! yasnippet :pin "3bf9a3b1af")
+(package! yasnippet :pin "ac03c2f192")
 (package! auto-yasnippet :pin "db9e0dd433")
 (package! doom-snippets
   :recipe (:host github
            :repo "hlissner/doom-snippets"
            :files ("*.el" "*"))
-  :pin "2781b782a3")
+  :pin "2a0c3cf901")
