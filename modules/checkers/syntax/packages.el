6b73f3839596e4e2cb560315571c137043907373
Henrik Lissner <henrik@lissner.net>
2020-03-27 01:08:36 -0400

    Bump to flycheck/flycheck@f19a51c
    From flycheck/flycheck@08345d3

--- modules/checkers/syntax/packages.el
+++ modules/checkers/syntax/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; checkers/syntax/packages.el
 
-(package! flycheck :pin "08345d38e2")
+(package! flycheck :pin "f19a51c0f1")
 (package! flycheck-popup-tip :pin "ef86aad907")
 (when (featurep! +childframe)
   (package! flycheck-posframe :pin "2b3e94c2e4"))
