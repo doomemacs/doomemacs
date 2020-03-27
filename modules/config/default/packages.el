5b0782f234965705d5f5ff6682b9ed0f2409c385
Henrik Lissner <henrik@lissner.net>
2020-03-27 18:12:27 -0400

    Bump :config default
    abo-abo/avy@cf95ba9 -> abo-abo/avy@3bf8314
    magnars/expand-region.el@1603d01 -> magnars/expand-region.el@ea6b4cb

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; config/default/packages.el
 
-(package! avy :pin "cf95ba9582")
+(package! avy :pin "3bf83140fa")
 (package! drag-stuff :pin "6d06d846cd")
 (package! link-hint :pin "0d9cabcdb7")
 
 (unless (featurep! :editor evil)
-  (package! expand-region :pin "1603d01fbf"))
+  (package! expand-region :pin "ea6b4cbb99"))
