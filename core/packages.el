fd177b971beb514ae6a280a457ef8eba11ea69d2
Henrik Lissner <henrik@lissner.net>
2020-03-27 00:55:37 -0400

    Bump core packages
    koral/gcmh@8867533 -> koral/gcmh@9e241e0
    domtronn/all-the-icons.el@1416f37 -> domtronn/all-the-icons.el@f6cbb51
    jscheid/dtrt-indent@48221c9 -> jscheid/dtrt-indent@1569b71
    Fuco1/smartparens@1f8857c -> Fuco1/smartparens@555626a
    bbatsov/projectile@341150c -> bbatsov/projectile@588692a
    noctuid/general.el@f6e9286 -> noctuid/general.el@14ad4c8
    justbur/emacs-which-key@7b068f3 -> justbur/emacs-which-key@8b49ae9

--- core/packages.el
+++ core/packages.el
@@ -3,10 +3,10 @@
 
 ;; core.el
 (package! auto-minor-mode :pin "17cfa1b548")
-(package! gcmh :pin "8867533a73")
+(package! gcmh :pin "9e241e0a9f")
 
 ;; core-ui.el
-(package! all-the-icons :pin "1416f37984")
+(package! all-the-icons :pin "f6cbb51c15")
 (package! hide-mode-line :pin "88888825b5")
 (package! highlight-numbers :pin "8b4744c7f4")
 (package! rainbow-delimiters :pin "5125f4e476")
@@ -14,12 +14,12 @@
 
 ;; core-editor.el
 (package! better-jumper :pin "6d240032ca")
-(package! dtrt-indent :pin "48221c928b")
+(package! dtrt-indent :pin "1569b712ea")
 (package! helpful :pin "c54e9ddbd6")
 (when IS-MAC
   (package! ns-auto-titlebar :pin "1efc30d385"))
 (package! pcre2el :pin "0b5b2a2c17")
-(package! smartparens :pin "1f8857c5fe")
+(package! smartparens :pin "555626a43f")
 (package! so-long
   :built-in 'prefer ; included in Emacs 27+
   ;; REVIEW so-long is slated to be published to ELPA eventually, but until then
@@ -37,11 +37,11 @@
   (package! xclip :pin "d022cf947d"))
 
 ;; core-projects.el
-(package! projectile :pin "341150c0e7")
+(package! projectile :pin "588692ad56")
 
 ;; core-keybinds.el
-(package! general :pin "f6e928622d")
-(package! which-key :pin "7b068f3e95")
+(package! general :pin "14ad4c888b")
+(package! which-key :pin "8b49ae978c")
 
 ;; autoload/cache.el
 (package! persistent-soft :pin "a1e0ddf2a1")
