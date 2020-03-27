5c3d54f19b99723652f9805265c94e7dc6de90b3
Henrik Lissner <henrik@lissner.net>
2020-03-27 16:32:39 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@355d4da -> emacs-lsp/lsp-mode@76fe399
    emacs-lsp/lsp-ui@70c2fec -> emacs-lsp/lsp-ui@134d9b7
    emacs-lsp/lsp-ivy@78c1429 -> emacs-lsp/lsp-ivy@39b90e7

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/lsp/packages.el
 
-(package! lsp-mode :pin "355d4da3ac")
-(package! lsp-ui :pin "70c2fecbab")
+(package! lsp-mode :pin "76fe399b40")
+(package! lsp-ui :pin "134d9b725d")
 (when (featurep! :completion company)
   (package! company-lsp :pin "f921ffa0cd"))
 (when (featurep! :completion ivy)
-  (package! lsp-ivy :pin "78c1429c62"))
+  (package! lsp-ivy :pin "39b90e7aef"))
 (when (featurep! :completion helm)
   (package! helm-lsp :pin "6f62659cc5"))
