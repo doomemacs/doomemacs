b7ba3fec56e2d67cf4633bc26d6b7dca1ca74627
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-03-28 16:06:11 +0430

    Add dart to `init.example.el`

--- init.example.el
+++ init.example.el
@@ -114,6 +114,7 @@
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
+       ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
