cf180bea9222e9867ab95324b6ffef2f1b6e94b8
Henrik Lissner <henrik@lissner.net>
2020-03-28 20:08:24 -0400

    Remove ivy-erlang-complete
    It's redundant with company-erlang, which uses ivy-erlang-complete as a
    backend anyhow.
    
    Fixes #2786

--- modules/lang/erlang/config.el
+++ modules/lang/erlang/config.el
@@ -3,7 +3,10 @@
 (use-package! erlang
   :mode ("\\.erlang\\'" . erlang-mode)
   :mode ("/rebar\\.config\\(?:\\.script\\)?\\'" . erlang-mode)
-  :mode ("/\\(?:app\\|sys\\)\\.config\\'" . erlang-mode))
+  :mode ("/\\(?:app\\|sys\\)\\.config\\'" . erlang-mode)
+  :config
+  (when (featurep! +lsp)
+    (add-hook 'erlang-mode-local-vars-hook #'lsp!)))
 
 
 (use-package! flycheck-rebar3
@@ -12,18 +15,10 @@
   :config (flycheck-rebar3-setup))
 
 
-(use-package! ivy-erlang-complete
-  :when (featurep! :completion ivy)
-  :hook (erlang-mode . ivy-erlang-complete-init)
-  :config
-  (add-hook! 'erlang-mode-hook
-    (add-hook 'after-save-hook #'ivy-erlang-complete-reparse nil t)))
-
-
 (use-package! company-erlang
   :when (featurep! :completion company)
   :unless (featurep! +lsp)
-  :hook (erlang-mode . company-erlang-init))
-
-(when (featurep! +lsp)
-  (add-hook 'erlang-mode-local-vars-hook #'lsp!))
+  :hook (erlang-mode . company-erlang-init)
+  :config
+  (add-hook! 'erlang-mode-hook
+    (add-hook 'after-save-hook #'ivy-erlang-complete-reparse nil t)))
--- modules/lang/erlang/packages.el
+++ modules/lang/erlang/packages.el
@@ -5,7 +5,5 @@
 (when (featurep! :checkers syntax)
   (package! flycheck-rebar3 :pin "3cca1268c5"))
 (unless (featurep! +lsp)
-  (when (featurep! :completion ivy)
-    (package! ivy-erlang-complete :pin "c443dba0c4"))
   (when (featurep! :completion company)
     (package! company-erlang :pin "bc0524a16f")))
