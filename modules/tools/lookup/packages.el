84d2e676335c9dbdd4ac165ea4fada29e3106d80
Henrik Lissner <henrik@lissner.net>
2020-03-31 15:20:58 -0400

    Fix missing request in lookup module

--- modules/tools/lookup/packages.el
+++ modules/tools/lookup/packages.el
@@ -14,6 +14,9 @@
 (when (featurep! :completion helm)
   (package! helm-xref :pin "6b4a8bd91f"))
 
+;; For dictionary and online lookup
+(package! request :pin "216d570a58")
+
 (when (featurep! +docsets)
   (package! dash-docs :pin "111fd9b970")
   (when (featurep! :completion helm)
@@ -26,7 +29,6 @@
       (package! osx-dictionary :pin "1b79ff64c7")
     (package! define-word :pin "d8c76d503b")
     (package! powerthesaurus :pin "81a262ec0c")
-    (package! request)
     (when (featurep! +offline)
       (package! wordnut :pin "feac531404")
       (package! synosaurus :pin "14d34fc92a"))))
