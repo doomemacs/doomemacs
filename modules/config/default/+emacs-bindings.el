8d77518710e4227c4e8ef87b4a2aa9bd1f22d26e
Henrik Lissner <henrik@lissner.net>
2020-04-08 04:15:16 -0400

    Add default undo-fu keybinds for vanilla users

--- modules/config/default/+emacs-bindings.el
+++ modules/config/default/+emacs-bindings.el
@@ -364,6 +364,12 @@
 
 (map! "C-'" #'imenu
 
+      "C-_"     #'undo-fu-only-undo
+      "M-_"     #'undo-fu-only-redo
+      "C-M-_"   #'undo-fu-only-redo-all
+      "C-x r u" #'undo-fu-session-save
+      "C-x r U" #'undo-fu-session-recover
+
       ;;; Text scaling
       [C-mouse-4] #'text-scale-increase
       [C-mouse-5] #'text-scale-decrease
