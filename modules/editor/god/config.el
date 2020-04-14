045e893e977677b6f607b841c5f93ee77e41d3c2
Akhil Wali <akhil.wali.10@gmail.com>
2020-04-14 20:41:20 +1200

    Use default god-exempt-major-modes

--- modules/editor/god/config.el
+++ modules/editor/god/config.el
@@ -3,26 +3,5 @@
 (use-package! god-mode
   :hook (doom-after-init-modules . god-mode-all)
   :config
-  (pushnew! god-exempt-major-modes
-            'Custom-mode
-            'Info-mode
-            'ag-mode
-            'calculator-mode
-            'calendar-mode
-            'cider-test-report-mode
-            'compilation-mode
-            'debugger-mode
-            'edebug-mode
-            'ediff-mode
-            'eww-mode
-            'geben-breakpoint-list-mode
-            'ibuffer-mode
-            'org-agenda-mode
-            'pdf-outline-buffer-mode
-            'recentf-dialog-mode
-            'sldb-mode
-            'sly-db-mode
-            'wdired-mode)
-
   (add-hook 'post-command-hook #'+god--configure-cursor-and-modeline-h)
   (add-hook 'overwrite-mode-hook #'+god--toggle-on-overwrite-h))
