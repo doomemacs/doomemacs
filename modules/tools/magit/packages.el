31a31fff46707727e165b3c3ca467f20bbd0e9c6
Henrik Lissner <henrik@lissner.net>
2020-04-14 19:18:26 -0400

    Bump :tools magit
    emacs-evil/evil-magit@0b79aa3 -> emacs-evil/evil-magit@253c644
    magit/magit@68b5a13 -> magit/magit@0f1234e

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "68b5a13fa1")
+(when (package! magit :pin "0f1234e785")
   (package! forge :pin "2e2d26cf42")
   (package! magit-gitflow :pin "cc41b561ec")
   (package! magit-todos :pin "a0e5d1f3c7")
   (package! github-review :pin "50c6bcc7cf")
   (when (featurep! :editor evil +everywhere)
-    (package! evil-magit :pin "0b79aa33a4")))
+    (package! evil-magit :pin "253c644807")))
