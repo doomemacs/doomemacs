088db18c6d81f7ad118e1178d141014c14a1869a
Brian McGillion <brian.mcgillion@darkmatter.ae>
2020-04-16 13:53:39 +0400

    Add bibtex-completion
    Helm-Bibtex was split to extract bibtex-completion into a standalone
    MELPA package. Adding the functionality here to account for the
    changes in the packaging.

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -1,6 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/biblio/packages.el
 
+(package! bibtex-completion :pin "b14b628261")
 (when (featurep! :completion ivy)
   (package! ivy-bibtex :pin "3cff6bd702"))
 (when (featurep! :completion helm)
