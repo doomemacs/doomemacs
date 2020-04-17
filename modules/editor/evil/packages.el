c96f553e436c36465798c3349158b91ecc3d8c65
Henrik Lissner <henrik@lissner.net>
2020-04-16 20:09:19 -0400

    Bump :editor evil
    emacs-evil/evil-collection@fe0700e -> emacs-evil/evil-collection@493d523
    emacs-evil/evil@2969324 -> emacs-evil/evil@8aa6337
    janpath/evil-numbers@d988041 -> janpath/evil-numbers@c2cfdd1
    redguardtoo/evil-nerd-commenter@4387407 -> redguardtoo/evil-nerd-commenter@60b8862
    
    Fixes #2912

--- modules/editor/evil/packages.el
+++ modules/editor/evil/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/evil/packages.el
 
-(package! evil :pin "296932406a")
+(package! evil :pin "8aa6337fa8")
 (package! evil-args :pin "758ad5ae54")
 (package! evil-easymotion :pin "79c13ed3bc")
 (package! evil-embrace :pin "4379adea03")
@@ -9,10 +9,10 @@
 (package! evil-exchange :pin "3030e21ee1")
 (package! evil-indent-plus :pin "0c7501e6ef")
 (package! evil-lion :pin "6b03593f5d")
-(package! evil-nerd-commenter :pin "4387407615")
+(package! evil-nerd-commenter :pin "60b8862864")
 (package! evil-numbers
   :recipe (:host github :repo "janpath/evil-numbers")
-  :pin "d988041c1f")
+  :pin "c2cfdd1eb1")
 (package! evil-snipe :pin "3ec8adfd49")
 (package! evil-surround :pin "9b0b17f06c")
 (package! evil-textobj-anyblock :pin "ff00980f06")
@@ -31,4 +31,4 @@
     (package! neotree)
     (autoload 'neotree-make-executor "neotree" nil nil 'macro))
 
-  (package! evil-collection :pin "fe0700ec16"))
+  (package! evil-collection :pin "493d523c9b"))
