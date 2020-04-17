a9d7fc099d02257434598a6c905552860445d4c7
Steven vanZyl <rushsteve1@rushsteve1.us>
2020-04-17 15:48:54 -0400

    Improved Eshell module part 1
    Added a README file detailing the Eshell module, a few minor tweaks to
    the config, some keybindings, and the eshell-did-you-mean package.
    
    More to come!

--- modules/term/eshell/config.el
+++ modules/term/eshell/config.el
@@ -82,6 +82,10 @@ You should use `set-eshell-alias!' to change this.")
   ;; Enable autopairing in eshell
   (add-hook 'eshell-mode-hook #'smartparens-mode)
 
+  ;; Comapny completion integration
+  (when (featurep! :completion company)
+    (add-hook 'eshell-mode-hook #'company-mode))
+
   ;; Persp-mode/workspaces integration
   (when (featurep! :ui workspaces)
     (add-hook 'persp-activated-functions #'+eshell-switch-workspace-fn)
@@ -152,7 +156,13 @@ You should use `set-eshell-alias!' to change this.")
             [remap doom/backward-kill-to-bol-and-indent] #'eshell-kill-input
             [remap evil-delete-back-to-indentation] #'eshell-kill-input
             [remap evil-window-split]   #'+eshell/split-below
-            [remap evil-window-vsplit]  #'+eshell/split-right))))
+            [remap evil-window-vsplit]  #'+eshell/split-right
+
+            ;; Local-leader keybindings
+            (:localleader
+             "b" #'eshell-insert-buffer-name
+             "e" #'eshell-insert-envvar
+             "s" #'+eshell/search-history)))))
 
 
 (use-package! eshell-up
@@ -183,3 +193,7 @@ You should use `set-eshell-alias!' to change this.")
   (defadvice! +eshell--fallback-to-bash-a (&rest _)
     :before-while #'fish-completion--list-completions-with-desc
     (executable-find "fish")))
+
+;; Active eshell-did-you-mean using its setup function which provides
+;; its own hooks.
+(eshell-did-you-mean-setup)
