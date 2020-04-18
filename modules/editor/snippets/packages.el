cf355d4d421b903ec02a7c057ec14cc43e89bcbe
Henrik Lissner <henrik@lissner.net>
2020-04-17 21:59:25 -0400

    Bump :editor snippets
    hlissner/doom-snippets@2a0c3cf -> hlissner/doom-snippets@feaedeb
    joaotavora/yasnippet@ac03c2f -> joaotavora/yasnippet@5b1217a

--- modules/editor/snippets/packages.el
+++ modules/editor/snippets/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/snippets/packages.el
 
-(package! yasnippet :pin "ac03c2f192")
+(package! yasnippet :pin "5b1217ab08")
 (package! auto-yasnippet :pin "db9e0dd433")
 (package! doom-snippets
   :recipe (:host github
            :repo "hlissner/doom-snippets"
            :files ("*.el" "*"))
-  :pin "2a0c3cf901")
+  :pin "feaedeb550")
