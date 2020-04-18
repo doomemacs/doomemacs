1bc65ab98004d41b7babb2aca119202cba2abf5a
Henrik Lissner <henrik@lissner.net>
2020-04-17 22:11:55 -0400

    Bump :lang emacs-lisp
    jorgenschaefer/emacs-buttercup@b360e35 -> jorgenschaefer/emacs-buttercup@a91f282
    xuchunyang/elisp-demos@0d74766 -> xuchunyang/elisp-demos@57dd4ae

--- modules/lang/emacs-lisp/packages.el
+++ modules/lang/emacs-lisp/packages.el
@@ -7,9 +7,8 @@
 (package! macrostep :pin "424e3734a1")
 (package! overseer :pin "02d49f582e")
 (package! elisp-def :pin "368b04da68")
-(package! elisp-demos :pin "0d74766f0c")
-
+(package! elisp-demos :pin "57dd4ae3e4")
 (when (featurep! :checkers syntax)
   (package! flycheck-cask :pin "3457ae553c"))
 
-(package! buttercup :pin "b360e35017")
+(package! buttercup :pin "a91f282025")
