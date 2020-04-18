e62f64d732eaf78093c7de24ad2227633a051c74
Henrik Lissner <henrik@lissner.net>
2020-04-17 22:22:10 -0400

    Encourage +lsp for :lang go #1245
    The non-LSP stack (gocode) appears to be unmaintained and is a poor
    experience. Could use some help from go users to figure out which of the
    dependencies in the lang/go's readme should be installed with gopls.

--- init.example.el
+++ init.example.el
@@ -121,7 +121,7 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
-       ;;go                ; the hipster dialect
+       ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
