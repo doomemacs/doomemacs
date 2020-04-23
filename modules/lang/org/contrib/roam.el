7d38f503e2f353009ff76e002d5d39fe0ed5b218
bdarcus <bdarcus@gmail.com>
2020-04-23 14:46:13 -0400

    updating per @hlissner suggestion

--- modules/lang/org/contrib/roam.el
+++ modules/lang/org/contrib/roam.el
@@ -36,7 +36,14 @@
   (setq org-roam-directory (expand-file-name (or org-roam-directory "")
                                              org-directory)
         org-roam-verbose nil  ; https://youtu.be/fn4jIlFwuLU
-        org-roam-completion-system 'default)
+        org-roam-completion-system
+        (cond ((featurep! :completion helm) 'helm)
+              ((featurep! :completion ivy) 'ivy)
+              ((featurep! :completion ido) 'ido)
+              ('default))
+        org-roam-completion-fuzzy-match
+        (or (featurep! :completion helm +fuzzy)
+            (featurep! :completion ivy +fuzzy)))
 
   ;; HACK Hide the mode line in the org-roam buffer, since it serves no purpose.
   ;;      This makes it easier to distinguish among other org buffers.
