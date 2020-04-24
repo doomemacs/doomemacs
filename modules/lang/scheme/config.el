74d05552c1302614b7efb967166fb193d7c67548
Henrik Lissner <henrik@lissner.net>
2020-04-24 00:33:54 -0400

    Refactor scheme module

--- modules/lang/scheme/config.el
+++ modules/lang/scheme/config.el
@@ -1,20 +1,19 @@
 ;;; lang/scheme/config.el -*- lexical-binding: t; -*-
 
 (use-package! scheme
-  :defer t
-  :hook ((scheme-mode . rainbow-delimiters-mode)
-         (scheme-mode . geiser-mode)))
+  :hook (scheme-mode . rainbow-delimiters-mode))
+
 
 (use-package! geiser
-  :defer t
+  :hook (scheme-mode . geiser-mode)
   :init
-  (setq geiser-active-implementations '(guile chicken mit chibi chez))
-  (setq geiser-mode-start-repl-p t)
-  (setq geiser-smart-tab-p t)
+  (setq geiser-active-implementations '(guile chicken mit chibi chez)
+        geiser-mode-start-repl-p t
+        geiser-smart-tab-p t)
   (unless (featurep! :lang racket)
     (push 'racket geiser-active-implementations))
   (after! scheme                        ; built-in
-    (set-repl-handler! 'scheme-mode '+scheme/open-repl)
+    (set-repl-handler! 'scheme-mode #'+scheme/open-repl)
     (set-eval-handler! 'scheme-mode #'geiser-eval-region)
     (set-lookup-handlers! 'scheme-mode
       :definition #'geiser-edit-symbol-at-point
@@ -30,7 +29,6 @@
         "["  #'geiser-squarify
         "\\" #'geiser-insert-lambda
         "s"  #'geiser-set-scheme
-
         (:prefix ("e" . "eval")
           "b" #'geiser-eval-buffer
           "B" #'geiser-eval-buffer-and-go
@@ -39,7 +37,6 @@
           "D" #'geiser-eval-definition-and-go
           "r" #'geiser-eval-region
           "R" #'geiser-eval-region-and-go)
-
         (:prefix ("h" . "help")
           "d" #'geiser-autodoc
           "<" #'geiser-xref-callers
