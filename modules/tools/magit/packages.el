217a032035c52261ede0975ad0c35f039d34d94a
Henrik Lissner <henrik@lissner.net>
2020-04-25 21:52:10 -0400

    Bump :tools magit
    magit/magit@0f1234e -> magit/magit@8de6ecf
    magit/forge@2e2d26c -> magit/forge@e2da806

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "0f1234e785")
+(when (package! magit :pin "8de6ecf5c5")
   (when (featurep! +forge)
-    (package! forge :pin "2e2d26cf42"))
+    (package! forge :pin "e2da80660a"))
   (package! magit-gitflow :pin "cc41b561ec")
   (package! magit-todos :pin "a0e5d1f3c7")
   (package! github-review :pin "50c6bcc7cf")
