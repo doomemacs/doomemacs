90197eed707a3e1ea3f439cbf9d1d394f78d54c9
Henrik Lissner <henrik@lissner.net>
2020-04-25 21:58:42 -0400

    Bump :ui treemacs
    Alexander-Miller/treemacs@2bb14ac -> Alexander-Miller/treemacs@ee1b523

--- modules/ui/treemacs/packages.el
+++ modules/ui/treemacs/packages.el
@@ -1,7 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/treemacs/packages.el
 
-(package! treemacs :pin "2bb14ac262")
+(package! treemacs :pin "ee1b523b28")
+;; These packages have no :pin because they're in the same repo
 (when (featurep! :editor evil +everywhere)
   (package! treemacs-evil))
 (package! treemacs-projectile)
