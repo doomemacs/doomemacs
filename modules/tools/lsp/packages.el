740b16a33f37ebfb918590975e8ab0d9d0ea8122
Henrik Lissner <henrik@lissner.net>
2020-04-25 21:50:03 -0400

    Bump :tools lsp
    emacs-lsp/helm-lsp@46a72f5 -> emacs-lsp/helm-lsp@db24399
    emacs-lsp/lsp-ivy@a6b7841 -> emacs-lsp/lsp-ivy@81e81ce
    emacs-lsp/lsp-mode@e8e52fd -> emacs-lsp/lsp-mode@4898d35

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/lsp/packages.el
 
-(package! lsp-mode :pin "e8e52fd4b1")
+(package! lsp-mode :pin "4898d35ace")
 (package! lsp-ui :pin "242dfe859c")
 (when (featurep! :completion company)
   (package! company-lsp :pin "f921ffa0cd"))
 (when (featurep! :completion ivy)
-  (package! lsp-ivy :pin "a6b7841e08"))
+  (package! lsp-ivy :pin "81e81ced99"))
 (when (featurep! :completion helm)
-  (package! helm-lsp :pin "46a72f5af1"))
+  (package! helm-lsp :pin "db243993ea"))
