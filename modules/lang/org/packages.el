d34633cb0ab737602c1ced0edb58779f1014d7d5
Henrik Lissner <henrik@lissner.net>
2020-04-28 14:39:10 -0400

    Bump to emacs-straight/org-mode@e68ae40
    From emacs-straight/org-mode@3106837
    
    Fixes #2993

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -27,7 +27,7 @@
   :recipe (:host github
            :repo "emacs-straight/org-mode"
            :files ("*.el" "lisp/*.el" "contrib/lisp/*.el"))
-  :pin "31068373dc")
+  :pin "e68ae40bdb")
 ;; ...And prevent other packages from pulling org; org-plus-contrib satisfies
 ;; the dependency already: https://github.com/raxod502/straight.el/issues/352
 (package! org :recipe (:local-repo nil))
