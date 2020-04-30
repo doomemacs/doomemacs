0ca9b4b12fb1886b10f09d9632be00e624f037a6
Henrik Lissner <henrik@lissner.net>
2020-04-30 00:18:31 -0400

    Bump :emacs
    dgutov/diff-hl@2cf8b48 -> dgutov/diff-hl@a625033
    ideasman42/emacs-undo-fu@0c34b67 -> ideasman42/emacs-undo-fu@0ce9ac3
    jtbm37/all-the-icons-dired@816987d -> jtbm37/all-the-icons-dired@fc2dfa1
    ralesi/ranger.el@af6f781 -> ralesi/ranger.el@ae9b381
    yqrashawn/fd-dired@fd4c3f4 -> yqrashawn/fd-dired@001cc95
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/emacs/ibuffer/packages.el
+++ modules/emacs/ibuffer/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; emacs/ibuffer/packages.el
 
-(package! ibuffer-projectile :pin "504b0edaa0")
-(package! ibuffer-vc :pin "1249c1e30c")
+(package! ibuffer-projectile :pin "504b0edaa0d937ce60ccc8fdf09f2dae0a90fbaf")
+(package! ibuffer-vc :pin "1249c1e30cf11badfe032ac3b1058f24ba510ace")
--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -2,6 +2,6 @@
 ;;; emacs/undo/packages.el
 
 (if (featurep! +tree)
-    (package! undo-tree :pin "5b6df03781")
-  (package! undo-fu :pin "0c34b6747e")
-  (package! undo-fu-session :pin "b808ef0cdc"))
+    (package! undo-tree :pin "5b6df03781495d8a25695d846b0cce496d3d3058")
+  (package! undo-fu :pin "0ce9ac36144e80316fff50bfe1bc5dd7e5e7ded6")
+  (package! undo-fu-session :pin "b808ef0cdcdd2eef221c67eda567eed7fcb3d4af"))
