1f52ae85669a6b83e6878107576ec05fe8eab231
Henrik Lissner <henrik@lissner.net>
2020-04-30 01:30:19 -0400

    Bump :config
    abo-abo/avy@3bf8314 -> abo-abo/avy@509471b
    noctuid/link-hint.el@0d9cabc -> noctuid/link-hint.el@7440704
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; config/default/packages.el
 
-(package! avy :pin "3bf83140fa")
-(package! drag-stuff :pin "6d06d846cd")
-(package! link-hint :pin "0d9cabcdb7")
+(package! avy :pin "509471bad0e8094b8639729ec39ca141fae7d4bd")
+(package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
+(package! link-hint :pin "7440704cacb5c0fab35fff8ec59d30fbea17f44a")
 
 (unless (featurep! :editor evil)
-  (package! expand-region :pin "ea6b4cbb99"))
+  (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781"))
