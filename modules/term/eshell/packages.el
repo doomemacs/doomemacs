9b1b8e2774b13d354ad35dd91503cd9c4e3d83f6
Henrik Lissner <henrik@lissner.net>
2020-04-30 01:27:21 -0400

    Bump :term
    akermu/emacs-libvterm@aa512b8 -> akermu/emacs-libvterm@e63bd65
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/term/eshell/packages.el
+++ modules/term/eshell/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; term/eshell/packages.el
 
-(package! eshell-up :pin "9c100bae5c")
-(package! eshell-z :pin "337cb241e1")
-(package! shrink-path :pin "c14882c859")
-(package! esh-help :pin "417673ed18")
+(package! eshell-up :pin "9c100bae5c3020e8d9307e4332d3b64e7dc28519")
+(package! eshell-z :pin "337cb241e17bd472bd3677ff166a0800f684213c")
+(package! shrink-path :pin "c14882c8599aec79a6e8ef2d06454254bb3e1e41")
+(package! esh-help :pin "417673ed18a983930a66a6692dbfb288a995cb80")
 
 (when (featurep! :completion company)
-  (package! fish-completion :pin "1038488181")
-  (package! bash-completion :pin "96ce14af96"))
+  (package! fish-completion :pin "10384881817b5ae38cf6197a077a663420090d2c")
+  (package! bash-completion :pin "96ce14af9674f3e605bacca87abc0c23b8f13cd5"))
