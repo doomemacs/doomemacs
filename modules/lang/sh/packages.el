885393ea1c9eec779b9da56038967f9b847bed56
Henrik Lissner <henrik@lissner.net>
2020-04-30 18:38:07 -0400

    Transition more :lang modules to long SHA1 format
    See 45cdfb125 for why.

--- modules/lang/sh/packages.el
+++ modules/lang/sh/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/sh/packages.el
 
 (when (featurep! :completion company)
-  (package! company-shell :pin "52f3bf26b7"))
+  (package! company-shell :pin "52f3bf26b74adc30a275f5f4290a1fc72a6876ff"))
 
 (when (featurep! +fish)
-  (package! fish-mode :pin "db257db810"))
+  (package! fish-mode :pin "db257db81058b0b12f788c324c264cc59b9a5bf4"))
