8b5525049d6a0c24a4b6209ed761e554f579c0fd
Henrik Lissner <henrik@lissner.net>
2020-04-30 00:21:21 -0400

    Bump :checkers
    d12frosted/flyspell-correct@e765d1a -> d12frosted/flyspell-correct@fd8ac7a
    flycheck/flycheck@f19a51c -> flycheck/flycheck@246e1d4
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/checkers/grammar/packages.el
+++ modules/checkers/grammar/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; checkers/grammar/packages.el
 
-(package! langtool :pin "a71ed02ce0")
-(package! writegood-mode :pin "b71757ec33")
+(package! langtool :pin "a71ed02ce06920ae3cafd6708de1c21811ce14c3")
+(package! writegood-mode :pin "b71757ec337e226909fb0422f0224e31acc71733")
