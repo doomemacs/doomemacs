b3c957065c036ad677b21aafcea5f726c303d653
Henrik Lissner <henrik@lissner.net>
2020-05-02 22:38:36 -0400

    Remove company-lsp fallback
    Issues with ts-ls and gopls have been reportedly resolved upstream, so
    we are fully committing to company-capf as LSP's chosen backend.

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -3,8 +3,6 @@
 
 (package! lsp-mode :pin "d0096ccf22e0e70eaad6cc99e5ae4bd9fc81f75f")
 (package! lsp-ui :pin "242dfe859c3497c456eaacfd84942e12419529fe")
-(when (featurep! :completion company)
-  (package! company-lsp :pin "f921ffa0cdc542c21dc3dd85f2c93df4288e83bd"))
 (when (featurep! :completion ivy)
   (package! lsp-ivy :pin "81e81ced99829358674c5a6bbe2c3e15cecd4ed8"))
 (when (featurep! :completion helm)
