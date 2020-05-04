1d85ab215c2860f2ca25661773580a0588525e6d
nitin <git@nitin.tv>
2020-05-04 09:08:36 -0400

    bump magit-delta

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -8,7 +8,7 @@
     (package! xterm-color :pin "d53a39a5af72cd340ebf686e59a37289b4cb6e8c")
     (package! magit-delta
       :recipe (:host github :repo "dandavison/magit-delta" :files ("magit-delta.el") )
-      :pin "a3081b030c1bc21a1c0ec4367a8b08a2a5f031f3"))
+      :pin "f429bf64bb398890b7c584d70660dd202e39824a"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "a0e5d1f3c7dfcb4f18c1b0d57f1746a4872df5c6")
   (package! github-review :pin "50c6bcc7cf4d7193577b3f74eea4dd72f2b7795b")
