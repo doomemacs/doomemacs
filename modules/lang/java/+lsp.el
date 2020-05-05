eb738d9e3a3209bd3d1ab3a7a5ffcc5fc9b02d82
James Ravn <james@r-vn.org>
2020-05-05 16:31:14 +0100

    Remove jt stuff

--- modules/lang/java/+lsp.el
+++ modules/lang/java/+lsp.el
@@ -9,17 +9,10 @@
 (add-hook! java-mode-local-vars #'lsp!)
 (map! :map java-mode-map
       :localleader
-      :desc "Update the Eclipse LSP server" "U" #'+java/update-lsp-server
-      :desc "Open the test browser" "T" #'lsp-jt-browser)
-
-;; TODO: This fails on the first java buffer opened - complains about the LSP server not having executeCode capabilities.
-(defun java--lsp-jt-lens-mode-hook ()
-  (when (derived-mode-p 'java-mode) (lsp-jt-lens-mode)))
+      :desc "Update the Eclipse LSP server" "U" #'+java/update-lsp-server)
 
 (use-package! lsp-java
   :after (lsp-clients dap-mode)
-  :hook (lsp-mode . java--lsp-jt-lens-mode-hook)
   :preface
   (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/")
-        lsp-java-workspace-dir (concat doom-etc-dir "java-workspace")
-        lsp-jt-root (concat doom-etc-dir "eclipse.jdt.ls/server/java-test/server")))
+        lsp-java-workspace-dir (concat doom-etc-dir "java-workspace")))
