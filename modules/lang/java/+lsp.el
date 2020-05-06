642efaf2b48450ce32c1972e11ef2fac9b47a117
James Ravn <james@r-vn.org>
2020-05-06 07:43:38 +0100

    Move hook into use-package, remove update-lsp-server

--- modules/lang/java/+lsp.el
+++ modules/lang/java/+lsp.el
@@ -1,18 +1,12 @@
 ;;; lang/java/+lsp.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! +lsp)
 
-;;;###autoload
-(defun +java/update-lsp-server ()
-  (interactive)
-  (lsp--install-server-internal (gethash 'jdtls lsp-clients) t))
-
-(add-hook! java-mode-local-vars #'lsp!)
-(map! :map java-mode-map
-      :localleader
-      :desc "Update the Eclipse LSP server" "U" #'+java/update-lsp-server)
-
 (use-package! lsp-java
   :after (lsp-clients dap-mode)
   :preface
   (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/")
-        lsp-java-workspace-dir (concat doom-etc-dir "java-workspace")))
+        lsp-java-workspace-dir (concat doom-etc-dir "java-workspace"))
+  (add-hook! java-mode-local-vars #'lsp!)
+  :config
+  ;; TODO keybinds
+  )
