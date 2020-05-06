b8e85a42c7a267891aec37ac893162d22799eb5a
Seong Yong-ju <sei40kr@gmail.com>
2020-05-07 08:28:12 +0900

    Remove .yas-setup.el for java-mode

--- modules/editor/file-templates/templates/java-mode/.yas-setup.el
+++
@@ -1,9 +0,0 @@
-(defun yas-java-project-package ()
-  (or (and (eq major-mode 'java-mode)
-           (+java-current-package))
-      ""))
-
-(defun yas-java-class-name ()
-  (or (and (eq major-mode 'java-mode)
-           (+java-current-class))
-      ""))
