57e019e62e110e8101bb4cf69b14339cc04921da
James Ravn <james@r-vn.org>
2020-05-06 07:52:33 +0100

    Remove unnecessary dap-mode dependency
    Only needed this when I was auto loading the jt lens. This should also
    be conditional on the debugger module being enabled.

--- modules/lang/java/+lsp.el
+++ modules/lang/java/+lsp.el
@@ -2,7 +2,7 @@
 ;;;###if (featurep! +lsp)
 
 (use-package! lsp-java
-  :after (lsp-clients dap-mode)
+  :after lsp-clients
   :preface
   (setq lsp-java-server-install-dir (concat doom-etc-dir "eclipse.jdt.ls/server/")
         lsp-java-workspace-dir (concat doom-etc-dir "java-workspace")
