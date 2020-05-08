0d821f4ebd0a18dd2a022165d8b1a1f1f3df6796
yoavm448 <yoavm448@gmail.com>
2020-05-08 14:06:54 +0300

    Add org-roam doctor check for the dot executable

--- modules/lang/org/doctor.el
+++ modules/lang/org/doctor.el
@@ -4,3 +4,7 @@
 (when (featurep! +gnuplot)
   (unless (executable-find "gnuplot")
     (warn! "Couldn't find gnuplot. org-plot/gnuplot will not work")))
+
+(when (featurep! +roam)
+  (unless (executable-find "dot")
+    (warn! "Couldn't find the dot executable (from graphviz). org-roam will not be able to generate graph visuallizations.")))
