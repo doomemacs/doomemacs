395c4e593b085e56164564c7298a989144415b7a
Henrik Lissner <henrik@lissner.net>
2020-05-10 18:46:44 -0400

    Bump :lang emacs-lisp
    Fanael/highlight-quoted@2410347 -> Fanael/highlight-quoted@2410347
    jorgenschaefer/emacs-buttercup@a91f282 -> jorgenschaefer/emacs-buttercup@532d082
    xuchunyang/elisp-demos@57dd4ae -> xuchunyang/elisp-demos@4cd55a3
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/lang/emacs-lisp/packages.el
+++ modules/lang/emacs-lisp/packages.el
@@ -3,15 +3,16 @@
 
 (package! elisp-mode :built-in t)
 
-(package! highlight-quoted :pin "2410347815")
+;; Fontification plugins
+(package! highlight-quoted :pin "24103478158cd19fbcfb4339a3f1fa1f054f1469")
 
 ;; Tools
-(package! macrostep :pin "424e3734a1")
-(package! overseer :pin "02d49f582e")
-(package! elisp-def :pin "368b04da68")
-(package! elisp-demos :pin "57dd4ae3e4")
+(package! macrostep :pin "424e3734a1ee526a1bd7b5c3cd1d3ef19d184267")
+(package! overseer :pin "02d49f582e80e36b4334c9187801c5ecfb027789")
+(package! elisp-def :pin "368b04da68783601b52e3169312183381871cf9e")
+(package! elisp-demos :pin "4cd55a30d5dbd8d36a0e6f87261c4fef17fc6db0")
 (when (featurep! :checkers syntax)
-  (package! flycheck-cask :pin "3457ae553c"))
+  (package! flycheck-cask :pin "3457ae553c4feaf8168008f063d78fdde8fb5f94"))
 
 ;; Libraries
-(package! buttercup :pin "a91f282025")
+(package! buttercup :pin "532d082a363add4f6d9838ca3f924a773ce12136")
