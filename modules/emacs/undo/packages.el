cc2be4fd0148f5d0567c477d701655ab5fad021d
Henrik Lissner <henrik@lissner.net>
2020-05-18 17:18:12 -0400

    Bump :emacs undo
    ideasman42/emacs-undo-fu@2b1e532 -> ideasman42/emacs-undo-fu@24101b4
    ideasman42/emacs-undo-fu-session@0400f15 -> ideasman42/emacs-undo-fu-session@e2043f8

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -3,5 +3,5 @@
 
 (if (featurep! +tree)
     (package! undo-tree :pin "5b6df03781495d8a25695d846b0cce496d3d3058")
-  (package! undo-fu :pin "2b1e53285a55ce50ca6fd60b050f2171e235d8f9")
-  (package! undo-fu-session :pin "0400f15f2a0cfcedb69c06c3ff62f3f8814b62fb"))
+  (package! undo-fu :pin "24101b44fe0ea97e880fb14372ef461ac2cec1da")
+  (package! undo-fu-session :pin "e2043f8350970e1a9ef06a94956a733826cdf32b"))
