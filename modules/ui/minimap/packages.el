e0cc0eba241492570b37106dfa63cd389ee957d9
Henrik Lissner <henrik@lissner.net>
2020-05-18 17:23:41 -0400

    Fix 'bad object 5765245...' error on minimap module

--- modules/ui/minimap/packages.el
+++ modules/ui/minimap/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/minimap/packages.el
 
-(package! minimap :pin "5765245dee97a3c8818fd5cd3db1eca7247fcbbc")
+(package! minimap :pin "d8850be6fb7b3dbff0cd9b5a04d10a6dcc527326")
