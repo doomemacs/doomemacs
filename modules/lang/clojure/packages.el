265e80123a261c83fd3fcc83cd41dc2cd28bf26f
Henrik Lissner <henrik@lissner.net>
2020-05-19 05:35:03 -0400

    Bump :lang clojure
    clojure-emacs/cider@3a59fe0 -> clojure-emacs/cider@b9d505b

--- modules/lang/clojure/packages.el
+++ modules/lang/clojure/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/clojure/packages.el
 
 (package! clojure-mode :pin "da9f1ec717dac1194404b4a4562dba6bd9a4ee3a")
-(package! cider :pin "3a59fe046c9884573512dff88d140d37c0be7b2c")
+(package! cider :pin "b9d505b5896452a9a2a533a6b233ea263182fa50")
 (package! clj-refactor :pin "8259791e054382457b87d1f78061b5e3ce948907")
 
 (when (featurep! :checkers syntax)
