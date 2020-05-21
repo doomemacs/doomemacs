c4e8aa0baa4d044092e9021e9309e20ca042722b
Henrik Lissner <henrik@lissner.net>
2020-05-21 01:59:06 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@9305777 -> emacs-lsp/lsp-mode@81d62d5
    
    Which includes a fix for emacs-lsp/lsp-mode#1705

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/lsp/packages.el
 
-(package! lsp-mode :pin "93057777c66e8f0e9140b8d0d3215beaa2c035a9")
+(package! lsp-mode :pin "81d62d581b21d847783831e6e5ca9d3c63fe9a4d")
 (package! lsp-ui :pin "271b47cb33f11915295911f7cf8575f8a82a5e1c")
 (when (featurep! :completion ivy)
   (package! lsp-ivy :pin "dce58b5509271bbedb53ba9d0278dcb563a43977"))
