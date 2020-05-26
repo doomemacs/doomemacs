101cd5019f26ce76006445657948a49c58f85f6e
Henrik Lissner <henrik@lissner.net>
2020-05-26 01:39:18 -0400

    Bump :emacs undo
    ideasman42/emacs-undo-fu@bcf7f92 -> ideasman42/emacs-undo-fu@46de023
    
    bcf7f92 no longer exists (probably force pushed).

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -3,5 +3,5 @@
 
 (if (featurep! +tree)
     (package! undo-tree :pin "5b6df03781495d8a25695d846b0cce496d3d3058")
-  (package! undo-fu :pin "bcf7f92a8da38c18b203aa3a1298fa554afc7b08")
+  (package! undo-fu :pin "46de023b5f8ddb989eeff7665feeec2877d8eda8")
   (package! undo-fu-session :pin "e2043f8350970e1a9ef06a94956a733826cdf32b"))
