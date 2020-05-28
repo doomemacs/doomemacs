95eb2989dcf6b395257836bc9d090cfc1c147ebc
Henrik Lissner <henrik@lissner.net>
2020-05-28 17:36:16 -0400

    Minimize custom-set-faces! clobbering doom-load-theme-hook
    Evaluating custom-set-faces! calls would add setter functions to
    doom-load-theme-hook indefinitely, which could add up each time you M-x
    doom/reload. Now it adds them to a subhook, which is reset whenever Doom
    is reloaded.

--- core/autoload/themes.el
+++ core/autoload/themes.el
@@ -10,25 +10,32 @@
         (`((,(car spec) ,(cdr spec))))))
 
 ;;;###autoload
+(defconst doom-customize-theme-hook nil)
+
+(add-hook! 'doom-load-theme-hook :append
+  (defun doom-apply-customized-faces-h ()
+    (run-hooks 'doom-customize-theme-hook)))
+
+;;;###autoload
 (defmacro custom-theme-set-faces! (theme &rest specs)
   "Apply a list of face SPECS as user customizations for THEME.
 
 THEME can be a single symbol or list thereof. If nil, apply these settings to
 all themes. It will apply to all themes once they are loaded."
   (declare (indent defun))
-  `(let ((fn (gensym "doom--customize-themes-h-")))
-     (fset
-      fn (lambda ()
-           (let (custom--inhibit-theme-enable)
-             (dolist (theme (doom-enlist (or ,theme 'user)))
-               (when (or (eq theme 'user)
-                         (custom-theme-enabled-p theme))
-                 (apply #'custom-theme-set-faces theme
-                        (mapcan #'doom--custom-theme-set-face
-                                (list ,@specs))))))))
-     (when (or doom-init-theme-p (null doom-theme))
-       (funcall fn))
-     (add-hook 'doom-load-theme-hook fn 'append)))
+  (let ((fn (gensym "doom--customize-themes-h-")))
+    `(progn
+       (defun ,fn ()
+         (let (custom--inhibit-theme-enable)
+           (dolist (theme (doom-enlist (or ,theme 'user)))
+             (when (or (eq theme 'user)
+                       (custom-theme-enabled-p theme))
+               (apply #'custom-theme-set-faces theme
+                      (mapcan #'doom--custom-theme-set-face
+                              (list ,@specs)))))))
+       (when (or doom-init-theme-p (null doom-theme))
+         (funcall #',fn))
+       (add-hook 'doom-customize-theme-hook #',fn 'append))))
 
 ;;;###autoload
 (defmacro custom-set-faces! (&rest specs)
