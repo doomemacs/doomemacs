f2a3dee7ffbca283cc7158b3a372bd8cfa9ab8ed
Henrik Lissner <henrik@lissner.net>
2020-05-28 03:02:24 -0400

    Bump :lang latex
    TheBB/company-reftex@275ef70 -> TheBB/company-reftex@275ef70
    alexeyr/company-auctex@48c42c5 -> alexeyr/company-auctex@48c42c5
    cdominik/cdlatex@a5cb624 -> cdominik/cdlatex@480387b
    emacs-straight/adaptive-wrap@1810c0e -> emacs-straight/adaptive-wrap@1810c0e
    emacs-straight/auctex@fafa28d -> emacs-straight/auctex@b233945
    jsinglet/latex-preview-pane@5297668 -> jsinglet/latex-preview-pane@5297668
    tom-tan/auctex-latexmk@4d35352 -> tom-tan/auctex-latexmk@4d35352
    vspinu/company-math@a796053 -> vspinu/company-math@a796053

--- modules/lang/latex/packages.el
+++ modules/lang/latex/packages.el
@@ -1,21 +1,21 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/latex/packages.el
 
-(package! auctex :pin "fafa28d542")
-(package! adaptive-wrap :pin "1810c0ee8d")
-(package! latex-preview-pane :pin "5297668a89")
+(package! auctex :pin "b233945ba2d0616851796f95c415aa79df1c2b66")
+(package! adaptive-wrap :pin "1810c0ee8d827dd502ddeaae5bd759d4811fcbce")
+(package! latex-preview-pane :pin "5297668a89996b50b2b62f99cba01cc544dbed2e")
 
 ;; Optional module features:
 
 (when (featurep! +latexmk)
-  (package! auctex-latexmk :pin "4d35352265"))
+  (package! auctex-latexmk :pin "4d353522650d7685acbf1d38f7dbc504f734bd84"))
 
 (when (featurep! +cdlatex)
-  (package! cdlatex :pin "a5cb624ef5"))
+  (package! cdlatex :pin "480387b39f6ddd9cd2a9511ecee064ad8e1dd324"))
 
 ;; Features according to other user selected options
 
 (when (featurep! :completion company)
-  (package! company-auctex :pin "48c42c58ce")
-  (package! company-reftex :pin "275ef708f0")
-  (package! company-math :pin "a796053590"))
+  (package! company-auctex :pin "48c42c58ce2f0e693301b0cb2d085055410c1b25")
+  (package! company-reftex :pin "275ef708f08d3bf0eb30632148e5c6184eeaacdb")
+  (package! company-math :pin "a796053590012e6a15c8b527b521ffc15d137bd0"))
