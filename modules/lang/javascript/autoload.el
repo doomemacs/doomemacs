2407db0508f27ce7ce4274ca59dcabb5309f47d6
Henrik Lissner <henrik@lissner.net>
2020-05-31 20:11:05 -0400

    Fix #3252: void-variable skewer-css-mode

--- modules/lang/javascript/autoload.el
+++ modules/lang/javascript/autoload.el
@@ -60,12 +60,17 @@ Run this for any buffer you want to skewer."
   (require 'skewer-mode)
   (unless (process-status "httpd")
     (run-skewer))
-  (unless (and skewer-mode skewer-css-mode skewer-html-mode)
-    (pcase major-mode
-      ((or 'css-mode 'scss-mode 'less-css-mode) (skewer-css-mode +1))
-      ((or 'web-mode 'html-mode) (skewer-html-mode +1))
-      ('js2-mode (skewer-mode +1))
-      (_ (error "Invalid mode %s" major-mode)))))
+  (pcase major-mode
+    ((or 'css-mode 'scss-mode 'less-css-mode)
+     (unless skewer-css-mode
+       (skewer-css-mode +1)))
+    ((or 'web-mode 'html-mode)
+     (unless skewer-html-mode
+       (skewer-html-mode +1)))
+    ('js2-mode
+     (unless skewer-mode
+       (skewer-mode +1)))
+    (_ (error "Invalid mode %s" major-mode))))
 
 ;;;###autoload
 (defun +javascript/skewer-cleanup ()
