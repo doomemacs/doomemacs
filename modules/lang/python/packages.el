5981d27994c54b71eb637a9d1481644260dc87f6
Henrik Lissner <henrik@lissner.net>
2020-06-02 19:48:15 -0400

    Bump :lang python
    cython/cython@48dc1f0 -> cython/cython@12ab8bc
    emacs-lsp/lsp-python-ms@5d0c799 -> emacs-lsp/lsp-python-ms@db6f263
    galaunay/poetry.el@6dcc9d2 -> galaunay/poetry.el@d876522
    necaris/conda.el@335474e -> necaris/conda.el@9d02130
    pwalsh/pipenv.el@b730bb5 -> pwalsh/pipenv.el@b730bb5
    pythonic-emacs/company-anaconda@a31354c -> pythonic-emacs/company-anaconda@da1566d
    pythonic-emacs/pyenv-mode@aec6f2a -> pythonic-emacs/pyenv-mode@d191037
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/lang/python/packages.el
+++ modules/lang/python/packages.el
@@ -2,30 +2,31 @@
 ;;; lang/python/packages.el
 
 ;; Major modes
-(package! pip-requirements :pin "216cd1690f")
+(package! pip-requirements :pin "216cd1690f80cc965d4ae47b8753fc185f778ff6")
 (when (featurep! +cython)
-  (package! cython-mode :pin "48dc1f0169")
+  (package! cython-mode :pin "12ab8bc0056a0e77ccc0756955eb1621fd3b35db")
   (when (featurep! :checkers syntax)
-    (package! flycheck-cython :pin "ecc4454d35")))
+    (package! flycheck-cython :pin "ecc4454d35ab5317ab66a04406f36f0c1dbc0b76")))
 
 ;; LSP
-(when (and (featurep! +lsp) (not (featurep! :tools lsp +eglot)))
-  (package! lsp-python-ms :pin "5d0c799099"))
+(when (and (featurep! +lsp)
+           (not (featurep! :tools lsp +eglot)))
+  (package! lsp-python-ms :pin "db6f2636da1037bda06d5a2d992c4857ce3ad9b0"))
 
 ;; Programming environment
-(package! anaconda-mode :pin "10299bd9ff")
+(package! anaconda-mode :pin "10299bd9ff38c4f0da1d892905d02ef828e7fdce")
 (when (featurep! :completion company)
-  (package! company-anaconda :pin "a31354ca8e"))
+  (package! company-anaconda :pin "da1566db41a68809ef7f91ebf2de28118067c89b"))
 
 ;; Environment management
-(package! pipenv :pin "b730bb509e")
-(package! pyvenv :pin "861998b6d1")
+(package! pipenv :pin "b730bb509e8b60af9f5ab1f1e6c3458d1d95d789")
+(package! pyvenv :pin "861998b6d157ae73b829f02a5a6c8a9118310831")
 (when (featurep! +pyenv)
-  (package! pyenv-mode :pin "aec6f2aa28"))
+  (package! pyenv-mode :pin "d191037fe62ed8d4fee5888845da3e2c386d8e89"))
 (when (featurep! +conda)
-  (package! conda :pin "335474e409"))
+  (package! conda :pin "9d0213020ff170b17e11b35cad40ac9a8bf30f4c"))
 (when (featurep! +poetry)
-  (package! poetry :pin "6dcc9d22ca"))
+  (package! poetry :pin "d876522e5af576d53c62b2838f85c9441fe62258"))
 
 ;; Testing frameworks
 (package! nose
@@ -35,5 +36,5 @@
 (package! python-pytest :pin "09ad688df207ee9b02c990d3897a9e2841931d97")
 
 ;; Import managements
-(package! pyimport :pin "a6f63cf7ed")
-(package! py-isort :pin "e67306f459")
+(package! pyimport :pin "a6f63cf7ed93f0c0f7c207e6595813966f8852b9")
+(package! py-isort :pin "e67306f459c47c53a65604e4eea88a3914596560")
