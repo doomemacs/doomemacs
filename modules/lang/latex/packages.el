beda7bb15f055887e88b6bad51e0b191fd24d772
Henrik Lissner <henrik@lissner.net>
2020-06-03 01:32:26 -0400

    Bump :lang latex
    emacs-straight/auctex@b233945 -> emacs-straight/auctex@6abf890
    alexeyr/company-auctex@48c42c5 -> alexeyr/company-auctex@9400a2e

--- modules/lang/latex/packages.el
+++ modules/lang/latex/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/latex/packages.el
 
-(package! auctex :pin "b233945ba2d0616851796f95c415aa79df1c2b66")
+(package! auctex :pin "6abf890a485b2ff734d8f87f38393f9b8f6bbbf6")
 (package! adaptive-wrap :pin "1810c0ee8d827dd502ddeaae5bd759d4811fcbce")
 (package! latex-preview-pane :pin "5297668a89996b50b2b62f99cba01cc544dbed2e")
 
@@ -16,6 +16,6 @@
 ;; Features according to other user selected options
 
 (when (featurep! :completion company)
-  (package! company-auctex :pin "48c42c58ce2f0e693301b0cb2d085055410c1b25")
+  (package! company-auctex :pin "9400a2ec7459dde8cbf1a5d50dfee4e300ed7e18")
   (package! company-reftex :pin "275ef708f08d3bf0eb30632148e5c6184eeaacdb")
   (package! company-math :pin "a796053590012e6a15c8b527b521ffc15d137bd0"))
