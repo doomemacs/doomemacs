ca99224ec986ce039fcd1d4f919b4e5c15d0ea31
Henrik Lissner <henrik@lissner.net>
2020-06-04 20:10:11 -0400

    Bump :lang haskell
    emacs-lsp/lsp-haskell@582fa27 -> emacs-lsp/lsp-haskell@17d7d4c
    haskell/haskell-mode@7032966 -> haskell/haskell-mode@41683c0
    jyp/dante@4955bc7 -> jyp/dante@7411904

--- modules/lang/haskell/packages.el
+++ modules/lang/haskell/packages.el
@@ -1,11 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/haskell/packages.el
 
-(package! haskell-mode :pin "7032966ee7")
+(package! haskell-mode :pin "41683c0e634bb3f54eac8747919a82132e1714fe")
 
 (when (featurep! +dante)
-  (package! dante :pin "4955bc7363")
-  (package! attrap :pin "4cf3e4a162"))
-(when (or (and (featurep! +lsp) (not (featurep! :tools lsp +eglot)))
+  (package! dante :pin "7411904bfbde25cdb986e001ec682593dcb7c5e3")
+  (package! attrap :pin "4cf3e4a16255997e7c3c39682a72866a0a37dd4b"))
+(when (or (and (featurep! +lsp)
+               (not (featurep! :tools lsp +eglot)))
           (featurep! +ghcide))
-  (package! lsp-haskell :pin "582fa27c88"))
+  (package! lsp-haskell :pin "17d7d4c6615b5e6c7442828720730bfeda644af8"))
