8848307fa6989d26fe59b72398b9e485a360a758
Maikol Solís <maikol-solis@users.noreply.github.com>
2020-06-09 13:53:18 -0600

    Update org-gcal
    Makes `org-gcal-post-at-point` work again.

--- modules/app/calendar/packages.el
+++ modules/app/calendar/packages.el
@@ -3,4 +3,4 @@
 
 (package! calfw :pin "03abce97620a4a7f7ec5f911e669da9031ab9088")
 (package! calfw-org :pin "03abce97620a4a7f7ec5f911e669da9031ab9088")
-(package! org-gcal :pin "2ee2b31547e6f4e33db70fb812d552e55d612fd6")
+(package! org-gcal :pin "744505832b34e07b44a5d97d8720b2d7492d7fc9")
