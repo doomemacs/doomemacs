6c8041947419854df7112ff6d39ffc6ba77de46e
Henrik Lissner <henrik@lissner.net>
2020-06-10 20:48:33 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@d5f0410 -> emacs-lsp/lsp-mode@5f3f984
    emacs-lsp/lsp-ui@c3e7a37 -> emacs-lsp/lsp-ui@d92cf83
    emacs-lsp/helm-lsp@6b5ce18 -> emacs-lsp/helm-lsp@5018af9
    
    Closes #3349

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -4,11 +4,12 @@
 (if (featurep! +eglot)
     (progn
       (package! eglot :pin "ac9239bed5e3bfbf057382d1a75cdfa23f2caddd")
-      (package! project :recipe (:host github :repo "emacs-straight/project")
+      (package! project
+        :recipe (:host github :repo "emacs-straight/project")
         :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
-  (package! lsp-mode :pin "d5f0410a88edcbcc183f5877a63b4896fd4f9941")
-  (package! lsp-ui :pin "c3e7a3759b52fe0f9c6f0f6668f1d6d88e4f784a")
+  (package! lsp-mode :pin "5f3f9848b2d4afc69049121c60126a6405447106")
+  (package! lsp-ui :pin "d92cf83d95c9ca177b735500ead88cf68dc2e2db")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "dce58b5509271bbedb53ba9d0278dcb563a43977"))
   (when (featurep! :completion helm)
-    (package! helm-lsp :pin "6b5ce182d7c94c62b55b8f7d0c7e643b2c30e560")))
+    (package! helm-lsp :pin "5018af9c709a783de1b9e101e07c948cceed67f1")))
