64b8b35bb9b9b456f9ad36819f4aebaf6abc9ad0
David Linus Briemann <dlb@mailbox.org>
2020-06-14 19:45:01 +0200

    update racket-mode to fix racket-xp-mode performance issue

--- modules/lang/racket/packages.el
+++ modules/lang/racket/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/racket/packages.el
 
-(package! racket-mode :pin "8f3b214a5ea06e6a2a9492e7d20b19badd0c3bdf")
+(package! racket-mode :pin "29afd2544a2f14bf29f3e542f6177579d0ae581f")
