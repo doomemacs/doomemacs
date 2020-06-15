468d731b141cbd88522719e1535e68e1547cdc1c
Justin Smestad <justin.smestad@gmail.com>
2020-06-15 09:12:02 -0600

    Update elixir-mode to support new sigil font locks

--- modules/lang/elixir/packages.el
+++ modules/lang/elixir/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/elixir/packages.el
 
 ;; +elixir.el
-(package! elixir-mode :pin "231291ecad")
+(package! elixir-mode :pin "670098a")
 (package! alchemist :pin "6f99367511")
 (when (featurep! :checkers syntax)
   (package! flycheck-credo :pin "e88f11ead5"))
