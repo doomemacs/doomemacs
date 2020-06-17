4aa98240dcfed71669a3117dfdc1280ce1eacf7c
Luis Osa <luis.osa.gdc@gmail.com>
2020-06-17 21:23:36 +0200

    Fix indentation

--- modules/lang/scala/packages.el
+++ modules/lang/scala/packages.el
@@ -5,4 +5,4 @@
 (package! scala-mode :pin "46bb948345")
 
 (when (featurep! +lsp)
-    (package! lsp-metals :pin "5468b638cd81b7d9ce9edc652c281b28bd775c23"))
+  (package! lsp-metals :pin "5468b638cd81b7d9ce9edc652c281b28bd775c23"))
