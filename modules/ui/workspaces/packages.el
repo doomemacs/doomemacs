2dd849e5f908c4189af2e96e280540271913b023
Henrik Lissner <henrik@lissner.net>
2020-06-17 17:31:58 -0400

    Bump :ui workspaces
    Bad-ptr/persp-mode.el@391a7dc -> hlissner/persp-mode.el@14325c1
    
    Until Bad-ptr/persp-mode.el#116 is merged.

--- modules/ui/workspaces/packages.el
+++ modules/ui/workspaces/packages.el
@@ -1,4 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/workspaces/packages.el
 
-(package! persp-mode :pin "391a7dc248c9c04b7ad424c696bdff578e14dd2c")
+(package! persp-mode
+  :recipe (:host github
+           :repo "hlissner/persp-mode.el"
+           :branch "remove-byte-compile")
+  :pin "14325c11f7a347717d7c3780f29b24a38c68fbfc")
