23780ae6c0887b88585eee21d7590b30a2472527
Henrik Lissner <henrik@lissner.net>
2020-06-17 14:20:49 -0400

    Bump :lang elixir
    elixir-editors/emacs-elixir@231291e -> elixir-editors/emacs-elixir@02a3922
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.
    
    Closes #3393

--- modules/lang/elixir/packages.el
+++ modules/lang/elixir/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/elixir/packages.el
 
 ;; +elixir.el
-(package! elixir-mode :pin "231291ecad")
-(package! alchemist :pin "6f99367511")
+(package! elixir-mode :pin "02a3922711b7dc6a25895df6d2bf00360fda45cc")
+(package! alchemist :pin "6f99367511ae209f8fe2c990779764bbb4ccb6ed")
 (when (featurep! :checkers syntax)
-  (package! flycheck-credo :pin "e88f11ead5"))
+  (package! flycheck-credo :pin "e88f11ead53805c361ec7706e44c3dfee1daa19f"))
