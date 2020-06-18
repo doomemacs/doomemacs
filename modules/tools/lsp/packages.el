305f57f62ce18dc4a980679a5d3cb5ad1e3cf107
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2020-06-18 23:45:02 +0200

    Remove forced project.el recipe hack
    Now that straight.el fixed the bug upstream there's no reason to force it anymore

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -4,9 +4,7 @@
 (if (featurep! +eglot)
     (progn
       (package! eglot :pin "ac9239bed5e3bfbf057382d1a75cdfa23f2caddd")
-      (package! project
-        :recipe (:host github :repo "emacs-straight/project")
-        :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
+      (package! project :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
   (package! lsp-mode :pin "5f3f9848b2d4afc69049121c60126a6405447106")
   (package! lsp-ui :pin "d92cf83d95c9ca177b735500ead88cf68dc2e2db")
   (when (featurep! :completion ivy)
