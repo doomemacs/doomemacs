5bacfd631110855e3d6f599fd7568989baaf64cc
Andrew Whatson <whatson@gmail.com>
2020-06-19 14:40:34 +1000

    Improve popup rule for geiser REPLs
    Fix the rule to properly match when multiple REPLs are opened.
    
    Add an explicit size to match other REPLs.

--- modules/lang/scheme/config.el
+++ modules/lang/scheme/config.el
@@ -25,7 +25,7 @@
       ("^\\*Geiser dbg\\*$"      :slot 1 :vslot -1)
       ("^\\*Geiser xref\\*$"     :slot 1 :vslot -1)
       ("^\\*Geiser documentation\\*$" :slot 2 :vslot 2 :select t :size 0.35)
-      ("^\\* [A-Za-z0-9_-]+ REPL \\*$" :quit nil :ttl nil)))
+      ("^\\* [A-Za-z0-9_-]+ REPL \\*" :size 0.3 :quit nil :ttl nil)))
   (map! :localleader
         :map scheme-mode-map
         "'"  #'geiser-mode-switch-to-repl
