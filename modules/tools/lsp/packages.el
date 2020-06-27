fe37bfc8e93e606606933cc905f301544ac7e016
Henrik Lissner <henrik@lissner.net>
2020-06-27 13:54:42 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@7b0b9c3 -> emacs-lsp/lsp-mode@029ec54
    emacs-lsp/lsp-ui@ce997c4 -> emacs-lsp/lsp-ui@1ee113c
    emacs-lsp/lsp-ivy@f6e3211 -> emacs-lsp/lsp-ivy@1f6d877
    
    Closes #3473

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -5,9 +5,9 @@
     (progn
       (package! eglot :pin "ac9239bed5e3bfbf057382d1a75cdfa23f2caddd")
       (package! project :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
-  (package! lsp-mode :pin "7b0b9c3785222ed89744a0d1376e764c30e950ac")
-  (package! lsp-ui :pin "ce997c4dabb494ec4aaa93373ae27cd4d5cd0a4d")
+  (package! lsp-mode :pin "029ec54f79aeeaee3b2683ea9112887d811d9cfa")
+  (package! lsp-ui :pin "1ee113cca78bcb680b9783d2d6253b2c54beeece")
   (when (featurep! :completion ivy)
-    (package! lsp-ivy :pin "f6e321187e773d7e5dfb215802fff5f308226cf9"))
+    (package! lsp-ivy :pin "1f6d8777a3445d23678216fe1f46475de5f4fb0e"))
   (when (featurep! :completion helm)
     (package! helm-lsp :pin "5018af9c709a783de1b9e101e07c948cceed67f1")))
