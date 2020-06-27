d94591b7f07d328250f124aa89d111df416e95ec
brotzeit <brotzeitmacher@gmail.com>
2020-06-27 14:47:13 +0200

    Bump :lang rust
    brotzeit/rustic@f7a94c4 -> brotzeit/rustic@7c0e113

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rustic :pin "f7a94c4f914f4037fb84d50e91b3de90f606efda")
+(package! rustic :pin "7c0e113d27ba414499f16c1667f58b28b35ffd0c")
 (unless (featurep! +lsp)
   (package! racer :pin "a0bdf778f01e8c4b8a92591447257422ac0b455b"))
