e5862c697116831c5413b02ab392a2dd3a7cc6de
Henrik Lissner <henrik@lissner.net>
2020-07-12 22:15:25 -0400

    Bump :tools debugger
    emacs-lsp/dap-mode@f52fa43 -> emacs-lsp/dap-mode@f0ed63e
    realgud/realgud@24c02f0 -> realgud/realgud@f0a3676
    tumashu/posframe@c445902 -> tumashu/posframe@922e4d2

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/debugger/packages.el
 
-(when (package! realgud :pin "24c02f07c4ae6610278da1f04a2479dc3d9b6bdf")
+(when (package! realgud :pin "f0a3676a7e09e88d94afbe830808b76de2664eb0")
   (when (featurep! :lang javascript)
     (package! realgud-trepan-ni :pin "6e9cac5e8097018aadf41c88de541168036cc227")))
 
 (when (featurep! +lsp)
-  (package! dap-mode :pin "f52fa4327147bccb7c93300d5f522e2fdcff1a70")
-  (package! posframe :pin "c4459028fbe6740315ff1ed6f37e8c4decd3d050"))
+  (package! dap-mode :pin "f0ed63e183dcd2e7f9eebe4f74d2c13808318bb9")
+  (package! posframe :pin "922e4d239f7a083213d856de67a9686a091b1e27"))
