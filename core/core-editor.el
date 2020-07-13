doomemacs/doomemacs

commit 8e83a8e8422c581a0d2d1e627b28ae5a027e417b

Fix #3491: void-function doom-set-jump-h

core/core-editor.el
@@ -312,6 +312,7 @@ files, so we replace calls to `pp' with the much faster `prin1'."
 (use-package! better-jumper
   :hook (doom-first-input . better-jumper-mode)
   :hook (better-jumper-post-jump . recenter)
+  :command doom-set-jump-a doom-set-jump-maybe-a doom-set-jump-h
   :preface
   ;; REVIEW Suppress byte-compiler warning spawning a *Compile-Log* buffer at
   ;; startup. This can be removed once gilbertw1/better-jumper#2 is merged.

commit c348b51e5f1162bdfd7ba769a81e3b93ed3b5d07

Fix typo regression from 8e83a8e84

core/core-editor.el
@@ -312,7 +312,7 @@ files, so we replace calls to `pp' with the much faster `prin1'."
 (use-package! better-jumper
   :hook (doom-first-input . better-jumper-mode)
   :hook (better-jumper-post-jump . recenter)
-  :command doom-set-jump-a doom-set-jump-maybe-a doom-set-jump-h
+  :commands doom-set-jump-a doom-set-jump-maybe-a doom-set-jump-h
   :preface
   ;; REVIEW Suppress byte-compiler warning spawning a *Compile-Log* buffer at
   ;; startup. This can be removed once gilbertw1/better-jumper#2 is merged.