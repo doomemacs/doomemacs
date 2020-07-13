129566e817751ed6322b53b7c6ba39e226ab3e32
Henrik Lissner <henrik@lissner.net>
2020-07-12 22:45:08 -0400

    Bump :lang cc
    MaskRay/emacs-ccls@0fd0b41 -> MaskRay/emacs-ccls@b8e2f4d
    Andersbakken/rtags@3a12552 -> Andersbakken/rtags@080cb0e

--- modules/lang/cc/packages.el
+++ modules/lang/cc/packages.el
@@ -19,7 +19,7 @@
 (if (featurep! +lsp)
     (unless (featurep! :tools lsp +eglot)
       ;; ccls package is necessary only for lsp-mode.
-      (package! ccls :pin "0fd0b410533f8be8d1a834834bae76ce7cbf7c84"))
+      (package! ccls :pin "b8e2f4d9b5bed5b5e8b387ac8e43eff723120b80"))
   (when (package! irony :pin "5f75fc0c9274f4622470e2324e2f4457087aa643")
     (package! irony-eldoc :pin "73e79a89fad982a2ba072f2fcc1b4e41f0aa2978")
     (when (featurep! :checkers syntax)
@@ -27,7 +27,7 @@
     (when (featurep! :completion company)
       (package! company-irony :pin "b44711dfce445610c1ffaec4951c6ff3882b216a")
       (package! company-irony-c-headers :pin "72c386aeb079fb261d9ec02e39211272f76bbd97")))
-  (when (package! rtags :pin "3a125528209b2674dac46ff80698d481870f5dc0")
+  (when (package! rtags :pin "080cb0e6b025b5d3d40fe9f7aecc791c0ea53f36")
     (when (featurep! :completion ivy)
       (package! ivy-rtags))
     (when (featurep! :completion helm)
