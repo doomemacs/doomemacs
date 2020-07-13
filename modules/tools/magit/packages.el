754ac13cae61d0bcace5f8eeb167dd9f476c4824
Henrik Lissner <henrik@lissner.net>
2020-07-12 22:37:23 -0400

    Bump :tools magit
    charignon/github-review@50c6bcc -> charignon/github-review@fab440a
    magit/forge@048efbb -> magit/forge@d1346f4
    magit/magit@0746bf1 -> magit/magit@312791e

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "0746bf1bacfe896d3917ccc19c7fb9d95c18b1e9")
+(when (package! magit :pin "312791e50bb56797c1ee36f4aa3fdc0885711b25")
   (when (featurep! +forge)
-    (package! forge :pin "048efbba83b1df591de0487202ff968250ea4fc5"))
+    (package! forge :pin "d1346f4df60c07ab2a8c0ead203e25d1ae65968d"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "a0e5d1f3c7dfcb4f18c1b0d57f1746a4872df5c6")
-  (package! github-review :pin "50c6bcc7cf4d7193577b3f74eea4dd72f2b7795b")
+  (package! github-review :pin "fab440aeae4fbf6a8192fd11795052e9eb5d27d1")
   (when (featurep! :editor evil +everywhere)
     (package! evil-magit :pin "88dc26ce59dbf4acb4e2891c79c4bd329553ba56")))
