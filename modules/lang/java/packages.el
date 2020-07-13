c73102dc3fd9a35d9552875a4a835208e8357d57
Henrik Lissner <henrik@lissner.net>
2020-07-12 22:42:27 -0400

    Bump :lang java
    mopemope/meghanada-emacs@70bfbf5 -> mopemope/meghanada-emacs@e119c7b
    emacs-eclim/emacs-eclim@23f5b29 -> emacs-eclim/emacs-eclim@222ddd4
    emacs-lsp/lsp-java@fe11824 -> emacs-lsp/lsp-java@811760a
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/lang/java/packages.el
+++ modules/lang/java/packages.el
@@ -1,16 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/java/packages.el
 
-(package! android-mode :pin "d5332e339a")
-(package! groovy-mode :pin "cafdd98e06")
+(package! android-mode :pin "d5332e339a1f5e30559a53feffb8442ca79265d6")
+(package! groovy-mode :pin "cafdd98e06a3bbff213f3ccb163de2c42d412b66")
 
 (when (featurep! +meghanada)
-  (package! meghanada :pin "70bfbf553c"))
+  (package! meghanada :pin "e119c7b3271281d60892b80a0cc6488503baf38f"))
 
 (when (featurep! +eclim)
-  (package! eclim :pin "23f5b294f8")
+  (package! eclim :pin "222ddd48fcf0ee01592dec77c58e0cf3f2ea1100")
   (when (featurep! :completion company)
-    (package! company-emacs-eclim :pin "23f5b294f8")))
+    (package! company-emacs-eclim :pin "222ddd48fcf0ee01592dec77c58e0cf3f2ea1100")))
 
 (when (featurep! +lsp)
-  (package! lsp-java :pin "fe118246903b9cfa9217f442f2b1b6524916253b"))
+  (package! lsp-java :pin "811760ad89a29939c28f47d0925f58d9eeea9fa3"))
