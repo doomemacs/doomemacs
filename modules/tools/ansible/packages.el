9a071b926ceeb8236c5ba4638c8280ac07416fde
Sean Farley <sean@farley.io>
2020-07-17 18:07:15 -0700

    Bump :tools ansible
    k1LoW/emacs-ansible@c6532e5 -> k1LoW/emacs-ansible@b5ef594

--- modules/tools/ansible/packages.el
+++ modules/tools/ansible/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/ansible/packages.el
 
-(package! ansible :recipe (:nonrecursive t) :pin "c6532e52161a381ed3dddfeaa7c92ae636d3f052")
+(package! ansible :recipe (:nonrecursive t) :pin "b5ef59406604bc5027f4d816d90e633feef0149c")
 (package! ansible-doc :pin "86083a7bb2ed0468ca64e52076b06441a2f8e9e0")
 (package! jinja2-mode :pin "4540f99a3e363403a633587e05a9707605c16473")
 (package! yaml-mode :pin "34648f2502f52f4744d62758fa381fa35db1da49")
