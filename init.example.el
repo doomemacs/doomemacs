a282730ce9698db7422835bfec8aec82538f9c0c
Paul A. Patience <paul@apatience.com>
2020-07-22 11:04:34 -0400

    Fix typos and reword docstrings

--- init.example.el
+++ init.example.el
@@ -40,7 +40,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;pretty-code       ; ligatures or substitute text with pretty symbols
-       ;;tabs              ; an tab bar for Emacs
+       ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
