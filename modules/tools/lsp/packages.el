5923208c2c0f3f2a2c298b3f8872002fabbdf0d1
Henrik Lissner <henrik@lissner.net>
2020-07-22 16:19:28 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@5ef8c1f -> emacs-lsp/lsp-mode@f04d018
    emacs-lsp/lsp-ui@7d53264 -> emacs-lsp/lsp-ui@1f3e970
    joaotavora/eglot@6d96601 -> joaotavora/eglot@e3ce64a

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -3,10 +3,10 @@
 
 (if (featurep! +eglot)
     (progn
-      (package! eglot :pin "6d9660152b6d86931a6b1dd25f81a354b77a5ae8")
+      (package! eglot :pin "e3ce64adc09922a87936617484e48cbecc5db729")
       (package! project :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
-  (package! lsp-mode :pin "5ef8c1f586663ecf8a93a14248ddaed900b5b59b")
-  (package! lsp-ui :pin "7d5326430eb88a58e111cb22ffa42c7d131e5052")
+  (package! lsp-mode :pin "f04d0182f08775529509cccc6079b5a8bdd77f9c")
+  (package! lsp-ui :pin "1f3e9700395f4fae024ca45ca64c8d70e99b39d2")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "4cdb739fc2bc47f7d4dcad824f9240c70c4cb37d"))
   (when (featurep! :completion helm)
