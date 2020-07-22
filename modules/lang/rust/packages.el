2cf35d7fcf56c18249b771a8a66816c68dc6cd2c
Henrik Lissner <henrik@lissner.net>
2020-07-22 00:24:51 -0400

    Bump :lang rust
    brotzeit/rustic@7c0e113 -> brotzeit/rustic@0ec0688

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rustic :pin "7c0e113d27ba414499f16c1667f58b28b35ffd0c")
+(package! rustic :pin "0ec0688c83cdf70be5eaaeacd96b067125fe968e")
 (unless (featurep! +lsp)
   (package! racer :pin "a0bdf778f01e8c4b8a92591447257422ac0b455b"))
