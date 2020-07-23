ddf2a1a2863d44628d2473f2a12e5440050a7b48
Henrik Lissner <henrik@lissner.net>
2020-07-23 01:25:12 -0400

    Bump :lang dart
    emacs-lsp/lsp-dart@10b898e -> emacs-lsp/lsp-dart@a06fc74
    amake/flutter.el@293b722 -> amake/flutter.el@78b3c57
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/lang/dart/packages.el
+++ modules/lang/dart/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/dart/packages.el
 
-(package! dart-mode :pin "04fcd649f1")
+(package! dart-mode :pin "04fcd649f19d49390079fbf2920a10bf37f6a634")
 
 (when (featurep! +lsp)
-  (package! lsp-dart :pin "10b898ebeb60e00395bbc72495afaf2f22b4fc9f"))
+  (package! lsp-dart :pin "a06fc74e4cb0a468b7166dd9a7072ef5e9bdcc34"))
 
 (when (featurep! +flutter)
-  (package! flutter :pin "293b7225b9")
-  (package! hover :pin "6f9ed1a651"))
+  (package! flutter :pin "78b3c572584c95220a40934bd67fd5e3fb096f96")
+  (package! hover :pin "6f9ed1a6517e3a43ef2deafc2f86c70b2abce008"))
