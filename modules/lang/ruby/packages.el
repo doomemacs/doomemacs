6e60091403402ed13cc13f724c860706fb465011
Henrik Lissner <henrik@lissner.net>
2020-07-23 14:46:37 -0400

    Bump :lang ruby
    dgutov/robe@68503b3 -> dgutov/robe@082da38
    nonsequitur/inf-ruby@41e5ed3 -> nonsequitur/inf-ruby@f3c927c
    pezra/rspec-mode@9a2a9d2 -> pezra/rspec-mode@f1029ca
    senny/rvm.el@134497b -> senny/rvm.el@081d517

--- modules/lang/ruby/packages.el
+++ modules/lang/ruby/packages.el
@@ -6,13 +6,13 @@
 (package! yard-mode :pin "ba74a47463b0320ae152bd42a7dd7aeecd7b5748")
 
 ;; REPL
-(package! inf-ruby :pin "41e5ed3a886fca56990486f1987bb3bae0dbd54b")
+(package! inf-ruby :pin "f3c927c1b917a20ce6b2228d480db43171aadd9b")
 (when (featurep! :completion company)
   (package! company-inf-ruby :pin "fe3e4863bc971fbb81edad447efad5795ead1b17"))
 
 ;; Programming environment
 (package! rubocop :pin "03bf15558a6eb65e4f74000cab29412efd46660e")
-(package! robe :pin "68503b32bb3a005787ecb7a7fdeb3bb4a2317e2b")
+(package! robe :pin "082da38797d247f9c64568ad712a345e041d5c84")
 
 ;; Project tools
 (package! bundler :pin "43efb6be4ed118b06d787ce7fbcffd68a31732a7")
@@ -22,12 +22,12 @@
 (when (featurep! +rbenv)
   (package! rbenv :pin "2ea1a5bdc1266caef1dd77700f2c8f42429b03f1"))
 (when (featurep! +rvm)
-  (package! rvm :pin "134497bc460990c71ab8fa75431156e62c17da2d"))
+  (package! rvm :pin "081d5173158054c6d0780b9462c74c5697eac1fc"))
 (when (featurep! +chruby)
   (package! chruby :pin "42bc6d521f832eca8e2ba210f30d03ad5529788f"))
 
 ;; Testing frameworks
-(package! rspec-mode :pin "9a2a9d2935ae17b8570485bdea7c347533b464f6")
+(package! rspec-mode :pin "f1029cad2a8d9fd096e9a0f1ae9e7d7e2c8bd1ec")
 (package! minitest :pin "ddd152c990a528ad09a696bfad23afa4330ea4d7")
 
 ;; Rails
