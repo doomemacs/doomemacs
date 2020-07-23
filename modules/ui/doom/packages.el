a6fa47dfc081c8b3335479578c26581ef0e2a77a
Maikol Solís <maikol-solis@users.noreply.github.com>
2020-07-23 08:54:29 -0600

    Bump ui: doom
    hlissner/emacs-doom-themes@7307dc6 -> hlissner/emacs-doom-themes@294a432

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "7307dc6ca2faf408aade90281179678a78193eec")
+(package! doom-themes :pin "294a4326dfaae417bb4e4fabddf3847b265a61ac")
 (package! solaire-mode :pin "25d1aafc03e3c7f9487532464dbffacce10d0c5d")
