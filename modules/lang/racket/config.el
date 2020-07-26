e6979166ee987e4d3a8ed6fce99964434dcec3dc
Henrik Lissner <henrik@lissner.net>
2020-07-26 15:08:28 -0400

    Disable flycheck in racket-xp-mode
    Plus minor refactors.
    
    Closes #3594
    
    Co-authored-by: sorawee <sorawee@users.noreply.github.com>

--- modules/lang/racket/config.el
+++ modules/lang/racket/config.el
@@ -28,39 +28,48 @@
              #'highlight-quoted-mode)
 
   (when (featurep! +xp)
-    (add-hook 'racket-mode-hook #'racket-xp-mode))
+    (add-hook 'racket-mode-local-vars-hook #'racket-xp-mode)
+    ;; Both flycheck and racket-xp produce error popups, but racket-xp's are
+    ;; higher quality so disable flycheck's:
+    (when (featurep! :checkers syntax)
+      (add-hook! 'racket-xp-mode-hook
+        (defun +racket-disable-flycheck-h ()
+          (flycheck-disable-checker 'racket)))))
 
   (unless (or (featurep! :editor parinfer)
               (featurep! :editor lispy))
     (add-hook 'racket-mode-hook #'racket-smart-open-bracket-mode))
 
-  (map! :localleader
-        :map racket-mode-map
-        "a" #'racket-align
-        "A" #'racket-unalign
-        "f" #'racket-fold-all-tests
-        "F" #'racket-unfold-all-tests
-        "h" #'racket-doc
-        "i" #'racket-unicode-input-method-enable
-        "l" #'racket-logger
-        "o" #'racket-profile
-        "p" #'racket-cycle-paren-shapes
-        "r" #'racket-run
-        "R" #'racket-run-and-switch-to-repl
-        "t" #'racket-test
-        "u" #'racket-backward-up-list
-        "y" #'racket-insert-lambda
-        (:prefix ("m" . "macros")
+  (map! (:map racket-xp-mode-map
+         [remap next-error]     #'racket-xp-next-error
+         [remap previous-error] #'racket-xp-previous-error)
+        (:localleader
+         :map racket-mode-map
+         "a" #'racket-align
+         "A" #'racket-unalign
+         "f" #'racket-fold-all-tests
+         "F" #'racket-unfold-all-tests
+         "h" #'racket-doc
+         "i" #'racket-unicode-input-method-enable
+         "l" #'racket-logger
+         "o" #'racket-profile
+         "p" #'racket-cycle-paren-shapes
+         "r" #'racket-run
+         "R" #'racket-run-and-switch-to-repl
+         "t" #'racket-test
+         "u" #'racket-backward-up-list
+         "y" #'racket-insert-lambda
+         (:prefix ("m" . "macros")
           "d" #'racket-expand-definition
           "e" #'racket-expand-last-sexp
           "r" #'racket-expand-region
           "a" #'racket-expand-again)
-        (:prefix ("g" . "goto")
+         (:prefix ("g" . "goto")
           "b" #'racket-unvisit
           "d" #'racket-visit-definition
           "m" #'racket-visit-module
           "r" #'racket-open-require-path)
-        (:prefix ("s" . "send")
+         (:prefix ("s" . "send")
           "d" #'racket-send-definition
           "e" #'racket-send-last-sexp
-          "r" #'racket-send-region)))
+          "r" #'racket-send-region))))
