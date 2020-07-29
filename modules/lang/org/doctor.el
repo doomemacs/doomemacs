d006f76dea8aa90e985965d782898f7229e2f638
wedens <kirill.wedens@gmail.com>
2020-07-29 09:50:22 +0700

    org-roam: check for sqlite3 executable in doctor

--- modules/lang/org/doctor.el
+++ modules/lang/org/doctor.el
@@ -6,5 +6,7 @@
     (warn! "Couldn't find gnuplot. org-plot/gnuplot will not work")))
 
 (when (featurep! +roam)
+  (unless (executable-find "sqlite3")
+    (warn! "Couldn't find the sqlite3 executable. org-roam will not work."))
   (unless (executable-find "dot")
     (warn! "Couldn't find the dot executable (from graphviz). org-roam will not be able to generate graph visuallizations.")))
