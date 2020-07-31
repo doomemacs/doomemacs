38390be0dbabcbf1cf64b943d891cd07280737cd
Henrik Lissner <henrik@lissner.net>
2020-07-31 16:05:56 -0400

    Bump :lang markdown
    jrblevin/markdown-mode@770e3aa -> jrblevin/markdown-mode@fa9fa20
    seagle0128/grip-mode@9615c47 -> seagle0128/grip-mode@52768a0

--- modules/lang/markdown/packages.el
+++ modules/lang/markdown/packages.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/markdown/packages.el
 
-(package! markdown-mode :pin "770e3aa7cdfc9d731119b9425e8a7c8ac6dd5f93")
+(package! markdown-mode :pin "fa9fa20e3236006c2cf278209356f60cc4175120")
 (package! markdown-toc :pin "9565eeaa1d26bc0ab83eb65bd30470888f724044")
 (package! edit-indirect :pin "935ded353b9ed3da67bc61abf245c21b58d88864")
 
 (when (featurep! +grip)
-  (package! grip-mode :pin "9615c4774727a719d38313a679d70f2a2c6aca68"))
+  (package! grip-mode :pin "52768a0187f8ce9b42010dc45bbc432551aeccee"))
 
 (when (featurep! :editor evil +everywhere)
   (package! evil-markdown
