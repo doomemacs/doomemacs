de343fd31483ebcb3967af9e2415bf15520fcfaf
Henrik Lissner <henrik@lissner.net>
2020-08-01 15:57:42 -0400

    lang/csharp: add csproj-mode package
    A major mode for .NET *.proj files

--- modules/lang/csharp/packages.el
+++ modules/lang/csharp/packages.el
@@ -2,6 +2,7 @@
 ;;; lang/csharp/packages.el
 
 (package! csharp-mode :pin "48851778e0f01a2b0395e054e418a1d8a1687a06")
+(package! csproj-mode :pin "a7f0f4610c976a28c41b9b8299892f88b5d0336c")
 (unless (featurep! +lsp)
   (package! omnisharp :pin "e658a18a762438c3e1737612737b05d02a21ca2a"))
 (when (featurep! +unity)
