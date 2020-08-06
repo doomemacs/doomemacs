c88eb0fff813f0025b7cb783ccabd1a4b27198c9
Henrik Lissner <henrik@lissner.net>
2020-08-05 20:51:47 -0400

    Bump :lang crystal
    crystal-lang-tools/emacs-crystal-mode@2428b01 -> crystal-lang-tools/emacs-crystal-mode@f9e4db1
    
    We're also transitioning from abbreviated SHA1 hashes to full ones. See
    45cdfb125 for why.

--- modules/lang/crystal/packages.el
+++ modules/lang/crystal/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/crystal/packages.el
 
-(package! crystal-mode :pin "2428b01624")
-(package! inf-crystal :pin "02007b2a2a")
+(package! crystal-mode :pin "f9e4db16ff9fdc6a296363aa35d19cfb4926e472")
+(package! inf-crystal :pin "02007b2a2a3bea44902d7c83c4acba1e39d278e3")
 (when (featurep! :checkers syntax)
-  (package! flycheck-crystal :pin "2428b01624")
-  (package! flycheck-ameba :pin "0c4925ae0e"))
+  (package! flycheck-crystal :pin "f9e4db16ff9fdc6a296363aa35d19cfb4926e472")
+  (package! flycheck-ameba :pin "0c4925ae0e998818326adcb47ed27ddf9761c7dc"))
