461b337734e7b5dc2140b462c23d1177532c6d0c
Henrik Lissner <henrik@lissner.net>
2020-08-06 17:45:39 -0400

    Bump :term eshell
    szermatt/emacs-bash-completion@96ce14a -> szermatt/emacs-bash-completion@d47edb9
    
    v3.1 release of bash-completion

--- modules/term/eshell/packages.el
+++ modules/term/eshell/packages.el
@@ -10,4 +10,4 @@
 (unless IS-WINDOWS
   (when (featurep! :completion company)
     (package! fish-completion :pin "10384881817b5ae38cf6197a077a663420090d2c")
-    (package! bash-completion :pin "96ce14af9674f3e605bacca87abc0c23b8f13cd5")))
+    (package! bash-completion :pin "d47edb9d4142a22746009c1f680df93a4fefd107")))
