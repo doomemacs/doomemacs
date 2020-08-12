ca7a18f3c6026519bd14e212efdbb05b50b51d12
Henrik Lissner <henrik@lissner.net>
2020-08-12 14:38:13 -0400

    Bump :lang java
    Groovy-Emacs-Modes/groovy-emacs-modes@cafdd98 -> Groovy-Emacs-Modes/groovy-emacs-modes@26da902
    emacs-lsp/lsp-java@811760a -> emacs-lsp/lsp-java@2600162
    
    Fixes #3737

--- modules/lang/java/packages.el
+++ modules/lang/java/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/java/packages.el
 
 (package! android-mode :pin "d5332e339a1f5e30559a53feffb8442ca79265d6")
-(package! groovy-mode :pin "cafdd98e06a3bbff213f3ccb163de2c42d412b66")
+(package! groovy-mode :pin "26da902d1158c0312628d57578109be54eca2415")
 
 (when (featurep! +meghanada)
   (package! meghanada :pin "e119c7b3271281d60892b80a0cc6488503baf38f"))
@@ -13,4 +13,4 @@
     (package! company-emacs-eclim :pin "222ddd48fcf0ee01592dec77c58e0cf3f2ea1100")))
 
 (when (featurep! +lsp)
-  (package! lsp-java :pin "811760ad89a29939c28f47d0925f58d9eeea9fa3"))
+  (package! lsp-java :pin "260016236fa0520b5b6ec7f51ca2086288524cba"))
