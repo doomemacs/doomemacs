ed8dca3c6949cc5a72fb3f8799fdae2b3c2166a3
Henrik Lissner <henrik@lissner.net>
2020-08-14 13:34:17 -0400

    Bump :lang ess
    emacs-ess/ESS@964bf64 -> emacs-ess/ESS@3c2fb63
    
    Fix unexpected '>' in ">" error in ESS R repl.

--- modules/lang/ess/packages.el
+++ modules/lang/ess/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/ess/packages.el
 
-(package! ess :pin "964bf64d8bb3c6dd710cd8f2259921099a50456a")
+(package! ess :pin "3c2fb63a2876362699eec922551db9f4685c2fc6")
 (package! ess-R-data-view :pin "d6e98d3ae1e2a2ea39a56eebcdb73e99d29562e9")
 (package! polymode :pin "3284ff10017d280ba82f27dc20fe5223b0df709c")
 (package! poly-R :pin "51ffeb6ec45dd44eafa4d22ad2d6150cc4b248fc")
