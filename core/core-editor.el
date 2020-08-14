9cab6a6fd9e43b6e95dbfa31a65b7119b7b11b0f
Henrik Lissner <henrik@lissner.net>
2020-08-13 22:46:51 -0400

    Fix #3723: auto-revert on window/buffer switch

--- core/core-editor.el
+++ core/core-editor.el
@@ -199,7 +199,8 @@ possible."
   (defun doom-auto-revert-buffer-h ()
     "Auto revert current buffer, if necessary."
     (unless (or auto-revert-mode (active-minibuffer-window))
-      (auto-revert-handler)))
+      (let ((auto-revert-mode t))
+        (auto-revert-handler))))
 
   (defun doom-auto-revert-buffers-h ()
     "Auto revert stale buffers in visible windows, if necessary."
