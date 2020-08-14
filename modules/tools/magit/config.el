77f9cc53ae2d258ec0a3dd09cf4e4bb88f042a7e
Henrik Lissner <henrik@lissner.net>
2020-08-14 16:51:30 -0400

    magit-revision-insert-related-refs = nil

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -22,8 +22,11 @@ For example, diffs and log buffers. Accepts `left', `right', `up', and `down'.")
         magit-diff-refine-hunk t ; show granular diffs in selected hunk
         ;; Don't autosave repo buffers. This is too magical, and saving can
         ;; trigger a bunch of unwanted side-effects, like save hooks and
-        ;; formatters. Trust us to know what we're doing.
-        magit-save-repository-buffers nil)
+        ;; formatters. Trust the user to know what they're doing.
+        magit-save-repository-buffers nil
+        ;; Don't display parent/related refs in commit buffers; they are rarely
+        ;; helpful and only add to runtime costs.
+        magit-revision-insert-related-refs nil)
   (add-hook 'magit-process-mode-hook #'goto-address-mode)
 
   (defadvice! +magit-revert-repo-buffers-deferred-a (&rest _)
