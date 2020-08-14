73c95bb1ad7dcf6e677596fd864467942e3ffbad
Henrik Lissner <henrik@lissner.net>
2020-08-13 22:39:19 -0400

    Optimize doom-visible-buffers

--- core/autoload/buffers.el
+++ core/autoload/buffers.el
@@ -179,9 +179,11 @@ If DERIVED-P, test with `derived-mode-p', otherwise use `eq'."
 ;;;###autoload
 (defun doom-visible-buffers (&optional buffer-list)
   "Return a list of visible buffers (i.e. not buried)."
-  (if buffer-list
-      (cl-remove-if-not #'get-buffer-window buffer-list)
-    (delete-dups (mapcar #'window-buffer (window-list)))))
+  (let ((buffers (delete-dups (mapcar #'window-buffer (window-list)))))
+    (if buffer-list
+        (cl-delete-if (lambda (b) (memq b buffer-list))
+                      buffers)
+      (delete-dups buffers))))
 
 ;;;###autoload
 (defun doom-buried-buffers (&optional buffer-list)
