8fa5b9406c93c27ee9564eb8773314a0ff8de047
Henrik Lissner <henrik@lissner.net>
2020-08-16 21:12:56 -0400

    Fix #3763: add let-alist to straight-built-in-pseudo-packages
    It is included with Emacs 26 and onwards, and was recently removed from
    emacs-mirror anyhow.

--- core/core-packages.el
+++ core/core-packages.el
@@ -101,6 +101,10 @@ uses a straight or package.el command directly).")
       ;; HACK Disable native-compilation for some troublesome files
       comp-deferred-compilation-black-list '("/evil-collection-vterm\\.el$"))
 
+(with-eval-after-load 'straight
+  ;; `let-alist' is built into Emacs 26 and onwards
+  (add-to-list 'straight-built-in-pseudo-packages 'let-alist))
+
 (defadvice! doom--read-pinned-packages-a (orig-fn &rest args)
   "Read `:pin's in `doom-packages' on top of straight's lockfiles."
   :around #'straight--lockfile-read-all
