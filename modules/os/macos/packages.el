f454d5fac43c0e9fa15525308d84a779d090be58
Henrik Lissner <henrik@lissner.net>
2020-08-20 21:40:06 -0400

    os/macos: add packages.el

---
+++ modules/os/macos/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; os/macos/packages.el
+
+(package! osx-trash :pin "0f1dc052d0a750b8c75f14530a4897f5d4324b4e")
+(package! ns-auto-titlebar :pin "60273e764bf8d95abc40dd2fdc23af87ea9ee33b")
