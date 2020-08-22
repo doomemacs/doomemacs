bc118042478a637299655352ce390dad97485a6c
Henrik Lissner <henrik@lissner.net>
2020-08-21 20:07:26 -0400

    Bump :tools pdf
    politza/pdf-tools@c510442 -> flatwhatson/pdf-tools@86db180
    
    Moved to flatwhatson's fork, which fixes
    politza/pdf-tools#588 (void-function errors on gccemacs).

--- modules/tools/pdf/packages.el
+++ modules/tools/pdf/packages.el
@@ -1,4 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/pdf/packages.el
 
-(package! pdf-tools :pin "c510442ab89c8a9e9881230eeb364f4663f59e76")
+(package! pdf-tools
+  ;; REVIEW waiting on politza/pdf-tools#588
+  :recipe (:host github
+           :repo "flatwhatson/pdf-tools"
+           :branch "fix-macros")
+  :pin "86db180be75254072dee5ea1611a4e8b9c0648de")
