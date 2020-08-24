af35e56abba155976046f2cb68338d5744767a87
Henrik Lissner <henrik@lissner.net>
2020-08-23 21:34:18 -0400

    Bump :completion
    + :completion company
      company-mode/company-mode@54f60ef -> company-mode/company-mode@4462e7d
      sebastiencs/company-box@20384f0 -> sebastiencs/company-box@d8f71fd
    + :completion helm
      emacs-helm/helm@e9a1e53 -> emacs-helm/helm@d2fbf66

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/company/packages.el
 
-(package! company :pin "54f60ef523878c4d332f29df380f36cf2f165935")
+(package! company :pin "4462e7d399c3d2e1ea1f5d018f4537f1f3d5acfc")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (featurep! +childframe)
-  (package! company-box :pin "20384f0e382c063173b9d863344b1b23bc1e4954"))
+  (package! company-box :pin "d8f71fde7a3301a62f9f6f627aa51610a294f4df"))
