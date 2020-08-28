b3077c96258cd8569fb1523251b5823199e2433a
Henrik Lissner <henrik@lissner.net>
2020-08-28 04:25:55 -0400

    Bump :emacs dired
    ralesi/ranger.el@d7c1837 -> ralesi/ranger.el@caf75f0
    
    See #2824

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -6,7 +6,7 @@
 (package! diff-hl :pin "2281a89a3ddc6616073da6f190dda08d23b18ba6")
 (package! dired-rsync :pin "bfd5c155be1cb6b71c83e5f41116c81b6532b6d5")
 (when (featurep! +ranger)
-  (package! ranger :pin "d7c18370981c9e585bc0fb78f7e55033457ca643"))
+  (package! ranger :pin "caf75f0060e503af078c7e5bb50d9aaa508e6f3e"))
 (when (featurep! +icons)
   (package! all-the-icons-dired :pin "fc2dfa1e9eb8bf1c402a675e7089638d702a27a5"))
 (package! fd-dired :pin "5622041068d5fa2f299dbc8aa91fece0ba260086")
