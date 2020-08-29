0ef8f0031fc6ded2e87027d80bf279d418df78f8
t-e-r-m <newenewen@tutanota.com>
2020-08-29 18:45:31 +0530

    configure dired-aux variables
    `dired-create-destination-dirs` : set to "ask", non-existent directories will be
    created when copying, if the user consents.
    `dired-vc-rename-file` : rename file using the vc system, if present

--- modules/emacs/dired/config.el
+++ modules/emacs/dired/config.el
@@ -168,6 +168,10 @@ we have to clean it up ourselves."
   (global-set-key [remap find-dired] #'fd-dired)
   (set-popup-rule! "^\\*F\\(?:d\\|ind\\)\\*$" :ignore t))
 
+(use-package! dired-aux
+  :config
+  (setq dired-create-destination-dirs 'ask
+        dired-vc-rename-file t))
 
 ;;;###package dired-git-info
 (map! :after dired
