5de263f6a490bb2e806b47f41dc3508adc59f325
Henrik Lissner <henrik@lissner.net>
2020-09-02 13:54:12 -0400

    Bump :lang common-lisp
    joaotavora/sly@155cb06 -> joaotavora/sly@34239bc
    
    And chain sly packages together, so disabling sly will disable them all.

--- modules/lang/common-lisp/packages.el
+++ modules/lang/common-lisp/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/common-lisp/packages.el
 
-(package! sly :pin "155cb0655e037477b592f8bde9c80022427293e5")
-(package! sly-macrostep :pin "5113e4e926cd752b1d0bcc1508b3ebad5def5fad")
-(package! sly-repl-ansi-color :pin "b9cd52d1cf927bf7e08582d46ab0bcf1d4fb5048")
+(when (package! sly :pin "34239bc5954ef41a4db49144925290dc794a6c4d")
+  (package! sly-macrostep :pin "5113e4e926cd752b1d0bcc1508b3ebad5def5fad")
+  (package! sly-repl-ansi-color :pin "b9cd52d1cf927bf7e08582d46ab0bcf1d4fb5048"))
