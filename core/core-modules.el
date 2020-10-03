3b59c62137af386f60de2abc4524f3acc1a5bf73
Anish Moorthy <anlsh@protonmail.com>
2020-10-03 15:22:13 -0400

    Remove unimplemented flags from use-package! docstring

--- core/core-modules.el
+++ core/core-modules.el
@@ -483,23 +483,7 @@ to least)."
 This is a thin wrapper around `use-package', and is ignored if the NAME package
 is disabled by the user (with `package!').
 
-See `use-package' to see what properties can be provided. Doom adds support for
-two extra properties:
-
-:after-call SYMBOL|LIST
-  Takes a symbol or list of symbols representing functions or hook variables.
-  The first time any of these functions or hooks are executed, the package is
-  loaded.
-
-:defer-incrementally SYMBOL|LIST|t
-  Takes a symbol or list of symbols representing packages that will be loaded
-  incrementally at startup before this one. This is helpful for large packages
-  like magit or org, which load a lot of dependencies on first load. This lets
-  you load them piece-meal during idle periods, so that when you finally do need
-  the package, it'll load quicker.
-
-  NAME is implicitly added if this property is present and non-nil. No need to
-  specify it. A value of `t' implies NAME."
+See `use-package' to see what properties can be provided."
   (declare (indent 1))
   (unless (or (memq name doom-disabled-packages)
               ;; At compile-time, use-package will forcibly load packages to
