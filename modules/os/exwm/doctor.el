635be39d23531108e9bb935de5b57405453ebcac
Christopher Hayward <chayward1@athabasca.edu>
2020-10-03 12:58:38 -0400

    https://github.com/hlissner/doom-emacs/pull/4004#discussion_r499152506

--- modules/os/exwm/doctor.el
+++ modules/os/exwm/doctor.el
@@ -1,5 +1,6 @@
 ;;; -*- lexical-binding: t; no-byte-compile: t; -*-
 ;;; os/exwm/doctor.el
 
-(unless (executable-find "firefox")
-  (warn! "Couldn't find firefox. exwm-firefox will not work"))
+(when (featurep! +firefox)
+  (unless (executable-find "firefox")
+    (warn! "Couldn't find firefox. exwm-firefox will not work")))
