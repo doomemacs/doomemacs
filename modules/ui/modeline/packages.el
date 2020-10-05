67c1e7c3f9610b8971bcd2b1b015e9dc72ece40b
Henrik Lissner <henrik@lissner.net>
2020-10-04 23:25:53 -0400

    Bump :ui
    Alexander-Miller/treemacs@027e03b -> Alexander-Miller/treemacs@6c00fe7
    Bad-ptr/persp-mode.el@14325c1 -> Bad-ptr/persp-mode.el@c132efe
    DarthFennec/highlight-indent-guides@a4f7714 -> DarthFennec/highlight-indent-guides@cf352c8
    dimitri/switch-window@8710f63 -> dimitri/switch-window@277706b
    ema2159/centaur-tabs@7e0332b -> ema2159/centaur-tabs@5453317
    emacs-straight/minimap@d8850be -> emacs-straight/minimap@37a0200
    hlissner/emacs-doom-themes@24023de -> hlissner/emacs-doom-themes@d6ee47d
    hlissner/emacs-solaire-mode@cd63b67 -> hlissner/emacs-solaire-mode@d751c1c
    jabranham/mixed-pitch@1cad46f -> jabranham/mixed-pitch@d305108
    seagle0128/doom-modeline@ffbaaee -> seagle0128/doom-modeline@538017a
    
    Fix #4023

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "ffbaaee832f1c97ff608bc4959b408997d959b7d"))
+  (package! doom-modeline :pin "538017a399cb7b499853bdaf50928ca8b270693d"))
 (package! anzu :pin "7b8688c84d6032300d0c415182c7c1ad6cb7f819")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "d3f6ed4773b48767bd5f4708c7f083336a8a8a86"))
--- modules/ui/tabs/packages.el
+++ modules/ui/tabs/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/tabs/packages.el
 
-(package! centaur-tabs :pin "7e0332b138f836b9d0b6d2134310f53369598cfd")
+(package! centaur-tabs :pin "5453317b6b9f68bfbd934eee3b883207bfa331e0")
