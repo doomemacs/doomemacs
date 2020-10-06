729e8d8f393d2610cd4007c8dde8fd2521742bc9
Henrik Lissner <henrik@lissner.net>
2020-10-06 13:30:06 -0400

    Fix #4035: fill-column toggle

--- modules/ui/fill-column/autoload.el
+++ modules/ui/fill-column/autoload.el
@@ -1,15 +1,14 @@
 ;;; ui/fill-column/autoload.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload 'hl-fill-column-mode "hl-fill-column" nil t)
+;;;###autoload
+(defalias '+fill-column/toggle
+  (if (fboundp 'display-fill-column-indicator-mode)
+      #'display-fill-column-indicator-mode  ; Emacs 27 only
+    (autoload 'hl-fill-column-mode "hl-fill-column" nil t)
+    #'hl-fill-column-mode))
 
 ;;;###autoload
-;; Emacs 27 introduced `display-fill-column-indicator-mode' which should be
-;; used instead of `hl-fill-column-mode'
-(defun +fill-column-enable-h (&optional arg)
-  (interactive "p")
-  (if (fboundp 'display-fill-column-indicator-mode)
-      (display-fill-column-indicator-mode arg)
-    (hl-fill-column-mode arg)))
+(defalias '+fill-column-enable-h #'+fill-column/toggle)
 
 ;;;###autoload
 (add-hook! '(text-mode-hook prog-mode-hook conf-mode-hook)
