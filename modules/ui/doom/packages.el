81f325f169ac2c5327fce7b849cd175a9a289ef1
Henrik Lissner <henrik@lissner.net>
2020-10-05 20:23:59 -0400

    Bump :ui doom
    hlissner/emacs-solaire-mode@d751c1c -> hlissner/emacs-solaire-mode@c697925
    
    Fix #4023

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -2,4 +2,4 @@
 ;;; ui/doom/packages.el
 
 (package! doom-themes :pin "d6ee47dc8ed2cf9e585f62243214af03ba5b1687")
-(package! solaire-mode :pin "d751c1c4e97fe6a57740be4da74a2f4b77e3d96d")
+(package! solaire-mode :pin "c697925f7e03819a4046a233f6ab31664aca9d6a")
