11faeae9a63a8ba6a0be7d26e46c76bf59c4d905
Henrik Lissner <henrik@lissner.net>
2020-10-09 03:56:03 -0400

    Bump :tools lsp
    emacs-lsp/lsp-mode@9a79593 -> emacs-lsp/lsp-mode@65034e1
    
    Fixes "LSP :: Error from the Language Server: Request
    textDocument/codeAction failed with message: Cannot read property
    'codeActions' of undefined (Internal Error)" error with lsp-vetur

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -5,7 +5,7 @@
     (progn
       (package! eglot :pin "61b71ea769fa14887465517f70832861f7052816")
       (package! project :pin "da0333a697b18f0a863c1b1523d2fc7991b31174"))
-  (package! lsp-mode :pin "9a79593e8a4efd1b07e6503fc646df726eeb66b6")
+  (package! lsp-mode :pin "65034e112b1bcdf9ebb4f7dce0268755ff5aceda")
   (package! lsp-ui :pin "25552041f5af110c282fe8a2c714dec0f7a2320e")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "20cac6296e5038b7131ee6f34a96635f1d30fe3c"))
