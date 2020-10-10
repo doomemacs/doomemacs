88f20cc54c3cd6869dd6f3140c15d4cd038d0168
Henrik Lissner <henrik@lissner.net>
2020-10-09 23:45:25 -0400

    Bump :lang ocaml
    ocaml-ppx/ocamlformat@cd0eaa8 -> ocaml-ppx/ocamlformat@1dec6c3
    ocaml/dune@daea74e -> ocaml/dune@f967df6
    ocaml/merlin@3751cbf -> ocaml/merlin@06833e9
    
    Closes #4068. Includes fix for ocaml-ppx/ocamlformat#1481

--- modules/lang/ocaml/packages.el
+++ modules/lang/ocaml/packages.el
@@ -4,7 +4,7 @@
 (package! tuareg :pin "ccde45bbc292123ec20617f1af7f7e19f7481545")
 
 (unless (featurep! +lsp)
-  (package! merlin :pin "3751cbfff75022c396c4ff4dc1729048f80daa4f")
+  (package! merlin :pin "06833e9dab5d5bb5ece68f11eb15797b6cd00933")
   (package! merlin-eldoc :pin "db7fab1eddfe34781b7e79694f8923b285698032")
   (when (featurep! :checkers syntax)
     (package! flycheck-ocaml :pin "8707a7bf545a8639a6a5c600a98d9a2ea1487dc9")))
@@ -17,8 +17,8 @@
 (when (featurep! :editor format)
   (package! ocamlformat
     :recipe (:host github :repo "ocaml-ppx/ocamlformat" :files ("emacs/*.el"))
-    :pin "cd0eaa8bbb7df431276cf65741c53eaa913f7807"))
+    :pin "1dec6c3ffb2572b21d43e99653cabdf0406e7eef"))
 
 (package! dune
   :recipe (:host github :repo "ocaml/dune" :files ("editor-integration/emacs/*.el"))
-  :pin "daea74e5366c8571af8674860a733b0c9e1eaf81")
+  :pin "f967df6f51f690eab612cdeb458926a4bdd41e11")
