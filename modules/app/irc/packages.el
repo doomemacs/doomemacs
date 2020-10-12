21d021580710853f4bebf5a6867433d16140d276
Henrik Lissner <henrik@lissner.net>
2020-10-11 23:31:56 -0400

    Bump :app
    jorgenschaefer/circe@89aac22 -> jorgenschaefer/circe@d98986c
    skeeto/elfeed@8fb09ad -> skeeto/elfeed@7b2b6fa

--- modules/app/irc/packages.el
+++ modules/app/irc/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/irc/packages.el
 
-(package! circe :pin "89aac22259e5d09ae1183e0df163338fe491e9e7")
+(package! circe :pin "d98986ce933c380b47d727beea8bad81bda65dc9")
 (package! circe-notifications :pin "291149ac12877bbd062da993479d3533a26862b0")
