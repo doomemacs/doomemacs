290deb87f30b9005a7504422cdc28197ea98c467
Henrik Lissner <henrik@lissner.net>
2020-10-11 20:32:40 -0400

    Bump :emacs
    ideasman42/emacs-undo-fu-session@e2043f8 -> ideasman42/emacs-undo-fu-session@5135b5c
    magit/magit@321214c -> magit/magit@2fb3bf7
    rmuslimov/browse-at-remote@6aecae4 -> rmuslimov/browse-at-remote@fadf99d

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -4,4 +4,4 @@
 (if (featurep! +tree)
     (package! undo-tree :pin "7523823ca3709e0327f3e9f38ddfec71a58084be")
   (package! undo-fu :pin "c0806c1903c5a0e4c69b6615cdc3366470a9b8ca")
-  (package! undo-fu-session :pin "e2043f8350970e1a9ef06a94956a733826cdf32b"))
+  (package! undo-fu-session :pin "5135b5cb610203cdb3e64227b8f437bb87292b18"))
--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,8 +5,8 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "6aecae4b5d202e582425fc8aa2c9c2b6a4779f25")
-(package! git-commit :pin "321214c3a2dd10fdf672ba96bd00703a51094bbe")
+(package! browse-at-remote :pin "fadf99d6d8e891f3b112e36c772e0eea0b9bc7f2")
+(package! git-commit :pin "2fb3bf782ccf5652b98f8de989f014749473eacf")
 (package! git-timemachine :pin "8d675750e921a047707fcdc36d84f8439b19a907")
 (package! gitconfig-mode :pin "55468314a5f6b77d2c96be62c7005ac94545e217")
 (package! gitignore-mode :pin "55468314a5f6b77d2c96be62c7005ac94545e217")
