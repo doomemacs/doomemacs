963f96b42f55f806ac9344188f45115422cbe1cb
Henrik Lissner <henrik@lissner.net>
2020-10-14 19:11:37 -0400

    Bump company
    company-mode/company-mode@053c50d -> company-mode/company-mode@5f5949b
    
    Fix #4092, due to company-mode/company-mode#1024

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/company/packages.el
 
-(package! company :pin "053c50d376e65e189a6b170d2fedc7d552a73907")
+(package! company :pin "5f5949b6ae8ea9df94c6cb3e01d9cae43623b794")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (featurep! +childframe)
   (package! company-box :pin "be37a9a30dc112ab172af21af694e2cb04a74f85"))
