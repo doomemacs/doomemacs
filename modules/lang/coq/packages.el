0a2203171a7fb57b721e7ce052584dff38fd7bd1
Brett Gilio <brettg@posteo.net>
2020-10-14 21:05:36 -0500

    Bump coq
    (package! proof-general :pin "cd6955d5a7fa35ab1aa6a7754d194a87f7c9aedf")

--- modules/lang/coq/packages.el
+++ modules/lang/coq/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/coq/packages.el
 
-(package! proof-general :pin "4f6b6027632f55d7a8248ae509cc6526a372ce9f")
+(package! proof-general :pin "cd6955d5a7fa35ab1aa6a7754d194a87f7c9aedf")
 (package! company-coq :pin "b096cb528de1e0bb31cc1059d0bd698b98a6cc6a")
