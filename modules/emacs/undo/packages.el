1cc060c928677f783720536e5bbd930a9d730d84
Henrik Lissner <henrik@lissner.net>
2020-10-18 17:23:42 -0400

    Bump :emacs undo
    ideasman42/emacs-undo-fu-session@5135b5c -> ideasman42/emacs-undo-fu-session@56cdd35
    
    undo-fu-session includes an update to suppress harmless warnings, which
    may have only served to alarm users.

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -4,4 +4,4 @@
 (if (featurep! +tree)
     (package! undo-tree :pin "7523823ca3709e0327f3e9f38ddfec71a58084be")
   (package! undo-fu :pin "c0806c1903c5a0e4c69b6615cdc3366470a9b8ca")
-  (package! undo-fu-session :pin "5135b5cb610203cdb3e64227b8f437bb87292b18"))
+  (package! undo-fu-session :pin "56cdd3538a058c6916bdf2d9010c2179f2505829"))
