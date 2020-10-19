aa460786b6d25d329c8a451c1108cae94c2330e7
Henrik Lissner <henrik@lissner.net>
2020-10-19 01:15:31 -0400

    Bump :lang coq
    ProofGeneral/PG@4f6b602 -> ProofGeneral/PG@0fdb1ae

--- modules/lang/coq/packages.el
+++ modules/lang/coq/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/coq/packages.el
 
-(package! proof-general :pin "4f6b6027632f55d7a8248ae509cc6526a372ce9f")
+(package! proof-general :pin "0fdb1ae633baeb9afb07bbd8632bece5976f95f2")
 (package! company-coq :pin "b096cb528de1e0bb31cc1059d0bd698b98a6cc6a")
