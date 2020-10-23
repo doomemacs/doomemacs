3788fd2c449bb6bcd71e42410a2d300573abfc3a
lifeainteasy@web.de <lifeainteasy@web.de>
2020-10-24 01:29:44 +0200

    Add Keybinding for testing all functions in the project with pytest

--- modules/lang/python/config.el
+++ modules/lang/python/config.el
@@ -189,6 +189,7 @@ called.")
         :localleader
         :map python-mode-map
         :prefix ("t" . "test")
+        "a" #'python-pytest
         "f" #'python-pytest-file-dwim
         "F" #'python-pytest-file
         "t" #'python-pytest-function-dwim
