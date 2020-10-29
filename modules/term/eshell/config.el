8eb8f41b339972c92f530e7887a2da05a737430a
Henrik Lissner <henrik@lissner.net>
2020-10-29 01:42:09 -0400

    EMACS28+ gate eshell keymap fix

--- modules/term/eshell/config.el
+++ modules/term/eshell/config.el
@@ -151,11 +151,12 @@ You should use `set-eshell-alias!' to change this.")
   ;;      is not for us mere mortals to question! Fun fact: there's a "FIXME
   ;;      What the hell?!" above the offending line in esh-mode.el.
   ;; DEPRECATED As of Emacs 28, binding keys in a hook is no longer necessary.
-  (add-hook! 'eshell-mode-hook
-    (defun +eshell-fix-keymap-h ()
-      "Undo buffer-local `eshell-mode-map', so global keybinds work."
-      (kill-local-variable 'eshell-mode-map)
-      (use-local-map eshell-mode-map))))
+  (unless EMACS28+
+    (add-hook! 'eshell-mode-hook
+      (defun +eshell-fix-keymap-h ()
+        "Undo buffer-local `eshell-mode-map', so global keybinds work."
+        (kill-local-variable 'eshell-mode-map)
+        (use-local-map eshell-mode-map)))))
 
 
 (use-package! eshell-up
