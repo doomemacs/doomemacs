7205b727a75b6a2342862cc8e0617684a2f45225
Henrik Lissner <henrik@lissner.net>
2020-11-06 21:15:21 -0500

    Bump :ui doom
    hlissner/emacs-doom-themes@5c12834 -> hlissner/emacs-doom-themes@144b2a8
    
    Fix #2825

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "5c12834e724869993e1fa62cb3be29fda994a0d6")
+(package! doom-themes :pin "144b2a8b8b443abfb7ee9f9209c769adaa4fd333")
 (package! solaire-mode :pin "c697925f7e03819a4046a233f6ab31664aca9d6a")
