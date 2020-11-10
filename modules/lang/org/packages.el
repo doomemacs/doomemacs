423dbe0992472731082cdc000b9f40a1937aea1a
Henrik Lissner <henrik@lissner.net>
2020-11-10 13:22:46 -0500

    Bump :lang org
    Kungsgeten/org-brain@e703ae0 -> Kungsgeten/org-brain@6e3d861
    bastibe/org-journal@fce4fa7 -> bastibe/org-journal@14ce011
    org-roam/org-roam@a7cf48e -> org-roam/org-roam@d913447
    snosov1/toc-org@ff8d49c -> snosov1/toc-org@aef220c

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -30,7 +30,7 @@
   :recipe (:host github :repo "TobiasZawada/org-yt")
   :pin "40cc1ac76d741055cbefa13860d9f070a7ade001")
 (package! ox-clip :pin "f5eac28734ea33d0b7a3dbe10b777907a91cf9f9")
-(package! toc-org :pin "ff8d49c2c7daab0061250b581d3eebc7265ee267")
+(package! toc-org :pin "aef220c266f53d36055f74f4a243c6483c563d2a")
 (package! org-cliplink :pin "82402cae7e118d67de7328417fd018a18f95fac2")
 
 (when (featurep! :editor evil +everywhere)
@@ -42,7 +42,7 @@
 (when (featurep! :tools magit)
   (package! orgit :pin "ac9b1a42863a864fde9d225890ef5464bffdc646"))
 (when (featurep! +brain)
-  (package! org-brain :pin "e703ae0f3fbdf488bf7442276a90fcb52e11cde7"))
+  (package! org-brain :pin "6e3d8618b0749742335289c32dea0b92966cf9c2"))
 (when (featurep! +dragndrop)
   (package! org-download :pin "42ac361ef5502017e6fc1bceb00333eba90402f4"))
 (when (featurep! +gnuplot)
@@ -53,7 +53,7 @@
 (when (featurep! +jupyter)
   (package! jupyter :pin "360cae2c70ab28c7a7848c0c56473d984f0243e5"))
 (when (featurep! +journal)
-  (package! org-journal :pin "fce4fa7e7286280ecd7b42c2f67f0d73048d2c7a"))
+  (package! org-journal :pin "14ce0119fe2d89f2f2207788be2edce0b9372822"))
 (when (featurep! +noter)
   (package! org-noter :pin "9ead81d42dd4dd5074782d239b2efddf9b8b7b3d"))
 (when (featurep! +pomodoro)
@@ -72,7 +72,7 @@
              :files ("css" "dist" "js" "plugin"))
     :pin "0582f57517c97a4c7bfeb58762138c78883f94c5"))
 (when (featurep! +roam)
-  (package! org-roam :pin "a7cf48ea895ee5aae93ad6d5b4550fb803f6ef8a"))
+  (package! org-roam :pin "d913447939baa0925eb4aeb719c31e6a84916e0f"))
 
 ;;; Babel
 (package! ob-async :pin "de1cd6c93242a4cb8773bbe115b7be3d4dd6b97e")
