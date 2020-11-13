62c1e5b6c9d95372d4b0f7007e8a55cc75bea837
Henrik Lissner <henrik@lissner.net>
2020-11-13 12:56:52 -0500

    Bump :checkers syntax
    flycheck/flycheck@cd8e0a2 -> flycheck/flycheck@79c9245
    
    Fixes #4264: flycheck-rebar3 moved into flycheck

--- modules/lang/erlang/packages.el
+++ modules/lang/erlang/packages.el
@@ -2,8 +2,6 @@
 ;;; lang/erlang/packages.el
 
 (package! erlang :pin "36b4ba407e6f3c70ff88f8e3321622372601f6f6")
-(when (featurep! :checkers syntax)
-  (package! flycheck-rebar3 :pin "3cca1268c54643204b5bae52e3f0bf5bc921018c"))
 (unless (featurep! +lsp)
   (when (featurep! :completion company)
     (package! company-erlang :pin "bc0524a16f17b66c7397690e4ca0e004f09ea6c5")))
