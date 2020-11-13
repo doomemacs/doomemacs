120530e353d1e4ae34ce82f6d4d301298f9d8e81
Henrik Lissner <henrik@lissner.net>
2020-11-13 12:54:00 -0500

    Bump :lang lua
    technomancy/fennel-mode@a395437 -> technomancy/fennel-mode@7a8824a
    
    Fixes #4263: invalid repl handler for fennel-mode

--- modules/lang/lua/packages.el
+++ modules/lang/lua/packages.el
@@ -11,7 +11,7 @@
       :pin "fcb99e5efcf31db05f236f02eaa575986a57172d")))
 
 (when (featurep! +fennel)
-  (package! fennel-mode :pin "a39543750ba337b9327108ba17ffb0cdb18c7f9b"))
+  (package! fennel-mode :pin "7a8824a44c231a984e582ef115cef620174b2ed5"))
 
 (when (featurep! :completion company)
   (package! company-lua :pin "29f6819de4d691e5fd0b62893a9f4fbc1c6fcb52"))
