3265c258217aaf0d8fee78e55d766271d88c95b2
Henrik Lissner <henrik@lissner.net>
2020-11-13 12:59:02 -0500

    Bump :lang erlang
    erlang/otp@36b4ba4 -> erlang/otp@8efb945

--- modules/lang/erlang/packages.el
+++ modules/lang/erlang/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/erlang/packages.el
 
-(package! erlang :pin "36b4ba407e6f3c70ff88f8e3321622372601f6f6")
+(package! erlang :pin "8efb945727effec82897c1ecdc97f007efdaf777")
 (unless (featurep! +lsp)
   (when (featurep! :completion company)
     (package! company-erlang :pin "bc0524a16f17b66c7397690e4ca0e004f09ea6c5")))
