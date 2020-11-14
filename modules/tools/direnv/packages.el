ecca37b07b96d5c08264261af71e2503a4e8398a
Henrik Lissner <henrik@lissner.net>
2020-11-14 13:05:28 -0500

    Bump :tools direnv
    purcell/envrc@da8e306 -> purcell/envrc@18caf51
    
    envrc-mode now provides its own envrc file major mode with basic syntax highlighting.

--- modules/tools/direnv/packages.el
+++ modules/tools/direnv/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/direnv/packages.el
 
-(package! envrc :pin "da8e306b0a562af05c5e990aced968d7fda06296")
+(package! envrc :pin "18caf5154f61d7f78cd4719d999e0fa6ef52345f")
