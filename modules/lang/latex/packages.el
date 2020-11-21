c83e5e75e24706a0d6c15f3156d33b1c4f9dc365
Henrik Lissner <henrik@lissner.net>
2020-11-21 14:09:08 -0500

    Bump :lang latex
    TheBB/company-reftex@275ef70 -> TheBB/company-reftex@291c283
    cdominik/cdlatex@480387b -> cdominik/cdlatex@adf96ba
    emacs-straight/auctex@59e251c -> emacs-straight/auctex@fb062a3
    iyefrat/evil-tex@a3b6875 -> iyefrat/evil-tex@ac313ef

--- modules/lang/latex/packages.el
+++ modules/lang/latex/packages.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/latex/packages.el
 
-(package! auctex :pin "59e251c4c82d878e605a13aec3752e544a99f21a")
+(package! auctex :pin "fb062a364fbc7d791707ff574b0f0a7f4c7a7269")
 (package! adaptive-wrap :pin "319db649fb083db3ad07b4c71ee6c9429497043b")
 (package! latex-preview-pane :pin "5297668a89996b50b2b62f99cba01cc544dbed2e")
 (when (featurep! :editor evil +everywhere)
-  (package! evil-tex :pin "a3b6875d4027c675cf490622026a15a12fe1a911"))
+  (package! evil-tex :pin "ac313efb22d621c093d8d30233bd7dc8b4cc54b4"))
 
 ;; Optional module features:
 
@@ -13,11 +13,11 @@
   (package! auctex-latexmk :pin "4d353522650d7685acbf1d38f7dbc504f734bd84"))
 
 (when (featurep! +cdlatex)
-  (package! cdlatex :pin "480387b39f6ddd9cd2a9511ecee064ad8e1dd324"))
+  (package! cdlatex :pin "adf96bab0bbf28f65c882c0874f1c14fdb216bd8"))
 
 ;; Features according to other user selected options
 
 (when (featurep! :completion company)
   (package! company-auctex :pin "9400a2ec7459dde8cbf1a5d50dfee4e300ed7e18")
-  (package! company-reftex :pin "275ef708f08d3bf0eb30632148e5c6184eeaacdb")
+  (package! company-reftex :pin "291c283c8a015fd7cbaa99f836e1a721f1e2c832")
   (package! company-math :pin "a796053590012e6a15c8b527b521ffc15d137bd0"))
