ed93ed9907c81148d9730b3d1911282585021d1c
Henrik Lissner <henrik@lissner.net>
2020-11-26 13:54:13 -0500

    Bump :config default
    abo-abo/avy@509471b -> abo-abo/avy@bbf1e73
    noctuid/link-hint.el@7440704 -> noctuid/link-hint.el@e897897

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; config/default/packages.el
 
-(package! avy :pin "509471bad0e8094b8639729ec39ca141fae7d4bd")
+(package! avy :pin "bbf1e7339eba06784dfe86643bb0fbddf5bb0342")
 (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-(package! link-hint :pin "7440704cacb5c0fab35fff8ec59d30fbea17f44a")
+(package! link-hint :pin "e897897d6a92b4ffdbff33acf69f5c661da1123a")
 
 (unless (featurep! :editor evil)
   (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781"))
