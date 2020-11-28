a3293e67d4b51c986f50677b85fbd5a5bda15d6d
Henrik Lissner <henrik@lissner.net>
2020-11-27 22:21:16 -0500

    Bump :core
    Fuco1/smartparens@c59bfef -> Fuco1/smartparens@7f5825d
    bbatsov/projectile@d1daf27 -> bbatsov/projectile@d1cfad0
    iqbalansari/restart-emacs@9aa90d3 -> iqbalansari/restart-emacs@e570749
    jscheid/dtrt-indent@50c440c -> jscheid/dtrt-indent@a7ade6d
    justbur/emacs-which-key@c011b26 -> justbur/emacs-which-key@ca268fd
    jwiegley/use-package@4fb1f9a -> jwiegley/use-package@caa92f1
    koral/gcmh@84c43a4 -> koral/gcmh@0089f9c

--- core/packages.el
+++ core/packages.el
@@ -3,7 +3,7 @@
 
 ;; core.el
 (package! auto-minor-mode :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
-(package! gcmh :pin "84c43a4c0b41a595ac6e299fa317d2831813e580")
+(package! gcmh :pin "0089f9c3a6d4e9a310d0791cf6fa8f35642ecfd9")
 (package! explain-pause-mode
   :recipe (:host github
            :repo "lastquestion/explain-pause-mode")
@@ -22,21 +22,21 @@
 ;; core-modules.el
 (package! use-package
   :type 'core
-  :pin "4fb1f9a68f1e7e7d614652afc017a6652fd029f1")
+  :pin "caa92f1d64fc25480551757d854b4b49981dfa6b")
 
 ;; core-ui.el
 (package! all-the-icons :pin "6917b08f64dd8487e23769433d6cb9ba11f4152f")
 (package! hide-mode-line :pin "88888825b5b27b300683e662fa3be88d954b1cea")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "f43d48a24602be3ec899345a3326ed0247b960c6")
-(package! restart-emacs :pin "9aa90d3df9e08bc420e1c9845ee3ff568e911bd9")
+(package! restart-emacs :pin "e5707491d7ac20879465bb52e282ad1416748378")
 
 ;; core-editor.el
 (package! better-jumper :pin "fe548d22c9228b60d9c8a2a452a6c2e03dfdf238")
-(package! dtrt-indent :pin "50c440c80e0d15303d8ab543bce4c56e9c2bf407")
+(package! dtrt-indent :pin "a7ade6d244eeeda2ada9f7eca565491cea4b622a")
 (package! helpful :pin "584ecc887bb92133119f93a6716cdf7af0b51dca")
 (package! pcre2el :pin "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
-(package! smartparens :pin "c59bfef7e8f1687ac77b0afaaaed86d8051d3de1")
+(package! smartparens :pin "7f5825dd655942c3d56d14acabab1ffab1aa2ae2")
 (package! so-long
   :built-in 'prefer ; included in Emacs 27+
   ;; REVIEW so-long is slated to be published to ELPA eventually, but until then
@@ -51,8 +51,8 @@
   :pin "2bb49d3ee7d2cba133bc7e9cdac416cd1c5e4fe0")
 
 ;; core-projects.el
-(package! projectile :pin "d1daf274e8ca2eb0f20475b8f314bb955167c6a1")
+(package! projectile :pin "d1cfad008b1719a6bee17fbe9479db414c0dc5d9")
 
 ;; core-keybinds.el
 (package! general :pin "a0b17d207badf462311b2eef7c065b884462cb7c")
-(package! which-key :pin "c011b268196b8356c70f668506a1133086bc9477")
+(package! which-key :pin "ca268fd313d3fb2bd03a8b5e4bdcca675ce58ca7")
