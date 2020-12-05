9137cbaaa472025e119097d4cb35ef26034a587b
Henrik Lissner <henrik@lissner.net>
2020-12-05 16:57:31 -0500

    Bump :completion
    Yevgnen/ivy-rich@1097013 -> Yevgnen/ivy-rich@c0a164c
    abo-abo/swiper@6a22192 -> abo-abo/swiper@778d8bf
    company-mode/company-mode@dd92593 -> company-mode/company-mode@97cfbc3
    emacs-helm/helm@5ad6c83 -> emacs-helm/helm@75562ee
    emacs-jp/helm-c-yasnippet@89cc856 -> emacs-jp/helm-c-yasnippet@28699d9
    raxod502/prescient.el@41443e1 -> raxod502/prescient.el@5d139e5
    tumashu/ivy-posframe@58ddf8c -> tumashu/ivy-posframe@08a00b8
    tumashu/posframe@395aca9 -> tumashu/posframe@e1552c8

--- modules/completion/helm/packages.el
+++ modules/completion/helm/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/helm/packages.el
 
-(package! helm :pin "5ad6c83681fda7bfd5a745fedecaa924630253dd")
+(package! helm :pin "75562eec4879e7f72119ceee40ab5087c705267e")
 (package! helm-rg :pin "ee0a3c09da0c843715344919400ab0a0190cc9dc")
-(package! helm-c-yasnippet :pin "89cc8561e7e57e9d1070ee3641df019c7f49c5dd")
+(package! helm-c-yasnippet :pin "28699d9a9caa8b4d37fd66368d93b6c65828c235")
 (package! helm-company :pin "6eb5c2d730a60e394e005b47c1db018697094dde")
 (package! helm-describe-modes
   :recipe (:host github :repo "emacs-helm/helm-describe-modes")
@@ -13,7 +13,7 @@
 (when (featurep! +fuzzy)
   (package! helm-flx :pin "6640fac5cb16bee73c95b8ed1248a4e5e113690e"))
 (when (featurep! +childframe)
-  (package! posframe :pin "395aca928b00c8f76aaeb65a85481c99e88c6873"))
+  (package! posframe :pin "e1552c82dffaadc5e7de09a44880a498197ffbee"))
 (when (featurep! :lang org)
   (package! helm-org :pin "b7a18dfc17e8b933956d61d68c435eee03a96c24"))
 (package! helm-descbinds :pin "b72515982396b6e336ad7beb6767e95a80fca192")
--- modules/completion/ivy/packages.el
+++ modules/completion/ivy/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/ivy/packages.el
 
-(package! swiper :pin "6a22192f11a4aa0d43692bf685fd9b9d75911d00")
+(package! swiper :pin "778d8bffa33ae0bf432aa23370bb2a535d2132ce")
 (package! ivy)
 (package! ivy-hydra)
 (package! ivy-avy)
@@ -9,16 +9,16 @@
 
 (package! amx :pin "ccfc92c600df681df5e8b5fecec328c462ceb71e")
 (package! counsel-projectile :pin "06b03c1080d3ccc3fa9b9c41b1ccbcf13f058e4b")
-(package! ivy-rich :pin "10970130b41c6ef9570893cdab8dfbe720e2b1a9")
+(package! ivy-rich :pin "c0a164ce42920df0629c87dad5ea1bddc79ceac3")
 (package! wgrep :pin "f0ef9bfa44db503cdb2f83fcfbd2fa4e2382ef1f")
 
 (if (featurep! +prescient)
-    (package! ivy-prescient :pin "41443e1c9f794b569dafdad4c0b64a608df64b99")
+    (package! ivy-prescient :pin "5d139e5b1fe03ccaddff8c250ab8e9d795071b95")
   (when (featurep! +fuzzy)
     (package! flx :pin "647cb2f92f9936c62e277d7a74ad54a80502d255")))
 
 (when (featurep! +childframe)
-  (package! ivy-posframe :pin "58ddf8c1622dfa2c91b6bfe6fee842abeff0b422"))
+  (package! ivy-posframe :pin "08a00b81747c6b623dd1adc7491a1e2a4c3a3d2a"))
 
 (when (featurep! +icons)
   (package! all-the-icons-ivy :pin "a70cbfa1effe36efc946a823a580cec686d5e88d"))
