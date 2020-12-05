3fd1d03b77349a89bd9f70cb46f8dc141a435200
Henrik Lissner <henrik@lissner.net>
2020-12-05 15:21:45 -0500

    Bump :email wanderlust
    wanderlust/apel@28bca5f -> wanderlust/apel@4e3269b
    wanderlust/flim@edb5982 -> wanderlust/flim@068b35e
    wanderlust/semi@939c805 -> wanderlust/semi@f279ebe
    wanderlust/wanderlust@c7043e6 -> wanderlust/wanderlust@5db307a
    
    Prompted by wanderlust/wanderlust#177 and #4364
    
    Co-authored-by: Kirill A. Korinsky <kirill@korins.ky>

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -4,8 +4,8 @@
 ;; HACK These are wanderlust's dependencies (wanderlust depends on semi, semi
 ;;      depends on flim, flim on apel), but they all have non-standard default
 ;;      branches which straight cannot detect without our help.
-(package! apel :recipe (:branch "apel-wl") :pin "28bca5f7027da26c90bf25ab835a1d615ce316e4")
-(package! flim :recipe (:branch "flim-1_14-wl") :pin "edb5982bdc24960798f6038db2c863d7c264cffb")
-(package! semi :recipe (:branch "semi-1_14-wl") :pin "939c80580101126c81a72f1643762fbc964f8b64")
+(package! apel :recipe (:branch "apel-wl") :pin "4e3269b6e702db2dba48cf560563ac883e81e3bf")
+(package! flim :recipe (:branch "flim-1_14-wl") :pin "068b35e863be9245e717e79def55f93580188bc8")
+(package! semi :recipe (:branch "semi-1_14-wl") :pin "f279ebe1c1f9c14bdd5d3da01af24277a6167b69")
 
-(package! wanderlust :pin "c7043e6446a302fee41b531d2daaa388c4d833a7")
+(package! wanderlust :pin "5db307a0441b6b7aa4ecfd34344842d78f15611b")
