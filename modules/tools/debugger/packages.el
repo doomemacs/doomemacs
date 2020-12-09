46ac4191e169b073df7dd46da52054686420f84b
Henrik Lissner <henrik@lissner.net>
2020-12-09 17:10:15 -0500

    Bump :tools debugger lsp
    emacs-lsp/dap-mode@e582ff9 -> emacs-lsp/dap-mode@041db8e
    emacs-lsp/lsp-mode@e1c4241 -> emacs-lsp/lsp-mode@61443f3
    emacs-lsp/lsp-ui@b1693d6 -> emacs-lsp/lsp-ui@49bc513
    emacs-straight/project@67216fb -> emacs-straight/project@0003fe4
    
    Fix #4396

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -6,5 +6,5 @@
     (package! realgud-trepan-ni :pin "6e38cf838c7b47b5f1353d00901b939ffa36d707")))
 
 (when (featurep! +lsp)
-  (package! dap-mode :pin "e582ff90c104703ed65c1f3174d4c4690e9cf1fd")
+  (package! dap-mode :pin "041db8eb7f0ceb9477ffff1730b5940814001158")
   (package! posframe :pin "e1552c82dffaadc5e7de09a44880a498197ffbee"))
--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -4,9 +4,9 @@
 (if (featurep! +eglot)
     (progn
       (package! eglot :pin "21726416e6e580b20dfa90833c6dab2a8a15ea48")
-      (package! project :pin "67216fba813461a8b8d5db1a12aaca08a0389423"))
-  (package! lsp-mode :pin "e1c42412eac0d742196efe64a82699326e72b391")
-  (package! lsp-ui :pin "b1693d610c4d2c44305eba2719e8d4097fdcdcb8")
+      (package! project :pin "0003fe4f3f81be08a9427888ead29f182e0b2527"))
+  (package! lsp-mode :pin "61443f32531d1e94026c379319c99f21414ee3e7")
+  (package! lsp-ui :pin "49bc5134c15d92b866c389c16e8e551a9285961a")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "c70ee8b54357c56d1b972393ee53e57a2e545fbb"))
   (when (featurep! :completion helm)
