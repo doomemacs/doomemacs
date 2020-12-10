1b6b122317c7245dd519e05bb8a8e1d535d1bed1
Henrik Lissner <henrik@lissner.net>
2020-12-10 16:14:05 -0500

    Bump :tools magit
    magit/magit@2e0c697 -> magit/magit@acfe22a
    magit/forge@844aa6d -> magit/forge@953764d
    
    Fixes odd errors on magit-status

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "2e0c697188ca728c240b5aa677f4a5e853692b6e")
+(when (package! magit :pin "acfe22ab60a56c61aae3ca6d4f2b7b826fe3b071")
   (when (featurep! +forge)
-    (package! forge :pin "844aa6d52792dd56a14f1759312c96a3de9b988e"))
+    (package! forge :pin "953764d2bb57b6bbaec4a2048722050fd15732db"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "78d24cf419138b543460f40509c8c1a168b52ca0")
   (package! github-review :pin "db723740e02348c0760407e532ad667ef89210ec"))
