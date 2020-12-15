65dfdd5c4a18d534be4d44a53a09f3427e17a62f
Nikita Bloshchanevich <nikblos@outlook.com>
2020-12-15 14:28:19 +0100

    [4415] remove FIXME comment
    Due to `ivy-call' still causing files to be opened in the
    background (which is not a bug in `ivy'),
    `+emacs-lisp-lookup-definition' still needs to return `deferred' for
    modules. The comment is as such misleading and needs to be removed (it
    has been resolved).

--- modules/lang/emacs-lisp/autoload.el
+++ modules/lang/emacs-lisp/autoload.el
@@ -61,8 +61,6 @@ to a pop up buffer."
 (defun +emacs-lisp-lookup-definition (_thing)
   "Lookup definition of THING."
   (if-let (module (+emacs-lisp--module-at-point))
-      ;; FIXME: this is probably a bug in `counsel'. See
-      ;; https://github.com/abo-abo/swiper/pull/2752.
       (progn (doom/help-modules (car module) (cadr module) 'visit-dir) 'deferred)
     (call-interactively #'elisp-def)))
 
