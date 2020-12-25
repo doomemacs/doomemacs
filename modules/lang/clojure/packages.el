bbea4f166f5a412707fb9851941c6abd8908b76e
Zachary <zackteo@gmail.com>
2020-12-26 05:32:30 +0800

    Bump :lang clojure

--- modules/lang/clojure/packages.el
+++ modules/lang/clojure/packages.el
@@ -2,7 +2,7 @@
 ;;; lang/clojure/packages.el
 
 (package! clojure-mode :pin "53ef8ac076ae7811627fbdd408e519ab7fca9a0b")
-(package! cider :pin "ef47c1de151c212b8d2ddeb9af6c8b0dfc0e300f")
+(package! cider :pin "4a7878e6078753eab599754135bdeca58c6d7b00")
 (package! clj-refactor :pin "6db85b37b57497b56d97d5e5512160e5db85f798")
 
 (when (featurep! :checkers syntax)
