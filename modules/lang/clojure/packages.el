a65138d4ce57a4580a0fd6623a64b12977deca5b
zackteo <zackteo@gmail.com>
2021-01-02 08:44:55 +0800

    Bump: Cider 1.0

--- modules/lang/clojure/packages.el
+++ modules/lang/clojure/packages.el
@@ -2,8 +2,8 @@
 ;;; lang/clojure/packages.el
 
 (package! clojure-mode :pin "53ef8ac076ae7811627fbdd408e519ab7fca9a0b")
-(package! cider :pin "4a7878e6078753eab599754135bdeca58c6d7b00")
-(package! clj-refactor :pin "6db85b37b57497b56d97d5e5512160e5db85f798")
+(package! cider :pin "6c20b75804cb17ae01a512ad421f2ddb10c3a06e")
+(package! clj-refactor :pin "9dcc50da7ce6f3c10276c87f09022e80c03e8bef")
 
 (when (featurep! :checkers syntax)
   (package! flycheck-clj-kondo :pin "a558bda44c4cb65b69fa53df233e8941ebd195c5"))
