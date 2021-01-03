4e2a473bc00b64cc52059fd4d3748b0f75070cf9
Henrik Lissner <henrik@lissner.net>
2021-01-03 18:13:12 -0500

    Bump :completion ivy
    Yevgnen/ivy-rich@c0a164c -> Yevgnen/ivy-rich@f8a1f5c
    abo-abo/swiper@778d8bf -> abo-abo/swiper@d2891aa
    raxod502/prescient.el@5d139e5 -> raxod502/prescient.el@42adc80
    tumashu/ivy-posframe@08a00b8 -> tumashu/ivy-posframe@83047d4
    
    Closes #4491: the previous pin broke regex. Using anything other than $
    would make the prompt hang. Looks like this pin fixes it.

--- modules/completion/ivy/packages.el
+++ modules/completion/ivy/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/ivy/packages.el
 
-(package! swiper :pin "778d8bffa33ae0bf432aa23370bb2a535d2132ce")
+(package! swiper :pin "d2891aab7b816aebf21ebd01ce33933a6ac6244f")
 (package! ivy)
 (package! ivy-hydra)
 (package! ivy-avy)
@@ -9,16 +9,16 @@
 
 (package! amx :pin "ccfc92c600df681df5e8b5fecec328c462ceb71e")
 (package! counsel-projectile :pin "06b03c1080d3ccc3fa9b9c41b1ccbcf13f058e4b")
-(package! ivy-rich :pin "c0a164ce42920df0629c87dad5ea1bddc79ceac3")
+(package! ivy-rich :pin "f8a1f5c90d2a113b597ef5903634c089fce3365b")
 (package! wgrep :pin "f0ef9bfa44db503cdb2f83fcfbd2fa4e2382ef1f")
 
 (if (featurep! +prescient)
-    (package! ivy-prescient :pin "5d139e5b1fe03ccaddff8c250ab8e9d795071b95")
+    (package! ivy-prescient :pin "42adc802d3ba6c747bed7ea1f6e3ffbbdfc7192d")
   (when (featurep! +fuzzy)
     (package! flx :pin "647cb2f92f9936c62e277d7a74ad54a80502d255")))
 
 (when (featurep! +childframe)
-  (package! ivy-posframe :pin "08a00b81747c6b623dd1adc7491a1e2a4c3a3d2a"))
+  (package! ivy-posframe :pin "83047d440ff132d5a45acde5955f71853edeefb9"))
 
 (when (featurep! +icons)
   (package! all-the-icons-ivy :pin "a70cbfa1effe36efc946a823a580cec686d5e88d"))
