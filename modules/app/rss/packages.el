426c50d61bd7260d5a24696562c18708f62e0a74
Henrik Lissner <henrik@lissner.net>
2021-02-07 23:47:33 -0500

    Bump :app :config
    abo-abo/avy@bbf1e73 -> abo-abo/avy@e92cb37
    jorgenschaefer/circe@d98986c -> jorgenschaefer/circe@265f36c
    kidd/org-gcal.el@2cad2d8 -> kidd/org-gcal.el@0a6f9a7
    noctuid/link-hint.el@e897897 -> noctuid/link-hint.el@09ba572
    skeeto/elfeed@7b2b6fa -> skeeto/elfeed@362bbe5

--- modules/app/rss/packages.el
+++ modules/app/rss/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/rss/packages.el
 
-(package! elfeed :pin "7b2b6fadaa498fef2ba212a50da4a8afa2a5d305")
+(package! elfeed :pin "362bbe5b38353d033c5299f621fea39e2c75a5e0")
 (package! elfeed-org :pin "77b6bbf222487809813de260447d31c4c59902c9")
