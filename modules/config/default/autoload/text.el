b8475dfb95d6235a7baa2d61b5709cc1e05556d3
Edmund Miller <edmund.a.miller@protonmail.com>
2021-02-16 20:53:11 -0600

    fix(selectrum): Remove unnecessary advice overrides

--- modules/config/default/autoload/text.el
+++ modules/config/default/autoload/text.el
@@ -28,8 +28,9 @@
   (interactive)
   (call-interactively
    (cond ((fboundp 'counsel-yank-pop)    #'counsel-yank-pop)
+         ((fboundp 'consult-yank-pop)    #'consult-yank-pop)
          ((fboundp 'helm-show-kill-ring) #'helm-show-kill-ring)
-         ((error "No kill-ring search backend available. Enable ivy or helm!")))))
+         ((error "No kill-ring search backend available. Enable ivy, helm or selectrum!")))))
 
 ;;;###autoload
 (defun +default/yank-buffer-path (&optional root)
