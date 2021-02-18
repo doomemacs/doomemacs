ec747395c64a1d6c64ce98dbe6b365f1578d2314
Johan Thorén <johanthoren@users.noreply.github.com>
2021-02-18 21:49:49 +0100

    Add support for parinfer-rust-mode (#1)
    This commit adds support for the module flag +rust, which will use parinfer-rust-mode
    instead of parinfer-mode. Parinfer-rust-mode makes use of an external library named parinfer-rust
    to do the heavy lifting. As such, --with-modules must be enabled.

--- modules/editor/parinfer/config.el
+++ modules/editor/parinfer/config.el
@@ -1,24 +1,48 @@
 ;;; editor/parinfer/config.el -*- lexical-binding: t; -*-
 
-(use-package! parinfer
-  :hook ((emacs-lisp-mode
-          clojure-mode
-          scheme-mode
-          lisp-mode
-          racket-mode
-          hy-mode) . parinfer-mode)
-  :init
-  (setq parinfer-extensions
-        '(defaults
-          pretty-parens
-          smart-tab
-          smart-yank))
-  (when (featurep! :editor evil +everywhere)
-    (push 'evil parinfer-extensions))
-  :config
-  (map! :map parinfer-mode-map
-        "\"" nil  ; smartparens handles this
-        :i "<tab>"     #'parinfer-smart-tab:dwim-right-or-complete
-        :i "<backtab>" #'parinfer-smart-tab:dwim-left
-        :localleader
-        :desc "Toggle parinfer-mode" "m" #'parinfer-toggle-mode))
+(if (not (featurep! +rust))
+    (use-package! parinfer
+      :hook ((emacs-lisp-mode
+              clojure-mode
+              scheme-mode
+              lisp-mode
+              racket-mode
+              hy-mode) . parinfer-mode)
+      :init
+      (setq parinfer-extensions
+            '(defaults
+              pretty-parens
+              smart-tab
+              smart-yank))
+      (when (featurep! :editor evil +everywhere)
+        (push 'evil parinfer-extensions))
+      :config
+      (map! :map parinfer-mode-map
+            "\"" nil  ; smartparens handles this
+            :i "<tab>"     #'parinfer-smart-tab:dwim-right-or-complete
+            :i "<backtab>" #'parinfer-smart-tab:dwim-left
+            :localleader
+            :desc "Toggle parinfer-mode" "m" #'parinfer-toggle-mode))
+  (defvar parinfer-rust--doom-lib-name (cond ((eq system-type 'darwin)
+                                              "parinfer-rust-darwin.so")
+                                             ((eq system-type 'gnu/linux)
+                                              "parinfer-rust-linux.so")
+                                             ((eq system-type 'windows-nt)
+                                              "parinfer-rust-windows.dll")))
+  (use-package! parinfer-rust-mode
+    :hook ((emacs-lisp-mode
+            clojure-mode
+            scheme-mode
+            lisp-mode
+            racket-mode
+            hy-mode) . parinfer-rust-mode)
+    :init
+    (setq! parinfer-rust-library (concat user-emacs-directory
+                                         ".local/etc/parinfer-rust/"
+                                         parinfer-rust--doom-lib-name))
+    :config
+    (map! :map parinfer-rust-mode-map
+          :localleader
+          :desc "Switch parinfer-rust-mode" "m" #'parinfer-rust-switch-mode
+          :desc "Temporarily disable parinfer-rust-mode" "M"
+                #'parinfer-rust-switch-mode)))
