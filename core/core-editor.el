d52abd37f79190e7cc20355a042938f90f2d1a24
Henrik Lissner <henrik@lissner.net>
2021-02-24 18:05:50 -0500

    Fix #4691: half revert 7e9911f642c9
    Seems this was not fixed upstream.

--- core/core-editor.el
+++ core/core-editor.el
@@ -435,8 +435,23 @@ files, so we replace calls to `pp' with the much faster `prin1'."
   (setq dtrt-indent-max-lines 2000)
 
   ;; always keep tab-width up-to-date
-  (push '(t tab-width) dtrt-indent-hook-generic-mapping-list))
+  (push '(t tab-width) dtrt-indent-hook-generic-mapping-list)
 
+  (defvar dtrt-indent-run-after-smie)
+  (defadvice! doom--fix-broken-smie-modes-a (orig-fn arg)
+    "Some smie modes throw errors when trying to guess their indentation, like
+`nim-mode'. This prevents them from leaving Emacs in a broken state."
+    :around #'dtrt-indent-mode
+    (let ((dtrt-indent-run-after-smie dtrt-indent-run-after-smie))
+      (letf! ((defun symbol-config--guess (beg end)
+                (funcall symbol-config--guess beg (min end 10000)))
+              (defun smie-config-guess ()
+                (condition-case e (funcall smie-config-guess)
+                  (error (setq dtrt-indent-run-after-smie t)
+                         (message "[WARNING] Indent detection: %s"
+                                  (error-message-string e))
+                         (message ""))))) ; warn silently
+        (funcall orig-fn arg)))))
 
 (use-package! helpful
   ;; a better *help* buffer
