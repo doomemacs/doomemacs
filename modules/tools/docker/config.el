3d114d635637c511c20f85c16f3615414ee3174d
Henrik Lissner <henrik@lissner.net>
2021-03-02 12:09:49 -0500

    tools/docker: default to evil normal state
    Evil-collection supports docker and its submodes, so these are no longer
    necessary.

--- modules/tools/docker/config.el
+++ modules/tools/docker/config.el
@@ -1,14 +1,5 @@
 ;;; tools/docker/config.el -*- lexical-binding: t; -*-
 
-(after! docker
-  (set-evil-initial-state!
-    '(docker-container-mode
-      docker-image-mode
-      docker-network-mode
-      docker-volume-mode
-      docker-machine-mode)
-    'emacs))
-
 (after! dockerfile-mode
   (set-docsets! 'dockerfile-mode "Docker")
 
