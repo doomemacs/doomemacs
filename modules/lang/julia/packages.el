f8ed309b50e8982315ecf3f68612289ca64923b6
Henrik Lissner <henrik@lissner.net>
2021-03-04 13:00:11 -0500

    lang/julia: include submodules for lsp-julia
    Mentioned in #4012

--- modules/lang/julia/packages.el
+++ modules/lang/julia/packages.el
@@ -7,4 +7,4 @@
 (when (featurep! +lsp)
   (if (featurep! :tools lsp +eglot)
       (package! eglot-jl :pin "84cff9d6ef1643f3eac6c9d620cc1e380a9847d9")
-    (package! lsp-julia :recipe (:nonrecursive t) :pin "c523c250c4bd2777203101ab417e9b7312472f46")))
+    (package! lsp-julia :pin "c523c250c4bd2777203101ab417e9b7312472f46")))
