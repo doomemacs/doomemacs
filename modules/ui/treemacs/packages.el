287e3e83b21a816ff70591ca915ae374ea300562
Henrik Lissner <henrik@lissner.net>
2021-03-06 00:23:12 -0500

    Bump :ui treemacs
    Alexander-Miller/treemacs@a7e2e43 -> Alexander-Miller/treemacs@6857816
    
    Include a fix for Treemacs + TRAMP (broken by
    Alexander-Miller/treemacs@6c43117)
    
    Closes #4735

--- modules/ui/treemacs/packages.el
+++ modules/ui/treemacs/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/treemacs/packages.el
 
-(package! treemacs :pin "a7e2e4388f3b588dff64536c811961b7f7945d13")
+(package! treemacs :pin "685781676acdca61b40f1932890230a741f2b82d")
 ;; These packages have no :pin because they're in the same repo
 (when (featurep! :editor evil +everywhere)
   (package! treemacs-evil))
