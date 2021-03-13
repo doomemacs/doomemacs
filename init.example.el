f37489656475f2f8ecf86e94e9e981bf122c5ea8
Rudi Grinberg <me@rgrinberg.com>
2021-03-12 18:49:46 -0800

    Add module for the dhall config language
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- init.example.el
+++ init.example.el
@@ -120,6 +120,7 @@
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
+       ;;dhall
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
