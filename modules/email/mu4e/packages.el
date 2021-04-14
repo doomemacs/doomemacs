3a48a75a836dbc0235358e19827aae97b4857bdd
Peter Hoeg <peter@hoeg.com>
2021-04-14 21:08:29 +0800

    bump: :email mu4e

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; email/mu4e/packages.el
 
-(package! org-msg :pin "557d490ecbd80522a42f7b1fb6aaacca504d4512")
+(package! org-msg :pin "e57af9c057d97d14536cef08aca7a19bdf194830")
