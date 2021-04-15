ee1885eb9ba47a6506faa2125086f30f8318543b
Henrik Lissner <henrik@lissner.net>
2021-04-14 23:41:52 -0400

    Bump :lang ocaml
    diml/utop@a5ff52b -> diml/utop@711c246
    ocaml-ppx/ocamlformat@448ac7c -> ocaml-ppx/ocamlformat@0ad8d0a
    ocaml/dune@a88ce5b -> ocaml/dune@65e04ba
    ocaml/merlin@cc17ed6 -> ocaml/merlin@635923d
    
    Fixes #4864 -- merlin-company was moved to its own package.

--- modules/lang/ocaml/packages.el
+++ modules/lang/ocaml/packages.el
@@ -4,21 +4,22 @@
 (package! tuareg :pin "37a673020152ae0dbcaa250118b155d84e448f68")
 
 (unless (featurep! +lsp)
-  (package! merlin :pin "cc17ed60630fb1831ad950fe62970b1c7a7f4c8b")
+  (package! merlin :pin "635923da0771cc0cb7154d3fc58e348e9148766d")
   (package! merlin-eldoc :pin "db7fab1eddfe34781b7e79694f8923b285698032")
+  (package! merlin-company :pin "635923da0771cc0cb7154d3fc58e348e9148766d")
   (when (featurep! :checkers syntax)
     (package! flycheck-ocaml :pin "8707a7bf545a8639a6a5c600a98d9a2ea1487dc9")))
 
 (package! ocp-indent :pin "9e26c0a2699b7076cebc04ece59fb354eb84c11c")
 
 (when (featurep! :tools eval)
-  (package! utop :pin "a5ff52bbf608e1112b5c0d41a36e3267f39f4084"))
+  (package! utop :pin "711c24661ce625070f8981fab9c6f31ce72b0a52"))
 
 (when (featurep! :editor format)
   (package! ocamlformat
     :recipe (:host github :repo "ocaml-ppx/ocamlformat" :files ("emacs/*.el"))
-    :pin "448ac7c1496e2a6e83d63a7bcd9cf4e35bb2b1fb"))
+    :pin "0ad8d0a5a55e28e425fdc89e220274447500f0d2"))
 
 (package! dune
   :recipe (:host github :repo "ocaml/dune" :files ("editor-integration/emacs/*.el"))
-  :pin "a88ce5bbc996b550071c8df890276eb1b10e778c")
+  :pin "65e04ba5646e8ba4a033b099c92fbda9b9aca341")
