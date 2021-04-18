7423711fa9443f09f450cae86169eeee0daa1c51
Luis Rangel <xlarsx@users.noreply.github.com>
2021-04-18 15:45:16 +0200

    fix(cloudformation): cfn-lint behind feature flag

--- modules/tools/cloudformation/doctor.el
+++ modules/tools/cloudformation/doctor.el
@@ -1,8 +1,9 @@
 ;;; tools/cloudformation/doctor.el -*- lexical-binding: t; -*-
 
-(unless (executable-find "cfn-lint")
-  (warn! "Couldn't find cfn-lint."))
+(when (featurep! :checkers syntax)
+  (unless (executable-find "cfn-lint")
+    (warn! "Couldn't find cfn-lint."))
 
-;; The binary is with underscore but the name is with hyphen
-(unless (executable-find "cfn_nag")
-  (warn! "Couldn't find cfn-nag."))
+  ;; The binary is with underscore but the name is with hyphen
+  (unless (executable-find "cfn_nag")
+    (warn! "Couldn't find cfn-nag.")))
