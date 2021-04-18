a0de71dee8330e4112bcb34d83fac25a67538bed
Henrik Lissner <henrik@lissner.net>
2021-04-18 12:41:34 -0400

    Fix #4886: fix +ivy-buffer-preview for SPC b B

--- modules/completion/ivy/config.el
+++ modules/completion/ivy/config.el
@@ -45,6 +45,9 @@ results buffer.")
     [remap switch-to-buffer-other-window] #'+ivy/switch-buffer-other-window
     [remap persp-switch-to-buffer]        #'+ivy/switch-workspace-buffer
     [remap evil-show-jumps]               #'+ivy/jump-list)
+
+  ;; Fix #4886: otherwise our remaps are overwritten
+  (setq ivy-mode-map (make-sparse-keymap))
   :config
   ;; The default sorter is much to slow and the default for `ivy-sort-max-size'
   ;; is way too big (30,000). Turn it down so big repos affect project
