bddb2b2a2dac17a51e37b99ed408abcb942d3f12
Henrik Lissner <henrik@lissner.net>
2021-04-20 22:34:59 -0400

    Refactor early-init.el

--- early-init.el
+++ early-init.el
@@ -19,22 +19,23 @@
 (setq package-enable-at-startup nil)
 (fset #'package--ensure-init-file #'ignore)  ; DEPRECATED Removed in 28
 
-;; `file-name-handler-alist' is consulted on every `require', `load' and various
-;; path/io functions. You get a minor speed up by nooping this. However, this
-;; may cause problems on builds of Emacs where its site lisp files aren't
-;; byte-compiled and we're forced to load the *.el.gz files (e.g. on Alpine)
-(unless (daemonp)
-  (defvar doom--initial-file-name-handler-alist file-name-handler-alist)
-  (setq file-name-handler-alist nil)
-  ;; Restore `file-name-handler-alist' later, because it is needed for handling
-  ;; encrypted or compressed files, among other things.
-  (defun doom-reset-file-handler-alist-h ()
-    ;; Re-add rather than `setq', because changes to `file-name-handler-alist'
-    ;; since startup ought to be preserved.
-    (dolist (handler file-name-handler-alist)
-      (add-to-list 'doom--initial-file-name-handler-alist handler))
-    (setq file-name-handler-alist doom--initial-file-name-handler-alist))
-  (add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h))
+(unless (or (daemonp) noninteractive)
+  (let ((old-file-name-handler-alist file-name-handler-alist))
+    ;; `file-name-handler-alist' is consulted on each `require', `load' and
+    ;; various path/io functions. You get a minor speed up by unsetting this.
+    ;; Some warning, however: this could cause problems on builds of Emacs where
+    ;; its site lisp files aren't byte-compiled and we're forced to load the
+    ;; *.el.gz files (e.g. on Alpine).
+    (setq-default file-name-handler-alist nil)
+    ;; ...but restore `file-name-handler-alist' later, because it is needed for
+    ;; handling encrypted or compressed files, among other things.
+    (defun doom-reset-file-handler-alist-h ()
+      (setq file-name-handler-alist
+            ;; Merge instead of overwrite because there may have bene changes to
+            ;; `file-name-handler-alist' since startup we want to preserve.
+            (delete-dups (append file-name-handler-alist
+                                 old-file-name-handler-alist))))
+    (add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h)))
 
 ;; Ensure Doom is running out of this file's directory
 (setq user-emacs-directory (file-name-directory load-file-name))
