c9b41205b2e69becf242d4f6875526c8004239c5
Henrik Lissner <henrik@lissner.net>
2021-04-25 18:33:23 -0400

    Fix #4925: defining lexical variables error in 28+

--- core/autoload/projects.el
+++ core/autoload/projects.el
@@ -1,10 +1,13 @@
 ;;; core/autoload/projects.el -*- lexical-binding: t; -*-
 
-(defvar projectile-project-root)
-(defvar projectile-enable-caching)
-(defvar projectile-require-project-root)
-
-;;;###autoload (autoload 'projectile-relevant-known-projects "projectile")
+;; HACK We forward declare these variables because they are let-bound in a
+;;      number of places with no guarantee that they've been defined yet (i.e.
+;;      that `projectile' is loaded). If a variable is defined with `defvar'
+;;      while it is lexically bound, you get "Defining as dynamic an already
+;;      lexical var" errors in Emacs 28+).
+;;;###autoload (defvar projectile-project-root nil)
+;;;###autoload (defvar projectile-enable-caching doom-interactive-p)
+;;;###autoload (defvar projectile-require-project-root 'prompt)
 
 ;;;###autodef
 (cl-defun set-project-type! (name &key predicate compile run test configure dir)
