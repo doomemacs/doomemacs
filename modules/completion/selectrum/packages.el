c1cbfa03f70b09e5f37177f6fb06cb356e65f2c3
Itai Y. Efrat <itai3397@gmail.com>
2021-05-01 14:38:54 +0300

    selectrum: use orderless by default

--- modules/completion/selectrum/packages.el
+++ modules/completion/selectrum/packages.el
@@ -3,10 +3,8 @@
 
 (package! selectrum :pin "093f7e96a323179ee2a68a5a674e7fa2c5d721b8")
 
-(when (featurep! +prescient)
-  (package! selectrum-prescient :pin "ed2b762241bbea03e374dc9dcd4fbe207c6b2ea4"))
-
-(when (featurep! +orderless)
+(if (featurep! +prescient)
+    (package! selectrum-prescient :pin "ed2b762241bbea03e374dc9dcd4fbe207c6b2ea4")
   (package! orderless :pin "87ab7e47e343285f7afd42779c78551332b8fd84"))
 
 (package! consult :pin "e04a404c8d8ca137be2b3b7cf664a11712639c31")
