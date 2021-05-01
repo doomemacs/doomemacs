bcd371e831128767aed6952de7be5b40e7aee336
Itai Y. Efrat <itai3397@gmail.com>
2021-05-01 16:15:50 +0300

    Bump bibtex-actions
    bdarcus/bibtex-actions@743f548 -> bdarcus/bibtex-actions@b1ddbb3

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -7,4 +7,4 @@
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
 (when (featurep! :completion selectrum)
-  (package! bibtex-actions :pin "743f548c0cd46e3418a7ca4736bde8c86f97c073"))
+  (package! bibtex-actions :pin "b1ddbb32373ac01b6bb46dfc4cdc143461e3c14c"))
