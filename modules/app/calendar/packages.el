4afdd4f78899dbb5e30fad8b76281c77bdb2bc1b
Henrik Lissner <henrik@lissner.net>
2021-05-01 01:22:39 -0400

    Bump :app
    jorgenschaefer/circe@e67e2d1 -> jorgenschaefer/circe@2f70aa2
    kidd/org-gcal.el@d38acda -> kidd/org-gcal.el@4c2616a
    tecosaur/emacs-everywhere@01e031e -> tecosaur/emacs-everywhere@64ba2e3

--- modules/app/calendar/packages.el
+++ modules/app/calendar/packages.el
@@ -5,4 +5,4 @@
 (package! calfw-org :pin "03abce97620a4a7f7ec5f911e669da9031ab9088")
 (package! calfw-cal :pin "03abce97620a4a7f7ec5f911e669da9031ab9088")
 (package! calfw-ical :pin "03abce97620a4a7f7ec5f911e669da9031ab9088")
-(package! org-gcal :pin "d38acdad70883dc3fe34a4cd41024e5a8099ddb2")
+(package! org-gcal :pin "4c2616a4f85adc77b91aa054bb10e76b06f706d5")
--- modules/app/irc/packages.el
+++ modules/app/irc/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/irc/packages.el
 
-(package! circe :pin "e67e2d1149ebf3e79cd2162e78802af3ed5f82da")
+(package! circe :pin "2f70aa236878d9c3726c31d6ba922e2d7076951d")
 (package! circe-notifications :pin "291149ac12877bbd062da993479d3533a26862b0")
