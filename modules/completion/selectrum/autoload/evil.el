fa527c00e86d6bbb4b5522e57a42bb253376d27f
Itai Y. Efrat <itai3397@gmail.com>
2021-05-03 11:13:28 +0300

    selectrum: fix project search evil command autoloads

--- modules/completion/selectrum/autoload/evil.el
+++ modules/completion/selectrum/autoload/evil.el
@@ -1,13 +1,13 @@
 ;; completion/selectrum/autoload/evil.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! :editor evil)
 
-;;;###autoload (autoload '+selectrum:project-search "completion/ivy/autoload/evil" nil t)
+;;;###autoload (autoload '+selectrum:project-search "completion/selectrum/autoload/evil" nil t)
 (evil-define-command +selectrum:project-search (query &optional all-files-p)
   "Ex interface for `+selectrum/project-search'."
   (interactive "<a><!>")
   (+selectrum/project-search all-files-p query))
 
-;;;###autoload (autoload '+selectrum:project-search-from-cwd "completion/ivy/autoload/evil" nil t)
+;;;###autoload (autoload '+selectrum:project-search-from-cwd "completion/selectrum/autoload/evil" nil t)
 (evil-define-command +selectrum:project-search-from-cwd (query &optional recurse-p)
   "Ex interface for `+selectrum/project-search-from-cwd'."
   (interactive "<a><!>")
