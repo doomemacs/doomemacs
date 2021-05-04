06d3d212fa8822502294bfe2bbf3efab8c95aae6
William Vaughn <vaughnwilld@gmail.com>
2021-05-03 23:40:04 -0700

    update org-mime

--- modules/email/notmuch/packages.el
+++ modules/email/notmuch/packages.el
@@ -2,7 +2,7 @@
 ;;; email/notmuch/packages.el
 
 (package! notmuch :pin "45193bab16c728ba892a5d45fc62ef59e2a6ef85")
-(package! org-mime :pin "9bb6351b25c62835c7881fc64096028eb8ef83ef")
+(package! org-mime :pin "eb21c02ba8f97fe69c14dc657a7883b982664649")
 (when (featurep! :completion ivy)
   (package! counsel-notmuch :pin "a4a1562935e4180c42524c51609d1283e9be0688"))
 (when (featurep! :completion helm)
