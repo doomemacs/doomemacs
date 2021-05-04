ed4b1ef8879d20868504805d838fd04a45011e84
Henrik Lissner <henrik@lissner.net>
2021-05-04 16:07:12 -0400

    Bump :email
    https://git.notmuchmail.org/git/notmuch@45193ba -> https://git.notmuchmail.org/git/notmuch@63413a5
    jeremy-compostella/org-msg@557d490 -> jeremy-compostella/org-msg@b0765b2
    org-mime/org-mime@9bb6351 -> org-mime/org-mime@eb21c02
    wanderlust/flim@068b35e -> wanderlust/flim@ddf5b6e
    wanderlust/semi@f279ebe -> wanderlust/semi@20d7530
    wanderlust/wanderlust@5db307a -> wanderlust/wanderlust@6e189fc
    
    Closes #4976

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -5,7 +5,7 @@
 ;;      depends on flim, flim on apel), but they all have non-standard default
 ;;      branches which straight cannot detect without our help.
 (package! apel :recipe (:branch "apel-wl") :pin "4e3269b6e702db2dba48cf560563ac883e81e3bf")
-(package! flim :recipe (:branch "flim-1_14-wl") :pin "068b35e863be9245e717e79def55f93580188bc8")
-(package! semi :recipe (:branch "semi-1_14-wl") :pin "f279ebe1c1f9c14bdd5d3da01af24277a6167b69")
+(package! flim :recipe (:branch "flim-1_14-wl") :pin "ddf5b6eceb73d7dbf6ff3a2d5281a2957cc2b836")
+(package! semi :recipe (:branch "semi-1_14-wl") :pin "20d75302509b5fba9849e74b61c1ce93e5819864")
 
-(package! wanderlust :pin "5db307a0441b6b7aa4ecfd34344842d78f15611b")
+(package! wanderlust :pin "6e189fc944a9bbde76c5a6d9b6a38d57e85e6390")
