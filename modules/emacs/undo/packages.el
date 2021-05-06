b12a0c02b3d4cb722f28fe5f59f54604cf771f24
Henrik Lissner <henrik@lissner.net>
2021-05-06 15:34:06 -0400

    Bump :emacs undo
    ideasman42/emacs-undo-fu@f4db4c9 -> ideasman42/emacs-undo-fu@e0ad06b
    ideasman42/emacs-undo-fu-session@a038914 -> ideasman42/emacs-undo-fu-session@243d93b
    
    Also refactors module to conform to conventions.

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -3,5 +3,5 @@
 
 (if (featurep! +tree)
     (package! undo-tree :pin "e326c6135e62f5fe8536528d3acd5e798f847407")
-  (package! undo-fu :pin "f4db4c9b9875134df6f5279281099361ae11c2e9")
-  (package! undo-fu-session :pin "a0389147365c10c974ad68b797b185affb935fe3"))
+  (package! undo-fu :pin "e0ad06b5ef2ac2733dad2ad48e3957b5c36edfa5")
+  (package! undo-fu-session :pin "243d93b4c7c1224e7067cd323f64d23dfdfe7c0e"))
