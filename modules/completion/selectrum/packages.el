1f1db328a07a6a06611e26ae784b8185d0fdcad1
Itai Y. Efrat <itai3397@gmail.com>
2021-05-16 03:03:32 +0300

    Bump :completion selectrum
    raxod502/selectrum@8629ab5 -> raxod502/selectrum@bfefb8e
    oantolin/orderless@87ab7e4 -> oantolin/orderless@d97a91f
    minad/consult@51c1437 -> minad/consult@c839b82
    oantolin/embark@05aa11b -> oantolin/embark@7c850c0
    minad/marginalia@ac4ab98 -> minad/marginalia@445d283

--- modules/completion/selectrum/packages.el
+++ modules/completion/selectrum/packages.el
@@ -1,19 +1,19 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/selectrum/packages.el
 
-(package! selectrum :pin "8629ab5a6de572ada9dd5b18162a393969d9ebdf")
+(package! selectrum :pin "bfefb8e1a350d44b56290b2c7ddc3418ec217b30")
 
 (if (featurep! +prescient)
     (package! selectrum-prescient :pin "4a0f5405798cfcb98ea005078ef2e2d490e922c4")
-  (package! orderless :pin "87ab7e47e343285f7afd42779c78551332b8fd84"))
+  (package! orderless :pin "d97a91f6e12ace638e65bdccefd14d1e638a2dae"))
 
-(package! consult :pin "51c1437fb555f4ff7234ed01c71e29f80138156e")
+(package! consult :pin "c839b82ce3e4db3faaecf3e9e5e2d98ad010eede")
 (when (featurep! :checkers syntax)
-  (package! consult-flycheck :pin "51c1437fb555f4ff7234ed01c71e29f80138156e"))
+  (package! consult-flycheck :pin "c839b82ce3e4db3faaecf3e9e5e2d98ad010eede"))
 
-(package! embark :pin "05aa11bca37db1751c86fe78f784741be5b1a066")
-(package! embark-consult :pin "05aa11bca37db1751c86fe78f784741be5b1a066")
+(package! embark :pin "7c850c07bfe29f3232ebb22793e8421772f820f1")
+(package! embark-consult :pin "7c850c07bfe29f3232ebb22793e8421772f820f1")
 
-(package! marginalia :pin "ac4ab987126c0366b876e7fdcfa797822dd3580b")
+(package! marginalia :pin "445d2832a2f06484ad28d9b55676c52d63cd0a46")
 
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
