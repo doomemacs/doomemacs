39b2ce4976bb886743c7877c781b4c27ef2fe73b
Prashant Vithani <prashantvithani@gmail.com>
2021-05-18 00:24:48 +0530

    Bump :ui doom-themes
    hlissner/emacs-doom-themes@4199e74 -> hlissner/emacs-doom-themes@4d24728

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "4199e74db170200995ee8dfbb55ffae004d6e219")
+(package! doom-themes :pin "4d24728f11853b5bd231687ac3f7a95108688313")
 (package! solaire-mode :pin "9d143db85428c5a7798e429f3b8abd2bf3f80747")
