cd6d33c4a3af23a7fe2d05b7321537c1d7661898
Henrik Lissner <henrik@lissner.net>
2021-05-22 00:12:39 -0400

    Bump :ui doom
    hlissner/emacs-doom-themes@4d24728 -> hlissner/emacs-doom-themes@9996e1b
    
    Includes a lot of bugfixes (and to adapt to changes in solaire-mode).

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "4d24728f11853b5bd231687ac3f7a95108688313")
+(package! doom-themes :pin "9996e1b5d54017a196225c134094d7508b5a167a")
 (package! solaire-mode :pin "186f97d7dddc85131fb1e7be5f89de226f26ae51")
