cf2a68f060e5b0c439e2f90fb00c4e16cf82fc41
Itai Y. Efrat <itai3397@gmail.com>
2021-05-25 16:44:32 +0300

    Bump :app irc
    jorgenschaefer/circe@2f70aa2 -> jorgenschaefer/circe@31ac6ba
    
    adds Libera Chat as the default network

--- modules/app/irc/packages.el
+++ modules/app/irc/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/irc/packages.el
 
-(package! circe :pin "2f70aa236878d9c3726c31d6ba922e2d7076951d")
+(package! circe :pin "31ac6ba777a92715ce8b0150854a38586de491ff")
 (package! circe-notifications :pin "291149ac12877bbd062da993479d3533a26862b0")
