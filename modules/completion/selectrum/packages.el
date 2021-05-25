59ca6186d226dc0c7f3a1155fa57cf470a2ffa07
Itai Y. Efrat <itai3397@gmail.com>
2021-05-26 02:09:58 +0300

    Bump :completion selectrum
    minad/consult@4ca7747 -> minad/consult@a4b4ced
    minad/consult-flycheck@4ca7747 -> minad/consult-flycheck@44e7528
    oantolin/embark@22875aa -> oantolin/embark@0da967a
    minad/marginalia@624028c -> minad/marginalia@3f33b38
    
    Note that before this bump consult-flycheck was in the consult repo, but
    has since been moved to it's own repo due to consult's pending
    submission to ELPA

--- modules/completion/selectrum/packages.el
+++ modules/completion/selectrum/packages.el
@@ -7,13 +7,13 @@
     (package! selectrum-prescient :pin "4a0f5405798cfcb98ea005078ef2e2d490e922c4")
   (package! orderless :pin "9637d7fd59f76a5b6d37470b1543ab827a0f9b8d"))
 
-(package! consult :pin "4ca77477e980df954d75a5abde0e6584365bf404")
+(package! consult :pin "a4b4ced2feda2153af2ba41fc6ee5453bac3d64f")
 (when (featurep! :checkers syntax)
-  (package! consult-flycheck :pin "4ca77477e980df954d75a5abde0e6584365bf404"))
+  (package! consult-flycheck :pin "44e7528d3a536755e731688c9ad9b5480b0eb880"))
 
-(package! embark :pin "22875aa5bda21b588487b719982cbaf8410830da")
-(package! embark-consult :pin "22875aa5bda21b588487b719982cbaf8410830da")
+(package! embark :pin "0da967adf0b1c17c59d1c0a1c166c983afe640b2")
+(package! embark-consult :pin "0da967adf0b1c17c59d1c0a1c166c983afe640b2")
 
-(package! marginalia :pin "624028c69b55deb3387452b9eeabe9cb963bd2a4")
+(package! marginalia :pin "3f33b38b7c1ecd7086942e1bd8284c54a6fd30a3")
 
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
