364e74a96b28b56f933571efcb0b43f33aa71a2d
Henrik Lissner <henrik@lissner.net>
2021-05-25 13:37:19 -0400

    Bump :emacs vc :tools magit
    magit/forge@37aa4e4 -> magit/forge@551e515
    magit/git-modes@14adca2 -> magit/git-modes@7678ead
    magit/magit@e378827 -> magit/magit@b68a760
    rmuslimov/browse-at-remote@fadf99d -> rmuslimov/browse-at-remote@e02ad21
    
    Fixes #5095: breakage due to upstream refactors

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,8 +5,8 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "fadf99d6d8e891f3b112e36c772e0eea0b9bc7f2")
-(package! git-commit :pin "577f16da3072a0ae85fba6b25a36a971a61ec6c2")
+(package! browse-at-remote :pin "e02ad2189c87da33f80bf4967a968772ce3e4431")
+(package! git-commit :pin "b68a760c9e7694c687adedec7dffab0a5609ea93")
 (package! git-timemachine :pin "8d675750e921a047707fcdc36d84f8439b19a907")
-(package! gitconfig-mode :pin "14adca24eb6b0b4e311ad144c5d41972c6b044b2")
-(package! gitignore-mode :pin "14adca24eb6b0b4e311ad144c5d41972c6b044b2")
+(package! gitconfig-mode :pin "7678ead3cdbb1692c9728b9730c016283ed97af1")
+(package! gitignore-mode :pin "7678ead3cdbb1692c9728b9730c016283ed97af1")
--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "e37882710bd91d95bac967d342e5aea846cce783")
+(when (package! magit :pin "b68a760c9e7694c687adedec7dffab0a5609ea93")
   (when (featurep! +forge)
-    (package! forge :pin "37aa4e4b82a99246b3551daee6104dc1d192174a"))
+    (package! forge :pin "551e51511e25505d14e05699a1707fd57e394a9a"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "78d24cf419138b543460f40509c8c1a168b52ca0")
   (package! github-review :pin "341b7a1352e4ee1f1119756360ac0714abbaf460"))
