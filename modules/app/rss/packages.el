05d85f5ef75987860238635a0d10c84f8b52e348
Henrik Lissner <henrik@lissner.net>
2021-05-27 23:06:12 -0400

    Bump :app
    https://git.savannah.gnu.org/git/emms.git@60c6172 -> https://git.savannah.gnu.org/git/emms.git@06ef243
    jorgenschaefer/circe@2f70aa2 -> jorgenschaefer/circe@4778675
    remyhonig/elfeed-org@77b6bbf -> remyhonig/elfeed-org@268efdd
    
    Closes #5094
    
    Co-authored-by: iyefrat <iyefrat@users.noreply.github.com>

--- modules/app/rss/packages.el
+++ modules/app/rss/packages.el
@@ -3,4 +3,4 @@
 
 (package! elfeed :pin "e29c8b91450bd42d90041231f769c4e5fe5070da")
 (when (featurep! +org)
-  (package! elfeed-org :pin "77b6bbf222487809813de260447d31c4c59902c9"))
+  (package! elfeed-org :pin "268efdd0121fa61f63b722c30e0951c5d31224a4"))
