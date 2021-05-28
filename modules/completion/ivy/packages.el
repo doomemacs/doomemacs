ad554b3826dd73a652c37ce988487a9890edd5a9
Henrik Lissner <henrik@lissner.net>
2021-05-28 17:58:40 -0400

    Bump :completion
    DarwinAwardWinner/ido-completing-read-plus@36a8bb9 -> DarwinAwardWinner/ido-completing-read-plus@0067472
    Yevgnen/ivy-rich@7b9b7b2 -> Yevgnen/ivy-rich@600b818
    abo-abo/swiper@bb7965a -> abo-abo/swiper@a5eade0
    company-mode/company-mode@7207cb1 -> company-mode/company-mode@ee3177c
    emacs-helm/helm@98af298 -> emacs-helm/helm@a7bd522
    emacs-jp/helm-c-yasnippet@28699d9 -> emacs-jp/helm-c-yasnippet@e214eec
    raxod502/prescient.el@8573df9 -> raxod502/prescient.el@4a0f540
    sebastiencs/company-box@ec8f446 -> sebastiencs/company-box@aa5f09a
    tumashu/ivy-posframe@3132719 -> tumashu/ivy-posframe@084cc59
    tumashu/posframe@fff21cc -> tumashu/posframe@739d8fd
    yyoncho/helm-icons@e4a2cd1 -> yyoncho/helm-icons@8d2f5e7

--- modules/completion/ivy/packages.el
+++ modules/completion/ivy/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/ivy/packages.el
 
-(package! swiper :pin "bb7965aa645982c9a80bd9e772538a210b645942")
+(package! swiper :pin "a5eade0c77b7dd78cdea0f246683c6cb00f7b7c6")
 (package! ivy)
 (package! ivy-hydra)
 (package! ivy-avy)
@@ -9,16 +9,16 @@
 
 (package! amx :pin "37f9c7ae55eb0331b27200fb745206fc58ceffc0")
 (package! counsel-projectile :pin "06b03c1080d3ccc3fa9b9c41b1ccbcf13f058e4b")
-(package! ivy-rich :pin "7b9b7b20c3ead81da90232cd6707dfad3c1f1eb3")
+(package! ivy-rich :pin "600b8183ed0be8668dcc548cc2c8cb94b001363b")
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
 
 (if (featurep! +prescient)
-    (package! ivy-prescient :pin "8573df977eaceffc6607b7242ff8c0dab02aad65")
+    (package! ivy-prescient :pin "4a0f5405798cfcb98ea005078ef2e2d490e922c4")
   (when (featurep! +fuzzy)
     (package! flx :pin "647cb2f92f9936c62e277d7a74ad54a80502d255")))
 
 (when (featurep! +childframe)
-  (package! ivy-posframe :pin "3132719a9a7c04431fe65f1dced8acafe789bdf6"))
+  (package! ivy-posframe :pin "084cc59ea2cd62afaa51445ada3d00404749a541"))
 
 (when (featurep! +icons)
   (package! all-the-icons-ivy :pin "a70cbfa1effe36efc946a823a580cec686d5e88d"))
