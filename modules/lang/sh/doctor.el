deb552826a034ac91f7b1dafa54c200f68da5e2e
Colin Woodbury <colin@fosskers.ca>
2021-06-01 10:15:20 -0700

    doctor: Warn if `shfmt` is missing

--- modules/lang/sh/doctor.el
+++ modules/lang/sh/doctor.el
@@ -3,3 +3,7 @@
 (when (featurep! :checkers syntax)
  (unless (executable-find "shellcheck")
   (warn! "Couldn't find shellcheck. Shell script linting will not work")))
+
+(when (featurep! :editor format)
+  (unless (executable-find "shfmt")
+    (warn! "Couldn't find shfmt. Code formatting in sh buffers will not work.")))
