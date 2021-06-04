99fbdb10927917f5fdc6d46750fec33c1f624e3d
Henrik Lissner <henrik@lissner.net>
2021-06-04 12:44:23 -0400

    lang/sh: make shfmt indent-style aware
    Fixes #2905

--- modules/lang/sh/config.el
+++ modules/lang/sh/config.el
@@ -16,6 +16,10 @@
   :config
   (set-docsets! 'sh-mode "Bash")
   (set-electric! 'sh-mode :words '("else" "elif" "fi" "done" "then" "do" "esac" ";;"))
+  (set-formatter! 'shfmt
+    '("shfmt" "-ci"
+      ("-i" "%d" (unless indent-tabs-mode tab-width))
+      ("-ln" "%s" (pcase sh-shell (`bash "bash") (`mksh "mksh") (_ "posix")))))
   (set-repl-handler! 'sh-mode #'+sh/open-repl)
   (set-lookup-handlers! 'sh-mode :documentation #'+sh-lookup-documentation-handler)
   (set-ligatures! 'sh-mode
