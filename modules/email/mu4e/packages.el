c9e816e9950d6fe9d49ba47d856523d091e89361
TEC <tec@tecosaur.com>
2021-06-19 03:28:25 +0800

    Bump org-msg
    jeremy-compostella/org-msg@b9b5b4e -> jeremy-compostella/org-msg@4c92c62
    
    Update to v3.8

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -2,7 +2,7 @@
 ;;; email/mu4e/packages.el
 
 (when (featurep! +org)
-  (package! org-msg :pin "b9b5b4ee9469969db9f150911d93b96fe84080d1"))
+  (package! org-msg :pin "4c92c627b6cfb234fd257b714a5dbfc72d7af8d2"))
 
 (package! mu4e-alert :pin "91f0657c5b245a9de57aa38391221fb5d141d9bd")
 
