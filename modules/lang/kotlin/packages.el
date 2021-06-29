7f87870099d32b026af55cdeb99a3c7127d7bea8
Connor Braa <cbraa@brex.com>
2021-06-29 10:02:51 -0700

    Bump flycheck-kotlin
    whirm/flycheck-kotlin@5104ee9a -> whirm/flycheck-kotlin@bf1b398b
    
    flycheck-kotlin: support newer versions of ktlint

--- modules/lang/kotlin/packages.el
+++ modules/lang/kotlin/packages.el
@@ -4,4 +4,4 @@
 (package! kotlin-mode :pin "0e4bafb31d1fc2a0a420a521c2723d5526646c0b")
 
 (when (featurep! :checkers syntax)
-  (package! flycheck-kotlin :pin "5104ee9a3fdb7f0a0a3d3bcfd8dd3c45a9929310"))
+  (package! flycheck-kotlin :pin "bf1b398bdde128806a0a7479ebbe369bbaa40dae"))
