3ccc12c16b9aba4d662ec8234a364571847b26dc
Henrik Lissner <henrik@lissner.net>
2021-07-06 02:16:25 -0400

    Add init.example.el one-liner for :tools lsp

--- init.example.el
+++ init.example.el
@@ -92,7 +92,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp
+       ;;lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
