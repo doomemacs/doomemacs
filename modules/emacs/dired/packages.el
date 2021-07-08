5be988ac31c82c8941b1d8292d4945ab11609e6b
Henrik Lissner <henrik@lissner.net>
2021-07-08 18:18:11 -0400

    Bump :emacs dired
    dgutov/diff-hl@1af31fe -> dgutov/diff-hl@992559f
    wyuenho/all-the-icons-dired@f401fe2 -> wyuenho/all-the-icons-dired@a758766
    yqrashawn/fd-dired@7d18938 -> yqrashawn/fd-dired@c223aee
    
    Fixes #4923: removes illegal --quoting-style=literal option passed to
    xargs by fd-dired package on BSD systems (like macOS).

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -3,10 +3,10 @@
 
 (package! diredfl :pin "4ca32658aebaf2335f0368a0fd08f52eb1aee960")
 (package! dired-git-info :pin "9461476a28a5fec0784260f6e318237c662c3430")
-(package! diff-hl :pin "1af31fe1c177646ef7419c95b5d9c25655187ceb")
+(package! diff-hl :pin "992559f98a1f0367ae2b73e94f69503da73f3a83")
 (package! dired-rsync :pin "fb0f161ac3cce1b224f52547f5bc7e1dcd283191")
 (when (featurep! +ranger)
   (package! ranger :pin "2498519cb21dcd5791d240607a72a204d1761668"))
 (when (featurep! +icons)
-  (package! all-the-icons-dired :pin "f401fe289cd93936e7747b1541aa98117b7ca96f"))
-(package! fd-dired :pin "7d18938751d047eef18bfb5975195419f0d1e2d3")
+  (package! all-the-icons-dired :pin "a758766878b6e8b9eaaf41d68599a2df99e37f48"))
+(package! fd-dired :pin "c223aee30af7dc7f52fb20045226ed9f49f4ec49")
