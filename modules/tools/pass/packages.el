04eb44a6104912342641839807230b21f72b76e6
Henrik Lissner <henrik@lissner.net>
2021-07-08 00:28:35 -0400

    tools/pass: remove auth-source-pass recipe
    A sufficiently up-to-date version of this package is built into Emacs
    27.1+. Since we dropped 26.x support in 044a1a5f2 this is no longer
    needed.
    
    Mentioned in https://discourse.doomemacs.org/t/1180

--- modules/tools/pass/packages.el
+++ modules/tools/pass/packages.el
@@ -5,12 +5,6 @@
 (package! password-store :pin "4e73cdc4c376ad61d2e4858bc87cf01eab0dd403")
 (package! password-store-otp :pin "04998c8578a060ab4a4e8f46f2ee0aafad4ab4d5")
 
-;; an older version of `auto-source-pass' is built into Emacs 26+, so we must
-;; install the new version directly from the source and with a psuedonym.
-(package! auth-source-pass
-  :recipe (:host github :repo "DamienCassou/auth-password-store")
-  :pin "fa8b964494c1ef42035fad340ff5f29fcdbed21c")
-
 (when (featurep! :completion ivy)
   (package! ivy-pass :pin "5b523de1151f2109fdd6a8114d0af12eef83d3c5"))
 (when (featurep! :completion helm)
