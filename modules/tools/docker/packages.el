15261875265d7f76d5eaa47c7acdfc8ffcd40b03
Henrik Lissner <henrik@lissner.net>
2021-07-09 17:37:59 -0400

    Bump :tools
    Silex/docker.el@bc2dc09 -> Silex/docker.el@9bcefbe
    cjohansson/emacs-ssh-deploy@fce4ea3 -> cjohansson/emacs-ssh-deploy@7c5fe8a
    emacs-lsp/dap-mode@4347846 -> emacs-lsp/dap-mode@01d9297
    emacs-lsp/lsp-mode@9aa22de -> emacs-lsp/lsp-mode@aec8968
    emacs-lsp/lsp-ui@9953a48 -> emacs-lsp/lsp-ui@cb02972
    emacs-pe/docker-tramp.el@8e2b671 -> emacs-pe/docker-tramp.el@aaee11c
    emacsorphanage/terraform-mode@a9fa5bd -> emacsorphanage/terraform-mode@e560caa
    jacktasia/dumb-jump@8f70acb -> jacktasia/dumb-jump@542e72d
    joaotavora/eglot@a5b7b7d -> joaotavora/eglot@5cc8df6
    magit/forge@551e515 -> magit/forge@b4fd066
    magit/magit@b68a760 -> magit/magit@9b7b25f
    tumashu/posframe@739d8fd -> tumashu/posframe@f97c4af
    xuchunyang/osx-dictionary.el@4d4cc19 -> xuchunyang/osx-dictionary.el@1a4479d
    zx2c4/password-store@4e73cdc -> zx2c4/password-store@3dd1469

--- modules/tools/docker/packages.el
+++ modules/tools/docker/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/docker/packages.el
 
-(package! docker :pin "bc2dc09edea167d4fdf022aa263bad71932bb0c4")
-(package! docker-tramp :pin "8e2b671eff7a81af43b76d9dfcf94ddaa8333a23")
+(package! docker :pin "9bcefbe54dbed4d364286fe65a87872e763ff7dc")
+(package! docker-tramp :pin "aaee11cedf7b4c31700f24a1fd88dcef9c2a7c3d")
 (package! dockerfile-mode :pin "ad06a41259ff2961d603bf23a3a8fbd22dde2161")
--- modules/tools/terraform/packages.el
+++ modules/tools/terraform/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/terraform/packages.el
 
-(package! terraform-mode :pin "a9fa5bdaf58e9cae32ee44b7d0883f5600441b05")
+(package! terraform-mode :pin "e560caaa9d9a11b0868adf6d9dcae5ebb5055730")
 (when (featurep! :completion company)
   (package! company-terraform :pin "2d11a21fee2f298e48968e479ddcaeda4d736e12"))
