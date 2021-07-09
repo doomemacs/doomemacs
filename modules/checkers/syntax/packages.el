9df3072e94bad7bd096e197ecd48917ab263b9f7
Henrik Lissner <henrik@lissner.net>
2021-07-09 17:40:54 -0400

    Bump :checkers
    bnbeckwith/writegood-mode@b71757e -> bnbeckwith/writegood-mode@ed42d91
    d12frosted/flyspell-correct@d19a090 -> d12frosted/flyspell-correct@4042336
    flycheck/flycheck@f8c679f -> flycheck/flycheck@21d5226
    ideasman42/emacs-spell-fu@30c5eea -> ideasman42/emacs-spell-fu@1abcb55

--- modules/checkers/syntax/packages.el
+++ modules/checkers/syntax/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; checkers/syntax/packages.el
 
-(package! flycheck :pin "f8c679fff349850c80541a31de50009c3c15d4c9")
+(package! flycheck :pin "21d52264aa80bfa4ede94c59e37a20fb6d033b0c")
 (package! flycheck-popup-tip :pin "ef86aad907f27ca076859d8d9416f4f7727619c6")
 (when (featurep! +childframe)
   (package! flycheck-posframe :pin "8f60c9bf124ab9597d681504a73fdf116a0bde12"))
