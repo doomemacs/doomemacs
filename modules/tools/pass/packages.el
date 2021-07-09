6997adb654f428b3c9e7dace3a7fea361d56c5d8
Henrik Lissner <henrik@lissner.net>
2021-07-09 17:37:59 -0400

    Bump :tools
    Silex/docker.el@bc2dc09 -> Silex/docker.el@9bcefbe
    cjohansson/emacs-ssh-deploy@fce4ea3 -> cjohansson/emacs-ssh-deploy@7c5fe8a
    emacs-lsp/dap-mode@4347846 -> emacs-lsp/dap-mode@01d9297
    emacs-lsp/lsp-mode@9aa22de -> emacs-lsp/lsp-mode@aec8968
    emacs-lsp/lsp-ui@9953a48 -> emacs-lsp/lsp-ui@cb02972
    emacs-pe/docker-tramp.el@8e2b671 -> emacs-pe/docker-tramp.el@aaee11c
    emacsorphanage/terraform-mode@a9fa5bd -> emacsorphanage/terraform-mode@e560caa
    jacktasia/dumb-jump@8f70acb -> jacktasia/dumb-jump@542e72d
    joaotavora/eglot@a5b7b7d -> joaotavora/eglot@5cc8df6
    magit/forge@551e515 -> magit/forge@b4fd066
    magit/magit@b68a760 -> magit/magit@9b7b25f
    tumashu/posframe@739d8fd -> tumashu/posframe@f97c4af
    xuchunyang/osx-dictionary.el@4d4cc19 -> xuchunyang/osx-dictionary.el@1a4479d
    zx2c4/password-store@4e73cdc -> zx2c4/password-store@3dd1469

--- modules/tools/pass/packages.el
+++ modules/tools/pass/packages.el
@@ -2,7 +2,7 @@
 ;;; tools/pass/packages.el
 
 (package! pass :pin "5651da53137db9adcb125b4897c2fe27eeb4368d")
-(package! password-store :pin "4e73cdc4c376ad61d2e4858bc87cf01eab0dd403")
+(package! password-store :pin "3dd14690c7c81ac80e32e942cf5976732faf0fb3")
 (package! password-store-otp :pin "04998c8578a060ab4a4e8f46f2ee0aafad4ab4d5")
 
 (when (featurep! :completion ivy)
