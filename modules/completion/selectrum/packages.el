debba414036464ff78a0ed7e50c1e3d59a3b843a
Itai Y. Efrat <itai3397@gmail.com>
2021-07-09 16:38:49 +0300

    Bump :completion selectrum
    minad/vertico@c915775 -> minad/vertico@9f6cd5d
    raxod502/selectrum@a19bbe9 -> raxod502/selectrum@48ea51a
    minad/consult@f1ae224 -> minad/consult@f17db95
    minad/marginalia@e31e03c -> minad/marginalia@3bf0a4d

--- modules/completion/selectrum/packages.el
+++ modules/completion/selectrum/packages.el
@@ -4,21 +4,21 @@
 (if (featurep! +vertico)
     (package! vertico
       :recipe (:host github :repo "minad/vertico")
-      :pin "c9157759a015ac32cb299c18c84c6d5fb34e0aa1")
-  (package! selectrum :pin "a19bbe94de492bf504399c093cfc5695eb630fa8"))
+      :pin "9f6cd5d431ec6d288676af80e932d928346a1b36")
+  (package! selectrum :pin "48ea51aa5b6959ea2a134e36cd21f727047b0677"))
 
 (if (featurep! +prescient)
     (package! selectrum-prescient :pin "4a0f5405798cfcb98ea005078ef2e2d490e922c4")
   (package! orderless :pin "2646dad28c0819fbe9ee521d39efb9ae40e03982"))
 
-(package! consult :pin "f1ae2244da20702525fe2991076322b9c6b34202")
+(package! consult :pin "f17db9520ddd612dc837f4112b6bcbb172acef85")
 (when (featurep! :checkers syntax)
   (package! consult-flycheck :pin "92b259e6a8ebe6439f67d3d7ffa44b7e64b76478"))
 
 (package! embark :pin "acbe1cba548832d295449da348719f69b9685c6f")
 (package! embark-consult :pin "acbe1cba548832d295449da348719f69b9685c6f")
 
-(package! marginalia :pin "e31e03c5857bf7aada333f693caedfc3087d6297")
+(package! marginalia :pin "3bf0a4db55f6267467f0a08715f4776509a3b503")
 
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
 
