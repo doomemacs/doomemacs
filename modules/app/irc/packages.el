bb04478e0994f1e225c0ff0219505a3073f82f91
Henrik Lissner <henrik@lissner.net>
2021-07-10 17:43:46 -0400

    Bump :app
    https://git.savannah.gnu.org/git/emms.git@06ef243 -> https://git.savannah.gnu.org/git/emms.git@6e0aaaf
    jorgenschaefer/circe@4778675 -> jorgenschaefer/circe@d6f1fa1
    skeeto/elfeed@e29c8b9 -> skeeto/elfeed@243add9

--- modules/app/irc/packages.el
+++ modules/app/irc/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/irc/packages.el
 
-(package! circe :pin "4778675e0c3bde1c028085b7d96693fe033d2c72")
+(package! circe :pin "d6f1fa18646f6ed2a1c0f06a4888130bd694ff19")
 (package! circe-notifications :pin "291149ac12877bbd062da993479d3533a26862b0")
