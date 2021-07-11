ab334559f5a88ca4d6ec1ab21e13075307800f0b
Henrik Lissner <henrik@lissner.net>
2021-07-10 22:35:10 -0400

    Bump :ui nav-flash
    rolandwalker/nav-flash@d763148 -> rolandwalker/nav-flash@55786c9

--- modules/ui/nav-flash/packages.el
+++ modules/ui/nav-flash/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/nav-flash/packages.el
 
-(package! nav-flash :pin "d76314802273153c2c38156250d75f95ca352482")
+(package! nav-flash :pin "55786c9582410a5637b5635fea022aae564205cd")
