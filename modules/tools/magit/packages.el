3bdae5bd20b0300908804c8dbb84b9b18de6f73c
Henrik Lissner <henrik@lissner.net>
2021-07-23 15:42:37 -0400

    Bump :tools magit
    magit/forge@b4fd066 -> magit/forge@49da45a
    magit/magit@9b7b25f -> magit/magit@0f4963b
    
    Fixes magit's handling of binary file headers (magit/magit@d3a729b1)

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "9b7b25f1e460f79de894f583727635134b7513a2")
+(when (package! magit :pin "0f4963b0ec68d96716a3f9aa9346bbe31611763b")
   (when (featurep! +forge)
-    (package! forge :pin "b4fd0666a4d3987fc41e08eda3f6b1db7b404697"))
+    (package! forge :pin "49da45ac4515d7442ebd606f4ad4922a3e1439ff"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "78d24cf419138b543460f40509c8c1a168b52ca0")
   (package! github-review :pin "341b7a1352e4ee1f1119756360ac0714abbaf460"))
