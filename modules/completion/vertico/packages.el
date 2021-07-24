ce1b9543e510ad1d4172944c50053f6f9dadf621
Itai Y. Efrat <itai3397@gmail.com>
2021-07-24 16:38:57 +0300

    Bump :completion vertico
    minad/consult@5fb6248 -> minad/consult@28f9ba8
    minad/marginalia@d4c2028 -> minad/marginalia@a3a8edb
    minad/vertico@e1faeb0 -> minad/vertico@4a90297
    oantolin/embark@9d56be1 -> oantolin/embark@be03ce9
    oantolin/orderless@2646dad -> oantolin/orderless@1e84120

--- modules/completion/vertico/packages.el
+++ modules/completion/vertico/packages.el
@@ -4,18 +4,18 @@
 (package! vertico
   :recipe (:host github :repo "minad/vertico"
            :files ("*.el" "extensions/*.el"))
-  :pin "e1faeb01ed379dd773116adbc22e8ca52f6f8162")
+  :pin "4a9029714e847832d3ecb3ae74a7049306924f2e")
 
-(package! orderless :pin "2646dad28c0819fbe9ee521d39efb9ae40e03982")
+(package! orderless :pin "1e84120a28525ccb47b602fc19b7afbeffbbe502")
 
-(package! consult :pin "5fb6248c8e12630ce1247985c67ea28ae4077e4f")
+(package! consult :pin "28f9ba8bdfdb13257862a658715b6ceb96f4951e")
 (when (featurep! :checkers syntax)
   (package! consult-flycheck :pin "92b259e6a8ebe6439f67d3d7ffa44b7e64b76478"))
 
-(package! embark :pin "9d56be162badbbfee405595f2ebdfe16a5bca47d")
-(package! embark-consult :pin "9d56be162badbbfee405595f2ebdfe16a5bca47d")
+(package! embark :pin "be03ce9ce1630b32e29cc50118d058c05696cb35")
+(package! embark-consult :pin "be03ce9ce1630b32e29cc50118d058c05696cb35")
 
-(package! marginalia :pin "d4c2028c7917b2ff926b3a67c3acc0351be658cc")
+(package! marginalia :pin "a3a8edbf25db4b1e167f1fdff6f60a065d0bf9cb")
 
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
 
