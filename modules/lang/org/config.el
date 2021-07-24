2855351b2c5507ac5b6be557d4fb71e6802d415b
Henrik Lissner <henrik@lissner.net>
2021-07-23 22:10:43 -0400

    lang/org: fix TAB override in GUI emacs
    Not sure if this was a recent change (28.0.50+), but a new conditional
    keybind in outline-mode-cycle-map was overriding TAB in insert mode in
    org-mode (in GUI Emacs).

--- modules/lang/org/config.el
+++ modules/lang/org/config.el
@@ -688,6 +688,11 @@ between the two."
             #'+org-delete-backward-char-and-realign-table-maybe-h)
 
   (map! :map org-mode-map
+        ;; Recently, a [tab] keybind in `outline-mode-cycle-map' has begun
+        ;; overriding org's [tab] keybind in GUI Emacs. This is needed to undo
+        ;; that, and should probably be PRed to org.
+        [tab]        #'org-cycle
+
         "C-c C-S-l"  #'+org/remove-link
         "C-c C-i"    #'org-toggle-inline-images
         ;; textmate-esque newline insertion
