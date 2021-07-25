eeb6c2d2c3d1fa9bd08c4cb640f256ea455ed7cb
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-25 05:10:00 -0400

    Add org-cite to :tools/biblio

--- modules/tools/biblio/config.el
+++ modules/tools/biblio/config.el
@@ -13,6 +13,63 @@
   :config
   (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-plus)))
 
+(use-package! citeproc
+  :defer t)
+
+;;; Org-Cite configuration
+
+(when (and (featurep! :lang org)
+            ;; Only run if org-cite present.
+            ;;
+            ;; REVIEW is this necessary? OC is new, but built-in.
+           (featurep 'oc))
+
+    ;;; Org-cite configuration
+  (use-package! oc
+    :after org
+    :config
+    ;; activate processor for fontification, preview, etc
+    ;; currently using basic, but would prefer org-cite-csl-activate
+    (setq org-cite-activate-processor 'basic)
+
+    (if (featurep! :completion vertico)
+        (setq org-cite-follow-processor 'oc-bibtex-actions
+              org-cite-insert-processor 'oc-bibtex-actions)
+      (setq org-cite-follow-processor 'basic
+            org-cite-insert-processor 'basic))
+
+    ;; setup export processor; default csl/citeproc-el, with biblatex for latex
+    (setq org-cite-export-processors
+          '((beamer natbib)
+            (latex biblatex)
+            (t csl)))
+
+    ;; need to set oc to use +biblio-default-bibliography-files
+    (setq org-cite-global-bibliography '("~/bib/references.bib")))
+
+  ;;; Org-cite processors
+  (use-package! oc-basic
+    :after oc)
+
+  (use-package! oc-biblatex
+    :after oc)
+
+  (use-package! oc-csl
+    :after oc
+    :config
+    ;; optional; add to docs instead?
+    (setq org-cite-csl-styles-dir "~/.local/share/csl/styles")
+    (setq org-cite-csl-locales-dir "~/.local/share/csl/locales"))
+
+  (use-package! oc-natbib
+    :after oc)
+
+  (use-package! oc-bibtex-actions
+    ;; REVIEW Currently, I (the author of this package) put org-cite-related
+    ;;   functions and variable setting in this separate package file. Is this
+    ;;   the best approach, or is there a better way?
+    :when (featurep! :completion vertico)
+    :after (embark org oc bibtex-actions)))
 
 (use-package! bibtex-actions
   :when (featurep! :completion vertico)
--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -1,10 +1,21 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/biblio/packages.el
 
-(package! bibtex-completion :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590")
+(package! bibtex-completion :pin "9f6ea920a49457d85096caa0e61f086a42b2908e")
+(package! citeproc :pin "0857973409e3ef2ef0238714f2ef7ff724230d1c")
+
 (when (featurep! :completion ivy)
-  (package! ivy-bibtex :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590"))
+  (package! ivy-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
 (when (featurep! :completion helm)
-  (package! helm-bibtex :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590"))
+  (package! helm-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
+<<<<<<< HEAD
+<<<<<<< HEAD
 (when (featurep! :completion vertico)
   (package! bibtex-actions :pin "6e3a194c3ab655693f8194be78542366755c58c9"))
+=======
+(when (featurep! :completion selectrum)
+=======
+(when (featurep! :completion vertico)
+>>>>>>> 83e43eb01 (completion/selectrum -> completion/vertico, part 2)
+  (package! bibtex-actions :pin "b1ddbb32373ac01b6bb46dfc4cdc143461e3c14c"))
+>>>>>>> bcd371e83 (Bump bibtex-actions)
