32dd99daa1076addd42ae9f9277690a0d6f6a5b1
Philip Heringlake <philip.heringlake@neel.cnrs.fr>
2021-07-28 09:34:16 +0200

    Bump :mail (mu4e) org-msg
    Bump jeremy-compostella/org-msg@b0765b2-> jeremy-compostella/org-msg@7cedeee
    
    jeremy-compostella/#118 allows adding attachments to org-msg mails from dired

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; email/mu4e/packages.el
 
-(package! org-msg :pin "b0765b2d0bc06cdd1fd78836ef958eb81e603dd1")
+(package! org-msg :pin "7cedeeece58879e05d3a3bab8af1385006f234df")
