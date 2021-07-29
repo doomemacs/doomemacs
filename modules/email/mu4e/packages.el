fe5e410b74ca1c267d64ddb644d0f0ff47c1d412
TEC <tec@tecosaur.com>
2021-07-29 18:55:58 +0800

    Mu4e: thread folding is too buggy, remove for now
    I bumped to get bugfixes and got more bugs. If the situation improves in
    future maybe we could add this back in.

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -5,6 +5,3 @@
   (package! org-msg :pin "4c92c627b6cfb234fd257b714a5dbfc72d7af8d2"))
 
 (package! mu4e-alert :pin "91f0657c5b245a9de57aa38391221fb5d141d9bd")
-
-(package! mu4e-thread-folding :recipe (:host github :repo "rougier/mu4e-thread-folding")
-  :pin "c6915585263a744b4da4a0e334393150603136dc")
