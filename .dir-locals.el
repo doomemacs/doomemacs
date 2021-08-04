2467729035e0779d37b593888ae05fe1a4a3527c
Henrik Lissner <henrik@lissner.net>
2021-08-04 01:20:15 -0400

    tweak: fill-column = 80 for project
    This is to prevent incorrect line length in code/doc contributions from
    users who have changed fill-column in their own configs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((nil      (git-commit-major-mode . git-commit-elisp-text-mode))
+((nil      (git-commit-major-mode . git-commit-elisp-text-mode)
+           (fill-column . 80))
  (org-mode (buffer-read-only . t)))
