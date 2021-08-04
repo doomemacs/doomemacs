11f7b9b41aab584066e9aaba7d06cc10303c7bcb
Itai Y. Efrat <itai3397@gmail.com>
2021-08-04 18:06:49 +0300

    docs(haskell): improve doctor checks
    Co-authored-by: Daanturo <daanturo@gmail.com>

--- modules/lang/haskell/doctor.el
+++ modules/lang/haskell/doctor.el
@@ -6,6 +6,21 @@
          "This module requires (:tools lsp)")
 
 (unless (executable-find "cabal")
-  (warn! "Couldn't find cabal, haskell-mode may have issues"))
-(unless (executable-find "hlint")
-  (warn! "Couldn't find hlint. Flycheck may have issues in haskell-mode"))
+  (warn! "Couldn't find cabal. haskell-mode may have issues."))
+
+(unless (executable-find "hoogle")
+  (warn! "Couldn't find hoogle. Documentation searching will not work."))
+
+(unless (or (featurep! +lsp)
+            (executable-find "hlint"))
+  (warn! "Couldn't find hlint. Flycheck may have issues in haskell-mode.
+  Install it or enable +lsp."))
+
+(when (and (featurep! :editor format)
+           (not (executable-find "brittany")))
+      (warn! "Couldn't find brittany. Code formatting will not work.
+  Install it or enable +lsp."))
+
+(when (and (featurep! +lsp)
+           (not (executable-find "haskell-language-server-wrapper")))
+  (warn! "Couldn't find haskell-language-server."))
