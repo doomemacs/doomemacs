08794a957fd4e5765074507a579144758fd06834
Prashant Vithani <prashantvithani@gmail.com>
2021-08-23 11:26:01 +0530

    Bump :ui modeline
    https://github.com/seagle0128/doom-modeline/commit/06606e0b8b3c19fbe56e25702e2a664deec593c3 -> https://github.com/seagle0128/doom-modeline/commit/749e860d4e18e67846b6a3ae2826fdad6abb839a
    
    Bug fix for incorrect VCS state for remote files

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "06606e0b8b3c19fbe56e25702e2a664deec593c3"))
+  (package! doom-modeline :pin "749e860d4e18e67846b6a3ae2826fdad6abb839a"))
 (package! anzu :pin "bdb3da5028935a4aea55c40769bc191a81afb54e")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "d3f6ed4773b48767bd5f4708c7f083336a8a8a86"))
