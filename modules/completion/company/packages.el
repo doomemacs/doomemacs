2ffb42e1da26e93c8ac47d7bf781c37d1119db0a
Sebastien Waegeneire <sebastien@waegeneire.com>
2021-08-26 14:57:07 +0200

    bump: :completion company-box
    sebastiencs/company-box/c8a867163b15 -> sebastiencs/company-box/156f65cfbf69
    
    bugfix for the current version

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -4,4 +4,4 @@
 (package! company :pin "d77184094b9a45b204813d824918e1ec2aac8504")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (featurep! +childframe)
-  (package! company-box :pin "c8a867163b15586cc9ed4accb992094308e54a9a"))
+  (package! company-box :pin "156f65cfbf690ed84e0e84f90277d665d873ff24"))
