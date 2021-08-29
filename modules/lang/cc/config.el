d43f260af64e07199144969e620e1b0ba4438a4c
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2021-08-29 11:09:32 +0200

    fix(cc): remove double lenses with lsp
    On current pins, ccls and lsp code lenses aren't mutually exclusive,
    and there is actually 2 sets of code lenses appearing

--- modules/lang/cc/config.el
+++ modules/lang/cc/config.el
@@ -276,7 +276,7 @@ If rtags or rdm aren't available, fail silently instead of throwing a breaking e
 (use-package! ccls
   :when (featurep! +lsp)
   :unless (featurep! :tools lsp +eglot)
-  :hook (lsp-lens-mode . ccls-code-lens-mode)
+  :defer t
   :init
   (defvar ccls-sem-highlight-method 'font-lock)
   (after! projectile
