accffb8f5e3f4e83746bcd175c8a857eabcec639
Aleksei Fedotov <lexa@cfotr.com>
2021-09-09 15:50:19 +0200

    bump!: :emacs vc
    rmuslimov/browse-at-remote@e02ad2189c87 -> rmuslimov/browse-at-remote@cef26f2c063f
    
    BREAKING CHANGE: browse-at-remote-remote-type-domains is replaced by
    browse-at-remote-remote-type-regexps. If you have a custom domain
    configured replace:
    
        (add-to-list 'browse-at-remote-remote-type-domains
          ("git.example.com" . "github")
    
    with
    
        (add-to-list 'browse-at-remote-remote-type-regexps
          ("^git\\.example\\.com$" . "github")
    
    Added support for Gittiles.
    
    Shortlog:
    
    Jack Rosenthal (3):
          Add support for Gitiles
          Fix broken tests
          More permissive regex in browse-at-remote--gerrit-url-cleanup
    
    Rustem Muslimov (2):
          Fix broken merge
          Fix savannah gnu message
    
    rmuslimov (3):
          Merge pull request #78 from jackrosenthal/gitiles
          Merge pull request #80 from jackrosenthal/broke_tests
          Merge pull request #81 from jackrosenthal/regex

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,7 +5,7 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "e02ad2189c87da33f80bf4967a968772ce3e4431")
+(package! browse-at-remote :pin "cef26f2c063f2473af42d0e126c8613fe2f709e4")
 (package! git-commit :pin "b68a760c9e7694c687adedec7dffab0a5609ea93")
 (package! git-timemachine :pin "8d675750e921a047707fcdc36d84f8439b19a907")
 (package! gitconfig-mode :pin "7678ead3cdbb1692c9728b9730c016283ed97af1")
