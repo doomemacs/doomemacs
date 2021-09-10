e67a02aa52a21c601b743787f0d0c668623a210e
Kirill A. Korinsky <kirill@korins.ky>
2021-09-10 14:57:56 +0200

    bump: :tools magit
    magit/magit@0f4963b0 -> magit/magit@73530108
    magit/forge@49da45ac -> magit/forge@9e7d6835
    alphapapa/magit-todos@78d24cf4 -> alphapapa/magit-todos@60152d5f
    
    Mainly for https://github.com/magit/forge/commit/a3e6f8aab16a8213cd389fe79d8c02300d26cab7

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "0f4963b0ec68d96716a3f9aa9346bbe31611763b")
+(when (package! magit :pin "73530108957c1167e155df9658d843c3bac0d9c7")
   (when (featurep! +forge)
-    (package! forge :pin "49da45ac4515d7442ebd606f4ad4922a3e1439ff"))
+    (package! forge :pin "9e7d6835b7eb3283eee84998de99af38cd9a2028"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
-  (package! magit-todos :pin "78d24cf419138b543460f40509c8c1a168b52ca0")
+  (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
   (package! github-review :pin "341b7a1352e4ee1f1119756360ac0714abbaf460"))
