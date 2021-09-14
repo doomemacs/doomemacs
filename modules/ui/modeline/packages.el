225f01d2f4c0e59714d7f3628a14c3b2fe6faad8
Henrik Lissner <henrik@lissner.net>
2021-09-14 13:17:50 +0200

    bump: :ui modeline
    seagle0128/doom-modeline@06606e0b8b3c -> seagle0128/doom-modeline@ef78fe07cd62
    
    - Fixes incorrect VCS state in remote files (#5408)
    - Fixes icons in TUI modeline (#5380)
    
    Close #5408
    Close #5380
    Fix #3514

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "06606e0b8b3c19fbe56e25702e2a664deec593c3"))
+  (package! doom-modeline :pin "ef78fe07cd62e87f1d4cf047ecb35a1780e2384e"))
 (package! anzu :pin "bdb3da5028935a4aea55c40769bc191a81afb54e")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "d3f6ed4773b48767bd5f4708c7f083336a8a8a86"))
