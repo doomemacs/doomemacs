7d42c60f5aca45515d8be2dd35cdde6fe3914016
Itai Y. Efrat <itai3397@gmail.com>
2021-09-15 01:54:56 +0300

    refactor(haskell): move +lsp setup to config.el

--- modules/lang/haskell/+lsp.el
+++
@@ -1,8 +0,0 @@
-;;; lang/haskell/+lsp.el -*- lexical-binding: t; -*-
-
-(use-package! lsp-haskell
-  :after lsp-mode
-  :preface (add-hook 'haskell-mode-local-vars-hook #'lsp!)
-  :config
-  ;; Does some strange indentation if it pastes in the snippet
-  (setq-hook! 'haskell-mode-hook yas-indent-line 'fixed))
--- modules/lang/haskell/config.el
+++ modules/lang/haskell/config.el
@@ -43,7 +43,10 @@
         "H" #'haskell-hide-toggle-all))
 
 
-;;
-;;; Backends
-
-(when (featurep! +lsp) (load! "+lsp"))
+(use-package! lsp-haskell
+  :when (featurep! +lsp)
+  :after lsp-mode
+  :preface (add-hook 'haskell-mode-local-vars-hook #'lsp!)
+  :config
+  ;; Does some strange indentation if it pastes in the snippet
+  (setq-hook! 'haskell-mode-hook yas-indent-line 'fixed))
