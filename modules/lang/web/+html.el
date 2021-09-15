0ad7874a28db1a7d002322b665997401b8b06a38
Henrik Lissner <henrik@lissner.net>
2021-09-16 00:29:21 +0200

    feat(web): make web-mode recognize *.[lh]eex

--- modules/lang/web/+html.el
+++ modules/lang/web/+html.el
@@ -66,6 +66,7 @@
     (delq! nil web-mode-engines-auto-pairs))
 
   (add-to-list 'web-mode-engines-alist '("elixir" . "\\.eex\\'"))
+  (add-to-list 'web-mode-engines-alist '("phoenix" . "\\.[lh]eex\\'"))
 
   ;; Use // instead of /* as the default comment delimited in JS
   (setf (alist-get "javascript" web-mode-comment-formats nil nil #'equal)
