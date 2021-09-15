dbe604519fbc074263cb113172d2458bfc1e571e
Henrik Lissner <henrik@lissner.net>
2021-09-15 17:38:21 +0200

    bump: :core
    Fuco1/smartparens@911cc896a0f2 -> Fuco1/smartparens@2834c66c4f09
    bbatsov/projectile@da08a9103b5b -> bbatsov/projectile@17c152b0628e
    domtronn/all-the-icons.el@9d97c074b080 -> domtronn/all-the-icons.el@c0d288a41fae
    gilbertw1/better-jumper@411ecdf6e7a3 -> gilbertw1/better-jumper@7f328a886ba4
    justbur/emacs-which-key@27d9fec33abb -> justbur/emacs-which-key@4790a14683a2
    raxod502/straight.el@b45dd00408ff -> raxod502/straight.el@45eb6112b2ea

--- core/packages.el
+++ core/packages.el
@@ -17,7 +17,7 @@
             :branch ,straight-repository-branch
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "b45dd00408ff8e922f2d7f75441fd5603e5222fa")
+  :pin "45eb6112b2ea1214bfbec35ea20df7a75147fc08")
 
 ;; core-modules.el
 (package! use-package
@@ -25,18 +25,18 @@
   :pin "a7422fb8ab1baee19adb2717b5b47b9c3812a84c")
 
 ;; core-ui.el
-(package! all-the-icons :pin "9d97c074b08000eacefc03ebc5dadbdc33888cc2")
+(package! all-the-icons :pin "c0d288a41faea2ecb7e8dd947486764a2ee17ec9")
 (package! hide-mode-line :pin "88888825b5b27b300683e662fa3be88d954b1cea")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "d576e6694ad3a3e88b2bb1363305b38fa364c149")
 (package! restart-emacs :pin "1607da2bc657fe05ae01f7fdf26f716eafead02c")
 
 ;; core-editor.el
-(package! better-jumper :pin "411ecdf6e7a3e1b4ced7605070d2309e5fc46556")
+(package! better-jumper :pin "7f328a886ba4dd01993d269eee01c8ee3d0ddf52")
 (package! dtrt-indent :pin "9714f2c5f1c9b7c21e732df8c15a870a88caba84")
 (package! helpful :pin "7e4b1f0d5572a4e2b8ee7a9b084ef863d0315a73")
 (package! pcre2el :pin "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
-(package! smartparens :pin "911cc896a0f2eb8b5fbdd6fc8331523ad9889a3a")
+(package! smartparens :pin "2834c66c4f09778d0c57e99886c329188eed591a")
 (package! ws-butler
   ;; Use my fork of ws-butler, which has a few choice improvements and
   ;; optimizations (the original has been abandoned).
@@ -44,9 +44,9 @@
   :pin "2bb49d3ee7d2cba133bc7e9cdac416cd1c5e4fe0")
 
 ;; core-projects.el
-(package! projectile :pin "da08a9103b5ba3b38ef031a9627a19436eb318b5")
-(package! project :pin "4072f35d85bf0a1c669329d66633e4819f497c1c")
+(package! projectile :pin "17c152b0628e1809f359492731b5af3aefc1c0d8")
+(package! project :pin "c6dc955f60825aabdd7d9248dcc388dad96932e1")
 
 ;; core-keybinds.el
 (package! general :pin "a0b17d207badf462311b2eef7c065b884462cb7c")
-(package! which-key :pin "27d9fec33abb989b030f7677ccf5f799287d6472")
+(package! which-key :pin "4790a14683a2f3e4f72ade197c78e4c0af1cdd4b")
