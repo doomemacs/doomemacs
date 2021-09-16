17e4791dab7cef5bddba867a4c6a7a0aaa5b53cf
A.I <merrick@luois.me>
2021-09-16 16:14:18 +0800

    feat(rest): add new feature flag +jq
    This enables restclient-mode to set variables
    by parsing response using jq.

--- modules/lang/rest/packages.el
+++ modules/lang/rest/packages.el
@@ -4,3 +4,11 @@
 (package! restclient :pin "176d9cb6552f04d98c33e29fc673862bdf3bca03")
 (when (featurep! :completion company)
   (package! company-restclient :pin "e5a3ec54edb44776738c13e13e34c85b3085277b"))
+
+(when (featurep! +jq)
+  (package! jq-mode :pin "42ad0a99f0114233e2cb317585cb9af494d18a2f")
+  (package! restclient-jq
+    :pin "2cc1fd3496f57288de3f97c27a5f018284db2d23"
+    :recipe (:host github
+             :repo "pashky/restclient.el"
+             :files ("restclient-jq.el"))))
