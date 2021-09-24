206063f27d60fe856981a35c0d275815c2058e7b
Henrik Lissner <henrik@lissner.net>
2021-09-25 00:54:13 +0200

    tweak(org): org-imenu-depth = 6
    Headings beyond 6 are excessive in almost any org document, and only
    muddy (and slow down) imenu search results, so I'm reducing it to the
    number of available HTML headings.

--- modules/lang/org/config.el
+++ modules/lang/org/config.el
@@ -115,7 +115,7 @@ Is relative to `org-directory', unless it is absolute. Is used in Doom's default
         org-fontify-whole-heading-line t
         org-hide-leading-stars t
         org-image-actual-width nil
-        org-imenu-depth 8
+        org-imenu-depth 6
         ;; Sub-lists should have different bullets
         org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+") ("1." . "a."))
         org-priority-faces
