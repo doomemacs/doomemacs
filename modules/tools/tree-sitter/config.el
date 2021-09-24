4552064a5aac2851f40746595821cffd0e386dbb
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-09-24 17:24:41 +0100

    refactor(tree-sitter): move ts-langs into use-pkg
    Move tree-sitter-langs into its own use-package call
    
    This is to reflect an accepted code pattern seen in the rest of doom's
    code.
    
    This does not cause any user facing changes

--- modules/tools/tree-sitter/config.el
+++ modules/tools/tree-sitter/config.el
@@ -4,12 +4,13 @@
   :hook (prog-mode . turn-on-tree-sitter-mode)
   :hook (tree-sitter-after-on . tree-sitter-hl-mode)
   :config
-  (require 'tree-sitter-langs)
-
   ;; This makes every node a link to a section of code
   (setq tree-sitter-debug-jump-buttons t
         ;; and this highlights the entire sub tree in your code
         tree-sitter-debug-highlight-jump-region t))
+
+(use-package! tree-sitter-langs
+  :after tree-sitter)
 
 (when (featurep! :editor evil +everywhere)
   (use-package! evil-textobj-tree-sitter
