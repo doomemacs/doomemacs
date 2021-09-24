d210b983d4408a7075cdcfa5ea77808be250979a
Henrik Lissner <henrik@lissner.net>
2021-09-25 00:55:19 +0200

    tweak(org): unset org-list-demote-modify-bullet
    Imposing different list bullets per-level was a poor and jarring
    default.

--- modules/lang/org/config.el
+++ modules/lang/org/config.el
@@ -116,8 +116,6 @@ Is relative to `org-directory', unless it is absolute. Is used in Doom's default
         org-hide-leading-stars t
         org-image-actual-width nil
         org-imenu-depth 6
-        ;; Sub-lists should have different bullets
-        org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+") ("1." . "a."))
         org-priority-faces
         '((?A . error)
           (?B . warning)
