498056bd05388084a6a2f62ad283198d6c54a24f
Itai Y. Efrat <itai3397@gmail.com>
2021-09-28 03:14:55 +0300

    module: add :input bidi
    Co-authored-by: Yoav Marco <yoavm448@gmail.com>

---
+++ modules/input/bidi/config.el
@@ -0,0 +1,23 @@
+;;; input/bidi/config.el -*- lexical-binding: t; -*-
+
+(defvar +bidi-mode-map (make-sparse-keymap)
+  "Keymap for `+bidi-mode'.")
+
+;;;###autoload
+(define-minor-mode +bidi-mode
+  "Minor mode for using bidirectional text in a buffer.
+
+Note that the whole buffer doesn't have to contain any
+bidirectional text at all, this mode just makes bidi editing
+easier."
+  :keymap +bidi-mode-map
+  (if +bidi-mode
+      (progn
+        (setq bidi-paragraph-direction nil   ; Do treat +Bidi as right-to-left
+              bidi-paragraph-separate-re "^" ; No need for empty lines to switch alignment
+              bidi-paragraph-start-re "^"    ; ^
+              bidi-inhibit-bpa nil))         ; Better bidi paren logic
+    (setq bidi-paragraph-direction 'left-to-right
+          bidi-paragraph-separate-re nil
+          bidi-paragraph-start-re nil
+          bidi-inhibit-bpa t)))
