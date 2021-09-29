79ebefa1546cc8ea044fb1cc0f7478f4c031446d
Kirill A. Korinsky <kirill@korins.ky>
2021-09-29 18:39:25 +0200

    bump: :tools magit
    magit/magit@1e40d0021790 -> magit/magit@f53148a56919
    magit/forge@f97bc47e9e2a -> magit/forge@483baae56df8
    
    Fix magit/forge#382
    Fix #5530

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "1e40d0021790707f6e88debda04f6b14d9429586")
+(when (package! magit :pin "f53148a569191bdbfb78d76f28481b91c60cb846")
   (when (featurep! +forge)
-    (package! forge :pin "f97bc47e9e2a2a6300dd267bdd67a88254f65aa7"))
+    (package! forge :pin "483baae56df8b1d8f1a9136512d23e106c831c0b"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
   (package! github-review :pin "341b7a1352e4ee1f1119756360ac0714abbaf460"))
