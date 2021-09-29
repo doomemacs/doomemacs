da24d7d47db77f1760e06cd7dcadf7d7d0b855d1
Henrik Lissner <henrik@lissner.net>
2021-09-29 17:57:25 +0200

    bump: :ui modeline
    seagle0128/doom-modeline@ef78fe07cd62 -> seagle0128/doom-modeline@ffedb34800db
    
    Fixes void-variable: flymake--backend-state error on latest commits of
    Emacs 28.

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "ef78fe07cd62e87f1d4cf047ecb35a1780e2384e"))
+  (package! doom-modeline :pin "ffedb34800db41f5e06e8a6f03698b56593c5024"))
 (package! anzu :pin "bdb3da5028935a4aea55c40769bc191a81afb54e")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "d3f6ed4773b48767bd5f4708c7f083336a8a8a86"))
