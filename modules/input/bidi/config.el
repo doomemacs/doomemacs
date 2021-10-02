e8f6b3643f8064a24441e49fca678a8eb909434f
Itai Y. Efrat <itai3397@gmail.com>
2021-10-02 19:17:07 +0300

    feat(bidi): add +bidi-(hebrew|arabic)-font
    Since the Hebrew and Arabic unicode blocks cover the vast majority of
    RTL languages used today, we provide these fonts so end users don't have
    to bother with setting up the hooks themselves.

--- modules/input/bidi/config.el
+++ modules/input/bidi/config.el
@@ -3,6 +3,20 @@
 (defvar +bidi-mode-map (make-sparse-keymap)
   "Keymap for `+bidi-mode'.")
 
+(defvar +bidi-hebrew-font (font-spec :family "DejaVu Sans")
+  "Overriding font for hebrew script.
+Must be a `font-spec', see `doom-font' for examples.
+
+WARNING: if you specify a size for this font it will hard-lock any usage of this
+font to that size. It's rarely a good idea to do so!")
+
+(defvar +bidi-arabic-font (font-spec :family "DejaVu Sans")
+  "Overriding font for arabic and arabic-derived scripts.
+Must be a `font-spec', see `doom-font' for examples.
+
+WARNING: if you specify a size for this font it will hard-lock any usage of this
+font to that size. It's rarely a good idea to do so!")
+
 ;;;###autoload
 (define-minor-mode +bidi-mode
   "Minor mode for using bidirectional text in a buffer.
@@ -23,3 +37,8 @@ easier."
           bidi-inhibit-bpa t)))
 
 (define-globalized-minor-mode +bidi-global-mode +bidi-mode +bidi-mode)
+
+(add-hook! 'after-setting-font-hook
+  (defun +bidi-set-fonts-h ()
+    (set-fontset-font t 'hebrew +bidi-hebrew-font)
+    (set-fontset-font t 'arabic +bidi-arabic-font)))
