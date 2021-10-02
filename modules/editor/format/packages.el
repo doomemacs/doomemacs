8d717607fb65fb1ba7e6ec3a1b56729fec1d2695
Justin Smestad <justin.smestad@gmail.com>
2021-10-02 12:54:38 -0600

    Bump format-all package
    This has not been updated since Oct 2020

--- modules/editor/format/packages.el
+++ modules/editor/format/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/format/packages.el
 
-(package! format-all :pin "47d862d40a088ca089c92cd393c6dca4628f87d3")
+(package! format-all :pin "772beb9acc3152cce10767ebba545c7af52b76e4")
