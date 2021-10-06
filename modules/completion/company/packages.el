85275a61f2db06f939846e997a07a9775b6a09b7
Henrik Lissner <henrik@lissner.net>
2021-10-06 11:40:50 +0200

    bump: :completion company
    company-mode/company-mode@1887974e7ad5 -> company-mode/company-mode@4c08ef468678

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/company/packages.el
 
-(package! company :pin "1887974e7ad50cb4884fbec27985078bba754b60")
+(package! company :pin "4c08ef468678bbf3b3c9e750f6e694eea1aa8423")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (featurep! +childframe)
   (package! company-box :pin "156f65cfbf690ed84e0e84f90277d665d873ff24"))
