1545500db5af35ec165026d10ff926fc3663d23d
Henrik Lissner <henrik@lissner.net>
2021-10-10 18:19:58 +0200

    bump: :tools lsp debugger
    emacs-lsp/dap-mode@00e1a5d70c32 -> emacs-lsp/dap-mode@a225e73026f2
    gagbo/consult-lsp@e8a50f2c94f4 -> gagbo/consult-lsp@b9aa9617f174
    joaotavora/eglot@c17bdf6c98d6 -> joaotavora/eglot@9665359bb6bf
    mohkale/consult-eglot@28a09cc839a9 -> mohkale/consult-eglot@f93c571dc392

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -6,5 +6,5 @@
     (package! realgud-trepan-ni :pin "0ec088ea343835e24ae73da09bea96bfb02a3130")))
 
 (when (featurep! +lsp)
-  (package! dap-mode :pin "00e1a5d70c32019800b8eb09d2fbcecbcb727a16")
+  (package! dap-mode :pin "a225e73026f28b67f1837e80497fe3ec641373f4")
   (package! posframe :pin "f97c4aff2c2c376ca62276d5597aa108546633a9"))
