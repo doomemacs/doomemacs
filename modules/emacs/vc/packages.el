9aa33d4ca52d6df4687fe3f0c4f1fae45442dd97
Kirill A. Korinsky <kirill@korins.ky>
2021-10-21 12:27:42 +0200

    bump: :emacs vc :tools magit
    charignon/github-review@4d91dd6c56be -> charignon/github-review@2a24e75dfc2d
    magit/forge@8264234db61b -> magit/forge@72b29bd7bc41
    magit/git-modes@433e1c57a63c -> magit/git-modes@62fbf2e5b84c
    magit/magit@1e40d0021790 -> magit/magit@aba0a596115b
    
    - gitconfig-mode and gitignore-mode were merged into git-modes.
    
    Close: #5665
    Fix: #5667

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -6,7 +6,6 @@
 (package! smerge-mode :built-in t)
 
 (package! browse-at-remote :pin "cef26f2c063f2473af42d0e126c8613fe2f709e4")
-(package! git-commit :pin "1e40d0021790707f6e88debda04f6b14d9429586")
+(package! git-commit :pin "aba0a596115b42fbd60347d893bcc319020ce5a2")
 (package! git-timemachine :pin "3381797bcbf906b18dff654a2361032d2d01b4a3")
-(package! gitconfig-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
-(package! gitignore-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
+(package! git-modes :pin "62fbf2e5b84ca789e7bc2f87939386023b5ba3df")
