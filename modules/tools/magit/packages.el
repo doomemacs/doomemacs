0869d28483b5d81b818b110af351fd5c4dc04dd9
Kirill A. Korinsky <kirill@korins.ky>
2021-10-21 12:27:42 +0200

    bump: :emacs vc :tools magit
    charignon/github-review@4d91dd6c56be -> charignon/github-review@2a24e75dfc2d
    magit/forge@8264234db61b -> magit/forge@72b29bd7bc41
    magit/git-modes@433e1c57a63c -> magit/git-modes@62fbf2e5b84c
    magit/magit@1e40d0021790 -> magit/magit@aba0a596115b
    
    - gitconfig-mode and gitignore-mode were merged into git-modes.
    
    Close: #5665
    Fix: #5667

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "bb7b7a402025e26aabc0aee6e97c2ee852a806a9")
+(when (package! magit :pin "aba0a596115b42fbd60347d893bcc319020ce5a2")
   (when (featurep! +forge)
-    (package! forge :pin "8264234db61b8a7f427b972aaef6235f9f6a13fa"))
+    (package! forge :pin "72b29bd7bc4172705b55bdd2a5070202ec154069"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
-  (package! github-review :pin "4d91dd6c56be1ae2b93b6b9e50c73f657cc461b6"))
+  (package! github-review :pin "2a24e75dfc2d9f37789ff60b4c10deb7c96f3f88"))
