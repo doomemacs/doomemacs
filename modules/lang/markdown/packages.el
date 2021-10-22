0d7463236f723dbf19273de755ef3a660b4b12bf
Eric Drechsel <eric@pdxhub.org>
2021-10-22 10:57:52 -0700

    bump: markdown-mode
    jrblevin/markdown-mode@862ae8addd29 -> jrblevin/markdown-mode@c3c2f0d473a3
    
    Improves markdown-do to follow markdown links and [[WikiLinks]]
    
    https://github.com/jrblevin/markdown-mode/pull/666

--- modules/lang/markdown/packages.el
+++ modules/lang/markdown/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/markdown/packages.el
 
-(package! markdown-mode :pin "862ae8addd29bf6affca1a35fd0176cb0c1392da")
+(package! markdown-mode :pin "c3c2f0d473a3f8ca8c4ffb2ecc094d5c3541769f")
 (package! markdown-toc :pin "3d724e518a897343b5ede0b976d6fb46c46bcc01")
 (package! edit-indirect :pin "bdc8f542fe8430ba55f9a24a7910639d4c434422")
 
