90b31f9268af9389efb28bf1022bbabbc0f29116
Kirill A. Korinsky <kirill@korins.ky>
2021-10-23 14:44:54 +0200

    fix(magit): add transient package
    transient magit/transient@022f20e9581b
    
    Close: #5670

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -4,6 +4,7 @@
 (when (package! magit :pin "aba0a596115b42fbd60347d893bcc319020ce5a2")
   (when (featurep! +forge)
     (package! forge :pin "72b29bd7bc4172705b55bdd2a5070202ec154069"))
+  (package! transient :pin "022f20e9581b317bffe3457a2422b3a7075761f6")
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
   (package! github-review :pin "2a24e75dfc2d9f37789ff60b4c10deb7c96f3f88"))
