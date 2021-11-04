0a6a0cbadf3ef6d07e262482034b1b0050217f5c
Henrik Lissner <henrik@lissner.net>
2021-11-04 01:15:15 +0100

    nit: add license, gitignore, & source.el to modules
    This is to prep our modules to be moved to their own repo in the
    future (doomemacs/modules).

---
+++ modules/source.el
@@ -0,0 +1,7 @@
+;; This file supplies some metadata about this module repo to Doom's module
+;; manager. None of them are required, but it's a good idea to include them so
+;; Doom can perform some sanity checks for you.
+
+:root     "./"     ; where the module tree starts
+:version  "21.12"  ; version of this repo
+:requires "3.0.0"  ; minimum supported version of Doom's core
