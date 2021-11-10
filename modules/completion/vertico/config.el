a310fb4a779dbcb931e553ef133a8400fc8ee14c
Itai Y. Efrat <itai3397@gmail.com>
2021-11-11 01:45:10 +0200

    bump: :completion vertico
    minad/consult@39f41edda6f3 -> minad/consult@57dc1adfdc0f
    minad/marginalia@09d8ab38a5a4 -> minad/marginalia@678b6528f390
    minad/vertico@0df38cf1d729 -> minad/vertico@75f33e36204f
    oantolin/embark@d09fff2da750 -> oantolin/embark@5b34b2b60370
    
    Update vertico-repeat configuration due to upstream changes.

--- modules/completion/vertico/config.el
+++ modules/completion/vertico/config.el
@@ -28,6 +28,7 @@ overrides `completion-styles' during company completion sessions.")
   ;; Cleans up path when moving directories with shadowed paths syntax, e.g.
   ;; cleans ~/foo/bar/// to /, and ~/foo/bar/~/ to ~/.
   (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
+  (add-hook 'minibuffer-setup-hook #'vertico-repeat-save)
   (map! :map vertico-map [backspace] #'vertico-directory-delete-char))
 
 
