5241555676a9395d3afa4ef1d9cb2fcf270c664d
Henrik Lissner <git@henrik.io>
2021-11-21 16:44:26 +0100

    bump: :editor multiple-cursors
    hlissner/evil-multiedit@15a47540db21 -> hlissner/evil-multiedit@23b53bc8743f
    
    Fix: hlissner/evil-multiedit#44
    Fix: hlissner/evil-multiedit#46

--- modules/editor/multiple-cursors/packages.el
+++ modules/editor/multiple-cursors/packages.el
@@ -3,7 +3,7 @@
 
 (cond
  ((featurep! :editor evil)
-  (package! evil-multiedit :pin "15a47540db21f2b34e8ed4fbeba33c5a63d8f317")
+  (package! evil-multiedit :pin "23b53bc8743fb82a8854ba907b1d277374c93a79")
   (package! evil-mc :pin "246aecc17481dd23c172a9b845f02a9d9e322c7f"))
 
  ((package! multiple-cursors :pin "8a60fc7ef0ae6e5ca089a7c95264cd0ae83e7274")))
