4cb1880945b6445a6833283d46825f1a30bcc0bc
Henrik Lissner <git@henrik.io>
2021-11-21 16:04:00 +0100

    bump: :tools magit
    charignon/github-review@2a24e75dfc2d -> charignon/github-review@725fbc7b3852
    magit/forge@72b29bd7bc41 -> magit/forge@41efa674cff0
    magit/magit@aba0a596115b -> magit/magit@877c389ca016

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "aba0a596115b42fbd60347d893bcc319020ce5a2")
+(when (package! magit :pin "877c389ca0161959081fa2c77045ce1ae9463be4")
   (when (featurep! +forge)
-    (package! forge :pin "72b29bd7bc4172705b55bdd2a5070202ec154069"))
+    (package! forge :pin "41efa674cff0b447efbc103494fd61ec9b9156ae"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
-  (package! github-review :pin "2a24e75dfc2d9f37789ff60b4c10deb7c96f3f88"))
+  (package! github-review :pin "725fbc7b385228f53a7ddc46a92c1276bab4aea8"))
