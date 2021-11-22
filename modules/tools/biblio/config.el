4f160bc09d2c3469ff182305fe562df340d02a81
Henrik Lissner <git@henrik.io>
2021-11-22 14:21:19 +0100

    refactor(biblio): reformat module
    - Defer citar-org, since citar autoloads it.
    - Add introducing PR to #+SINCE and use new calver scheme.
    - Move <localleader> @ keybind to :lang org
    
    Ref: #5810

--- modules/tools/biblio/config.el
+++ modules/tools/biblio/config.el
@@ -1,61 +1,55 @@
 ;;; tools/biblio/config.el -*- lexical-binding: t; -*-
 
-(use-package! bibtex-completion
-  :when (or (featurep! :completion ivy)
-            (featurep! :completion helm))
-  :defer t
-  :config
-  (setq bibtex-completion-additional-search-fields '(keywords)
-        bibtex-completion-pdf-field "file"));; This tells bibtex-completion to look at the File field of the bibtex to figure out which pdf to open
-
-(use-package! ivy-bibtex
-  :when (featurep! :completion ivy)
-  :defer t
-  :config
-  (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-plus)))
-
-
-;;; Org-Cite configuration
+;;
+;;; `org-cite'
 
 (use-package! oc
-  :after org
+  :defer t
   :config
-  (map! :map org-mode-map
-        :localleader
-        :desc "Insert citation" "@" #'org-cite-insert)
   (setq org-cite-global-bibliography
-        (let ((paths
-               (cond
-                ((boundp 'citar-bibliography) citar-bibliography)
-                ((boundp 'bibtex-completion-bibliography) bibtex-completion-bibliography))))
-          ;; Always return bibliography paths as list for org-cite.
-          (if (stringp paths) (list paths) paths))
-        ;; setup export processor; default csl/citeproc-el, with biblatex for
+        (doom-enlist
+         (or (bound-and-true-p citar-bibliography)
+             (bound-and-true-p bibtex-completion-bibliography)))
+        ;; Setup export processor; default csl/citeproc-el, with biblatex for
         ;; latex
-        org-cite-export-processors
-        '((latex biblatex)
-          (t csl))
+        org-cite-export-processors '((latex biblatex) (t csl))
         org-cite-insert-processor 'citar
         org-cite-follow-processor 'citar
         org-cite-activate-processor 'citar
         org-support-shift-select t))
 
 
-  ;;; Org-cite processors
-(use-package! oc-biblatex
-  :after oc)
+;; `org-cite' processors
+(use-package! oc-biblatex :after oc)
+(use-package! oc-csl :after oc)
+(use-package! oc-natbib :after oc)
 
-(use-package! oc-csl
-  :after oc)
 
-(use-package! oc-natbib
-  :after oc)
-
-;;;; Third-party
+;;
+;;; Third-party
 
 (use-package! citar-org
-  :no-require
+  :when (featurep! :completion vertico)
   :when (featurep! :lang org +roam2)
+  :defer t
   :config
   ;; Include property drawer metadata for 'org-roam' v2.
   (setq citar-org-note-include '(org-id org-roam-ref)))
+
+
+(use-package! bibtex-completion
+  :when (or (featurep! :completion ivy)
+            (featurep! :completion helm))
+  :defer t
+  :config
+  (setq bibtex-completion-additional-search-fields '(keywords)
+        ;; Tell bibtex-completion to look at the File field of the bibtex to
+        ;; figure out which pdf to open:
+        bibtex-completion-pdf-field "file"))
+
+
+(use-package! ivy-bibtex
+  :when (featurep! :completion ivy)
+  :defer t
+  :config
+  (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-plus)))
