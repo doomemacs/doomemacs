cd64f4067aa8f88349cafb67ef2f130bf266c306
Henrik Lissner <git@henrik.io>
2021-11-25 01:23:55 +0100

    refactor(notmuch): unlink ol-notmuch from +org flag
    Amend: db94569143be
    Ref: hlissner/doom-emacs@d522cc347d94

--- modules/email/notmuch/packages.el
+++ modules/email/notmuch/packages.el
@@ -20,7 +20,8 @@
   :pin "63413a5563450bdedee4c077f2f998578e75083a")
 
 (when (featurep! +org)
-  (package! org-mime :pin "eb21c02ba8f97fe69c14dc657a7883b982664649")
+  (package! org-mime :pin "eb21c02ba8f97fe69c14dc657a7883b982664649"))
+(when (featurep! :lang org)
   (package! ol-notmuch :pin "126fb446d8fa9e54cf21103afaf506fd81273c02"))
 (when (featurep! :completion ivy)
   (package! counsel-notmuch :pin "a4a1562935e4180c42524c51609d1283e9be0688"))
