68ed4e629855af66e0032e468fdb7b4d7a2c2c1a
Henrik Lissner <git@henrik.io>
2021-11-25 01:17:42 +0100

    tweak: suppress 'Loading X...' messages at startup
    From site-files, particularly. This *might* fix the white flash while
    Emacs is starting up in some scenarios.

--- early-init.el
+++ early-init.el
@@ -49,7 +49,19 @@
             (lambda ()
               (setq-default inhibit-redisplay nil
                             inhibit-message nil)
-              (redisplay))))
+              (redisplay)))
+
+  ;; Site files tend to use `load-file', which emits "Loading X..." messages in
+  ;; the echo area, which in turn triggers a redisplay. Redisplays can have a
+  ;; substantial effect on startup times and in this case happens so early that
+  ;; Emacs may flash white while starting up.
+  (define-advice load-file (:override (file) silence)
+    (load file nil 'nomessage))
+
+  ;; Undo our `load-file' advice above, to limit the scope of any edge cases it
+  ;; may introduce down the road.
+  (define-advice startup--load-user-init-file (:before (&rest _) init-doom)
+    (advice-remove #'load-file #'load-file@silence)))
 
 
 ;;
