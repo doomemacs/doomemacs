e8124b5d24193606d365e1121921fbfa8f158675
Afiq Nazrie <afnazrie@gmail.com>
2021-11-30 13:03:19 +0800

    bump: :tools lookup
    SavchenkoValeriy/emacs-powerthesaurus@93036d3b1119 -> SavchenkoValeriy/emacs-powerthesaurus@02c9d11a3f40
    jacktasia/dumb-jump@f3176fbf9c11 -> jacktasia/dumb-jump@dbb915441a2b
    tkf/emacs-request@68003b3f8597 -> tkf/emacs-request@3336eaa97de9
    
    Fix https://github.com/SavchenkoValeriy/emacs-powerthesaurus/issues/17

--- modules/tools/lookup/packages.el
+++ modules/tools/lookup/packages.el
@@ -8,14 +8,14 @@
   (package! helm))
 
 ;;
-(package! dumb-jump :pin "f3176fbf9c11b94cf05bd8279399d9536115ff3c")
+(package! dumb-jump :pin "dbb915441a2b66f2fbb954ff5de2723c5a4771d4")
 (when (featurep! :completion ivy)
   (package! ivy-xref :pin "a82e8e117d2dd62c28b6a3e3d6e4cfb11c0bda38"))
 (when (featurep! :completion helm)
   (package! helm-xref :pin "ea0e4ed8a9baf236e4085cbc7178241f109a53fa"))
 
 ;; For dictionary and online lookup
-(package! request :pin "68003b3f859724de621d0e5a8b0aae51ce708d1e")
+(package! request :pin "3336eaa97de923f74b90dda3e35985e122d40805")
 
 (when (featurep! +docsets)
   (package! dash-docs :pin "29848b6b347ac520f7646c200ed2ec36cea3feda")
@@ -28,7 +28,7 @@
   (if IS-MAC
       (package! osx-dictionary :pin "1a4479d9f44ef1e6e5f7643c172c32f6fe6cce21")
     (package! define-word :pin "6e4a427503aef096484f88332962c346cdd10847")
-    (package! powerthesaurus :pin "93036d3b111925ebc34f747ff846cb0b8669b92e")
+    (package! powerthesaurus :pin "02c9d11a3f407023aa7c7b080bb9f8a5f5e7cd7a")
     (when (featurep! +offline)
       (package! wordnut :pin "feac531404041855312c1a046bde7ea18c674915")
       (package! synosaurus :pin "14d34fc92a77c3a916b4d58400424c44ae99cd81"))))
