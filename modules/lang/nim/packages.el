fd2788c2681950c99415162413df7dae0d511aad
James Ravn <james@r-vn.org>
2021-12-01 13:59:46 +0000

    bump: :lang nim
    nim-lang/nim-mode@d832d6b1fb5e -> nim-lang/nim-mode@744e076f0bea
    
    Includes nim-suggest.el fix for nim-mode on Emacs 27+ (void function
    error).
    
    Ref: nim-lang/nim-mode@744e076f0bea

--- modules/lang/nim/packages.el
+++ modules/lang/nim/packages.el
@@ -3,7 +3,7 @@
 
 ;;; requires nim nimsuggest nimble
 
-(package! nim-mode :pin "d832d6b1fb5e69fedcdddf442d62251dd0f1f489")
+(package! nim-mode :pin "744e076f0bea1e69fedcdddf442d62251dd0f1f489")
 
 (when (featurep! :checkers syntax)
   (package! flycheck-nim :pin "ddfade51001571c2399f78bcc509e0aa8eb752a4"))
