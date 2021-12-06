60d7b48c3e8c0268d3c3f910dc1cbed89a17381c
Narice <narice@tutanota.com>
2021-12-06 20:54:36 +0000

    module: add :ui good-scroll

---
+++ modules/ui/good-scroll/autoload.el
@@ -0,0 +1,3 @@
+;;; ui/tabs/autoload.el -*- lexical-binding: t; -*-
+
+(good-scoll-mode 1)
---
+++ modules/ui/good-scroll/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/tabs/packages.el
+
+(package! good-scroll :pin "8530d6697b15e0146275c80faccae35fa36fa8b424")
