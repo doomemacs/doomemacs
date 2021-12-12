bca75fc5c59518a91661fe3837853970f05ca2e0
Ellis Kenyő <elken@users.noreply.github.com>
2021-12-12 22:09:16 +0000

    feat(fsharp): use dotnet for inferior-fsharp-program
    Applies from .NET Core 2 onwards.

--- modules/lang/fsharp/config.el
+++ modules/lang/fsharp/config.el
@@ -1,6 +1,8 @@
 ;;; lang/fsharp/config.el -*- lexical-binding: t; -*-
 
 (after! fsharp-mode
+  (when (executable-find "dotnet")
+    (setq inferior-fsharp-program "dotnet fsi --readline-"))
   (if (featurep! +lsp)
       (progn
         (setq fsharp-ac-intellisense-enabled nil)
@@ -15,5 +17,5 @@
         "e" #'fsharp-eval-region
         "l" #'fsharp-load-buffer-file
         (:unless (featurep! +lsp)
-          "q" #'fsharp-ac/stop-process
-          "t" #'fsharp-ac/show-tooltip-at-point)))
+         "q" #'fsharp-ac/stop-process
+         "t" #'fsharp-ac/show-tooltip-at-point)))
