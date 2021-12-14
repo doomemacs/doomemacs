343086e09735859e3485efb53132ecc5ca425dd9
Henrik Lissner <git@henrik.io>
2021-12-14 01:08:44 +0100

    bump: :completion
    abo-abo/swiper@6a8e5611f32c -> abo-abo/swiper@1c6b3da377a8
    company-mode/company-mode@4c08ef468678 -> company-mode/company-mode@8b58e5895c2e
    emacs-helm/helm@4fcb36f1b9ec -> emacs-helm/helm@a246a9b278fb
    ericdanan/counsel-projectile@06b03c1080d3 -> ericdanan/counsel-projectile@e30150792a96
    lewang/flx@647cb2f92f99 -> lewang/flx@e3b3f0533e44
    lewang/flx@647cb2f92f99 -> lewang/flx@e3b3f0533e44
    minad/consult@85008702de2d -> minad/consult@cc8eff9578f5
    oantolin/embark@725794f01347 -> oantolin/embark@e08899ef2e7f
    raxod502/prescient.el@027c2137a8d9 -> raxod502/prescient.el@292ac9fe351d
    sebastiencs/company-box@156f65cfbf69 -> sebastiencs/company-box@f9cbbc7df8ef
    tumashu/ivy-posframe@9c8382823392 -> tumashu/ivy-posframe@5d9420252ca8
    tumashu/posframe@f97c4aff2c2c -> tumashu/posframe@3b1dc400d286
    
    Fix: tumashu/ivy-posframe#114
    Close: #5869

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/company/packages.el
 
-(package! company :pin "4c08ef468678bbf3b3c9e750f6e694eea1aa8423")
+(package! company :pin "8b58e5895c2eaf8686de0e25c807b00fdb205c7a")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (featurep! +childframe)
-  (package! company-box :pin "156f65cfbf690ed84e0e84f90277d665d873ff24"))
+  (package! company-box :pin "f9cbbc7df8efbb56a8d31a5b422d158660d9109e"))
--- modules/completion/ido/packages.el
+++ modules/completion/ido/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/ido/packages.el
 
-(package! flx-ido :pin "647cb2f92f9936c62e277d7a74ad54a80502d255")
+(package! flx-ido :pin "e3b3f0533e44c5250ce73d728b59a7e96c692b5d")
 (package! ido-completing-read+ :pin "49e7967ea8c0ab0a206b40d70fc19be115083fa1")
 (package! ido-sort-mtime :pin "f638ff0c922af862f5211779f2311a27fde428eb")
 (package! ido-vertical-mode :pin "b1659e967da0687abceca733b389ace24004fa66")
