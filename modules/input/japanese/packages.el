948459ad88830266180283637beb3dd4e51978aa
Henrik Lissner <git@henrik.io>
2021-12-14 01:03:17 +0100

    bump: :input
    cute-jumper/ace-pinyin@8b2e9335b024 -> cute-jumper/ace-pinyin@47662c0b0577
    skk-dev/ddskk@7a7e1ecaf7f4 -> skk-dev/ddskk@251df5b02c91
    tumashu/pyim@f48c3edee78b -> tumashu/pyim@02c50045cb14

--- modules/input/japanese/packages.el
+++ modules/input/japanese/packages.el
@@ -3,5 +3,5 @@
 
 (package! migemo :pin "f756cba3d5268968da361463c2e29b3a659a3de7")
 (package! avy-migemo :pin "922a6dd82c0bfa316b0fbb56a9d4dd4ffa5707e7")
-(package! ddskk :pin "7a7e1ecaf7f4f68058f1b8831d0b7b839d228614")
+(package! ddskk :pin "251df5b02c91311140d2375b019c1de836655fd0")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
