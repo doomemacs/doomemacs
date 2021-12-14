76014223ee082490506c98d47710030d10bb211d
Henrik Lissner <git@henrik.io>
2021-12-14 01:21:13 +0100

    bump: :config
    noctuid/link-hint.el@d3c5bacc9c69 -> noctuid/link-hint.el@3be270f3a732

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -3,7 +3,7 @@
 
 (package! avy :pin "e92cb37457b43336b765630dbfbea8ba4be601fa")
 (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-(package! link-hint :pin "d3c5bacc9c697c4cf8b14616c4199210f9267068")
+(package! link-hint :pin "3be270f3a732dc4acae6a20ff449eef0c4f9a966")
 
 (unless (featurep! :editor evil)
   (package! expand-region :pin "95a773bd8f557cbd43d3b2dab2fa4417ec5927ab"))
