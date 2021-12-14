9c81b25b2a941f38d6994a5c4a533477b22fa643
Henrik Lissner <git@henrik.io>
2021-12-14 01:03:17 +0100

    bump: :input
    cute-jumper/ace-pinyin@8b2e9335b024 -> cute-jumper/ace-pinyin@47662c0b0577
    skk-dev/ddskk@7a7e1ecaf7f4 -> skk-dev/ddskk@251df5b02c91
    tumashu/pyim@f48c3edee78b -> tumashu/pyim@02c50045cb14

--- modules/input/chinese/packages.el
+++ modules/input/chinese/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese/packages.el
 
-(package! pyim :pin "f48c3edee78ba5f020bcb42830db99a14761f176")
+(package! pyim :pin "02c50045cb14ab253d8d8435e83e7f10b0bbc130")
 (package! fcitx :pin "12dc2638ddd15c8f6cfaecb20e1f428ab2bb5624")
-(package! ace-pinyin :pin "8b2e9335b02486730ea4ceee790130cc5328f9ea")
+(package! ace-pinyin :pin "47662c0b05775ba353464b44c0f1a037c85e746e")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
