2d3a68df496f300ac219db124f44ff1bde90e464
Ellis Kenyo <me@elken.dev>
2021-12-15 07:05:57 +0000

    feat(magit): replace github-review for code-review

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -6,4 +6,5 @@
     (package! forge :pin "41efa674cff0b447efbc103494fd61ec9b9156ae"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
-  (package! github-review :pin "725fbc7b385228f53a7ddc46a92c1276bab4aea8"))
+  (package! code-review :pin "f1a79c20ae51d23f76067a1e5a2f5c1c4db42ec9"
+    :recipe (:files ("graphql" "code-review*.el"))))
