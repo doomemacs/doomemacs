fffcae893d3961e91abac8ad925dfd624e448601
GilbertWong <gilbertwong96@icloud.com>
2021-12-17 02:20:31 +0800

    refactor!(erlang): remove company-erlang
    BREAKING CHANGE: Remove `company-erlang` which depends on
    `ivy-erlang-complete`. `company-erlang` and `ivy-erlang-complte` have
    not been maintained now. The lsp mode provides much better experience
    than `company-erlang` and `ivy-erlang-complete`. The
    `ivy-erlang-complete` package also has the defect that slow down the
    erlang-mode buffer when the buffer is huge enough. It is not a good
    choice to integrate ivy-erlang-complete nowadays even as a non-LSP
    option.

--- modules/lang/erlang/packages.el
+++ modules/lang/erlang/packages.el
@@ -2,6 +2,3 @@
 ;;; lang/erlang/packages.el
 
 (package! erlang :pin "c1ab4b5424be7504cfc3c4e87a2116b7731d8f2d")
-(unless (featurep! +lsp)
-  (when (featurep! :completion company)
-    (package! company-erlang :pin "bc0524a16f17b66c7397690e4ca0e004f09ea6c5")))
