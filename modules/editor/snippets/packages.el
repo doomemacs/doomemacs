28253b19e343d437bce75d3116e207e79f9e7005
Henrik Lissner <git@henrik.io>
2021-12-18 00:18:36 +0100

    bump: :editor snippets
    hlissner/doom-snippets@3083b2342f95 -> doomemacs/snippets@3083b2342f95

--- modules/editor/snippets/packages.el
+++ modules/editor/snippets/packages.el
@@ -5,6 +5,6 @@
 (package! auto-yasnippet :pin "db9e0dd4335b2202cd5dac95bbbc87a1032d9bbe")
 (package! doom-snippets
   :recipe (:host github
-           :repo "hlissner/doom-snippets"
-           :files ("*.el" "*"))
+           :repo "doomemacs/snippets"
+           :files (:defaults "*"))
   :pin "3083b2342f95fa55c1fd3b4a16229c5d867a02b0")
