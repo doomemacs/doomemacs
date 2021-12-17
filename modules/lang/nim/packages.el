db20b5b6612aff42c720e5c3dcb56c33661e541e
Damien Pollet <damien.pollet@gmail.com>
2021-12-17 13:05:17 +0100

    fix(nim): correct :lang nim SHA from last bump
    Wrong SHA for package nim-mode (looks like a copy-paste mishap).
    
    Amend: hlissner/doom-emacs@fd2788c26819

--- modules/lang/nim/packages.el
+++ modules/lang/nim/packages.el
@@ -3,7 +3,7 @@
 
 ;;; requires nim nimsuggest nimble
 
-(package! nim-mode :pin "744e076f0bea1e69fedcdddf442d62251dd0f1f489")
+(package! nim-mode :pin "744e076f0bea1c5ddc49f92397d9aa98ffa7eff8")
 
 (when (featurep! :checkers syntax)
   (package! flycheck-nim :pin "ddfade51001571c2399f78bcc509e0aa8eb752a4"))
