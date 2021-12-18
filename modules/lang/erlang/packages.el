beda11068b752543e8965458328174fe04582b78
GilbertWong <gilbertwong96@icloud.com>
2021-12-18 14:03:50 +0800

    bump: :lang erlang
    erlang/otp@118cb37bd5b9 -> erlang/otp@c1ab4b5424be
    
    Ref: erlang/otp#5314

--- modules/lang/erlang/packages.el
+++ modules/lang/erlang/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/erlang/packages.el
 
-(package! erlang :pin "118cb37bd5b9e9cb792f0463e46fdb04f151dcd5")
+(package! erlang :pin "c1ab4b5424be7504cfc3c4e87a2116b7731d8f2d")
 (unless (featurep! +lsp)
   (when (featurep! :completion company)
     (package! company-erlang :pin "bc0524a16f17b66c7397690e4ca0e004f09ea6c5")))
