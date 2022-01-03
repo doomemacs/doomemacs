692cf75a9a91367efa3fcc527e7c285e3bf48bc3
Henrik Lissner <git@henrik.io>
2022-01-03 16:31:06 +0100

    bump: :tools magit
    magit/forge@402773ef7e83 -> magit/forge@a44afa81f142
    magit/magit@65c4485e19bf -> magit/magit@2e73b66c2980
    wandersoncferreira/code-review@b0bedbdb30e0 -> wandersoncferreira/code-review@136c0933ba9d
    
    Close: #5947

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "65c4485e19bf570ebcb81fbaa6352c4e94bb05da")
+(when (package! magit :pin "2e73b66c2980abb9211d9881a8710c8ac5a33184")
   (when (featurep! +forge)
-    (package! forge :pin "402773ef7e83ddfab64bfee23daea2776d50dbc1"))
+    (package! forge :pin "a44afa81f14255905d6728337abb2729b9aca840"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "60152d5c4e4b73e72e15f23ca16e8cc7734906bc")
-  (package! code-review :pin "b0bedbdb30e019ed8c40fedf1087c3ad28e72c59"
+  (package! code-review :pin "136c0933ba9dc19ce3efedb36a7dbd401e2e98b2"
     :recipe (:files ("graphql" "code-review*.el"))))
