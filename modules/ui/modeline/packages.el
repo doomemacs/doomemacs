78324248435b395e25edbd2e06c352b1062e5395
Henrik Lissner <git@henrik.io>
2022-01-03 19:22:28 +0100

    bump: :ui modeline
    seagle0128/doom-modeline@69ede7d71976 -> seagle0128/doom-modeline@84573ae5e7db
    
    - Fixes an issue where doom-modeline was generating too many iex child
      processes while trying to read the installed version of elixir (see
      #5895).
    
    Fix: #5895

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (featurep! +light)
-  (package! doom-modeline :pin "69ede7d719764f26671897c5020f295e5eb1e6c8"))
+  (package! doom-modeline :pin "84573ae5e7db2705da67c2eda94b786a99346272"))
 (package! anzu :pin "5abb37455ea44fa401d5f4c1bdc58adb2448db67")
 (when (featurep! :editor evil)
   (package! evil-anzu :pin "d3f6ed4773b48767bd5f4708c7f083336a8a8a86"))
