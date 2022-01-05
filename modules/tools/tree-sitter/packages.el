c1bea42fa089a88e50ad571b4976d5e0a74323c8
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-01-05 20:14:42 +0000

    bump: :tools tree-sitter
    emacs-tree-sitter/elisp-tree-sitter@8bbbfa4fc5f4 -> emacs-tree-sitter/elisp-tree-sitter@48b06796a3b2
    emacs-tree-sitter/tree-sitter-langs@86a894a61797 -> emacs-tree-sitter/tree-sitter-langs@3c0c82f9fb0a
    meain/evil-textobj-tree-sitter@4d79ea71219c -> meain/evil-textobj-tree-sitter@08823ff97277
    
    - This fixes the java issue where switch expressions were erroring,
    - Roll back evil text obj tree sitter as ruby text objects became un
    - usable, see https://github.com/emacs-tree-sitter/elisp-tree-sitter/issues/180

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -2,11 +2,11 @@
 ;;; tools/tree-sitter/packages.el
 
 (package! tree-sitter
-  :pin "8bbbfa4fc5f478f10c7cb968177d5a907fe5928f")
+  :pin "48b06796a3b2e76ce004972d929de38146eafaa0")
 
 (package! tree-sitter-langs
-  :pin "86a894a617976aefa453fa6ce8dd9871c58f733e")
+  :pin "3c0c82f9fb0a796f5ebd7e1e4c89f13d5ab6ef58")
 
 (when (featurep! :editor evil +everywhere)
   (package! evil-textobj-tree-sitter
-    :pin "4d79ea71219cb0153baf4046af8aae6b1ed2fcfb"))
+    :pin "08823ff97277fe50540d8226c7d298e06fb14932"))
