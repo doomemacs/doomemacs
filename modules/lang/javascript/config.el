5191bfa453e6aa869130db087766008a9b620022
James Ravn <james@r-vn.org>
2022-01-10 09:39:04 +0000

    fix(javascript): incorrect lsp formatting for tsx

--- modules/lang/javascript/config.el
+++ modules/lang/javascript/config.el
@@ -122,7 +122,10 @@
                     'jsx-tide)))))
   :config
   (when (fboundp 'web-mode)
-    (define-derived-mode typescript-tsx-mode web-mode "TypeScript-TSX"))
+    (define-derived-mode typescript-tsx-mode web-mode "TypeScript-TSX")
+    (when (featurep! +lsp)
+      (after! lsp-mode
+        (add-to-list 'lsp--formatting-indent-alist '(typescript-tsx-mode . typescript-indent-level)))))
 
   (set-docsets! '(typescript-mode typescript-tsx-mode)
     :add "TypeScript" "AngularTS")
