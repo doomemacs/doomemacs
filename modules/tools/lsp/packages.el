42b28287fb9d1b812e9bfd7081544d1763622176
Samuel Tschiedel <mukaobr@gmail.com>
2022-01-10 18:21:10 -0300

    bump: :tools lsp
    emacs-lsp/lsp-mode@3d6a01dde958 -> emacs-lsp/lsp-mode@355c9e183c9b
    
    Ref: emacs-lsp/lsp-mode#3295

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -6,7 +6,7 @@
       (package! eglot :pin "28092ba3af6041a44227b92397c148a902413d1c")
       (when (featurep! :completion vertico)
         (package! consult-eglot :pin "f93c571dc392a8b11d35541bffde30bd9f411d30")))
-  (package! lsp-mode :pin "3d6a01dde958db77a4597bf52e079f5b96469527")
+  (package! lsp-mode :pin "355c9e183c9b3a2872487a3068c5902326b58be4")
   (package! lsp-ui :pin "e5d464537286b494334bab1056f8699a650d9d1f")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "3e87441a625d65ced5a208a0b0442d573596ffa3"))
