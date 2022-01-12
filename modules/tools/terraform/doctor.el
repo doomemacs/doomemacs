0ba69494d30bf06a28c4bfd268c240bf844c3b03
somallg <tq.duong@icloud.com>
2022-01-12 14:53:14 +0700

    docs(terraform): add +lsp flag

--- modules/tools/terraform/doctor.el
+++ modules/tools/terraform/doctor.el
@@ -2,3 +2,7 @@
 
 (unless (executable-find "terraform")
   (warn! "Couldn't find terraform."))
+
+(when (featurep! +lsp)
+  (unless (executable-find "terraform-ls"))
+  (warn! "Couldn't find terraform-ls."))
