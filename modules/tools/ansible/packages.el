f87f52a7ce95149cb15ffec3bb6ca36bbea62f20
Henrik Lissner <git@henrik.io>
2022-01-12 01:31:26 +0100

    bump: :tools
    andras-simonyi/citeproc-el@538fed794c29 -> andras-simonyi/citeproc-el@9f783967c190
    bdarcus/citar@51b30f2e4091 -> bdarcus/citar@b24b5b94b7d5
    emacs-lsp/lsp-mode@3d6a01dde958 -> emacs-lsp/lsp-mode@c228bce435b9
    gagbo/consult-lsp@aaa9a31bc822 -> gagbo/consult-lsp@f4f195046b97
    joaotavora/eglot@28092ba3af60 -> joaotavora/eglot@2c0f262c2402
    magit/forge@a44afa81f142 -> magit/forge@0ff9b8a0dea2
    magit/magit@2e73b66c2980 -> magit/magit@0ac05f39624d
    millejoh/emacs-ipython-notebook@c6696c12eb7c -> millejoh/emacs-ipython-notebook@6063cee7fb0b
    purcell/envrc@8a9a142cf9d3 -> purcell/envrc@456c4100de41
    realgud/realgud@978b455d7da4 -> realgud/realgud@3c88611c4ed5
    tumashu/posframe@3b1dc400d286 -> tumashu/posframe@6c0e63d6b3b6
    vedang/pdf-tools@a8847b75d348 -> vedang/pdf-tools@4e6c778194be
    wandersoncferreira/code-review@136c0933ba9d -> wandersoncferreira/code-review@ccc3795a7255
    yoshiki/yaml-mode@63b637f84641 -> yoshiki/yaml-mode@a79d2a7b9281
    
    Close: #5977
    Ref: emacs-lsp/lsp-mode#3295

--- modules/tools/ansible/packages.el
+++ modules/tools/ansible/packages.el
@@ -4,7 +4,7 @@
 (package! ansible :recipe (:nonrecursive t) :pin "40af0d2bbb6c5bbcf7aa9269ac9a07e22622d263")
 (package! ansible-doc :pin "86083a7bb2ed0468ca64e52076b06441a2f8e9e0")
 (package! jinja2-mode :pin "ecd19a40b7832bb00f0a2244e3b0713d0bf3850d")
-(package! yaml-mode :pin "63b637f846411806ae47e63adc06fe9427be1131")
+(package! yaml-mode :pin "a79d2a7b9281f8c56f461d717b1ba40fc58e22fd")
 
 (when (featurep! :completion company)
   (package! company-ansible :pin "79dd421b161efa49fbdffad57fa40edb41f484a3"))
