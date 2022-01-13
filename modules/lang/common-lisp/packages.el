ec67a6fb2ba958b0a799113093cbef6a9f5b1402
Payas Relekar <relekarpayas@gmail.com>
2022-01-13 19:00:55 +0530

    bump: :lang common-lisp
    joaotavora/sly@540a8c5b9a04 -> joaotavora/sly@2e00c3bd4bdf

--- modules/lang/common-lisp/packages.el
+++ modules/lang/common-lisp/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/common-lisp/packages.el
 
-(when (package! sly :pin "540a8c5b9a04af0a6907e07cb070f1fed8a76f48")
+(when (package! sly :pin "2e00c3bd4bdf6658f258cc78b3254f0ec24591e5")
   (package! sly-macrostep :pin "5113e4e926cd752b1d0bcc1508b3ebad5def5fad")
   (package! sly-repl-ansi-color :pin "b9cd52d1cf927bf7e08582d46ab0bcf1d4fb5048"))
