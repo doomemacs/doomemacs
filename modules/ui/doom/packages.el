ff4609835a4c47312b3d685e09330034d6f4c8a4
Seong Yong-ju <sei40kr@gmail.com>
2022-01-16 17:41:58 +0900

    bump: :ui doom
    doomemacs/themes@7d1a56623c08 -> doomemacs/themes@56e8a93b2dd8

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "7d1a56623c08da769fd424e901916e9c3d8fdb25")
+(package! doom-themes :pin "56e8a93b2dd8f2c10a693f36c3317833211201f2")
 (package! solaire-mode :pin "8af65fbdc50b25ed3214da949b8a484527c7cc14")
