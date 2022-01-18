ddf56191f78d4ab1d4e2f710916e34e91fc8f675
M. Yas. Davoodeh <30480116+Davoodeh@users.noreply.github.com>
2022-01-19 00:05:15 +0330

    Update doctor.el

--- modules/lang/plantuml/doctor.el
+++ modules/lang/plantuml/doctor.el
@@ -7,7 +7,7 @@
     (warn! "Couldn't find java. PlantUML preview or syntax checking won't work"))
   ;; graphviz
   (unless (executable-find "dot")
-    (warn! "Couldn't fiind dot. PlantUML might not show any output"))
+    (warn! "Couldn't find dot. PlantUML will only show outputs for sequence and activity diagrams"))
   ;; plantuml.jar
   (unless (file-exists-p plantuml-jar-path)
     (warn! "Couldn't find plantuml.jar. Install it with M-x plantuml-download-jar")))
