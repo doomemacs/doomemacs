98e74707e0575456f94e6e85e41b8fac5e0dc3a1
M. Yas. Davoodeh <30480116+Davoodeh@users.noreply.github.com>
2022-01-18 23:45:32 +0330

    Add graphviz/dot check to plantuml/doctor.el

--- modules/lang/plantuml/doctor.el
+++ modules/lang/plantuml/doctor.el
@@ -5,6 +5,9 @@
   ;; java
   (unless (executable-find "java")
     (warn! "Couldn't find java. PlantUML preview or syntax checking won't work"))
+  ;; graphviz
+  (unless (executable-find "dot")
+    (warn! "Couldn't fiind dot. PlantUML might not show any output"))
   ;; plantuml.jar
   (unless (file-exists-p plantuml-jar-path)
     (warn! "Couldn't find plantuml.jar. Install it with M-x plantuml-download-jar")))
