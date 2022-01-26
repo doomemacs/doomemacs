9c2621020e65a7d95ecd17cd22291ecfca36bb4e
Henrik Lissner <git@henrik.io>
2022-01-26 15:49:36 +0100

    tweak(java): default to +lsp instead of +meghanada
    The latter is a simpler default: requires no config and works well ootb.

--- init.example.el
+++ init.example.el
@@ -138,7 +138,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
