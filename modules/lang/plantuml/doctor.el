4808313889d56b89b14cf26c7f8bf9ca8a9f03fd
M. Yas. Davoodeh <30480116+Davoodeh@users.noreply.github.com>
2022-01-27 21:17:07 +0330

    docs(plantuml): add graphviz/dot doctor

--- modules/lang/plantuml/doctor.el
+++ modules/lang/plantuml/doctor.el
@@ -5,6 +5,9 @@
   ;; java
   (unless (executable-find "java")
     (warn! "Couldn't find java. PlantUML preview or syntax checking won't work"))
+  ;; graphviz
+  (unless (executable-find "dot")
+    (warn! "Couldn't find dot. PlantUML will only show outputs for sequence and activity diagrams"))
   ;; plantuml.jar
   (unless (file-exists-p plantuml-jar-path)
     (warn! "Couldn't find plantuml.jar. Install it with M-x plantuml-download-jar")))
