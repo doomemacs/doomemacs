cc9d71f53144e6913c0e42c8fd6fe7bd264983e7
Henrik Lissner <git@henrik.io>
2022-01-27 03:49:33 +0100

    bump: :core
    Fanael/rainbow-delimiters@d576e6694ad3 -> Fanael/rainbow-delimiters@a32b39bdfe6c
    bbatsov/projectile@2bb7ec28b127 -> bbatsov/projectile@4e6f66c329e5
    domtronn/all-the-icons.el@483dba65e897 -> domtronn/all-the-icons.el@2c963ebb75f2
    gilbertw1/better-jumper@3148a17b5920 -> gilbertw1/better-jumper@47622213783e
    justbur/emacs-which-key@1bb1f723dab2 -> justbur/emacs-which-key@9f64733e4ac5
    raxod502/straight.el@92d4153362a8 -> raxod502/straight.el@3b0f1a1d215f

--- core/packages.el
+++ core/packages.el
@@ -17,7 +17,7 @@
             :branch ,straight-repository-branch
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "92d4153362a87a585d16f31a2591324dac48e9f0")
+  :pin "3b0f1a1d215f3dd441bca6a31313aaf91874a311")
 
 ;; core-modules.el
 (package! use-package
@@ -25,16 +25,16 @@
   :pin "a7422fb8ab1baee19adb2717b5b47b9c3812a84c")
 
 ;; core-ui.el
-(package! all-the-icons :pin "483dba65e897071c156cefec937edcf51aa333db")
+(package! all-the-icons :pin "2c963ebb75f211d2f7ac3d2db5f4a9ee2f3e27da")
 (package! hide-mode-line :pin "bc5d293576c5e08c29e694078b96a5ed85631942")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
-(package! rainbow-delimiters :pin "d576e6694ad3a3e88b2bb1363305b38fa364c149")
+(package! rainbow-delimiters :pin "a32b39bdfe6c61c322c37226d66e1b6d4f107ed0")
 (package! restart-emacs :pin "1607da2bc657fe05ae01f7fdf26f716eafead02c")
 
 ;; core-editor.el
-(package! better-jumper :pin "3148a17b5920bba8ec4f81b717b99acde5fd5b74")
-(package! dtrt-indent :pin "1986ad4e60f2e21f69d77ef9fb14da80a6157866")
-(package! helpful :pin "2afbde902742b1aa64daa31a635ba564f14b35ae")
+(package! better-jumper :pin "47622213783ece37d5337dc28d33b530540fc319")
+(package! dtrt-indent :pin "926fc4260c3f71f5aac2e0becb9ee435a4124d5d")
+(package! helpful :pin "f865f17ad04cd270685187b0a5331ec8eb06e541")
 (package! pcre2el :pin "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
 (package! smartparens :pin "f59a40d54f35299007c396bd667ce3e9ec4714e3")
 (package! ws-butler
@@ -44,9 +44,9 @@
   :pin "572a10c11b6cb88293de48acbb59a059d36f9ba5")
 
 ;; core-projects.el
-(package! projectile :pin "2bb7ec28b1275bbce7cac743ee9e7b2cf41c5bbd")
-(package! project :pin "401e4aa68e5f46fe72ca6f9a1cb07cd84f5ee981")
+(package! projectile :pin "4e6f66c329e57d66269b4bd3fc02518eb0c677bc")
+(package! project :pin "5347dfc5f32b0408bf4ff7bbace7b2356cf59a8f")
 
 ;; core-keybinds.el
 (package! general :pin "9651024e7f40a8ac5c3f31f8675d3ebe2b667344")
-(package! which-key :pin "1bb1f723dab2fc8b88b7f7273d0a7fa11134b936")
+(package! which-key :pin "9f64733e4ac563c0cda3685acf4e1c2cf600319b")
