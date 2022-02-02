ee33bff115465da8da195ad73f40509e7958450e
Henrik Lissner <git@henrik.io>
2022-02-02 01:54:58 +0100

    bump: :emacs
    dgutov/diff-hl@6fa3af084309 -> dgutov/diff-hl@4a08b02afec1
    ideasman42/emacs-undo-fu-session@1810251485a5 -> ideasman42/emacs-undo-fu-session@edf050d61334
    magit/magit@1eb183e7672b -> magit/magit@846d153a2e71
    purcell/diredfl@4ca32658aeba -> purcell/diredfl@f9140b2c4215

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; emacs/dired/packages.el
 
-(package! diredfl :pin "4ca32658aebaf2335f0368a0fd08f52eb1aee960")
+(package! diredfl :pin "f9140b2c42151dca669003d685c9f079b2e3dc37")
 (package! dired-git-info :pin "9461476a28a5fec0784260f6e318237c662c3430")
-(package! diff-hl :pin "6fa3af0843093f44e028584a93eef091ec7e79d2")
+(package! diff-hl :pin "4a08b02afec1fc6b1e84de46cc34f75f6c9c3bcc")
 (package! dired-rsync :pin "7940d9154d0a908693999b0e1ea351a6d365c93d")
 (when (featurep! +ranger)
   (package! ranger :pin "2498519cb21dcd5791d240607a72a204d1761668"))
--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -4,4 +4,4 @@
 (if (featurep! +tree)
     (package! undo-tree :pin "e326c6135e62f5fe8536528d3acd5e798f847407")
   (package! undo-fu :pin "ab8bc10e424bccc847800c31ab41888db789d55d")
-  (package! undo-fu-session :pin "1810251485a551bc41472ec9e7e7bfab72a45a3c"))
+  (package! undo-fu-session :pin "edf050d6133478d04fc06cc65914517b18d6bcc6"))
