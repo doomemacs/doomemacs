fca8bd7f3fa697a91774c2ddedcd1a47cd7da01a
Henrik Lissner <git@henrik.io>
2022-02-02 01:58:55 +0100

    bump: :config default
    abo-abo/avy@e92cb37457b4 -> abo-abo/avy@ba5f035be336
    magnars/expand-region.el@95a773bd8f55 -> magnars/expand-region.el@7e5bbe2763c1
    noctuid/link-hint.el@3be270f3a732 -> noctuid/link-hint.el@676dac6621e3

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; config/default/packages.el
 
-(package! avy :pin "e92cb37457b43336b765630dbfbea8ba4be601fa")
+(package! avy :pin "ba5f035be33693d1a136a5cbeedb24327f551a92")
 (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-(package! link-hint :pin "3be270f3a732dc4acae6a20ff449eef0c4f9a966")
+(package! link-hint :pin "676dac6621e321b33a8d396fa27dd0ea619d21e3")
 
 (unless (featurep! :editor evil)
-  (package! expand-region :pin "95a773bd8f557cbd43d3b2dab2fa4417ec5927ab"))
+  (package! expand-region :pin "7e5bbe2763c12bae3e77fe0c49bcad05ff91dbfe"))
