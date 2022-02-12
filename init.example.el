5b2f3fa1bbc9df5775cfa1388dfce0727c17d5d3
Zero King <l2dy@icloud.com>
2022-02-12 03:00:12 +0000

    tweak(cc): default to +lsp
    Without +lsp, both irony-mode and rtags require extra configuration for
    system header path on macOS.
    
    With +lsp, clangd works well out of the box, and it's easier to
    configure than two separate daemons.
    
    Ref: https://github.com/Sarcasm/irony-mode/wiki/Mac-OS-X-issues-and-workaround
    Ref: Andersbakken/rtags#811

--- init.example.el
+++ init.example.el
@@ -113,7 +113,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;cc                ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
