1a7af22d2d135280164d80cde67c2a72fd96f0e5
shenleban tongying <shenlebantongying@gmail.com>
2022-02-23 02:18:41 -0500

    bump: :lang coq
    ProofGeneral/PG@2145c23f44a0 -> ProofGeneral/PG@df19c7ba0ead

--- modules/lang/coq/packages.el
+++ modules/lang/coq/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/coq/packages.el
 
-(package! proof-general :pin "2145c23f44a0951a14240d3b85a1a3d08aade9bb")
+(package! proof-general :pin "df19c7ba0eadf9f47e9dd757a7d0350fc967da99")
 (package! company-coq :pin "382db93374380e5db56f02934ee32bbe39159019")
