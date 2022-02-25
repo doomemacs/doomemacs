77a45e4341435f89e7eff3496ccf4070c54ce260
brotzeit <brotzeitmacher@gmail.com>
2022-02-25 18:13:44 +0100

    bump: :lang rust
    brotzeit/rustic@804ebfe0295a -> brotzeit/rustic@13ba8cd0c7f9

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rustic :pin "804ebfe0295a6bf37870e06f84a8d35f55c9f1a6")
+(package! rustic :pin "13ba8cd0c7f99dcc7baa55b0d5c68bc03ac5b9e0")
 (unless (featurep! +lsp)
   (package! racer :pin "1e63e98626737ea9b662d4a9b1ffd6842b1c648c"))
