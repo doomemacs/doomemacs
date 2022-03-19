23d77e688bc94b6d5cfdadc9dcd7d40d85f1b540
Henrik Lissner <git@henrik.io>
2022-03-19 02:53:37 +0100

    bump: :lang agda
    agda/agda@af9c4b968698 -> agda/agda@2816605bfdc2
    
    Close: #6150

--- modules/lang/agda/packages.el
+++ modules/lang/agda/packages.el
@@ -6,11 +6,11 @@
     :recipe (:host github :repo "agda/agda"
              :files ("src/data/emacs-mode/agda-input.el")
              :nonrecursive t)
-    :pin "af9c4b968698e285c453516b2104a347ad954849")
+    :pin "2816605bfdc24c93d8801016d31beeed7608ad02")
 
   (package! agda2-mode
     :recipe (:host github :repo "agda/agda"
              :files ("src/data/emacs-mode/*.el"
                      (:exclude "agda-input.el"))
              :nonrecursive t)
-    :pin "af9c4b968698e285c453516b2104a347ad954849"))
+    :pin "2816605bfdc24c93d8801016d31beeed7608ad02"))
