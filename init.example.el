28a929663bc56f523bef5f0c8a364258b992f02c
Henrik Lissner <git@henrik.io>
2022-03-21 04:01:23 +0100

    tweak: disable :ui doom-quit by default
    This module doesn't offer any real value, so shouldn't be enabled by
    default. Plus, some of its messages may give off the wrong impression to
    folks that aren't expecting them.
    
    Fix: #5733

--- init.example.el
+++ init.example.el
@@ -30,7 +30,7 @@
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
