48658d2136c3ae08fcf6ea0e94804d2a2cc1455d
Henrik Lissner <git@henrik.io>
2022-03-30 17:49:15 +0200

    bump: :lang org
    alf/ob-restclient.el@f81f2f4f3fe6 -> alf/ob-restclient.el@586f1fa07f76
    awth13/org-appear@303fcc8d5d85 -> awth13/org-appear@ffbd742267ff
    emacs-gnuplot/gnuplot@7138b139d2dc -> emacs-gnuplot/gnuplot@57be3c7addec
    emacs-straight/org-mode@e7ea951ac976 -> emacs-straight/org-mode@971eb6885ec9
    emacsmirror/org-contrib@e3183921779e -> emacsmirror/org-contrib@17f3c5143564
    fuxialexander/org-pdftools@a5b61bca3f8c -> fuxialexander/org-pdftools@967f48fb5038
    hakimel/reveal.js@19f04bcbcbc3 -> hakimel/reveal.js@918ee5610a50
    kaushalmodi/ox-hugo@1b6b3dc8f9f3 -> kaushalmodi/ox-hugo@65e349b306b7
    magit/orgit-forge@365b75609a94 -> magit/orgit-forge@36e57a035999
    magit/orgit@66367d6bfc5e -> magit/orgit@42b7f682b3e4
    marcinkoziej/org-pomodoro@aa07c11318f9 -> marcinkoziej/org-pomodoro@3f5bcfb80d61
    nnicandro/emacs-jupyter@42a9765897ad -> nnicandro/emacs-jupyter@0a7055d7b12c
    oer/org-re-reveal@55fca47c740c -> oer/org-re-reveal@e5bae22b9ef6
    org-roam/org-roam@eed1df90f571 -> org-roam/org-roam@36152590ad1e

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -27,11 +27,11 @@
                        (format "(defun org-git-version (&rest _) \"%s-??-%s\")\n"
                                version (cdr (doom-call-process "git" "rev-parse" "--short" "HEAD")))
                        "(provide 'org-version)\n"))))
-  :pin "e7ea951ac976ac78d4f6c3df9979bb9e942ef086")
+  :pin "971eb6885ec996c923e955730df3bafbdc244e54")
 (package! org-contrib
   :recipe (:host github
            :repo "emacsmirror/org-contrib")
-  :pin "e3183921779eb4f36a2170ebb58e43eb0e84a07e")
+  :pin "17f3c514356430448627104e015f155008b45575")
 
 (package! avy)
 (package! htmlize :pin "dd27bc3f26efd728f2b1f01f9e4ac4f61f2ffbf9")
@@ -47,30 +47,30 @@
     :recipe (:host github :repo "hlissner/evil-org-mode")
     :pin "a9706da260c45b98601bcd72b1d2c0a24a017700"))
 (when (featurep! :tools pdf)
-  (package! org-pdftools :pin "a5b61bca3f8c91b0859bb0df1a929f9a31a57b99"))
+  (package! org-pdftools :pin "967f48fb5038bba32915ee9da8dc4e8b10ba3376"))
 (when (featurep! :tools magit)
-  (package! orgit :pin "66367d6bfc5e00726fb74f7cd20c32175ab8521b")
+  (package! orgit :pin "42b7f682b3e4e487ff209a44221a729921241133")
   (when (featurep! :tools magit +forge)
-    (package! orgit-forge :pin "365b75609a9454dccf5681eb6075ca53bd32af85")))
+    (package! orgit-forge :pin "36e57a0359992e02312f453b8086512e77beb150")))
 (when (featurep! +brain)
   (package! org-brain :pin "46ca9f766322cff31279ecdf02251ff24a0e9431"))
 (when (featurep! +dragndrop)
   (package! org-download :pin "947ca223643d28e189480e607df68449c15786cb"))
 (when (featurep! +gnuplot)
-  (package! gnuplot :pin "7138b139d2dca9683f1a81325c643b2744aa1ea3")
+  (package! gnuplot :pin "57be3c7addec31e226a5a27aa553e996f9c684e3")
   (package! gnuplot-mode :pin "601f6392986f0cba332c87678d31ae0d0a496ce7"))
 (when (featurep! +ipython) ; DEPRECATED
   (package! ob-ipython :pin "7147455230841744fb5b95dcbe03320313a77124"))
 (when (featurep! +jupyter)
-  (package! jupyter :pin "42a9765897ad36518b5371f558b36cdac3a0ec74"))
+  (package! jupyter :pin "0a7055d7b12cf98723110415b08ee91869fa7d94"))
 (when (featurep! +journal)
   (package! org-journal :pin "f121450610650c63aabf13afd0d2089e05fad2e4"))
 (when (featurep! +noter)
   (package! org-noter :pin "9ead81d42dd4dd5074782d239b2efddf9b8b7b3d"))
 (when (featurep! +pomodoro)
-  (package! org-pomodoro :pin "aa07c11318f91219336197e62c47bc7a3d090479"))
+  (package! org-pomodoro :pin "3f5bcfb80d61556d35fc29e5ddb09750df962cc6"))
 (when (featurep! +pretty)
-  (package! org-appear :pin "303fcc8d5d85a4ebff2798dab50b2ccc0255ea5f")
+  (package! org-appear :pin "ffbd742267ff81ba8433177fac5d7fe22b6d68a9")
   (package! org-superstar :pin "03be6c0a3081c46a59b108deb8479ee24a6d86c0")
   (package! org-fancy-priorities :pin "7f677c6c14ecf05eab8e0efbfe7f1b00ae68eb1d"))
 (when (featurep! +present)
@@ -78,11 +78,11 @@
     :recipe (:host github :repo "anler/centered-window-mode")
     :pin "80965f6c6afe8d918481433984b493de72af5399")
   (package! org-tree-slide :pin "3faa042393ebfe5699a3bffce775f039d7416ceb")
-  (package! org-re-reveal :pin "55fca47c740c50fe04cbf2b8ae90e02174626c0c")
+  (package! org-re-reveal :pin "e5bae22b9ef63a9fcfbbaa000e087703751eaad0")
   (package! revealjs
     :recipe (:host github :repo "hakimel/reveal.js"
              :files ("css" "dist" "js" "plugin"))
-    :pin "19f04bcbcbc3f83a6f90de4f7b2bf994c983d302"))
+    :pin "918ee5610a5062d872fdda43351de09f526a71bd"))
 (cond
  ((featurep! +roam)
   (package! org-roam
@@ -93,7 +93,7 @@
     ;; FIXME A :recipe isn't strictly necessary, but without it, our package
     ;;       bumper fails to distinguish between org-roam v1 and v2.
     :recipe (:host github :repo "org-roam/org-roam")
-    :pin "eed1df90f571b19cae1581e7f5d938892b00c24d")))
+    :pin "36152590ad1e8ffea86cb909e5ef818cbdb2a22d")))
 
 ;;; Babel
 (package! ob-async :pin "9aac486073f5c356ada20e716571be33a350a982")
@@ -112,7 +112,7 @@
     :recipe (:host github :repo "DEADB17/ob-racket")
     :pin "d8fd51bddb019b0eb68755255f88fc800cfe03cb"))
 (when (featurep! :lang rest)
-  (package! ob-restclient :pin "f81f2f4f3fe6882947b8547ccd570f540106ed4d"))
+  (package! ob-restclient :pin "586f1fa07f76aaca13cb3f86945759f4b9fb8db7"))
 (when (featurep! :lang scala)
   (package! ob-ammonite :pin "39937dff395e70aff76a4224fa49cf2ec6c57cca"))
 
@@ -122,6 +122,6 @@
 (when (featurep! +hugo)
   (package! ox-hugo
     :recipe (:host github :repo "kaushalmodi/ox-hugo" :nonrecursive t)
-    :pin "1b6b3dc8f9f310102d1a6862889c6ff5e0fb7b13"))
+    :pin "65e349b306b7fa27285f4c663e44bb36f6c8e653"))
 (when (featurep! :lang rst)
   (package! ox-rst :pin "99fa790da55b57a3f2e9aa187493ba434a64250e"))
