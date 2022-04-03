doomemacs/doomemacs

commit cf362bc1a0e3a19ceecc022b8146df1b9f7cee9f

fix(lib): update doom/set-indent-width for dtrt-indent
dtrt-indent-hook-mapping-list entries now accept lists of variables.
This updates doom/set-indent-width to accommodate that.

Ref: jscheid/dtrt-indent@1986ad4e60f2

core/autoload/text.el
@@ -315,9 +315,10 @@ editorconfig or dtrt-indent installed."
          (let (editorconfig-lisp-use-default-indent)
            (editorconfig-set-indentation nil width)))
         ((require 'dtrt-indent nil t)
-         (when-let (var (nth 2 (assq major-mode dtrt-indent-hook-mapping-list)))
-           (doom-log "Updated %s = %d" var width)
-           (set var width))))
+         (when-let (vars (nth 2 (assq major-mode dtrt-indent-hook-mapping-list)))
+           (dolist (var (doom-enlist var))
+             (doom-log "Updated %s = %d" var width)
+             (set var width)))))
   (message "Changed indentation to %d" width))
 
 

commit d35157ce50876a78b7a089da71b0d14d28339705

fix(lib): void-variable var
Caused by a typo introduced in cf362bc.

Fix: #6240
Amend: cf362bc1a0e3

core/autoload/text.el
@@ -316,7 +316,7 @@ editorconfig or dtrt-indent installed."
            (editorconfig-set-indentation nil width)))
         ((require 'dtrt-indent nil t)
          (when-let (vars (nth 2 (assq major-mode dtrt-indent-hook-mapping-list)))
-           (dolist (var (doom-enlist var))
+           (dolist (var (doom-enlist vars))
              (doom-log "Updated %s = %d" var width)
              (set var width)))))
   (message "Changed indentation to %d" width))