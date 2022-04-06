fd991026ff4d3dcebb443cf2b028f2746fa28667
Henrik Lissner <git@henrik.io>
2022-04-06 18:44:38 +0200

    fix: void-function doom-partial
    Fix: #6259

--- core/core.el
+++ core/core.el
@@ -171,7 +171,7 @@ users).")
 
   (with-eval-after-load 'comp
     ;; HACK Disable native-compilation for some troublesome packages
-    (mapc (doom-partial #'add-to-list 'native-comp-deferred-compilation-deny-list)
+    (mapc (apply-partially #'add-to-list 'native-comp-deferred-compilation-deny-list)
           (let ((local-dir-re (concat "\\`" (regexp-quote doom-local-dir))))
             (list (concat local-dir-re ".*/emacs-jupyter.*\\.el\\'")
                   (concat local-dir-re ".*/evil-collection-vterm\\.el\\'")
