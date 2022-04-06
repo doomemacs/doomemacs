6bbf837c9f4bc5e50f241b3439f8b76c76f878e2
Ellis Kenyő <ellis@coreblue.co.uk>
2022-04-06 08:18:07 +0100

    fix(graphql): remove *Messages* spam
    The faces were defined wrong, so every time a buffer came into focus, it
    would spam *Messages*.

--- modules/lang/graphql/config.el
+++ modules/lang/graphql/config.el
@@ -5,15 +5,14 @@
     '((t (:foreground "#E10098")))
     "Face for GraphQL icon."
     :group 'all-the-icons-faces)
-
   ;; Define a doom-modeline compatiable major-mode icon
   (after! all-the-icons
     (setf (alist-get "graphql" all-the-icons-extension-icon-alist)
-          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face 'all-the-icons-rhodamine))
+          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face all-the-icons-rhodamine))
     (setf (alist-get "gql" all-the-icons-extension-icon-alist)
-          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face 'all-the-icons-rhodamine))
+          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face all-the-icons-rhodamine))
     (setf (alist-get 'graphql-mode all-the-icons-mode-icon-alist)
-          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face 'all-the-icons-rhodamine)))
+          '(all-the-icons-fileicon "graphql" :v-adjust -0.05 :face all-the-icons-rhodamine)))
   (if (featurep! +lsp)
       (add-hook 'graphql-mode-local-vars-hook #'lsp! 'append)
     (set-company-backend! 'graphql-mode 'company-graphql))
