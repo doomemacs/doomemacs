136e7df76ea3edc09c712ee6f6d80d0bb225ac4a
Itai Y. Efrat <itai3397@gmail.com>
2022-04-09 15:38:39 +0300

    bump: :completion vertico consult-lsp
    gagbo/consult-lsp@5a3c4e3f4233 -> gagbo/consult-lsp@a8eb3a062feb
    iyefrat/all-the-icons-completion@9e7d456b0934 -> iyefrat/all-the-icons-completion@286e2c064a12
    minad/consult@36b8bc706500 -> minad/consult@d30213aa2093
    minad/marginalia@a514c024ac27 -> minad/marginalia@dbc37b373e73
    minad/vertico@7ec0f0c07694 -> minad/vertico@46e8e0565079
    
    Close: #6279
    Close: #6277

--- modules/completion/vertico/packages.el
+++ modules/completion/vertico/packages.el
@@ -4,11 +4,11 @@
 (package! vertico
   :recipe (:host github :repo "minad/vertico"
            :files ("*.el" "extensions/*.el"))
-  :pin "7ec0f0c0769496edf4c8376950c2fdeb44b98102")
+  :pin "46e8e0565079b7161ada4beb94c8938ee9c04bfb")
 
 (package! orderless :pin "8f64537f556f26492fe5ee401d8d578d7d88684b")
 
-(package! consult :pin "36b8bc7065005778db83f12a594b6929bfd3319c")
+(package! consult :pin "d30213aa209391e03b1c1011df92d91a1fc5ef32")
 (package! consult-dir :pin "08f543ae6acbfc1ffe579ba1d00a5414012d5c0b")
 (when (featurep! :checkers syntax)
   (package! consult-flycheck :pin "9b40f136c017fadf6239d7602d16bf73b4ad5198"))
@@ -16,9 +16,9 @@
 (package! embark :pin "2890e535f55b1f08f379fd761b263fa337a72185")
 (package! embark-consult :pin "2890e535f55b1f08f379fd761b263fa337a72185")
 
-(package! marginalia :pin "a514c024ac2796ec9d52f65c1f51b51f96bcb1c7")
+(package! marginalia :pin "dbc37b373e734269bd75d1763e7309863508bf10")
 
 (package! wgrep :pin "f9687c28bbc2e84f87a479b6ce04407bb97cfb23")
 
 (when (featurep! +icons)
-  (package! all-the-icons-completion :pin "9e7d456b0934ecb568b6f05a8445e3f4ce32261f"))
+  (package! all-the-icons-completion :pin "286e2c064a1298be0d8d4100dc91d7a7a554d04a"))
