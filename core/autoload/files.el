doomemacs/doomemacs

commit 10d00b7cc43323e79f365d3a9be2fa37ed398d88

fix(lib): unused :map property in doom-files-in

core/autoload/files.el
@@ -111,9 +111,9 @@ MATCH is a string regexp. Only entries that match it will be included."
                    (not (and (file-symlink-p file)
                              (not follow-symlinks)))
                    (<= mindepth 0)
-                   (list (cond (map (funcall map file))
-                               (relative-to (file-relative-name file relative-to))
-                               (file))))
+                   (list (if relative-to
+                             (file-relative-name file relative-to)
+                           file)))
               (and (>= depth 1)
                    (apply #'doom-files-in file
                           (append (list :mindepth (1- mindepth)
@@ -128,7 +128,9 @@ MATCH is a string regexp. Only entries that match it will be included."
                        (file-relative-name file relative-to)
                      file)
                    result))))
-    result))
+    (if map
+        (mapcar map result)
+      result)))
 
 ;;;###autoload
 (defun doom-file-cookie-p (file &optional cookie null-value)

commit 80cd7557e147ccd0abef05ff4eafb93fe9545ad4

fix(lib): doom-files-in's :map causing type errors
This regression was introduced in 10d00b7cc433, causing
"wrong-type-argument: stringp (X . Y)" errors. It is triggered when
doom-files-in is used with a non-nil :map on a nested directory
tree (like our module tree).

Fix: #6370
Amend: 10d00b7cc433

core/autoload/files.el
@@ -118,7 +118,8 @@ MATCH is a string regexp. Only entries that match it will be included."
                    (apply #'doom-files-in file
                           (append (list :mindepth (1- mindepth)
                                         :depth (1- depth)
-                                        :relative-to relative-to)
+                                        :relative-to relative-to
+                                        :map nil)
                                   rest)))))
             ((and (memq type '(t files))
                   (string-match-p match file)