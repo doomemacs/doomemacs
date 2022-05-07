7d6d01b1f4ed246f77a76226bec105d0da67ab3f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-05-08 01:09:20 +0200

    bump: :tools debugger dap-mode
    emacs-lsp/dap-mode@f918c0580bd1 -> emacs-lsp/dap-mode@61b5938d9a61

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -6,5 +6,5 @@
     (package! realgud-trepan-ni :pin "0ec088ea343835e24ae73da09bea96bfb02a3130")))
 
 (when (featurep! +lsp)
-  (package! dap-mode :pin "f918c0580bd17105cbe50aa701a2375abca5a6ab")
+  (package! dap-mode :pin "61b5938d9a61882c2e76c58ca7f70e9337ddba00")
   (package! posframe :pin "c91d4d53fa479ceb604071008ce0a901770eff57"))
