ff543e0cbb407c9a14d7b3701e699077508b1aa9
Ellis Kenyő <ellis@coreblue.co.uk>
2022-05-12 16:29:46 +0100

    fix(lsp): add warning for missing npm

--- modules/tools/lsp/doctor.el
+++ modules/tools/lsp/doctor.el
@@ -3,3 +3,7 @@
 (assert! (not (and (featurep! +eglot)
                    (featurep! +peek)))
          "+eglot and +peek flags are not compatible. Peek uses lsp-mode, while Eglot is another package altogether for LSP.")
+
+(unless (executable-find "npm")
+  (warn! "Couldn't find npm, most server installers won't work and will have to be installed manually.
+For more information, see https://emacs-lsp.github.io/lsp-mode/page/languages/."))
