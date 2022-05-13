770bf9687aa906cf86bb783f94eed84e5900b136
Ellis Kenyő <ellis@coreblue.co.uk>
2022-05-13 08:28:04 +0100

    bump: lsp-mode
    emacs-lsp/lsp-mode@f49ea4e36528 -> emacs-lsp/lsp-mode@465bcc5fbeb6

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -6,7 +6,7 @@
       (package! eglot :pin "fd9a5646d1b49ef5968713005d83131dd75a52ad")
       (when (featurep! :completion vertico)
         (package! consult-eglot :pin "f93c571dc392a8b11d35541bffde30bd9f411d30")))
-  (package! lsp-mode :pin "1e7128c43f611ed78752130ce5ce67e641a276df")
+  (package! lsp-mode :pin "465bcc5fbeb6293446a3241f5f2c1b872337958c")
   (package! lsp-ui :pin "96b1ecbfbf87a775f05b5f0b55253376a3bd61e7")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "3e87441a625d65ced5a208a0b0442d573596ffa3"))
