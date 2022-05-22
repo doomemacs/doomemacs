a143753a4f2d9f28618ee6e899f0b24965055632
Pi-Cla <pirateclip@protonmail.com>
2022-05-22 03:59:15 +0000

    Bump it again
    This bump also removes C++ entirely from pdf-tools, as it is not needed anymore for
    correct compilation of `epdfinfo`. The following changes are made:
    
    - Remove private headers from all the server code. This involves:
      - Replace private header functionality with the correct public
        functions provided by poppler.
      - Remove unneeded headers around date checks
      - Delete dead-code (headers, m4 macros)
    
    - Remove C++, g++ and similar aliases from all the code. Also:
      - Remove related information from the README
      - Remove related information from CI and Dockerfiles
    
    Work done by me

--- modules/tools/pdf/packages.el
+++ modules/tools/pdf/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/pdf/packages.el
 
-(package! pdf-tools :pin "ab61b0472980200f52e2e23782bc07255baebe72")
+(package! pdf-tools :pin "f9ccdf99e560bae70d3a13325cec9dc0e3cc45b0")
 (package! saveplace-pdf-view :pin "54ed966b842501c3c092dbf57b372e37b033c578")
