33de147b8f29ee0626f6b4b85c8ca343ce537d2e
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-05-23 20:01:59 +0100

    bump: :tools tree-sitter
    emacs-tree-sitter/tree-sitter-langs@0dd5e56e2f56 -> emacs-tree-sitter/tree-sitter-langs@deb2d8674be8
    meain/evil-textobj-tree-sitter@bfdef5a292f7 -> meain/evil-textobj-tree-sitter@0bf5bbbfecba

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -5,8 +5,8 @@
   :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
 
 (package! tree-sitter-langs
-  :pin "0dd5e56e2f5646aa51ed0fc9eb869a8f7090228a")
+  :pin "deb2d8674be8f777ace50e15c7c041aeddb1d0b2")
 
 (when (featurep! :editor evil +everywhere)
   (package! evil-textobj-tree-sitter
-    :pin "bfdef5a292f7dde36967bb86eb2f7009b03631b1"))
+    :pin "0bf5bbbfecba95d49d40441ea54c6130e52bbeb1"))
