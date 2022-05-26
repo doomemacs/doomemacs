866e1b32bf422c5d0770f55b69016585ed830a86
Pi-Cla <pirateclip@protonmail.com>
2022-05-26 21:47:00 +0000

    bump: :tools pdf
    vedang/pdf-tools@326552eef71a -> vedang/pdf-tools@f9ccdf99e560
    
    This bump also removes C++ entirely from pdf-tools, as it is not needed anymore for
    correct compilation of `epdfinfo`. The following changes are made:
    
    - Remove private headers from all the server code. This involves:
      - Replace private header functionality with the correct public
        functions provided by poppler.
      - Remove unneeded headers around date checks
      - Delete dead-code (headers, m4 macros)
    - Remove C++, g++ and similar aliases from all the code. Also:
      - Remove related information from the README
      - Remove related information from CI and Dockerfiles
    
    Ref: vedang/pdf-tools#96
    Ref: vedang/pdf-tools@4460a4fd3122
    Fix: vedang/pdf-tools#102
    Fix: politza/pdf-tools#706
    Fix: vedang/pdf-tools#90
    Fix: vedang/pdf-tools#86

--- modules/tools/pdf/packages.el
+++ modules/tools/pdf/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/pdf/packages.el
 
-(package! pdf-tools :pin "326552eef71ae6d53e215c46be5bf532575b7abb")
+(package! pdf-tools :pin "f9ccdf99e560bae70d3a13325cec9dc0e3cc45b0")
 (package! saveplace-pdf-view :pin "54ed966b842501c3c092dbf57b372e37b033c578")
