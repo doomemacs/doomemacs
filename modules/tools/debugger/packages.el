1b8f46c7c5893d63e4bcebc203c0d28df9f5981b
Henrik Lissner <git@henrik.io>
2022-05-27 03:03:21 +0200

    bump: :tools debugger lsp
    emacs-lsp/dap-mode@f918c0580bd1 -> emacs-lsp/dap-mode@67fd9e5d4e15
    emacs-lsp/lsp-mode@1e7128c43f61 -> emacs-lsp/lsp-mode@9faa49269234
    emacs-lsp/lsp-ui@96b1ecbfbf87 -> emacs-lsp/lsp-ui@370022b6785e
    gagbo/consult-lsp@a8eb3a062feb -> gagbo/consult-lsp@19606a03cf85
    joaotavora/eglot@fd9a5646d1b4 -> joaotavora/eglot@2b87b06d9ef1
    mohkale/consult-eglot@f93c571dc392 -> mohkale/consult-eglot@0da8801dd843
    
    - Debuggers (see #6381):
      - Add support for new cpptools (upgraded from 0.29.0 to 1.9.8).
      - Add support for new codelldb (from 1.6.0 to 1.7.0).
      - Fix download issue of gdb-lldb (aka webfreak.debug).
    - LSP support:
      - Fix inlay hints for rust-analyzer (see emacs-lsp/lsp-mode#3467)
    
    Close: #6395
    Close: #6381
    Fix: #6394
    Fix: #6403
    Ref: emacs-lsp/lsp-mode#3467

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -6,5 +6,5 @@
     (package! realgud-trepan-ni :pin "0ec088ea343835e24ae73da09bea96bfb02a3130")))
 
 (when (featurep! +lsp)
-  (package! dap-mode :pin "f918c0580bd17105cbe50aa701a2375abca5a6ab")
+  (package! dap-mode :pin "67fd9e5d4e157e72b09ab34daca67619cb98c673")
   (package! posframe :pin "c91d4d53fa479ceb604071008ce0a901770eff57"))
