d1ba626a2a4b4579933d8817886baa64636aac4f
Yaman Qalieh <ybq987@gmail.com>
2022-05-29 11:18:14 -0400

    tweak(mu4e): increase human-date width to fit year

--- modules/email/mu4e/config.el
+++ modules/email/mu4e/config.el
@@ -71,7 +71,7 @@
         ;; remove 'lists' column
         mu4e-headers-fields
         '((:account-stripe . 1)
-          (:human-date . 8)
+          (:human-date . 10)
           (:flags . 6) ; 3 icon flags
           (:from-or-to . 25)
           (:subject)))
