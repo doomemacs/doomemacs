55d34be546ab1f4dc75916f678d74f0a2e6ee781
Nopanun Laochunhanun (Thaen) <nopanun@central.tech>
2022-06-05 15:33:53 +0700

    fix(graphql): workaround to build company-graphql

--- modules/lang/graphql/packages.el
+++ modules/lang/graphql/packages.el
@@ -4,4 +4,6 @@
 (package! graphql-mode :pin "9740e4027bd9313697d5cac5caaa5b15626ab1da")
 (package! graphql-doc :pin "6ba7961fc9c5c9818bd60abce6ba9dfef2dad452")
 (when (not (featurep! +lsp))
-  (package! company-graphql :pin "757dfa45ad0cef9b9c362c8993d6474a2426c01c"))
+  (package! company-graphql
+    :recipe (:host github :repo "thaenalpha/company-graphql")
+    :pin "aed9f5109e87..."))
