032ba01d1465607c54c1c839d7ed256df27b743a
Jonathan Pieper <ody55eus@mailbox.org>
2022-06-14 10:07:18 +0200

    fix(evil): nv mode binding "K" should be "gK"
    When typing the letter "~K~" in normal or visual mode,
    evil starts ~lookup/documentation~.
    This behavior sometimes overlaps with other major/minor modes.
    
    Solution:
    This Keybinding is probably supposed to be "~gK~" originally and
    should be changed back.

--- modules/editor/evil/config.el
+++ modules/editor/evil/config.el
@@ -494,7 +494,7 @@ directives. By default, this only recognizes C directives.")
       :v  "g-"    #'evil-numbers/dec-at-pt-incremental
       :v  "g+"    #'evil-numbers/inc-at-pt
       (:when (featurep! :tools lookup)
-       :nv "K"   #'+lookup/documentation
+       :nv "gK"   #'+lookup/documentation
        :nv "gd"  #'+lookup/definition
        :nv "gD"  #'+lookup/references
        :nv "gf"  #'+lookup/file
