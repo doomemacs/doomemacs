33d2e223e52dc043eb75651b3314c562f546ffc8
TEC <tec@tecosaur.com>
2022-06-16 19:05:01 +0800

    fix(emacs-lisp): don't expect all buf to be files
    Fix #6181

--- modules/lang/emacs-lisp/autoload.el
+++ modules/lang/emacs-lisp/autoload.el
@@ -12,7 +12,8 @@ to a pop up buffer."
     (condition-case-unless-debug e
         (let ((result
                (let* ((buffer-file-name (buffer-file-name (buffer-base-buffer)))
-                      (buffer-file-truename (file-truename buffer-file-name))
+                      (buffer-file-truename
+                       (and buffer-file-name (file-truename buffer-file-name)))
                       (doom--current-module
                        (ignore-errors (doom-module-from-path buffer-file-name)))
                       (debug-on-error t))
