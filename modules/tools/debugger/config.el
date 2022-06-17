005bc24dda3b2f79297ed2d136a91c7accad1e1f
mohenoo <mohenoo@gmail.com>
2022-06-18 00:15:34 +0800

    fix(debugger): dap-go is deprecated, use dap-dlv-go instead

--- modules/tools/debugger/config.el
+++ modules/tools/debugger/config.el
@@ -19,7 +19,7 @@
 (defvar +debugger--dap-alist
   `(((:lang cc +lsp)         :after ccls        :require (dap-lldb dap-gdb-lldb))
     ((:lang elixir +lsp)     :after elixir-mode :require dap-elixir)
-    ((:lang go +lsp)         :after go-mode     :require dap-go)
+    ((:lang go +lsp)         :after go-mode     :require dap-dlv-go)
     ((:lang java +lsp)       :after java-mode   :require lsp-java)
     ((:lang php +lsp)        :after php-mode    :require dap-php)
     ((:lang python +lsp)     :after python      :require dap-python)
