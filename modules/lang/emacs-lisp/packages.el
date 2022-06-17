52d0c92feaf5988787142534d4479f8e79dbd4d7
Henrik Lissner <git@henrik.io>
2022-06-17 21:53:21 +0200

    bump: :lang emacs-lisp
    jorgenschaefer/emacs-buttercup@f5cbf97e1086 -> jorgenschaefer/emacs-buttercup@ceedad5efa79
    purcell/flycheck-package@ecd03f837906 -> purcell/flycheck-package@615c1ed8c6fb
    xuchunyang/elisp-demos@924b07d28e4f -> xuchunyang/elisp-demos@01c301b516e9

--- modules/lang/emacs-lisp/packages.el
+++ modules/lang/emacs-lisp/packages.el
@@ -10,10 +10,10 @@
 (package! macrostep :pin "424e3734a1ee526a1bd7b5c3cd1d3ef19d184267")
 (package! overseer :pin "02d49f582e80e36b4334c9187801c5ecfb027789")
 (package! elisp-def :pin "dfca043ec0cbead67bd9c526cb009daf771d0fa2")
-(package! elisp-demos :pin "924b07d28e4f5b82f0e1377bcde800068f0a6d9d")
+(package! elisp-demos :pin "01c301b516e9949d0239d20f6834afbc9acf0abb")
 (when (featurep! :checkers syntax)
-  (package! flycheck-package :pin "ecd03f83790611888d693c684d719e033f69cb40")
+  (package! flycheck-package :pin "615c1ed8c6fb7c73abec6aaa73d3fef498d231bc")
   (package! flycheck-cask :pin "4b2ede6362ded4a45678dfbef1876faa42edbd58"))
 
 ;; Libraries
-(package! buttercup :pin "f5cbf97e1086441b2d6fa3ea240758b932c7c5e1")
+(package! buttercup :pin "ceedad5efa797e860dbb356bc2c3028a4e0321ec")
