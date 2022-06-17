7a164f9d4ccb935028802fd5800e70088ff7e4bf
Filipe Regadas <oss@regadas.email>
2022-06-17 21:47:23 +0500

    bump: :lang dhall
    psibi/dhall-mode@ad259c8a2292 -> psibi/dhall-mode@c77f1c1e75b6
    
    Ref: psibi/dhall-mode#36

--- modules/lang/dhall/packages.el
+++ modules/lang/dhall/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/dhall/packages.el
 
-(package! dhall-mode :pin "ad259c8a2292fb398dff1ce7d25c686edb02945d")
+(package! dhall-mode :pin "c77f1c1e75b6d2725019c5275fc102ae98d25628")
