149306ef016dd9710402f1190ad175d193897482
Henrik Lissner <git@henrik.io>
2022-06-18 22:04:54 +0200

    fix(ci): update ci config
    To reflect recent changes to Doom's CLI framework.

--- .github/ci.el
+++ .github/ci.el
@@ -2,19 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-;;; Types
-(add-to-list 'doom-cli-commit-types 'module)
-(add-to-list 'doom-cli-commit-scopeless-types 'module)
+(after! core-cli-ci
+  ;;; Commit linter types
+  (add-to-list 'doom-ci-commit-types 'module)
+  (add-to-list 'doom-ci-commit-scopeless-types 'module)
 
+  ;;; Commit linter scopes
+  (add-to-list 'doom-ci-commit-scopes "cli")
+  (add-to-list 'doom-ci-commit-scopes "lib")
+  (add-to-list 'doom-ci-commit-scopes "docs")
+  (add-to-list 'doom-ci-commit-scopes '(docs "install" ci-check-docs-scope))
+  (add-to-list 'doom-ci-commit-scopes #'ci-check-module-scope)
+  ;; DEPRECATED Will be removed once modules live in their own repo
+  (add-to-list 'doom-ci-commit-scopes '(release "modules")))
 
-;;; Scopes
-(add-to-list 'doom-cli-commit-scopes "cli")
-(add-to-list 'doom-cli-commit-scopes "lib")
-(add-to-list 'doom-cli-commit-scopes "docs")
-(add-to-list 'doom-cli-commit-scopes '(docs "install" ci-check-docs-scope))
-(add-to-list 'doom-cli-commit-scopes #'ci-check-module-scope)
-;; DEPRECATED Will be removed once modules live in their own repo
-(add-to-list 'doom-cli-commit-scopes '(release "modules"))
+(after! core-cli-make
+  ;;; Codeowners
+  (dolist (path (cdr (doom-module-load-path (list doom-modules-dir))))
+    (save-match-data
+      (when (string-match "/modules/\\([^/]+\\)/\\([^/]+\\)/$" path)
+        (add-to-list 'doom-make-codeowners
+                     (cons (format "%s*" (substring (match-string 0 path) 1))
+                           (list "@doomemacs/maintainers"
+                                 (format "@doomemacs/%s-%s"
+                                         (match-string 1 path)
+                                         (match-string 2 path))))))))
+
+  ;;; Documentation exporters
+  (add-to-list 'doom-ci-docs-title-replace '("^Doom Emacs " . "")))
 
 
 ;;; Helpers
