53f950928c9c83b91b8e5c9d47b3a538c8802917
David Widmann <david.widmann@it.uu.se>
2022-06-28 11:49:52 +0200

    bump: :tools lsp|lsp-mode
    emacs-lsp/lsp-mode@6b6afc00deec -> emacs-lsp/lsp-mode@4b23ed460243

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -6,7 +6,7 @@
       (package! eglot :pin "e835996e16610d0ded6d862214b3b452b8803ea8")
       (when (featurep! :completion vertico)
         (package! consult-eglot :pin "0da8801dd8435160ce1f62ad8066bd52e38f5cbd")))
-  (package! lsp-mode :pin "6b6afc00deec6dacb78834c02ed5f262e72ce337")
+  (package! lsp-mode :pin "4b23ed460243f860327ba105e753326a721edd82")
   (package! lsp-ui :pin "a0b97db2ada163453c9072d3640202a0b27c29f5")
   (when (featurep! :completion ivy)
     (package! lsp-ivy :pin "3e87441a625d65ced5a208a0b0442d573596ffa3"))
