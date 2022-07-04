e35cc0dfe7c804dc9d5501bb38e497139433d985
Henrik Lissner <git@henrik.io>
2022-07-04 21:12:13 +0200

    release(modules): 22.07.0-dev
    Ref: 2b39e4136850

--- core/core.el
+++ core/core.el
@@ -112,7 +112,7 @@
 (defconst doom-version "3.0.0-dev"
   "Current version of Doom Emacs core.")
 
-(defconst doom-modules-version "22.06.0-dev"
+(defconst doom-modules-version "22.07.0-dev"
   "Current version of Doom Emacs.")
 
 
