8db87f54df4d3e7989052016a28c32985dbe77e2
Gavin Downard <gavin.downard@runbox.com>
2022-07-05 11:59:07 -0700

    bump: :lang lua fennel-mode
    https://git.sr.ht/~technomancy/fennel-mode@50ef3c6246f3 -> https://git.sr.ht/~technomancy/fennel-mode@566435734946
    
    A fix was added upstream to fix `fennel-repl` no longer returning the
    repl buffer. This fixes `+eval-open-repl` in fennel-mode.

--- modules/lang/lua/packages.el
+++ modules/lang/lua/packages.el
@@ -11,7 +11,7 @@
       :pin "fcb99e5efcf31db05f236f02eaa575986a57172d")))
 
 (when (featurep! +fennel)
-  (package! fennel-mode :pin "50ef3c6246f36085cd908cf5432133cadb792304"))
+  (package! fennel-mode :pin "5664357349462d0564c0bb55cb289a6722f0ecbc"))
 
 (when (featurep! :completion company)
   (package! company-lua :pin "29f6819de4d691e5fd0b62893a9f4fbc1c6fcb52"))
