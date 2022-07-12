5bbc299b8bfb49c20d9034ddd2d0d29f7152e05b
Dejan Benedik <dejan.benedik@3fs.si>
2022-07-12 06:38:47 +0200

    docs(sh): add doctor check for shfmt

--- modules/lang/sh/doctor.el
+++ modules/lang/sh/doctor.el
@@ -4,6 +4,10 @@
  (unless (executable-find "shellcheck")
   (warn! "Couldn't find shellcheck. Shell script linting will not work")))
 
+(when (featurep! :editor format)
+  (unless (executable-find "shfmt")
+    (warn! "Couldn't find shfmt. Code formatting will not work.")))
+
 (assert! (or (not (featurep! +tree-sitter))
              (featurep! :tools tree-sitter))
          "This module requires (:tools tree-sitter)")
