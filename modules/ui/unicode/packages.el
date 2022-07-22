84f0ec567e287e53bf44ac3aebe3abd9542130e3
Antonio Ruiz <luneth1314@gmail.com>
2022-07-22 10:13:05 -0400

    fix(unicode): fix long startup times on Emacs29
    This patch fixes Pcache for users of Emacs29 so that users of Unicode
    Fonts on Emacs29 do not have to have 30 seconds plus initialization
    times on every startup.

--- modules/ui/unicode/packages.el
+++ modules/ui/unicode/packages.el
@@ -2,3 +2,8 @@
 ;;; ui/unicode/packages.el
 
 (package! unicode-fonts :pin "47f2397ade28eba621baa865fd69e4efb71407a5")
+(package! pcache
+  ;; Use a fork of `pcache', which has been patched to support Emacs 29 (the
+  ;; original has been abandoned).
+  :recipe (:host github :repo "LemonBreezes/pcache")
+  :pin "2004af4cf978ca8fcbeaa4da6b9b12600a202aeb")
