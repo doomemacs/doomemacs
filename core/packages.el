068966a4cd415ac9d9a9ceb6ffddf41d04f4d587
Henrik Lissner <git@henrik.io>
2022-07-29 12:31:44 +0200

    bump: :core
    Fuco1/smartparens@ec15aaa748b2 -> Fuco1/smartparens@8b6a3c3b31af
    bbatsov/projectile@4d6da873ae54 -> bbatsov/projectile@dc6e7ff65878
    domtronn/all-the-icons.el@ca1ef30004d3 -> domtronn/all-the-icons.el@b18db6be0a29
    jscheid/dtrt-indent@57f4072fa8ac -> jscheid/dtrt-indent@d4fd1b4977eb
    jwiegley/use-package@a7422fb8ab1b -> jwiegley/use-package@0ad5d9d5d8a6
    radian-software/straight.el@0e204d418d11 -> radian-software/straight.el@fed215348076

--- core/packages.el
+++ core/packages.el
@@ -17,15 +17,15 @@
             :branch ,straight-repository-branch
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "0e204d418d11e00006b83ff56ce7df03efc6046a")
+  :pin "fed215348076ba9182f634e0770a175220474b5a")
 
 ;; core-modules.el
 (package! use-package
   :type 'core
-  :pin "a7422fb8ab1baee19adb2717b5b47b9c3812a84c")
+  :pin "0ad5d9d5d8a61517a207ab04bf69e71c081149eb")
 
 ;; core-ui.el
-(package! all-the-icons :pin "ca1ef30004d3fb76aaa70bd47cb83e9fe017a7f7")
+(package! all-the-icons :pin "b18db6be0a290e8f91fd8d8340b2b44c8b75049a")
 (package! hide-mode-line :pin "bc5d293576c5e08c29e694078b96a5ed85631942")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "a32b39bdfe6c61c322c37226d66e1b6d4f107ed0")
@@ -33,10 +33,10 @@
 
 ;; core-editor.el
 (package! better-jumper :pin "47622213783ece37d5337dc28d33b530540fc319")
-(package! dtrt-indent :pin "57f4072fa8acd5f7af40b11f5f33607bca324fe1")
-(package! helpful :pin "209971ba9f576ba080352642cfbf25df5692b1d7")
+(package! dtrt-indent :pin "d4fd1b4977eb0d534844fddf01c3c51c70c57205")
+(package! helpful :pin "94a07d49a80f66f8ebc54a49a4b4f6899a65fbe3")
 (package! pcre2el :pin "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
-(package! smartparens :pin "ec15aaa748b21c9b8453dd95604ccc95fc1138cf")
+(package! smartparens :pin "8b6a3c3b31afd5b3f846e09859b5fc62eb06e7c1")
 (package! ws-butler
   ;; Use my fork of ws-butler, which has a few choice improvements and
   ;; optimizations (the original has been abandoned).
@@ -44,8 +44,8 @@
   :pin "572a10c11b6cb88293de48acbb59a059d36f9ba5")
 
 ;; core-projects.el
-(package! projectile :pin "4d6da873ae54dbf6043b015efd9b737e2ce152c6")
-(package! project :pin "c52ec9368928eb7ef321fd6af082832494c20a3b")
+(package! projectile :pin "dc6e7ff658789e4c6cf7c32a4b4fdf22ef9bc5a3")
+(package! project :pin "2e8da1b811d5213bd143d9d036bf7ea14741a1ae")
 
 ;; core-keybinds.el
 (package! general :pin "9651024e7f40a8ac5c3f31f8675d3ebe2b667344")
