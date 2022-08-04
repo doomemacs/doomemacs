1e9d8ab35e658b8b8a7b0c6c0d54304c800862e2
Henrik Lissner <git@henrik.io>
2022-08-04 17:43:36 +0200

    bump: :lang python
    cython/cython@fdea2d6bed82 -> cython/cython@8afd932c28d0
    emacs-lsp/lsp-pyright@3598bc7c47c2 -> emacs-lsp/lsp-pyright@c745228f39fd
    necaris/conda.el@9c28d7a853b4 -> necaris/conda.el@a65ed0084876
    pwalsh/pipenv.el@8f50c68d4153 -> pwalsh/pipenv.el@3af159749824
    pythonic-emacs/anaconda-mode@cbea0fb31823 -> pythonic-emacs/anaconda-mode@160e4e718588
    wbolster/emacs-python-pytest@ea53891a2196 -> wbolster/emacs-python-pytest@9bf8db38bf18
    
    - Updating conda.el fixes compatibility with Conda 4.13+.
    
    Fix: #6626
    Close: #6630
    Co-authored-by: sleepyeye <sleepyeye@users.noreply.github.com>

--- modules/lang/python/packages.el
+++ modules/lang/python/packages.el
@@ -4,7 +4,7 @@
 ;; Major modes
 (package! pip-requirements :pin "216cd1690f80cc965d4ae47b8753fc185f778ff6")
 (when (featurep! +cython)
-  (package! cython-mode :pin "fdea2d6bed8260a92fbabb43bec4c53996566dfe")
+  (package! cython-mode :pin "8afd932c28d08428d45bba03d6b642093e4c973b")
   (when (featurep! :checkers syntax)
     (package! flycheck-cython :pin "ecc4454d35ab5317ab66a04406f36f0c1dbc0b76")))
 
@@ -12,27 +12,27 @@
 (when (featurep! +lsp)
   (unless (featurep! :tools lsp +eglot)
     (if (featurep! +pyright)
-        (package! lsp-pyright :pin "3598bc7c47c2f2ff6fc94ca50b5a4e4db4c25f97")
-      (package! lsp-python-ms :pin "a56459216b3fdd99f7e3703ce0d20bb517b0222e"))))
+        (package! lsp-pyright :pin "c745228f39fdb35372b29b909f25fa6c98dc7c88")
+      (package! lsp-python-ms :pin "f8e7c4bcaefbc3fd96e1ca53d17589be0403b828"))))
 
 ;; Programming environment
-(package! anaconda-mode :pin "cbea0fb3182321d34ff93981c5a59f8dd72d82a5")
+(package! anaconda-mode :pin "160e4e7185881233d96da6722332bd6d038187a9")
 (when (featurep! :completion company)
   (package! company-anaconda :pin "da1566db41a68809ef7f91ebf2de28118067c89b"))
 
 ;; Environment management
-(package! pipenv :pin "8f50c68d415307a2cbc65cc4df20df18e1776e9b")
+(package! pipenv :pin "3af159749824c03f59176aff7f66ddd6a5785a10")
 (package! pyvenv :pin "31ea715f2164dd611e7fc77b26390ef3ca93509b")
 (when (featurep! +pyenv)
   (package! pyenv-mode :pin "b818901b8eac0e260ced66a6a5acabdbf6f5ba99"))
 (when (featurep! +conda)
-  (package! conda :pin "9c28d7a853b4b4bd00215cf7f07856c1563f2ad7"))
+  (package! conda :pin "a65ed0084876366fd1b238bb2b8b36ee75ac98d8"))
 (when (featurep! +poetry)
   (package! poetry :pin "5b9ef569d629d79820e73b5380e54e443ba90616"))
 
 ;; Testing frameworks
 (package! nose :pin "f8528297519eba911696c4e68fa88892de9a7b72")
-(package! python-pytest :pin "ea53891a219659d9339220d5db50a8c525f199af")
+(package! python-pytest :pin "9bf8db38bf18feb0484931877210cecfaa96bfc6")
 
 ;; Import managements
 (package! pyimport :pin "a6f63cf7ed93f0c0f7c207e6595813966f8852b9")
