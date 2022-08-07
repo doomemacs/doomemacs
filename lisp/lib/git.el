d92a81bf2dc2e36f2eb5975481f273af47bee909
Henrik Lissner <git@henrik.io>
2022-08-07 18:39:08 +0200

    feat(lib): add lib/git
    Where future git wrappers will live.

---
+++ lisp/lib/git.el
@@ -0,0 +1,11 @@
+;;; lisp/lib/git.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun doom-git-toplevel (&rest segments)
+  "Return the path to the current repo's root."
+  (cl-destructuring-bind (code . output)
+      (doom-call-process "git" "rev-parse" "--show-toplevel")
+    (if (zerop code)
+        (apply #'file-name-concat output segments)
+      ;; TODO throw stderr as error
+      (user-error "Not in a git repo: %s" default-directory))))
