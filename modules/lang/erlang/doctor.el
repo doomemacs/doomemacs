a9b4f6e21979683a107094c028a9f62a1fdde6e5
Ellis Kenyő <me@elken.dev>
2022-08-16 08:16:08 +0100

    feat(format): add :lang erlang formatter

--- modules/lang/erlang/doctor.el
+++ modules/lang/erlang/doctor.el
@@ -4,3 +4,7 @@
 (assert! (or (not (modulep! +lsp))
              (modulep! :tools lsp))
          "This module requires (:tools lsp)")
+
+(when (modulep! :editor format)
+  (unless (executable-find "efmt")
+    (warn! "Couldn't find efmt. Formatting will be disabled.")))
