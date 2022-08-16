53fe5df6b569ca098f9857446d11b1b3b071536f
Ellis Kenyő <me@elken.dev>
2022-08-16 08:18:29 +0100

    feat(format): add :lang csharp formatter

--- modules/lang/csharp/doctor.el
+++ modules/lang/csharp/doctor.el
@@ -9,3 +9,10 @@
 (assert! (or (not (modulep! +tree-sitter))
              (modulep! :tools tree-sitter))
          "This module requires (:tools tree-sitter)")
+
+(when (modulep! :editor format)
+  (unless (and (file-exists-p (expand-file-name "~/.dotnet/tools/dotnet-csharpier"))
+               (file-exists-p ".config/dotnet-tools.json")
+               (eq 0 (call-process-shell-command
+                      (format "grep -q 'dotnet-csharpier' %s" (expand-file-name ".config/dotnet-tools.json")) nil nil)))
+    (warn! "csharpier is not installed or setup as a local tool. Please see the module README. \nOtherwise, formatting will be disabled.")))
