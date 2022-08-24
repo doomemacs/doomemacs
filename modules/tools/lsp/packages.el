cc19d2841d8816e143174677c47a7859fd6a2dee
Jean-Pierre de Villiers <jeanpierre@jdvl.io>
2022-08-24 20:29:41 +0200

    bump: `lsp-mode`
    emacs-lsp/lsp-mode@3fa645c -> emacs-lsp/lsp-mode@b02bde6

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -6,7 +6,7 @@
       (package! eglot :pin "bd970be047aba4513c2970b9ff2d842f0472cb0e")
       (when (modulep! :completion vertico)
         (package! consult-eglot :pin "0da8801dd8435160ce1f62ad8066bd52e38f5cbd")))
-  (package! lsp-mode :pin "3fa645c0397b8f438f2db2dd288b899ba330d410")
+  (package! lsp-mode :pin "b02bde6587e1e621caa141b7176ca54e8a295c5b")
   (package! lsp-ui :pin "0a6368d38dc32e52abcbd52b63d1e557e42c66e6")
   (when (modulep! :completion ivy)
     (package! lsp-ivy :pin "3e87441a625d65ced5a208a0b0442d573596ffa3"))
