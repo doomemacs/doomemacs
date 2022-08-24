4c731a1d6b632d76aa1d215b5fd342bead5ae2cc
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-08-24 15:49:46 +0200

    bump: :tools debugger dap-mode realgud
    realgud/realgud@3c88611c4ed5 -> realgud/realgud@aff03aeef1e4
    emacs-lsp/dap-mode@50c2a99059da -> emacs-lsp/dap-mode@7f924d7feacb
    
    Several fixes, DAP servers updates, and support for VSCode tasks in
    dap-mode.

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/debugger/packages.el
 
-(when (package! realgud :pin "3c88611c4ed59069093187c2a039b8d05cbe53e8")
+(when (package! realgud :pin "aff03aeef1e40d2abb244240bab9787f4b3e6035")
   (when (modulep! :lang javascript)
     (package! realgud-trepan-ni :pin "0ec088ea343835e24ae73da09bea96bfb02a3130")))
 
 (when (modulep! +lsp)
-  (package! dap-mode :pin "50c2a99059da03ac5274d6794ea4758ddf253dc1")
+  (package! dap-mode :pin "7f924d7feacb2c271e5cde179f6fe1c390a78223")
   (package! posframe :pin "0d23bc5f7cfac00277d83ae7ba52c48685bcbc68"))
