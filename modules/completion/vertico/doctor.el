9e6d6a512abce736043c20117ee14256adc667b8
Dusk Banks <me@bb010g.com>
2022-09-05 02:27:03 -0700

    fix(vertico): erroring when grep or rg not found
    Additionally, warn when grep or ripgrep aren't found.

--- modules/completion/vertico/doctor.el
+++ modules/completion/vertico/doctor.el
@@ -2,10 +2,14 @@
 
 (require 'consult)
 
-(unless (consult--grep-lookahead-p "grep" "-P")
-  (warn! "The installed grep binary was not built with support for PCRE lookaheads.
-  Some advanced consult filtering features will not work as a result, see the module readme."))
+(if (executable-find "grep")
+  (unless (consult--grep-lookahead-p "grep" "-P")
+    (warn! "The installed grep binary was not built with support for PCRE lookaheads.
+    Some advanced consult filtering features will not work as a result, see the module readme."))
+  (warn! "Couldn't find grep."))
 
-(unless (consult--grep-lookahead-p "rg" "-P")
-  (warn! "The installed ripgrep binary was not built with support for PCRE lookaheads.
-  Some advanced consult filtering features will not work as a result, see the module readme."))
+(if (executable-find "rg")
+  (unless (consult--grep-lookahead-p "rg" "-P")
+    (warn! "The installed ripgrep binary was not built with support for PCRE lookaheads.
+    Some advanced consult filtering features will not work as a result, see the module readme."))
+  (warn! "Couldn't find ripgrep."))
