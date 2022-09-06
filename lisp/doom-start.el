7e6532928902f8b46e286997c00ad5f34992a5ed
Henrik Lissner <git@henrik.io>
2022-09-06 20:01:50 +0200

    refactor: move gcmh config to doom-start
    Configuration for interactive sessions ought to live in doom-start.
    
    Ref: 3ce4b38c3d92

--- lisp/doom-start.el
+++ lisp/doom-start.el
@@ -206,6 +206,14 @@ If RETURN-P, return the message as a string instead of displaying it."
 (doom-run-hook-on 'doom-first-file-hook   '(find-file-hook dired-initial-position-hook))
 (doom-run-hook-on 'doom-first-input-hook  '(pre-command-hook))
 
+;; The GC introduces annoying pauses and stuttering into our Emacs experience,
+;; so we use `gcmh' to stave off the GC while we're using Emacs, and provoke it
+;; when it's idle. However, if the idle delay is too long, we run the risk of
+;; runaway memory usage in busy sessions. If it's too low, then we may as well
+;; not be using gcmh at all.
+(setq gcmh-idle-delay 'auto  ; default is 15s
+      gcmh-auto-idle-delay-factor 10
+      gcmh-high-cons-threshold (* 16 1024 1024))  ; 16mb
 (add-hook 'doom-first-buffer-hook #'gcmh-mode)
 
 ;; There's a chance the user will later use package.el or straight in this
--- lisp/doom.el
+++ lisp/doom.el
@@ -396,15 +396,6 @@ Otherwise, `en/disable-command' (in novice.el.gz) is hardcoded to write them to
 ;; fonts that are larger than the system default (which would resize the frame).
 (setq frame-inhibit-implied-resize t)
 
-;; The GC introduces annoying pauses and stuttering into our Emacs experience,
-;; so we use `gcmh' to stave off the GC while we're using Emacs, and provoke it
-;; when it's idle. However, if the idle delay is too long, we run the risk of
-;; runaway memory usage in busy sessions. If it's too low, then we may as well
-;; not be using gcmh at all.
-(setq gcmh-idle-delay 'auto  ; default is 15s
-      gcmh-auto-idle-delay-factor 10
-      gcmh-high-cons-threshold (* 16 1024 1024))  ; 16mb
-
 ;; Emacs "updates" its ui more often than it needs to, so slow it down slightly
 (setq idle-update-delay 1.0)  ; default is 0.5
 
