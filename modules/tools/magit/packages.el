ea84212d0a03f03be34a4c601d8d3a41e768fa64
Henrik Lissner <git@henrik.io>
2022-09-07 00:52:49 +0200

    fix(magit): void-function (:files) error
    Due to a missed syntax error in d8f87ad.
    
    Amend: d8f87ada96e9

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -5,7 +5,8 @@
   (package! compat :pin "cc1924fd8b3f9b75b26bf93f084ea938c06f9615")
   (when (modulep! +forge)
     (package! forge :pin "36208c43bf41782cfe81fccc904f8adbe57818e1")
-    (package! code-review :pin "d38fbe59304ed31c759ce733cda16f69a8ef2d8c")
-      :recipe (:files ("graphql" "code-review*.el"))))
+    (package! code-review
+      :recipe (:files ("graphql" "code-review*.el"))
+      :pin "d38fbe59304ed31c759ce733cda16f69a8ef2d8c"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
   (package! magit-todos :pin "67fd80c2f10aec4d5b2a24b5d3d53c08cc1f05dc")
