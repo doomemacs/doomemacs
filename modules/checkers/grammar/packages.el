149853b97283aeb6835d9a77d71f662e60beb869
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-09-08 13:00:07 +0200

    fix(grammar): add `github-tags` dependency

--- modules/checkers/grammar/packages.el
+++ modules/checkers/grammar/packages.el
@@ -3,7 +3,12 @@
 
 (when (and (modulep! +lsp)
            (not (modulep! :tools lsp +eglot)))
-  (package! lsp-ltex :pin "18b0e8608408f9e913d89075e78c2b4e3f69cf1c"))
+  (package! lsp-ltex :pin "18b0e8608408f9e913d89075e78c2b4e3f69cf1c")
+  ;; Optional dependency of lsp-ltex, needed for installing/updating ltex-ls LSP server
+  (package! github-tags
+    :recipe (:host github
+             :repo "jcs-elpa/github-tags")
+    :pin "7b02d6e883ac3d766106de30c60b22609c4515f9"))
 (unless (modulep! +lsp)
   (package! langtool :pin "8276eccc5587bc12fd205ee58a7a982f0a136e41"))
 (package! writegood-mode :pin "ed42d918d98826ad88928b7af9f2597502afc6b0")
