0f769dd89e4053f97c5a83776f9f2651a1a2d426
TEC <git@tecosaur.net>
2022-09-17 16:42:30 +0800

    fix(literate): use same org install when using CLI

--- modules/config/literate/cli.el
+++ modules/config/literate/cli.el
@@ -2,5 +2,18 @@
 
 (load! "autoload")
 
+(defun +literate-add-installed-org-to-load-path-h ()
+  "Use the straight-installed, not bundled Org."
+  (let ((straight-org-build-dir
+         (doom-path straight-base-dir "straight" straight-build-dir "org"))
+        (straight-org-repo-dir
+         (doom-path straight-base-dir "straight" "repos" "org")))
+    (cond
+     ((file-exists-p straight-org-build-dir)
+      (add-to-list 'load-path straight-org-build-dir))
+     ((file-exists-p straight-org-repo-dir)
+      (add-to-list 'load-path straight-org-repo-dir)))))
+
 ;; Tangle the user's config.org before 'doom sync' runs
 (add-hook 'doom-before-sync-hook #'+literate-tangle-h)
+(add-hook 'doom-before-sync-hook #'+literate-add-installed-org-to-load-path-h)
