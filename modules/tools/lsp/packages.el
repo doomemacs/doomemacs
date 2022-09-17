87f85ab459f1610d4f62155277996737228c57f7
Henrik Lissner <git@henrik.io>
2022-09-18 00:23:31 +0200

    bump: :tools debugger lsp
    emacs-lsp/dap-mode@50c2a99059da -> emacs-lsp/dap-mode@5d5043f962de
    emacs-lsp/lsp-ivy@3e87441a625d -> emacs-lsp/lsp-ivy@9ecf4dd9b120
    emacs-lsp/lsp-mode@3fa645c0397b -> emacs-lsp/lsp-mode@68bdac0f8067
    joaotavora/eglot@bd970be047ab -> joaotavora/eglot@aeea7c719a05
    realgud/realgud@3c88611c4ed5 -> realgud/realgud@aff03aeef1e4
    
    Close: #6721

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -3,13 +3,13 @@
 
 (if (modulep! +eglot)
     (progn
-      (package! eglot :pin "bd970be047aba4513c2970b9ff2d842f0472cb0e")
+      (package! eglot :pin "aeea7c719a05f729af2df2f0dc6cbd4337df140f")
       (when (modulep! :completion vertico)
         (package! consult-eglot :pin "0da8801dd8435160ce1f62ad8066bd52e38f5cbd")))
-  (package! lsp-mode :pin "3fa645c0397b8f438f2db2dd288b899ba330d410")
-  (package! lsp-ui :pin "0a6368d38dc32e52abcbd52b63d1e557e42c66e6")
+  (package! lsp-mode :pin "68bdac0f806789c064856a47d0e413b65a260e1e")
+  (package! lsp-ui :pin "d8cce7dc154aa3216c080dd5c6fb827bdba9a312")
   (when (modulep! :completion ivy)
-    (package! lsp-ivy :pin "3e87441a625d65ced5a208a0b0442d573596ffa3"))
+    (package! lsp-ivy :pin "9ecf4dd9b1207109802bd1882aa621eb1c385106"))
   (when (modulep! :completion helm)
     (package! helm-lsp :pin "c2c6974dadfac459b1a69a1217441283874cea92"))
   (when (modulep! :completion vertico)
