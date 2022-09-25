c5e89cef78c5f886707f005bcebe8f29c6d6d7eb
TEC <git@tecosaur.net>
2022-09-26 02:39:26 +0800

    tweak(org): have doom links use path for info

--- modules/lang/org/autoload/org-link.el
+++ modules/lang/org/autoload/org-link.el
@@ -77,36 +77,36 @@ exist, and `org-link' otherwise."
                                     keystr t t)))
   keystr)
 
-(defun +org-link--read-module-link (link)
+(defun +org-link--read-module-spec (module-spec-str)
   (cl-destructuring-bind (category &optional module flag)
-      (let ((desc (+org-link-read-desc-at-point link)))
-        (if (string-prefix-p "+" (string-trim-left desc))
-            (list nil nil (intern desc))
-          (mapcar #'intern (split-string desc " " nil))))
+      (if (string-prefix-p "+" (string-trim-left module-spec-str))
+          (list nil nil (intern module-spec-str))
+        (mapcar #'intern (split-string module-spec-str " " nil)))
     (list :category category
           :module module
           :flag flag)))
 
 ;;;###autoload
-(defun +org-link--doom-module-link-face-fn (link)
+(defun +org-link--doom-module-link-face-fn (module-path)
   (cl-destructuring-bind (&key category module flag)
-      (+org-link--read-module-link link)
+      (+org-link--read-module-spec module-path)
     (if (doom-module-locate-path category module)
         `(:inherit org-priority
           :weight bold)
       'error)))
+
 ;;;###autoload
-(defun +org-link-follow-doom-module-fn (link)
+(defun +org-link-follow-doom-module-fn (module-path _prefixarg)
   "TODO"
   (cl-destructuring-bind (&key category module flag)
-      (+org-link--read-module-link link)
+      (+org-link--read-module-spec module-path)
     (when category
       (let ((doom-modules-dirs (list doom-modules-dir)))
         (if-let* ((path (doom-module-locate-path category module))
                   (path (or (car (doom-glob path "README.org"))
                             path)))
             (find-file path)
-          (user-error "Can't find Doom module '%s'" link))))
+          (user-error "Can't find Doom module '%s'" module-path))))
     (when flag
       (goto-char (point-min))
       (when (and (re-search-forward "^\\*+ \\(?:TODO \\)?Module flags")
@@ -118,11 +118,9 @@ exist, and `org-link' otherwise."
         (recenter)))))
 
 ;;;###autoload
-(defun +org-link-follow-doom-package-fn (link)
+(defun +org-link-follow-doom-package-fn (pkg _prefixarg)
   "TODO"
-  (doom/describe-package
-   (intern-soft
-    (+org-link-read-desc-at-point link))))
+  (doom/describe-package (intern-soft pkg)))
 
 
 ;;
