0aeb25e3e0283a130e908a605a151f7c3efa7c75
Ben Lovell <ben.j.lovell@gmail.com>
2022-09-27 16:16:06 +0200

    bump: :lang clojure
    clojure-emacs/clojure-mode@b6f41d74904daa9312648f3a7bea7a72fd8e140b -> clojure-emacs/clojure-mode@d47298212ffc486ade3f2428f103feba3a467af0
    clojure-emacs/clj-refactor.el@f368c56c83843396b160440f472a661a3b639862 -> clojure-emacs/clj-refactor.el@cd97626fd952f824564808a2f21b40bfcb59ff0f
    clojure-emacs/cider@b9e1cc26e2afda003a4b6c1f2a26e04f1c45f3d0 -> clojure-emacs/cider@700ded1d6c245e01fca7f364db1561ad1e1bb595
    
    versions:
    emacs-cider 1.4.1->1.5.0
    clojure-mode 5.15.0->5.15.x
    clj-refactor 3.5.3->3.5.6

--- modules/lang/clojure/packages.el
+++ modules/lang/clojure/packages.el
@@ -14,8 +14,8 @@
 (package! parseedn :pin "a09686fbb9113b8b1b4f20c9e1dc0d6fea01a64f")
 
 ;;; Core packages
-(package! clojure-mode :pin "b6f41d74904daa9312648f3a7bea7a72fd8e140b")
-(package! clj-refactor :pin "f368c56c83843396b160440f472a661a3b639862")
-(package! cider :pin "b9e1cc26e2afda003a4b6c1f2a26e04f1c45f3d0")
+(package! clojure-mode :pin "d47298212ffc486ade3f2428f103feba3a467af0")
+(package! clj-refactor :pin "cd97626fd952f824564808a2f21b40bfcb59ff0f")
+(package! cider :pin "700ded1d6c245e01fca7f364db1561ad1e1bb595")
 (when (modulep! :checkers syntax)
   (package! flycheck-clj-kondo :pin "ff7bed2315755cfe02ef471edf522e27b78cd5ca"))
