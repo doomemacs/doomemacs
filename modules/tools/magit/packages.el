c9cc3f8d36b02052c5bf998996aac44588e70931
Henrik Lissner <git@henrik.io>
2022-10-22 22:06:23 +0200

    bump: :tools magit
    alphapapa/magit-todos@67fd80c2f10a -> alphapapa/magit-todos@c5030cc27c7c
    magit/forge@36208c43bf41 -> magit/forge@ce212f8f9583
    magit/magit@c1fb53d3de63 -> magit/magit@0ef98ef51811
    wandersoncferreira/code-review@d38fbe59304e -> wandersoncferreira/code-review@95b36ec8e793
    
    Fix: #6875
    Fix: #6509

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "c1fb53d3de6390961ccd8dfb1cc135383508d0fc")
+(when (package! magit :pin "0ef98ef51811807952a4c3c677cbf3dfb269de2e")
   (package! compat :pin "cc1924fd8b3f9b75b26bf93f084ea938c06f9615")
   (when (modulep! +forge)
-    (package! forge :pin "36208c43bf41782cfe81fccc904f8adbe57818e1")
+    (package! forge :pin "ce212f8f95838889c51d0327eb8c3979bec6665c")
     (package! code-review
       :recipe (:files ("graphql" "code-review*.el"))
-      :pin "d38fbe59304ed31c759ce733cda16f69a8ef2d8c"))
+      :pin "95b36ec8e7935f96b2f3c6c9086d49cf4a72cbff"))
   (package! magit-gitflow :pin "cc41b561ec6eea947fe9a176349fb4f771ed865b")
-  (package! magit-todos :pin "67fd80c2f10aec4d5b2a24b5d3d53c08cc1f05dc"))
+  (package! magit-todos :pin "c5030cc27c7c1a48db52b0134bf2648a59a43176"))
