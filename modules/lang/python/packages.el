1961003d00f086c69696c43801e5a8437b60f221
Henrik Lissner <git@henrik.io>
2022-10-23 02:36:20 +0200

    bump: :lang python
    Wilfred/pyimport@a6f63cf7ed93 -> Wilfred/pyimport@c006a5fd0e5c
    cybniv/poetry.el@5b9ef569d629 -> cybniv/poetry.el@5a8575bc78d7
    cython/cython@8afd932c28d0 -> cython/cython@b2015596b38d
    emacs-lsp/lsp-pyright@c745228f39fd -> emacs-lsp/lsp-pyright@2fa2c8976599
    necaris/conda.el@a65ed0084876 -> necaris/conda.el@cb9544e8f7d6
    pythonic-emacs/anaconda-mode@160e4e718588 -> pythonic-emacs/anaconda-mode@ca8edbaa7662
    wbolster/emacs-python-pytest@9bf8db38bf18 -> wbolster/emacs-python-pytest@33c921adaa6c
    
    Ref: Wilfred/pyimport#12
    Close: #6779
    Co-authored-by: corrafig <corrafig@users.noreply.github.com>

--- modules/lang/python/packages.el
+++ modules/lang/python/packages.el
@@ -4,7 +4,7 @@
 ;; Major modes
 (package! pip-requirements :pin "216cd1690f80cc965d4ae47b8753fc185f778ff6")
 (when (modulep! +cython)
-  (package! cython-mode :pin "8afd932c28d08428d45bba03d6b642093e4c973b")
+  (package! cython-mode :pin "b2015596b38df4b7b2978d87385da20d69e4e822")
   (when (modulep! :checkers syntax)
     (package! flycheck-cython :pin "ecc4454d35ab5317ab66a04406f36f0c1dbc0b76")))
 
@@ -12,11 +12,11 @@
 (when (modulep! +lsp)
   (unless (modulep! :tools lsp +eglot)
     (if (modulep! +pyright)
-        (package! lsp-pyright :pin "c745228f39fdb35372b29b909f25fa6c98dc7c88")
+        (package! lsp-pyright :pin "2fa2c897659909ba9804baba72a108578d007677")
       (package! lsp-python-ms :pin "f8e7c4bcaefbc3fd96e1ca53d17589be0403b828"))))
 
 ;; Programming environment
-(package! anaconda-mode :pin "160e4e7185881233d96da6722332bd6d038187a9")
+(package! anaconda-mode :pin "ca8edbaa7662d97e4a4416ec9a8d743863303911")
 (when (modulep! :completion company)
   (package! company-anaconda :pin "da1566db41a68809ef7f91ebf2de28118067c89b"))
 
@@ -26,14 +26,14 @@
 (when (modulep! +pyenv)
   (package! pyenv-mode :pin "b818901b8eac0e260ced66a6a5acabdbf6f5ba99"))
 (when (modulep! +conda)
-  (package! conda :pin "a65ed0084876366fd1b238bb2b8b36ee75ac98d8"))
+  (package! conda :pin "cb9544e8f7d6c642b7fd8c1ecc2cf4fc7370c352"))
 (when (modulep! +poetry)
-  (package! poetry :pin "5b9ef569d629d79820e73b5380e54e443ba90616"))
+  (package! poetry :pin "5a8575bc78d7fc5a39b2ba4665a909e36961fceb"))
 
 ;; Testing frameworks
 (package! nose :pin "f8528297519eba911696c4e68fa88892de9a7b72")
-(package! python-pytest :pin "9bf8db38bf18feb0484931877210cecfaa96bfc6")
+(package! python-pytest :pin "33c921adaa6c9c8f7cceba2342114c6b406e0d7c")
 
 ;; Import managements
-(package! pyimport :pin "a6f63cf7ed93f0c0f7c207e6595813966f8852b9")
+(package! pyimport :pin "c006a5fd0e5c9e297aa2ad71b2f02f463286b5e3")
 (package! py-isort :pin "e67306f459c47c53a65604e4eea88a3914596560")
