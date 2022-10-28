068bb4ed546994a1c524ef61b53615f00a42169f
Vincent Bernat <vincent@bernat.ch>
2022-10-28 23:03:37 +0200

    bump: :ui modeline
    seagle0128/doom-modeline@bed80b82d7bf -> seagle0128/doom-modeline@b66d5e5006df
    
    - Fixes error with eglot 1.9 (due to rename of eglot--major-mode)

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (modulep! +light)
-  (package! doom-modeline :pin "bed80b82d7bf4bd85904d9b093ab690b7bcbac1c")
+  (package! doom-modeline :pin "b66d5e5006df4cedb1119da3d83fd6c08965b830")
   (package! compat :pin "cc1924fd8b3f9b75b26bf93f084ea938c06f9615"))
 (package! anzu :pin "5abb37455ea44fa401d5f4c1bdc58adb2448db67")
 (when (modulep! :editor evil)
