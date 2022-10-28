3d8f9ae5ea90aad27d3f40ff701bc2f368391420
Henrik Lissner <git@henrik.io>
2022-10-28 23:22:52 +0200

    fix(rest): gnutls-verify-error: defining as dynamic error
    An error results if you let-bind a dynamic variable before it's defined,
    so I forcibly ensure gnutls-verify-error is defined.
    
    Fix: #6073
    Close: #6525

--- modules/lang/rest/config.el
+++ modules/lang/rest/config.el
@@ -14,7 +14,8 @@
     "Forces underlying SSL verification to prompt for self-signed or invalid
 certs, rather than reject them silently."
     :around #'restclient-http-do
-    (let (gnutls-verify-error tls-checktrust)
+    (require 'gnutls)
+    (let (gnutls-verify-error)
       (apply fn args)))
 
   (map! :map restclient-mode-map
