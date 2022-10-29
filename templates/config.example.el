834e21b89b2e844877642f1aec2cbb966175f4e4
Vincent Bernat <vincent@bernat.ch>
2022-10-29 18:41:41 +0200

    docs: use floating point for font sizes
    As mentioned in #6131, when using a non-standard DPI, setting `:size` as
    an integer will be converted to pixel size and it does not account for
    DPI settings. Instead, when using a float, this is converted to point
    size and DPI is respected.
    
    Ref: #6131

--- templates/config.example.el
+++ templates/config.example.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;(setq doom-font (font-spec :family "Fira Code" :size 12.0 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13.0))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
