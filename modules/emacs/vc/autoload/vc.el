4e3b94c60262fdabec15991d1befbc5dceb6ba3c
Henrik Lissner <git@henrik.io>
2022-10-31 18:36:18 +0100

    tweak(vc): add feedback on +vc/browse-at-remote-kill
    Otherwise there's no indication that the command worked, when invoked
    interactively.

--- modules/emacs/vc/autoload/vc.el
+++ modules/emacs/vc/autoload/vc.el
@@ -21,17 +21,18 @@ If prefix ARG, negate the default value of `browse-at-remote-prefer-symbolic'."
     (browse-at-remote)))
 
 ;;;###autoload
-(defun +vc/browse-at-remote-kill (&optional arg)
+(defun +vc/browse-at-remote-kill (&optional arg interactive?)
   "Copy URL to current file (and line if selection is active) to clipboard.
 If prefix ARG, negate the default value of `browse-at-remote-prefer-symbolic'."
-  (interactive "P")
+  (interactive (list current-prefix-arg 'interactive))
   (require 'browse-at-remote)
   (let ((vc-ignore-dir-regexp locate-dominating-stop-dir-regexp)
         (browse-at-remote-prefer-symbolic
          (if arg
              (not browse-at-remote-prefer-symbolic)
            browse-at-remote-prefer-symbolic)))
-    (browse-at-remote-kill)))
+    (browse-at-remote-kill)
+    (if interactive? (message "Copied to clipboard"))))
 
 ;;;###autoload
 (defun +vc/browse-at-remote-homepage ()
