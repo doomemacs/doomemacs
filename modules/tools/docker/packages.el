380674e7b699de6d531cfad8b6d207b2e50c9f4b
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2022-12-08 10:14:26 +0100

    fix(docker): load docker-tramp conditionnally
    Trying to use `docker-tramp` in Emacs in version 29 or more trigger a warning about `docker-tramp` being deprecated in favour of `tramp-container`, stopping the loading of modules altogether. This leads notably to `:config default` not being loaded, so no binding is available

--- modules/tools/docker/packages.el
+++ modules/tools/docker/packages.el
@@ -2,5 +2,6 @@
 ;;; tools/docker/packages.el
 
 (package! docker :pin "44f0bbec9b3deb027d17f4c10d8ec4106ed89dfb")
-(package! docker-tramp :pin "930d7b46c180d8a13240a028c1b40af84f2a3219")
+(unless EMACS29+
+  (package! docker-tramp :pin "930d7b46c180d8a13240a028c1b40af84f2a3219"))
 (package! dockerfile-mode :pin "b63a3d12b7dea0cb9efc7f78d7ad5672ceab2a3f")
