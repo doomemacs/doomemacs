c7d4de1b50b9aa2d83ef052aa85bd6c8d3773866
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-02-06 15:48:12 +0100

    bump: :lang haskell lsp-haskell
    emacs-lsp/lsp-mode@a3b3c15359405f442fc51a2db09e503ca3b39f3d -> emacs-lsp/lsp-mode@06c28ca780dedea32e150462f1b9aa9c910c2952442fc51a2db09e503ca3b39f3d
    
    Gives us latest updates and solved laggy LSP sessions for me.

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -6,7 +6,7 @@
       (package! eglot :pin "e501275e06952889056268dabe08ccd0dbaf23e5")
       (when (modulep! :completion vertico)
         (package! consult-eglot :pin "0da8801dd8435160ce1f62ad8066bd52e38f5cbd")))
-  (package! lsp-mode :pin "a3b3c15359405f442fc51a2db09e503ca3b39f3d")
+  (package! lsp-mode :pin "06c28ca780dedea32e150462f1b9aa9c910c2952442fc51a2db09e503ca3b39f3d")
   (package! lsp-ui :pin "3cd7cc61273341023b863dcf45906ac9142fd1aa")
   (when (modulep! :completion ivy)
     (package! lsp-ivy :pin "9ecf4dd9b1207109802bd1882aa621eb1c385106"))
