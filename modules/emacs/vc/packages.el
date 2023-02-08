2c3930d9dd43e7338b1f197e70b7bab4174c8928
Itai Y. Efrat <itai3397@gmail.com>
2023-02-09 01:42:21 +0200

    bump: :completion vertico compat consult-lsp magit git-commit
    emacs-straight/compat@7ca7d300d1d2 -> emacs-straight/compat@2bedcb5ea919
    mhayashi1120/Emacs-wgrep@f9687c28bbc2 -> mhayashi1120/Emacs-wgrep@edf768732a56
    minad/consult-flycheck@7a10be316d72 -> minad/consult-flycheck@51b1b48e8dad
    minad/consult@16b2dc5e34c8 -> oantolin/orderless@ae849b3d9f8c
    minad/marginalia@c1365bf0c7b5 -> minad/marginalia@6d48ed54be87
    minad/vertico@bedd146c3ffc -> minad/vertico@f303790546ed
    oantolin/embark@629cce948c56 -> oantolin/embark@4882b395cef9
    oantolin/orderless@847694e78c12 -> minad/vertico@f303790546ed
    tumashu/vertico-posframe@a3d0802d7b4a -> tumashu/vertico-posframe@790f74b49d53
    gagbo/consult-lsp@58b541476203 -> gagbo/consult-lsp@f8db3252c0da
    magit/magit@0ef98ef51811 -> magit/magit@6d325d90ba13
    
    consult-lsp needed to be bumped to work with some changed consult
    internals, and due to compat shenanigans we have to bump magit to latest
    as well.
    
    Includes fixes to stay up to date with upstream api changes to.
    consult-customize and the deprecation of embark-define-keymap
    
    Close: #7064

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -6,7 +6,7 @@
 (package! smerge-mode :built-in t)
 
 (package! browse-at-remote :pin "cef26f2c063f2473af42d0e126c8613fe2f709e4")
-(package! git-commit :pin "8a0cc83eff98489d3685b8585afdcebbb47c1393")
+(package! git-commit :pin "6d325d90ba1374d48c4c7088f96864b678155f48")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -3,7 +3,7 @@
 
 (unless (modulep! +light)
   (package! doom-modeline :pin "b66d5e5006df4cedb1119da3d83fd6c08965b830")
-  (package! compat :pin "7ca7d300d1d256f674f83932d2918d8e70cd28f6"))
+  (package! compat :pin "2bedcb5ea91914e75d4905bc53e537b33f8f51e9"))
 (package! anzu :pin "5abb37455ea44fa401d5f4c1bdc58adb2448db67")
 (when (modulep! :editor evil)
   (package! evil-anzu :pin "d1e98ee6976437164627542909a25c6946497899"))
