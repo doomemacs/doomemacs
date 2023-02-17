3a4afcef430676eb1f033e76db86ac69771aec2a
Henrik Lissner <git@henrik.io>
2023-02-17 12:24:23 -0500

    bump: :lang nix
    NixOS/nix-mode@20ee8d88900b -> NixOS/nix-mode@c18a24e9ac56
    jwiegley/nix-update-el@fc6c39c2da3f -> jwiegley/nix-update-el@aab70a381655
    
    Close: #7059
    Co-authored-by: thomasjm <thomasjm@users.noreply.github.com>

--- modules/lang/nix/packages.el
+++ modules/lang/nix/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/nix/packages.el
 
-(package! nix-mode :pin "20ee8d88900b169831d6b0783bd82d2625e940c7")
-(package! nix-update :pin "fc6c39c2da3fcfa62f4796816c084a6389c8b6e7")
+(package! nix-mode :pin "c18a24e9ac569a221e88ba9d74d52c7b02b6eb77")
+(package! nix-update :pin "aab70a38165575a9cb41726f1cc67df60fbf2832")
 
 (when (modulep! :completion company)
   (package! company-nixos-options :pin "053a2d5110ce05b7f99bcc2ac4804b70cbe87916"))
