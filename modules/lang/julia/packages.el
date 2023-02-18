7983f8e17d7c491a48298ba3499cf9f2c57879e5
Henrik Lissner <git@henrik.io>
2023-02-18 14:01:24 -0500

    bump: :lang julia
    JuliaEditorSupport/julia-emacs@47f43f7d8390 -> JuliaEditorSupport/julia-emacs@7aafa8e77df6
    tpapp/julia-repl@6c1d63511fb2 -> tpapp/julia-repl@57a15dfafed6
    non-Jedi/eglot-jl@2e35cf9768d9 -> non-Jedi/eglot-jl@2e0459722355
    
    - (#7016) Fixes the "Failed to find command: julia-repl--show" error.
    
    Close: #7016
    Fix: wentasah/EmacsVterm.jl#3

--- modules/lang/julia/packages.el
+++ modules/lang/julia/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/julia/packages.el
 
-(package! julia-mode :pin "47f43f7d839019cac3ba6559d93b29487ca118cb")
-(package! julia-repl :pin "6c1d63511fb2b3b3f2e342eff6a375d78be6c12c")
+(package! julia-mode :pin "7aafa8e77df64a47fa4729a0c1ea572b5bc8e30e")
+(package! julia-repl :pin "57a15dfafed680ad7d81f779d414e8cb6717417c")
 
 (when (modulep! +lsp)
   (if (modulep! :tools lsp +eglot)
-      (package! eglot-jl :pin "2e35cf9768d97a0429a72deddbe30d6d7722d454")
+      (package! eglot-jl :pin "2e04597223553a369dd5b6520b6365b41e6ea508")
     (package! lsp-julia :pin "d6688bb131ff4a5a0201f6d3826ef0b018265389")))
