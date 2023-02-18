4513efe15932e7c4b5c6e2c2e8a4978786952f98
Henrik Lissner <git@henrik.io>
2023-02-18 15:18:26 -0500

    bump: :core
    Fuco1/smartparens@8b6a3c3b31af -> Fuco1/smartparens@7afd64739501
    Wilfred/helpful@6633d82c6e3c -> Wilfred/helpful@94c25337b2de
    Wilfred/helpful@6633d82c6e3c -> Wilfred/helpful@94c25337b2de
    bbatsov/projectile@20aa2adccc1e -> bbatsov/projectile@e18ad4d6111e
    domtronn/all-the-icons.el@4a4d6269b8b8 -> domtronn/all-the-icons.el@51bf77da1ebc
    emacs-straight/project@2e8da1b811d5 -> emacs-straight/project@e086bdc7c515
    joddie/pcre2el@0b5b2a2c173a -> joddie/pcre2el@38c6f80c787d
    radian-software/straight.el@3eca39dfc679 -> radian-software/straight.el@56a8b6c8a46f

--- lisp/packages.el
+++ lisp/packages.el
@@ -17,10 +17,10 @@
             :branch ,straight-repository-branch
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "3eca39dfc6797243ec7d1c6a7d45142407f73f88")
+  :pin "56a8b6c8a46fb78655d512402ad7840d85167225")
 
 ;; doom-ui.el
-(package! all-the-icons :pin "4a4d6269b8b85b0b15954f063e6ce378630d80c0")
+(package! all-the-icons :pin "51bf77da1ebc3c199dfc11f54c0dce67559f5f40")
 (package! hide-mode-line :pin "bc5d293576c5e08c29e694078b96a5ed85631942")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "a32b39bdfe6c61c322c37226d66e1b6d4f107ed0")
@@ -29,9 +29,9 @@
 ;; doom-editor.el
 (package! better-jumper :pin "47622213783ece37d5337dc28d33b530540fc319")
 (package! dtrt-indent :pin "d4fd1b4977eb0d534844fddf01c3c51c70c57205")
-(package! helpful :pin "6633d82c6e3c921c486ec284cb6542f33278b605")
-(package! pcre2el :pin "0b5b2a2c173aab3fd14aac6cf5e90ad3bf58fa7d")
-(package! smartparens :pin "8b6a3c3b31afd5b3f846e09859b5fc62eb06e7c1")
+(package! helpful :pin "94c25337b2de2f9da60914a7c0c6cca9584c0231")
+(package! pcre2el :pin "38c6f80c787da547287db96b495e5b695ca0b4b8")
+(package! smartparens :pin "7afd647395018a26633673ed92ce7a9cb3ccb8f2")
 (package! ws-butler
   ;; Use my fork of ws-butler, which has a few choice improvements and
   ;; optimizations (the original has been abandoned).
@@ -39,8 +39,8 @@
   :pin "572a10c11b6cb88293de48acbb59a059d36f9ba5")
 
 ;; doom-projects.el
-(package! projectile :pin "20aa2adccc1ecb19f54f2fbc428b4904071fcab8")
-(package! project :pin "2e8da1b811d5213bd143d9d036bf7ea14741a1ae")
+(package! projectile :pin "e18ad4d6111eb9e975ccce028baf5e4bb786bfcf")
+(package! project :pin "e086bdc7c515f8bfc22598d473ba888ba7683f0c")
 
 ;; doom-keybinds.el
 (package! general :pin "9651024e7f40a8ac5c3f31f8675d3ebe2b667344")
