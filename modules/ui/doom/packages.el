1451c552a07471e1a667cbb19995e1e7ccc1377c
Ellis Kenyo <me@elken.dev>
2023-02-20 15:09:31 +0000

    bump: :ui doom
    doomemacs/themes@535800fd6ca7 -> doomemacs/themes@b5ff201f4bea
    
    Fixes https://github.com/doomemacs/themes/issues/776

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "535800fd6ca7f5af56f7aa3d0e8f46fef8b7999b")
+(package! doom-themes :pin "b5ff201f4bea4286e9ed015a2043cf2394182232")
 (package! solaire-mode :pin "8af65fbdc50b25ed3214da949b8a484527c7cc14")
