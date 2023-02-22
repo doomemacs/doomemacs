ff2ce04ccf1a08434493a762e8ea0b1c680aaa06
Andrej Novikov <jazzaiman@gmail.com>
2023-02-22 17:12:43 +0200

    bump: :tools code-review

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -6,5 +6,5 @@
     (package! forge :pin "ce212f8f95838889c51d0327eb8c3979bec6665c")
     (package! code-review
       :recipe (:files ("graphql" "code-review*.el"))
-      :pin "95b36ec8e7935f96b2f3c6c9086d49cf4a72cbff"))
+      :pin "26f426e99221a1f9356aabf874513e9105b68140"))
   (package! magit-todos :pin "c5030cc27c7c1a48db52b0134bf2648a59a43176"))
