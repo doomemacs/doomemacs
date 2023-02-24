fe2cb5e70532ab42906f22ba611ac8578ee2ff26
Henrik Lissner <git@henrik.io>
2023-02-24 18:02:02 -0500

    fix(evil): premature "too early" warnings
    A warning gets logged if a lazily-loaded evil-collection package is
    loaded too early at startup, but the goal posts were in the wrong place,
    resulting in unavoidable warnings for packages that weren't actually
    loaded too early.

--- modules/editor/evil/init.el
+++ modules/editor/evil/init.el
@@ -279,7 +279,7 @@ and complains if a module is loaded too early (during startup)."
     (unless (memq (or (car-safe module) module) disabled-list)
       (doom-log "editor:evil: loading evil-collection-%s %s"
                 (or (car-safe module) module)
-                (if doom-init-time "" "(too early!)"))
+                (if after-init-time "" "(too early!)"))
       (with-demoted-errors "evil-collection error: %s"
         (evil-collection-init (list module)))))
 
