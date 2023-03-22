512828078f03727d57d1bcdb0c3d7f15baa1c23c
Henrik Lissner <git@henrik.io>
2023-03-22 01:21:04 -0400

    bump: :emacs undo
    emacs-straight/vundo@d78b02ab8995 -> emacs-straight/vundo@26c929229ae0
    
    Ref: #6263

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -6,4 +6,4 @@
   (package! undo-fu :pin "0e22308de8337a9291ddd589edae167d458fbe77")
   (package! undo-fu-session :pin "7b3fd0647dd1fbd02101eec61440e6d44953bcd9")
   (when (> emacs-major-version 27)  ; unsupported in 27
-    (package! vundo :pin "d78b02ab89955f3a3273884f6d799889a2ef6b6f")))
+    (package! vundo :pin "26c929229ae03b8d942907541f0844988b36d0ce")))
