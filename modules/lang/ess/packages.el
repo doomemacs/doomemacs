fd84c3d882065f645b93a2b0de5b1dcdc5cb25c3
Maikol Solís <maikol.solis@ucr.ac.cr>
2023-04-16 07:44:17 -0600

    bump: :lang ess
    emacs-ess/ESS@8b4664e4a76a -> emacs-ess/ESS@3bff8e738c88
    polymode/polymode@15b6c1e94a45 -> polymode/polymode@ca060e081a1f

--- modules/lang/ess/packages.el
+++ modules/lang/ess/packages.el
@@ -4,10 +4,10 @@
 (package! ess :pin "0eb240bcb6d0e933615f6cfaa9761b629ddbabdd")
 (package! ess-view-data :pin "dd6a85935bbee0f497d0e8731abdfa07150600b7")
 (package! ess-smart-equals :pin "fea9eea4b59c3e9559b379508e3500076ca99ef1")
+(package! ess-r-insert-obj :pin "2ded9c23d0af2a7f6c0e02f9ea4af0e5b3cb7fb4")
 (package! polymode :pin "74ba75d4bcfbea959ccc9080a95ab9ef759849f2")
 (package! poly-R :pin "8024e852cfca642dea2045a41b2033baa2f1f9a5")
 (package! ess-r-insert-obj :pin "2ded9c23d0af2a7f6c0e02f9ea4af0e5b3cb7fb4")
-(package! poly-R :pin "8024e852cfca642dea2045a41b2033baa2f1f9a5")
 (package! quarto-mode :pin "a7b974f7d22ef939eaed8b9919434bcf20b1438f")
 
 (when (modulep! +stan)
