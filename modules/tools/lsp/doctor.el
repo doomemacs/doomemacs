23b00c96bf11f090aefdea19fe65b16c8b46772a
Ellis Kenyo <me@elken.dev>
2023-05-04 07:01:00 +0100

    fix(lsp): warn about server installers for lsp-mode
    Having the warning show up for Eglot is confusing UX since it doesn't
    apply.

--- modules/tools/lsp/doctor.el
+++ modules/tools/lsp/doctor.el
@@ -4,6 +4,7 @@
                    (modulep! +peek)))
          "+eglot and +peek flags are not compatible. Peek uses lsp-mode, while Eglot is another package altogether for LSP.")
 
-(unless (executable-find "npm")
+(unless (and (not (modulep! +eglot))
+             (executable-find "npm"))
   (warn! "Couldn't find npm, most server installers won't work and will have to be installed manually.
 For more information, see https://emacs-lsp.github.io/lsp-mode/page/languages/."))
