d9a92c691c4be2d2079e987c570003d3e1d9d00e
Zaz Brown <zazbrown@zazbrown.com>
2023-05-31 22:25:28 +0000

    fix: do not set the user's name to John Doe
    Do not set the user's email to john@doe.com either. It is better to
    leave these variables unset than to set them incorrectly.

--- templates/config.example.el
+++ templates/config.example.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
