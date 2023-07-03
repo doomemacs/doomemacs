cf863cbf72643df7894071476a45db7f67b0dc36
Álan Crístoffer <acristoffers@startmail.com>
2023-07-03 14:37:17 +0200

    bump: tree-sitter-langs.

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -2,7 +2,7 @@
 ;;; tools/tree-sitter/packages.el
 
 (package! tree-sitter :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
-(package! tree-sitter-langs :pin "d8f8ac4faeb4564fbb61e94a631b4672523d84a9")
+(package! tree-sitter-langs :pin "ac6084b62c15803ea42d996815795afa32f6cd9f")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
 (when (modulep! :editor evil +everywhere)
