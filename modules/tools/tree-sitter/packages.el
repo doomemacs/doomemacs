2b79787f1e597a045cbd40f806d8269b4bc3ec14
Henrik Lissner <git@henrik.io>
2023-07-22 14:35:52 +0200

    bump: :tools tree-sitter
    emacs-tree-sitter/tree-sitter-langs@d8f8ac4faeb4 -> emacs-tree-sitter/tree-sitter-langs@944a734b7c3a
    meain/evil-textobj-tree-sitter@02f825303404 -> meain/evil-textobj-tree-sitter@e8bb9d63deeb
    
    Close: #7286
    Co-authored-by: acristoffers <acristoffers@users.noreply.github.com>

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -2,9 +2,9 @@
 ;;; tools/tree-sitter/packages.el
 
 (package! tree-sitter :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
-(package! tree-sitter-langs :pin "d8f8ac4faeb4564fbb61e94a631b4672523d84a9")
+(package! tree-sitter-langs :pin "944a734b7c3a5922a7e9a2009a9165e2dc195b18")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
 (when (modulep! :editor evil +everywhere)
   (package! evil-textobj-tree-sitter
-    :pin "02f8253034042d8f171bc0ef93e3538b71a29153"))
+    :pin "e8bb9d63deeb2953ff9600e1633de667b3d7673e"))
