9245a347a4c2e11341eedef2e62ab23510131305
Jonathan Ming <jming422@gmail.com>
2023-07-24 10:29:46 -0700

    module: add :tools collab
    Add a collaborative editing module, powered by crdt.el.
    
    Ref: https://code.librehq.com/qhong/crdt.el
    Co-authored-by: Arte Ebrahimi <arteebrahimi@gmail.com>

---
+++ modules/tools/collab/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; tools/collab/packages.el
+
+(package! crdt
+  :recipe (:host nil :repo "https://code.librehq.com/qhong/crdt.el")
+  :pin "3ba890658d657db5d6aaedd5c2a78b6f93a5f139")
