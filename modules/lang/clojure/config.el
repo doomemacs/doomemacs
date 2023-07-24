906428710231512f6a4b6edf69e80e5276a7fa15
Henrik Lissner <git@henrik.io>
2023-07-24 20:27:23 +0200

    fix(clojure): tree-sitter in clojure{c,script}-mode
    Fix: #7250

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -36,7 +36,11 @@
                  clojurec-mode-local-vars-hook
                  clojurescript-mode-local-vars-hook)
                :append
-               #'tree-sitter!)))
+               #'tree-sitter!)
+    ;; TODO: PR this upstream
+    (after! tree-sitter-langs
+      (add-to-list 'tree-sitter-major-mode-language-alist '(clojurec-mode . clojure))
+      (add-to-list 'tree-sitter-major-mode-language-alist '(clojurescript-mode . clojure)))))
 
 
 ;; `cider-mode' is used instead of the typical `cider' package due to the main
