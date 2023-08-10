e64df07278a3a5fa44fb60cd8fb10ed1fc3a736e
Hanno Perrey <hperrey@users.noreply.github.com>
2023-08-10 12:12:43 +0200

    bump::config
     noctuid/link-hint.el@1f9bb60289d8 ->  noctuid/link-hint.el@36ce929331f2

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -3,7 +3,7 @@
 
 (package! avy :pin "955c8dedd68c74f3cf692c1249513f048518c4c9")
 (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-(package! link-hint :pin "1f9bb60289d87f31f92b86ed22d1f2c0a7af0222")
+(package! link-hint :pin "36ce929331f2838213bcaa1145ece4b73ce84afe")
 
 (unless (modulep! :editor evil)
   (package! expand-region :pin "b70feaa644310dc2d599dc277cd20a1f2b6446ac"))
