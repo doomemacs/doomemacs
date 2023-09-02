62074bfcbb7f332ca8aa123f1a4828a5e3ba98e7
Liam Hupfer <liam@hpfr.net>
2023-09-02 11:44:48 -0500

    tweak(emoji): add emoji-search binding for Emacs 29
    The emojify command takes precedence in case users want ASCII/GitHub
    string completion/insertion.
    
    Ref: https://git.savannah.gnu.org/cgit/emacs.git/tree/etc/NEWS?h=emacs-29.1#n884

--- modules/config/default/+emacs-bindings.el
+++ modules/config/default/+emacs-bindings.el
@@ -148,7 +148,10 @@
 
       ;;; <leader> i --- insert
       (:prefix-map ("i" . "insert")
-       :desc "Emoji"                         "e"   #'emojify-insert-emoji
+       (:when (> emacs-major-version 28)
+         :desc "Emoji"                       "e"   #'emoji-search)
+       (:when (modulep! :ui emoji)
+         :desc "Emoji"                       "e"   #'emojify-insert-emoji)
        :desc "Current file name"             "f"   #'+default/insert-file-path
        :desc "Current file path"             "F"   (cmd!! #'+default/insert-file-path t)
        :desc "Snippet"                       "s"   #'yas-insert-snippet
