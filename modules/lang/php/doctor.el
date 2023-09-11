dababf9ae99382bcb2f3f7c06257925f5ab91961
Henrik Lissner <git@henrik.io>
2023-09-12 01:20:52 +0200

    feat(php): doctor: rudimentary php & composer checks

--- modules/lang/php/doctor.el
+++ modules/lang/php/doctor.el
@@ -8,3 +8,9 @@
 (assert! (or (not (modulep! +tree-sitter))
              (modulep! :tools tree-sitter))
          "This module requires (:tools tree-sitter)")
+
+(unless (executable-find "php")
+  (warn! "Couldn't find php in your PATH"))
+
+(unless (executable-find "composer")
+  (warn! "Couldn't find composer in your PATH"))
