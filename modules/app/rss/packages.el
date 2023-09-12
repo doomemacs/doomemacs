89421f018be70d0a6c34009ce461a56b202b637c
Henrik Lissner <git@henrik.io>
2023-09-12 05:14:39 +0200

    bump: :app rss
    remyhonig/elfeed-org@3242ec051980 -> remyhonig/elfeed-org@f1c1fd6b1694
    skeeto/elfeed@162d7d545ed4 -> skeeto/elfeed@55fb162fa27e
    
    Close: #7407
    Co-authored-by: fosskers <fosskers@users.noreply.github.com>

--- modules/app/rss/packages.el
+++ modules/app/rss/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/rss/packages.el
 
-(package! elfeed :pin "162d7d545ed41c27967d108c04aa31f5a61c8e16")
+(package! elfeed :pin "55fb162fa27e71b88effa59a83c57842e262b00f")
 (package! elfeed-goodies :pin "544ef42ead011d960a0ad1c1d34df5d222461a6b")
 (when (modulep! +org)
-  (package! elfeed-org :pin "3242ec0519800a58f20480c8a6e3b3337d137084"))
+  (package! elfeed-org :pin "f1c1fd6b1694028ab7f2dd7e9ddbbef12711b353"))
