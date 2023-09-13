1fce8522f8f987d8d1ac1e74e6009db670c3d93b
Henrik Lissner <git@henrik.io>
2023-09-14 00:50:54 +0200

    bump: :editor multiple-cursors
    gabesoft/evil-mc@63fd2fe0c213 -> gabesoft/evil-mc@bdf893ea6f52
    magnars/multiple-cursors.el@16223efc2d6d -> magnars/multiple-cursors.el@234806c83299
    
    - Fixes evil-mc for recent versions of evil (#7415).
    
    Close: #7415
    Co-authored-by: Mo0dy <Mo0dy@users.noreply.github.com>

--- modules/editor/multiple-cursors/packages.el
+++ modules/editor/multiple-cursors/packages.el
@@ -4,6 +4,6 @@
 (cond
  ((modulep! :editor evil)
   (package! evil-multiedit :pin "23b53bc8743fb82a8854ba907b1d277374c93a79")
-  (package! evil-mc :pin "63fd2fe0c213a4cc31c464d246f92931c4cb720f"))
+  (package! evil-mc :pin "bdf893ea6f52fd0f10bece8ddae813658e17bbb4"))
 
- ((package! multiple-cursors :pin "16223efc2d6dece2d43bbccc189d7a4bab6de571")))
+ ((package! multiple-cursors :pin "234806c832994cadedb42596fe235e91bbd59e8c")))
