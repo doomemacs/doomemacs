e41a3514064decfb59c3bf9053052aa4bf22f071
Mo0dy <felix.muehlenberend@mailbox.org>
2023-09-13 22:24:56 +0200

    Updated evil-mc version to contain fix to handle pre-read-key-sequence type
    The old version of evil-mc could not handle evil commands such as "ciw" since evil was updated to contain a new key sequence type.

--- modules/editor/multiple-cursors/packages.el
+++ modules/editor/multiple-cursors/packages.el
@@ -4,6 +4,6 @@
 (cond
  ((modulep! :editor evil)
   (package! evil-multiedit :pin "23b53bc8743fb82a8854ba907b1d277374c93a79")
-  (package! evil-mc :pin "63fd2fe0c213a4cc31c464d246f92931c4cb720f"))
+  (package! evil-mc :pin "bdf893ea6f52fd0f10bece8ddae813658e17bbb4a4cc31c464d246f92931c4cb720f"))
 
  ((package! multiple-cursors :pin "16223efc2d6dece2d43bbccc189d7a4bab6de571")))
