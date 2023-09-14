eec8808f453d551b6b32fbedfb506d4d706c0665
Henrik Lissner <git@henrik.io>
2023-09-14 15:38:01 +0200

    bump: :app rss
    remyhonig/elfeed-org@3242ec051980 -> remyhonig/elfeed-org@7f3ad868f2fa
    
    Ref: remyhonig/elfeed-org#87

--- modules/app/rss/packages.el
+++ modules/app/rss/packages.el
@@ -4,4 +4,4 @@
 (package! elfeed :pin "55fb162fa27e71b88effa59a83c57842e262b00f")
 (package! elfeed-goodies :pin "544ef42ead011d960a0ad1c1d34df5d222461a6b")
 (when (modulep! +org)
-  (package! elfeed-org :pin "f1c1fd6b1694028ab7f2dd7e9ddbbef12711b353"))
+  (package! elfeed-org :pin "7f3ad868f2fa3c17492340f7af84f208b9c25635"))
