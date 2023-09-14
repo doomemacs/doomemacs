cd2477e79c95088148403d591864652704c56a86
Henrik Lissner <git@henrik.io>
2023-09-14 03:38:45 +0200

    refactor(collab): s/featurep!/modulep!
    featurep! was renamed modulep! in ad6a3d0.
    
    Close: #7420
    Ref: ad6a3d0f33ee
    Co-authored-by: lagman <lagman@users.noreply.github.com>

--- modules/tools/collab/doctor.el
+++ modules/tools/collab/doctor.el
@@ -1,4 +1,5 @@
 ;;; tools/collab/doctor.el -*- lexical-binding: t; -*-
 
-(when (and (featurep! +tunnel) (not (executable-find "tuntox")))
+(when (and (modulep! +tunnel)
+           (not (executable-find "tuntox")))
   (warn! "Couldn't find tuntox command. This needs to be on your path for the +tunnel flag to work properly."))
