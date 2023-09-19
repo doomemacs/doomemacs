70eee39f923095f1440d769eb4123667fa338c74
Liam Hupfer <liam@hpfr.net>
2023-09-19 13:16:31 -0500

    feat(pdf): prefer built-in pdf-tools
    Nixpkgs provides a pdf-tools with prebuilt epdfinfo, much like vterm.

--- modules/tools/pdf/packages.el
+++ modules/tools/pdf/packages.el
@@ -1,5 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/pdf/packages.el
 
-(package! pdf-tools :pin "c69e7656a4678fe25afbd29f3503dd19ee7f9896")
+(package! pdf-tools
+  :built-in 'prefer
+  :pin "c69e7656a4678fe25afbd29f3503dd19ee7f9896")
 (package! saveplace-pdf-view :pin "abfb5e1f463cffc18218a0f7f2fa141a271b1813")
