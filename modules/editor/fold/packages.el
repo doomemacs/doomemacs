80f8b6b6802c38c3aa07bc4c428ca8fd7e9770dc
Henrik Lissner <git@henrik.io>
2023-11-23 18:05:01 -0500

    bump: :editor fold
    emacs-tree-sitter/ts-fold@9d9e0c5cf7b5 -> emacs-tree-sitter/ts-fold@75e72c658ad8
    
    - (#7480) Adds/enhances support for new languages.
    
    Co-authored-by: jperras <jperras@users.noreply.github.com>
    Close: #7480
    Ref: https://github.com/emacs-tree-sitter/ts-fold/releases/tag/0.2.0

--- modules/editor/fold/packages.el
+++ modules/editor/fold/packages.el
@@ -7,5 +7,5 @@
 (when (modulep! :editor evil)
   (package! evil-vimish-fold :pin "b6e0e6b91b8cd047e80debef1a536d9d49eef31a"))
 (when (modulep! :tools tree-sitter)
-  (package! ts-fold :pin "9d9e0c5cf7b5397e06571bb6bf497598dc8796a8"
+  (package! ts-fold :pin "75e72c658ad8d8aac3af554a6b51b3c5c22dd0aa"
     :recipe (:host github :repo "emacs-tree-sitter/ts-fold")))
