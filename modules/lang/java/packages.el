13fa55da8200e4482d5cb0d473762a3e08e21057
Henrik Lissner <git@henrik.io>
2023-11-23 18:23:08 -0500

    bump: :lang java
    Groovy-Emacs-Modes/groovy-emacs-modes@c612ac1e9f74 -> Groovy-Emacs-Modes/groovy-emacs-modes@7b8520b2e2d3
    emacs-lsp/lsp-java@dbe448a886e2 -> emacs-lsp/lsp-java@449673da7221
    mopemope/meghanada-emacs@59c46cabb7ee -> mopemope/meghanada-emacs@fb29746e442e
    
    Co-authored-by: aveseli <aveseli@users.noreply.github.com>
    Close: #7554

--- modules/lang/java/packages.el
+++ modules/lang/java/packages.el
@@ -2,10 +2,10 @@
 ;;; lang/java/packages.el
 
 (package! android-mode :pin "d5332e339a1f5e30559a53feffb8442ca79265d6")
-(package! groovy-mode :pin "c612ac1e9f742856914ad6e8eb9e9dc169f489ab")
+(package! groovy-mode :pin "7b8520b2e2d3ab1d62b35c426e17ac25ed0120bb")
 
 (when (modulep! +meghanada)
-  (package! meghanada :pin "59c46cabb7eee715fe810ce59424934a1286df84"))
+  (package! meghanada :pin "fb29746e442e3d7b903759d15977d142a4bf2131"))
 
 (when (modulep! +eclim)
   (package! eclim :pin "222ddd48fcf0ee01592dec77c58e0cf3f2ea1100")
@@ -14,4 +14,4 @@
 
 (when (modulep! +lsp)
   (unless (modulep! :tools lsp +eglot)
-    (package! lsp-java :pin "dbe448a886e2f4fb5d3a616f4499adbe643ba7a5")))
+    (package! lsp-java :pin "449673da7221a30f1b1756cedcc48b9a2b52a51e")))
