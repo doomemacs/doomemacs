cb3d01920c83904a8af0805346c460d2f4f5a698
Henrik Lissner <git@henrik.io>
2023-11-23 22:05:33 -0500

    bump: :tools debugger lsp
    emacs-lsp/dap-mode@096070aacff8 -> emacs-lsp/dap-mode@755845ae053b
    emacs-lsp/lsp-mode@266945b3e470 -> emacs-lsp/lsp-mode@d441f3d268a3
    emacs-straight/eglot@8ccec6532e70 -> emacs-straight/eglot@2b145778ba5e
    realgud/realgud@220ce2c348da -> realgud/realgud@365063ea8ce8
    
    Fix: #7540

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/debugger/packages.el
 
-(when (package! realgud :pin "220ce2c348dab6cfc1cfa3c3f59644e777f9e8ff")
+(when (package! realgud :pin "365063ea8ce8ec6a852cb388088d84147421c3c2")
   (when (modulep! :lang javascript)
     (package! realgud-trepan-ni :pin "0ec088ea343835e24ae73da09bea96bfb02a3130")))
 
 (when (modulep! +lsp)
-  (package! dap-mode :pin "096070aacff875a09c13e596e5d60a55e0f07ab1")
+  (package! dap-mode :pin "755845ae053bbfdd3f7b3dca13efa4be480370b5")
   (package! posframe :pin "017deece88360c7297265680d78a0bb316470716"))
--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -3,13 +3,13 @@
 
 (if (modulep! +eglot)
     (progn
-      (package! eglot :pin "8ccec6532e70f68289a06acc24437986a8a8a6c1")
+      (package! eglot :pin "2b145778ba5e57f393e50aea76b28e518c401828")
       (when (modulep! :completion vertico)
         (package! consult-eglot :pin "db9d41c9812a5a8a7b9a22fa7f3c314e37584d41"))
       (when (and (modulep! :checkers syntax)
              (not (modulep! :checkers syntax +flymake)))
         (package! flycheck-eglot :pin "9ff8d0068be59b1450964b390349d75a68af21ed")))
-  (package! lsp-mode :pin "266945b3e470212305812581d24a938a96c47a3a")
+  (package! lsp-mode :pin "d441f3d268a31a4a514af2db506a5eec47ee617d")
   (package! lsp-ui :pin "0dd39900c8ed8145d207985cb2f65cedd1ffb410")
   (when (modulep! :completion ivy)
     (package! lsp-ivy :pin "9ecf4dd9b1207109802bd1882aa621eb1c385106"))
