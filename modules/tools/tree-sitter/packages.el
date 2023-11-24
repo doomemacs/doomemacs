88966d7949aef3c4f93cfa3e91d50c3c98c01fcf
Zero King <l2dy@icloud.com>
2023-11-24 21:40:00 +0800

    bump: :tools tree-sitter
    emacs-tree-sitter/tree-sitter-langs@2f0f97abbd97 -> emacs-tree-sitter/tree-sitter-langs@3a3ad0527d5f

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -2,7 +2,7 @@
 ;;; tools/tree-sitter/packages.el
 
 (package! tree-sitter :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
-(package! tree-sitter-langs :pin "2f0f97abbd97e17773a24eec3f329d262f81d020")
+(package! tree-sitter-langs :pin "3a3ad0527d5f8c7768678878eb5cfe399bedf703")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
 (when (modulep! :editor evil +everywhere)
