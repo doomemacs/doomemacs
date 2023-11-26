d9d4f3ac7f4234023fac4760367c7d4ca7aea57c
Maoli <liumaolimm@gmail.com>
2023-11-26 18:32:08 +0800

    bump: :completion company
    sebastiencs/company-box@766546b2668b -> sebastiencs/company-box@b6f53e26adf9
    company-mode/company-mode@2ca3e29abf87 -> company-mode/company-mode@9b21604d1969
    
    Fix: doomemacs/doomemacs#6710

--- modules/completion/company/packages.el
+++ modules/completion/company/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/company/packages.el
 
-(package! company :pin "2ca3e29abf87392714bc2b26e50e1c0f4b9f4e2c")
+(package! company :pin "9b21604d19696de2c79ee28931620839b3a908b4")
 (package! company-dict :pin "cd7b8394f6014c57897f65d335d6b2bd65dab1f4")
 (when (modulep! +childframe)
-  (package! company-box :pin "766546b2668b5ef4eb4abbde632c9acd370c7788"))
+  (package! company-box :pin "b6f53e26adf948aca55c3ff6c22c21a6a6614253"))
