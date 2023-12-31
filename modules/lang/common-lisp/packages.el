1767ab8baf7004778efc9852d3f664f01c5477f5
Colin Woodbury <colin@fosskers.ca>
2023-12-31 23:07:04 +0900

    bump: :lang common-lisp
    joaotavora/sly@f34c22289a2b -> joaotavora/sly@ed17d2c2bd7a

--- modules/lang/common-lisp/packages.el
+++ modules/lang/common-lisp/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/common-lisp/packages.el
 
-(when (package! sly :pin "f34c22289a2b3ab10e607f9f8822d62bb5c98cf5")
+(when (package! sly :pin "ed17d2c2bd7aead0fbb09c3d22861c80a522a097")
   (package! sly-macrostep :pin "5113e4e926cd752b1d0bcc1508b3ebad5def5fad")
   (package! sly-repl-ansi-color :pin "b9cd52d1cf927bf7e08582d46ab0bcf1d4fb5048"))
