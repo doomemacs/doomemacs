819f3f11ccb8f8185dbeb48afd8988a5f0ba6214
Ivan Necas <necasik@gmail.com>
2024-01-03 22:14:34 +0100

    bump: :ui doom
    doomemacs/themes@4aee1f5a0e54 -> doomemacs/themes@ff26f26ea3d7
    
    Improves Emacs 30 compatibility (see ref).
    
    Ref: https://github.com/doomemacs/themes/issues/809

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "4aee1f5a0e54552669f747aa7c25e6027e73d76d")
+(package! doom-themes :pin "ff26f26ea3d761375f5fc4070438fbd0f3473d33")
 (package! solaire-mode :pin "8af65fbdc50b25ed3214da949b8a484527c7cc14")
