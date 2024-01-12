1ddd74b003cf6b803c15a64a1e97bb754f042497
Kirill A. Korinsky <kirill@korins.ky>
2024-01-12 02:57:25 +0100

    bump: :email wanderlust
    wanderlust/flim@80b8121f05a5 -> wanderlust/flim@abdd2315006e
    wanderlust/semi@9370961ddcee -> wanderlust/semi@9063a4485b14
    wanderlust/wanderlust@8369b2d5170a -> wanderlust/wanderlust@9fd2c65e8d69

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -5,7 +5,7 @@
 ;;      depends on flim, flim on apel), but they all have non-standard default
 ;;      branches which straight cannot detect without our help.
 (package! apel :recipe (:branch "apel-wl") :pin "82eb2325bd149dc57b43a9ce9402c6c6183e4052")
-(package! flim :recipe (:branch "flim-1_14-wl") :pin "80b8121f05a5a0d7fcfe3e54085467a646dd2028")
-(package! semi :recipe (:branch "semi-1_14-wl") :pin "9370961ddcee78e389e44b36d38c3d93f8351619")
+(package! flim :recipe (:branch "flim-1_14-wl") :pin "abdd2315006eb31476249223569808adb1c0f7b2")
+(package! semi :recipe (:branch "semi-1_14-wl") :pin "9063a4485b148a767ea924f0e7cc78d3524ba256")
 
-(package! wanderlust :pin "8369b2d5170a174652294835dd9a18ed21a38cb2")
+(package! wanderlust :pin "9fd2c65e8d690625f35035a71e73f51f740dbe04")
