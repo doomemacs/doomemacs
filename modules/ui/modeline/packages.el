6275ed7e8f67d4633ee83c80336603ee48fff772
George Thomas <thegeorgeous@users.noreply.github.com>
2024-01-27 09:14:35 +0100

    bump: :ui modeline
    seagle0128/doom-modeline@93f240f7a0bf -> seagle0128/doom-modeline@bf880ae56f3f

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -2,7 +2,7 @@
 ;;; ui/modeline/packages.el
 
 (unless (modulep! +light)
-  (package! doom-modeline :pin "93f240f7a0bf35511cfc0a8dd75786744b4bcf77"))
+  (package! doom-modeline :pin "bf880ae56f3f6aab7bd334de9bd9b455c63a24c0"))
 (package! anzu :pin "5abb37455ea44fa401d5f4c1bdc58adb2448db67")
 (when (modulep! :editor evil)
   (package! evil-anzu :pin "d1e98ee6976437164627542909a25c6946497899"))
