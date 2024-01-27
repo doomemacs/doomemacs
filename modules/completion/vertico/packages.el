828445dbfb66355fd1e2f3e244ccc45bc078b722
Itai Y. Efrat <itai3397@gmail.com>
2024-01-27 23:59:34 +0200

    bump: :completion vertico
    mhayashi1120/Emacs-wgrep@3132abd3750b -> mhayashi1120/Emacs-wgrep@208b9d01cfff
    minad/consult-flycheck@0662839aa5db -> minad/consult-flycheck@d83f87581af7
    minad/consult@9c7dbbe4d626 -> minad/consult@e4d371235647
    minad/marginalia@27ccfd2213bb -> minad/marginalia@ea356ebb1ddb
    minad/vertico@e8edac107df5 -> minad/vertico@cf8b2abf5207
    oantolin/embark@b9f2b3b9a5b9 -> oantolin/embark@33c392cf3ce5
    tumashu/vertico-posframe@15168c92ca06 -> tumashu/vertico-posframe@bc0e67cbbba4

--- modules/completion/vertico/packages.el
+++ modules/completion/vertico/packages.el
@@ -1,21 +1,21 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/vertico/packages.el
 
-(package! vertico  :pin "e8edac107df5883f2fa8690356a8d8deee1a83de")
+(package! vertico  :pin "cf8b2abf5207696c054c33214c86e3969d415054")
 
 (package! orderless :pin "b24748093b00b37c3a572c4909f61c08fa27504f")
 
-(package! consult :pin "9c7dbbe4d626987e40d7a157430021e13a6906bd")
+(package! consult :pin "e4d371235647a7f4967f093eff2125652796957c")
 (package! consult-dir :pin "3f5f4b71ebe819392cb090cda71bd39a93bd830a")
 (when (and (modulep! :checkers syntax)
            (not (modulep! :checkers syntax +flymake)))
-  (package! consult-flycheck :pin "0662839aa5db429130f5ffd15c14d4a980b2e694"))
-(package! embark :pin "b9f2b3b9a5b9c72cf1416097b9941c4f275dae94")
-(package! embark-consult :pin "b9f2b3b9a5b9c72cf1416097b9941c4f275dae94")
+  (package! consult-flycheck :pin "d83f87581af74f7a2739d8b1b90c37da5ae3d310"))
+(package! embark :pin "33c392cf3ce5b92ad73ed5c4f44dbca5d0741cde")
+(package! embark-consult :pin "33c392cf3ce5b92ad73ed5c4f44dbca5d0741cde")
 
-(package! marginalia :pin "27ccfd2213bb9432883427cf058c63af10196aa6")
+(package! marginalia :pin "ea356ebb1ddb8d6da78574b517155475cf52d46f")
 
-(package! wgrep :pin "3132abd3750b8c87cbcf6942db952acfab5edccd")
+(package! wgrep :pin "208b9d01cfffa71037527e3a324684b3ce45ddc4")
 
 (when (modulep! +icons)
   (package! nerd-icons-completion :pin "c2db8557a3c1a9588d111f8c8e91cae96ee85010"))
@@ -23,4 +23,4 @@
 (when (modulep! +childframe)
   (package! vertico-posframe
     :recipe (:host github :repo "tumashu/vertico-posframe")
-    :pin "15168c92ca06a39fbf84ac27d4e2ae02216300b5"))
+    :pin "bc0e67cbbba4daaf6ce7b8701a0dc7797d468752"))
