d38787edf48f4c08bdf7ea8b3b54fcbc268f13b5
Henrik Lissner <git@henrik.io>
2024-02-02 18:49:05 -0500

    bump: :lang emacs-lisp
    Wilfred/elisp-def@1d2e88a232ec -> Wilfred/elisp-def@1ad4baccbf3d
    jorgenschaefer/emacs-buttercup@30c703d215b0 -> jorgenschaefer/emacs-buttercup@24d43b2ce262
    purcell/flycheck-package@3a6aaed29ff6 -> purcell/flycheck-package@75efa098cf17
    tonini/overseer.el@02d49f582e80 -> tonini/overseer.el@7fdcf1a6fba6
    xuchunyang/elisp-demos@8d0cd806b109 -> xuchunyang/elisp-demos@1a108d1c5011

--- modules/lang/emacs-lisp/packages.el
+++ modules/lang/emacs-lisp/packages.el
@@ -8,13 +8,13 @@
 
 ;; Tools
 (package! macrostep :pin "0b04a89f698c335c9ea492553470a8d45c113edd")
-(package! overseer :pin "02d49f582e80e36b4334c9187801c5ecfb027789")
-(package! elisp-def :pin "1d2e88a232ec16bce036b49577c4d4d96035f9f7")
-(package! elisp-demos :pin "8d0cd806b109076e6c4383edf59dbab9435dc5dc")
+(package! overseer :pin "7fdcf1a6fba6b1569a09c1666b4e51bcde266ed9")
+(package! elisp-def :pin "1ad4baccbf3d0d13e7607d332ae6bc60a5dd7360")
+(package! elisp-demos :pin "1a108d1c5011f9ced58be2ca98bea1fbd4130a2f")
 (when (and (modulep! :checkers syntax)
            (not (modulep! :checkers syntax +flymake)))
-  (package! flycheck-package :pin "3a6aaed29ff61418c48c0251e1432c30748ae739")
+  (package! flycheck-package :pin "75efa098cf17dc14c363e2ca9b68afdac7766b5b")
   (package! flycheck-cask :pin "4b2ede6362ded4a45678dfbef1876faa42edbd58"))
 
 ;; Libraries
-(package! buttercup :pin "30c703d215b075aaede936a2c424f65b5f7b6391")
+(package! buttercup :pin "24d43b2ce262faf59e5ff9f72466efb293aa6154")
