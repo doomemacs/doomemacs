83f5c4f81844364733f09a545807585161036e91
Henrik Lissner <git@henrik.io>
2024-02-05 17:18:13 -0500

    bump: :input
    cute-jumper/fcitx.el@12dc2638ddd1 -> cute-jumper/fcitx.el@b399482ed8db
    emacs-jp/migemo@f756cba3d526 -> emacs-jp/migemo@7d78901773da
    laishulu/evil-pinyin@3e9e501ded86 -> laishulu/evil-pinyin@0fae5ad87614
    merrickluo/liberime@8291e22cd099 -> merrickluo/liberime@cc9eb9812fd6
    skk-dev/ddskk@c24a624884a3 -> skk-dev/ddskk@8c47f46e38a2
    tumashu/posframe@0d23bc5f7cfa -> tumashu/posframe@017deece8836
    tumashu/pyim@de7eff2a58d8 -> tumashu/pyim@64067b20ce0e

--- modules/input/japanese/packages.el
+++ modules/input/japanese/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/japanese/packages.el
 
-(package! migemo :pin "f756cba3d5268968da361463c2e29b3a659a3de7")
+(package! migemo :pin "7d78901773da3b503e5c0d5fa14a53ad6060c97f")
 (package! avy-migemo :pin "922a6dd82c0bfa316b0fbb56a9d4dd4ffa5707e7")
-(package! ddskk :pin "c24a624884a3cfb0c28d6d5b9eb19e01387f0917")
+(package! ddskk :pin "8c47f46e38a29a0f3eabcd524268d20573102467")
 (package! pangu-spacing :pin "2303013e5cd7852136f1429162fea0e1c8cb0221")
