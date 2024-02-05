792c41f638907e2515956d29c6aaf1f65ec39747
Henrik Lissner <git@henrik.io>
2024-02-05 17:16:50 -0500

    bump: :email mu4e notmuch
    https://git.notmuchmail.org/git/notmuch@b6f144abe1f5 -> https://git.notmuchmail.org/git/notmuch@2f0320c5f24a
    jao/consult-notmuch@d0d4129d45cc -> jao/consult-notmuch@d8022e2ddc67
    jeremy-compostella/org-msg@055de4abf611 -> jeremy-compostella/org-msg@0b65f0f77a7a
    org-mime/org-mime@d368bd4119bf -> org-mime/org-mime@9d4584651d89
    tarsius/ol-notmuch@781c3518a537 -> tarsius/ol-notmuch@881991d94a1a

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -2,6 +2,6 @@
 ;;; email/mu4e/packages.el
 
 (when (modulep! +org)
-  (package! org-msg :pin "055de4abf611c5d5e12c770fe149c1861b402817"))
+  (package! org-msg :pin "0b65f0f77a7a71881ddfce19a8cdc60465bda057"))
 
 (package! mu4e-alert :pin "6beda20fc69771f2778f507c4a9e069dbaf1b628")
