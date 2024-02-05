b070a801ed3d95e896cf87044223d24dfb0a7b19
Henrik Lissner <git@henrik.io>
2024-02-04 19:02:33 -0500

    bump: :tools collab
    https://code.librehq.com/qhong/crdt.el@3ba890658d65 -> emacs-straight/crdt@4a18cd8539c8
    
    Moved to github mirror since old source was unreliable.

--- modules/tools/collab/packages.el
+++ modules/tools/collab/packages.el
@@ -1,6 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/collab/packages.el
 
-(package! crdt
-  :recipe (:host nil :repo "https://code.librehq.com/qhong/crdt.el")
-  :pin "3ba890658d657db5d6aaedd5c2a78b6f93a5f139")
+(package! crdt :pin "4a18cd8539c8f3364e3ae903580328dd8b05fa78")
