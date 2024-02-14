5e1a018b88ef5ec4630620661f63b56d3f86016f
Kirill A. Korinsky <kirill@korins.ky>
2024-02-14 08:52:58 +0100

    bump: :email wanderlust
    wanderlust/wanderlust@9fd2c65e8d69 -> wanderlust/wanderlust@c15e8ece4f34

--- modules/email/wanderlust/packages.el
+++ modules/email/wanderlust/packages.el
@@ -8,7 +8,7 @@
 (package! flim :recipe (:branch "flim-1_14-wl") :pin "abdd2315006eb31476249223569808adb1c0f7b2")
 (package! semi :recipe (:branch "semi-1_14-wl") :pin "9063a4485b148a767ea924f0e7cc78d3524ba256")
 
-(package! wanderlust :pin "9fd2c65e8d690625f35035a71e73f51f740dbe04")
+(package! wanderlust :pin "c15e8ece4f34f10479e17cda19d10b98f6be3ec1")
 
 (when (modulep! +xface)
   (package! x-face-e21
