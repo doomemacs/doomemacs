35dc13632b3177b9efedad212f2180f69e756853
Henrik Lissner <git@henrik.io>
2024-02-28 20:47:56 -0500

    bump: :lang rust
    rust-lang/rust-mode@HEAD -> rust-lang/rust-mode@f74dd1cd8798
    
    - rust-lang/rust-mode@08cea61 introduced some clumsy autoloads that will
      cause treesit and rust-ts-mode (and more in later commits) to be
      eagerly loaded at startup. This causes other problems, but the primary
      issue is that it causes errors at startup or when installing
      rust-mode (#7698), which this bump addresses. Eager-loading will have
      to be addressed upstream.
    
    Ref: rust-lang/rust-mode@08cea6139097
    Fix: #7698

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,4 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
+(package! rust-mode :pin "f74dd1cd87987ea7faf0cfc6240c2284ef9133cb")
 (package! rustic :pin "39423d1cf4fa054c36bf9577356451f4c06ee148")
