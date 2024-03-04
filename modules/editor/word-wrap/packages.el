179e300bfdd55d6dc8b002a63481990a1e86b574
Henrik Lissner <git@henrik.io>
2024-03-04 03:26:07 -0500

    bump: visual-fill-column
    https://codeberg.org/joostkremers/visual-fill-column@db7c7c236555 -> emacsmirror/visual-fill-column@5e74afe39d0a
    
    codeberg has uptime issues. Where possible, we should prefer Github
    sources.
    
    Fix: #7013

--- modules/editor/word-wrap/packages.el
+++ modules/editor/word-wrap/packages.el
@@ -2,4 +2,6 @@
 ;;; editor/word-wrap/packages.el
 
 (package! adaptive-wrap :pin "70005d2012ab57c20be03c05aebd49318fe49c99")
-(package! visual-fill-column :pin "db7c7c236555c9c684e1294a277efefdc25fa5c4")
+(package! visual-fill-column
+  :recipe (:host github :repo "emacsmirror/visual-fill-column")
+  :pin "5e74afe39d0afb911dae51af4e7a60ccdf9701f3")
