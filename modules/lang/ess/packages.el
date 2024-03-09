94732b53832fc09174590738801b6d2b7317a057
Maikol Solís <maikol.solis@ucr.ac.cr>
2024-03-09 07:50:48 -0600

    bump: :lang ess
    ShuguangSun/ess-view-data@c0b20e503c77 -> ShuguangSun/ess-view-data@c077741bc338
    emacs-ess/ESS@5c4ae91cefa5 -> emacs-ess/ESS@cf2372530495

--- modules/lang/ess/packages.el
+++ modules/lang/ess/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/ess/packages.el
 
-(package! ess :pin "5c4ae91cefa5c56fd13b204a9a996825af836a67")
-(package! ess-view-data :pin "c0b20e503c772eebbef6bbc6c89a707ba6ec42ec")
+(package! ess :pin "cf237253049583a547bafc16edb9eb9a52e1ebf6")
+(package! ess-view-data :pin "c077741bc3386a469635ca7438db4cf58b7541b9")
 (package! ess-smart-equals :pin "fea9eea4b59c3e9559b379508e3500076ca99ef1")
 (package! ess-r-insert-obj :pin "2ded9c23d0af2a7f6c0e02f9ea4af0e5b3cb7fb4")
 (package! polymode :pin "ca060e081a1f849a880732670dc15370ac987b89")
