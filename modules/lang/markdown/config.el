3820ead9e3b029abb2e09289d4b875e25cdea332
Henrik Lissner <git@henrik.io>
2024-03-10 00:38:13 -0500

    tweak(markdown): disable wiki links & math highlights by default
    These two features are relatively expensive and shouldn't be enabled as
    a global default.

--- modules/lang/markdown/config.el
+++ modules/lang/markdown/config.el
@@ -18,9 +18,7 @@ capture, the end position, and the output buffer.")
 (use-package! markdown-mode
   :mode ("/README\\(?:\\.md\\)?\\'" . gfm-mode)
   :init
-  (setq markdown-enable-math t ; syntax highlighting for latex fragments
-        markdown-enable-wiki-links t
-        markdown-italic-underscore t
+  (setq markdown-italic-underscore t
         markdown-asymmetric-header t
         markdown-gfm-additional-languages '("sh")
         markdown-make-gfm-checkboxes-buttons t
