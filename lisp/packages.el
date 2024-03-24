2df24d29f3923aa736692f9d986024dca3381b71
Henrik Lissner <git@henrik.io>
2024-03-24 18:08:09 -0400

    bump: :core
    Fuco1/smartparens@0778a8a84064 -> Fuco1/smartparens@ddc6233ea6fc
    bbatsov/projectile@e45f0b0cc43f -> bbatsov/projectile@0163b335a18a
    emacs-compat/compat@eb8fbfa5582a -> emacs-compat/compat@8d4e8a366681
    emacs-straight/project@10a6b691e36f -> emacs-straight/project@b6989856abe9
    joddie/pcre2el@018531ba0cf8 -> joddie/pcre2el@380723b2701c
    jscheid/dtrt-indent@0230ec503283 -> jscheid/dtrt-indent@5d1b44f9a1a4
    justbur/emacs-which-key@4d20bc852545 -> justbur/emacs-which-key@96911a1d3faf
    noctuid/general.el@bda777cd303d -> noctuid/general.el@ced143c30de8
    radian-software/straight.el@b3760f5829db -> radian-software/straight.el@b1062df10ba4
    rainstormstudio/nerd-icons.el@c6a4acf19454 -> rainstormstudio/nerd-icons.el@8095215a503d

--- lisp/packages.el
+++ lisp/packages.el
@@ -6,7 +6,7 @@
   :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
 (package! compat
   :recipe (:host github :repo "emacs-compat/compat")
-  :pin "eb8fbfa5582a8e5880e2eaa66d15d498bca6a45a")
+  :pin "8d4e8a366681def88751f5e9975738ecd3180deb")
 (package! gcmh
   :pin "0089f9c3a6d4e9a310d0791cf6fa8f35642ecfd9")
 
@@ -18,10 +18,10 @@
             :branch ,straight-repository-branch
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "b3760f5829dba37e855add7323304561eb57a3d4")
+  :pin "b1062df10ba4c10ff7a3c61b9e124b3242b11bb2")
 
 ;; doom-ui.el
-(package! nerd-icons :pin "c6a4acf19454b415cba1c43daf4bfca8fccdd9ba")
+(package! nerd-icons :pin "8095215a503d8048739de8b4ea4066598edb8cbb")
 (package! hide-mode-line :pin "bc5d293576c5e08c29e694078b96a5ed85631942")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "f40ece58df8b2f0fb6c8576b527755a552a5e763")
@@ -29,10 +29,10 @@
 
 ;; doom-editor.el
 (package! better-jumper :pin "47622213783ece37d5337dc28d33b530540fc319")
-(package! dtrt-indent :pin "0230ec503283b895bd3df6c1e30b35a01aa0b9af")
+(package! dtrt-indent :pin "5d1b44f9a1a484ca229cc14f8062609a10ef4891")
 (package! helpful :pin "a32a5b3d959a7fccf09a71d97b3d7c888ac31c69")
-(package! pcre2el :pin "018531ba0cf8e2b28d1108136a0e031b6a45f1c1")
-(package! smartparens :pin "0778a8a84064cf2bc3a9857bd0e7a4619cc1e5c3")
+(package! pcre2el :pin "380723b2701cceb75c266440fb8db918f3340d50")
+(package! smartparens :pin "ddc6233ea6fc2da7a3a8e44face465c15631b02b")
 (package! ws-butler
   ;; Use my fork of ws-butler, which has a few choice improvements and
   ;; optimizations (the original has been abandoned).
@@ -40,9 +40,9 @@
   :pin "572a10c11b6cb88293de48acbb59a059d36f9ba5")
 
 ;; doom-projects.el
-(package! projectile :pin "e45f0b0cc43fdc066e7971ff3ed3bf4c78015ed0")
-(package! project :pin "10a6b691e36ff897fb2a4b48896e08818afa77b0")
+(package! projectile :pin "0163b335a18af0f077a474d4dc6b36e22b5e3274")
+(package! project :pin "b6989856abe9411872bdff5c8aa190bef4d86409")
 
 ;; doom-keybinds.el
-(package! general :pin "bda777cd303db217fd2fbf2087eff40ec4aafda1")
-(package! which-key :pin "4d20bc852545a2e602f59084a630f888542052b1")
+(package! general :pin "ced143c30de8e20f5a3761a465e684a1dc48471e")
+(package! which-key :pin "96911a1d3faf8426a33241f4821319e98421f380")
