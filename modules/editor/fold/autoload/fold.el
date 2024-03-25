doomemacs/doomemacs

commit 7b9d00d6616d67cbc92765bb9da0dd0d2d9e9581

feat(tree-sitter): add ts to open/close all
also refactor +fold-open-all, +fold-close-all to use cond instead of the
when case for vimish fold.

modules/editor/fold/autoload/fold.el
@@ -109,18 +109,20 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
   "Open folds at LEVEL (or all folds if LEVEL is nil)."
   (interactive
    (list (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
-  (when (featurep 'vimish-fold)
-    (vimish-fold-unfold-all))
-  (save-excursion
-    (+fold--ensure-hideshow-mode)
-    (if (integerp level)
-        (progn
-          (outline-hide-sublevels (max 1 (1- level)))
-          (hs-life-goes-on
-           (hs-hide-level-recursive (1- level) (point-min) (point-max))))
-      (hs-show-all)
-      (when (fboundp 'outline-show-all)
-        (outline-show-all)))))
+  (cond ((+fold--ts-fold-p)
+         (ts-fold-open-all))
+        ((featurep 'vimish-fold)
+         (vimish-fold-unfold-all))
+        (t (save-excursion
+             (+fold--ensure-hideshow-mode)
+             (if (integerp level)
+                 (progn
+                   (outline-hide-sublevels (max 1 (1- level)))
+                   (hs-life-goes-on
+                    (hs-hide-level-recursive (1- level) (point-min) (point-max))))
+               (hs-show-all)
+               (when (fboundp 'outline-show-all)
+                 (outline-show-all)))))))
 
 ;;;###autoload
 (defun +fold/close-all (&optional level)
@@ -128,13 +130,15 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
   (interactive
    (list (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
   (save-excursion
-    (when (featurep 'vimish-fold)
-      (vimish-fold-refold-all))
-    (+fold--ensure-hideshow-mode)
-    (hs-life-goes-on
-     (if (integerp level)
-         (hs-hide-level-recursive (1- level) (point-min) (point-max))
-       (hs-hide-all)))))
+    (cond ((+fold--ts-fold-p)
+           (ts-fold-close-all))
+          (t (when (featurep 'vimish-fold)
+               (vimish-fold-refold-all))
+             (+fold--ensure-hideshow-mode)
+             (hs-life-goes-on
+              (if (integerp level)
+                  (hs-hide-level-recursive (1- level) (point-min) (point-max))
+                (hs-hide-all)))))))
 
 ;;;###autoload
 (defun +fold/next (count)

commit 08dc0fa6c17f132279b1d6b06faf65ddacc342da

fix(fold): properly support outline-minor-mode
- Make `+fold/close-all` work for outline-minor-mode.

- Distinguish between `zo` and `zO` for outline-minor-mode alone, by
  binding `zO` to a new function `+fold/open-rec`. `zO` will now trigger
  `outline-show-subtree` in outline-minor-mode, while `zo` will only
  trigger `outline-show-branches`, keeping the subheadings folded. This
  matches the behavior in org-mode.

- Fix a bug in `+fold/open-all`, introduced in
  7b9d00d6616d67cbc92765bb9da0dd0d2d9e9581, that makes it only operate
  on vimish-fold folds.

Note that outline-minor-mode is still not enabled by default anywhere.
These changes are only to prevent incorrect behavior for users who
decide to turn it on themselves.

modules/editor/fold/autoload/fold.el
@@ -79,6 +79,18 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
           ((+fold--ts-fold-p) (ts-fold-toggle)))))
 
 ;;;###autoload
+(defun +fold/open-rec ()
+  "Recursively open the folded region at point.
+
+Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
+  (interactive)
+  (save-excursion
+    (cond ((+fold--vimish-fold-p) (vimish-fold-unfold))
+          ((+fold--outline-fold-p) (outline-show-subtree))
+          ((+fold--hideshow-fold-p) (+fold-from-eol (hs-show-block)))
+          ((+fold--ts-fold-p) (ts-fold-open)))))
+
+;;;###autoload
 (defun +fold/open ()
   "Open the folded region at point.
 
@@ -87,7 +99,7 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
   (save-excursion
     (cond ((+fold--vimish-fold-p) (vimish-fold-unfold))
           ((+fold--outline-fold-p)
-           (outline-show-children)
+           (outline-show-branches)
            (outline-show-entry))
           ((+fold--hideshow-fold-p) (+fold-from-eol (hs-show-block)))
           ((+fold--ts-fold-p) (ts-fold-open)))))
@@ -111,7 +123,7 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
    (list (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
   (cond ((+fold--ts-fold-p)
          (ts-fold-open-all))
-        ((featurep 'vimish-fold)
+        ((and (featurep 'vimish-fold) (+fold--vimish-fold-p))
          (vimish-fold-unfold-all))
         ((save-excursion
            (+fold--ensure-hideshow-mode)
@@ -138,8 +150,12 @@ Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
         (+fold--ensure-hideshow-mode)
         (hs-life-goes-on
          (if (integerp level)
-             (hs-hide-level-recursive (1- level) (point-min) (point-max))
-           (hs-hide-all)))))))
+             (progn
+               (outline--show-headings-up-to-level (1+ level))
+               (hs-hide-level-recursive (1- level) (point-min) (point-max)))
+           (hs-hide-all)
+           (when (fboundp 'outline-hide-sublevels)
+             (outline-show-only-headings))))))))
 
 ;;;###autoload
 (defun +fold/next (count)

modules/editor/fold/config.el
@@ -6,7 +6,7 @@
     [remap evil-toggle-fold]   #'+fold/toggle
     [remap evil-close-fold]    #'+fold/close
     [remap evil-open-fold]     #'+fold/open
-    [remap evil-open-fold-rec] #'+fold/open
+    [remap evil-open-fold-rec] #'+fold/open-rec
     [remap evil-close-folds]   #'+fold/close-all
     [remap evil-open-folds]    #'+fold/open-all)
   (after! evil