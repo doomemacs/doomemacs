d256b597f2c6768e759ac2b866b110ba580c6f9f
Henrik Lissner <git@henrik.io>
2024-03-26 12:07:51 -0400

    bump: :tools magit
    alphapapa/magit-todos@1e9acc0ba63f -> alphapapa/magit-todos@332ce763f733
    magit/forge@68771ca4d53c -> magit/forge@2a3b41eb6235
    magit/magit@0963697f24cf -> magit/magit@0e8f25a8d801

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "0963697f24cfbe80f92312044bd9ab28b914b053")
+(when (package! magit :pin "0e8f25a8d8011328f2bf082232c720b24c2a12c2")
   (when (modulep! +forge)
-    (package! forge :pin "68771ca4d53c3aea5c860eeb888cee8e9cb5ca37")
+    (package! forge :pin "2a3b41eb6235b3f39c017c1f86b3928a45c5a64d")
     (package! code-review
       :recipe (:host github
                :repo "doomelpa/code-review"
                :files ("graphql" "code-review*.el"))
       :pin "e4c34fa284da25d8e0bafbae4300f1db5bdcda44"))
-  (package! magit-todos :pin "1e9acc0ba63fbc297001bf334d63cb4326be80df"))
+  (package! magit-todos :pin "332ce763f7336ea356964b92723678aa1ed4640f"))
