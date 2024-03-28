doomemacs/doomemacs

commit b6b755dea4546a7ae6d7dfb3fbfdbd71036a01fc

fix(cli): doom install: $DOOMDIR paths in output
Close: #7768
Co-authored-by: plajjan <plajjan@users.noreply.github.com>

lisp/cli/install.el
@@ -55,20 +55,19 @@ Change `$DOOMDIR' with the `--doomdir' option, e.g.
             (setq doom-user-dir (expand-file-name "doom/" xdg-config-dir)))))
 
       (if (file-directory-p doom-user-dir)
-          (print! (item "Skipping %s (already exists)") (relpath doom-user-dir))
+          (print! (item "Skipping %s (already exists)") (path doom-user-dir))
         (make-directory doom-user-dir 'parents)
-        (print! (success "Created %s") (relpath doom-user-dir)))
+        (print! (success "Created %s") (path doom-user-dir)))
 
       ;; Create init.el, config.el & packages.el
       (print-group!
         (mapc (lambda (file)
                 (cl-destructuring-bind (filename . template) file
-                  (if (file-exists-p! filename doom-user-dir)
-                      (print! (item "Skipping %s (already exists)")
-                              (path filename))
-                    (print! (item "Creating %s%s") (relpath doom-user-dir) filename)
-                    (with-temp-file (doom-path doom-user-dir filename)
-                      (insert-file-contents template))
+                  (setq filename (doom-path doom-user-dir filename))
+                  (if (file-exists-p filename)
+                      (print! (item "Skipping %s (already exists)...") (path filename))
+                    (print! (item "Creating %s...") (path filename))
+                    (doom-file-write filename template)
                     (print! (success "Done!")))))
               (let ((template-dir (doom-path doom-emacs-dir "templates")))
                 `((,doom-module-init-file

commit b44337198171bf612047d78f99968e17054cf48c

fix(cli): doom install: $DOOMDIR templates (part 2)
Caused by a regression originally introduced in b6b755d, but incorrectly
fixed in 89c56a3. Don't code while sleep deprived, kids.

Amend: 89c56a339346
Amend: b6b755dea454

lisp/cli/install.el
@@ -67,7 +67,7 @@ Change `$DOOMDIR' with the `--doomdir' option, e.g.
                   (if (file-exists-p filename)
                       (print! (item "Skipping %s (already exists)...") (path filename))
                     (print! (item "Creating %s...") (path filename))
-                    (with-temp-file template (insert-file-contents template))
+                    (with-temp-file filename (insert-file-contents template))
                     (print! (success "Done!")))))
               (let ((template-dir (doom-path doom-emacs-dir "templates")))
                 `((,doom-module-init-file