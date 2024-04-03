e5fb4f3dd90034c8315393b0cff24cd5b6ab801c
Henrik Lissner <git@henrik.io>
2024-04-03 15:54:51 -0400

    bump: :lang scala
    emacs-lsp/lsp-metals@da7e54ed65f4 -> emacs-lsp/lsp-metals@e55d544996f7
    hvesalai/emacs-sbt-mode@9fe1e8807c22 -> hvesalai/emacs-sbt-mode@bcf8d6040021

--- modules/lang/scala/packages.el
+++ modules/lang/scala/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/scala/packages.el
 
-(package! sbt-mode :pin "9fe1e8807c22cc1dc56a6233e000969518907f4d")
+(package! sbt-mode :pin "bcf8d6040021013430b39d6f6766ce1aab0b691a")
 (package! scala-mode :pin "4c6d636b86e3bb1d95de819dc48dda92abdfbcf4")
 
 (when (and (modulep! +lsp)
            (not (modulep! :tools lsp +eglot)))
-  (package! lsp-metals :pin "da7e54ed65f4e153c94b9c54689908dce142ef37"))
+  (package! lsp-metals :pin "e55d544996f7321622e1eeafdc3dd128f8e72ce5"))
