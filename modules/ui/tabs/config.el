91328f30fd280f71d2975cce768c6132bc467bdb
gpanago <georgepanagopo@gmail.com>
2024-04-04 00:15:02 -0400

    fix(tabs): speed up tabs by making it patient
    Limit the number of times that `centaur-tabs-buffer-update-groups'
    gets called per second.

--- modules/ui/tabs/config.el
+++ modules/ui/tabs/config.el
@@ -20,7 +20,26 @@
     (defun +tabs-disable-centaur-tabs-mode-maybe-h ()
       "Disable `centaur-tabs-mode' in current buffer."
       (when (centaur-tabs-mode-on-p)
-        (centaur-tabs-local-mode)))))
+        (centaur-tabs-local-mode))))
+
+  ;; HACK: The function centaur-tabs-buffer-update-groups gets called way too
+  ;; frequently leading to performance degredation.
+  ;; There is really no reason to call it more than 10 times a second,
+  ;; as buffers do not change groups more frequently than that.
+  (defvar centaur-tabs-buffer-groups-patience 0.1
+    "The amount of time (in seconds) to wait before recalculating groups.")
+  (setq centaur-tabs--buffers-time (float-time))
+  (defadvice! centaur-tabs-buffer-update-groups--patience (fn)
+    "Add patience to centaur-tabs-buffer-update-groups"
+    :around #'centaur-tabs-buffer-update-groups
+    (if (and (< (time-to-seconds)
+                (+ centaur-tabs-buffer-groups-patience
+                   centaur-tabs--buffers-time))
+             centaur-tabs--buffers
+             (assq (current-buffer) centaur-tabs--buffers))
+        (car (nth 2 (assq (current-buffer) centaur-tabs--buffers)))
+      (setq centaur-tabs--buffers-time (float-time))
+      (funcall fn))))
 
 
 ;; TODO tab-bar-mode (emacs 27)
