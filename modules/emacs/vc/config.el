57f43e095344c562778859acfa300668f78af031
Henrik Lissner <git@henrik.io>
2024-04-05 19:05:54 -0400

    fix(vc): *vc-{diff,change-log}* popup rules
    The former rules weren't proccing on these buffers. Also
    changes *vc-diff* popups to resize based on contents.

--- modules/emacs/vc/config.el
+++ modules/emacs/vc/config.el
@@ -28,10 +28,13 @@
         "k" #'log-view-msg-prev))
 
 
-(after! vc-annotate
+(after! vc
   (set-popup-rules!
-    '(("^\\*vc-diff" :select nil)   ; *vc-diff*
-      ("^\\*vc-change" :select t))) ; *vc-change-log*
+    '(("^\\*vc-diff" :select nil :size '+popup-shrink-to-fit)
+      ("^\\*vc-change-log" :select t))))
+
+
+(after! vc-annotate
   (set-evil-initial-state! 'vc-annotate-mode 'normal)
 
   ;; Clean up after itself
