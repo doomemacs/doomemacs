f99863b9f5edaffb4f48135fb165fe6ec3ea8d06
Henrik Lissner <git@henrik.io>
2024-04-07 16:10:39 -0400

    bump: :ui doom
    doomemacs/themes@37d2182f5078 -> doomemacs/themes@3b2422b208d2
    hlissner/emacs-solaire-mode@8af65fbdc50b -> hlissner/emacs-solaire-mode@8ccdceeb9298

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "37d2182f5078d29fb22bbace346e523bb84b2c4e")
-(package! solaire-mode :pin "8af65fbdc50b25ed3214da949b8a484527c7cc14")
+(package! doom-themes :pin "3b2422b208d28e8734b300cd3cc6a7f4af5eba55")
+(package! solaire-mode :pin "8ccdceeb9298b3c4e35f630914f467bf164f39ad")
