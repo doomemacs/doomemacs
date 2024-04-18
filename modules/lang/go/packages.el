46bcd7e922f5370043252f23cd150d2a3fa33012
Henrik Lissner <git@henrik.io>
2024-04-18 13:38:03 -0400

    bump: :lang go
    dominikh/go-mode.el@8dce1e3ba1cd -> dominikh/go-mode.el@6f4ff9ef874d
    weijiangan/flycheck-golangci-lint@9def093e416e -> weijiangan/flycheck-golangci-lint@91c59b128aa6
    
    - Fixes weijiangan/flycheck-golangci-lint#15: golangci-lint's renamed
      the --deadline option to --timeout. (#7833, #7834)
    
    Close: #7833
    Close: #7834
    Ref: weijiangan/flycheck-golangci-lint#15
    Co-authored-by: blez <blez@users.noreply.github.com>
    Co-authored-by: samcrang <samcrang@users.noreply.github.com>

--- modules/lang/go/packages.el
+++ modules/lang/go/packages.el
@@ -2,8 +2,8 @@
 ;;; lang/go/packages.el
 
 (package! go-eldoc :pin "cbbd2ea1e94a36004432a9ac61414cb5a95a39bd")
-(package! go-guru :pin "8dce1e3ba1cdc34a856ad53c8421413cfe33660e")
-(package! go-mode :pin "8dce1e3ba1cdc34a856ad53c8421413cfe33660e")
+(package! go-guru :pin "6f4ff9ef874d151ed8d297a80f1bf27db5d9dbf0")
+(package! go-mode :pin "6f4ff9ef874d151ed8d297a80f1bf27db5d9dbf0")
 (package! gorepl-mode :pin "6a73bf352e8d893f89cad36c958c4db2b5e35e07")
 (package! go-tag :pin "33f2059551d5298ca228d90f525b99d1a8d70364")
 (package! go-gen-test :pin "af00a9abbaba2068502327ecdef574fd894a884b")
@@ -13,4 +13,4 @@
 
 (when (and (modulep! :checkers syntax)
            (not (modulep! :checkers syntax +flymake)))
-  (package! flycheck-golangci-lint :pin "9def093e416e9a6ddd3cae8590dbb7ff6314925a"))
+  (package! flycheck-golangci-lint :pin "91c59b128aa6f719069cfb3e5df77588691a3e14"))
