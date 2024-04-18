0349dab09abf6fe918a2fb59a6723d3573d8b4b3
Henrik Lissner <git@henrik.io>
2024-04-18 13:48:36 -0400

    tweak(default): ensure '<leader> g S' is silent
    magit-stage-file prompts the user to select a file to stage, but the
    intention for these two keys has always been to (un)stage the current
    file (and without prompting). This ensures that is the case.
    
    I've also tweaked their description to make that intention clearer.

--- modules/config/default/+evil-bindings.el
+++ modules/config/default/+evil-bindings.el
@@ -493,8 +493,8 @@
         :desc "Magit clone"               "C"   #'magit-clone
         :desc "Magit fetch"               "F"   #'magit-fetch
         :desc "Magit buffer log"          "L"   #'magit-log-buffer-file
-        :desc "Git stage file"            "S"   #'magit-stage-file
-        :desc "Git unstage file"          "U"   #'magit-unstage-file
+        :desc "Git stage this file"       "S"   #'magit-stage-buffer-file
+        :desc "Git unstage this file"     "U"   #'magit-unstage-file
         (:prefix ("f" . "find")
          :desc "Find file"                 "f"   #'magit-find-file
          :desc "Find gitconfig file"       "g"   #'magit-find-git-config-file
