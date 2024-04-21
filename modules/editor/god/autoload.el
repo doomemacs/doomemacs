doomemacs/doomemacs

commit aa079b0e3ccc0548a677907a08cd12c5b878e44c

fix(god): remove hard coding of cursor color

modules/editor/god/autoload.el
@@ -1,8 +1,5 @@
 ;;; editor/god/autoload.el -*- lexical-binding: t; -*-
 
-(defvar +god-default-color (face-background 'cursor)
-  "Default cursor and bar color.")
-
 (defvar +god-read-only-mode-color "Gray"
   "Cursor and bar color when `read-only-mode' is enabled.")
 
@@ -29,7 +26,7 @@
           (cond (buffer-read-only +god-read-only-mode-color)
                 (is-fill-overflow +god-fill-overflow-color)
                 (overwrite-mode +god-overwrite-mode-color)
-                (t +god-default-color))))
+                (t previous-cursor-color))))
     (setq cursor-type next-cursor-type)
     (unless (eq previous-cursor-color next-cursor-and-modeline-color)
       (set-cursor-color next-cursor-and-modeline-color))

commit 9292fd609effdf7f05b3c1b1aec855a1919d5c43

fix(god): fix cursor color
This was broken in aa079b0e3ccc0548a677907a08cd12c5b878e44c.

Since the default cursor in the Doom theme uses the `highlight` color,
we can use that when setting the cursor color when this module is
enabled.

modules/editor/god/autoload.el
@@ -26,7 +26,7 @@
           (cond (buffer-read-only +god-read-only-mode-color)
                 (is-fill-overflow +god-fill-overflow-color)
                 (overwrite-mode +god-overwrite-mode-color)
-                (t previous-cursor-color))))
+                (t (face-background 'highlight)))))
     (setq cursor-type next-cursor-type)
     (unless (eq previous-cursor-color next-cursor-and-modeline-color)
       (set-cursor-color next-cursor-and-modeline-color))