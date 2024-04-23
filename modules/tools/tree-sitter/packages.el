477398e861b3290ed5ccb7a5903f0cc9cab93981
Martin Edström <meedstrom91@gmail.com>
2024-04-23 20:53:38 +0200

    bump: :tools tree-sitter
    emacs-tree-sitter/elisp-tree-sitter@3cfab8a0e945 -> emacs-tree-sitter/elisp-tree-sitter@e0ce25406706

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/tree-sitter/packages.el
 
-(package! tree-sitter :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
+(package! tree-sitter :pin "e0ce25406706e0a164bd565a391f5bb8f54f995f")
 (package! tree-sitter-langs :pin "b7895ca759563f3c7c3b928eb4f816bb4099d866")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
