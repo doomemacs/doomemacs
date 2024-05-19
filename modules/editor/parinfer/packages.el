395c4a05e14b1425f2a0c581f1de65ff6bf4e1c2
welcome-linja <eleanor@eleanor-nb.com>
2024-05-19 20:33:27 +1000

    bump :editor parinfer

--- modules/editor/parinfer/packages.el
+++ modules/editor/parinfer/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/parinfer/packages.el
 
-(package! parinfer-rust-mode :pin "8df117a3b54d9e01266a3905b132a1d082944702")
+(package! parinfer-rust-mode :pin "d3bfb2745cc0858e2741dc2a2f00a86f456656ec")
