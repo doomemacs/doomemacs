932df4d03f004fb3aa2e9cdc1a8c7a49b8c21997
welcome-linja <eleanor@eleanor-nb.com>
2024-05-30 16:36:03 +1000

    bump: :lang zig
    `zig-mode` doesn't do majors, but Zig has had a major since last bump.

--- modules/lang/zig/packages.el
+++ modules/lang/zig/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/zig/packages.el
 
-(package! zig-mode :pin "079149a19fc869343130e69d7b944afd3a1813cc")
+(package! zig-mode :pin "b4170b747ae4c45d145ff8bcb7fafe095e17b4c6")
