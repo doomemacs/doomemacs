5a8d65bc515d8a0372e4cb6a4c2be7a09c458eb0
Henrik Lissner <git@henrik.io>
2024-06-03 16:01:55 -0400

    bump: :tools magit :emacs vc
    alphapapa/magit-todos@332ce763f733 -> alphapapa/magit-todos@501c8db90ab5
    magit/forge@ad94b5665de3 -> magit/forge@c3675fd06876
    magit/git-modes@3cc94974c09c -> magit/git-modes@52ea2a1281ea
    magit/magit@b5637d665c1e -> magit/magit@f9268a959828

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -6,9 +6,9 @@
 (package! smerge-mode :built-in t)
 
 (package! browse-at-remote :pin "76aa27dfd469fcae75ed7031bb73830831aaccbf")
-(package! git-commit :pin "b5637d665c1e5bd5b76ffb072dbac387f37a5f63")
+(package! git-commit :pin "f9268a959828d0c6ab26171dd2fb1ffc55e5ae70")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
   :pin "5ed73c3831cf6da10ba941e6abba708a86853e8f")
-(package! git-modes :pin "3cc94974c09c43462dfbfbe20396a414352dbb92")
+(package! git-modes :pin "52ea2a1281ea9df9b8732fe2add0e6a0c9c2cd11")
--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "b5637d665c1e5bd5b76ffb072dbac387f37a5f63")
+(when (package! magit :pin "f9268a959828d0c6ab26171dd2fb1ffc55e5ae70")
   (when (modulep! +forge)
-    (package! forge :pin "ad94b5665de357347bfc52910eef46a79f74988d")
+    (package! forge :pin "c3675fd068767c694177a310d4fa0a01f81bb2d3")
     (package! code-review
       :recipe (:host github
                :repo "doomelpa/code-review"
                :files ("graphql" "code-review*.el"))
       :pin "e4c34fa284da25d8e0bafbae4300f1db5bdcda44"))
-  (package! magit-todos :pin "332ce763f7336ea356964b92723678aa1ed4640f"))
+  (package! magit-todos :pin "501c8db90ab59f8b619618b9d10db2a32a113727"))
