3037d364b2d2d907467998af5694960f1dbcc7a2
Henrik Lissner <git@henrik.io>
2024-06-20 17:16:28 -0400

    bump: :lang javascript
    abicky/nodejs-repl.el@3b841055cad0 -> abicky/nodejs-repl.el@03d0b64768b4
    emacs-typescript/typescript.el@4fcb4594819c -> emacs-typescript/typescript.el@fc3a4f3b275e
    js-emacs/xref-js2@fd6b723e7f1f -> js-emacs/xref-js2@e215af9eedac
    
    - (#7865) Adds support for satsifies keyword in typescript-mode.
    
    Close: #7865
    Co-authored-by: kisaragi-hiu <kisaragi-hiu@users.noreply.github.com>

--- modules/lang/javascript/packages.el
+++ modules/lang/javascript/packages.el
@@ -3,17 +3,17 @@
 
 ;; Major modes
 (package! rjsx-mode :pin "b697fe4d92cc84fa99a7bcb476f815935ea0d919")
-(package! typescript-mode :pin "4fcb4594819caf472ae42ea068a1c7795cf07f46")
+(package! typescript-mode :pin "fc3a4f3b275e8cf6cf41aa0c9ef42e25ef908feb")
 
 ;; Tools
 (package! js2-refactor :pin "a0977c4ce1918cc266db9d6cd7a2ab63f3a76b9a")
 (package! npm-mode :pin "3ee7c0bad5b7a041d4739ef3aaa06a3dc764e5eb")
 
 ;; Eval
-(package! nodejs-repl :pin "3b841055cad00f442e4a9159b1056f59411b6646")
+(package! nodejs-repl :pin "03d0b64768b40b71ae54de1b50eb58719e615746")
 (package! skewer-mode :pin "e5bed351939c92a1f788f78398583c2f83f1bb3c")
 
 ;; Programming environment
 (package! tide :pin "b38dfc3f8fb754e64e48e76fc92d472cb3d1a3dc")
 (when (modulep! :tools lookup)
-  (package! xref-js2 :pin "fd6b723e7f1f9793d189a815e1904364dc026b03"))
+  (package! xref-js2 :pin "e215af9eedac69b40942fff9d5514704f9f4d43e"))
