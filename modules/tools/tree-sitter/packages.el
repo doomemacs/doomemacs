ee10764c220c790625381c245416cb0864a4168d
Henrik Lissner <git@henrik.io>
2024-06-20 18:23:30 -0400

    bump: :tools tree-sitter
    emacs-tree-sitter/elisp-tree-sitter@3cfab8a0e945 -> emacs-tree-sitter/elisp-tree-sitter@02fe7b86d92b
    emacs-tree-sitter/tree-sitter-langs@b7895ca75956 -> emacs-tree-sitter/tree-sitter-langs@a06804e13fc1
    
    Close: #7842
    Ref: emacs-tree-sitter/elisp-tree-sitter#250
    Co-authored-by: meedstrom <meedstrom@users.noreply.github.com>

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/tree-sitter/packages.el
 
-(package! tree-sitter :pin "3cfab8a0e945db9b3df84437f27945746a43cc71")
-(package! tree-sitter-langs :pin "b7895ca759563f3c7c3b928eb4f816bb4099d866")
+(package! tree-sitter :pin "02fe7b86d92b1aab954045146469b7893f0ab371")
+(package! tree-sitter-langs :pin "a06804e13fc1d24d0099b44ce23d6ac5e6bac5b8")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
 (when (modulep! :editor evil +everywhere)
