3431ddd44c105083a2eccfa454b159f466750d0d
Henrik Lissner <git@henrik.io>
2024-06-23 15:46:34 -0400

    refactor!(ophints): replace volatile-highlights w/ goggles
    BREAKING CHANGE: This replaces volatile-highlights.el with goggles.el,
    since the former is no longer maintained and the latter is a much
    simpler implementation for the same functionality. No adjustments are
    needed for end-users to adopt this change, unless they have
    volatile-highlights-specific configuration.
    
    Ref: https://github.com/orgs/doomemacs/projects/5/views/9?pane=issue&itemId=326184
    Close: #7810
    Co-authored-by: Dev380 <Dev380@users.noreply.github.com>

--- modules/ui/ophints/packages.el
+++ modules/ui/ophints/packages.el
@@ -3,4 +3,4 @@
 
 (if (modulep! :editor evil)
     (package! evil-goggles :pin "34ca276a85f615d2b45e714c9f8b5875bcb676f3")
-  (package! volatile-highlights :pin "fcf6e2778454ce514c189a7d1fe70e03ad81c325"))
+  (package! goggles :pin "41d3669d7ae7b73bd39d298e5373ece48b656ce3"))
