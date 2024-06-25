cc7a509e10785dba94f899a0d428d01e6b8bb078
Henrik Lissner <git@henrik.io>
2024-06-25 19:33:15 -0400

    bump: :editor fold
    emacs-tree-sitter/ts-fold@6ce584d1ac39 -> emacs-tree-sitter/ts-fold@5b8abb69cdef

--- modules/editor/fold/packages.el
+++ modules/editor/fold/packages.el
@@ -7,5 +7,5 @@
 (when (modulep! :editor evil)
   (package! evil-vimish-fold :pin "b6e0e6b91b8cd047e80debef1a536d9d49eef31a"))
 (when (modulep! :tools tree-sitter)
-  (package! ts-fold :pin "6ce584d1ac39f1e2bc2f66f815964e3047bd6f16"
+  (package! ts-fold :pin "5b8abb69cdef3128f1fdb69176b9963b13ceaf02"
     :recipe (:host github :repo "emacs-tree-sitter/ts-fold")))
