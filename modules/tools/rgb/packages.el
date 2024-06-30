4d2eeab7f024300c77ab5c3c3142029c0404ecac
Henrik Lissner <git@henrik.io>
2024-06-30 17:24:07 -0400

    module: remove :tools rgb
    This module has been deprecated for some time; it's too niche, too
    trivial, and is mostly centered around a single hydra (official support
    for which I'll be phasing out of Doom).

--- modules/tools/rgb/packages.el
+++
@@ -1,5 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; tools/rgb/packages.el
-
-(package! rainbow-mode :pin "0740f31f300982534183a2f60b1918de418a6f2c")
-(package! kurecolor :pin "fbf98e1e384dd11bbb5764ae54b36353f0691d1e")
--- templates/init.example.el
+++ templates/init.example.el
@@ -102,7 +102,6 @@
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
