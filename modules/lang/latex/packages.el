76845a2ea8f21f15e561201122cfa8d0a3800e7c
Henrik Lissner <git@henrik.io>
2024-07-11 23:13:09 -0400

    bump: :lang latex
    emacs-straight/adaptive-wrap@dea4e32c18d2 -> emacs-straight/adaptive-wrap@f5bc153273f4
    emacs-straight/auctex@764a53c8e931 -> emacs-straight/auctex@451b0f08b04d
    
    - Thankfully, emacs-straight/auctex@451b0f08b04d removed the make step
      for building tex-site.el, but it also meant that anyone who has
      unpinned auctex would get file-missing errors for tex-site.el.in.
    
    Fix: #7925
    Ref: emacs-straight/auctex@451b0f08b04d

--- modules/lang/latex/packages.el
+++ modules/lang/latex/packages.el
@@ -3,41 +3,9 @@
 
 (package! auctex
   :recipe (:files ("*.el" "*.info" "dir"
-                   "doc" "etc" "images" "latex" "style")
-           ;; HACK: Auctex has a post-install step that generates tex-site.el
-           ;;   and *.texi documentation files, which largely assumes the user
-           ;;   is on a Linux system with various shell tools like sed, gnu
-           ;;   make, tex, etc. -- an assumption I can't safely make about
-           ;;   Doom's users, so reinvent its 'make tex-site.el' task in elisp:
-           :pre-build
-           (with-temp-file "tex-site.el"
-             (insert-file-contents "tex-site.el.in")
-             (while (re-search-forward "@\\(\\(?:AUCTEX\\|lisp\\)[^@]+\\)@" nil t)
-               (pcase (match-string 1)
-                 ("AUCTEXVERSION"
-                  (replace-match (with-temp-buffer
-                                   (insert-file-contents "auctex.el" nil 0 1024)
-                                   (save-match-data
-                                     (if (re-search-forward "^;; Version: \\([^\n-]+\\)" nil t)
-                                         (match-string-no-properties 1)
-                                       "Unknown")))
-                                 t t))
-                 ("AUCTEXDATE"
-                  (when-let* ((time (cdr (doom-call-process "git" "log" "-n1" "--pretty=tformat:%ct")))
-                              (time (string-to-number time)))
-                    (replace-match (format-time-string "%Y-%m-%d %T" time) t t)))
-                 ("lispautodir"
-                  (replace-match
-                   (prin1-to-string
-                    `(if (file-writable-p "/usr/local/var/auctex")
-                         "/usr/local/var/auctex"
-                       ,(file-name-concat doom-data-dir "auctex")))
-                   t t))
-                 ((or "lisppackagedatadir" "lisppackagelispdir")
-                  (replace-match "(directory-file-name (file-name-directory load-file-name))" t t))
-                 (it (error "Unknown substitution variable in tex-site.el.in: %s" it))))))
-  :pin "764a53c8e93150f0edd169593a4d453810792abe")
-(package! adaptive-wrap :pin "dea4e32c18d285a6ca9f72b1eadd61e27a555ed3")
+                   "doc" "etc" "images" "latex" "style"))
+  :pin "451b0f08b04d66ff4ffd2a5fac4ee646730c0720")
+(package! adaptive-wrap :pin "f5bc153273f4c3cd03966dbf568dd921b01323ec")
 (package! latex-preview-pane :pin "5297668a89996b50b2b62f99cba01cc544dbed2e")
 (when (modulep! :editor evil +everywhere)
   (package! evil-tex :pin "2a3177c818f106e6c11032ac261f8691f5e11f74"))
