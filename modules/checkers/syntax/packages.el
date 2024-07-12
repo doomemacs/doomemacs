0f5864a0e014d80e45588bdc75082e7efc1029bc
Prashant Vithani <prashantvithani@gmail.com>
2024-07-12 16:02:47 +0530

    bump: :checkers syntax
    flycheck/flycheck@02148c6ce7edb0fd0986460db327cc9463939747 -> flycheck/flycheck@e8d1472aeab6ac4e19c8339e6be93e91e878f819

--- modules/checkers/syntax/packages.el
+++ modules/checkers/syntax/packages.el
@@ -2,7 +2,7 @@
 ;;; checkers/syntax/packages.el
 
 (unless (modulep! +flymake)
-  (package! flycheck :pin "02148c6ce7edb0fd0986460db327cc9463939747")
+  (package! flycheck :pin "e8d1472aeab6ac4e19c8339e6be93e91e878f819")
   (package! flycheck-popup-tip :pin "ef86aad907f27ca076859d8d9416f4f7727619c6")
   (when (modulep! +childframe)
     (package! flycheck-posframe :pin "19896b922c76a0f460bf3fe8d8ebc2f9ac9028d8")))
