15ea9b6452ea4206d4c0252bfc68237d5b81688c
Henrik Lissner <git@henrik.io>
2024-07-12 20:28:02 -0400

    refactor(gdscript): remove format keybinds
    Rely on :editor format's interface, instead, which is less janky.

--- modules/lang/gdscript/config.el
+++ modules/lang/gdscript/config.el
@@ -38,24 +38,17 @@
 
   (map! :localleader
         :map gdscript-mode-map
-
         (:prefix ("r" . "run")
-         :desc "Open project in Godot" "e" #'gdscript-godot-open-project-in-editor
-         :desc "Run project" "p" #'gdscript-godot-run-project
-         :desc "Run debug" "d" #'gdscript-godot-run-project-debug
-         :desc "Run current scene" "s" #'gdscript-godot-run-current-scene)
-
+         :desc "Open project in Godot"     "e" #'gdscript-godot-open-project-in-editor
+         :desc "Run project"               "p" #'gdscript-godot-run-project
+         :desc "Run debug"                 "d" #'gdscript-godot-run-project-debug
+         :desc "Run current scene"         "s" #'gdscript-godot-run-current-scene)
         (:prefix ("d" . "debug")
-         :desc "Toggle breakpoint" "d" #'gdscript-debug-toggle-breakpoint
+         :desc "Toggle breakpoint"         "d" #'gdscript-debug-toggle-breakpoint
          :desc "Display breakpoint buffer" "b" #'gdscript-debug-display-breakpoint-buffer
-         :desc "Continue execution" "c" #'gdscript-debug-continue
-         :desc "Next" "n" #'gdscript-debug-next
-         :desc "Step" "s" #'gdscript-debug-step)
-
+         :desc "Continue execution"        "c" #'gdscript-debug-continue
+         :desc "Next"                      "n" #'gdscript-debug-next
+         :desc "Step"                      "s" #'gdscript-debug-step)
         (:prefix ("h" . "help")
-         :desc "Browse online API" "b" #'gdscript-docs-browse-api
-         :desc "Browse API at point" "f" #'gdscript-docs-browse-symbol-at-point)
-
-        (:prefix ("f" . "format")
-         :desc "Format buffer" "b" #'gdscript-format-buffer
-         :desc "Format region" "r" #'gdscript-format-region)))
+         :desc "Browse online API"   "b" #'gdscript-docs-browse-api
+         :desc "Browse API at point" "f" #'gdscript-docs-browse-symbol-at-point)))
