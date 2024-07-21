f5b3958331cebf66383bf22bdc8b61cd44eca645
Henrik Lissner <git@henrik.io>
2024-07-21 19:12:21 -0400

    fix(gdscript): make dtrt-indent aware
    This allows dtrt-indent (and commands that rely on it, like
    doom/set-indent-width) to work in gdscript-mode buffers.

--- modules/lang/gdscript/config.el
+++ modules/lang/gdscript/config.el
@@ -36,6 +36,9 @@
   (when (modulep! +lsp)
     (add-hook 'gdscript-mode-local-vars-hook #'lsp! 'append))
 
+  (after! dtrt-indent
+    (add-to-list 'dtrt-indent-hook-mapping-list '(gdscript-mode default gdscript-indent-offset)))
+
   (map! :localleader
         :map gdscript-mode-map
         (:prefix ("r" . "run")
