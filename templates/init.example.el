740aa4d786d14ebc68da5819d463cd70956b0d36
Henrik Lissner <git@henrik.io>
2024-07-29 01:46:23 -0400

    tweak: use :completion corfu by default
    Ref: 559c1eef39e3

--- templates/init.example.el
+++ templates/init.example.el
@@ -21,8 +21,8 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
-       ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
+       ;;company           ; the ultimate code completion backend
+       (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
