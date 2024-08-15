916180a6bfddc9e7ba4c945141d63205af6774a1
Henrik Lissner <git@henrik.io>
2024-08-15 13:11:13 -0400

    fix: better-jumper evil integration
    Fix: #7997
    Amend: ebf0e1ac30f9
    Ref: gilbertw1/better-jumper#26
    Ref: emacs-evil/evil@9cdd55bff829

--- lisp/doom-editor.el
+++ lisp/doom-editor.el
@@ -450,6 +450,8 @@ files, so this replace calls to `pp' with the much faster `prin1'."
   ;; REVIEW Suppress byte-compiler warning spawning a *Compile-Log* buffer at
   ;; startup. This can be removed once gilbertw1/better-jumper#2 is merged.
   (defvar better-jumper-local-mode nil)
+  ;; REVIEW: Remove if/when gilbertw1/better-jumper#26 is addressed.
+  (defvaralias 'evil--jumps-jump-command 'evil--jumps-jumping-backard)
   :init
   (global-set-key [remap evil-jump-forward]  #'better-jumper-jump-forward)
   (global-set-key [remap evil-jump-backward] #'better-jumper-jump-backward)
