doomemacs/doomemacs

commit c0a1b9efc9a89c240fb8a76424c4d227dcbfc2d8

fix(format): prettier: don't override prettier indent config
Apheleia's default config for prettier adds --use-tabs/--tab-width to
force prettier to match Emacs' indent settings, which is fine until a
prettier config file tries to overwrite it and is ignored. As a
compromise (and stopgap solution), I omit these flags if any prettier
settings are set in the current project.

Fix: #7998

modules/editor/format/config.el
@@ -76,4 +76,30 @@ This is controlled by `+format-on-save-disabled-modes'."
   ;; `lsp-mode' or `eglot') that is capable of formatting. Without +lsp, users
   ;; must manually set `+format-with' to `lsp' to use it, or activate
   ;; `+format-with-lsp-mode' in the appropriate modes.
-  (add-to-list 'apheleia-formatters '(lsp . +format-lsp-buffer)))
+  (add-to-list 'apheleia-formatters '(lsp . +format-lsp-buffer))
+
+  ;; Apheleia's default config for prettier passes an explicit --tab-width N to
+  ;; all prettier formatters, respecting your indent settings in Emacs, but
+  ;; overriding any indent settings in your prettier config files. This changes
+  ;; it to omit indent switches if any configuration for prettier is present in
+  ;; the current project.
+  (dolist (formatter '(prettier prettier-css prettier-html prettier-javascript
+                       prettier-json prettier-scss prettier-svelte
+                       prettier-typescript prettier-yaml))
+    (setf (alist-get formatter apheleia-formatters)
+          (append (delete '(apheleia-formatters-js-indent "--use-tabs" "--tab-width")
+                          (alist-get formatter apheleia-formatters))
+                  '(unless (or (cl-loop for file
+                                        in '(".prettierrc"
+                                             ".prettierrc.json"
+                                             ".prettierrc.yml"
+                                             ".prettierrc.yaml"
+                                             ".prettierrc.json5"
+                                             ".prettierrc.js" "prettier.config.js"
+                                             ".prettierrc.mjs" "prettier.config.mjs"
+                                             ".prettierrc.cjs" "prettier.config.cjs"
+                                             ".prettierrc.toml")
+                                        if (locate-dominating-file default-directory file)
+                                        return t)
+                               (assq 'prettier (+javascript-npm-conf)))
+                     (apheleia-formatters-indent "--use-tabs" "--tab-width"))))))

commit 00868fb2065f17e1c010967c8a9d4b063e747e09

fix(format): avoid `+javascript-npm-conf` definition void
`+javascript-npm-conf` is defined in lang/javascript, so c0a1b9ef broke
formatting JSON files when the javascript module isn't enabled in
init.el.

modules/editor/format/config.el
@@ -102,5 +102,6 @@ This is controlled by `+format-on-save-disabled-modes'."
                                                 ".prettierrc.toml")
                                            if (locate-dominating-file default-directory file)
                                            return t)
-                                  (assq 'prettier (+javascript-npm-conf)))
+                                  (and (modulep! :lang javascript)
+                                       (assq 'prettier (+javascript-npm-conf))))
                         (apheleia-formatters-indent "--use-tabs" "--tab-width"))))))))