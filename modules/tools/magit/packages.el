5e70fe169764bd272e73adbe56d67251fe9a886a
Henrik Lissner <git@henrik.io>
2024-08-20 21:41:12 -0400

    bump: :tools magit
    magit/forge@a56eb3cbb27c -> magit/forge@1e7ee99c7f76
    magit/magit@cf94190410ef -> magit/magit@3b37048688e5
    
    Due to a force push upstream, the magit ref we were pinned to no longer
    worked, prompting this bump.
    
    Fix: #8007

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(package! magit :pin "cf94190410ef163fd102cdbcb67f28599d31eabc")
+(package! magit :pin "3b37048688e50ba5897945ce08eeaed34433c9b7")
 (when (modulep! +forge)
-  (package! forge :pin "a56eb3cbb27c61387d35cbff6b036a2c1bc1559d")
+  (package! forge :pin "1e7ee99c7f76034e40210a6fd6007015b1998f6d")
   (package! code-review
     :recipe (:host github
              :repo "doomelpa/code-review"
