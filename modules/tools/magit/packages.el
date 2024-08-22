1b336d05491e1559de3e27e88b23c49f78ae0eec
Henrik Lissner <git@henrik.io>
2024-08-22 11:29:46 -0400

    bump: forge
    magit/forge@1e7ee99c7f76 -> magit/forge@30f181f78552
    
    - Same issue with forge as with magit (see 5e70fe1).
    
    Fix: #8007
    Amend: 5e70fe169764

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-;; NOTE: Always bump this to HEAD~1, not HEAD, because the latest commit on
-;;   magit's melpa branch is auto-generated and moved to HEAD every time there's
-;;   a commit to its main branch.
+;; NOTE: Always bump magit and forge to HEAD~1, not HEAD, because the latest
+;;   commit on their melpa branches are auto-generated and moved to HEAD every
+;;   time there's a commit to its main branch.
 (package! magit :pin "2da34f1317c619ec2dfb9e0d969449261ca7f31f")
 (when (modulep! +forge)
-  (package! forge :pin "1e7ee99c7f76034e40210a6fd6007015b1998f6d")
+  (package! forge :pin "30f181f785522f2debf60945d6b589a65bc415f6")
   (package! code-review
     :recipe (:host github
              :repo "doomelpa/code-review"
