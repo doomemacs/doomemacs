b521492bf445887181860b626ee94be45a8b227e
Henrik Lissner <git@henrik.io>
2024-08-22 15:43:58 -0400

    fix(default): feature-gate deft keybinds
    Close: #7935
    Co-authored-by: edmundmiller <edmundmiller@users.noreply.github.com>

--- modules/config/default/+emacs-bindings.el
+++ modules/config/default/+emacs-bindings.el
@@ -170,9 +170,10 @@
 
        :desc "Toggle last org-clock"          "c" #'+org/toggle-last-clock
        :desc "Cancel current org-clock"       "C" #'org-clock-cancel
-       :desc "Open deft"                      "d" #'deft
+       (:when (modulep! :ui deft)
+        :desc "Open deft"                     "d" #'deft)
        (:when (modulep! :lang org +noter)
-        :desc "Org noter"                    "e" #'org-noter)
+        :desc "Org noter"                     "e" #'org-noter)
 
        :desc "Find file in notes"             "f" #'+default/find-in-notes
        :desc "Browse notes"                   "F" #'+default/browse-notes
