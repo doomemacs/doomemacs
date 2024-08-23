4bc4b54eb9971ff0abf5884bc0e5360c3d0f70a3
Henrik Lissner <git@henrik.io>
2024-08-23 18:04:27 -0400

    bump: :lang php
    emacs-php/composer.el@42cf9848d438 -> emacs-php/composer.el@791a7104be2e
    emacs-php/php-mode@4792988a120d -> emacs-php/php-mode@59814bd80c59
    jwiegley/emacs-async@cff2bd0be3c7 -> jwiegley/emacs-async@f317b0c9c3e6

--- modules/lang/php/packages.el
+++ modules/lang/php/packages.el
@@ -5,10 +5,10 @@
 (package! php-extras
   :recipe (:host github :repo "arnested/php-extras")
   :pin "d410c5af663c30c01d461ac476d1cbfbacb49367")
-(package! php-mode :pin "4792988a120d6ac515ba16605278d04cb8be0d69")
+(package! php-mode :pin "59814bd80c59894022bc5950fb3bdf02420e8a89")
 (package! php-refactor-mode :pin "7a794b0618df2882b1bd586fdd698dba0bc5130d")
 (package! phpunit :pin "650a50898de1fa4eeb47360b12aeb1126b2448c2")
-(package! composer :pin "42cf9848d438f8dc4c07ac684a83280ace7bb94c")
+(package! composer :pin "791a7104be2ef2748757a186094c1e8f7f531a01")
 
 (when (modulep! +hack)
   (package! hack-mode
@@ -16,4 +16,4 @@
     :pin "ccf20511f0f2ed45d00d423c703bb91ab6a8b80c"))
 
 ;; For building php-extras
-(package! async :pin "cff2bd0be3c78a2eb76717eed60302972fe9b8c5")
+(package! async :pin "f317b0c9c3e60a959f45d035ed5e31a78f1263ac")
