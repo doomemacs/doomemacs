4f5f9d606555de2f645cb9c6e8367d359ff6bfa9
Henrik Lissner <git@henrik.io>
2024-08-26 17:03:04 -0400

    refactor(evil): remove unused +evil-repeat-keys variable

--- modules/editor/evil/config.el
+++ modules/editor/evil/config.el
@@ -1,13 +1,5 @@
 ;;; editor/evil/config.el -*- lexical-binding: t; -*-
 
-(defvar +evil-repeat-keys (cons ";" ",")
-  "The keys to use for universal repeating motions.
-
-This is a cons cell whose CAR is the key for repeating a motion forward, and
-whose CDR is for repeating backward. They should both be `kbd'-able strings.
-
-Set this to `nil' to disable universal-repeating on these keys.")
-
 (defvar +evil-want-o/O-to-continue-comments t
   "If non-nil, the o/O keys will continue comment lines if the point is on a
 line with a linewise comment.")
