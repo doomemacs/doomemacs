35dd2bb33ced07983e30c03cc7f4b3061089a563
Henrik Lissner <git@henrik.io>
2024-08-28 15:32:42 -0400

    bump: :lang rust
    emacs-rustic/rustic@39423d1cf4fa -> emacs-rustic/rustic@d76568037323
    rust-lang/rust-mode@d00d83d3a207 -> rust-lang/rust-mode@a529a4518120
    
    Fix: #8029
    Ref: emacs-rustic/rustic#3

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rust-mode :pin "d00d83d3a207a5b7c2994392b2781f627e3159ce")
-(package! rustic :pin "39423d1cf4fa054c36bf9577356451f4c06ee148")
+(package! rust-mode :pin "a529a4518120bd1c662edc31b82062f41bbfb990")
+(package! rustic :pin "d765680373234a6c231acf20c76b07422afcfdf9")
