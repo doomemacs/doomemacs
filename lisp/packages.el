c1b5f48f07e41604024bdcbe030ed0fc9abedcb7
Henrik Lissner <git@henrik.io>
2024-08-29 02:47:21 -0400

    bump: :core
    Fuco1/smartparens@ab475c78916d -> Fuco1/smartparens@c7519a1b69f1
    bbatsov/projectile@0163b335a18a -> bbatsov/projectile@8cc2ee8937b8
    emacs-compat/compat@09dce8a193c5 -> emacs-compat/compat@e9203e164903
    emacs-straight/project@093f42a1b612 -> emacs-straight/project@5c77d7893636
    hlissner/emacs-hide-mode-line@bc5d293576c5 -> hlissner/emacs-hide-mode-line@ddd154f1e04d
    jscheid/dtrt-indent@339755e4fb52 -> jscheid/dtrt-indent@a8aa35668480
    rainstormstudio/nerd-icons.el@4322290303f2 -> rainstormstudio/nerd-icons.el@c3d641d8e14b

--- lisp/packages.el
+++ lisp/packages.el
@@ -6,7 +6,7 @@
   :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
 (package! compat
   :recipe (:host github :repo "emacs-compat/compat")
-  :pin "09dce8a193c5a70277512263782b82fa1cba84c0")
+  :pin "e9203e164903a6bb7de3e58aa0d653bbcff9d3d1")
 (package! gcmh
   :pin "0089f9c3a6d4e9a310d0791cf6fa8f35642ecfd9")
 
@@ -21,22 +21,22 @@
   :pin "88e574ae75344e39b436f863ef0344135c7b6517")
 
 ;; doom-ui.el
-(package! nerd-icons :pin "4322290303f2e12efd5685a0d22d76ed76ec7349")
-(package! hide-mode-line :pin "bc5d293576c5e08c29e694078b96a5ed85631942")
+(package! nerd-icons :pin "c3d641d8e14bd11b5f98372da34ee5313636e363")
+(package! hide-mode-line :pin "ddd154f1e04d666cd004bf8212ead8684429350d")
 (package! highlight-numbers :pin "8b4744c7f46c72b1d3d599d4fb75ef8183dee307")
 (package! rainbow-delimiters :pin "f40ece58df8b2f0fb6c8576b527755a552a5e763")
 (package! restart-emacs :pin "1607da2bc657fe05ae01f7fdf26f716eafead02c")
 
 ;; doom-editor.el
 (package! better-jumper :pin "47622213783ece37d5337dc28d33b530540fc319")
-(package! dtrt-indent :pin "339755e4fb5245862737babf7f2c1e3bae1c129c")
+(package! dtrt-indent :pin "a8aa356684804c52f26602d4e315f1306c6f3e59")
 (package! helpful :pin "4ba24cac9fb14d5fdc32582cd947572040e82b2c")
-(package! smartparens :pin "ab475c78916d7b1666a495e3fe9c54b250195637")
+(package! smartparens :pin "c7519a1b69f196050a13e2230b7532893b077086")
 (package! ws-butler :pin "e3a38d93e01014cd47bf5af4924459bd145fd7c4")
 
 ;; doom-projects.el
-(package! projectile :pin "0163b335a18af0f077a474d4dc6b36e22b5e3274")
-(package! project :pin "093f42a1b612eaae0d2bdd475663c14973fe0325")
+(package! projectile :pin "8cc2ee8937b89f1639304cbd2526e85b17135372")
+(package! project :pin "5c77d78936364e2e6e9641af2091fde0bee729ce")
 
 ;; doom-keybinds.el
 (package! general :pin "826bf2b97a0fb4a34c5eb96ec2b172d682fd548f")
