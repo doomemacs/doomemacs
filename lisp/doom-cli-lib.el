76f738462184f6b0dbabe6eb5774ec46d41afcad
Henrik Lissner <git@henrik.io>
2024-09-01 17:26:51 -0400

    fix(cli): "void-function: doom-modules-initialize" error
    This reference to a function that doesn't exist (yet) snuck into
    3bced4d.
    
    Amend: 3bced4dbbe78

--- lisp/doom-cli-lib.el
+++ lisp/doom-cli-lib.el
@@ -1845,7 +1845,7 @@ errors to `doom-cli-error-file')."
     (error "Cannot nest `run!' calls"))
   (doom-run-hooks 'doom-after-init-hook)
   (doom-context-with 'cli
-    (doom-modules-initialize)
+    ;; (doom-modules-initialize)
     (let* ((args (flatten-list args))
            (context (make-doom-cli-context :prefix prefix :whole args))
            (doom-cli--context context)
