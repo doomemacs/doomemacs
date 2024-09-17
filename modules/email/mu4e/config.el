31e2ba97df5de0f89f436090e09b42e8aeec3725
Martin Myrseth <mm@myme.no>
2024-09-17 09:12:30 +0200

    fix(mu4e): org-msg: Fix toggle behavior
    Toggle behavior:
    
    The `(integerp current-prefix-arg)` check didn't appropriately catch
    `C-u`/`SPC u` prefixes. This attempts to use `(prefix-numeric-value
    current-prefix-arg)` instead to normalize these cases.
    
    Variable naming:
    
    I'm not sure if this was supposed to continue to be called
    `+mu4e-compose-org-msg-toggle-next` or if the rename to private
    `+mu4e--compose-org-msg-toggle-next` was intentional and the usage
    wasn't updated accordingly. I see the `mu4e` README still mentions it,
    so I would guess the former and this was a typo.
    
    Issue: #8073
    Relevant change: https://github.com/doomemacs/doomemacs/commit/a78237cc01adeabe0fd4a83ff8b8aca7cb269482#diff-e4ae34fcacdffca06aea528ca819a5b6b2332032b20005392503b6ac0e9d04aaR445

--- modules/email/mu4e/config.el
+++ modules/email/mu4e/config.el
@@ -442,14 +442,14 @@ This should already be the case yet it does not always seem to be."
       (org-msg-set-prop "attachment" (nconc files (list file)))))
 
   ;; HACK: Toggle `org-msg' where sensible.
-  (defvar +mu4e--compose-org-msg-toggle-next t)
+  (defvar +mu4e-compose-org-msg-toggle-next t)
   (defadvice! +mu4e-maybe-toggle-org-msg-a (&rest _)
     :before #'+mu4e/attach-files
     :before #'mu4e-compose-new
     :before #'mu4e-compose-reply
     :before #'mu4e-compose-forward
     :before #'mu4e-compose-resend
-    (when (xor (/= 1 (if (integerp current-prefix-arg) current-prefix-arg 0))
+    (when (xor (/= 1 (prefix-numeric-value current-prefix-arg))
                +mu4e-compose-org-msg-toggle-next)
       (org-msg-mode (if org-msg-mode -1 1))
       (cl-callf not +mu4e-compose-org-msg-toggle-next)))
