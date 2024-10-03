8b9168de6e6a9cabf13d1c92558e2ef71aa37a72
Henrik Lissner <git@henrik.io>
2024-10-03 02:26:28 -0400

    release(modules): 24.11.0-dev
    Ref: 2b39e4136850

--- lisp/doom.el
+++ lisp/doom.el
@@ -204,7 +204,7 @@
   "Current version of Doom Emacs core.")
 
 ;; DEPRECATED: Remove these when the modules are moved out of core.
-(defconst doom-modules-version "24.10.0-pre"
+(defconst doom-modules-version "24.11.0-pre"
   "Current version of Doom Emacs.")
 
 (defvar doom-init-time nil
