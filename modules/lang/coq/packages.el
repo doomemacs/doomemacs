03e545592130aa1370049b0953049ec971390ae1
Jérémy Thibault <jeremy.thibault@mpi-sp.org>
2024-10-31 15:23:08 +0100

    fix(coq): add `+no-opam` flag
    Some Rocq users don't use OPAM, so I add a new flag
    that can be used to turn off loading features that
    depend on `opam`, such as `opam-switch-mode`.

--- modules/lang/coq/packages.el
+++ modules/lang/coq/packages.el
@@ -4,4 +4,5 @@
 (package! proof-general :pin "3a99da275523c8f844fdfa3dd073295eece939f3")
 (package! company-coq :pin "5affe7a96a25df9101f9e44bac8a828d8292c2fa")
 
-(package! opam-switch-mode :pin "1069e56a662f23ea09d4e05611bdedeb99257012")
+(unless (modulep! +no-opam)
+  (package! opam-switch-mode :pin "1069e56a662f23ea09d4e05611bdedeb99257012"))
