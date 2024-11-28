a39dd36e97076459e6155158d3593dfd0ce78fc5
Henrik Lissner <git@henrik.io>
2024-11-28 16:17:40 -0500

    refactor: move templates/ to static/
    To better reflect what this directory is intended to store (i.e. not
    just templates).

--- static/packages.example.el
+++ static/packages.example.el
@@ -2,7 +2,7 @@
 ;;; $DOOMDIR/packages.el
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect.
+;; on the command line, then restart Emacs for the changes to take effect -- or
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
