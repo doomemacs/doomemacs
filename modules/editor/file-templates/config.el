52d1c208d431c3f2f190d1171820a2db07de6165
dmr <dradetsky@gmail.com>
2024-12-01 16:23:51 +0000

    feat(file-templates): add apply method
    Factor template application code into command file-templates-apply.
    This allows it to be invoked by the user.

--- modules/editor/file-templates/config.el
+++ modules/editor/file-templates/config.el
@@ -146,9 +146,13 @@ must be non-read-only, empty, and there must be a rule in
        (not (file-exists-p buffer-file-name))  ; ...is a new file
        (not (buffer-modified-p))    ; ...hasn't been modified
        (null (buffer-base-buffer))  ; ...isn't an indirect clone
-       (when-let (rule (cl-find-if #'+file-template-p +file-templates-alist))
-         (apply #'+file-templates--expand rule))))
+       (+file-templates/apply)))
 
+(defun +file-templates/apply ()
+  "Actually expand a file template if one exists"
+  (interactive)
+  (when-let* ((rule (cl-find-if #'+file-template-p +file-templates-alist)))
+    (apply #'+file-templates--expand rule)))
 
 ;;
 ;;; Bootstrap
