2feeeff9f5d888c4d3ccb0d824f86eeacf820d32
Zero King <l2dy@icloud.com>
2024-12-30 20:44:45 +0800

    bump: :tools tree-sitter
    emacs-tree-sitter/elisp-tree-sitter@02fe7b86d92b -> emacs-tree-sitter/elisp-tree-sitter@cc12de34803e
    emacs-tree-sitter/tree-sitter-langs@465a64032d45 -> emacs-tree-sitter/tree-sitter-langs@e2ee3f66c621
    meain/evil-textobj-tree-sitter@5056ebc23149 -> meain/evil-textobj-tree-sitter@bce236e5d2cc

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/tree-sitter/packages.el
 
-(package! tree-sitter :recipe (:branch "master") :pin "02fe7b86d92b1aab954045146469b7893f0ab371")
-(package! tree-sitter-langs :pin "465a64032d45f51347a9b68a0e8f33d658039172")
+(package! tree-sitter :recipe (:branch "master") :pin "cc12de34803ee8c7e5376c41ec171dc484762c27")
+(package! tree-sitter-langs :pin "e2ee3f66c62139f4cd4483c4d97ec34cb279df9d")
 (package! tree-sitter-indent :pin "4ef246db3e4ff99f672fe5e4b416c890f885c09e")
 
 (when (modulep! :editor evil +everywhere)
   (package! evil-textobj-tree-sitter
-    :pin "5056ebc231492827c159c878be30b55a42ae68be"))
+    :pin "bce236e5d2cc2fa4eae7d284ffd19ad18d46349a"))
