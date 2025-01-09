b31eef9336ae416c45b4141a35371ca88a465aac
Henrik Lissner <git@henrik.io>
2025-01-09 14:33:25 -0500

    revert: fix(lib): doom-copy: copy first level of records
    Shallow should mean shallow.
    
    Revert: 82cfe98ccc0c

--- lisp/doom-lib.el
+++ lisp/doom-lib.el
@@ -331,10 +331,12 @@ TRIGGER-HOOK is a list of quoted hooks and/or sharp-quoted functions."
 (cl-defgeneric doom-copy (val &optional deep?)
   "Return a (optionally deep) copy of VAL."
   (if (recordp val)  ; `record' specializer not supported until Emacs 30
-      (cl-loop with newval = (copy-sequence val)
-               for idx from 1 to (length (cdr (cl-struct-slot-info (type-of val))))
-               do (aset newval idx (doom-copy (aref newval idx) deep?))
-               finally return newval)
+      (if deep?
+          (cl-loop with newval = (copy-sequence val)
+                   for idx from 1 to (length (cdr (cl-struct-slot-info (type-of val))))
+                   do (aset newval idx (doom-copy (aref newval idx) t))
+                   finally return newval)
+        (copy-sequence val))
     (cl-check-type val (or integer float boolean symbol null))
     val))
 
