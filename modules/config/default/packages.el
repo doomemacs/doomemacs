47ef79b3b3a2379b147bc51a403d29703a0c671a
Henrik Lissner <git@henrik.io>
2025-01-09 23:40:44 -0500

    bump: :config
    abo-abo/avy@be612110cb11 -> abo-abo/avy@933d1f36cca0
    emacs-straight/use-package@fc8449bb593d -> emacs-straight/use-package@e8c659a050e3
    magnars/expand-region.el@e8f4e0fe9c9a -> magnars/expand-region.el@351279272330
    noctuid/link-hint.el@9153eafc7765 -> noctuid/link-hint.el@826993a0ab73

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; config/default/packages.el
 
-(package! avy :pin "be612110cb116a38b8603df367942e2bb3d9bdbe")
-(package! link-hint :pin "9153eafc776549376bb85d9ff555fef83aca8285")
+(package! avy :pin "933d1f36cca0f71e4acb5fac707e9ae26c536264")
+(package! link-hint :pin "826993a0ab736ab09f53a0623fb44edf2182b07c")
 
 (unless (modulep! :editor evil)
   (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-  (package! expand-region :pin "e8f4e0fe9c9a80a6a26e2b438502aba9a799d580"))
+  (package! expand-region :pin "351279272330cae6cecea941b0033a8dd8bcc4e8"))
--- modules/config/use-package/packages.el
+++ modules/config/use-package/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; core/use-package/packages.el
 
-(package! use-package :pin "fc8449bb593de8fce0a8bdd4ee2cb78219cf8c76")
+(package! use-package :pin "e8c659a050e36b100b83d3139c585a2d6c462845")
