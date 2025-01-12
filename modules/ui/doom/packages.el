a7aa65eee432ae72abf850a4d308ee4bb936898e
Henrik Lissner <git@henrik.io>
2025-01-12 13:34:10 -0500

    bump: :ui doom
    doomemacs/themes@3c03f525d5c0 -> doomemacs/themes@e506a8724156
    
    - To include upstream fixes that resolve warnings about invalid 'nil'
      values.

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "3c03f525d5c0ac0859f31231778f97e10a705e0d")
+(package! doom-themes :pin "e506a8724156da3b1e62cb8136265e9705549d04")
 (package! solaire-mode :pin "c9334666bd208f3322e6118d30eba1b2438e2bb9")
