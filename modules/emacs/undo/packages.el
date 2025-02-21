38dbb1a25bfb215913714b5cb1bf37c031cc7daf
Henrik Lissner <git@henrik.io>
2025-02-21 14:32:11 -0500

    bump: :emacs
    emacs-straight/vundo@d5869788a06f -> emacs-straight/vundo@791211a89fc9

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -10,4 +10,4 @@
     :recipe (:host github :repo "emacsmirror/undo-fu-session")
     :pin "beb0e285d074145eaf481a959c903b77c19ae91e")
   (when (> emacs-major-version 27)  ; unsupported in 27
-    (package! vundo :pin "d5869788a06f08d652d4786574ab546b71d57964")))
+    (package! vundo :pin "791211a89fc992383bbef2322e587d305f2428bc")))
