04cd16a5cdec736bec12f44decd83424e13a3a81
Henrik Lissner <git@henrik.io>
2025-02-24 23:37:33 -0500

    bump: package-lint
    purcell/package-lint@HEAD -> purcell/package-lint@21edc6d0d0ea
    
    - Fixes invalid-read-syntax errors associated with flycheck until
      purcell/package-lint#285 is dealt with upstream.
    
    Fix: #8286
    Ref: purcell/package-lint#285

--- modules/checkers/syntax/packages.el
+++ modules/checkers/syntax/packages.el
@@ -4,6 +4,8 @@
 (unless (modulep! +flymake)
   (package! flycheck :pin "86715c0f293738623f632d6f54a7d1be78b1c48f")
   (package! flycheck-popup-tip :pin "ef86aad907f27ca076859d8d9416f4f7727619c6")
+  ;; REVIEW: Remove when purcell/package-lint#285 is dealt with.
+  (package! package-lint :pin "21edc6d0d0eadd2d0a537f422fb9b7b8a3ae6991")
   (when (modulep! +childframe)
     (package! flycheck-posframe :pin "19896b922c76a0f460bf3fe8d8ebc2f9ac9028d8")))
 
