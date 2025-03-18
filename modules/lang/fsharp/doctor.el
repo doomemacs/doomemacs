28e61bad7b269d8ec37771c2783a5ea3b5918064
ncihnegn <ncihnegn@users.noreply.github.com>
2025-03-17 23:02:08 -0700

    Add macOS support

--- modules/lang/fsharp/doctor.el
+++ modules/lang/fsharp/doctor.el
@@ -1,5 +1,5 @@
 ;;; lang/fsharp/doctor.el -*- lexical-binding: t; -*-
 
 (when (require 'fsharp-mode nil t)
-  (unless (cl-some #'fsharp-mode--executable-find '("fsharpc" "fsc"))
+  (unless (or (cl-some #'fsharp-mode--executable-find '("fsharpc" "fsc")) (zerop (car (doom-call-process "dotnet" "fsi" "--version"))))
     (warn! "Cannot find the F# compiler. Most features will not work.")))
