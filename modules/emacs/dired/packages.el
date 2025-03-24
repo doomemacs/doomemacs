960b537cf67abfdceae65ad1f4b063f99064539f
Henrik Lissner <git@henrik.io>
2025-03-23 23:04:54 -0400

    bump: :emacs dired
    alexluigit/dirvish@f449754dd591 -> alexluigit/dirvish@1d8de07f4e1b
    
    Fix: #8253

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -3,5 +3,5 @@
 
 (package! dirvish
   :recipe (:host github :repo "alexluigit/dirvish")
-  :pin "f449754dd591524f2be8fe58f8ab7f0298dbe11b")
+  :pin "1d8de07f4e1b24b2dbe1435263d84a7864b1ef47")
 (package! diredfl :pin "fe72d2e42ee18bf6228bba9d7086de4098f18a70")
