0163aef765968e4ce36daca0019e772e9b1ca06e
Henrik Lissner <git@henrik.io>
2025-03-24 18:10:52 -0400

    bump: :tools magit
    magit/forge@33e240d360b8 -> magit/forge@1c904090dfdc
    magit/magit@f52dfada8fa0 -> magit/magit@28d272ce0bce

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(package! magit :pin "f52dfada8fa0fa6cd70886819868d84e198773a3") ; 4.3.0
+(package! magit :pin "28d272ce0bcecc2e312d22ed15a48ad4cea564eb") ; 4.3.1
 (when (modulep! +forge)
-  (package! forge :pin "33e240d360b8e3950e9c8b7024e3e763465c0c13") ; 0.4.7
+  (package! forge :pin "1c904090dfdcd201d9170997052c43846ddce149") ; 0.4.8
   (package! code-review
     :recipe (:host github
              :repo "doomelpa/code-review"
