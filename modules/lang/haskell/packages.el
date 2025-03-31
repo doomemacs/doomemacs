d8bbe03d0c7f3c8d970049ae15826b7291ae3940
Henrik Lissner <git@henrik.io>
2025-03-30 21:30:27 -0400

    bump: :lang haskell
    emacs-lsp/lsp-haskell@b2edf1a9f8ae -> emacs-lsp/lsp-haskell@cd0f5d251c14
    haskell/haskell-mode@7f452cc9e6c3 -> haskell/haskell-mode@be2639592f0f
    
    Close: #8332
    Ref: haskell/haskell-mode#1867
    Co-authored-by: ElleNajt <ElleNajt@users.noreply.github.com>

--- modules/lang/haskell/packages.el
+++ modules/lang/haskell/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/haskell/packages.el
 
-(package! haskell-mode :pin "7f452cc9e6c3316b5a4a2b790d3a396f271609d9")
+(package! haskell-mode :pin "be2639592f0fd3c779bcdcec54e2124277baa03f")
 
 (when (and (modulep! +lsp)
            (modulep! :tools lsp -eglot))
-  (package! lsp-haskell :pin "b2edf1a9f8ae97d6fdc566de7706e8c9c0a26606"))
+  (package! lsp-haskell :pin "cd0f5d251c14e90f2896d26d18de8ace462e011b"))
