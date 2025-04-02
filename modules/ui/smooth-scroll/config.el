061189953d1d8955d8b42eef3020fe654e8481f1
Henrik Lissner <git@henrik.io>
2025-04-02 17:23:38 -0400

    perf(smooth-scroll): suppress hl-todo, diff-hl-flydiff, & jit-lock
    While scrolling.

--- modules/ui/smooth-scroll/config.el
+++ modules/ui/smooth-scroll/config.el
@@ -6,4 +6,8 @@
   :hook (doom-first-file . ultra-scroll-mode)
   :init
   (setq scroll-conservatively 101
-        scroll-margin 0))
+        scroll-margin 0)
+  :config
+  (add-hook 'ultra-scroll-hide-functions #'hl-todo-mode)
+  (add-hook 'ultra-scroll-hide-functions #'diff-hl-flydiff-mode)
+  (add-hook 'ultra-scroll-hide-functions #'jit-lock-mode))
