63c29561dfbc04a8d1431c37685e5265954b593d
Henrik Lissner <git@henrik.io>
2025-04-02 17:15:36 -0400

    fix(smooth-scroll): add Emacs 29 check
    This package requires 29+ and will fail otherwise (it depends on
    pixel-scroll-precision-mode).

--- modules/ui/smooth-scroll/config.el
+++ modules/ui/smooth-scroll/config.el
@@ -1,6 +1,7 @@
 ;;; ui/smooth-scroll/config.el -*- lexical-binding: t; -*-
 
 (use-package! ultra-scroll
+  :when (fboundp 'pixel-scroll-precision-mode)
   :hook (doom-first-input . ultra-scroll-mode)
   :hook (doom-first-file . ultra-scroll-mode)
   :init
