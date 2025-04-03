318e6300373f5d67f5d4972897eddb2b3bcff85b
Henrik Lissner <git@henrik.io>
2025-04-03 17:26:48 -0400

    feat(smooth-scroll): add +interpolate flag
    Adds optional scroll interpolation/animation in #8335 via the
    good-scroll package. Command support is light right now, but I plan to
    expand on it later, and I welcome PRs to do so in the meantime.
    
    Ref: #8335

--- modules/ui/smooth-scroll/config.el
+++ modules/ui/smooth-scroll/config.el
@@ -11,3 +11,23 @@
   (add-hook 'ultra-scroll-hide-functions #'hl-todo-mode)
   (add-hook 'ultra-scroll-hide-functions #'diff-hl-flydiff-mode)
   (add-hook 'ultra-scroll-hide-functions #'jit-lock-mode))
+
+
+(use-package good-scroll
+  :when (modulep! +interpolate)
+  :hook (doom-first-input . good-scroll-mode)
+  :config
+  (defun good-scroll--convert-line-to-step (line)
+    (cond ((integerp line) (* line (line-pixel-height)))
+          ((or (null line) (memq '- line))
+           (- (good-scroll--window-usable-height)
+              (* next-screen-context-lines (line-pixel-height))))
+          ((line-pixel-height))))
+
+  (defadvice! good-scroll--scroll-up (&optional arg)
+    :override #'scroll-up
+    (good-scroll-move (good-scroll--convert-line-to-step arg)))
+
+  (defadvice! good-scroll--scroll-down (&optional arg)
+    :override #'scroll-down
+    (good-scroll-move (- (good-scroll--convert-line-to-step arg)))))
--- modules/ui/smooth-scroll/packages.el
+++ modules/ui/smooth-scroll/packages.el
@@ -4,3 +4,6 @@
 (package! ultra-scroll
   :recipe (:host github :repo "jdtsmith/ultra-scroll")
   :pin "b72c507f6702db18d971a6b6bdc692e260f21159")
+
+(when (modulep! +interpolate)
+  (package! good-scroll :pin "a7ffd5c0e5935cebd545a0570f64949077f71ee3"))
