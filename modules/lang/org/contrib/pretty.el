9dfcb5401f5655168bb90fcaec5149e8c159f535
Henrik Lissner <git@henrik.io>
2025-04-04 16:12:23 -0400

    tweak(org): disable org-modern-hide-stars w/ org-indent-mode

--- modules/lang/org/contrib/pretty.el
+++ modules/lang/org/contrib/pretty.el
@@ -11,4 +11,13 @@
 (use-package! org-modern
   :hook (org-mode . org-modern-mode)
   :hook (org-agenda-finalize . org-modern-mode)
-  :hook (org-modern-mode . +org-pretty-mode))
+  :hook (org-modern-mode . +org-pretty-mode)
+  :config
+  ;; HACK: If `org-indent-mode' is active, org-modern's default of hiding
+  ;;   leading stars makes sub-headings look too sunken into the left margin.
+  ;;   Those stars are already "hidden" by `org-hide-leading-stars' anyway, so
+  ;;   rely on just that.
+  (add-hook! 'org-modern-mode-hook
+    (defun +org-modern-show-hidden-stars-in-indent-mode-h ()
+      (when org-indent-mode
+        (setq-local org-modern-hide-stars nil)))))
