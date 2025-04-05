b2252bfba9ed4b72a20895bae5688cb0ad3b8808
pancho horrillo <pancho@pancho.name>
2025-04-05 21:09:08 +0200

    fix(magit): +evil-bindings: replace deprecated fn
    `magit-stage-buffer-file` is an obsolete command (as of Magit 4.3.2);
    use `magit-file-stage` instead.

--- modules/config/default/+evil-bindings.el
+++ modules/config/default/+evil-bindings.el
@@ -528,7 +528,7 @@
         :desc "Magit clone"               "C"   #'magit-clone
         :desc "Magit fetch"               "F"   #'magit-fetch
         :desc "Magit buffer log"          "L"   #'magit-log-buffer-file
-        :desc "Git stage this file"       "S"   #'magit-stage-buffer-file
+        :desc "Git stage this file"       "S"   #'magit-file-stage
         :desc "Git unstage this file"     "U"   #'magit-unstage-buffer-file
         (:prefix ("f" . "find")
          :desc "Find file"                 "f"   #'magit-find-file
