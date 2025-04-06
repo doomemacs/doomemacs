810d304f77b30b3aa9328509595aafe879a44227
flakeuser <2792697+Jdogzz@users.noreply.github.com>
2025-04-05 20:39:01 -0700

    refactor(latex): replace latex-preview-pane w/ auctex-cont-latexmk
    Since latex-preview-pane is unmaintained, replace it with auctex-cont-latexmk.el
    and a simple function to compile the document and open the default viewer.
    
    Close: #3128
    Close: #5249

--- modules/lang/latex/+viewers.el
+++ modules/lang/latex/+viewers.el
@@ -1,9 +1,5 @@
 ;;; lang/latex/+viewers.el -*- lexical-binding: t; -*-
 
-;; Fall back PDF previewing to `latex-preview-pane-mode'.
-(add-to-list 'TeX-view-program-selection '(output-pdf "preview-pane") 'append)
-(add-to-list 'TeX-view-program-list '("preview-pane" latex-preview-pane-mode))
-
 (letf! (defun prepend-to-list (list-var value &optional append)
          (set list-var (delete value (symbol-value list-var)))
          (add-to-list list-var value append))
@@ -48,23 +44,3 @@
            (add-to-list 'TeX-view-program-list '("PDF Tools" TeX-pdf-tools-sync-view)))
          ;; Update PDF buffers after successful LaTeX runs.
          (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer))))))
-
-
-(after! latex-preview-pane
-  (setq latex-preview-pane-multifile-mode 'auctex)
-
-  ;; TODO: PR this to maintained fork by arifer48. The original project appears abandoned.
-  (defadvice! +latex--dont-reopen-preview-pane-a (fn &rest args)
-    "Once the preview pane has been closed it should not be reopened."
-    :around #'latex-preview-pane-update
-    (letf! (defun init-latex-preview-pane (&rest _)
-             ;; HACK Avoid the function because it tries to delete the current
-             ;;      window, but it's already gone, so it ends up deleting the
-             ;;      wrong window.
-             (setq-local latex-preview-pane-mode nil))
-      (apply fn args)))
-
-  (define-key! doc-view-mode-map
-    "ESC" #'delete-window
-    "q"   #'delete-window
-    "k"   (cmd! (quit-window) (delete-window))))
