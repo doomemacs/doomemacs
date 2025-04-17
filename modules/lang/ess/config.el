8eb4e37ffb6730f7260dcd7f407cfe6afb9710a4
Maikol Solís <maikol-solis@users.noreply.github.com>
2025-04-16 22:32:50 -0400

    perf(ess): make REPL *R* buffer more responsive
    Fix: #6455

--- modules/lang/ess/config.el
+++ modules/lang/ess/config.el
@@ -44,6 +44,12 @@
     ;;      have a sane `comment-line-break-function', so...
     comment-line-break-function nil)
 
+  ;; HACK: make the REPL buffer more responsive.
+  (setq-hook! 'inferior-ess-mode-hook
+    comint-scroll-to-bottom-on-input t
+    comint-scroll-to-bottom-on-output t
+    comint-move-point-for-output t)
+
   (map! (:after ess-help
           (:map ess-help-mode-map
             :n "q"  #'kill-current-buffer
