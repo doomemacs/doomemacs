baf680f9c8dc699f458888583423789fd41f8c19
Henrik Lissner <git@henrik.io>
2025-04-16 23:07:44 -0400

    bump: :lang ess
    emacs-ess/ESS@0eb240bcb6d0 -> emacs-ess/ESS@56f355acbd83
    
    Fix: #6455

--- modules/lang/ess/packages.el
+++ modules/lang/ess/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/ess/packages.el
 
-(package! ess :pin "0eb240bcb6d0e933615f6cfaa9761b629ddbabdd")
+(package! ess :pin "56f355acbd835a5a0d65c495e6a80dc01a36d556")
 (package! ess-view-data :pin "dd6a85935bbee0f497d0e8731abdfa07150600b7")
 (package! polymode :pin "74ba75d4bcfbea959ccc9080a95ab9ef759849f2")
 (package! poly-R :pin "8024e852cfca642dea2045a41b2033baa2f1f9a5")
