66f1b25dac30ca97779e8a05e735e14230556492
Henrik Lissner <git@henrik.io>
2025-05-09 21:31:11 -0400

    fix(upload): install ssh-deploy from emacsmirror
    cjohansson/emacs-ssh-deploy has inexplicably vanished, causing `doom
    sync` to fail when trying to install it, so we switch to a mirror.
    
    Fix: #8383

--- modules/tools/upload/packages.el
+++ modules/tools/upload/packages.el
@@ -1,4 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/upload/packages.el
 
-(package! ssh-deploy :pin "dc8882d1806c0fdd635bc625b109179dfa3c929c")
+(package! ssh-deploy
+  :recipe (:host github :repo "emacsmirror/ssh-deploy")
+  :pin "dc8882d1806c0fdd635bc625b109179dfa3c929c")
