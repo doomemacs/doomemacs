44136a66dd16b1a5146a2d650419730a1e99cc43
Henrik Lissner <git@henrik.io>
2025-05-10 13:33:08 -0400

    bump: :emacs dired
    alexluigit/dirvish@e8ec5765da12 -> alexluigit/dirvish@d877433f957a
    
    Fix: #8384

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; emacs/dired/packages.el
 
-(package! dirvish :pin "e8ec5765da1284be88b0cbf190362205a31fb19a")
+(package! dirvish :pin "d877433f957a363ad78b228e13a8e5215f2d6593")
 (package! diredfl :pin "fe72d2e42ee18bf6228bba9d7086de4098f18a70")
