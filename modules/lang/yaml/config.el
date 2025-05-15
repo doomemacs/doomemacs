2556cb58f263cbf2c46f62295a751a81c71e31e8
Henrik Lissner <git@henrik.io>
2025-05-15 18:55:42 +0200

    feat(yaml): add treesit support

--- modules/lang/yaml/config.el
+++ modules/lang/yaml/config.el
@@ -2,10 +2,22 @@
 
 (use-package! yaml-mode
   :mode "Procfile\\'"
-  :init
+  :config
   (when (modulep! +lsp)
     (add-hook 'yaml-mode-local-vars-hook #'lsp! 'append))
-  (when (modulep! +tree-sitter)
-    (add-hook 'yaml-mode-local-vars-hook #'tree-sitter! 'append))
-  :config
   (setq-hook! 'yaml-mode-hook tab-width yaml-indent-offset))
+
+
+(use-package! yaml-ts-mode
+  :when (modulep! +tree-sitter)
+  :when (fboundp 'yaml-ts-mode) ; 29.1+ only
+  :init
+  (set-tree-sitter! 'yaml-mode 'yaml-ts-mode
+    '((yaml :url "https://github.com/tree-sitter-grammars/tree-sitter-yaml"
+            :rev "v0.7.0")))
+  :config
+  ;; HACK: Rely on `major-mode-remap-defaults'.
+  (cl-callf2 rassq-delete-all 'yaml-ts-mode auto-mode-alist)
+
+  (when (modulep! +lsp)
+    (add-hook 'yaml-ts-mode-local-vars-hook #'lsp! 'append)))
---
+++ modules/lang/yaml/doctor.el
@@ -0,0 +1,9 @@
+;;; lang/yaml/doctor.el -*- lexical-binding: t; -*-
+
+(assert! (or (not (modulep! +lsp))
+             (modulep! :tools lsp))
+         "This module requires (:tools lsp)")
+
+(assert! (or (not (modulep! +tree-sitter))
+             (modulep! :tools tree-sitter))
+         "This module requires (:tools tree-sitter)")
