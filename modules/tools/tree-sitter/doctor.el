b5465222573419b8880fd4e442fab4c7274f6c06
Henrik Lissner <git@henrik.io>
2025-05-15 18:55:52 +0200

    docs(tree-sitter): revise doctor output

--- modules/tools/tree-sitter/doctor.el
+++ modules/tools/tree-sitter/doctor.el
@@ -1,8 +1,10 @@
 ;;; tools/tree-sitter/doctor.el -*- lexical-binding: t; -*-
 
 (unless (fboundp 'module-load)
-  (warn! "Emacs was not built with dynamic modules support, which the treesit.el library requires"))
+  (error! "Emacs not built with dynamic modules support"))
 
-(unless (and (fboundp 'treesit-available-p)
-             (treesit-available-p))
-  (error! "Treesit library not available. Did you build Emacs with tree-sitter support?"))
+(if (version< emacs-version "29.1")
+    (error! "Emacs 29.1 or newer is required for tree-sitter support")
+  (unless (and (fboundp 'treesit-available-p)
+               (treesit-available-p))
+    (error! "Emacs not built with tree-sitter support!")))
