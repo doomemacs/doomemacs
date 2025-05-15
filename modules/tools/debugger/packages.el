50200762cdf581e6ce5c230d890673ed396de276
Henrik Lissner <git@henrik.io>
2025-05-15 11:37:07 +0200

    bump: :tools
    doomelpa/code-review@fba8fe334366 -> doomelpa/code-review@303edcfbad81
    emacs-citar/citar@ce5e9644ed02 -> emacs-citar/citar@5ef097beba28
    emacs-lsp/lsp-mode@54251da4ffcf -> emacs-lsp/lsp-mode@c77ba1410639
    emacs-straight/dape@588a907c87d9 -> emacs-straight/dape@8dbe56d7c755
    emacs-straight/eglot@6a9e0c76b9a7 -> emacs-straight/eglot@0b0f89471327
    emacs-tree-sitter/elisp-tree-sitter@12f92ef3992c -> emacs-tree-sitter/elisp-tree-sitter@1c455b0953da
    emacs-tree-sitter/tree-sitter-langs@4eb4989d1a70 -> emacs-tree-sitter/tree-sitter-langs@becd29c756a3
    emacsorphanage/quickrun@a5c9a5e8c674 -> emacsorphanage/quickrun@bae8efb8c5bc
    karthink/gptel@1aa5f1c10df3 -> karthink/gptel@e1050ef6e5ec
    magit/magit@531e7ca6190e -> magit/magit@04ee83d93fab

--- modules/tools/debugger/packages.el
+++ modules/tools/debugger/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/debugger/packages.el
 
-(package! dape :pin "588a907c87d964dbe75fba444fe65f41b4146b91")
+(package! dape :pin "8dbe56d7c7557229e7fda5cca6be39dc712e72ce")
 
 ;; DEPRECATED
 (when (modulep! +lsp)
--- modules/tools/eval/packages.el
+++ modules/tools/eval/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/eval/packages.el
 
-(package! quickrun :pin "a5c9a5e8c674e0279b0bcec4dcac4f7583f1cfe9")
+(package! quickrun :pin "bae8efb8c5bc428e4df731b5c214aae478c707da")
 (when (modulep! +overlay)
   (package! eros :pin "a9a92bdc6be0521a6a06eb464be55ed61946639c"))
