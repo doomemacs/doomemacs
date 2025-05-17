11b4b8d2e5072f1f62df27943ca378c616678a72
Henrik Lissner <git@henrik.io>
2025-05-18 00:24:22 +0200

    revert: transient
    magit/transient@HEAD -> magit/transient@25b994a565ce
    
    Seems to be a source of many issues the past month or so. About time we
    pin it by stable release.
    
    Fix: #8394

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,6 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
+(package! transient :pin "25b994a565ce8035330b0a3071ee430c0282349e") ; 0.8.8
 (package! magit :pin "04ee83d93fabbfbe202e9e7dc781b0dcd4d5b502") ; 4.3.5
 (when (modulep! +forge)
   (package! forge :pin "9db4d386a1ce32b574e413771996d41d9b2407e8") ; 0.5.0
