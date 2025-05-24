9acc5f48b6e2d8b8d29a0d5c07a775cc33904116
Henrik Lissner <git@henrik.io>
2025-05-24 17:18:08 +0200

    feat(zig): add treesit support

--- modules/lang/zig/packages.el
+++ modules/lang/zig/packages.el
@@ -2,3 +2,7 @@
 ;;; lang/zig/packages.el
 
 (package! zig-mode :pin "c46d024733b7c1d6af829bb610fc9629b060bc9e")
+
+(when (and (modulep! +tree-sitter)
+           (fboundp 'treesit-available-p))
+  (package! zig-ts-mode :pin "3898b70d6f72da688e086323fa2922f1542d1318"))
