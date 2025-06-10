442f4db4d43dfea5304dd32fcff39a77ec439e37
Henrik Lissner <git@henrik.io>
2025-06-10 21:28:19 +0200

    bump: :ui
    Alexander-Miller/treemacs@820b09db106a -> Alexander-Miller/treemacs@abd27c86df6b
    dgutov/diff-hl@aa667ac58ab9 -> dgutov/diff-hl@9b032018fda8
    doomemacs/themes@88126db5e63d -> doomemacs/themes@729ad034631c
    jdtsmith/indent-bars@ea74161b4601 -> jdtsmith/indent-bars@87729d685d49
    jdtsmith/ultra-scroll@f2e4fba601b6 -> jdtsmith/ultra-scroll@b447044b9de6
    minad/goggles@6141d31c51dd -> minad/goggles@5176114e99d4
    tarsius/hl-todo@0ce21c329b68 -> tarsius/hl-todo@7ed8bbcadb52

--- modules/ui/smooth-scroll/config.el
+++ modules/ui/smooth-scroll/config.el
@@ -4,9 +4,6 @@
   :when (fboundp 'pixel-scroll-precision-mode)
   :hook (doom-first-input . ultra-scroll-mode)
   :hook (doom-first-file . ultra-scroll-mode)
-  :init
-  (setq scroll-conservatively 101
-        scroll-margin 0)
   :config
   (add-hook 'ultra-scroll-hide-functions #'hl-todo-mode)
   (add-hook 'ultra-scroll-hide-functions #'diff-hl-flydiff-mode)
