197419f041673dcc94cceed700a123af77aadd0c
Henrik Lissner <git@henrik.io>
2025-06-12 15:24:53 +0200

    fix(magit): +forge: fail gracefully on <29.1
    Amend: aa7dd61a2c77

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -3,13 +3,15 @@
 
 (package! transient :pin "f3f498aa155f88c7e2ab6d1d01d1361813059db8") ; 0.9.2
 (package! magit :pin "2f1ff91f128f28aa277e0e060ef44b4be8a989c1") ; 4.3.6
-(when (and (modulep! +forge)
-           ;; code-review depends on forge, and forge depends on ghub, which
-           ;; requires Emacs 29.1+
-           (version<= "29.1" emacs-version))
-  (package! forge :pin "a31859547a1ea5e2acbab67b6b64f90134e2a156") ; 0.5.3
+(when (modulep! +forge)
+  (package! forge
+    :pin "a31859547a1ea5e2acbab67b6b64f90134e2a156" ; 0.5.3
+    ;; forge depends on ghub, which requires Emacs 29.1+
+    :disable (version< emacs-version "29.1"))
   (package! code-review
     :recipe (:host github
              :repo "doomelpa/code-review"
              :files ("graphql" "code-review*.el"))
-    :pin "303edcfbad8190eccb9a9269dfc58ed26d386ba5"))
+    :pin "303edcfbad8190eccb9a9269dfc58ed26d386ba5"
+    ;; ...code-review depends on forge
+    :disable (version< emacs-version "29.1")))
