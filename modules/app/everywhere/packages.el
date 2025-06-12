4d92921b597baa7ecd10930f939438b34a4211c9
Henrik Lissner <git@henrik.io>
2025-06-12 15:28:24 +0200

    bump: :app
    https://git.savannah.gnu.org/git/emms.git@abb4f614dae6 -> https://git.savannah.gnu.org/git/emms.git@e5f46561c3c1
    tecosaur/emacs-everywhere@caeab3948ff0 -> tecosaur/emacs-everywhere@4ec16c12ce1e

--- modules/app/everywhere/packages.el
+++ modules/app/everywhere/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/everywhere/packages.el
 
-(package! emacs-everywhere :pin "caeab3948ff02acec4e4bbc2ff17090f56c7040e")
+(package! emacs-everywhere :pin "4ec16c12ce1eef3c35b5ebb9ddadd928b907f823")
