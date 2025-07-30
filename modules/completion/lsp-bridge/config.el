0b41a4566a3f448869edb5323563425930b9bf3b
Joxos <xujunhao61@163.com>
2025-07-30 22:20:53 +0800

    dev: simplify the configuration

--- modules/completion/lsp-bridge/config.el
+++ modules/completion/lsp-bridge/config.el
@@ -2,12 +2,9 @@
 
 (use-package! lsp-bridge
   :init
-  (setq lsp-bridge-python-command "python")
   (setq lsp-bridge-python-multi-lsp-server "pyright-langserver")
   (setq lsp-bridge-python-multi-lsp-server "pyright_ruff")
-  (setq acm-backend-lsp-enable-auto-import nil)
   :config
   (global-lsp-bridge-mode))
 (after! lsp-bridge
-  (map! :map lsp-bridge-mode-map "RET" 'newline-and-indent)
   (map! :map evil-normal-state-map "C-]" 'lsp-bridge-find-def))
