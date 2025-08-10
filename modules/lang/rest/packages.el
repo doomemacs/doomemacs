dd33275dc2fc68dbd2cff9c76df416e9d8165713
Henrik Lissner <git@henrik.io>
2025-08-10 14:08:17 +0200

    bump: :lang rest
    emacsorphanage/restclient@e2a2b13482d7 -> emacsorphanage/restclient@bba663590f4f
    ljos/jq-mode@eeb86b4d5ad8 -> ljos/jq-mode@3275c3f53fdc
    
    Fix: doomemacs/discussions#14

--- modules/lang/rest/packages.el
+++ modules/lang/rest/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rest/packages.el
 
-(package! restclient :pin "e2a2b13482d72634f8e49864cd9e5c907a5fe137")
+(package! restclient :pin "bba663590f4f34e9b39cc30c9563ca8f829cf574")
 (when (modulep! :completion company)
   (package! company-restclient :pin "e5a3ec54edb44776738c13e13e34c85b3085277b"))
 
 (when (modulep! +jq)
-  (package! jq-mode :pin "eeb86b4d5ad823e97bd19979fcb22d0aa90ff07b")
-  (package! restclient-jq :pin "e2a2b13482d72634f8e49864cd9e5c907a5fe137"))
+  (package! jq-mode :pin "3275c3f53fdc60c8065a8b05395b559ecb9edfaf")
+  (package! restclient-jq :pin "bba663590f4f34e9b39cc30c9563ca8f829cf574"))
