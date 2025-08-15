498b6bdc81d72955aa2950c328a33a83f5d7057b
Leni Aniva <v@leni.sh>
2025-08-15 00:28:06 -0700

    chore: Use spaces instead of tabs

--- modules/lang/lean4/config.el
+++ modules/lang/lean4/config.el
@@ -8,20 +8,19 @@
     (sp-local-pair "«" "»")
     (sp-local-pair "⟨" "⟩")
     (sp-local-pair "⟪" "⟫"))
-	(map! :map lean4-mode-map
-		:localleader
-		:desc "Execute"               "R" #'lean4-execute
-		:desc "Execute in standalone" "r" #'lean4-std-exe
-		:desc "Toggle info buffer"    "t" #'lean4-toggle-info
-		(:prefix ("e" . "Error")
-		 :desc "Previous error"       "p" #'flycheck-previous-error
-		 :desc "Next error"           "n" #'flycheck-next-error
-		 :desc "List error"           "l" #'flycheck-list-errors
-		 )
-		:desc "Lake build"            "b" #'lean4-lake-build
-		(:prefix ("p" . "leanpkg")
-		 :desc "Test"                 "t" #'lean4-leanpkg-test
-		 :desc "Build"                "b" #'lean4-leanpkg-build
-		 :desc "Configure"            "c" #'lean4-leanpkg-configure
-		 )
-		))
+    (map! :map lean4-mode-map
+      :localleader
+      :desc "Execute"               "R" #'lean4-execute
+      :desc "Execute in standalone" "r" #'lean4-std-exe
+      :desc "Toggle info buffer"    "t" #'lean4-toggle-info
+      (:prefix ("e" . "Error")
+        :desc "Previous error"       "p" #'flycheck-previous-error
+        :desc "Next error"           "n" #'flycheck-next-error
+        :desc "List error"           "l" #'flycheck-list-errors
+      )
+      :desc "Lake build"            "b" #'lean4-lake-build
+      (:prefix ("p" . "leanpkg")
+        :desc "Test"                 "t" #'lean4-leanpkg-test
+        :desc "Build"                "b" #'lean4-leanpkg-build
+        :desc "Configure"            "c" #'lean4-leanpkg-configure
+      )))
--- modules/lang/lean4/packages.el
+++ modules/lang/lean4/packages.el
@@ -2,6 +2,6 @@
 ;;; lang/lean4/packages.el
 
 (package! lean4-mode :pin "1388f9d1429e38a39ab913c6daae55f6ce799479"
-	:recipe (:host github
-		:repo "leanprover/lean4-mode"
-		:files ("*.el" "data")))
+  :recipe (:host github
+    :repo "leanprover/lean4-mode"
+    :files ("*.el" "data")))
