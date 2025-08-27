bee45157c6817264af5f766e60cf5ac8a5d28ee0
Henrik Lissner <git@henrik.io>
2025-08-27 17:14:53 +0200

    refactor(elm): remove tree-sitter support
    No *-ts-mode exists for elm-mode. The elm module will fall back to
    font-lock rules so this is not a breaking changing.

--- modules/lang/elm/doctor.el
+++ modules/lang/elm/doctor.el
@@ -1,5 +1,2 @@
 ;;; lang/elm/doctor.el -*- lexical-binding: t; -*-
 
-(assert! (or (not (modulep! +tree-sitter))
-             (modulep! :tools tree-sitter))
-         "This module requires (:tools tree-sitter)")
