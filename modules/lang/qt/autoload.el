93c085fa13fd9a4249328e47e35ecfebb9c223da
Henrik Lissner <git@henrik.io>
2025-08-28 14:00:48 +0200

    feat(qt): add treesit (and lsp) support

--- modules/lang/qt/autoload.el
+++ modules/lang/qt/autoload.el
@@ -2,3 +2,13 @@
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.pr[io]\\'" . qt-pro-mode))
+
+;;;###autoload
+(when (modulep! +tree-sitter)
+  (set-tree-sitter! 'qml-mode 'qml-ts-mode
+    '((qmljs :url "https://github.com/yuja/tree-sitter-qmljs"))))
+
+;;;###autoload
+(when (modulep! +lsp)
+  (add-hook 'qml-mode-local-vars-hook #'lsp! 'append)
+  (add-hook 'qml-ts-mode-local-vars-hook #'lsp! 'append))
