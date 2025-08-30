4b9f23725009d14671f2a5b720a1d73ffbe83c3c
Henrik Lissner <git@henrik.io>
2025-08-30 13:39:39 +0200

    bump: :doom
    bbatsov/projectile@0da59734fbc2 -> bbatsov/projectile@9325c45e0fd9
    emacs-compat/compat@2577cc74d996 -> emacs-compat/compat@cccd41f549fa
    emacs-straight/project@de0deb8dbfb6 -> emacs-straight/project@a0ebb2e9cfda
    emacsmirror/nongnu_elpa@9ee5a7657a22 -> emacsmirror/nongnu_elpa@67c49cfdf5a5
    jscheid/dtrt-indent@015b26d6d6af -> jscheid/dtrt-indent@9108979357e8
    noctuid/general.el@74d4541781d3 -> noctuid/general.el@a48768f85a65
    radian-software/straight.el@4128d4239986 -> radian-software/straight.el@8b3a54b24781
    rainstormstudio/nerd-icons.el@d972dee34939 -> rainstormstudio/nerd-icons.el@6868c05c6eb5

--- lisp/packages.el
+++ lisp/packages.el
@@ -6,7 +6,7 @@
   :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
 (package! compat
   :recipe (:host github :repo "emacs-compat/compat")
-  :pin "2577cc74d996620766adf1c9ec8f44ecbac32e79")
+  :pin "cccd41f549fa88031a32deb26253b462021d7e12")
 (package! gcmh
   :pin "0089f9c3a6d4e9a310d0791cf6fa8f35642ecfd9")
 
@@ -18,16 +18,16 @@
             :branch "develop"
             :local-repo "straight.el"
             :files ("straight*.el"))
-  :pin "4128d423998696d03cca2acb6b8828e907e8e0d9")
+  :pin "8b3a54b247811d146954ddff881273438eaa9932")
 
 ;; doom-ui.el
-(package! nerd-icons :pin "d972dee349395ffae8fceae790d22fedc8fe08e8")
+(package! nerd-icons :pin "6868c05c6eb56c6625ee7fa38450b514542ab636")
 (package! hide-mode-line :pin "ddd154f1e04d666cd004bf8212ead8684429350d")
 (package! restart-emacs :pin "1607da2bc657fe05ae01f7fdf26f716eafead02c")
 
 ;; doom-editor.el
 (package! better-jumper :pin "b1bf7a3c8cb820d942a0305e0e6412ef369f819c")
-(package! dtrt-indent :pin "015b26d6d6af9465c1dc48ef721db119ecd78437")
+(package! dtrt-indent :pin "9108979357e8c9a1015baa5d37c0b596e2dda11b")
 (package! smartparens :pin "b629b4e893ba21ba5a381f6c0054bb72f8e96df2")
 (package! ws-butler
   ;; REVIEW: emacsmirror/nongnu_elpa serves this package from a branch. To stop
@@ -37,12 +37,12 @@
            :repo "emacsmirror/nongnu_elpa"
            :branch "elpa/ws-butler"
            :local-repo "ws-butler")
-  :pin "9ee5a7657a22e836618813c2e2b64a548d27d2ff")
+  :pin "67c49cfdf5a5a9f28792c500c8eb0017cfe74a3a")
 
 ;; doom-projects.el
-(package! projectile :pin "0da59734fbc23fc26222a7d03f6671b3116b0b77")
-(package! project :pin "de0deb8dbfb65eae6eaaf45aea77f12aa9a25f89")
+(package! projectile :pin "9325c45e0fd96d5421e75ad901a91ee5353e10ad")
+(package! project :pin "a0ebb2e9cfda339d8ebda77447c9d822fe313c2d")
 
 ;; doom-keybinds.el
-(package! general :pin "74d4541781d3a01933dda298eb932c06e67413f9")
+(package! general :pin "a48768f85a655fe77b5f45c2880b420da1b1b9c3")
 (package! which-key :pin "38d4308d1143b61e4004b6e7a940686784e51500")
