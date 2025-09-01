a9338d2a4ab24ce3644154ba7aa49eced171c8ce
Henrik Lissner <git@henrik.io>
2025-09-02 00:16:33 +0200

    docs(lsp): doctor: warn about +booster w/o +eglot

--- modules/tools/lsp/doctor.el
+++ modules/tools/lsp/doctor.el
@@ -8,5 +8,7 @@
     (warn! "Couldn't find npm. `lsp-mode' needs npm to auto-install some LSP servers. For more information, see https://emacs-lsp.github.io/lsp-mode/page/languages/.")))
 
 (when (modulep! +booster)
-  (unless (executable-find "emacs-lsp-booster")
-    (warn! "Couldn't find emacs-lsp-booster executable.")))
+  (if (modulep! +eglot)
+      (unless (executable-find "emacs-lsp-booster")
+        (warn! "Couldn't find emacs-lsp-booster executable."))
+    (error! "+booster does nothing without +eglot")))
