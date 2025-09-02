75c3df7e87d3a76454fca0216c4a5d2b5e907b0c
Henrik Lissner <git@henrik.io>
2025-09-02 02:57:51 +0200

    bump: gptel-magit
    ragnard/gptel-magit@f27c01821b67 -> ArthurHeymans/gptel-magit@4a40c3fc201d
    
    Move to ArthurHeymans/gptel-magit fork until ragnard/gptel-magit#7 is
    merged.
    
    Fix: #8484
    Ref: ragnard/gptel-magit#7

--- modules/tools/llm/packages.el
+++ modules/tools/llm/packages.el
@@ -8,4 +8,8 @@
   :pin "495b5e0b5348dbced1448bd12cbf8847e30b5175")
 
 (when (modulep! :tools magit)
-  (package! gptel-magit :pin "f27c01821b67ed99ddf705c2b995f78b71394d8b"))
+  (package! gptel-magit
+    ;; REVIEW: Revert to upstream if ragnard/gptel-magit#7 is merged.
+    :recipe (:host github
+             :repo "ArthurHeymans/gptel-magit")
+    :pin "4a40c3fc201d60d2f0589c2e1a6693fd94bb4c98"))
