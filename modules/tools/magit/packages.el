69ecefa4d6eb268786426b09fae3a891e7ad149b
Henrik Lissner <git@henrik.io>
2025-09-14 11:20:55 -0400

    bump: :tools magit
    magit/forge@a31859547a1e -> magit/forge@bbecd8947a19
    magit/ghub@97a07691efad -> magit/ghub@d76cdac69f9a
    magit/magit@5b820a1d1e94 -> magit/magit@dc0094bd88a5
    magit/transient@aa32e0d66cc3 -> magit/transient@1d109f34b7c6
    
    Fix: #8440

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,15 +1,15 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(package! transient :pin "aa32e0d66cc389befed7a8e8df9439d92a729daa") ; 0.9.4
-(package! magit :pin "5b820a1d1e94649e0f218362286d520d9f29ac2c") ; 4.3.8
+(package! transient :pin "1d109f34b7c6af7af830c141bb0a3b4d33803e75") ; 0.10.0
+(package! magit :pin "dc0094bd88a5307fdfa1c2a48f3ec5b33891f1f0") ; 4.4.0
 (when (modulep! +forge)
   (package! forge
-    :pin "a31859547a1ea5e2acbab67b6b64f90134e2a156" ; 0.5.3
+    :pin "bbecd8947a190894570b8344490a50ebf0efd394" ; 0.6.0
     ;; forge depends on ghub, which requires Emacs 29.1+
     :disable (version< emacs-version "29.1"))
   (package! ghub
-    :pin "97a07691efad6fc16bc000a35be80d4f8dae251a" ; 4.3.2
+    :pin "d76cdac69f9afb5c462c88bdc37c6192b43ac615" ; 5.0.0
     ;; ghub requires Emacs 29.1+
     :disable (version< emacs-version "29.1"))
   (package! code-review
