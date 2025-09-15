64802ae654c622afbea707f40fc855e0e3ae2636
Henrik Lissner <git@henrik.io>
2025-09-15 17:06:31 -0400

    nit(ligatures): packages.el: add missing comment header

--- modules/ui/ligatures/packages.el
+++ modules/ui/ligatures/packages.el
@@ -1,5 +1,7 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/ligeratures/packages.el
+
 (when (and (or (featurep 'ns)
                (featurep 'harfbuzz))
            (featurep 'composite))
-  (package! ligature
-    :pin "6ac1634612dbd42f7eb81ecaf022bd239aabb954"))
+  (package! ligature :pin "6ac1634612dbd42f7eb81ecaf022bd239aabb954"))
