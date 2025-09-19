31c43cdceb5d1390246414d9f081ef9ffe02416c
Troy Brown <brownts@troybrown.dev>
2025-09-19 00:45:15 -0400

    fix(ada): use gpr-specific modes and indent properly
    GNAT Project modes (`gpr-mode` and `gpr-ts-mode`) exist to handle .gpr files.
    These modes are now used instead of the Ada major mode.
    
    Lines may need to be re-indented when RET is pressed.  This is to handle cases
    of incomplete syntax and ambiguity in what may be entered when an empty line is
    initially indented.  Re-indenting after text has been entered corrects
    incorrectly guessed initial indentation.  To accommodate this scenario, RET is
    remapped to `reindent-then-newline-and-indent`.
    
    Also updates documentation to reflect these changes.

--- modules/lang/ada/packages.el
+++ modules/lang/ada/packages.el
@@ -4,3 +4,6 @@
 (package! ada-mode :pin "ce8a2dfebc2b738f32b61dbe2668f7acb885db93")
 (when (and (modulep! +tree-sitter) (treesit-available-p))
   (package! ada-ts-mode :pin "d0c1c124b236b402b884188948cb1f3502ef8779"))
+(package! gpr-mode :pin "03141c6b9a39c31a6e759b594b41617b97b02753")
+(when (and (modulep! +tree-sitter) (treesit-available-p))
+  (package! gpr-ts-mode :pin "b8aeca2c8fd5ed370dad0676da8f380627c916d5"))
