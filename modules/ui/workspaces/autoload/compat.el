a307383ecb0317d4663ddae05078952fcc90b3f4
Henrik Lissner <git@henrik.io>
2025-09-23 17:10:39 -0400

    fix(workspaces): unbalances parens
    Amend: 243b258f8d1a

--- modules/ui/workspaces/autoload/compat.el
+++ modules/ui/workspaces/autoload/compat.el
@@ -18,11 +18,9 @@ and the newly selected tab."
                           ((zerop tab-number) (1+ from-index))
                           (t tab-number)))
          (to-index (1- (max 1 (min to-number (length tabs)))))
-         (minibuffer-was-active (minibuffer-window-active-p (selected-window)))
          (from-tab (tab-bar--tab))
          (to-tab (nth to-index tabs)))
-
     (funcall fn tab-number)
     (unless (eq from-index to-index)
       (run-hook-with-args 'tab-bar-tab-post-select-functions
-                          from-tab to-tab)))
+                          from-tab to-tab))))
