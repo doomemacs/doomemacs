ead254e15269bf8564625df4c8d2af6690a0df49
Henrik Lissner <git@henrik.io>
2025-11-04 22:17:08 -0500

    bump: :config
    emacs-straight/use-package@29552cf3af75 -> emacs-straight/use-package@c80c883deac6
    noctuid/link-hint.el@826993a0ab73 -> noctuid/link-hint.el@8fda5dcb9caf

--- modules/config/default/packages.el
+++ modules/config/default/packages.el
@@ -2,7 +2,7 @@
 ;;; config/default/packages.el
 
 (package! avy :pin "933d1f36cca0f71e4acb5fac707e9ae26c536264")
-(package! link-hint :pin "826993a0ab736ab09f53a0623fb44edf2182b07c")
+(package! link-hint :pin "8fda5dcb9caff5a3c49d22b82e570ac9e29af7dd")
 
 (unless (modulep! :editor evil)
   (package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
--- modules/config/use-package/packages.el
+++ modules/config/use-package/packages.el
@@ -8,4 +8,4 @@
   :recipe (:files ("bind-key.el"))
   :pin "ec9d0505febe2556b47457355763f5f1408a35ac")
 
-(package! use-package :pin "29552cf3af755f17e297862a7f635c469a28b09f")
+(package! use-package :pin "c80c883deac67a3ff3dda2d2af6d947a15062aff")
