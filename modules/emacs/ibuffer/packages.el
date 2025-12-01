9d13dccf54f289215728646dfc60b44a8a67bff0
Henrik Lissner <git@henrik.io>
2025-12-01 18:41:01 -0500

    bump: :emacs
    emacs-straight/vundo@f57937d7f57e -> emacs-straight/vundo@caad94b3cd11
    emacsmirror/undo-fu-session@99d1b5099f43 -> emacsmirror/undo-fu-session@366717d88f77
    magit/git-modes@7063d6685702 -> magit/git-modes@34b83f341464
    purcell/ibuffer-vc@c516e72ea490 -> purcell/ibuffer-vc@11f2d44a2807
    rmuslimov/browse-at-remote@76aa27dfd469 -> rmuslimov/browse-at-remote@cf0269f3db9e

--- modules/emacs/ibuffer/packages.el
+++ modules/emacs/ibuffer/packages.el
@@ -2,4 +2,4 @@
 ;;; emacs/ibuffer/packages.el
 
 (package! ibuffer-projectile :pin "d012ccf7f1e3abe33e67953a5ce2774ae1437467")
-(package! ibuffer-vc :pin "c516e72ea490b887f3f16af38ee502ac89291d21")
+(package! ibuffer-vc :pin "11f2d44a280728e963d81b28455f1c6091718120")
