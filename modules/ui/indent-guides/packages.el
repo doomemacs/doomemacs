ae48bc323e9117b399be277a880d8b5dc7984a80
Henrik Lissner <git@henrik.io>
2025-12-03 00:02:01 -0500

    revert: indent-bars
    jdtsmith/indent-bars@01856e19628c -> jdtsmith/indent-bars@01856e19628c
    
    Partially reverts the indent-bars bump in 2e16934 due to breaking
    incompatibilities (with other plugins that use face-remapping, like
    solaire-mode or user-configured remappings) caused by the new
    face-remapping facilities introduced in
    jdtsmith/indent-bars@40bb86f47bd8. Until it's resolved upstream, we
    revert to the last known working commit.
    
    Amend: 2e16934f020e
    Ref: jdtsmith/indent-bars@40bb86f47bd8

--- modules/ui/indent-guides/packages.el
+++ modules/ui/indent-guides/packages.el
@@ -3,4 +3,4 @@
 
 (package! indent-bars
   :recipe (:host github :repo "jdtsmith/indent-bars")
-  :pin "01856e19628c13fa27560bdd2b451bd5fae0d982")
+  :pin "c3384ec7ff6250f78b118c452458f1a7121841cf")
