0dfb874b57690dbbddd8167792de5ea9ff9a7d76
Henrik Lissner <git@henrik.io>
2025-12-17 03:21:55 -0500

    bump: cond-let
    tarsius/cond-let@HEAD -> tarsius/cond-let@288b7d365632 (v0.2.0)
    
    Fix: #8606

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -4,7 +4,13 @@
 ;; HACK: Fixes magit/magit#5462. Remove when addressed upstream.
 (defvar magit-auto-revert-mode nil)
 
+;; REVIEW: This file contains pinned dependencies. This goes against our policy
+;;   of only pinning primary packages, but an exception is made because the
+;;   Magit ecosystem seems prone to breakage.
+
 (package! transient :pin "0d3f8d4fb6d41b841126820a06ecc98579bd8265") ; 0.11.0
+(package! cond-let :pin "288b7d36563223ebaf64cb220a3b270bdffb63f1") ; 0.2.0
+
 (package! magit :pin "b828afbb4b45641998fb6483a08effb1efb214e1") ; 4.4.2
 (when (modulep! +forge)
   (package! closql :pin "4a60723ae4bfe809a9affcf3306cb40e0ca1ca6c") ; 2.3.2
