6c26c7b87bff304fc97003a1c3b14b64f02fac5a
Henrik Lissner <git@henrik.io>
2025-12-30 04:42:35 -0500

    feat(lean): add v4 support & +v3 flag
    Close: #7439
    Close: #8614
    Co-authored-by: tani <tani@users.noreply.github.com>

--- modules/lang/lean/config.el
+++ modules/lang/lean/config.el
@@ -1,6 +1,9 @@
 ;;; lang/lean/config.el -*- lexical-binding: t; -*-
 
-(after! lean-mode
+(use-package! lean-mode  ; lean 3 support
+  :when (modulep! +v3)
+  :defer t
+  :config
   (set-lookup-handlers! 'lean-mode
     :definition #'lean-find-definition)
   (sp-with-modes 'lean-mode
@@ -22,6 +25,46 @@
           "t" #'lean-leanpkg-test
           "b" #'lean-leanpkg-build
           "c" #'lean-leanpkg-configure)
+        "f" #'lean-fill-placeholder
+        "h" #'lean-hole
+        "m" #'lean-message-boxes-toggle
+        "e" #'lean-execute))
+
+
+(use-package! nael  ; lean 4 support
+  :defer t
+  :init
+  (add-hook 'nael-mode-hook #'abbrev-mode)
+  (after! org-src
+    (add-to-list 'org-src-lang-modes '("lean" . nael)))
+  (after! markdown-mode
+    (add-to-list 'markdown-code-lang-modes '("lean" . nael-mode)))
+  :config
+  (sp-with-modes 'nael-mode
+    (sp-local-pair "/-" "-/")
+    (sp-local-pair "`" "`")
+    (sp-local-pair "{" "}")
+    (sp-local-pair "«" "»")
+    (sp-local-pair "⟨" "⟩")
+    (sp-local-pair "⟪" "⟫"))
+  (when (modulep! +lsp)
+    (if (modulep! :tools lsp +eglot)
+        (setq nael-prepare-lsp nil)
+      (setq nael-prepare-eglot nil))
+    (add-hook 'nael-mode-local-vars-hook #'lsp! 'append))
+  (map! :map lean-mode-map
+        :localleader
+        "a" #'nael-abbrev-help
+        "b" #'project-build     ; REVIEW: redundant with '<leader> p'?
+        "e" #'eldoc-doc-buffer  ; REVIEW: redundant with +lookup/documentation?
+        (:prefix ("s" . "server")
+                 "r" #'lean-server-restart
+                 "s" #'lean-server-stop
+                 "v" #'lean-server-switch-version)
+        (:prefix ("p" . "leanpkg")
+                 "t" #'lean-leanpkg-test
+                 "b" #'lean-leanpkg-build
+                 "c" #'lean-leanpkg-configure)
         "f" #'lean-fill-placeholder
         "h" #'lean-hole
         "m" #'lean-message-boxes-toggle
--- modules/lang/lean/packages.el
+++ modules/lang/lean/packages.el
@@ -1,4 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/lean/packages.el
 
-(package! lean-mode :pin "99d6a34dc5b12f6e996e9217fa9f6fe4a6af037a")
+(when (modulep! +v3)
+  (package! lean-mode :pin "99d6a34dc5b12f6e996e9217fa9f6fe4a6af037a"))
+
+(package! nael :pin "101726eb47fc2562f49e9e7c1de4fc959229ca0c")
+(when (and (modulep! +lsp)
+           (modulep! :tools lsp -eglot))
+  (package! nael-lsp :pin "101726eb47fc2562f49e9e7c1de4fc959229ca0c"))
