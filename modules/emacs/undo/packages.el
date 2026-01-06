baab9d260d3afcd4ebf2da48ee42842bc4089f42
Henrik Lissner <git@henrik.io>
2026-01-06 00:58:49 -0500

    bump: :emacs
    emacs-straight/vundo@caad94b3cd11 -> emacs-straight/vundo@288d1b06ea92
    emacsmirror/undo-fu-session@366717d88f77 -> emacsmirror/undo-fu-session@58bd7f321e6f
    emacsmirror/undo-fu@545e29459e71 -> emacsmirror/undo-fu@17fc25be69c0
    magit/git-modes@34b83f341464 -> magit/git-modes@c3faeeea1982 (v1.4.8)
    rmuslimov/browse-at-remote@cf0269f3db9e -> rmuslimov/browse-at-remote@27b17cc63b9f

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -5,9 +5,9 @@
     (package! undo-tree :pin "d8f72bbe7d3c3a2808986febd3bb1a46d4da7f51")
   (package! undo-fu
     :recipe (:host github :repo "emacsmirror/undo-fu")
-    :pin "545e29459e71a9aca81c96c1385d43a5696e27e9")
+    :pin "17fc25be69c06225b5b74f65e7ff6c212919bd3f")
   (package! undo-fu-session
     :recipe (:host github :repo "emacsmirror/undo-fu-session")
-    :pin "366717d88f77182f780829f2041f1a16ca2970c7")
+    :pin "58bd7f321e6fb2a359d32f23a962fd1d74966314")
   (when (> emacs-major-version 27)  ; unsupported in 27
-    (package! vundo :pin "caad94b3cd11cb7fe4179b9d9e9811686e8a716b")))
+    (package! vundo :pin "288d1b06ea9283852640bb49532e57ae0515492f")))
--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,9 +5,9 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "cf0269f3db9e968c819b1d85b33d791c20c2e495")
+(package! browse-at-remote :pin "27b17cc63b9f9dca893425908373251eb9b10f44")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
   :pin "d1346a76122595aeeb7ebb292765841c6cfd417b")
-(package! git-modes :pin "34b83f341464eb0fb53eecbc6199246623aab473")
+(package! git-modes :pin "c3faeeea1982786f78d8c38397dec0f078eaec84") ; v1.4.8
