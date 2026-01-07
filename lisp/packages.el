3e15fb36d7f94f0a218bda977be4d3f5da983a71
Henrik Lissner <git@henrik.io>
2026-01-07 03:05:43 -0500

    bump: :doom
    emacs-compat/compat@ad6a2b82f7bc -> emacs-compat/compat@cf1426019167
    emacs-straight/project@83c91f5c8407 -> emacs-straight/project@a66b0e0987ce
    rainstormstudio/nerd-icons.el@772987a28d64 -> rainstormstudio/nerd-icons.el@d41902fe68a9

--- lisp/packages.el
+++ lisp/packages.el
@@ -6,7 +6,7 @@
   :pin "17cfa1b54800fdef2975c0c0531dad34846a5065")
 (package! compat
   :recipe (:host github :repo "emacs-compat/compat")
-  :pin "ad6a2b82f7bc2ea698b9684501963dfa71ffdc27")
+  :pin "cf142601916763ca6203c953b28055dce8cfd2e5")
 (package! gcmh
   :pin "0089f9c3a6d4e9a310d0791cf6fa8f35642ecfd9")
 
@@ -21,7 +21,7 @@
   :pin "562639bd97cc3f62df24b168b8eb440bcd790650")
 
 ;; doom-ui.el
-(package! nerd-icons :pin "772987a28d6408f840331c52c91d04b623a87048")
+(package! nerd-icons :pin "d41902fe68a94fcf4394f25a89ba2d510bec4461")
 (package! hide-mode-line :pin "ddd154f1e04d666cd004bf8212ead8684429350d")
 (package! restart-emacs :pin "1607da2bc657fe05ae01f7fdf26f716eafead02c")
 
@@ -31,7 +31,7 @@
 
 ;; doom-projects.el
 (package! projectile :pin "9325c45e0fd96d5421e75ad901a91ee5353e10ad")
-(package! project :pin "83c91f5c8407b7ba8376fa966b1e573517a74e9d")
+(package! project :pin "a66b0e0987ce5b81a36d81cc43eed2c9dc0bdeeb")
 
 ;; doom-keybinds.el
 (package! general :pin "a48768f85a655fe77b5f45c2880b420da1b1b9c3")
