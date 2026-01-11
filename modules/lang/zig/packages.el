c58cf242faf64e95be80aa1109a1f3a6015d6d1f
Henrik Lissner <git@henrik.io>
2026-01-11 14:41:50 -0500

    bump: :lang zig
    meow_king/zig-ts-mode@3898b70d6f72 -> meow_king/zig-ts-mode@89b52c865c64
    
    Fixes tree-sitter-based syntax highlighting in zig-ts-mode (which was
    updated to use tree-sitter-grammars/tree-sitter-zig in
    meow_king/zig-ts-mode@e0fcb1b115ad).
    
    Fix: doomemacs/community#59

--- modules/lang/zig/packages.el
+++ modules/lang/zig/packages.el
@@ -4,4 +4,4 @@
 (package! zig-mode :pin "20e395f940afe1e19e965050b0284ec418d6a9d5")
 
 (when (and (modulep! +tree-sitter) (treesit-available-p))
-  (package! zig-ts-mode :pin "3898b70d6f72da688e086323fa2922f1542d1318"))
+  (package! zig-ts-mode :pin "89b52c865c64d4f887c94445e3283486cf358782"))
