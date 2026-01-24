6f40ad55f555f6169d248d6f62f330629b5336b7
Henrik Lissner <git@henrik.io>
2026-01-23 20:43:46 -0500

    docs: discourage after! and use-package! use
    There's little reason for users to use these macros in their private
    configs over plain ol' `with-eval-after-load` and `use-package`, unless
    they're writing their own modules.
    
    It's my fault for signal boosting them in documentation and whenever I'm
    asked for help, because beginners now believe they are somehow
    required for Doom to work correctly (there are guides out there
    telling beginners that migrating to Doom involves replacing all
    instances of `with-eval-after-load` and `use-package` in their
    pre-existing configs with `after!` and `use-package!` -- which was never
    true).
    
    What's more, I plan to replace `use-package`, internally, so the
    `use-package!` macro won't exist for much longer.

--- static/config.example.el
+++ static/config.example.el
@@ -44,23 +44,22 @@
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;; `with-eval-after-load' block, otherwise Doom's defaults may override your
+;; settings. E.g.
 ;;
-;;   (after! PACKAGE
+;;   (with-eval-after-load 'PACKAGE
 ;;     (setq x y))
 ;;
 ;; The exceptions to this rule:
 ;;
 ;;   - Setting file/directory variables (like `org-directory')
 ;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;     package is loaded (see 'C-h v VARIABLE' to look them up).
 ;;   - Setting doom variables (which start with 'doom-' or '+').
 ;;
 ;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
