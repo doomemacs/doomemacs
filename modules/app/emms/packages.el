9ffaac485dfbfe9b2aadd38c9041426d2c2d3b71
Henrik Lissner <git@henrik.io>
2026-02-10 18:15:42 -0500

    bump: :app
    https://git.savannah.gnu.org/git/emms.git@47c68835b213 -> https://git.savannah.gnu.org/git/emms.git@1d48a1133db2
    skeeto/elfeed@a39fb78e34ee -> skeeto/elfeed@66cfe43dfc61

--- modules/app/emms/packages.el
+++ modules/app/emms/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/emms/packages.el
 
-(package! emms :pin "47c68835b213e79c3651b81a6579a114d2450a52")
+(package! emms :pin "1d48a1133db2670d839f2cf2aff17d6c32aeb15f")
--- modules/app/rss/packages.el
+++ modules/app/rss/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/rss/packages.el
 
-(package! elfeed :pin "a39fb78e34ee25dc8baea83376f929d7c128344f")
+(package! elfeed :pin "66cfe43dfc61d0b56d9e4e72aba3dfab0ed4bdf7")
 (package! elfeed-goodies :pin "544ef42ead011d960a0ad1c1d34df5d222461a6b")
 (when (modulep! +org)
   (package! elfeed-org :pin "34c0b4d758942822e01a5dbe66b236e49a960583"))
