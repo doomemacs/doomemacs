39253bcee670716a7d3c7d2302589a4b9e089096
Henrik Lissner <git@henrik.io>
2026-02-11 17:48:12 -0500

    bump: :emacs
    emacsmirror/undo-fu-session@58bd7f321e6f -> emacsmirror/undo-fu-session@34ae31308d2f
    emacsmirror/undo-fu@17fc25be69c0 -> emacsmirror/undo-fu@b4ce5ed20c1c
    rmuslimov/browse-at-remote@27b17cc63b9f -> rmuslimov/browse-at-remote@38e5ffd77493

--- modules/emacs/undo/packages.el
+++ modules/emacs/undo/packages.el
@@ -5,9 +5,9 @@
     (package! undo-tree :pin "d8f72bbe7d3c3a2808986febd3bb1a46d4da7f51")
   (package! undo-fu
     :recipe (:host github :repo "emacsmirror/undo-fu")
-    :pin "17fc25be69c06225b5b74f65e7ff6c212919bd3f")
+    :pin "b4ce5ed20c1cf0591e497e6998a7634a172726fa")
   (package! undo-fu-session
     :recipe (:host github :repo "emacsmirror/undo-fu-session")
-    :pin "58bd7f321e6fb2a359d32f23a962fd1d74966314")
+    :pin "34ae31308d2f290f87a330c76a627b7fe8ebb720")
   (when (> emacs-major-version 27)  ; unsupported in 27
     (package! vundo :pin "288d1b06ea9283852640bb49532e57ae0515492f")))
