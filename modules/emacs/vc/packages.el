8ea3dc9d0e2c5363dbc0fdb73d16b6bd763b37e2
Henrik Lissner <git@henrik.io>
2026-02-11 17:48:12 -0500

    bump: :emacs
    emacsmirror/undo-fu-session@58bd7f321e6f -> emacsmirror/undo-fu-session@34ae31308d2f
    emacsmirror/undo-fu@17fc25be69c0 -> emacsmirror/undo-fu@b4ce5ed20c1c
    rmuslimov/browse-at-remote@27b17cc63b9f -> rmuslimov/browse-at-remote@38e5ffd77493

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -5,7 +5,7 @@
 (package! vc-annotate :built-in t)
 (package! smerge-mode :built-in t)
 
-(package! browse-at-remote :pin "27b17cc63b9f9dca893425908373251eb9b10f44")
+(package! browse-at-remote :pin "38e5ffd77493c17c821fd88f938dbf42705a5158")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
