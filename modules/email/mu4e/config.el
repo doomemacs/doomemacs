54852119ac5d06da7388f0140bfc2b316cee16b5
Henrik Lissner <git@henrik.io>
2026-02-13 18:23:13 -0500

    tweak(mu4e): remove +mu4e-headers-frame-maybe
    Changing the size of the frame is a mote more intrusive than I'd like
    any Doom module to be.
    
    Fix: #5870
    Close: #8674

--- modules/email/mu4e/config.el
+++ modules/email/mu4e/config.el
@@ -190,9 +190,6 @@ is non-nil."
       nerd-icons-yellow)
     "Faces to use when coloring folders and account stripes.")
 
-  (defvar +mu4e-min-header-frame-width 120
-    "Minimum reasonable with for the header view.")
-
   ;; Add a column to display what email account the email belongs to,
   ;; and an account color stripe column
   (defvar +mu4e-header--maildir-colors nil)
@@ -262,20 +259,6 @@ is non-nil."
             (end-of-line)
             (insert (read-string "Subject (optional): "))
             (message "Sending..."))))))
-
-  ;; The header view needs a certain amount of horizontal space to actually show
-  ;; you all the information you want to see so if the header view is entered
-  ;; from a narrow frame, it's probably worth trying to expand it
-  (defvar +mu4e-min-header-frame-width 120
-    "Minimum reasonable with for the header view.")
-  (add-hook! 'mu4e-headers-mode-hook
-    (defun +mu4e-widen-frame-maybe ()
-      "Expand the mu4e-headers containing frame's width to `+mu4e-min-header-frame-width'."
-      (dolist (frame (frame-list))
-        (when (and (string= (buffer-name (window-buffer (frame-selected-window frame)))
-                            mu4e-headers-buffer-name)
-                   (< (frame-width) +mu4e-min-header-frame-width))
-          (set-frame-width frame +mu4e-min-header-frame-width)))))
 
   ;; Fix columns misalignment in Headers buffers
   (add-hook! 'mu4e-headers-mode-hook
