doomemacs/doomemacs

commit 1b8ea914de5b1ce8e1a699b5859032e5a1dc4804

fix: only defer site-lisp in non-interactive sessions
And no-op on Emacs 31, which has changed startup order such that
site-lisp is loaded *before* early-init (and is therefore out of our
hands).

lisp/doom.el
@@ -859,29 +859,21 @@ appropriately against `noninteractive' or the `cli' context."
 
         ;; Ensure the CLI framework is ready.
         (require 'doom-cli)
-        (add-hook 'doom-cli-initialize-hook #'doom-finalize)))
+        (add-hook 'doom-cli-initialize-hook #'doom-finalize)
 
-    ;; HACK: I suppress loading of site files here to load them manually later.
-    ;;   Why? To suppress the otherwise unavoidable output they commonly produce
-    ;;   (like deprecation notices, file-loaded messages, and linter warnings).
-    ;;   This output pollutes Emacs' log and the output of doom's CLI (or
-    ;;   scripts derived from it) with potentially confusing or alarming -- but
-    ;;   always unimportant and rarely actionable -- information to the user. To
-    ;;   see that output, turn on debug mode!
-    (let ((site-loader
-           (lambda ()
-             (quiet!!
-               (unless interactive?
-                 (require 'cl nil t))  ; "Package cl is deprecated"
-               (unless site-run-file
-                 (when-let* ((site-file (get 'site-run-file 'initial-value)))
-                   (let ((inhibit-startup-screen inhibit-startup-screen))
-                     (setq site-run-file site-file)
-                     (load site-run-file t))))))))
-      (if interactive?
-          (define-advice startup--load-user-init-file (:before (&rest _) load-site-files 100)
-            (funcall site-loader))
-        (funcall site-loader)))
+        ;; HACK: bin/doom suppresses site-lisp to silence otherwise the
+        ;;   unavoidable output it produces (like deprecation notices,
+        ;;   file-loaded messages, and linter warnings). This output can confuse
+        ;;   or alarm consumers Doom-derived CLIs, and are rarely important or
+        ;;   actionable for the end-user. Turn on debug mode if you actually
+        ;;   want to see it!
+        (quiet!!
+          (require 'cl nil t)   ; "Package cl is deprecated"
+          (unless site-run-file
+            (when-let* ((site-file (get 'site-run-file 'initial-value)))
+              (let ((inhibit-startup-screen inhibit-startup-screen))
+                (setq site-run-file site-file)
+                (load site-run-file t)))))))
 
     ;; A last ditch opportunity to undo hacks or do extra configuration before
     ;; the session is complicated by user config and packages.

commit 290003ae7afd7b9a951beab22669cbc86da2d71e

fix: loading site-lisp in interactive sessions
1b8ea91 accidentally inhibited site-lisp in all sessions.

Amend: 1b8ea914de5b
Fix: #8682

lisp/doom.el
@@ -525,16 +525,6 @@ uses a straight or package.el command directly).")
     ;;   later (see `startup--load-user-init-file@undo-hacks').
     (advice-add #'tool-bar-setup :override #'ignore)
 
-    ;; PERF,UX: site-lisp files are often obnoxiously noisy (emitting output
-    ;;   that isn't useful to end-users, like load messages, deprecation
-    ;;   notices, and linter warnings). Displaying these in the minibuffer
-    ;;   causes unnecessary redraws at startup which can impact startup time
-    ;;   drastically and cause flashes of white. It also pollutes the logs. I
-    ;;   suppress it here and load it myself, later, in a more controlled way
-    ;;   (see `doom-initialize').
-    (put 'site-run-file 'initial-value site-run-file)
-    (setq site-run-file nil)
-
     (define-advice startup--load-user-init-file (:around (fn &rest args) undo-hacks 95)
       "Undo Doom's startup optimizations to prep for the user's session."
       (unwind-protect (apply fn args)
@@ -861,19 +851,16 @@ appropriately against `noninteractive' or the `cli' context."
         (require 'doom-cli)
         (add-hook 'doom-cli-initialize-hook #'doom-finalize)
 
-        ;; HACK: bin/doom suppresses site-lisp to silence otherwise the
-        ;;   unavoidable output it produces (like deprecation notices,
-        ;;   file-loaded messages, and linter warnings). This output can confuse
-        ;;   or alarm consumers Doom-derived CLIs, and are rarely important or
-        ;;   actionable for the end-user. Turn on debug mode if you actually
-        ;;   want to see it!
+        ;; HACK: site-lisp files can be obnoxiously noisy (emitting output that
+        ;;   can pollute logs and isn't useful to (and may even alarm)
+        ;;   end-users, like file load messages, deprecation notices, and linter
+        ;;   warnings). bin/doom suppresses site-lisp in its shebang line so we
+        ;;   can load it here with output suppressed (unless debug mode is on).
         (quiet!!
           (require 'cl nil t)   ; "Package cl is deprecated"
           (unless site-run-file
-            (when-let* ((site-file (get 'site-run-file 'initial-value)))
-              (let ((inhibit-startup-screen inhibit-startup-screen))
-                (setq site-run-file site-file)
-                (load site-run-file t)))))))
+            (let ((inhibit-startup-screen inhibit-startup-screen))
+              (load "site-start" t))))))
 
     ;; A last ditch opportunity to undo hacks or do extra configuration before
     ;; the session is complicated by user config and packages.