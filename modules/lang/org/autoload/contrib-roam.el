c59c7680be3bd3d4885402d8e1f1bc33517788a7
Tim Ruffing <me@real-or-random.org>
2026-03-17 11:49:59 +0100

    docs(org): +roam: update outdated docstring
    Amend: d92883bff8ff

--- modules/lang/org/autoload/contrib-roam.el
+++ modules/lang/org/autoload/contrib-roam.el
@@ -71,7 +71,7 @@ If there's none, return an empty string."
   "Open or close roam backlinks buffer depending on visible org-roam buffers.
 
 Intended to be added to `doom-switch-buffer-hook' in `org-roam-find-file-hook'.
-Controlled by `+org-roam-open-buffer-on-find-file'."
+Controlled by `+org-roam-auto-backlinks-buffer'."
   (when (and +org-roam-auto-backlinks-buffer
              (not org-roam-capture--node)  ; not for roam capture buffers
              (not org-capture-mode)        ; not for capture buffers
