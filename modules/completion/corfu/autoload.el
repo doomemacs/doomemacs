aeb81e81a71e5a16154b2b3cc23ecc33cbf8cae3
Henrik Lissner <git@henrik.io>
2026-03-23 15:17:26 -0400

    fix(corfu): +corfu/toggle-auto-complete: void-function corfu--auto-post-command
    Ref: minad/corfu@037d756a861e
    Amend: d71cfe163715

--- modules/completion/corfu/autoload.el
+++ modules/completion/corfu/autoload.el
@@ -62,8 +62,8 @@
     (with-current-buffer buf
       (when corfu-mode
         (if corfu-auto
-            (remove-hook 'post-command-hook #'corfu--auto-post-command 'local)
-          (add-hook 'post-command-hook #'corfu--auto-post-command nil 'local)))))
+            (remove-hook 'post-command-hook #'corfu-auto--post-command 'local)
+          (add-hook 'post-command-hook #'corfu-auto--post-command nil 'local)))))
   (when interactive
     (message "Corfu auto-complete %s" (if corfu-auto "disabled" "enabled")))
   (setq corfu-auto (not corfu-auto)))
