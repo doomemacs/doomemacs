e81228b42dc3787a0e7f5d258ea2b1cfef94deda
Henrik Lissner <git@henrik.io>
2026-03-26 18:59:23 -0400

    refactor(calendar): make calfw-* package contingent on calfw
    Since all these packages are in the same repo, having redundant pins
    makes it harder for users to repin or disable `calfw` (they'd have to
    repin/disable *all* these packages). Now users only need to target
    `calfw`.

--- modules/app/calendar/packages.el
+++ modules/app/calendar/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/calendar/packages.el
 
-(package! calfw :pin "36846cdca91794cf38fa171d5a3ac291d3ebc060")
-(package! calfw-org :pin "36846cdca91794cf38fa171d5a3ac291d3ebc060")
-(package! calfw-cal :pin "36846cdca91794cf38fa171d5a3ac291d3ebc060")
-(package! calfw-ical :pin "36846cdca91794cf38fa171d5a3ac291d3ebc060")
+(when (package! calfw :pin "36846cdca91794cf38fa171d5a3ac291d3ebc060")
+  (package! calfw-org)   ; part of calfw
+  (package! calfw-cal)   ; part of calfw
+  (package! calfw-ical)) ; part of calfw
 (package! org-gcal :pin "0f46c08f60355729526e970e370defe624e84956")
