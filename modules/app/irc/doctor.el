651c713e0cc96db3fdd075ea84854101bbae92ec
Henrik Lissner <git@henrik.io>
2026-03-26 19:14:54 -0400

    revert: docs(irc): add circe notification warning
    A user disabling a package isn't accidental. No need to warn the user
    about it.
    
    Revert: 7833c71e2ed3

--- modules/app/irc/doctor.el
+++
@@ -1,5 +0,0 @@
-;; -*- lexical-binding: t; no-byte-compile: t; -*-
-;;; app/irc/doctor.el
-
-(when (memq 'circe-notifications doom-disabled-packages)
-  (warn! "Circe Notifications has been disabled, You will not receive desktop notifications from IRC channels."))
