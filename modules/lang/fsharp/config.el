d0ee96687a4e55d91ff5311e462510d8d6997aaa
Henrik Lissner <git@henrik.io>
2026-04-02 22:27:10 -0400

    feat(fsharp): add +tree-sitter

--- modules/lang/fsharp/config.el
+++ modules/lang/fsharp/config.el
@@ -1,17 +1,28 @@
 ;;; lang/fsharp/config.el -*- lexical-binding: t; -*-
 
-(after! fsharp-mode
+(defun +fsharp-common-config (mode)
+  (set-formatter! 'fantomas '("fantomas" "--stdin") :modes (list mode))
+  (when (modulep! +lsp)
+    (add-hook (intern (format "%s-local-vars-hook" mode)) #'lsp! 'append)))
+
+
+;;
+;;; Packages
+
+(use-package! fsharp-mode
+  :defer t
+  :config
+  (+fsharp-common-config 'fsharp-mode)
+
+  ;; REVIEW: Most of these can't be directly ported to fsharp-ts-mode, so...?
   (when (executable-find "dotnet")
     (setq inferior-fsharp-program "dotnet fsi --readline-"))
   (if (modulep! +lsp)
-      (progn
-        (setq fsharp-ac-intellisense-enabled nil)
-        (add-hook 'fsharp-mode-local-vars-hook #'lsp! 'append))
+      (setq fsharp-ac-intellisense-enabled nil)
     (setq fsharp-ac-use-popup nil) ; Use a buffer for docs rather than a pop-up
     (set-lookup-handlers! 'fsharp-mode :async t :definition #'fsharp-ac/gotodefn-at-point)
     (set-company-backend! 'fsharp-mode 'fsharp-ac/company-backend))
   (set-repl-handler! 'fsharp-mode #'run-fsharp)
-  (set-formatter! 'fantomas '("fantomas" "--stdin") :modes '(fsharp-mode))
   (set-indent-vars! 'fsharp-mode '(fsharp-indent-offset fsharp-continuation-offset))
   (map! :localleader
         :map fsharp-mode-map
@@ -21,3 +32,15 @@
         (:unless (modulep! +lsp)
          "q" #'fsharp-ac/stop-process
          "t" #'fsharp-ac/show-tooltip-at-point)))
+
+
+(use-package! fsharp-ts-mode
+  :when (modulep! +tree-sitter)
+  :defer t
+  :init
+  (set-tree-sitter! 'fsharp-mode 'fsharp-ts-mode
+    '((fsharp :url "https://github.com/ionide/tree-sitter-fsharp"
+              :rev "v0.2.0"
+              :commit "594c500ecace8618db32dd1144307897277db067")))
+  :config
+  (+fsharp-common-config 'fsharp-ts-mode))
--- modules/lang/fsharp/packages.el
+++ modules/lang/fsharp/packages.el
@@ -2,3 +2,6 @@
 ;;; lang/fsharp/packages.el
 
 (package! fsharp-mode :pin "5212c9359180c0a3c01f22060d9836d5165988a3")
+
+(when (and (modulep! +tree-sitter) (treesit-available-p))
+  (package! fsharp-ts-mode :pin "3817559f052e9693b34efbe02d6650db0ede1b74"))
