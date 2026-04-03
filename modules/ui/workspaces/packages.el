289d89377ba8606ef779d576ef7d7b41f197dc9b
Henrik Lissner <git@henrik.io>
2026-04-03 17:01:22 -0400

    fix(workspaces): freeze persp-mode

--- modules/ui/workspaces/packages.el
+++ modules/ui/workspaces/packages.el
@@ -1,4 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/workspaces/packages.el
 
-(package! persp-mode :pin "40e9993a9711cba5fb56dfec81a507fabeba9668")
+;; REVIEW: persp-mode introduced breaking changes in later commits, but will be
+;;   replaced soon; this is frozen in the meantime.
+(package! persp-mode :pin "40e9993a9711cba5fb56dfec81a507fabeba9668" :freeze t)
