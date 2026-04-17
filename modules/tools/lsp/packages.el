944a70eaa81c54bbb9f499089314889fc8102a1e
Henrik Lissner <git@henrik.io>
2026-04-17 00:19:55 -0400

    bump: :tools lsp
    emacs-lsp/lsp-mode@fb384fed198f -> emacs-lsp/lsp-mode@4c74da7ae511
    emacs-straight/eglot@d3c43da26a08 -> emacs-straight/eglot@9509b7e7e1f1
    
    Also freezes jsonrpc, b/c the latest commit is incompatible with
    eglot@HEAD atm (see 8ee87d8).
    
    Ref: 8ee87d89bdd9

--- modules/tools/lsp/packages.el
+++ modules/tools/lsp/packages.el
@@ -3,8 +3,8 @@
 
 (if (modulep! +eglot)
     (progn
-      (package! eglot :pin "d3c43da26a083ab8cbe37ef6f934532936d573da")
-      (package! jsonrpc :pin "70dc1806ae83abae7c6c9924a29d736d9393c279")
+      (package! eglot :pin "9509b7e7e1f1e4214dc196bafcf56f42570fff9a")
+      (package! jsonrpc :pin "70dc1806ae83abae7c6c9924a29d736d9393c279" :freeze t)
       (when (modulep! :completion vertico)
         (package! consult-eglot :pin "d8b444aac39edfc6473ffbd228df3e9119451b51"))
       (when (modulep! :checkers syntax -flymake)
@@ -19,7 +19,7 @@
   (eval-and-compile (defvar lsp-use-plists t))
 
   (package! lsp-mode
-    :pin "fb384fed198fa9012fd9788220e23e672f7d8619"
+    :pin "4c74da7ae51145f8e49c3544c90b410d96a742fa"
     :env `(("LSP_USE_PLISTS" . ,(and lsp-use-plists "1"))))
   (package! lsp-ui :pin "ff349658ed69086bd18c336c8a071ba15f7fd574")
   (when (modulep! :completion ivy)
