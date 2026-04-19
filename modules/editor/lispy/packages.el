707da6f7e90f26a4e00e5f8f98f29fd08824e71e
Henrik Lissner <git@henrik.io>
2026-04-19 05:10:53 -0400

    bump: :editor lispy
    abo-abo/lispy@fe44efd21573 -> enzuru/lispy@c42d3737c57d
    
    abo-abo/lispy hasn't been updated in 3 years. The fork is maintained and
    has fixed a number of issues.
    
    Fix: #8755

--- modules/editor/lispy/packages.el
+++ modules/editor/lispy/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/lispyville/packages.el
 
-(package! lispy :pin "fe44efd21573868638ca86fc8313241148fabbe3")
+(package! lispy
+  :recipe (:host github :repo "enzuru/lispy")
+  :pin "c42d3737c57de86a5aa321d7efa29390aeedcf6f")
 (when (modulep! :editor evil)
   (package! lispyville :pin "14ee8711d58b649aeac03581d22b10ab077f06bd"))
