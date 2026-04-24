ef7189da041838ea4b719fd8b2e4e16904faf141
Henrik Lissner <git@henrik.io>
2026-04-24 06:47:15 -0400

    fix(magit): update incremental-load list
    Magit no longer depends on dash (which depends on f.el and s.el), and
    now depends on compat.el. Also doesn't seem like package.el is needed.
    
    On the off chance the user installed the magit module without installing
    any other package that depends on these packages, they'll see
    file-missing errors while Doom's incremental loader loads these
    packages.
    
    Ref: magit/magit@e40e8f199447
    Fix: doomemacs/community#98

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -40,7 +40,7 @@ FUNCTION
 
 (use-package! magit
   :commands magit-file-delete
-  :defer-incrementally (dash f s with-editor package eieio transient git-commit)
+  :defer-incrementally (compat with-editor eieio transient git-commit)
   :init
   (setq magit-auto-revert-mode nil)  ; we do this ourselves further down
   ;; Must be set early to prevent ~/.config/emacs/transient from being created
